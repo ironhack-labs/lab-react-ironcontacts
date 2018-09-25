b5777dd8eca5cee7dfccaf9534fe4186149f6c81
alexrojas <alex.rojas@agintechllc.com>
2018-09-25 14:17:59 -0400

    3rd iteration

--- starter-code/src/App.js
+++ starter-code/src/App.js
@@ -8,11 +8,37 @@ class App extends Component {
     super(props)
     this.state = {
       contacts: contacts,
+      selected: contacts.slice(0, 5)
 
+      //
     }
+
+
   }
+  addContact = ()=> {
+    var random= this.state.contacts[Math.floor(Math.random() * contacts.length + 5)]
+    console.log(this.state.selected)
+    var newContacts = this.state.selected
+    newContacts.push(random)
+    this.setState({
+      selected: newContacts
+    })
+  }
+
+
+  sortName = () =>{
+    let contactsOrg = this.state.selected
+    contactsOrg.sort((a, b)=>{
+      return b.popularity - a.popularity
+    })
+    this.setState({
+      selected: contactsOrg
+    })
+  }
+
   render() {
-    var result = this.state.contacts.slice(0, 5).map((contact)=>{
+
+    const result = this.state.selected.map((contact)=>{
       return <tr>
                 <td>
                   <img src={contact.pictureUrl} width="100px" />
@@ -21,7 +47,7 @@ class App extends Component {
                   <h1>{contact.name}</h1>
                 </td>
                 <td>
-                  <h4>{contact.popularity}</h4>
+                  <h4>{contact.popularity.toFixed(2)}</h4>
                 </td>
               </tr>
     })
@@ -29,13 +55,16 @@ class App extends Component {
     return (
       <div className="App">
           <h1>IronContacts</h1>
+          <button onClick={this.addContact}>Add Random Contact</button>
+          <button onClick={this.sortName}>Sort By Name</button>
+
           <table>
             <tr>
               <th>Picture</th>
               <th>Name</th>
               <th>Popularity</th>
             </tr>
-            
+
               {result}
 
 
