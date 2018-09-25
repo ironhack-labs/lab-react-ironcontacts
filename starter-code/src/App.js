ccc90ed0b67fcc7b2b415c229f265584c8c27de6
alexrojas <alex.rojas@agintechllc.com>
2018-09-25 11:52:17 -0400

    first itiration 1 done

--- starter-code/src/App.js
+++ starter-code/src/App.js
@@ -1,18 +1,46 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import contacts from "./contacts.json";
 
 class App extends Component {
+  constructor(props){
+    super(props)
+    this.state = {
+      contacts: contacts,
+
+    }
+  }
   render() {
+    var result = this.state.contacts.slice(0, 5).map((contact)=>{
+      return <tr>
+                <td>
+                  <img src={contact.pictureUrl} width="100px" />
+                </td>
+                <td>
+                  <h1>{contact.name}</h1>
+                </td>
+                <td>
+                  <h4>{contact.popularity}</h4>
+                </td>
+              </tr>
+    })
+
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+          <h1>IronContacts</h1>
+          <table>
+            <tr>
+              <th>Picture</th>
+              <th>Name</th>
+              <th>Popularity</th>
+            </tr>
+            
+              {result}
+
+
+
+          </table>
       </div>
     );
   }
