34fa337b6cf2ad8c0653935e54b1801bc2451e81
Nicholas Crispino <crispinonicky@gmail.com>
2018-09-25 11:57:03 -0400

    first iteration

---
+++ starter-code/src/TopRow.js
@@ -0,0 +1,19 @@
+// import React, { Component } from 'react';
+// import logo from './logo.svg';
+// import './App.css';
+
+// class TopRow extends Component{
+//   render () {
+//     return (
+      
+
+//     <div className="field">
+// <h1>Picture Name Popularity</h1>
+//   </div>
+
+
+//       )
+//   }
+// }
+
+// export default TopRow;
