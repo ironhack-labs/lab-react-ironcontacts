52adfbcf3c0dd7dd7c19ef45345244af606ae008
Dennis <ratenniel@gmail.com>
2018-10-02 17:00:34 +0200

    done with bonus 🤪

--- starter-code/src/components/Table.js
+++ starter-code/src/components/Table.js
@@ -3,26 +3,69 @@ import contacts from '../contacts.json';
 
 const newArr = contacts.splice(0, 5);
 
-export const Table = () => {
-  console.log(contacts)
+
+export class Table extends Component {
+  
+  constructor(){
+    super();
+    this.state = {
+      contacts: newArr
+    }}
+    
+    addContactToArray(e) {
+      newArr.push(e);
+      this.setState({contacts:newArr});
+    }
+
+    sortByName(e) {
+      console.log(e)
+      let copy=e.sort(function(a, b){
+        var x = a.name.toLowerCase();
+        var y = b.name.toLowerCase();
+        if (x < y) {return -1;}
+        if (x > y) {return 1;}
+    });
+    this.setState({contacts:copy});
+    }
+
+    sortByPopularity(e) {
+      console.log(e)
+      e.sort(function(a, b){return a.popularity - b.popularity});
+    this.setState({contacts:e});
+    }
+
+    deleteMovieByIndex(idx){
+      this.state.contacts.splice(idx,1);
+      this.setState({contacts:this.state.contacts});
+  }
+    
+    
+    render() {
+      
+    const randomContact = contacts[Math.floor(Math.random() * contacts.length)];
     return (
       <div className="Table">
-        <table>
+      <button onClick={() => this.addContactToArray(randomContact)}>Add Random Contact</button>
+      <button onClick={() => this.sortByName(newArr)}>Short by Name</button>
+      <button onClick={() => this.sortByPopularity(newArr)}>Short by Popularity</button>
+
+        <table className="dennis">
           <thead>
           <tr>
             <th>Picture</th>
             <th>Name</th> 
             <th>Popularity</th>
+            <th>Action</th>
           </tr>
           </thead>
           <tbody>
-            { newArr.map((oneContact,index) => {
+            { this.state.contacts.map((oneContact,index) => {
               return (
               <tr>
-              {/* <contacts key={index} picture={oneContact.pictureUrl} name={oneContact.name} popularity={oneContact.popularity} /> */}
         <td><img src={oneContact.pictureUrl} alt="" width="100"></img></td>
         <td>{oneContact.name}</td>
         <td>{Math.round(oneContact.popularity * 100) / 100}</td>
+        <td><button onClick={() => this.deleteMovieByIndex(index)}>Delete</button></td>
           </tr>)
             }
             )}
@@ -30,4 +73,5 @@ export const Table = () => {
         </table>
       </div>
     );
+}
 }
