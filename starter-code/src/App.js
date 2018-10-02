f7048f35ba34305d2afa1310d2113ff20ad79237
Gonlos <gonzalo.cortazar.kao@gmail.com>
2018-10-02 15:59:19 +0200

    added random contacts

--- starter-code/src/App.js
+++ starter-code/src/App.js
@@ -5,18 +5,37 @@ import contacts from "./contacts.json";
 import Row from "./rowcontact.js"
 
 
+
+
 class App extends Component {
-  render() {
+   constructor () {
+     super();
+
     let contactsToShow = [];
     for(let i = 0; i < 5; i++){
       contactsToShow.push( <Row{...contacts[i]}></Row>)
     }
+    this.state = {contactsToShow}
+
+  }
+
+  addRandomContact =()=> {
+   let indexRandom = Math.floor(Math.random()*Object.keys(contacts).length + 4)
+   this.state.contactsToShow.push(<Row{...contacts[indexRandom]}></Row>)
+     this.setState(
+       {contactsToShow: this.state.contactsToShow }
+     )
+   }
+
+
+
+  render() {
     return (
       <div className="App">
-
       <h1>IronContacts</h1>
+      <button onClick={this.addRandomContact}>Add Random Contact</button>
       <table>
-          {contactsToShow}
+          {this.state.contactsToShow}
       </table>
       </div>
     );
--- starter-code/src/rowcontact.js
+++ starter-code/src/rowcontact.js
@@ -1,8 +1,5 @@
 import React, { Component } from 'react';
 
-
-
-
 const Row = (props) => {
     return (
      <tr>
