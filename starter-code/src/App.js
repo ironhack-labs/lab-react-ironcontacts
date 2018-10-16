4d6e674ef1edb839b636bd94fa40e85c5c477a05
HananeDUP <hanane.moualil@gmail.com>
2018-10-16 21:55:19 +0200

    iteration 2 done

--- starter-code/src/App.js
+++ starter-code/src/App.js
@@ -1,8 +1,37 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import contacts from './contacts.json';
+
+
+class Contacts extends Component {
+  constructor(props) {
+    super(props);
+  }
+  render() {
+    return (
+      <tr>
+        <td><img src={this.props.picture} /></td>
+        <td>{this.props.name}</td>
+        <td>{this.props.popularity}</td>
+      </tr>
+    )
+
+  }
+}
+
+
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { contacts, compteur:5 }
+    this.handleClick = this.handleClick.bind(this)
+  }
+
+  handleClick() {
+    this.setState({compteur: this.state.compteur+1 })
+  }
   render() {
     return (
       <div className="App">
@@ -13,6 +42,19 @@ class App extends Component {
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
+        <button onClick={this.handleClick}></button>
+        <table>
+          <thead><td>Picture</td><td>Name</td><td>Popularity</td></thead>
+          {
+            this.state.contacts.slice(0, this.state.compteur).map(contact => {
+              return (
+                <Contacts name={contact.name} picture={contact.pictureUrl} popularity={contact.popularity} />
+              )
+            }
+            )
+          }
+
+        </table>
       </div>
     );
   }
