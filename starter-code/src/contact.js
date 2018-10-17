837bfe88e05aead2627b8a8cd915c43e11bd1de8
Kevin Rabesaotra <kevin.rabesaotra@gmail.com>
2018-10-17 08:17:18 +0200

    iteration 4

--- starter-code/src/contact.js
+++ starter-code/src/contact.js
@@ -2,34 +2,54 @@ import React, { Component } from "react";
 import './App.css';
 import contacts from './contacts.json';
 
-
 class Contact extends Component {
     constructor(props) {
         super(props)
         this.state = {
-            listArticle: contacts.sort( () => Math.random() -0.5),
+            contact: contacts.sort(() => Math.random() - 0.5),
             clickCount: 5
         }
     }
     clickHandler = () => {
         this.setState({
             clickCount: this.state.clickCount + 1
-        })   
+        })
     }
-    addRandomContact = () => {
+    sortByName = () => {
+        this.setState({
+            contact: contacts.sort((a, b) => a.name > b.name)//Ne pas mettre de slice à cet endroit, sinon on bloque l'array à une certaine taille
+        })
+    }
 
+    sortByPopularity = () => {
+        this.setState({
+            contact: contacts.sort((a, b) => a.popularity > b.popularity)
+        })
     }
+    
+    deleteSelectedElement = (movieIndex) => {
+        const moviesCopy = this.state.contact
+        moviesCopy.splice(movieIndex, 1)
+        this.setState({
+            clickCount : this.state.clickCount - 1,
+            contact : moviesCopy.slice(this.state.clickCount),
+        })
+    }
+
     render() {
         return (
             <div>
                 <button onClick={this.clickHandler}>Add Random Button</button>
+                <button onClick={this.sortByName}>Sort By Name</button>
+                <button onClick={this.sortByPopularity}>Sort By Popularity</button>
                 {
-                    contacts.slice(0, this.state.clickCount).map((film, key) => {
+                    this.state.contact.slice(0, this.state.clickCount).map((film, key) => {
                         return <article key={key}>
                             <p>Picture</p>
                             <img src={film.pictureUrl}></img>
                             <p>Name : {film.name}</p>
                             <p> Popularity {Math.round(film.popularity * 100) / 100}</p>
+                            <button onClick={this.deleteSelectedElement}>DELETE</button>
                         </article>
                     })
                 }
