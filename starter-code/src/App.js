025e41759533d4acf040089c442cccb1233c9d98
Crazyunicorn <revire.alexandre@gmail.com>
2018-10-18 18:36:01 +0200

    step 1

--- starter-code/src/App.js
+++ starter-code/src/App.js
@@ -1,21 +1,99 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import contacts from './contacts.json'
+
+
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
+  constructor(props) {
+      super(props)
+      this.state = {
+          listContacts: contacts.slice(0, 5)
+
+        //  [contacts[0], contacts[1], contacts[2], contacts[3], contacts[4]]
+      }
+  }
+
+addRandomContact(){
+let newsList =  this.contacts.slice(0, 6);
+
+}
+
+
+ render() {
+
+   return (
+     <div className="App">
+       <header className="App-header">
+         <img src={logo} className="App-logo" alt="logo" />
+         <h1 className="App-title">Welcome to React</h1>
+       </header>
+
+
+       <div className="App">
+
+
+         <RandomButton onClick={this.addRandomContact} title ="Add random"></RandomButton>
+
+
+        <table>
+
+           {
+               this.state.listContacts.map(contact => {
+                   return (
+
+                        <Table name={contact.name} pictureUrl={contact.pictureUrl} popularity={contact.popularity}>
+               </Table>
+                    );
+               })
+           }
+         </table>
+
+       </div>
+
+     </div>
+   );
+ }
+}
+
+
+
+class Table extends Component {
+    render () {
+        return (
+            <div className="table">
+
+                <tr>
+                  <td><h4> {this.props.name}</h4></td>
+                  <td><img src={this.props.pictureUrl}></img></td>
+                  <td><p>{this.props.popularity}</p></td>
+                </tr>
+
+            </div>
+        )
+    }
+}
+
+
+class RandomButton extends Component {
+  constructor(props) {
+      super(props)
+      this.state = {
+
+      }
+  }
+  render(){
+    return(
+
+
+    <button onClick = {this.props.onClick}>{this.props.title}</button>
+
+    )
   }
 }
+
+
+
 
 export default App;
