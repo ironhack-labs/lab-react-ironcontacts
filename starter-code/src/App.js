e5bb0d273cfd9adc8f5dddba59cbbe968c8d1a79
Robert V <vivanco.robert@gmail.com>
2018-10-18 18:47:08 +0200

    iteraction 2

--- starter-code/src/App.js
+++ starter-code/src/App.js
@@ -1,18 +1,57 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+// import logo from './logo.svg';
+import "./App.css";
+import contacts from "./contacts.json";
+var counter = 5;
+
+function selected(counter) {
+  const shuffled = contacts.sort(() => 0.5 - Math.random());
+  return shuffled.slice(0, counter);
+}
+var selectedArray = selected(counter);
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+  }
+
+  state = {
+    counter: 5
+  };
+
+  clickHandler = () => {
+    this.setState({
+      counter: this.state.counter + 1
+    });
+    console.log(this.state.counter);
+  };
+
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <button onClick={this.clickHandler}>Click me</button>
+        <ul>
+          <table>
+            <tr>
+              <td>Picture</td>
+              <td>Name</td>
+              <td>Popularity</td>
+            </tr>
+          </table>
+          {selected(this.state.counter).map((actor, index) => {
+            return (
+              <table>
+                <tr>
+                  <td key1={index}>
+                    <img width="100" src={actor.pictureUrl} />
+                  </td>
+                  <td key2={index}>{actor.name}</td>
+                  <td key3={index}>{actor.popularity}</td>
+                </tr>
+              </table>
+            );
+          })}
+        </ul>
       </div>
     );
   }
