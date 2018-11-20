a118e7450fc82d5193e525bfe2ffa0bb98d714d2
Felicitas Deutges <deutges.ironhack@gmail.com>
2018-11-20 16:21:26 +0100

    Done

--- starter-code/src/App.js
+++ starter-code/src/App.js
@@ -1,18 +1,78 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import contacts from './contacts.json'
 
 class App extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      firstFive: contacts.slice(0,5)
+    }
+  }
+
+  addCelebrity() {
+    const randomIndex = Math.floor(Math.random() * contacts.length);
+    this.setState({
+      firstFive: [...this.state.firstFive, contacts[randomIndex]]
+    })
+
+  }
+
+  sortName() {
+    let sortedArray = this.state.firstFive.slice().sort((a, b) => {
+      return a.name.localeCompare(b.name);
+    })
+    this.setState({
+      firstFive: sortedArray
+    })
+  }
+
+  sortPopularity() {
+    let sortedArray = this.state.firstFive.slice().sort((a, b) => {
+      return b.popularity - a.popularity;
+    })
+    this.setState({
+      firstFive: sortedArray
+    })
+  }
+
+  delete(i) {
+    let newArray = this.state.firstFive.slice();
+    newArray.splice(i, 1);
+    this.setState({
+      firstFive: newArray
+    })
+  }
+
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <h1>IronContacts</h1>
+        <button onClick={this.addCelebrity.bind(this)}>Add Random Contact</button>
+        <button onClick={this.sortName.bind(this)}>Sort By Name</button>
+        <button onClick={this.sortPopularity.bind(this)}>Sort by Popularity</button>
+        <table>
+          <thead>
+            <tr>
+              <th>Picture</th>
+              <th>Name</th>
+              <th>Popularity</th>
+            </tr>
+          </thead>
+
+          <tbody>
+            {this.state.firstFive.map((c, i) => {
+              return(
+              <tr key={c.name}>
+                <td><img src={c.pictureUrl} alt={c.name} style={{height: 50}}/></td>
+                <td>{c.name}</td>
+                <td>{c.popularity}</td>
+                <td><button onClick={this.delete.bind(this, i)}>Delete</button></td>
+              </tr>)
+            })}
+          </tbody>
+        </table>
+       
       </div>
     );
   }
