9d9791255d509408a5dd761a24de547375f42487
Daniel Ferrari <danielferrari@live.com>
2018-11-27 17:18:27 -0200

    text formatting

--- starter-code/src/Components/Row.js
+++ starter-code/src/Components/Row.js
@@ -4,10 +4,10 @@ const row = (props) => {
 
     return (
       <tr>
-      <td><img width="100px" height="150px" src={props.pictureUrl} alt=''/></td>
-      <td>{props.name}</td>
-      <td>{props.popularity}</td>
-      <td><button onClick={props.deleteBtn}>Delete</button></td>
+        <td><img width="100px" height="150px" src={props.pictureUrl} alt=''/></td>
+        <td>{props.name}</td>
+        <td>{props.popularity}</td>
+        <td><button onClick={props.deleteBtn}>Delete</button></td>
       </tr>
     );
 }
