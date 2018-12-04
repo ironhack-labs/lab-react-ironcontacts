6b11b2eb0c95444c3f357c289960fd1ff41fa8b2
aterron84 <aterron84@gmail.com>
2018-12-04 15:42:19 +0100

    Iteration 2

--- starter-code/src/components/ListContacts/ListContacts.js
+++ starter-code/src/components/ListContacts/ListContacts.js
@@ -1,34 +1,50 @@
-import React, { Component } from 'react'
-import contacts from '../../contacts.json';
-import "./ListContacts.css"
+import React, { Component } from "react";
+import contacts from "../../contacts.json";
+import "./ListContacts.css";
 
 export default class ListContacts extends Component {
+  constructor() {
+    super();
+    this.state = {
+      contacts: contacts.slice(0, 5)
+    };
+  }
+
+  addRandomContact = () => {
+    let randomContact = contacts[parseInt(Math.random() * Object.keys(contacts).length)]
+     this.setState({...this.state, contact: this.state.contacts.push(randomContact)});
+  }
+
   render() {
     return (
       <div>
         <h1>IronContacts</h1>
+        <button onClick={this.addRandomContact}>Add Random Contact</button>
         <table>
-            <thead>
+          <thead>
             <tr>
-                <td>Picture</td>
-                <td>Name</td>
-                <td>Popularity</td>
+              <td>Picture</td>
+              <td>Name</td>
+              <td>Popularity</td>
             </tr>
-            </thead>
-            <tbody>
-                
-            </tbody>
-           
-            {contacts.slice(0, 5).map(contact=>
-            <tr>
-                <td><img className="image-contact" src={contact.pictureUrl} alt=""/></td>
-                <td>{contact.name}</td>
-                <td>{parseFloat(contact.popularity).toFixed(2)}</td>           
+          </thead>
+          <tbody />
+
+          {this.state.contacts.map((contact,i) => (
+            <tr key={i}>
+              <td>
+                <img
+                  className="image-contact"
+                  src={contact.pictureUrl}
+                  alt=""
+                />
+              </td>
+              <td>{contact.name}</td>
+              <td>{parseFloat(contact.popularity).toFixed(2)}</td>
             </tr>
-            
-            )}
+          ))}
         </table>
       </div>
-    )
+    );
   }
 }
