4b791a5d853351d1299ac2df4c66843e67646984
Eduardo Gandía Fernández <eduardogandiafernandez@MacBook-Pro-de-Eduardo.local>
2018-12-04 16:17:03 +0100

    Done

--- starter-code/src/App.js
+++ starter-code/src/App.js
@@ -37,6 +37,14 @@ class App extends Component {
     })
   }
 
+  deleteContact = (e) =>{
+    const updatedContacts = [...this.state.contacts]
+    updatedContacts.splice( e.target.parentNode.parentNode.id, 1 )
+    this.setState({
+      ...this.state, contacts: updatedContacts
+    })
+  }
+
   componentWillMount() {
     this.pushContacts(5);
   }
@@ -45,24 +53,28 @@ class App extends Component {
     return (
       <React.Fragment>
         <h1>IronContacts</h1>
-        <button onClick={this.addRandomContact}>Add Random Contact</button>
-        <button onClick={this.sortByName}>Sort by Name</button>
-        <button onClick={this.sortByPopularity}>Sort by popularity</button>        
+        <div>
+          <button onClick={this.addRandomContact}>Add Random Contact</button>
+          <button onClick={this.sortByName}>Sort by Name</button>
+          <button onClick={this.sortByPopularity}>Sort by popularity</button>  
+        </div>      
         <table>
           <thead>
             <tr>
               <th>Picture</th>
               <th>Name</th>
               <th>Popularity</th>
+              <th>Action</th>
             </tr>
           </thead>
 
           <tbody>
             {this.state.contacts.map((elem, i) => (
-                <tr key={i}>
+                <tr key={i} id={i}>
                   <td><img style={{width: 100}} src={elem.pictureUrl} alt={elem.name}/></td>
                   <td>{elem.name}</td>
                   <td>{elem.popularity}</td>
+                  <td><button onClick={this.deleteContact} >Delete</button></td>
                 </tr>
               ))}
           </tbody>
