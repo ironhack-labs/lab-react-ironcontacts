a5cc31ebb3ad667c7eb881ae2dfef725a5c59455
Rodrigo.Munoz <rodrigomunozga@gmail.com>
2018-12-04 16:56:57 +0100

    Iteration 2

--- starter-code/src/App.js
+++ starter-code/src/App.js
@@ -15,12 +15,19 @@ class App extends Component {
     
     }
 
+   contactRandom = () => {
+     const pusher = [...this.state.contacts]
+     pusher.push(contacts[Math.floor(Math.random()* this.state.contacts.length)])
+    this.setState({...this.state, contacts: pusher}) 
+  }
     
 
   render() {
     const someContact = this.state.contacts.map((oneContact,index) => <div key={index}> <img src={oneContact.pictureUrl}/> {oneContact.name} <br></br> {oneContact.popularity}</div>); 
     return (
       <div className="App">
+        <h1>IronContacts</h1>
+        <button onClick={this.contactRandom}></button>
         {someContact}
       </div>
     );
