6595298a0032905dd787fa56195dcd7a51770f5f
Jorge Devesa <jrgdevesa@gmail.com>
2018-12-04 17:00:47 +0100

    A medias de la iteracion 4

--- starter-code/src/components/card/card.jsx
+++ starter-code/src/components/card/card.jsx
@@ -16,6 +16,35 @@ class Card extends Component {
       this.setState({...this.state, contacts: arr})
     console.log()
   }
+  sortByName = ()=>{
+    let contacts = this.state.contacts
+    this.setState({...this.state, contacts:  contacts.sort((a,b)=> {
+      if (a.name > b.name){
+        return 1
+      }
+      if (a.name <b.name){
+        return -1
+      }
+      return 0
+    })})
+  }
+  sortByPopularity = ()=>{
+    let contacts = this.state.contacts
+    this.setState({...this.state, contacts:  contacts.sort((a,b)=> {
+      if (a.popularity > b.popularity){
+        return 1
+      }
+      if (a.popularity <b.popularity){
+        return -1
+      }
+      return 0
+    })})
+  }
+
+  deleteContact = (index)=>{
+    let contacts = this.state.contacts;
+    this.setState({...this.state, contacts: contacts.slice(index)})
+  }
 
   render() {
     const tableFields = this.state.contacts.map((contact, index) => (
@@ -23,17 +52,20 @@ class Card extends Component {
         <th key={index}><img src={contact.pictureUrl}></img></th>
         <th key={index}><h1>{contact.name}</h1></th>
         <th key={index}><h1>{contact.popularity}</h1></th>
+        <th><button onclick={this.deleteContact(index)}>Delete</button></th>
         </tbody>
     ));
     
     return (
       <div>
       <div className="card">
+      <button onClick={this.addRandomCard}>Add random Contact</button>
+      <button onClick={this.sortByName}>Sort by name</button>
+      <button onClick={this.sortByPopularity}>Sort by popularity</button>
         <table>
           {tableFields}
         </table>
       </div>
-      <button onClick={this.addRandomCard}>Add random Contact</button>
       </div>
     );
   }
