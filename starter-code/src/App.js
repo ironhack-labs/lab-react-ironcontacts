ad015386c167ac070e869cbde95324541bf4850d
David <davidmunozvi@icloud.com>
2018-12-04 18:10:05 +0100

    Iteration 3

--- starter-code/src/App.js
+++ starter-code/src/App.js
@@ -12,7 +12,7 @@ class App extends Component {
     }
 }
 randomContact = () => {
-  
+
   let newActors = [...this.state.actors]
   var newContact = contacts[(Math.floor(Math.random()*contacts.length)+1)];
   newActors.push(newContact);
@@ -24,10 +24,47 @@ randomContact = () => {
 
 }
 
+sortName = () => {
+  let sortedNames = [...this.state.actors]
+  function compare(a,b) {
+    if (a.name < b.name)
+      return -1;
+    if (a.name > b.name)
+      return 1;
+    return 0;
+  }
+  sortedNames.sort(compare);
+  this.setState({
+    ...this.state,
+    actors: sortedNames
+  })
+
+}
+
+
+sortPopularity = () => {
+  let sortedPopularity = [...this.state.actors]
+  function compare(a,b) {
+    if (a.popularity < b.popularity)
+      return -1;
+    if (a.popularity > b.popularity)
+      return 1;
+    return 0;
+  }
+  sortedPopularity.sort(compare);
+  this.setState({
+    ...this.state,
+    actors: sortedPopularity
+  })
+
+}
+
   render() {
     return (
       <div className="App">   
       <button onClick={this.randomContact}>Add Random Contact</button>
+      <button onClick={this.sortName}>Sort by Name</button>
+      <button onClick={this.sortPopularity}>Sort by Popularity</button>
    <table>
            <tr>
              <th>Picture</th>
