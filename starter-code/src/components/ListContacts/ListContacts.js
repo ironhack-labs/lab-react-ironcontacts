b1a3833a9b488e622ac31955852bb4aeccd25d5e
aterron84 <aterron84@gmail.com>
2018-12-04 16:15:22 +0100

    Iteration 3

--- starter-code/src/components/ListContacts/ListContacts.js
+++ starter-code/src/components/ListContacts/ListContacts.js
@@ -12,14 +12,40 @@ export default class ListContacts extends Component {
 
   addRandomContact = () => {
     let randomContact = contacts[parseInt(Math.random() * Object.keys(contacts).length)]
-     this.setState({...this.state, contact: this.state.contacts.push(randomContact)});
+    let newContacts = [...this.state.contacts]
+    newContacts.push(randomContact)
+    this.setState({...this.state, contacts: newContacts});
+  }
+
+  sortByName = () =>{
+    let newContactsSorted = [...this.state.contacts]
+    newContactsSorted.sort(function(a,b){
+        if(a.name<b.name){return -1}
+        if(a.name>b.name){return 1}
+        return 0;
+    })
+      this.setState({...this.state, contacts:newContactsSorted})
+  }
+
+  sortByPopularity =() =>{
+      let newSortedByPopoularity = [...this.state.contacts]
+      newSortedByPopoularity.sort(function(a,b){
+          if(a.popularity<b.popularity){return 1}
+          if(a.popularity>b.popularity){return -1}
+          return 0;
+      })
+      this.setState({...this.state, contacts : newSortedByPopoularity})
   }
 
   render() {
     return (
       <div>
         <h1>IronContacts</h1>
+        <div className = "groupButtons">
         <button onClick={this.addRandomContact}>Add Random Contact</button>
+        <button onClick={this.sortByName}>Sort by Name</button>
+        <button onClick={this.sortByPopularity}>Sort by Popularity</button>
+        </div>
         <table>
           <thead>
             <tr>
