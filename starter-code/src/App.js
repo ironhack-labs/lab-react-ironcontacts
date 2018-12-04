daac65946958feb70a29628d67318e86996dc170
cgandiaf <c.gandia.f@gmail.com>
2018-12-04 18:49:23 +0100

    Done sin Delete

--- starter-code/src/App.js
+++ starter-code/src/App.js
@@ -7,47 +7,62 @@ let list = [];
 for (var i = 0; i < 5; i++) {
   list.push(contacts[i])
 }
-let minilist = list.map(celebrity => {
-  console.log(celebrity)
-  return (
-    <Contacts {...celebrity}/>
-  )
-})
 
 class App extends Component {
 
-    constructor() {
-      super();
-      this.state = { contacts: minilist }
-    }
-
-    addRandomContact = () => {
-      let arr = [...this.state.contacts]
-      let idxRd = parseInt(Math.random() * contacts.length);
-      arr.push(<Contacts {...contacts[idxRd]}/>)
-      this.setState({
-        ...this.state, contacts: arr
-      })
-    }
-
-    render() {
-      return (
-        <div className="App">
-          <h1>IronContacts</h1>
-          <button onClick={this.addRandomContact}>Add Random Contact</button>
-          <table>
-            <tr>
-              <th>Picture</th>
-              <th>Name</th>
-              <th>Popularity</th>
-            </tr>
-
-            {this.state.contacts}
-
-          </table>
-        </div>
-      );
-    }
+  constructor() {
+    super();
+    this.state = { contacts: list }
   }
+
+  addRandomContact = () => {
+    let arr = [...this.state.contacts]
+    let idxRd = parseInt(Math.random() * contacts.length);
+    arr.push(contacts[idxRd])
+    this.setState({
+      ...this.state, contacts: arr
+    })
+  }
+  sortByName = () => {
+    let arr = [...this.state.contacts];
+    arr.sort((a, b) => b.name < a.name ? 1 : -1);
+    this.setState({
+      ...this.state, contacts: arr
+    })
+  }
+  sortByPopularity = () => {
+    let arr = [...this.state.contacts];
+    arr.sort(( a, b ) => b.popularity - a.popularity );
+    this.setState({
+      ...this.state, contacts: arr
+    })
+  }
+  
+  render() {
+    return (
+      <div className="App">
+        <h1>IronContacts</h1>
+        <button onClick={this.addRandomContact}>Add Random Contact</button>
+        <button onClick={this.sortByName}>Sort by name</button>
+        <button onClick={this.sortByPopularity}>Sort by popularity</button>
+        <table>
+          <tr>
+            <th>Picture</th>
+            <th>Name</th>
+            <th>Popularity</th>
+          </tr>
+
+          {this.state.contacts.map(celebrity => {
+            console.log(celebrity)
+            return (
+              <Contacts {...celebrity} />
+            )
+          })}
+
+        </table>
+      </div>
+    );
+  }
+}
 
 export default App;
--- starter-code/src/components/Contacts/Contacts.js
+++ starter-code/src/components/Contacts/Contacts.js
@@ -4,25 +4,34 @@ import './Contacts.css';
 
 
 class Contact extends Component {
-   
-       
-       
+
+    delete = (e) => {
+        let arr = [...this.props];
+        console.log(e.target.parentNode.parentNode.id)
+        
+        ///arr.splice(e.target.parentNode.parentNode, 1)
+        this.setState({
+            ...this.state, contacts: arr
+        })
+    }
+
     render() {
-     
-          
-    return (
-      
-        
-        <tr className="Contact">
-        <td><img src={this.props.pictureUrl} width="20%"></img></td>
-        <td>{this.props.name}</td>
-        <td>{this.props.popularity}</td>
-        </tr>
-        
-    
-      
-    );
-  }
+
+
+        return (
+
+
+            <tr className="Contact">
+                <td><img src={this.props.pictureUrl} width="20%"></img></td>
+                <td>{this.props.name}</td>
+                <td>{this.props.popularity}</td>
+                <td><button onClick={this.delete}>Delete</button></td>
+            </tr>
+
+
+
+        );
+    }
 }
 
 export default Contact;
