c4baef7753d3946231b74647a823f0dfd5e2b930
Rodrigo.Munoz <rodrigomunozga@gmail.com>
2018-12-04 15:15:33 +0100

    Primer Commit, names drawed

---
+++ starter-code/src/components/Table/Table.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Table extends Component {
+  render() {
+    return (
+      <div className="table">
+        
+      </div>
+    );
+  }
+}
+
+export default Table;
