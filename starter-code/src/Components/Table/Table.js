c7799cc6370aaf09df4bd81b0425a8871eeedc20
Patricia LG <patri.lopg5@gmail.com>
2018-12-04 16:31:05 +0100

    new ite 2 done

--- starter-code/src/Components/Table/Table.js
+++ starter-code/src/Components/Table/Table.js
@@ -5,11 +5,29 @@ import Actor from "../actor/Actor";
 import contacts from "../../contacts.json";
 
 class Table extends Component {
+
+  constructor () {
+    super()
+    this.state = {
+      contact: contacts.splice(0,5).map((contact)=> contact)
+    }
+  }
+
+  addRandom = () => {
+    const _contact = [...this.state.contact]
+    _contact.push(contacts[Math.floor(Math.random() * contacts.length)])
+    this.setState({...this.state, contact:_contact})
+    console.log(this.state)
+  }
+
+
   render() {
     
        return (
       <div className="Table">
         <h1>IronContacts</h1>
+
+        <button onClick={this.addRandom}>Add Random</button>
         <table>
           <tr>
             <th>{Object.keys(contacts[0])[1]}</th>
@@ -18,10 +36,10 @@ class Table extends Component {
           </tr>
 
          
-          {contacts.splice(0,5).map((contact, index)=>{
+          {/* {contacts.splice(0,5).map((contact, index)=>{
             return (<Actor {...contact} />)
-          })}
-         
+          })} */}
+         {this.state.contact.map(contact => <Actor {...contact}/>)}
         </table>
       </div>
     );
