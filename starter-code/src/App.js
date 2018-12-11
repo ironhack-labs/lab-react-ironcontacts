a4f63262b23cbe6dceab705a1545a698de124a85
Carlos Rivera <carlos.rivera.cabrera@gmail.com>
2018-12-11 13:09:11 -0600

    Iteration 3 done

--- starter-code/src/App.js
+++ starter-code/src/App.js
@@ -29,13 +29,43 @@ class App extends Component {
     })
   }
 
+  compareName = (a,b) => {
+    if(a.name < b.name) return -1
+    if(a.name > b.name) return 1
+    return 0
+  }
+
+  sortByName = () => {
+    let currContacts = this.state.contacts
+    currContacts.sort(this.compareName)
+    this.setState({
+      contacts:currContacts
+    })
+  }
+
+  comparePopularity = (a,b) => {
+    if(a.popularity > b.popularity) return -1
+    if(a.popularity < b.popularity) return 1
+    return 0
+  }
+
+  sortByPopularity = () => {
+    let currContacts = this.state.contacts
+    currContacts.sort(this.comparePopularity)
+    this.setState({
+      contacts:currContacts
+    })
+  }
+
   render() {
     console.log(this.state.contacts)
-    const {addRandom} = this
+    const {addRandom, sortByName, sortByPopularity} = this
     return (
       <Fragment>
         <h1>Iron Contacts</h1>
         <button onClick={addRandom}>Add Random Contact</button>
+        <button onClick={sortByName}>Sort by name</button>
+        <button onClick={sortByPopularity}>Sort by popularity</button>
         {this.drawContacts()}
       </Fragment>
     )
