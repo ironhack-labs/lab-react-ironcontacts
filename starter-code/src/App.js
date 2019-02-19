60f1a2e43381f169dfde8ed89cd835bc789787ec
Sofía <sofianombela@gmail.com>
2019-02-19 18:47:43 +0100

    iteration 1 done

--- starter-code/src/App.js
+++ starter-code/src/App.js
@@ -1,21 +1,29 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
+import ContactTable from "./components/ContactTable/ContactTable";
+import contacts from './contacts.json'
 
 class App extends Component {
+  state = {
+    list: contacts.slice(0, 5)
+  }
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <h1>IronContacts</h1>
+        <ContactTable contactsProp={this.state.list}/> 
       </div>
     );
   }
 }
 
 export default App;
+
+ // constructor() {
+  //   super(); //este es el padre
+  //   this.state = {
+  //     contacts: contacts.slice(0, 5)
+  //   };
+  // }
+
+  // <ContactTable contacts={this.state.contacts}/> Entre llaves es la forma de pasar las propiedades. Le pasamos el objeto state que ahora es un array de 5.
---
+++ starter-code/src/components/ContactTable/ContactTable.jsx
@@ -0,0 +1,35 @@
+import React, { Component } from "react";
+import './ContacTable.css'
+
+class ContactTable extends Component {
+  render() {
+    return (
+      <table>
+        <thead>
+          <tr>
+            <th>Picture</th>
+            <th>Name</th>
+            <th>Popularity</th>
+          </tr>
+        </thead>
+        <tbody>
+          {this.props.contactsProp.map((e, idx) => {
+            return <tr key ={idx}>
+              <td><img className="img-table" src={e.pictureUrl}/></td>
+              <td><h2>{e.name}</h2></td>
+              <td><h4>{e.popularity.toFixed(2)}</h4></td>
+            </tr>
+          })}
+        </tbody>
+      </table>
+    );
+  }
+}
+
+export default ContactTable;
+
+//También puedes hacerlo de la siguiente manera export default class ContactTable extends React.Component
+
+//Para escribir JS en un JSX y siempre que esté dentro del return hay que poner llaves {}
+
+//toFixed()Le pasamos los números que queremos que muestre pero lo transforma a string
