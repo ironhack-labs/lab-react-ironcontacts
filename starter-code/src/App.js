55157bddff25eacbf13c854cd9b541199fe9d4bc
j0nl1 <jonlitech@gmail.com>
2019-02-19 16:35:36 +0100

    Second Iteration

--- starter-code/src/App.js
+++ starter-code/src/App.js
@@ -6,10 +6,22 @@ class App extends Component {
   state = {
     contacts: contacts.slice(0, 5)
   }
+  addRandom = () => {
+    let newState = {
+      ...this.state
+    }
+
+    newState.contacts.push(contacts[Math.floor(Math.random() * contacts.length)])
+
+    this.setState(newState)
+  }
   render() {
     return (
       <div className="App">
         <h1>IronContacts</h1>
+        <div className="Controller">
+          <button onClick={this.addRandom}>Add Random Contact</button>
+        </div>
         <ContactsTablet contacts={this.state.contacts} />
       </div>
     );
