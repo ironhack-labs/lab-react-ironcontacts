a4aab66612e43b3c0e510fd8b4304639396eed4f
Nira <niralonso@gmail.com>
2019-02-20 09:07:23 +0100

    .

--- starter-code/src/App.js
+++ starter-code/src/App.js
@@ -1,29 +1,81 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import './App.css';
 import ContactTable from './components/Contacts table/ContactTable';
-import contacts from "./contacts.json"
+import contacts from './contacts.json';
 import FunctionButton from './components/functionButton/FunctionButton';
 
-export default class App extends Component {
-  state ={
-    list:contacts.slice(0,5)
+
+class App extends Component {
+
+  state = {
+    list: contacts.slice(0, 5)
   }
-  addRandomContact = () =>{
-    let newState ={
+
+  addRandomContact = () => {
+    let newState = {
       ...this.state
-    }
-    newState.list.push(contacts[Math.floor(Math.random()*contacts.length)])
+    }    
+
+    newState.list.push(contacts[Math.floor(Math.random() * contacts.length)])
 
     this.setState(newState)
   }
-    render() {
+
+  sortByName = () => {
+    let newState = {
+      ...this.state
+    }    
+
+    newState.list.sort((a, b) => {
+    if (a.name < b.name) {return -1;}
+    if (a.name > b.name) {return 1;}
+    return 0
+  })
+  this.setState(newState)
+
+}
+
+  sortByPopularity = () => {
+    let newState = {
+      ...this.state
+    }  
+
+    newState.list.sort((a, b) => b.popularity - a.popularity)
+
+    this.setState(newState)
+
+  }
+
+  deleteContact = (name) => {
+    let newState = {
+      ...this.state
+    }  
+    // Necesitamos guardarlo 
+    newState.list = newState.list.filter(e => e.name !== name) 
+
+    this.setState(newState)
+  }
+  
+
+
+  render() {
+
     return (
-      <div>
-         <h1>IronContacts</h1>
-         <FunctionButton functionProp={this.addRandomContact}> Add Random</FunctionButton>
-         <div className="controller"></div>
-        <ContactTable contactsPro ={this.state.list}/>
-        
+      <div className="App">
+      <h1>IronContacts</h1>
+      <div className="controller">
+      <div className="App-header">
+      <FunctionButton functionProp={this.addRandomContact}>Add Random Contacts</FunctionButton>
+      <FunctionButton functionProp={this.sortByName}>Sort by name</FunctionButton>
+      <FunctionButton functionProp={this.sortByPopularity}>Sort by popularity</FunctionButton>
       </div>
-    )
+     
+      </div>
+     
+      <ContactTable deleteProp={this.deleteContact} contactsProp={this.state.list}/>
+      </div>
+    );
   }
 }
+
+export default App;
--- starter-code/src/components/Contacts table/ContactTable.jsx
+++ starter-code/src/components/Contacts table/ContactTable.jsx
@@ -1,33 +1,51 @@
-import React, { Component } from 'react'
-import "./table.css"
+import React from "react";
+import './table.css'
+import FunctionButton from "../functionButton/FunctionButton";
 
-export default class ContactTable extends Component {
+class ContactTable extends React.Component {
+
     render() {
         return (
-            
+
             <table>
-               
                 <thead>
-                    <tr>
-                        <th>Picture</th>
-                        <th>Name</th>
-                        <th>Popularity</th>
-                        <th>Action</th>
-                    </tr>
+                <tr>
+                    <th>Picture</th>
+                    <th>Name</th>
+                    <th>Popularity</th>
+                    <th>Action</th>
+                </tr>
                 </thead>
                 <tbody>
-                {this.props.contactsPro.map((e, idx)=>{
-                    return <tr key={idx}>
-                       <td><img className="img-table" src={e.pictureUrl} /></td>
-                        <td>{e.name}</td>
-                        <td>{e.popularity.toFixed(2)}</td>
+                {this.props.contactsProp.map((e, idx) => {
+                    return <tr className="tr" key={idx}>
+                        <td><img className="img-table" src={e.pictureUrl} /></td>
+                        <td><p>{e.name}</p></td>
+                        <td><p>{e.popularity.toFixed(2)}</p></td>
+                        <td><FunctionButton functionProp={() => this.props.deleteProp(e.name)}>Delete</FunctionButton></td>
                     </tr>
-                })}
 
+                })}
                 </tbody>
             </table>
-        )
 
+
+
+
+        )
     }
+
+
 }
-            
+
+
+
+export default ContactTable;
+
+
+
+
+
+
+
+
