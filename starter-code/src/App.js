fee488698f4a778ec8f79660d6b52e5a0af35c28
Estelle Olawinski <estelle.olawinski@me.com>
2019-02-20 09:27:04 +0100

    Paris - Estelle

--- starter-code/src/App.js
+++ starter-code/src/App.js
@@ -1,19 +1,64 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
+import contacts from "./contacts.json";
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = { contactArray: contacts.slice(0, 5) };
+  }
+
+  newRandomContacts() {
+    const anything = this.state.contactArray;
+    console.log("this the anything object: ", anything);
+
+    console.log("this is the contacts list: ", contacts);
+
+    const randomIndex = Math.floor(Math.random() * contacts.length);
+    console.log("randomIndex: ", randomIndex);
+
+    const randomContact = contacts[randomIndex];
+    console.log("this is the random contact: ", randomContact);
+    anything.push(randomContact);
+
+    console.log("this is the new anything:", anything);
+    this.setState({ contactArray: anything });
+  }
+
   render() {
+    const { contactArray } = this.state;
+
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+      <table className="App">
+        <caption>
+          <button onClick={() => this.newRandomContacts()}>
+            Add a random contact
+          </button>
+        </caption>
+        <tr>
+          <th>Picture</th>
+          <th>Name</th>
+          <th>Popularity</th>
+        </tr>
+        {contactArray.map((oneContact, index) => {
+          return (
+            // add a UNIQUE key to the HTML tag you return in map()
+            // (this helps React be more efficient when updating the DOM)
+            <tr key={oneContact._id}>
+              <td>
+                <img src={oneContact.pictureUrl} className="image" />
+              </td>
+              <td>
+                <h3>{oneContact.name}</h3>
+              </td>
+              <td>
+                <p>Popularity: {oneContact.popularity}</p>
+              </td>
+            </tr>
+          );
+        })}
+      </table>
     );
   }
 }
