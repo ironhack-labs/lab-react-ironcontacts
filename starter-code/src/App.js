07cd9abcd767052330469f3d539e7d96c957c6a5
Juliana <juliana.jeanmarie972@gmail.com>
2019-02-21 21:32:38 +0100

    wip

--- starter-code/src/App.js
+++ starter-code/src/App.js
@@ -1,18 +1,32 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import contacts from './contacts.json'
 
 class App extends Component {
   render() {
+    const contactList =  contacts.map((contact, i) =>{
+      return(
+      <div className="contact-item" key={i}>
+       <img src={contact.pictureUrl} alt=""/>
+        <p>{contact.name}</p>
+        <p>{contact.popularity}</p>
+      </div>
+      )
+    })
     return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Welcome to React</h1>
         </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <h1>IronContacts</h1>
+        <div className="contact-header">
+          <p>Picture</p>
+          <p>Name</p>
+          <p>Popularity</p>
+        </div>
+        { contactList }
       </div>
     );
   }
