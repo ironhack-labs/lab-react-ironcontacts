855e199f6f59a0fa68ead7f70208aa4493843015
Cdelatorre <carlosdelatorrediaz@gmail.com>
2019-02-24 21:04:18 +0100

    feat: iteration 2 completed

--- starter-code/src/components/contacts/ContactList.js
+++ starter-code/src/components/contacts/ContactList.js
@@ -5,33 +5,49 @@ import ContactRow from './ContactRow'
 
 
 export default class ContactList extends Component {
- 
+  constructor(props) {
+    super(props);
+    this.state = {
+      contacts: contacts.slice(0, 5) || [],
+      allContacts: contacts || [],
+      sortedBy: undefined
+    }
+  }
 
-  state = {
-    contacts: [...contacts],
-    limit: 5
+
+  onClickAddRandom = () => {
+  let restOfContacts = this.state.allContacts.filter(contact => !this.state.contacts.some(cont => cont.name === contact.name))
+  let randomContact = restOfContacts[Math.floor(Math.random()*restOfContacts.length)];  
+  this.setState({
+    contacts: [...this.state.contacts, randomContact]
+  })
+  // console.log(restOfContacts)
+  // console.log(this.state.allContacts)
+  // console.log(randomContact)
+  // console.log(this.state.contacts)
   }
 
   roundRate = (x) => {
     return x.toFixed(1)
   }
 
+  updateArray = () => {
+    this.setState({
+      limit :  this.state.limit + 1
+    }) 
+  }
+
  
   renderItems = () => {
-    let ContactArray = this.state.contacts.map((contact, index) => {
-       return index < this.state.limit ? contact : undefined })
-       .filter(contact => contact !== undefined);
-    
-    return ContactArray.map((contact) => {
+    return this.state.contacts.map((contact) => {
       return <ContactRow key={contact.name} {...contact} popularity={this.roundRate(contact.popularity)}/>
     })
   }
 
   render() {
-   
-    
     return (
       <div className="container row mx-auto col-xl-6 col-md-8 col-11">
+      <button onClick={this.onClickAddRandom }>Add random celebrity</button>
          {this.renderItems()}
       </div>
     );
