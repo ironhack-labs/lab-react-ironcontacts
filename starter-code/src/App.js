8d105b0354d766d772d1f76e5ddd01da10e95681
Sandra Dupont <ssduptcom@gmail.com>
2019-02-26 15:00:12 +0100

    done

--- starter-code/src/App.js
+++ starter-code/src/App.js
@@ -2,7 +2,6 @@ import React, { Component } from "react";
 import "./App.css";
 import TabContacts from "./components/TabContacts.js";
 
-
 class App extends Component {
   render() {
     return (
--- starter-code/src/components/TabContacts.js
+++ starter-code/src/components/TabContacts.js
@@ -1,32 +1,86 @@
 import React from "react";
 import contacts from "../contacts.json";
+import "../TabContacts.css";
 
 const fiveCeleb = contacts.slice(0, 5);
 
 class TabContacts extends React.Component {
+  constructor() {
+    super();
+    this.state = { celebrities: fiveCeleb };
+  }
+
+  addContact = () => {
+    const randomIndex = Math.round(Math.random() * contacts.length);
+    const newContact = contacts[randomIndex];
+
+    const contactsCopy = [...this.state.celebrities];
+    contactsCopy.push(newContact);
+    this.setState({ celebrities: contactsCopy });
+  };
+
+  sortByName = () => {
+    const contactsCopy = [...this.state.celebrities];
+    contactsCopy.sort((a, b) => {
+      if (a.name < b.name) return -1;
+      else if (a.name === b.name) return 0;
+      else return 1;
+    });
+    this.setState({ celebrities: contactsCopy });
+  };
+
+  sortByPopularity = () => {
+    const contactsCopy = [...this.state.celebrities];
+    contactsCopy.sort((a, b) => {
+      if (a.popularity < b.popularity) return -1;
+      else if (a.popularity === b.popularity) return 0;
+      else return 1;
+    });
+    this.setState({ celebrities: contactsCopy });
+  };
+
+  deleteContact = (index) => {
+    const contactsCopy = [...this.state.celebrities];
+    contactsCopy.splice(index, 1);
+    this.setState({ celebrities: contactsCopy });
+  };
+
   render() {
     return (
-      <table>
-        
-        <thead>
-          <tr>
-            <th>PICTURE</th>
-            <th>NAME</th>
-            <th>POPULARITY</th>
-          </tr>
-        </thead>
-        <tbody>
-          {fiveCeleb.map(oneCeleb => (
+        <div className="container">
+      <div className="Tab">
+        <button className="choice" onClick={this.addContact}> Add New Random Contacts </button>
+        <button className="choice" onClick={this.sortByName}> sortByName </button>
+        <button className="choice" onClick={this.sortByPopularity}> sortByPopularity </button>
+
+        <table>
+          <thead className="Tab-head">
             <tr>
-              <td>
-                <img src={oneCeleb.pictureUrl} alt={oneCeleb.name} width="60px" height="60px" />
-              </td>
-              <td>{oneCeleb.name}</td>
-              <td>{oneCeleb.popularity.toFixed(2)}</td>
+              <th>PICTURE</th>
+              <th>NAME</th>
+              <th>POPULARITY</th>
             </tr>
-          ))}
-        </tbody>
-      </table>
+          </thead>
+          <tbody className="Tab-body">
+            {this.state.celebrities.map((oneCeleb, index) => (
+              <tr>
+                <td>
+                  <img
+                    src={oneCeleb.pictureUrl}
+                    alt={oneCeleb.name}
+                    width="60px"
+                    height="60px"
+                  />
+                </td>
+                <td>{oneCeleb.name}</td>
+                <td>{oneCeleb.popularity.toFixed(2)}</td>
+                <button className="delete" onClick = {()=> this.deleteContact(index)}> Delete </button>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
+      </div>
     );
   }
 }
