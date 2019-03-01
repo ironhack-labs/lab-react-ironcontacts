4470d223265402d15b57ca97cba325751b27749f
Javier Requejo Melchor <javierrequejomelchor@gmail.com>
2019-03-01 20:34:52 +0100

    Style improvements

--- starter-code/src/components/ContactItem.jsx
+++ starter-code/src/components/ContactItem.jsx
@@ -4,7 +4,7 @@ const ContactItem = (props) => {
     return (
       <div className="columns row level-item">
         <div className="column">
-          <figure className="image container is-96x96">
+          <figure className="image container is-64x64">
             <img src={props.pictureUrl} alt={props.name} />
           </figure>
         </div>
@@ -15,7 +15,7 @@ const ContactItem = (props) => {
           <strong className="is-size-5">{props.popularity.toFixed(2)}</strong>
         </div>
         <div className="column">
-          <button type="button" className="button is-normal" onClick={props.onClickDelete}>Remove</button>
+          <button type="button" className="button is-normal is-danger is-inverted" onClick={props.onClickDelete}>Remove</button>
         </div>
       </div>
     );
--- starter-code/src/components/ContactList.jsx
+++ starter-code/src/components/ContactList.jsx
@@ -27,9 +27,9 @@ export default class ContactList extends Component {
   sortContacts = (sortedType) => {
     const contacts = [...this.state.contacts].sort((c1, c2) => {
       switch (sortedType) {
-        case 'name': 
+        case 'name':
           return c1.name.localeCompare(c2.name);
-        case 'popularity': 
+        case 'popularity':
           return c2.popularity - c1.popularity;
       }
     });
@@ -37,11 +37,11 @@ export default class ContactList extends Component {
       contacts: contacts,
       sortedType: sortedType
     })
-  } 
+  }
 
   onClickDelete = (contact) => {
     this.setState({
-      contacts: [...this.state.contacts].filter(c => c!== contact)
+      contacts: [...this.state.contacts].filter(c => c !== contact)
     });
   }
 
@@ -49,20 +49,22 @@ export default class ContactList extends Component {
 
     return (
       <Fragment>
-        <div className="buttons row level-item">
-          <button type="button" className="button is-normal" onClick={this.randomContact}>Add random contact</button>
-          <button type="button" className="button is-normal" onClick={this.sortContacts.bind(this, 'name')}>Sort by name</button>
-          <button type="button" className="button is-normal" onClick={this.sortContacts.bind(this, 'popularity')}>Sort by popularity</button>
+        <div className="container contactList">
+          <div className="buttons row level-item">
+            <button type="button" className="button is-normal is-info is-inverted" onClick={this.randomContact}>Add random contact</button>
+            <button type="button" className="button is-normal is-info is-inverted" onClick={this.sortContacts.bind(this, 'name')}>Sort by name</button>
+            <button type="button" className="button is-normal is-info is-inverted" onClick={this.sortContacts.bind(this, 'popularity')}>Sort by popularity</button>
+          </div>
+          <div className="columns row">
+            <div className="column"><h3 className="title">Picture</h3></div>
+            <div className="column"><h3 className="title">Name</h3></div>
+            <div className="column"><h3 className="title">Popularity</h3></div>
+            <div className="column"><h3 className="title">Remove this contact</h3></div>
+          </div>
+          {this.state.contacts.map((contact, index) => {
+            return <ContactItem key={index} {...contact} onClickDelete={this.onClickDelete.bind(this, contact)} />
+          })}
         </div>
-        <div className="columns row">
-          <div className="column"><h3 className="title">Picture</h3></div>
-          <div className="column"><h3 className="title">Name</h3></div>
-          <div className="column"><h3 className="title">Popularity</h3></div>
-          <div className="column"><h3 className="title">Remove this contact</h3></div>
-        </div>
-        {this.state.contacts.map((contact, index) => {
-          return <ContactItem key={index} {...contact} onClickDelete={this.onClickDelete.bind(this,contact)} />
-        })}
       </Fragment>
     );
   }
