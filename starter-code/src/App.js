280cf3889c8f79f8daa781e9371a32eab4e5d73e
Manuel Garaboa <mannygaraboa@gmail.com>
2019-03-04 17:47:00 -0500

    first commit

--- starter-code/src/App.js
+++ starter-code/src/App.js
@@ -1,8 +1,11 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import contacts from './contacts.json'
+
 
 class App extends Component {
+  
   render() {
     return (
       <div className="App">
@@ -13,6 +16,7 @@ class App extends Component {
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
+        <p>Howdy partner</p>
       </div>
     );
   }
