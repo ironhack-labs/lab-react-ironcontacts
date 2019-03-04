fd9b5a0222fd42c2dfe86249202cab4a2cecc4e7
Victoria Hickam <vhickam@gmail.com>
2019-03-04 17:46:18 -0500

    done

--- starter-code/src/App.js
+++ starter-code/src/App.js
@@ -5,14 +5,17 @@ import './App.css';
 import contacts from './contacts.json'
 
 class App extends Component {
-  
+  state = {
+    contacts: contacts.splice(0,5)
+  }
   showContacts = () => {
-    let list = contacts.splice(0,5).map((contact,i) => {
+    let list = this.state.contacts.map((contact,i) => {
       return ( 
       <tr key={i}> 
         <td><img src={contact.pictureUrl} alt={contact.name} height="100" width="auto" ></img> </td>
         <td> {contact.name} </td>
         <td> {contact.popularity.toFixed(2)} </td>
+        <td> <button onClick={()=>{this.deleteContact(i)}}>Delete</button></td>
       </tr>
       
         )
@@ -20,12 +23,57 @@ class App extends Component {
     return list;
   }
 
+
+  //document.getElementById("sjkfh").onclick = function(){ delete()}
+
   addContact = () => {
     let randomContact = contacts[Math.floor(Math.random()*contacts.length)];
-    list.push(randomContact);
-    return list;
-    
-    
+    const contactsCopy = [...this.state.contacts];
+    contactsCopy.push(randomContact);
+    this.setState({
+      contacts: contactsCopy
+    })   
+  }
+
+  sortByPopularity = () => {
+    const contactsByPopularity = [...this.state.contacts];
+    contactsByPopularity.sort(function(a,b){
+      var popA=a.popularity, popB=b.popularity
+      if(popA < popB)
+        return -1
+      if (popA > popB)
+        return 1
+      return 0
+      })
+
+    this.setState({
+      contacts: contactsByPopularity
+    })
+  }
+
+  sortByName = () => {
+    const contactsByName = [...this.state.contacts];
+    contactsByName.sort(function(a,b){
+      var nameA=a.name.toLowerCase(), nameB=b.name.toLowerCase()
+      if(nameA < nameB)
+        return -1
+      if (nameA > nameB)
+        return 1
+      return 0
+      })
+
+    this.setState({
+      contacts: contactsByName
+    })
+  }
+
+  deleteContact = (i) => {
+    const contactsDelete = [...this.state.contacts];
+    contactsDelete.splice(i,1);
+    this.setState({
+      contacts: contactsDelete
+    })
+
   }
 
   render() {
@@ -34,7 +82,9 @@ class App extends Component {
         <header>
           <h1 className="AppTitle">IronContacts</h1>
         </header>
-        <button>Add Random Contact</button>
+        <button onClick={this.addContact}>Add Random Contact</button>
+        <button onClick={this.sortByName}>Sort by Name </button>
+        <button onClick={this.sortByPopularity}>Sort by Popularity </button>
         <table>
           <tr>
             <th>Image</th>
