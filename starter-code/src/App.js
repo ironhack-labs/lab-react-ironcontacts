ae6f1f6693aee95ea0b6bc843e162f7b37efdd85
Roberto Konanz <robkonanz@gmail.com>
2019-03-04 17:12:48 -0500

    Now sorts by Name and by Popularity.

--- starter-code/src/App.js
+++ starter-code/src/App.js
@@ -1,18 +1,80 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import contacts from './contacts.json'
 
 class App extends Component {
+
+  state = {
+    contacts: contacts.splice(0,5)
+  }
+
+  showContacts = () => {
+    let list = this.state.contacts.map((contact,i) => {
+      return <tr key={i}>
+        <td><img src={contact.pictureUrl} className="contactPhoto"></img></td>
+        <td>{contact.name}</td>
+        <td>{contact.popularity}</td>
+        </tr>
+    })
+    return list;
+  }
+
+  addRandomContact = () => {
+    let newList = [...this.state.contacts]
+    newList.push(contacts[Math.floor(Math.random() * 199)+5])
+    this.setState({
+      contacts: newList
+    })
+  }
+
+  sortByName = () => {
+    let sortedByName = [...this.state.contacts]
+    sortedByName = sortedByName.sort(this.compareNames)
+    this.setState({
+      contacts: sortedByName
+    })
+  }
+
+  sortByPopularity = () => {
+    let sortedByPopularity = [...this.state.contacts]
+    sortedByPopularity = sortedByPopularity.sort(this.comparePopularity)
+    this.setState({
+      contacts: sortedByPopularity
+    })
+  }
+
+  compareNames = (a,b) => {
+    if (a.name < b.name)
+      return -1;
+    if (a.name > b.name)
+      return 1;
+    return 0;
+  }
+
+  comparePopularity = (a, b) => {
+    if (a.popularity < b.popularity)
+      return -1;
+    if (a.popularity > b.popularity)
+      return 1;
+    return 0;
+  }
+
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <h1>IronContacts</h1>
+        <button onClick={this.addRandomContact} className="action-buttons">Add Random Contact</button>
+        <button onClick={this.sortByName} className="action-buttons">Sort by Name</button>
+        <button onClick={this.sortByPopularity} className="action-buttons">Sort by Popularity</button>
+        <table className="ironcontacts-table">
+          <tr>
+            <th>Picture</th>
+            <th>Name</th>
+            <th>Popularity</th>
+          </tr>
+          {this.showContacts()}
+        </table>
       </div>
     );
   }
