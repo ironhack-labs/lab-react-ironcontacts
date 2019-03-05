771ef7b8efc4fc977c192145c908c53792aa4ab8
Henry Doce <henrydoce@protonmail.com>
2019-03-05 16:03:56 -0500

    Iteration 1 done

--- starter-code/src/App.js
+++ starter-code/src/App.js
@@ -1,18 +1,50 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import contacts from './contacts.json'
 
 class App extends Component {
+  state = {
+    contacts:contacts.splice(0,5) //all the contacts we are messing with 
+  }
+
+  showCelebs = () => {
+    let list = this.state.contacts.map((contact, i)=>{ //loo[ps throught contacts and returns an html list
+      return (
+        <tr>
+          <td><img src={contact.pictureUrl}/></td> 
+          <td>{contact.name}</td>
+          <td> {contact.popularity}</td>
+        </tr>
+      )
+    })
+    return list;
+  }
+
+  addContact = () => {
+    let newContacts = [...this.state.contacts] //makes a copy of the contact state
+    newContacts.push({ //adds to the copy
+      name:"Henry"
+    })
+    this.setState({ //and resets the state to be this newlist with another henry
+      contacts:newContacts
+    })
+  }
+
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div class="contactTable">
+        <h1>IronContacts</h1>
+        <table>
+          <tr>
+            <th>Picture</th>
+            <th>Name</th>
+            <th>Popularity</th>
+          </tr>
+          {this.showCelebs()} {/* Calls function once */}
+        </table>
+        
+          
+        <button onClick={this.addContact}>Add Contact</button>
       </div>
     );
   }
