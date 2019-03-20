93330097b0b54dcaa087636c82152604e0619418
Cristina Blanco <cristinablanco@MacBook-Pro-de-Cristina-5.local>
2019-03-20 09:33:50 +0100

    done

--- starter-code/src/App.js
+++ starter-code/src/App.js
@@ -1,21 +1,133 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import contacts from './contacts.json'
+
+import Contact from './components/stateless/Contact'
+import Title from './components/stateless/Title'
 
 class App extends Component {
+
+  constructor() {
+    super()
+    this.state = {
+      contacts: contacts.splice(0, 5) 
+    }
+  }
+
+
+
+  clickHandler = () => {
+
+    const randomContact = Math.floor(Math.random() * contacts.length) 
+    contacts[randomContact] 
+    console.log(contacts[randomContact] )
+
+      let newContacts = this.state.contacts;
+      newContacts.push(contacts[randomContact])
+
+      this.setState({
+        contacts: newContacts
+
+
+      })
+    }
+
+    
+    sortByName = () => {
+
+      let sortedContacts = this.state.contacts.sort((a, b) => {
+
+        if (a.name > b.name) {
+          return 1
+        } 
+        if (a.name < b.name) {
+          return -1
+        }
+          return 0
+        }
+
+      )
+        this.setState({
+          contacts : sortedContacts
+        })
+      
+    }
+
+
+
+    
+    sortByPopularity = () => {
+      console.log("pollas")
+      let sortedPopContacts = this.state.contacts.sort((a, b) => {
+
+        if (a.popularity > b.popularity) {
+          return 1
+        } 
+        if (a.popularity < b.popularity) {
+          return -1
+        }
+          return 0
+        }
+
+      )
+        this.setState({
+          contacts : sortedPopContacts
+        })
+      
+    }
+
+    deleteContact = (contactIndex) => {
+      const contactsCopy = [...this.state.contacts];
+      contactsCopy.splice(contactIndex, 1);
+      this.setState({
+          contacts: contactsCopy
+      })
+  }
+
+
+
+
   render() {
+
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+    <main>
+
+    <section className="contactos">
+
+
+                <div>
+                  <Title text="IronContacts" />
+                  <button onClick={this.clickHandler}>Add Random Contact</button>
+                  <button onClick={this.sortByName}>Sort by name</button>
+                  <button onClick={this.sortByPopularity}>Sort by popularity</button>
+                  
+                  <table>
+                <tr>
+                <th>Picture</th>
+                <th>Name</th>
+                <th>Popularity</th>
+                </tr>
+
+                {this.state.contacts.map((contact, index) => {
+                    return <Contact key={index} {...contact} deleteContact= {() => this.deleteContact(index) }
+                    />
+
+                })}
+                </table>
+                
+                </div>
+
+    </section>
+
+
+  </main>
+
     );
   }
 }
+
+
+
 
 export default App;
---
+++ starter-code/src/components/stateless/Contact.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+const Contact = props => {
+    return (
+
+        
+            <tr>
+                <td><img src={props.pictureUrl} /> </td>
+                <td>{props.name}</td>
+                <td>{props.popularity}</td>
+                <td><button onClick={props.deleteContact}>Delete</button></td>
+            </tr>
+
+    )
+}
+
+export default Contact;
