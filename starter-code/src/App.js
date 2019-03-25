158599c12efe49fed960c936119c28ecac1825df
williamfiallos <wfiallos10@gmail.com>
2019-03-24 20:07:23 -0400

    iteration 1 done

--- starter-code/src/App.js
+++ starter-code/src/App.js
@@ -1,18 +1,41 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import contacts from './contacts.json';
+// import ProducerContacts from './components/ProducerContacts';
 
 class App extends Component {
+  contacts = () => {
+    let contactList = contacts.splice(0,5).map((contact, i) => {
+        return (
+            <tr key={i}> 
+            {/* need to put "key" in <tr> due to warning message in console saying each child in a list should have a unique "key" prop */}
+                <td><img src={ contact.pictureUrl } alt={contact.name} height="100" width="auto" /></td>
+                <td> { contact.name } </td>
+                <td> { contact.popularity } </td>
+            </tr>
+        )
+    })
+    return contactList;
+  }
+
   render() {
     return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+          <h1 className="App-title">IronContacts</h1>
         </header>
         <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
+          {/* To get started, edit <code>src/App.js</code> and save to reload. */}
         </p>
+        <tr>
+          <th>Picture</th>
+          <th>Name</th>
+          <th>Popularity</th>
+        </tr>
+        {/* <ProducerContacts /> */}
+        {this.contacts()}
       </div>
     );
   }
---
+++ starter-code/src/components/ProducerContacts.js
@@ -0,0 +1,22 @@
+// import React, { Component } from 'react';
+// import contacts from './contacts.json'
+
+
+// class ProducerContacts extends Component {
+//     contacts = () => {
+//         let contactList = contacts.splice(0,5).map((contact, i) => {
+//             return (
+//                 <tr>
+//                     <td><img src={ contact.pictureUrl } alt="contact image" height="100" width="auto" /></td>
+//                     <td>{ contact.name }</td>
+//                     <td>{ contact.popularity }</td>
+//                 </tr>
+//             )
+//         })
+//         return contactList;
+//     }
+
+
+// }
+
+// export default ProducerContacts.js
