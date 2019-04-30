da1ba9750b132fb6aa4a421d92695a7015c0fd8a
ernestogl88 <ernestogomezlopez@gmail.com>
2019-04-30 10:46:49 +0200

    Iteration 1

--- starter-code/src/App.js
+++ starter-code/src/App.js
@@ -1,18 +1,32 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import contacts from "./contacts.json";
+import Row from "./Row";
 
 class App extends Component {
+  state = contacts.slice(0, 5);
+
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <h1>IronContacts</h1>
+        <table>
+          <tr>
+            <th>Picture</th>
+            <th>Name</th>
+            <th>Popularity</th>
+          </tr>
+          {this.state.map(contact => {
+            return (
+              <Row
+                name={contact.name}
+                picture={contact.pictureUrl}
+                popularity={contact.popularity}
+              />
+            );
+          })}
+        </table>
       </div>
     );
   }
