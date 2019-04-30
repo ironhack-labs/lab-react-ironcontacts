46980207f37b6bedf9e1cd81c9926ecf19b04c6c
Raúl Contreras <raulc98@gmail.com>
2019-04-30 13:46:23 +0200

    Finished iteration 1

--- starter-code/src/App.js
+++ starter-code/src/App.js
@@ -1,19 +1,27 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import contacts from './contacts.json';
+import Contact from './Contact';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+      <React.Fragment>
+        <h1>IronContacts</h1>
+        <table>
+          <thead>
+            <tr>
+              <th>Picture</th>
+              <th>Name</th>
+              <th>Popularity</th>
+            </tr>
+          </thead>
+          <tbody>
+            {contacts.slice(0, 5).map((contact, index) => <Contact key={contact.name} {...contact}/>)}
+          </tbody>
+        </table>
+      </React.Fragment>
     );
   }
 }
