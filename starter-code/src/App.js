3671ff7e3043285226ee557e37cc77fcd7118624
ernestogl88 <ernestogomezlopez@gmail.com>
2019-04-30 12:09:00 +0200

    Iteration 2 and 4, in proccess of 3

--- starter-code/src/App.js
+++ starter-code/src/App.js
@@ -2,30 +2,77 @@ import React, { Component } from "react";
 import logo from "./logo.svg";
 import "./App.css";
 import contacts from "./contacts.json";
-import Row from "./Row";
+import Contact from "./Contact";
 
 class App extends Component {
-  state = contacts.slice(0, 5);
+  state = {
+    contacts: contacts.slice(0, 5)
+  };
+
+  addRandomContact() {
+    let newContacts = [...this.state.contacts]
+    newContacts.push(contacts[Math.floor(Math.random() * contacts.length)])
+    this.setState({
+      ...this.state,
+      contacts: newContacts
+    });
+  }
+
+  sortContacts() {
+    let newContacts = [...this.state.contacts]
+    newContacts.sort((a,b)=>a.Name - b.Name)
+    this.setState({
+      ...this.state,
+      contacts: newContacts
+    });
+
+  }
+
+  removeContact(id) {
+    let newContacts = [...this.state.contacts]
+    newContacts.splice(id,1)
+    this.setState({
+      ...this.state,
+      contacts: newContacts
+    })
+  }
 
   render() {
     return (
       <div className="App">
         <h1>IronContacts</h1>
+        <button onClick={() => this.addRandomContact()}>
+          Add Random Contact
+        </button>
+        <button onClick={() => this.sortContacts()}>Sort By Name</button>
+        <button onClick={() => this.sortContacts()}>Sort By Popularity</button>
         <table>
-          <tr>
-            <th>Picture</th>
-            <th>Name</th>
-            <th>Popularity</th>
-          </tr>
-          {this.state.map(contact => {
-            return (
-              <Row
-                name={contact.name}
-                picture={contact.pictureUrl}
-                popularity={contact.popularity}
-              />
-            );
-          })}
+          <tbody>
+            <tr>
+              <th>Picture</th>
+              <th>Name</th>
+              <th>Popularity</th>
+            </tr>
+            {this.state.contacts.map((contact, i) => {
+              return (
+                <React.Fragment>
+                  <tr>
+                    <Contact
+                      key={i}
+                      name={contact.name}
+                      picture={contact.pictureUrl}
+                      popularity={contact.popularity}
+                    />
+                    <td>
+                      <button onClick={() => this.removeContact(i)}>
+                        Delete
+                      </button>
+                    </td>
+                  </tr>
+                </React.Fragment>
+              );
+            })}
+          </tbody>
         </table>
       </div>
     );
---
+++ starter-code/src/Contact.js
@@ -0,0 +1,19 @@
+import React from "react";
+
+export default class Contact extends React.Component {
+  render() {
+    return (
+      <React.Fragment>
+        <td>
+          <img src={this.props.picture} height="100" weight="100" />
+        </td>
+        <td>
+          <p>{this.props.name}</p>
+        </td>
+        <td>
+          <p>{this.props.popularity}</p>
+        </td>
+      </React.Fragment>
+    );
+  }
+}
