63e18cac5a2f5cb02afa641a401b18857db6db00
Felipe <fpr29.10@gmail.com>
2019-05-07 17:09:39 -0500

    first iteration

--- starter-code/src/App.js
+++ starter-code/src/App.js
@@ -3,10 +3,24 @@ import './App.css';
 import contacts from './contacts'
 
 class App extends Component {
+  state = {
+    allContacts: contacts,
+    firstContacts: [ contacts[0], contacts[1], contacts[2], contacts[3], contacts[4] ]
+  }
   render() {
     return (
       <div className="App">
-        
+        {this.state.firstContacts.map(e => {
+          return (
+            <div key={e.name}>
+              <p>
+                <img height="80" src={e.pictureUrl}/>
+                <h4>{e.name}</h4>
+                <span>{e.popularity}</span>
+              </p>
+            </div>
+          )
+        })}
       </div>
     );
   }
