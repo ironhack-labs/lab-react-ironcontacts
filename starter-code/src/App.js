ba2b457af507600c9cf4bf5967c815435c9dd081
Marypztr <nutrimary.77@gmail.com>
2019-05-07 20:51:08 -0500

    No estan terminados los estilos y me costo un poco de trabajo el recordar el map jeje

--- starter-code/src/App.js
+++ starter-code/src/App.js
@@ -1,21 +1,82 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import contacts from './contacts.json'
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
+  contactSlided = contacts.slice(0, 5)
+  state = {
+    data: this.contactSlided
   }
+
+  addRandomContact = () => {
+    const newContacts = [...this.state.data]
+    const filterContacts = contacts.filter(e => !newContacts.includes(e))
+    const contact =
+      filterContacts[Math.floor(Math.random() * filterContacts.length)]
+    if (filterContacts.length === 1) {
+      return
+    } else {
+      newContacts.push(contact)
+    }
+    this.setState({ data: newContacts })
+    console.log(this.state.data)
+  }
+
+  sortByName = () => {
+    const newContacts = [...this.state.data]
+    const sortedContacts = newContacts.sort((a, b) => (a.name < b.name ? -1 : 1))
+    console.log(sortedContacts)
+    this.setState({ data: sortedContacts })
+  }
+
+
+  sortByPopularity = () => {
+    const newContacts = [...this.state.data]
+    const sortedContacts = newContacts.sort((a, b) => (a.popularity < b.popularity ? -1 : 1))
+    console.log(sortedContacts)
+    this.setState({ data: sortedContacts })
+  }
+
+hanldleDelete = celebrityIndex =>{
+  const newContacts= this.state.data
+  newContacts.splice(celebrityIndex, 1)
+  this.setState({data: newContacts})
+
 }
 
+  render() {
+    return (
+      <div className="body">
+        <div className="botones">
+          <button onClick={this.sortByName}>Sort by name</button>
+          <button onClick={this.sortByPopularity}>Sort by popularity</button>
+          <button onClick={this.addRandomContact}>Add character</button>
+        </div>
+        <h1>Celebrity List</h1>
+        <table>
+          <thead className="columnName">
+            <tr>
+              <th>Photo</th>
+              <th>Name</th>
+              <th>popularity</th>
+              <th>Delete</th>
+            </tr>
+          </thead>
+          <tbody>
+            {this.state.data.map((e, i, a) => {
+              return (
+                <tr key={e.index}>
+                  <td><img src={e.pictureUrl} /></td>
+                  <td>{e.name}</td>
+                  <td>{e.popularity}</td>
+                  <td><button onClick={this.hanldleDelete}>Delete</button></td>
+                </tr>
+              )
+            })}
+          </tbody>
+        </table>
+      </div>
+    )
+  }
+}
 export default App;
