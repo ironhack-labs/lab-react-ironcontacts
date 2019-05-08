0ce2567daea3972e636c835235c54af285eddf21
Jairo Garcia <jgarcia@indratek.com>
2019-05-08 09:17:15 -0500

    Ordenarlos me costo bastante trabajo

--- starter-code/src/App.js
+++ starter-code/src/App.js
@@ -1,19 +1,116 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import styled from "styled-components";
 
+import ContactRow from "./components/ContactRow";
+
+import contacts from "./contacts.json";
+
+const Layout = styled.main`
+  margin: 0 auto;
+  max-width: 600px;
+  font-family: "Lucida Sans", "Lucida Sans Regular", "Lucida Grande",
+    "Lucida Sans Unicode", Geneva, Verdana, sans-serif;
+`;
+
+const ContactsTable = styled.table`
+  width: 100%;
+  text-align: center;
+  font-weight: bold;
+`;
+
+const Button = styled.button`
+  font-weight: bold;
+`;
 class App extends Component {
+  state = {
+    record: [0, 1, 2, 3, 4],
+    data: contacts.slice(0, 5)
+  };
+
+  addRandomContact = () => {
+    const { record, data } = this.state;
+    let exist = false;
+
+    do {
+      const index = Math.floor(Math.random() * contacts.length);
+
+      if (!record.includes(index)) {
+        this.setState({
+          record: [...record, index],
+          data: [...data, contacts[index]]
+        });
+
+        exist = true;
+      }
+    } while (!exist);
+  };
+
+  sortBy(type) {
+    const data = [...this.state.data];
+
+    const lowest = (a, b) => (a[type] > b[type] ? +1 : -1);
+
+    const highest = (a, b) => (a[type] < b[type] ? -1 : +1);
+
+    if (type === "name") {
+      data.sort(lowest);
+    } else if (type === "popularity") {
+      data.sort(highest);
+    }
+
+    this.setState({ data });
+  }
+
+  handleDelete = index => {
+    const { record, data } = this.state;
+
+    record.splice(index, 1);
+    data.splice(index, 1);
+
+    this.setState({
+      record,
+      data
+    });
+  };
+
   render() {
+    const { data } = this.state;
+
+    const contacts = data.map((contact, i) => (
+      <ContactRow
+        key={i}
+        id={i}
+        {...contact}
+        handleDelete={this.handleDelete}
+      />
+    ));
+
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+      <Layout>
+        <header>
+          <h1>Contacts</h1>
         </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+        <section>
+          <Button onClick={this.addRandomContact}>Add Random Contact</Button>
+          <Button onClick={() => this.sortBy("name")}>Sort by name</Button>
+          <Button onClick={() => this.sortBy("popularity")}>
+            Sort by popularity
+          </Button>
+        </section>
+        <section>
+          <ContactsTable>
+            <thead>
+              <tr>
+                <th>Picture</th>
+                <th>Name</th>
+                <th>Popularity</th>
+                <th>Action</th>
+              </tr>
+            </thead>
+            <tbody>{contacts}</tbody>
+          </ContactsTable>
+        </section>
+      </Layout>
     );
   }
 }
---
+++ starter-code/src/components/ContactRow.js
@@ -0,0 +1,22 @@
+import React from "react";
+import styled from "styled-components";
+
+const Avatar = styled.img`
+  width: 150px;
+  border: 3px black solid;
+`;
+
+const ContactRow = ({ id, pictureUrl, name, popularity, handleDelete }) => (
+  <tr>
+    <td>
+      <Avatar src={pictureUrl} alt={name} />
+    </td>
+    <td>{name}</td>
+    <td>{popularity.toFixed(2)}</td>
+    <td>
+      <button onClick={() => handleDelete(id)}>Delete</button>
+    </td>
+  </tr>
+);
+
+export default ContactRow;
