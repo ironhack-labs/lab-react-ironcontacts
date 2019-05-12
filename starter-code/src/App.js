7728b8f4d4bf11f395e23b143efb4388fe760594
lockeas16 <lockeas16@gmail.com>
2019-05-11 23:40:37 -0500

    second iteration

--- starter-code/src/App.js
+++ starter-code/src/App.js
@@ -8,7 +8,6 @@ class App extends Component {
   constructor() {
     super();
     this.state = { contacts: this.getContacts(5) };
-    console.log(this.state);
   }
 
   state = {
@@ -19,17 +18,45 @@ class App extends Component {
     return contactsData.slice(0, qty);
   };
 
+  getRandomContact = () => {
+    return contactsData[Math.floor(Math.random() * (contactsData.length + 1))];
+  };
+
   handleDelete = (e, index) => {
     console.log(index);
+  };
+
+  handleAddRandom = e => {
+    const randomContact = this.getRandomContact();
+    let {contacts} = this.state;
+    contacts.push(randomContact);
+    this.setState({contacts});
   };
 
   render() {
     const { contacts } = this.state;
     return (
       <div className="App">
-        <section className="uk-section uk-section-secondary">
+        <section className="uk-section uk-section-default">
           <div className="uk-container uk-container-xsmall">
             <h1 className="uk-heading-medium">IronContacts</h1>
+            <div className="uk-grid-small uk-child-width-1-3" uk-grid="true">
+              <div>
+                <button className="uk-button uk-button-primary uk-button-small uk-width-1-1" onClick={this.handleAddRandom}>
+                  Add Random Contact
+                </button>
+              </div>
+              <div>
+                <button className="uk-button uk-button-primary uk-button-small uk-width-1-1">
+                  Sort by Name
+                </button>
+              </div>
+              <div>
+                <button className="uk-button uk-button-primary uk-button-small uk-width-1-1">
+                  Sort by Popularity
+                </button>
+              </div>
+            </div>
             <Table>
               {contacts.map((contact, index) => (
                 <TableRow
