f2e424d09376b03b78bae17ba3bcfcd6e128fdc8
jvelez523 <justin.velez0523@gmail.com>
2019-05-14 14:49:50 -0400

    Finished

--- starter-code/src/App.js
+++ starter-code/src/App.js
@@ -1,18 +1,91 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import contacts from './contacts.json'
+
+let showContacts = contacts.splice(0,5)
+//console.log(showContacts)
 
 class App extends Component {
+
+  state = {
+    contacts: contacts.splice(0,5),
+    otherContacts: contacts,
+  }
+
+  randomcontact = () => {
+    let contactcopy = [... this.state.contacts]
+    let random = this.state.otherContacts[Math.round(Math.random()*189)]
+    contactcopy.push(random)
+    console.log("This is a copy of contacts",contactcopy)
+    this.setState({
+      contacts: contactcopy
+    })
+  }
+
+  sortbyname = (a) => {
+    let contacts = this.state.contacts
+    //let sorted = contactcopy.sort(a.name)
+    console.log("this is sorted", contacts.name)
+    this.setState({
+      contacts: contacts.sort((a,b) => {
+        return a.name > b.name ? 1: a.name < b.name ? -1: 0
+      })
+    })
+  }
+
+  sortbypop = (a) => {
+    let contacts = this.state.contacts
+    //let sorted = contactcopy.sort(a.name)
+    console.log("this is sorted", contacts.name)
+    this.setState({
+      contacts: contacts.sort((a,b) => {
+        return a.popularity > b.popularity ? 1: a.popularity < b.popularity ? -1: 0
+      })
+    })
+  }
+
+  deletecontact = (a) => {
+    let contactcopy = [... this.state.contacts]
+    contactcopy.splice(a, 1)
+    this.setState({
+      contacts: contactcopy
+    })
+  }
+
+
   render() {
+
+    console.log("other contacts", this.state.otherContacts)
+
+    console.log(this.state.contacts)
+
+
+
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <button onClick={ () => { this.randomcontact() } }>Add Random Contact</button>
+      <button onClick={ () => { this.sortbyname() } }>Sort by Name</button>
+      <button onClick={ () => { this.sortbypop() } }>Sort by Popularity</button>
+      <div class="content">
+        <table>
+          <tr>
+            <th>Name</th>
+            <th>Popularity</th>
+            <th>Picture</th>
+          </tr>
+            {this.state.contacts.map((name, i) => {
+              return <tr><th>{name.name}</th>
+              <th>{name.popularity}</th>
+              <th><img class="headshot"src={name.pictureUrl}></img></th>
+              <button onClick={ () => { this.deletecontact(i) } }>Delete</button>
+
+              </tr>
+              
+              
+            })}
+        </table>
+        </div>
       </div>
     );
   }
