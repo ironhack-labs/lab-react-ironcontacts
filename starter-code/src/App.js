a934cc5d4dacce2a446bfc1be49fb55701c437b4
Ryan Kegerreis <ryankegerreis@icloud.com>
2019-05-14 12:32:13 -0400

    First iteration done.

--- starter-code/src/App.js
+++ starter-code/src/App.js
@@ -1,8 +1,45 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
+import contacts from './contacts.json'
 import './App.css';
 
 class App extends Component {
+  //Functions
+
+  state = {
+    counter: 0,
+    // loggedIn: false,
+    // color: null,
+    contacts: contacts,
+    filteredContacts: contacts
+  };
+
+
+
+
+  showArray = () => {
+    // let showContacts = contacts;
+    let contactsCopy = this.state.contacts.map((person, i) => {
+      return (
+        <table key={i}>
+          <tr>
+            <th>{person.name}</th>
+            <th>
+              <img src={person.pictureUrl} width="75em" alt="img" />
+            </th>
+            <th>{person.popularity}</th>
+          </tr>
+        </table >
+      )
+    })
+    console.log('')
+    return contactsCopy.splice(0, 5)
+  }
+
+
+
+
+
   render() {
     return (
       <div className="App">
@@ -13,6 +50,10 @@ class App extends Component {
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
+
+        {this.showArray()}
+
+
       </div>
     );
   }
