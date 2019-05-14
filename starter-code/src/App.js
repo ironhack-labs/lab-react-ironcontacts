886e9938b12ae5ac385e2b1a93feec78c6014f97
Ryan Kegerreis <ryankegerreis@icloud.com>
2019-05-14 17:03:39 -0400

    Complete

--- starter-code/src/App.js
+++ starter-code/src/App.js
@@ -10,51 +10,94 @@ class App extends Component {
     counter: 0,
     // loggedIn: false,
     // color: null,
-    contacts: contacts,
+    contacts: contacts.splice(0, 5),
     filteredContacts: contacts
   };
 
 
-
-
   showArray = () => {
-    // let showContacts = contacts;
+    console.log(this.state.contacts)
     let contactsCopy = this.state.contacts.map((person, i) => {
       return (
-        <table key={i}>
-          <tr>
-            <th>{person.name}</th>
-            <th>
-              <img src={person.pictureUrl} width="75em" alt="img" />
-            </th>
-            <th>{person.popularity}</th>
-          </tr>
-        </table >
+        <tr key={i}>
+          <th>{person.name}</th>
+          <th>
+            <img src={person.pictureUrl} width="75em" alt="img" />
+          </th>
+          <th>{person.popularity}</th>
+        </tr>
       )
     })
     console.log('')
+
     return contactsCopy.splice(0, 5)
   }
 
+  //Creates new celeb on click
+  celebClick = e => {
+    let randomNumber = Math.floor((Math.random() * this.state.contacts.length) + 1);
+    let contactArray = this.state.contacts
+
+    contactArray.push(contacts[randomNumber])
+
+    this.setState({ contacts: contactArray })
+  }
+
+  //Deletes the corresponding contact
+  deleteCeleb = (something) => {
+    //Copy array is the same as all other copy arrays. Name is different to avoid confusion
+    let copyArray = this.state.contacts
+    copyArray.splice(something, 1)
+
+    this.setState({
+      contacts: copyArray
+    })
+
+  }
+
+  //Sorts by both pop and name depending on input 
+  sortBy = (field, x, y) => {
+    let sortedContacts = this.state.contacts.sort((a, b) => (a[field] < b[field] ? x : y));
+
+    this.setState({
+      contacts: sortedContacts
+    });
+  }
 
 
-
-
+  //Shows the page
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-
-        {this.showArray()}
 
 
-      </div>
+        <button onClick={this.celebClick}>Add Random Contact</button>
+        <button onClick={_ => this.sortBy('name', '-1', '1')}>Sort By Name</button>
+        <button onClick={_ => this.sortBy('popularity', '1', '-1')}>Sort By Popularity</button>
+
+
+        <table>
+          <tr>
+            <th>Image</th>
+            <th>Name</th>
+            <th>Populariy</th>
+          </tr>
+          {this.state.contacts.map((celeb, i) => {
+            return (
+              <tr key={i}>
+                <td><img width="70px" src={celeb.pictureUrl} /></td>
+                <td>{celeb.name}</td>
+                <td>{celeb.popularity}</td>
+                <td><button onClick={_ => this.deleteCeleb(i)}>Delete</button></td>
+              </tr>
+            )
+          })}
+        </table >
+
+
+
+
+      </div >
     );
   }
 }
