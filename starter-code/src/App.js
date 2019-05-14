043d518c621967603a046ac086ed697b2e57b887
Chris Cardelle <chris.cardelle@gmail.com>
2019-05-14 13:54:21 -0400

    Initial Commit

--- starter-code/src/App.js
+++ starter-code/src/App.js
@@ -1,18 +1,82 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+// import TableComponent from "./TableComponent";
+import Contacts from "./contacts.json";
 
 class App extends Component {
+  state = {
+    mainCounter: 13,
+    contacts: Contacts.splice(0, 5),
+    randomContacts: Contacts.splice(Math.floor(Math.random() * 100), 5),
+    addContacts: Contacts
+  };
+  addContact = () => {
+    let contactsCopy = [...this.state.contacts];
+    let i = Math.floor(Math.random() * 200);
+    contactsCopy.push(this.state.addContacts[i]);
+    this.setState({
+      contacts: contactsCopy
+    });
+    console.log("TESTING ADD");
+  };
+
+  deleteContact = i => {
+    {
+      let contactsCopy = [...this.state.contacts];
+      contactsCopy.splice(i, 1);
+      this.setState({
+        contacts: contactsCopy
+      });
+    }
+  };
+
+  displayAll = () => {
+    return this.state.contacts.map((oneContact, i) => {
+      return (
+        <tr key={i}>
+          <td>
+            <img className="contactpic" src={oneContact.pictureUrl} />
+          </td>
+          <td>{oneContact.name}</td>
+          <td>{oneContact.popularity}</td>
+          <td>
+            <button
+              onClick={() => {
+                this.deleteContact(i);
+              }}
+            >
+              Delete
+            </button>
+          </td>
+        </tr>
+      );
+    });
+  };
+
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <h1 className="App-title">IronContacts</h1>
+        <button
+          onClick={() => {
+            this.addContact();
+          }}
+        >
+          Add A Random Contact
+        </button>
+        <button>Sort By Name</button>
+        <button>Sort By Popularity</button>
+        <table className="main-table">
+          <tbody>
+            <tr>
+              <th>Picture</th>
+              <th>Name</th>
+              <th>Popularity</th>
+            </tr>
+            {this.displayAll()}
+          </tbody>
+        </table>
       </div>
     );
   }
---
+++ starter-code/src/TableComponent.js
@@ -0,0 +1,30 @@
+// import React from "react";
+// import Contacts from "./contacts.json";
+
+// class TableComponent extends React.Component {
+//   state = {
+//     newContacts: Contacts
+//   };
+
+// }
+
+// export default TableComponent;
+
+// newContacts.forEach(item => {
+//   <table>
+//     <tr>
+//       <th>Picture</th>
+//       <th>Name</th>
+//       <th>Popularity</th>
+//     </tr>
+
+//     <tr>
+//       <td>
+//         <img src="{item.pictureUrl}" />
+//         {item.pictureUrl}
+//       </td>
+//       <td>{item.name}</td>
+//       <td>{item.popularity}</td>
+//     </tr>
+//   </table>;
+// });
