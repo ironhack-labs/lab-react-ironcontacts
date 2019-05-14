38589e747edeb54006a3cb49ede6f988b7521667
Kenneth Solano <klsolano77@gmail.com>
2019-05-14 15:23:03 -0400

    s

--- starter-code/src/App.js
+++ starter-code/src/App.js
@@ -1,18 +1,100 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import contacts from "./contacts.json";
 
 class App extends Component {
+  state = {
+    contactsList: contacts.slice(0, 5),
+    contacts: contacts,
+    filteredContacts: contacts
+    // sortedOutContacts: contacts
+  };
+
+  iAddedContact = e => {
+    let randomContact = this.state.contacts[
+      Math.floor(Math.random() * this.state.contacts.length + 1)
+    ];
+    let contactsList = this.state.contactsList;
+
+    let contactsListCopy = [...this.state.contactsList];
+
+    if (contactsList.includes(randomContact)) {
+      this.iAddedContact();
+      console.log("pappppiiiiiii");
+    } else {
+      contactsListCopy.push(randomContact);
+      this.setState({
+        contactsList: contactsListCopy
+      });
+    }
+  };
+
+  iSortedContacts = e => {
+    let sortedContacts = this.state.contactsList;
+    this.setState({
+      contactsList: sortedContacts.sort((a, b) => {
+        return a.name > b.name ? 1 : a.name < b.name ? -1 : 0;
+      })
+    });
+  };
+
+  iPopulatedContacts = e => {
+    let sortedContacts = this.state.contactsList;
+    this.setState({
+      contactsList: sortedContacts.sort((a, b) => {
+        return a.popularity > b.popularity
+          ? 1
+          : a.popularity < b.popularity
+          ? -1
+          : 0;
+      })
+    });
+  };
+
+  deletedContacts = i => {
+    console.log();
+    let contactCopy = [...this.state.contactsList];
+    contactCopy.splice(i, 1);
+    this.setState({
+      contactsList: contactCopy
+    });
+  };
+
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <h1>Contacts</h1>
+        <button className="first-btn" onClick={this.iAddedContact}>
+          Add random Contact
+        </button>
+        <button className="second-btn" onClick={this.iSortedContacts}>
+          Sort Contact
+        </button>
+        <button className="third-btn" onClick={this.iPopulatedContacts}>
+          Popularity
+        </button>
+
+        <div class="table">
+          {this.state.contactsList.map((contact, i) => {
+            return (
+              <tr key={i}>
+                <td>
+                  <img src={contact.pictureUrl} />
+                </td>
+                <td>{contact.name}</td>
+                <td>{contact.popularity}</td>
+
+                <button
+                  className="fourth-btn"
+                  onClick={() => this.deletedContacts(i)}
+                >
+                  Delete
+                </button>
+              </tr>
+            );
+          })}
+        </div>
       </div>
     );
   }
