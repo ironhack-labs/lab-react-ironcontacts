645a86259512a8b387e57fc1a3f195cec8752fda
Fran Enriquez <fjenriglez@gmail.com>
2019-06-02 17:05:09 +0200

    fixed bugs

--- starter-code/src/App.js
+++ starter-code/src/App.js
@@ -12,24 +12,30 @@ class App extends React.Component {
   }
 
   addContact = () => {
-    const ramdonContact = contacts.slice(this.state.choosenContacts.length, this.state.choosenContacts.length+1)
-    this.setState({
-      choosenContacts: [...this.state.choosenContacts, ...ramdonContact]
-    })
+    let newCon = contacts[(Math.floor(Math.random() * contacts.length))]
+    if (this.state.choosenContacts.length !== contacts.length && this.state.choosenContacts.find(e => e.name === newCon.name) === undefined) {
+      this.setState({
+        choosenContacts: [...this.state.choosenContacts, newCon]
+      })
+    } else if (this.state.choosenContacts.length ) {
+      document.getElementById('addActor').disabled = true;
+    } else {
+      this.addContact()
+    }
   }
 
   deleteContact = (contact) => {
     this.setState({
       choosenContacts: this.state.choosenContacts.filter(contactToDel => contactToDel.name !== contact.name)
     })
+    document.getElementById('addActor').disabled = false;
   }
 
   sortByName = () => {
 
     this.setState({
       choosenContacts: this.state.choosenContacts.sort((a, b) => {
-        if (a.name < b.name) { return -1 }
-        if (b.name < a.name) { return 1 }
+        if (a.name < b.name) { return -1 } else if (b.name < a.name) { return 1 }
         return 0
       })
     })
@@ -49,7 +55,7 @@ class App extends React.Component {
             <h1 className="display-4">IronContacts</h1>
           </div>
         </div>
-        <ButtonActions text="Add Ramdon Actor" action={this.addContact}/>
+        <ButtonActions id="addActor" text="Add Ramdon Actor" action={this.addContact}/>
         <ButtonActions text="Sort by name" action={this.sortByName}/>
         <ButtonActions text="Sort by popularity" action={this.sortByPopularity}/>
         <ListContacts choosenContacts={this.state.choosenContacts} deleteContact={this.deleteContact}/>
--- starter-code/src/component/ButtonAction.js
+++ starter-code/src/component/ButtonAction.js
@@ -1,8 +1,9 @@
 import React from 'react';
 
-const ButtonActions = ({text, action}) => (
+const ButtonActions = ({text, action, id}) => (
   <button 
     type="button" 
+    id={id}
     className="my-3 ml-1 btn btn-dark" 
     onClick={action}>{text}</button>
 );
