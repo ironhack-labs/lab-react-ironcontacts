fc5d2edbc707d5607122ed2ea6d1404b8573a997
Juliette <wilkorguitarrista@.com>
2019-06-02 14:26:03 -0300

    exercicio finalizado, conforme solicitado.

---
+++ starter-code/src/components/App.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+// import logo from './logo.svg';
+import './App.css';
+import Contacts from './Contacts';
+
+class App extends Component {
+  render() {
+    return (
+      <main className="container">
+      <Contacts/>
+      </main>
+    );
+  }
+}
+
+export default App;
+
