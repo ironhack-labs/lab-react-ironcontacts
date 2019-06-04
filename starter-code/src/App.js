bb47b40c5579d8d5a533b2989d482ffdc35c6a28
Marnelas <marnelasbenito@gmail.com>
2019-06-04 17:10:46 +0200

    interaction 2 complete

--- starter-code/src/App.js
+++ starter-code/src/App.js
@@ -5,20 +5,36 @@ import contacts from "./contacts.json"
 import Table from "./components/Table"
 
 class App extends Component {
-  render() {
+  constructor(){
+    super()
+    this.state = {
 
-    let firstContacts = []
-
-    for(let i = 0;i < 6; i++){
-      firstContacts.push(contacts[i])
+      firstContacts: [contacts[0],contacts[1],contacts[2],contacts[3],contacts[4]]
     }
+  }
+ 
+  addRandom = () =>{
+    let randomNumber = Math.floor(Math.random() * contacts.length);
+    console.log(randomNumber)
+    const firstContactsCopy = [...this.state.firstContacts]
+    firstContactsCopy.push(contacts[randomNumber])
+    this.setState({
+      firstContacts:firstContactsCopy
+    })
+    console.table(this.state.firstContacts);
+    
+  }
+  
 
+  render() {
     return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Welcome to React</h1>
         </header>
+        <button onClick={() => this.addRandom()}>Add Random Contact</button>
+
         <table className="container">
           <thead>
             <tr className="title-table">
@@ -28,7 +44,7 @@ class App extends Component {
             </tr>
           </thead>
           <tbody>
-            {firstContacts.map((elm, idx) => (
+            {this.state.firstContacts.map((elm, idx) => (
               <Table
                 key={idx}
                 name={elm.name}
