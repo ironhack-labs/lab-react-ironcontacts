a752d821b8a3ffc040d60a9aeefa4a6bcd34181a
Alejandro <alejandro.galvez.rico@gmail.com>
2019-07-16 23:50:26 -0500

    first 2 iterations

--- starter-code/src/App.js
+++ starter-code/src/App.js
@@ -1,18 +1,58 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
+import Contact from "./components/Contact";
+import contactsList from "./contacts.json";
+import ObjectComponent from "./components/Contact";
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state = { contacts: contactsList.splice(0, 5) };
+  }
+
+  addRandomArtist = () => {
+    const { contacts } = this.state;
+
+    const randomNumber = Math.floor(Math.random() * contacts.length);
+    contacts.push(contactsList[randomNumber]);
+    console.log(this.state.contacts);
+    this.setState({
+      contacts
+    });
+  };
+
+  sortName = () => {
+    const { contacts } = this.state;
+    this.state.contacts.sort();
+    this.setState({
+      contacts
+    });
+  };
+
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
+        <button onClick={() => this.addRandomArtist()}>
+          Add random Artist
+        </button>
+        <button onClick={() => this.sortName()}>Sort by Name</button>
+        <button onClick={() => this.sortPopularity()}>
+          Sort by Popularity
+        </button>
+
+        <table>
+          <th>Picture</th>
+          <th>Name</th>
+          <th>Popularity</th>
+          {this.state.contacts.map(e => (
+            <tr>
+              <img src={e.pictureUrl} width="180px" /> {e.name} {e.popularity}
+            </tr>
+          ))}
+        </table>
       </div>
     );
   }
