33a40e313e625525df8a906d37e061305408f5ab
mariana-gamino <mariianagamino@gmail.com>
2019-07-17 00:40:12 -0500

    Almost finished

--- starter-code/src/App.js
+++ starter-code/src/App.js
@@ -1,18 +1,62 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import contacts from './contacts.json'
+import HomeContacts from './components/HomeContacts'
+import { Button } from "antd";
+
+const contactArray = contacts.map((contact, i) => {
+  return {...contact, i}
+})
 
 class App extends Component {
+  state = {
+    initialContacts: contactArray.slice(0,5)
+  }
+
+  addRandomContact = () => <div className="">Aiuda</div>
+
+	sortByName = () => {
+		const contacts = [...this.state.initialContacts];
+		this.setState({ initialContacts: contacts.sort((a, b) => {
+      if (a.name > b.name) return 1
+      if (a.name < b.name) return -1
+      return 0
+    }
+    )})
+  }
+
+	sortByPopularity = () => {
+		const contacts = [...this.state.initialContacts];
+    this.setState({ initialContacts: contacts.sort((a, b) =>
+      { 
+      if (a.popularity > b.popularity) return 1
+      if (a.popularity < b.popularity) return -1
+      return 0
+      }
+      )})
+	}
+
+	removeContact = id => {
+		const contacts = [...this.state.initialContacts];
+		this.setState({ initialContacts: contacts.filter(c => c.id !== id) });
+	};
+
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <h1>Iron Contacts</h1>
+        <div className="buttons">
+          <Button onClick={this.addRandomContact}>
+						Add Random Contact
+					</Button>
+					<Button onClick={this.sortByName}>
+						Sort by name
+					</Button>
+					<Button onClick={this.sortByPopularity}>
+						Sort by popularity
+					</Button>
+        </div>
+        <HomeContacts contacts={this.state.initialContacts} removeContact ={this.removeContact}/>
       </div>
     );
   }
---
+++ starter-code/src/components/HomeContacts.js
@@ -0,0 +1,35 @@
+import React from 'react'
+import { Table, Button } from 'antd'
+
+function HomeContacts(props){
+  const columns = [
+    {
+      title: 'Picture',
+      dataIndex: 'pictureUrl',
+      key: 'pictureUrl',
+      render: picture => <img src={picture} alt="img" width = "100"/>
+    },
+    {
+      title: 'Name',
+      dataIndex: 'name',
+      key: 'name',
+      render: name => <b>{name}</b>
+    },
+    {
+      title: 'Popularity',
+      dataIndex: 'popularity',
+      key: 'popularity',
+      render: popularity => <b>{popularity.toFixed(2)}</b>
+    }
+    // {
+    //   title: 'Action',
+    //   key: 'action',
+    //   render: action => (
+    //     <Button onClick = {props.removeContact(action)}></Button>
+    //   ),
+    // }
+  ]
+  return <Table dataSource={props.contacts} columns={columns} />
+}
+
+export default HomeContacts
