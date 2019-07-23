4b26234181cc3eda9753f0e9509e5a853bc4758f
Sergui Morejon <serguinajibm20@gmail.com>
2019-07-23 11:13:12 -0400

    rounding popularity

--- starter-code/src/components/Table.js
+++ starter-code/src/components/Table.js
@@ -18,7 +18,7 @@ function Table(props) {
                   <tr key={index}>
                     <td><img className="movie-img" src={contact.pictureUrl} alt="movie-img"/></td>
                     <td>{contact.name}</td>
-                    <td>{contact.popularity}</td>
+                    <td>{contact.popularity.toFixed(2)}</td>
                     <td><button id={index} className="btn" onClick={props.onClick}>Delete</button></td>
                   </tr>
                 )
