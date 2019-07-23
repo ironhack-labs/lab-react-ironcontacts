1287f9a1388b6ee245cb544e6c12834918d17466
dgentil13 <ahoydanni3@gmail.com>
2019-07-23 17:55:00 -0300

    Done

--- starter-code/src/App.js
+++ starter-code/src/App.js
@@ -1,18 +1,84 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+// import contacts json
+import contacts from './contacts.json'
+
 
 class App extends Component {
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      contacts: contacts.slice(0,5)
+    }
+  }
+      addHandler() {
+        let newContacts = [...this.state.contacts];
+        let idx = Math.floor(Math.random() * contacts.length);
+        //  pushes to array newMovies
+        newContacts.push(contacts[idx])
+        // changes the state of movies array in state obj
+        this.setState({
+          contacts: newContacts
+        });
+      }
+    
+    sortNameHandler(){
+      let sortName = [...this.state.contacts];
+
+      sortName.sort((a, b) => {
+        if(a.name < b.name) { return -1; }
+        if(a.name > b.name) { return 1; }
+        return 0;
+      })
+
+      this.setState({
+        contacts: sortName
+      })
+    }
+
+    sortPopularityHandler(){
+      let sortPop = [...this.state.contacts];
+
+      sortPop.sort((a, b) => {
+        return a.popularity - b.popularity;
+      });
+
+      this.setState({
+        contacts: sortPop
+      })
+    }
+
+    deleteHandler(index) {
+      let newContacts = [...this.state.contacts];
+      newContacts.splice(index, 1);
+
+      this.setState({
+        contacts: newContacts
+      });
+    }
+  
+
   render() {
+
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <button onClick= {() => this.addHandler()}> Add Random Contacts </button> 
+        <button onClick= {() => this.sortNameHandler()}> Sort by name </button> 
+        <button onClick= {() => this.sortPopularityHandler()}> Sort by popularity </button> 
+
+
+        {this.state.contacts.map((contact, index) =>
+
+        <div className="contacts" key={index}>
+          <img height="150" width="100" src={contact.pictureUrl}/>
+          <h4>{contact.name}</h4>
+          <p> {contact.popularity.toFixed(2)} </p>
+
+          <button className="btn-del" onClick={this.deleteHandler.bind(this, index)}> Delete</button>
+        </div>
+        )
+      }
       </div>
     );
   }
