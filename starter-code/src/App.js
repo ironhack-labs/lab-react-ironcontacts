51d3f5d0789236ed696964299aec0b06b27ff171
Mark Trevail <trevail.m@gmail.com>
2019-08-06 16:36:22 +0200

    Done v1

--- starter-code/src/App.js
+++ starter-code/src/App.js
@@ -10,7 +10,7 @@ class App extends Component {
     this.state = {
       contactsBackup: contacts,
       contactsAll: contacts,
-      contactsDisplay: contacts
+      contactsDisplay: contacts.slice(0, 3)
     };
   };
 
@@ -29,6 +29,16 @@ class App extends Component {
     this.setState({contactsAll: [...this.state.contactsBackup]});
     this.setState({contactsDisplay: [...this.state.contactsBackup]});
   }
+
+  top5 = () => {
+    this.setState({contactsDisplay: this.state.contactsAll.slice(0,5)});
+  }
+
+  addRandomContact = () => {
+    debugger
+    let randomContact = this.state.contactsAll[Math.floor(Math.random()*this.state.contactsAll.length)];
+    this.setState({contactsDisplay: [...this.state.contactsDisplay, randomContact]});    
+  };
 
   searchContacts = (event) => {
     let searchTerm = event.target.value;
@@ -67,10 +77,12 @@ class App extends Component {
         <h1>IronContacts</h1>
 
         <input onChange={this.searchContacts} placeholder='Search for contact' type='search'></input>
+        <button onClick={this.addRandomContact}>Add random contact</button>
+        <button onClick={this.top5}>Top 5</button>
         <button onClick={this.sortContactsByName}>Sort by Name</button>
         <button onClick={this.sortContactsByPopularity}>Sort by Popularity</button>
         <button onClick={this.resetRemovals}>Reset removals</button>
-
+        
         <table>
           <thead>
             <tr>
