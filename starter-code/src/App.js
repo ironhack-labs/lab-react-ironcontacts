0ab7071f5510c00e41ba3a9cc0fff62b51eb50c3
Luis Orihuela <luis.orihuela20@gmail.com>
2019-08-21 18:17:17 -0500

    initial commit

--- starter-code/src/App.js
+++ starter-code/src/App.js
@@ -1,21 +1,32 @@
-import React, { Component } from 'react';
+import React, {useState, useContext} from 'react';
 import logo from './logo.svg';
 import './App.css';
+import contacts from './contacts.json'
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
-  }
+
+function App() {
+  
+  const [contactList, setContact] = useContext(contacts);
+  console.log(contactList)
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <h1 className="App-title">Welcome to React</h1>
+      </header>
+      <p className="App-intro">
+        To get started, edit <code>src/App.js</code> and save to reload.
+      </p>
+    </div>
+  );
 }
+  
+  
+
+ 
+
+  
+    
+  
 
 export default App;
