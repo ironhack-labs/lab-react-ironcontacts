67ee265f74a1cd5893e3fe121466229a7e6bad15
Daniel Garcia <jdanielink@gmail.com>
2019-09-24 20:33:41 -0500

    done :octocat:

--- starter-code/src/App.js
+++ starter-code/src/App.js
@@ -1,18 +1,137 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import { Table, Button, Icon } from 'antd';
+import contactsRaw from './contacts.json';
 import './App.css';
 
+const contacts = contactsRaw.map((contact, index) => {
+  return { ...contact, key: index };
+});
+
 class App extends Component {
+  state = {
+    contacts: [contacts[0], contacts[1], contacts[2], contacts[3], contacts[4]]
+  };
+
+  addRandom = () => {
+    this.setState(prevState => {
+      const randomNumber = Math.floor(Math.random() * (contacts.length - 1));
+
+      let isRepeated;
+
+      prevState.contacts.forEach(contact => {
+        if (contact.key === contacts[randomNumber].key) isRepeated = true;
+      });
+
+      if (isRepeated) return this.addRandom();
+
+      return { contacts: [...prevState.contacts, contacts[randomNumber]] };
+    });
+  };
+
+  sortByName = () => {
+    this.setState(prevState => {
+      const { contacts } = prevState;
+
+      contacts.sort((a, b) => {
+        if (a.name > b.name) return 1;
+        else if (a.name < b.name) return -1;
+        return 0;
+      });
+
+      return { contacts };
+    });
+  };
+
+  sortByPopularity = () => {
+    this.setState(prevState => {
+      const { contacts } = prevState;
+
+      contacts.sort((a, b) => a.popularity - b.popularity);
+
+      return { contacts };
+    });
+  };
+
+  deleteOne = key => () => {
+    this.setState(prevState => {
+      const contacts = prevState.contacts.filter(contact => contact.key !== key);
+
+      return { contacts };
+    });
+  };
+
   render() {
+    const columns = [
+      {
+        title: 'Picture',
+        dataIndex: 'pictureUrl',
+        key: 'picture',
+        render: img => (
+          <div
+            style={{
+              width: '50px',
+              height: '50px',
+              borderRadius: '50%',
+              overflow: 'hidden'
+            }}
+          >
+            <img
+              style={{
+                width: '100%',
+                objectFit: 'contain'
+              }}
+              src={img}
+              alt="avatar"
+            />
+          </div>
+        )
+      },
+      {
+        title: 'Name',
+        dataIndex: 'name',
+        key: 'name'
+      },
+      {
+        title: 'Popularity',
+        dataIndex: 'popularity',
+        key: 'popularity'
+      },
+      {
+        title: 'Delete',
+        dataIndex: 'key',
+        key: 'key',
+        render: key => (
+          <Button type="danger" onClick={this.deleteOne(key)}>
+            <Icon type="delete" /> Delete
+          </Button>
+        )
+      }
+    ];
+
+    const { contacts } = this.state;
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div>
+        <h1
+          style={{
+            textAlign: 'center'
+          }}
+        >
+          Ironcontacts
+        </h1>
+        <div
+          style={{
+            display: 'flex',
+            justifyContent: 'center',
+            margin: '30px 0'
+          }}
+        >
+          <Button type="primary" onClick={this.addRandom}>
+            Add random
+          </Button>
+          <Button onClick={this.sortByName}>Sort by name</Button>
+          <Button onClick={this.sortByPopularity}>Sort by popularity</Button>
+        </div>
+        <Table columns={columns} dataSource={contacts} />
       </div>
     );
   }
