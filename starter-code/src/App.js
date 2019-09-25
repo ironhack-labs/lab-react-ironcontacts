3625bedf2fbfc865d502a121a038e6a20a592257
erivasf <elias_rf@icloud.com>
2019-09-24 23:20:01 -0500

    yayaya

--- starter-code/src/App.js
+++ starter-code/src/App.js
@@ -1,20 +1,113 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import {Table, Icon} from 'antd'
+import {Button} from 'antd'
+import contactsData   from './contacts.json'
+
+const contacts = contactsData.map((contact, i)=>{
+  return {...contact, key: i}
+})
 
 class App extends Component {
+
+  state = {
+    contacts: [contacts[0], contacts[1], contacts[2], contacts[3], contacts[4]]
+  }
+
+  addRandom = () =>{
+    this.setState((prevState) =>{
+      const randomContact = Math.floor(Math.random()*(contacts.length-1)+1)
+      let isTheSame
+
+      prevState.contacts.forEach(contact => {
+        if(contact.key === contacts[randomContact].key)isTheSame = true
+      })
+      if(isTheSame) return this.addRandom()
+      return {contacts:[...prevState.contacts, contacts[randomContact]]}
+    })
+  }
+
+  sortByName = () =>{
+    this.setState((prevState)=>{
+      const {contacts} = prevState
+      contacts.sort((a,b)=>{
+        
+        if(a.name>b.name) return 1;
+        else if(a.name<b.name) return -1;
+        else return 0
+      })
+      return {contacts}
+
+    })
+  }
+
+  sortByPopularity = () =>{
+    this.setState((prevState)=>{
+      const {contacts} = prevState
+      contacts.sort((a,b)=>{
+        return b.popularity - a.popularity
+      })
+      return {contacts}
+
+    })
+  }
+
+  deleteOne = (key)=> () => {
+    this.setState((prevState)=>{
+      const contacts = prevState.contacts.filter((contact)=> contact.key !==key
+      )
+      return{contacts}
+    })
+  }
+
   render() {
+    const data = [
+      {
+        title: 'Picture',
+        dataIndex: 'pictureUrl',
+        key: 'picture',
+        render: img =>(
+          <div style=
+          {{
+            width: '150px',
+            height: '150px',
+            borderRadius: '50%',
+            overflow: 'hidden'
+          }}>
+         <img style={{
+          width: '100%'
+        }}src={img} alt="avatar"/>
+        </div>)
+      },
+      {
+        title: 'Name',
+        dataIndex: 'name',
+        key: 'name',
+      },
+      {
+        title: 'Popularity',
+        dataIndex: 'popularity',
+        key: 'popularity',
+      },
+      {
+        title: 'Delete',
+        dataIndex: 'key',
+        key: 'key',
+        render: key => <Button type="danger" onClick={this.deleteOne(key)}><Icon type="delete"/></Button>
+      }    
+    ]
+      const {contacts} = this.state
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div id="main">
+        <h1>Contacts</h1>
+        <div className="buttons">
+          <Button type= 'default' onClick={this.addRandom} >Add Random Contact</Button>
+          <Button type= 'default' onClick={this.sortByName}>Sort By Name</Button>
+          <Button type= 'default' onClick={this.sortByPopularity}>Sort By Popularity</Button>
+        </div>
+        <Table columns={data} dataSource={contacts}/>
       </div>
-    );
+    )
   }
 }
 
