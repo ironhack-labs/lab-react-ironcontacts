9d8999271d454c5d49447603421c84a1f1aa04a1
Jose Galindo <jgalindo.ironhack@gmail.com>
2019-10-01 16:01:52 -0400

    sorting iteration

--- starter-code/src/App.js
+++ starter-code/src/App.js
@@ -5,50 +5,75 @@ import contacts from './contacts.json';
 
 
 
+function compare( a, b ) {
+  if ( a.name < b.name ){
+    return -1;
+  }
+  if ( a.name > b.name ){
+    return 1;
+  }
+  return 0;
+}
+
+function compared( a, b ) {
+  if ( a.popularity < b.popularity ){
+    return -1;
+  }
+  if ( a.popularity > b.popularity ){
+    return 1;
+  }
+  return 0;
+}
+
 class App extends Component {
   // actors = contacts.splice(0,5);
   state = {
     actors : contacts.splice(0,5),
     actorsLeft: contacts
   }
-  
   showListOfActors = () => {
     console.log(this.state.actors)
     let listOfActors = this.state.actors.map((eachActor,i)=> {
       console.log(eachActor.name)
-      return <tr >
+      return <tr key={i} >
         <td><img width="100px" src={eachActor.pictureUrl}></img></td>
         <td>{eachActor.name}</td>
         <td>{eachActor.popularity}</td>
+        <button>Delete</button>
       </tr>
     })
     return listOfActors
 }
 
-
-
-
-getRandomActor = () => {
+  getRandomActor = () => {
       let allActors = [...this.state.actors]
       let randomIndex = Math.floor(Math.random()*(allActors.length))
       allActors.push(contacts[randomIndex])
-      
       this.setState({
-          actors: allActors
+          actors: allActors,
       })
-}
+  }
 
+  sortByName = ()=>{
+    this.setState({
+      actors:[...this.state.actors].sort( compare )
+    });
 
+  }
+  sortByPopularity = ()=>{
+    this.setState({
+      actors:[...this.state.actors].sort( compared )
+    });
 
-
-
+  }
 
   render() {
     return (
       <div className="App">
         <h1>IronContacts</h1>
         <button onClick={this.getRandomActor} className="button">Add Random Actor</button>
-
+        <button onClick={this.sortByName} className="button">Sort By Name</button>
+        <button onClick={this.sortByPopularity} className="button">Sort By Popularity</button>
         <table >
             <tr>
               <th>PICTURE</th>
@@ -62,7 +87,6 @@ getRandomActor = () => {
     );
   }
 }
-
 export default App;
 
 
