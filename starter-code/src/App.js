000a4073bbebb7713cb7d6de9ab19214da74003d
Jose Galindo <jgalindo.ironhack@gmail.com>
2019-10-01 14:22:52 -0400

    second iteration

--- starter-code/src/App.js
+++ starter-code/src/App.js
@@ -6,43 +6,21 @@ import contacts from './contacts.json';
 
 
 class App extends Component {
-  
+  // actors = contacts.splice(0,5);
   state = {
-    actors : [
-      {
-        "name": "Idris Elba",
-        "pictureUrl": "https://image.tmdb.org/t/p/w500/d9NkfCwczP0TjgrjpF94jF67SK8.jpg",
-        "popularity": 11.622713
-      },
-      {
-        "name": "Jessica Chastain",
-        "pictureUrl": "https://image.tmdb.org/t/p/w500/nkFrkn5NZVGWb4b2X0yIcXezhyt.jpg",
-        "popularity": 8.324357
-      },
-      {
-        "name": "Johnny Depp",
-        "pictureUrl": "https://image.tmdb.org/t/p/w500/kbWValANhZI8rbWZXximXuMN4UN.jpg",
-        "popularity": 15.656534
-      },
-      {
-        "name": "Emilia Clarke",
-        "pictureUrl": "https://image.tmdb.org/t/p/w500/j7d083zIMhwnKro3tQqDz2Fq1UD.jpg",
-        "popularity": 16.211837
-      },
-      {
-        "name": "Leonardo DiCaprio",
-        "pictureUrl": "https://image.tmdb.org/t/p/w500/A85WIRIKVsD2DeUSc8wQ4fOKc4e.jpg",
-        "popularity": 11.245333
-      }
-    ]
+    actors : contacts.splice(0,5),
+    actorsLeft: contacts
   }
   
   showListOfActors = () => {
     console.log(this.state.actors)
     let listOfActors = this.state.actors.map((eachActor,i)=> {
       console.log(eachActor.name)
-      return <div><li key={i}>{eachActor.name}</li><img src={eachActor.pictureUrl}></img>
-      </div>
+      return <tr >
+        <td><img width="100px" src={eachActor.pictureUrl}></img></td>
+        <td>{eachActor.name}</td>
+        <td>{eachActor.popularity}</td>
+      </tr>
     })
     return listOfActors
 }
@@ -50,24 +28,36 @@ class App extends Component {
 
 
 
-  
+getRandomActor = () => {
+      let allActors = [...this.state.actors]
+      let randomIndex = Math.floor(Math.random()*(allActors.length))
+      allActors.push(contacts[randomIndex])
+      
+      this.setState({
+          actors: allActors
+      })
+}
+
+
+
+
+
 
   render() {
     return (
       <div className="App">
         <h1>IronContacts</h1>
+        <button onClick={this.getRandomActor} className="button">Add Random Actor</button>
 
-        <table className="table">
+        <table >
             <tr>
               <th>PICTURE</th>
               <th>NAME</th> 
               <th>POPULARITY</th>
            </tr>
+           { this.showListOfActors()}
         </table>
-
-         <ul>
-          { this.showListOfActors()}
-         </ul>
+        
       </div>
     );
   }
