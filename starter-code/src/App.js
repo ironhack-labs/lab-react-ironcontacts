45bc0440054344693be0c610387322051e9b9aea
Elba Chimilio <elbachimilio@gmail.com>
2019-10-01 10:32:05 -0400

    displayed list of names

--- starter-code/src/App.js
+++ starter-code/src/App.js
@@ -1,8 +1,25 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import contacts from './contacts.json';
 
 class App extends Component {
+  state = {
+    allContacts: contacts
+  }
+
+  // showContacts = () => {
+  //   <p>{this.state.contacts}</p>
+  //   // return contactsList = contacts.map()
+  // }
+
+  showListOfContacts = () => {
+    let listOfContacts = this.state.allContacts.map((eachContact,i)=> {
+        return <li>{eachContact.name}</li>;
+    })
+    return listOfContacts;
+}
+
   render() {
     return (
       <div className="App">
@@ -13,6 +30,9 @@ class App extends Component {
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
+        <table>
+        {this.showListOfContacts()}
+        </table>
       </div>
     );
   }
