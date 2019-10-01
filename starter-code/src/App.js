6afc174bd07c273060e0e0442c5e6625770b9b95
“Natalia <natgarea@gmail.com>
2019-10-01 15:22:11 +0200

    iteration 2: adds random contacts

--- starter-code/src/App.js
+++ starter-code/src/App.js
@@ -7,27 +7,46 @@ class App extends Component {
   constructor(props) {
     super(props);
 
-    this.firstFive = contacts.slice(0, 5);
+    this.state = {
+      contacts: contacts.slice(0, 5)
+    };
   }
+  addRandomContact() {
+    let randomNumber = Math.floor(Math.random() * contacts.length);
+    let randomContact = contacts[randomNumber];
 
+    this.setState({
+      ...this.state,
+      contacts: [...this.state.contacts, randomContact]
+    });
+
+    console.log(this.setState.contacts)
+  }
   render() {
     return (
       <div className="App">
         <h1>IronContacts</h1>
+        <button onClick={() => this.addRandomContact()}>
+          Add random contact
+        </button>
         <table className="contacts">
-          <tr>
-            <th>Picture</th>
-            <th>Name</th>
-            <th>Popularity</th>
-          </tr>
-          {this.firstFive.map((contact, i) => (
-            <Contact
-              key={i}
-              pictureUrl={contact.pictureUrl}
-              name={contact.name}
-              popularity={contact.popularity}
-            ></Contact>
-          ))}
+          <thead>
+            <tr>
+              <th>Picture</th>
+              <th>Name</th>
+              <th>Popularity</th>
+            </tr>
+          </thead>
+          <tbody>
+            {this.state.contacts.map((contact, i) => (
+              <Contact
+                key={i}
+                pictureUrl={contact.pictureUrl}
+                name={contact.name}
+                popularity={contact.popularity}
+              ></Contact>
+            ))}
+          </tbody>
         </table>
       </div>
     );
