7f778be1f045951e72216f6bfcf420572d24a804
Nick Sherman <ogleffeckt2@yahoo.com>
2019-10-01 12:30:01 -0400

    first commit

---
+++ starter-code/src/components/Row.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Row = (props) => {
+  return (
+      <tr>
+        <td><img src={props.pictureUrl} alt={props.name} width="70px" /></td>
+        <td>{props.name}</td>
+        <td>{props.popularity.toFixed(2)}</td>
+        <td><button className="delete-btn" onClick={props.deleteHandler}>Delete</button></td>
+      </tr>
+  );
+};
+
+export default Row;
