2b0203d7d4f7e6b69a8c839f8fe53d5ae7d2ed44
Elba Chimilio <elbachimilio@gmail.com>
2019-10-02 11:51:25 -0400

    Done; not finish styling

--- starter-code/src/App.js
+++ starter-code/src/App.js
@@ -1,37 +1,76 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 import contacts from './contacts.json';
 
 class App extends Component {
+
   state = {
-    allContacts: contacts
+    currentList : contacts.splice(0,5),
+    allContacts : contacts
   }
 
-  // showContacts = () => {
-  //   <p>{this.state.contacts}</p>
-  //   // return contactsList = contacts.map()
-  // }
+  deleteName = (index) => {
+    let everyContact = [...this.state.currentList]
+    everyContact.splice(index,1)
+      this.setState({
+        currentList : everyContact
+      })
+  }
 
-  showListOfContacts = () => {
-    let listOfContacts = this.state.allContacts.map((eachContact,i)=> {
-        return <li>{eachContact.name}</li>;
+  getContacts = () => {
+    let contact = this.state.currentList.map((contact, i)=>{
+      return <tr className='contact-info' key={i}> 
+              <td><img src={contact.pictureUrl} alt='celebrity'/></td>
+              <td>{contact.name}</td>
+              <td>{contact.popularity}</td>
+              <td><button onClick={()=>this.deleteName(i)}> Delete </button></td>
+             </tr>
     })
-    return listOfContacts;
-}
+    return contact;
+  }
+
+  addRandomContact = () => {
+    let everyContact = [...this.state.currentList]
+    let randomIndex = Math.floor(Math.random()*(this.state.allContacts.length))
+    let newContact = this.state.allContacts[randomIndex]
+    everyContact.push(newContact);
+      this.setState({
+        currentList : everyContact
+      })
+  }
+
+  sortName = () => {
+    let everyContact = this.state.currentList.sort((a, b) => (a.name > b.name) ? 1 : -1)
+      this.setState({
+        currentList : everyContact
+      })
+  }
+
+  sortPopularity = () => {
+    let everyContact = this.state.currentList.sort((a, b) => (a.popularity > b.popularity) ? -1 : 1)
+      this.setState({
+        currentList : everyContact
+      })
+  }
 
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <button onClick={this.addRandomContact}>Add Random Contact</button>
+        <button onClick={this.sortName}>Sort by Name</button>
+        <button onClick={this.sortPopularity}>Sort by Popularity</button>
+
         <table>
-        {this.showListOfContacts()}
+          <thead>
+            <tr>
+              <th>Picture</th>
+              <th>Name</th>
+              <th>Popularity</th>
+            </tr>
+          </thead>
+          <tbody>
+            {this.getContacts()}
+          </tbody>
         </table>
       </div>
     );
