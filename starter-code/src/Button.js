4e3fc6752d24e8b5ba891ded41b312df60bbb052
chris bolivar <christopherbolivar@hotmail.com>
2019-10-02 10:50:13 -0400

    done

---
+++ starter-code/src/Button.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class Button extends Component {
+    render() {
+        return (
+            <React.Fragment>
+                <button onClick={this.props.onClick}>{this.props.children}</button>
+            </React.Fragment>
+        )
+    }
+}
