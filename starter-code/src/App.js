7331288fd9802f8ff16b2e7310ea328b79e44bca
bruno <brunno-dev@gmail.com>
2019-10-16 07:14:14 -0300

    update

--- starter-code/src/App.js
+++ starter-code/src/App.js
@@ -1,18 +1,86 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
+import contacts from './contacts.json';
 
 class App extends Component {
+  constructor(props) {
+    super (props)
+    this.AddContact = this.AddContact.bind(this);
+  }
+    state = {
+      contacts: contacts.slice(0,5)
+    }
+    AddContact() {
+    let max = contacts.length - 1
+    let min = this.state.contacts.length
+    let newContacts = [...this.state.contacts];
+    console.log(newContacts)
+    newContacts.push(contacts[(Math.floor(Math.random() * (max - min + 1)) + min)])
+    this.setState({
+      contacts: newContacts
+      })
+    }
+    sortByName = ()=>{
+      let sortedList = [...this.state.contacts].sort((a,b) => a.name.localeCompare(b.name))
+      // console.log(sortedList)
+      this.setState ({
+        contacts:sortedList
+      })
+    }
+    sortByPopularity = ()=>{
+      let sortedPopularity = [...this.state.contacts].sort((a,b) => b.popularity - a.popularity)
+      // console.log(sortedPopularity)
+      this.setState ({
+        contacts:sortedPopularity
+      })
+    }
+
+    deleteContact(index) {
+      let newContacts = [...this.state.contacts];
+  
+      newContacts.splice(index, 1)
+      this.setState({
+        contacts: newContacts
+      });
+    }
+
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+        <header>
+          <h1 className="App-title">IronContacts</h1>
         </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <button className ="button-random-contact" onClick = {this.AddContact}>Add Random Contact</button>
+        <button className ="button-sort-name" onClick = {this.sortByName}>Sort By Name</button>
+        <button className ="button-sort-popularity" onClick = {this.sortByPopularity}>Sort By Popularity</button>
+        
+        <div className="Contact-Table">
+        <table>
+          <thead>
+            <tr>
+              <th>Picture</th>
+              <th>Name</th>
+              <th>Popularity</th>
+            </tr>
+          </thead>
+          <tbody className = "body-artists">
+          {this.state.contacts.map((contact,index) => 
+               ( 
+                <tr key={index}>
+                  <td><img className="Contact-Image" alt="" src={contact.pictureUrl}></img></td>
+                  <td>{contact.name}</td>
+                  <td>{contact.popularity.toFixed(2)}</td>
+                  <td><button className="delete-btn" onClick={this.deleteContact.bind(this, index)}>Delete</button></td>
+                </tr>
+              )
+            )
+          }
+         
+          </tbody>
+        </table>
+
+        </div>
       </div>
     );
   }
