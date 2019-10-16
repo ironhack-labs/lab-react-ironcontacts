118dbbdb624295f4ffa5c3ebcd2f4696530b277b
Adamy <viniciusbohreradamy@gmail.com>
2019-10-15 23:45:29 -0300

    Done

--- starter-code/src/App.js
+++ starter-code/src/App.js
@@ -1,18 +1,73 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import contacts from './contacts.json';
 
 class App extends Component {
+  state = {
+    myContacts: contacts.slice(0, 5)
+  }
+
+  addRandomContact = () => {
+    if (this.state.myContacts.length < 200) {
+      this.setState({
+        myContacts: [...this.state.myContacts, contacts[this.state.myContacts.length]]
+      });
+    }  
+  }
+
+  sortByName = () => {
+    this.setState({
+      myContacts: this.state.myContacts.sort((a, b) => {
+        return a.name.localeCompare(b.name);
+      })
+    });
+  }
+
+  sortByPopularity = () => {
+    this.setState({
+      myContacts: this.state.myContacts.sort((a, b) => {
+        return a.popularity - b.popularity;
+      })
+    });
+  }
+
+  deleteContact = (index) => {
+    this.state.myContacts.splice(index, 1);
+    this.setState({
+      myContacts: this.state.myContacts
+    });
+  }
+
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <h1 className="app-header">IronContacts</h1>
+
+        <div className="btns-container">
+          <button className="myBtn" onClick={this.addRandomContact}>Add Random Contact</button>
+          <button className="myBtn" onClick={this.sortByName}>Sort by name</button>
+          <button className="myBtn" onClick={this.sortByPopularity}>Sort by popularity</button>
+        </div>
+
+        <div className="contacts-container">
+          <div className="contacts-container__row">
+            <h5 className="contacts-container__row--header">Picture</h5>
+            <h5 className="contacts-container__row--header">Name</h5>
+            <h5 className="contacts-container__row--header">Popularity</h5>
+            <h5 className="contacts-container__row--header">Action</h5>
+          </div>
+
+          {this.state.myContacts.map((currentActor, index) => {
+            return (
+               <div className="contacts-container__row" key={index}>
+                <img className="contacts-container__row--img" src={currentActor.pictureUrl} alt="Actor"/>
+                <p className="contacts-container__row--name">{currentActor.name}</p>
+                <p className="contacts-container__row--popularity">{currentActor.popularity.toFixed(2)}</p>
+                <button className="btn-delete" onClick={() => this.deleteContact(index)}>Delete</button>
+              </div>
+            )     
+          })}
+        </div>
       </div>
     );
   }
