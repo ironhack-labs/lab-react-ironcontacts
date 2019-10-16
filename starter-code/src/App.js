716907a6e5fbdfb395a1a01fbb17b9ecd65a7fd6
LsM Lucas Martins <lucas.smar@gmail.com>
2019-10-16 06:59:36 -0300

    iteration 1

--- starter-code/src/App.js
+++ starter-code/src/App.js
@@ -1,18 +1,55 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+// import logo from "./logo.svg";
+import "./App.css";
+import contacts from "./contacts.json";
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      list: this.getFirstFive()
+    };
+    this.getFirstFive = this.getFirstFive.bind(this);
+  }
+  getFirstFive() {
+    let result = [];
+    console.log(contacts);
+    for (let idx = 0; idx < 5; idx += 1) {
+      result.push(contacts[idx]);
+    }
+    // this.setState({list:result});
+    return result;
+  }
+
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+          {/* <img src={logo} className="App-logo" alt="logo" /> */}
+          <h1 className="App-title">IronContacts</h1>
         </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <div className="Table-container">
+          <table>
+            <thead>
+              <tr>
+                <th>Picture</th>
+                <th>Name</th>
+                <th>Popularity</th>
+              </tr>
+            </thead>
+            <tbody>
+              {this.state.list.map(item => (
+                <tr>
+                  <td>
+                    <img src={item.pictureUrl} alt="" />
+                  </td>
+                  <td>{item.name}</td>
+                  <td>{Math.round(item.popularity * 100) / 100}</td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+        </div>
       </div>
     );
   }
