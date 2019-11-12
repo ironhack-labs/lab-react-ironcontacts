515ff464e77b9403b5c4831a0bc9f7e297b7f3f8
CodesGM <54691076+CodesGM@users.noreply.github.com>
2019-11-12 14:11:31 -0500

    Iteration 2 is complete

--- starter-code/src/App.js
+++ starter-code/src/App.js
@@ -1,23 +1,35 @@
 import React, { Component } from "react";
 import Contacts from "./contacts.json";
 import FirstFive from "./Comps/FirstFive";
+import RandomCon from "./Comps/RandomCon";
 
 export default class App extends Component {
-  state = { contacts: Contacts };
+  state = {
+    vcontacts: Contacts.slice(0, 5),
+    icontacts: Contacts.slice(5)
+  };
+
+  addRandom = () => {
+    let icon = [...this.state.icontacts];
+    let rand = icon.splice(Math.floor(Math.random() * icon.length), 1)[0];
+    let clone = [...this.state.vcontacts];
+    if (rand) clone.unshift(rand);
+    this.setState({ vcontacts: clone, icontacts: icon });
+  };
   showFirstFive = () => {
-    return this.state.contacts.map((eachOne, i) => {
-      if (i < 5) {
-        return <FirstFive key={i} thePerson={eachOne} />;
-      }
+    return this.state.vcontacts.map((eachOne, i) => {
+      return <FirstFive key={i} thePerson={eachOne} />;
     });
   };
   render() {
     return (
       <div>
         <h1>IronContacts</h1>
-        <button className="btn btn-info">Add Random Contact</button>
-        <br />  
-        <br />  
+        <button className="btn btn-info" onClick={this.addRandom}>
+          Add Random Contact
+        </button>
+        <br />
+        <br />
         <table>
           <thead>
             <tr>
---
+++ starter-code/src/Comps/RandomCon.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+
+export default function RandomCon(props) {
+  return (
+    <tr>
+      <th className="imgTh">
+        <img
+          src={props.thePerson.pictureUrl}
+          className="imgstyle"
+          alt={props.thePerson.name}
+          title={props.thePerson.name}
+        />
+      </th>
+      <th>
+        <h4>{props.thePerson.name}</h4>
+      </th>
+      <th>{props.thePerson.popularity}</th>
+    </tr>
+  );
+}
