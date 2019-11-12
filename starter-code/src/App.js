156f8f088a207e0dfa64af5e053775631ed6c2b0
Shahriyar <shahryar102@yahoo.com>
2019-11-12 16:33:48 -0500

    first commit

--- starter-code/src/App.js
+++ starter-code/src/App.js
@@ -1,18 +1,58 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import contactsJson from './contacts.json'
+window.contactsJson=contactsJson;
 
 class App extends Component {
+
+  state={
+    contacts:contactsJson.splice(0,5),
+    restOfContacts: contactsJson
+  }
+
+
+  showContacts=()=>{
+    let allContacts = this.state.contacts.map(eachContact=>{
+      return(
+        <div className="gallery">
+        <li><span>{eachContact.name} </span><span>     </span><span>{eachContact.popularity}</span></li>
+        <img src={eachContact.pictureUrl}/>
+        </div>
+      )
+    })
+    return allContacts
+  }
+
+  addRandom=()=>{
+    let randomIndex=Math.floor(Math.random()*this.state.restOfContacts.length)
+    console.log(randomIndex)
+    //this.contacts=[...this.restOfContacts.splice(randomIndex,1)]
+    let randomContact = [...this.state.restOfContacts][randomIndex]
+    console.log(randomContact)
+    
+    
+  
+    let newContacts=[...this.state.contacts]
+    newContacts.push(randomContact)
+
+    let newRestOfContacts=[...this.state.restOfContacts]
+    newRestOfContacts.splice(randomIndex,1)
+    //this.state.restOfContacts.splice(randomIndex,1)
+
+    this.setState({
+      contacts: newContacts,
+      restOfContacts: newRestOfContacts
+    })
+    
+  }
+
   render() {
+    console.log(this.state)
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+       <button onClick={this.addRandom}>Click</button> 
+        {this.showContacts()}
       </div>
     );
   }
