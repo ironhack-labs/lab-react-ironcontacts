c656a0c69f5043dd83847f399830d8ec8618951e
mendosaurus <masaki.endo@nex.nikkei.com>
2019-11-12 13:01:28 -0500

    rfix iteration 1 using map instead of hard-code

--- starter-code/src/App.js
+++ starter-code/src/App.js
@@ -1,47 +1,56 @@
 import React, { Component } from "react";
-import contact from "./contacts.json";
+import contacts from "./contacts.json";
 import "./App.css";
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      allContacts: contacts,
+      contacts: contacts.splice(0, 5)
+    };
+  }
+
+  // Create Row
+  createRow = _ => {
+    let movieTable = this.state.contacts.map((contact, i) => {
+      return (
+        <tr key={i}>
+          <th>
+            <img src={contact.pictureUrl} alt="{contact.name}" />
+          </th>
+          <th>{contact.name}</th>
+          <th>{Math.round(contact.popularity * 100) / 100}</th>
+        </tr>
+      );
+    });
+    return movieTable;
+  };
+
+  // createTable
+  createTable = _ => {
+    return (
+      <table>
+        <thead>
+          <tr>
+            <th>Picture</th>
+            <th>Name</th>
+            <th>Popularity</th>
+          </tr>
+        </thead>
+        <tbody>{this.createRow()}</tbody>
+      </table>
+    );
+  };
+
   render() {
+    console.log(this.state.contacts);
     return (
       <>
-        <table>
-          <thead>
-            <tr>
-              <th>Picture</th>
-              <th>Name</th>
-              <th>Popularity</th>
-            </tr>
-          </thead>
-          <tbody>
-            <tr>
-              <td><img src={contact[0].pictureUrl} alt='{contact.name}'/></td>
-              <td>{contact[0].name}</td>
-              <td>{Math.round(contact[0].popularity*100)/100}</td>
-            </tr>
-            <tr>
-              <td><img src={contact[1].pictureUrl} alt='{contact.name}'/></td>
-              <td>{contact[1].name}</td>
-              <td>{Math.round(contact[1].popularity*100)/100}</td>
-            </tr>
-            <tr>
-              <td><img src={contact[2].pictureUrl} alt='{contact.name}'/></td>
-              <td>{contact[2].name}</td>
-              <td>{Math.round(contact[2].popularity*100)/100}</td>
-            </tr>
-            <tr>
-              <td><img src={contact[3].pictureUrl} alt='{contact.name}'/></td>
-              <td>{contact[3].name}</td>
-              <td>{Math.round(contact[3].popularity*100)/100}</td>
-            </tr>
-            <tr>
-              <td><img src={contact[4].pictureUrl} alt='{contact.name}'/></td>
-              <td>{contact[4].name}</td>
-              <td>{Math.round(contact[4].popularity*100)/100}</td>
-            </tr>
-          </tbody>
-        </table>
+        <div>
+          <h1>IronhackContacts</h1>
+          {this.createTable()}
+        </div>
       </>
     );
   }
