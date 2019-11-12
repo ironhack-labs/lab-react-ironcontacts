0ac3efedc58bf3d85ba30e66adf5ac13ee57eda2
jnguyen3436 <jnguyen3436@gmail.com>
2019-11-12 12:20:13 -0500

    done

--- starter-code/src/App.js
+++ starter-code/src/App.js
@@ -1,21 +1,102 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react'
+import contacts from './contacts.json'
 import './App.css';
+import Button from './Button'
 
 class App extends Component {
+  state = {
+    showContacts : contacts.splice(0,5),
+    restContacts : contacts 
+  } 
+
+  getContacts = () => {
+      let contactArray = this.state.showContacts.map((thisContact,i) => {
+        return <div><img width="25px" height="40px" src={thisContact.pictureUrl} alt="celeb"/> &nbsp; {thisContact.name} &nbsp; {thisContact.popularity} <Button onClick={()=>this.deleteContact(i)}>Delete</Button></div>
+      })
+      return contactArray
+    }
+
+
+
+  getRandomContact = () => {
+    let contactsCopy = [...this.state.showContacts]
+    let randomContactIndex = Math.floor(Math.random()*(contacts.length))
+    contactsCopy.push(contacts[randomContactIndex]);
+    contactsCopy.splice(randomContactIndex,1)
+    this.setState({
+      showContacts: contactsCopy
+    })
+
+    console.log(this.state.showContacts);
+  }
+
+ sortContacts = () =>{
+   let contactsCopy = [...this.state.showContacts]
+   let state = 0;
+   
+   let sorted = contactsCopy.sort((a,b) =>{
+    if(a.name < b.name){
+      return -1
+    }
+    if(a.name > b.name){
+      return 1
+    }
+   })
+
+    console.log(state)
+   this.setState({
+    showContacts: sorted
+  })
+
+
+
+ }
+ sortPop = () =>{
+  let contactsCopy = [...this.state.showContacts]
+  let sorted = contactsCopy.sort((a,b) =>{
+   if(a.popularity < b.popularity){
+     return 1
+   }
+   if(a.popularity > b.popularity){
+     return -1
+   }
+  })
+  this.setState({
+   showContacts: sorted
+ })
+}
+ deleteContact = (i) =>{
+   console.log('delete',i)
+   let contactsCopy = [...this.state.showContacts]
+   contactsCopy.splice(i,1)
+   this.setState({
+    showContacts: contactsCopy
+  })
+ }
+
+
+
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      
+      <div id="menu"> 
+        <h1>Iron Contacts</h1>
+        <Button style ="text-align:center;"onClick={this.getRandomContact}>Add Random Contact</Button>
+        <Button style ="text-align:center;" onClick={this.sortContacts}>Sort by Name</Button>
+        <Button style ="text-align:center;" onClick={this.sortPop}>Sort by Popularity</Button>
+        <div className="flex-inline">
+        <p>Picture &nbsp;</p>
+        <p>Name&nbsp;</p>
+        <p>popularity&nbsp;</p>
       </div>
-    );
+        <div id="contactList">
+        {this.getContacts()}
+        </div>
+      </div>
+    )
   }
 }
+
+
 
 export default App;
