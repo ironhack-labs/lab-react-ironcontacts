7b39d6c0c0aa404a347835d08a3476dcea9e44b5
mendosaurus <masaki.endo@nex.nikkei.com>
2019-11-12 11:57:04 -0500

    rounf popularity on iteration 1

--- starter-code/src/App.js
+++ starter-code/src/App.js
@@ -18,27 +18,27 @@ class App extends Component {
             <tr>
               <td><img src={contact[0].pictureUrl} alt='{contact.name}'/></td>
               <td>{contact[0].name}</td>
-              <td>{contact[0].popularity}</td>
+              <td>{Math.round(contact[0].popularity*100)/100}</td>
             </tr>
             <tr>
               <td><img src={contact[1].pictureUrl} alt='{contact.name}'/></td>
               <td>{contact[1].name}</td>
-              <td>{contact[1].popularity}</td>
+              <td>{Math.round(contact[1].popularity*100)/100}</td>
             </tr>
             <tr>
               <td><img src={contact[2].pictureUrl} alt='{contact.name}'/></td>
               <td>{contact[2].name}</td>
-              <td>{contact[2].popularity}</td>
+              <td>{Math.round(contact[2].popularity*100)/100}</td>
             </tr>
             <tr>
               <td><img src={contact[3].pictureUrl} alt='{contact.name}'/></td>
               <td>{contact[3].name}</td>
-              <td>{contact[3].popularity}</td>
+              <td>{Math.round(contact[3].popularity*100)/100}</td>
             </tr>
             <tr>
               <td><img src={contact[4].pictureUrl} alt='{contact.name}'/></td>
               <td>{contact[4].name}</td>
-              <td>{contact[4].popularity}</td>
+              <td>{Math.round(contact[4].popularity*100)/100}</td>
             </tr>
           </tbody>
         </table>
