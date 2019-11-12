ae0155ee5851be512fb9e29787d792752870c4fd
maerd19 <revolutions_333@hotmail.com>
2019-11-12 06:16:58 -0600

    first iteration

--- starter-code/src/App.js
+++ starter-code/src/App.js
@@ -1,18 +1,21 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import ContactList from './components/ContactList'
+import contacts from './contacts.json';
 import './App.css';
 
+const fiveContacts = contacts.slice(0,5);
+
 class App extends Component {
+  state = {
+    data: {
+      contacts: fiveContacts
+    }
+  }
+
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <ContactList contacts={fiveContacts}/>
       </div>
     );
   }
---
+++ starter-code/src/components/ContactList.js
@@ -0,0 +1,28 @@
+import React from 'react';
+
+const ContactList = props => {
+    console.log(props);
+    
+    return (
+        <table>
+            <thead>
+                <tr>
+                    <th>Picture</th>
+                    <th>Name</th>
+                    <th>Popularity</th>
+                </tr>
+            </thead>
+            <tbody>
+                {props.contacts.map( (contact, idx) => (
+                    <tr key={idx}>
+                        <td><img src={contact.pictureURL} alt={contact.name} width='100'/></td>
+                        <td>{contact.name}</td>
+                        <td>{contact.popularity}</td>
+                    </tr>
+                 ))}
+            </tbody>
+        </table>
+    )
+}
+
+export default ContactList;
