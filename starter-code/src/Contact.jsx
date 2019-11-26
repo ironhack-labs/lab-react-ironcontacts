74048e274a3872571130c5d5ac39c18f02420b12
Camilosorio <tech@camilo.fr>
2019-11-27 00:01:50 +0100

    Modification of Contact template

--- starter-code/src/Contact.jsx
+++ starter-code/src/Contact.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 export default function Contact({ picture, name, popularity }) {
     return (
         <tr className="contact">
-            <td>{picture}</td>
+            <td><img id="img" src={picture} alt="" /></td>
             <td>{name}</td>
             <td>{popularity}</td>
         </tr>
