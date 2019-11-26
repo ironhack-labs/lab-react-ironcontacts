0e0239c37e210ba153a00286fc291780d1e5466f
Camilosorio <tech@camilo.fr>
2019-11-27 00:03:07 +0100

    Adding the findOneRandom function and push

--- starter-code/src/App.js
+++ starter-code/src/App.js
@@ -1,19 +1,50 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import Contacts from './contacts.json'
+import Contact from './Contact'
 import './App.css';
 
 class App extends Component {
+
+  state = {
+    Contacts: Contacts.slice(0, 5)
+  }
+
+  findOneRandomContact() {
+    const randomContact = Contacts.slice(5)[9];
+    return randomContact;
+  }
+
+  addRandomContact = evt => {
+    const contactsArr = [this.state.Contacts];
+    contactsArr.push(this.findOneRandomContact());
+    this.setState({ Contacts: contactsArr });
+  }
+
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+      <>
+        <section>
+          <button onClick={this.addRandomContact}>Add random contact</button>
+        </section>
+        <table>
+          <thead>
+            <tr className="tableHead">
+              <th>Picture</th>
+              <th>Name</th>
+              <th>Popularity</th>
+            </tr>
+          </thead>
+          <tbody>
+            {this.state.Contacts.map((cont, i) => (
+              <Contact key={i}
+                picture={cont.pictureUrl}
+                name={cont.name}
+                popularity={cont.popularity.toFixed(2)}
+              />
+            ))}
+          </tbody>
+        </table>
+      </>
     );
   }
 }
