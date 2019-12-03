13de8b3fb6316d724e3f13533a9281084e07f433
grxxts <hectorarrieta14@gmail.com>
2019-12-03 22:02:12 +0100

    wip css

--- starter-code/src/ironContacts/ironContacts.js
+++ starter-code/src/ironContacts/ironContacts.js
@@ -1,13 +1,16 @@
 import React, { Component } from 'react'
+import "../ironContacts.css"
 
 export default class ironContacts extends Component {
     render() {
         return (
-            <div>
-                <h1>{this.props.name}</h1>
-                <img src={this.props.img} alt="image actor" height="100" />
-                <p>{this.props.popularity}</p>
-                <button onClick={this.props.removeClick}>Delete</button>
+            <div  className="contactStyle">
+                <div className="containerStyle">
+                    <h1>{this.props.name}</h1>
+                    <img src={this.props.img} alt="image actor" height="100" />
+                    <p>{this.props.popularity}</p>
+                    <button class = "delete" onClick={this.props.removeClick}>Delete</button>
+                </div>
             </div>
         )
     }
