5bbc64d5d4df97adb89c6ec686dbe3f4ca1eb77d
Daniel <daniel.gonvie@gmail.com>
2019-12-03 19:28:23 +0100

    Stranger things

--- starter-code/src/App.js
+++ starter-code/src/App.js
@@ -4,12 +4,69 @@ import contacts from './contacts.json'
 import Row from './Row/Row';
 
 class App extends Component {
-  constructor(name, pictureUrl, popularity){
+  constructor(){
     super();
-    this.contacts = [contacts]
-    this.indexContacts = contacts.splice(0,5)
-
+    this.contactsClone = [...contacts]
+    this.indexContacts = this.contactsClone.splice(0,5)
+    this.state = {
+      someContacts: this.indexContacts,
+      deleted: [],
+      
+     
+    };
   }
+
+  
+
+
+    addRandom(){
+      this.indexContacts.push(this.contactsClone[[Math.floor(Math.random()*this.contactsClone.length)]])
+      this.contactsClone = this.contactsClone.filter(contact => !this.indexContacts.includes(contact));
+      
+      this.setState({
+        ...this.state,
+        someContacts: this.indexContacts,
+      });
+    }
+
+ 
+    
+
+    sortByName(){
+      this.setState({
+        ...this.state,
+        someContacts: this.indexContacts.sort(function(a, b){
+          if(a.name < b.name) { return -1; }
+          if(a.name > b.name) { return 1; }
+          return 0;
+      }),
+      });
+    }
+
+
+
+    sortByPopularity(){
+      this.setState({
+        ...this.state,
+        someContacts: this.indexContacts.sort(function(a, b){
+          if(a.popularity < b.popularity) { return 1; }
+          if(a.popularity > b.popularity) { return -1; }
+          return 0;
+      }),
+      });
+    }
+
+    deleteContact(contacts) {
+      let newContacts = [...this.state.someContacts]
+      newContacts.splice(contacts.key, 1)
+  
+      this.setState({
+        ...this.state,
+        someContacts: newContacts
+      });
+    }
+
+
   render() {
     return (
       <div className="App">
@@ -17,14 +74,21 @@ class App extends Component {
         <table>
         <thead>
         <tr>
-          <td>Picture</td>
-          <td>Name</td>
-          <td>Popularity</td>
+        <td>
+          <button onClick={() => this.addRandom()}>Add Random Contact</button></td>
+          <td><button onClick={() => this.sortByName()}>Sort By Name</button></td>
+          <td><button onClick={() => this.sortByPopularity()}>Sort By Popularity</button></td>
+        </tr>
+        <tr>
+          <td><h3>Picture</h3></td>
+          <td><h3>Name</h3></td>
+          <td><h3>Popularity</h3></td>
+          <td><h3>Action</h3></td>
         </tr>
         </thead>
         <tbody>
-        {this.indexContacts.map((contact,idx) => 
-        <Row key={idx} name={contact.name} pictureUrl={contact.pictureUrl} popularity={contact.popularity}> </Row>)}
+        {this.state.someContacts.map((contact,idx) => {
+        return <Row key={idx} name={contact.name} pictureUrl={contact.pictureUrl} popularity={contact.popularity} delete={() => this.deleteContact(contacts)} >  </Row>})}
         </tbody>
         </table>
       </div>
--- starter-code/src/Row/Row.js
+++ starter-code/src/Row/Row.js
@@ -1,12 +1,22 @@
 import React, { Component } from 'react'
-
+let counter = 0;
 export default class Row extends Component {
+   
+
     render() {
         return (
             <tr>
                 <td><img src={this.props.pictureUrl} alt="Fran guapo" ></img></td>
                 <td>{this.props.name}</td>
                 <td>{this.props.popularity}</td>
+                <td>
+                <button
+              type="button"
+              onClick={() => this.props.delete(this.props)}
+            >
+              Delete
+            </button>
+                </td>
             </tr>
         )
     }
