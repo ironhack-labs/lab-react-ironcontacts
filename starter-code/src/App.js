0d0fee0dcfa1f58777509769380ebb1047cbeb4f
Johann <johanson88@gmail.com>
2019-12-03 21:18:02 +0100

    iteration3

--- starter-code/src/App.js
+++ starter-code/src/App.js
@@ -4,11 +4,25 @@ import contacts from './contacts.json';
 import ContactsTable from './components/ContactsTable'
 
 class App extends Component {
+  state = {contactsArr : contacts.slice(0,5)}
+  
+  generateRandomContact = this.generateRandomContact.bind(this);
+  generateRandomContact () {
+    const randomNum = Math.floor(Math.random()*this.state.contactsArr.length);
+    const contactsArrCopy = [...this.state.contactsArr];
+    //if (contactsArrCopy.find(contacts[randomNum])) {
+      contactsArrCopy.unshift(contacts[randomNum]);
+      this.setState({contactsArr:contactsArrCopy});
+    //}
+  }
   
   render() {
+    console.log('render');
+    //console.log(this.state.contactsArr);
     return (
       <div className="App">
-        <ContactsTable contactsArr={contacts} />
+        <button onClick={this.generateRandomContact}>Generate Random Contact</button>
+        <ContactsTable contactsArr={this.state.contactsArr} />
       </div>
     );
   }
