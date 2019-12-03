156ff976ccff4d49c08f32d31190373a6866161a
grxxts <hectorarrieta14@gmail.com>
2019-12-03 17:16:16 +0100

    iteration2 done

--- starter-code/src/App.js
+++ starter-code/src/App.js
@@ -3,33 +3,41 @@ import logo from './logo.svg';
 import './App.css';
 import contacts from './contacts.json'
 import IronContacts from './ironContacts/ironContacts'
+import ironContacts from './ironContacts/ironContacts';
 
 
 class App extends Component {
- constructor() {
-   super();
-   this.contacts = [...contacts]
-   this.fiveContacts = this.contacts.splice(0,5);
-   this.state = {
-     actors: this.fiveContacts
-   }
+  constructor() {
+    super();
+    this.contacts = [...contacts]
+    this.fiveContacts = this.contacts.splice(0, 5);
+    this.state = {
+      actors: this.fiveContacts
+    }
+  }
+
+    randomContacts() {
+      let otherContacts = this.contacts.filter((contact) => !this.state.actors.includes(contact))
+      this.state.actors.push(otherContacts[Math.floor(Math.random()*otherContacts.length)])
+      this.setState({
+        ...this.state.actors
+      })
+    }
 
   
-
- }
 
   render() {
     return (
       <div className="App">
-        <button>Add Random</button>
+        <button onClick={()=>this.randomContacts()}>Add Random Actor</button>
         <button>Sort by Name</button>
         <button>Sort by Popularity</button>
 
-       {this.state.actors.map((actor,idx) => 
-         <IronContacts name = {actor.name} img = {actor.pictureUrl} popularity = {actor.popularity} key = {idx} ></IronContacts>
-       )}
-       
-       </div>
+        {this.state.actors.map((actor, idx) =>
+          <IronContacts name={actor.name} img={actor.pictureUrl} popularity={actor.popularity} key={idx} ></IronContacts>
+        )}
+
+      </div>
     );
   }
 }
