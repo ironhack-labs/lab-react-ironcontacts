3d9ed4d272050cb0d8a45c897f7233b3b205c096
Flor Urbinati <flor29600@hotmail.com>
2019-12-03 17:06:07 +0100

    it1

--- starter-code/src/App.js
+++ starter-code/src/App.js
@@ -1,18 +1,70 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import contacts from './contacts.json';
+
+
+let contactsList= [...contacts].splice(0,5)
+console.log (contactsList)
+
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+    list: [...contactsList]
+  }
+    console.log(this.state)
+  }
+
+  // addRandom = () => {
+  //   let filteredArr=[...contacts].filter(contact=> this.state.list.includes(contact))
+  //   console.log(filteredArr.length)
+
+  //   let newContact = filteredArr[Math.floor(Math.random() * contacts.length)];
+
+  //   let newArray = [...this.state.list];
+  //   newArray.push(newContact);
+  //   this.setState({
+  //     ...this.state,
+  //     list: newArray
+  //   });
+  // };
+
+
+  
+
   render() {
     return (
+
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+          <h1 className="App-title">Ironcontacts</h1>
         </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <button onClick={() => this.addRandom()}>Add Random Contact</button>
+        <table>
+        <thead>
+        <tr>
+    <th>Photo</th>
+    <th>Name</th>
+    <th>Popularity</th>
+  </tr>
+  </thead>
+ 
+  <tbody>
+  {this.state.list.map((contact, idx) => {
+          return (
+            <tr key={contact.id}>
+            <td><img src={contact.pictureUrl} alt=""/></td>
+    <td>{contact.name}</td>
+    <td>{contact.popularity}</td>
+    </tr>
+          );
+
+  })}
+ 
+  </tbody>
+        </table>
       </div>
     );
   }
