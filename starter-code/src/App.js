950083baafad797b6434462faf643d3413af9ef3
enriqueMontano <montanoenrique@outlook.com>
2019-12-03 16:46:42 +0100

    iteration 1 done

--- starter-code/src/App.js
+++ starter-code/src/App.js
@@ -1,18 +1,39 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import contacts from "./contacts.json";
+import ContactList from "./ContactList/ContactList";
+
+// class ContactList {
+//   constructor(picture, name, popularity) {
+//     this.picture = picture;
+//     this.name = name;
+//     this.popularity = popularity;
+//   }
+// }
 
 class App extends Component {
+  constructor() {
+    super();
+    this.contacts = [...contacts];
+    this.newContacts = this.contacts.splice(0, 5);
+
+    this.state = {
+      actors: this.newContacts
+    };
+  }
   render() {
+    // return <div className="App"></div>;
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div>
+        {this.state.actors.map((actor, idx) => (
+          <ContactList
+            key={idx}
+            picture={actor.pictureUrl}
+            name={actor.name}
+            popularity={actor.popularity}
+          ></ContactList>
+        ))}
       </div>
     );
   }
---
+++ starter-code/src/ContactList/ContactList.js
@@ -0,0 +1,21 @@
+import React, { Component } from "react";
+
+export default class ContactList extends Component {
+  render() {
+    return (
+      <div>
+        <tr class="contacts-table">
+          <td>
+            <img
+              src={this.props.picture}
+              alt=""
+              style={{ height: 100, with: 100 }}
+            />
+          </td>
+          <td>{this.props.name}</td>
+          <td>{this.props.popularity}</td>
+        </tr>
+      </div>
+    );
+  }
+}
