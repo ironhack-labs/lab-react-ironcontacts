7feb28118af3f85aba28ebbf9074002100e812b2
grxxts <hectorarrieta14@gmail.com>
2019-12-03 17:39:30 +0100

    iteration3 done

--- starter-code/src/App.js
+++ starter-code/src/App.js
@@ -16,22 +16,50 @@ class App extends Component {
     }
   }
 
-    randomContacts() {
-      let otherContacts = this.contacts.filter((contact) => !this.state.actors.includes(contact))
-      this.state.actors.push(otherContacts[Math.floor(Math.random()*otherContacts.length)])
-      this.setState({
-        ...this.state.actors
-      })
-    }
+  randomContacts() {
+    let otherContacts = this.contacts.filter((contact) => !this.state.actors.includes(contact))
+    this.state.actors.push(otherContacts[Math.floor(Math.random() * otherContacts.length)])
+    this.setState({
+      ...this.state.actors
+    })
+  }
 
-  
+
+  sortByName() {
+    this.state.actors.sort(function (a, b) {
+      if (a.name < b.name) { return -1; }
+      if (a.name > b.name) { return 1; }
+      return 0;
+
+    })
+    this.setState({
+      ...this.state.actors
+    })
+  }
+
+
+  sortByPopularity() {
+    this.state.actors.sort(function (a, b) {
+      if (a.popularity < b.popularity) { return 1; }
+      if (a.popularity > b.popularity) { return -1; }
+      return 0;
+
+    })
+    this.setState({
+      ...this.state.actors
+    })
+  }
+
+
+
+
 
   render() {
     return (
       <div className="App">
-        <button onClick={()=>this.randomContacts()}>Add Random Actor</button>
-        <button>Sort by Name</button>
-        <button>Sort by Popularity</button>
+        <button onClick={() => this.randomContacts()}>Add Random Actor</button>
+        <button onClick={() => this.sortByName()}>Sort by Name</button>
+        <button onClick={() => this.sortByPopularity()}>Sort by Popularity</button>
 
         {this.state.actors.map((actor, idx) =>
           <IronContacts name={actor.name} img={actor.pictureUrl} popularity={actor.popularity} key={idx} ></IronContacts>
