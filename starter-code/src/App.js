7f26ea408997ea51dba6088e2d53803196494c32
María Simó <maria@coponstudio.es>
2019-12-03 16:59:33 +0100

    It 2

--- starter-code/src/App.js
+++ starter-code/src/App.js
@@ -6,18 +6,33 @@ import Contact from "./contact/Contact";
 class App extends Component {
   constructor() {
     super();
-
     this.actors = [...Contacts];
+    this.actorsIni = this.actors.splice(0, 5)
+    this.actorsRest = this.actors.splice(5)
+    
     this.state = {
-      actors: this.actors.splice(0, 5)
+      contacts: this.actorsIni
     };
   }
 
+  addRandom(){
+    const randomInt = (min, max) => Math.floor(Math.random() * (max - min + 1) + min);  
+   
+    let contactsCopy = [...this.state.contacts]
+    contactsCopy.push(this.actorsRest[randomInt(0, this.actorsRest.length -1)])
+    
+    this.setState({
+      contacts : contactsCopy
+    });
+  }
+
   render() {
-    console.log(this.state.actors);
+    console.log(this.state.contacts);
     return (
       <div className="App">
         <h1>IronContacts</h1>
+
+        <button onClick={() => this.addRandom()}>Add Random Contact</button>
         <table>
           <thead>
             <tr>
@@ -28,8 +43,8 @@ class App extends Component {
           </thead>
 
           <tbody>
-            {this.state.actors.map(actor => (
-              <Contact key={actor.id} name={actor.name} picture={actor.pictureUrl} popularity={actor.popularity}></Contact>
+            {this.state.contacts.map(contact => (
+              <Contact key={contact.id} name={contact.name} picture={contact.pictureUrl} popularity={contact.popularity}></Contact>
             ))}
           </tbody>
         </table>
