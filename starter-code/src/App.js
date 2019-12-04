04c025f29face85ae1a083afab54fb2a62bf5763
Robert <robcg1102@gmail.com>
2019-12-03 23:55:28 -0600

    done

--- starter-code/src/App.js
+++ starter-code/src/App.js
@@ -1,18 +1,80 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import contacts from './contacts.json';
+import TableContacts from './components/TableContacts';
 
 class App extends Component {
+  
+  state = {
+      person: contacts.slice(0, 5), 
+  }
+
+  addRandom() {
+    const newList = [...this.state.person];
+    const randomContact = contacts.slice(5,contacts.length)[Math.floor(Math.random() * contacts.length)];
+    newList.push(randomContact);
+    this.setState({
+      person: newList,
+    })
+  }
+
+  compare(a, b) {
+    if ( a.name < b.name ){
+      return -1;
+    }
+    if ( a.name > b.name ){
+      return 1;
+    }
+    return 0;
+  }
+
+  sortName() {
+    const newArr = [...this.state.person];
+    newArr.sort(this.compare);
+    this.setState({
+      person: newArr,
+    })
+  }
+
+  sortPopularity() {
+    const newArr = [...this.state.person];
+    newArr.sort((a, b) => {
+      return b.popularity - a.popularity;
+    });
+    this.setState({
+      person: newArr,
+    })
+  }
+
+  delete(contactIdx) {
+    const newArr = [...this.state.person];
+    newArr.splice(contactIdx, 1);
+    this.setState({
+      person: newArr,
+    })
+  }
+
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <h1>IronContacts</h1>
+        <button onClick={() => this.addRandom()}>Add Random Contact</button>
+        <button onClick={() => this.sortName()}>Sort by name</button>
+        <button onClick={() => this.sortPopularity()}>Sort by popularity</button>
+        <table className="tab">
+          <thead>
+            <tr>
+              <th>Picture</th>
+              <th>Name</th>
+              <th>Popularity</th>
+            </tr>
+          </thead>
+          <tbody>
+            { this.state.person.map((contact,index) => 
+            <TableContacts key={index} delete={() => 
+              this.delete(index)} {...contact} />) 
+            }
+          </tbody>
+        </table>
       </div>
     );
   }
---
+++ starter-code/src/components/TableContacts.js
@@ -0,0 +1,15 @@
+ import React from "react";
+
+const TableContacts = (props) => {
+  return (
+      
+          <tr>
+            <td><img src={props.pictureUrl} alt={props.name} width="150px"/></td>
+            <td>{props.name}</td>
+            <td>{props.popularity}</td>
+            <td><button className="delete-btn" onClick={props.delete}>Delete</button></td>
+          </tr>
+  );
+};
+
+export default TableContacts;
