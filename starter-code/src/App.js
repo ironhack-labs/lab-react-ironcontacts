309caf0ec7fd9ce50bd85fb01b9b49b09a2c7fd0
roberto araya gil <naggas@Naggas.local>
2020-01-09 21:05:18 +0100

    config ok and first array with length 5

--- starter-code/src/App.js
+++ starter-code/src/App.js
@@ -1,18 +1,23 @@
 import React, { Component } from 'react';
+import contacts from './contacts.json'
 import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  state = {
+    contacts: contacts.slice(0,5)
+  }
+
+  componentDidMount(){
+    
+  }
+
+  
+
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        {alert(this.state.contacts.length)}
       </div>
     );
   }
