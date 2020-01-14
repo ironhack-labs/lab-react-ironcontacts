2a86dd42345ec46db7fa7105a8751deee984876a
Monika <mswidzinska87@gmail.com>
2020-01-14 15:12:28 +0100

    iteration2

--- starter-code/src/App.jsx
+++ starter-code/src/App.jsx
@@ -9,14 +9,22 @@ export default class App extends Component {
 constructor (props){
     super(props)
     this.state = {
-    visibleCelebrities: celebrities.slice(0,5)
+    visibleCelebrities: celebrities.slice(0,5),
 }}
+pickRandomElementFromArray = (anArray)=> {
+    return anArray[Math.floor(Math.random()*anArray.length)];
+}
+pickRandomCelebrity = ()=> {
+this.setState({
+    visibleCelebrities: [...this.state.visibleCelebrities,  this.pickRandomElementFromArray(celebrities)]
+})
+}
 
     render() {
         return ( 
         <div className = "App">
             <h1 className = "IronTitle">IronContacts</h1>
-            <div>
+            <button onClick={this.pickRandomCelebrity}className="buttonAdd">Add Random Contact</button>
             <table>
                 <tr className="titles">
                     <th>Picture</th>
@@ -25,17 +33,13 @@ constructor (props){
                 </tr>
                     {this.state.visibleCelebrities.map((celebrity)=>
                 <Row
-                        pictureUrl={celebrity.pictureUrl}
-                        name={celebrity.name}
-                        popularity={celebrity.popularity.toFixed(2)}
+                    key={celebrity.pictureUrl}
+                    pictureUrl={celebrity.pictureUrl}
+                    name={celebrity.name}
+                    popularity={celebrity.popularity.toFixed(2)}
                 />
                 )}
-                
             </table>
-                <div>
-                
-                </div>
-            </div>
         </div>
         );
     }
