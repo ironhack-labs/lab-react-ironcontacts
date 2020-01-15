b8a83cc3860b45cf13c53520a5a672fe6e5d6f78
Gabriel Zsigmond <gaazsig@gmail.com>
2020-01-15 16:21:18 -0300

    sorting contacts and updating state

--- starter-code/src/components/ContactsPage/ContactsPage.js
+++ starter-code/src/components/ContactsPage/ContactsPage.js
@@ -12,32 +12,65 @@ class ContactsPage extends Component {
     let randI = Math.floor(Math.random() * ((DB.length - 1) - (this.state.DB.length) + (this.state.DB.length - 1)))
     console.log(randI);
     this.setState(
-      {DB: [...this.state.DB, DB[randI]]}
+      { DB: [...this.state.DB, DB[randI]] }
     )
-    
+  }
+  sortContacts = () => {
+    let sorted = this.state.DB.sort(
+      (a, b) => {
+        if (a.name < b.name) {
+          return -1
+        } else {
+          return 1
+        }
+      }
+    )
+    this.setState(
+      {
+        DB: sorted
+      }
+    )
+  }
+  sortContactsPopularity = () => {
+    let sorted = this.state.DB.sort(
+      (a, b) => {
+        if (a.popularity < b.popularity) {
+          return -1
+        } else {
+          return 1
+        }
+      }
+    )
+    this.setState(
+      {
+        DB: sorted
+      }
+    )
   }
   render() {
     return (
       <div>
-      <h1>Iron Contacts</h1>
-      <button onClick={this.addContact}>Add Random Contact</button>
-      <table >
-        <thead>
-          <tr>
-            <th>Picture</th>
-            <th>Name</th>
-            <th>Populatity</th>
-          </tr>
-        </thead>
-        <tbody>
-          {
-            this.state.DB.map(p => {
-              return (
-                <Contact key={p.id} name={p.name} pictureUrl={p.pictureUrl} popularity={p.popularity} />
-              )
-            })}
-        </tbody>
-      </table>
+        <h1>Iron Contacts</h1>
+        <button onClick={this.addContact}>Add Random Contact</button>
+        <button onClick={this.sortContacts}>sort by name</button>
+        <button onClick={this.sortContactsPopularity}>sort by popularity</button>
+        <table >
+          <thead>
+            <tr>
+              <th>Picture</th>
+              <th>Name</th>
+              <th>Populatity</th>
+            </tr>
+          </thead>
+          <tbody>
+            {
+              this.state.DB.map(p => {
+                return (
+                  <Contact key={p.id} name={p.name} pictureUrl={p.pictureUrl} popularity={p.popularity} />
+                )
+              })}
+          </tbody>
+        </table>
       </div>
     )
   }
