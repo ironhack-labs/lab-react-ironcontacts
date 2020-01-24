5e43291bc608d81e65e211432a652170d68f5e8f
Zahn <zahnk@media-saturn.com>
2020-01-24 16:09:09 +0100

    incl iteration4

--- starter-code/src/App.js
+++ starter-code/src/App.js
@@ -2,45 +2,51 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import contacts from './contacts.json';
-import Contacts from  './components/contacts.js';
+import Contacts from './components/contacts.js';
 
 class App extends Component {
-  
+
   state = {
-    filteredContacts: [...contacts.filter((elem,ind) => ind < 5)]
+    filteredContacts: [...contacts.filter((elem, ind) => ind < 5)]
   }
-  
+
   addMovieHandler = () => {
-    let randomMovie = contacts[Math.floor(Math.random()*contacts.length)];
-    this.setState( prevState => ({
+    let randomMovie = contacts[Math.floor(Math.random() * contacts.length)];
+    this.setState(prevState => ({
       filteredContacts: [...prevState.filteredContacts, randomMovie]
     }))
   }
 
   sortNameHandler = () => {
-    let sortNameArr = this.state.filteredContacts.slice().sort((a,b)=>{
-          let aName = a.name, bName = b.name;
-          return aName.localeCompare(bName);
+    let sortNameArr = this.state.filteredContacts.slice().sort((a, b) => {
+      let aName = a.name, bName = b.name;
+      return aName.localeCompare(bName);
     });
     //console.log(sortNameArr);
-    this.setState( 
-      {filteredContacts: sortNameArr}
+    this.setState(
+      { filteredContacts: sortNameArr }
     )
   }
 
   sortPopulHandler = () => {
-    let sortPopulArr = this.state.filteredContacts.slice().sort((a,b)=>{
-          let aName = a.popularity, bName = b.popularity;
-          return (bName - aName);
+    let sortPopulArr = this.state.filteredContacts.slice().sort((a, b) => {
+      let aName = a.popularity, bName = b.popularity;
+      return (bName - aName);
     });
     //console.log(sortNameArr);
-    this.setState( 
-      {filteredContacts: sortPopulArr}
+    this.setState(
+      { filteredContacts: sortPopulArr }
     )
   }
-  
+
+  deleteMovieHandler = movieId => {
+    this.setState({
+      filteredContacts: this.state.filteredContacts.filter(movie => movie.id !== movieId)
+    })
+  }
+
   render() {
-   //console.log(this.state.filteredContacts);
+    //console.log(this.state.filteredContacts);
     return (
       <div className="App">
         <header className="App-header">
@@ -48,24 +54,24 @@ class App extends Component {
           <h1 className="App-title">Welcome to React</h1>
         </header>
         <div className="App-intro">
-        <button onClick = {this.addMovieHandler}>Add random contact</button>
-        <button onClick = {this.sortNameHandler}>Sort by name</button>
-        <button onClick = {this.sortPopulHandler}>Sort by popularity</button>
-        <table style={{width: "100%"}}> 
-        <thead>
-        <tr>
+          <button onClick={this.addMovieHandler}>Add random contact</button>
+          <button onClick={this.sortNameHandler}>Sort by name</button>
+          <button onClick={this.sortPopulHandler}>Sort by popularity</button>
+          <table style={{ width: "100%" }}>
+            <thead>
+              <tr>
                 <th>Picture</th>
                 <th>Name</th>
                 <th>Popularity</th>
-            </tr>
+              </tr>
             </thead>
             <tbody>
-        {this.state.filteredContacts.map(oneMovie =>{
-          return <Contacts key={oneMovie.id} {...oneMovie}/>
-        })}
-        </tbody>
-        </table>
-          
+              {this.state.filteredContacts.map(oneMovie => {
+                return <Contacts key={oneMovie.id} {...oneMovie} clickToDelete={() => this.deleteMovieHandler(oneMovie.id)} />
+              })}
+            </tbody>
+          </table>
+
         </div>
       </div>
     );
--- starter-code/src/components/contacts.js
+++ starter-code/src/components/contacts.js
@@ -11,21 +11,22 @@ const imgstyle1 = {
     width: "100px",
     height: "auto",
     margin: "10px 400px 2px 10px"
-  };
+};
 
 
 
 export default class Contacts extends Component {
     render() {
-        let myPopul = Math.round(this.props.popularity * 100)/100; 
+        let myPopul = Math.round(this.props.popularity * 100) / 100;
         return (
-            
+
             <tr>
-                <td><img style={imgstyle1} src={this.props.pictureUrl} alt="Bild"/></td>
+                <td><img style={imgstyle1} src={this.props.pictureUrl} alt="Bild" /></td>
                 <td>{this.props.name}</td>
                 <td>{myPopul}</td>
+                <td><button onClick={this.props.clickToDelete}>Delete</button></td>
             </tr>
-                    )
+        )
     }
 }
 
