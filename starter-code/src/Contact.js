9a2ee2da10c0edf8f0f22aad4aec734dc839be84
Anastasia Schmitke <schmitke@mediamarktsaturn.com>
2020-01-24 14:31:03 +0100

    Iteration 1 finished

---
+++ starter-code/src/Contact.js
@@ -0,0 +1,70 @@
+import React, { Component } from 'react';
+import contacts from './contacts.json'
+
+export default class Contact extends Component {
+
+    constructor () {
+        super();
+
+        this.state = {
+           contacts: [{
+                name: "Idris Elba",
+                pictureUrl: "https://image.tmdb.org/t/p/w500/d9NkfCwczP0TjgrjpF94jF67SK8.jpg",
+                popularity: 11.622713,
+                id: "11731993-0604-4bee-80d5-67ad845d0a38"
+              },
+              {
+                name: "Jessica Chastain",
+                pictureUrl: "https://image.tmdb.org/t/p/w500/nkFrkn5NZVGWb4b2X0yIcXezhyt.jpg",
+                popularity: 8.324357,
+                id: "17980511-75ca-48b0-bea8-462fec2ee43d"
+              },
+              {
+                name: "Johnny Depp",
+                pictureUrl: "https://image.tmdb.org/t/p/w500/kbWValANhZI8rbWZXximXuMN4UN.jpg",
+                popularity: 15.656534,
+                id: "7dad00f7-3949-477d-a7e2-1467fd2cfc06"
+              },
+              {
+                name: "Emilia Clarke",
+                pictureUrl: "https://image.tmdb.org/t/p/w500/j7d083zIMhwnKro3tQqDz2Fq1UD.jpg",
+                popularity: 16.211837,
+                id: "e14aa81d-b812-412d-bc4d-4a0d2c9c66f4"
+              },
+              {
+                name: "Leonardo DiCaprio",
+                pictureUrl: "https://image.tmdb.org/t/p/w500/A85WIRIKVsD2DeUSc8wQ4fOKc4e.jpg",
+                popularity: 11.245333,
+                id: "b4d2c7b8-fdd5-426a-85bd-011c3f50a6c6"
+              }]
+        }
+
+    }
+
+    render() {
+        return (
+            <div className="contacttable">
+                <table >
+                    <thead>
+                        <tr>
+                            <th>Picture</th>
+                            <th>Name</th>
+                            <th>Popularity</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {this.state.contacts.map(oneP => {
+                            return (
+                            <tr>
+                                <td><img src={oneP.pictureUrl} alt="" /> </td>
+                                <td>{oneP.name}</td>
+                                <td>{oneP.popularity.toFixed(2)}</td>
+                            </tr>)
+                        })}
+                        
+                    </tbody>
+                </table>
+            </div>
+        )
+    }
+}
