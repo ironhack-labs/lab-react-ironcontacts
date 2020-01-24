c1a7ffcfc22348e35ceaf3c7efa48635d11e2cf7
Rochowski <rochowskim@media-saturn.com>
2020-01-24 17:30:35 +0100

    iteration 3 completed

--- starter-code/src/components/DynamicContact.js
+++ starter-code/src/components/DynamicContact.js
@@ -11,13 +11,56 @@ export default class DynamicContact extends Component {
   }
 
   addRandomContactHandler = () => {
-    const randomContact = contacts[Math.floor(Math.random() * contacts.length)]; //fix contact for test
+    const randomContact = contacts[Math.floor(Math.random() * contacts.length)];
     
     this.setState({
       contacts: [...this.state.contacts, randomContact]
     });
-    console.log(this.state.contacts);
   };
+
+  sortByNameHandler = () => {
+    function compare(a, b) {
+        // Use toUpperCase() to ignore character casing
+        const contactA = a.name.toUpperCase();
+        const contactB = b.name.toUpperCase();
+      
+        let comparison = 0;
+        if (contactA > contactB) {
+          comparison = 1;
+        } else if (contactA < contactB) {
+          comparison = -1;
+        }
+        return comparison;
+      };
+
+      console.log(this.state.contacts.sort(compare))
+      
+    this.setState({
+      contacts: [...this.state.contacts.sort(compare)]
+    })
+  }
+
+  sortByPopularity = () => {
+    function compare(a, b) {
+        // Use toUpperCase() to ignore character casing
+        const contactA = a.popularity;
+        const contactB = b.popularity;
+      
+        let comparison = 0;
+        if (contactA > contactB) {
+          comparison = -1;
+        } else if (contactA < contactB) {
+          comparison = 1;
+        }
+        return comparison;
+      };
+
+      //console.log(this.state.contacts.sort(compare))
+      
+    this.setState({
+      contacts: [...this.state.contacts.sort(compare)]
+    })
+  }
 
   render() {
     console.log("random contact added ", this.addRandomContactHandler);
@@ -25,6 +68,8 @@ export default class DynamicContact extends Component {
     return (
       <div>
         <button onClick={this.addRandomContactHandler}>Add Rondom Contact</button>
+        <button onClick={this.sortByNameHandler}>Sort By Name</button>
+        <button onClick={this.sortByPopularity}>Sort By Popularity</button>
         <div className="contacttable">
           <table>
             <thead>
