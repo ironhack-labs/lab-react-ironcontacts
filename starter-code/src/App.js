dd0a0ecba9d1f6455ac949b9fcb30b53ec48fe76
Zahn <zahnk@media-saturn.com>
2020-01-24 15:24:07 +0100

    until iteration2

--- starter-code/src/App.js
+++ starter-code/src/App.js
@@ -1,18 +1,49 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import contacts from './contacts.json';
+import Contacts from  './components/contacts.js';
 
 class App extends Component {
+  
+  state = {
+    filteredContacts: [...contacts.filter((elem,ind) => ind < 5)]
+  }
+  
+  addMovieHandler = () => {
+    let randomMovie = contacts[Math.floor(Math.random()*contacts.length)];
+    console.log(randomMovie);
+    this.setState( prevState => ({
+      filteredContacts: [...prevState.filteredContacts, randomMovie]
+    }))
+  }
+  
   render() {
+   console.log(this.state.filteredContacts);
     return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Welcome to React</h1>
         </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <div className="App-intro">
+        <button onClick = {this.addMovieHandler}>Add random contact</button>
+        <table style={{width: "100%"}}> 
+        <thead>
+        <tr>
+                <th>Picture</th>
+                <th>Name</th>
+                <th>Popularity</th>
+            </tr>
+            </thead>
+            <tbody>
+        {this.state.filteredContacts.map(oneMovie =>{
+          return <Contacts key={oneMovie.id} {...oneMovie}/>
+        })}
+        </tbody>
+        </table>
+          
+        </div>
       </div>
     );
   }
---
+++ starter-code/src/components/contacts.js
@@ -0,0 +1,32 @@
+import React, { Component } from 'react'
+
+
+const imgstyle1 = {
+
+    color: "black",
+    backgroundColor: "white",
+    padding: "5px",
+    fontFamily: "Arial",
+    border: "2px solid black",
+    width: "100px",
+    height: "auto",
+    margin: "10px 400px 2px 10px"
+  };
+
+
+
+export default class Contacts extends Component {
+    render() {
+        let myPopul = Math.round(this.props.popularity * 100)/100; 
+        return (
+            
+            <tr>
+                <td><img style={imgstyle1} src={this.props.pictureUrl} alt="Bild"/></td>
+                <td>{this.props.name}</td>
+                <td>{myPopul}</td>
+            </tr>
+                    )
+    }
+}
+
+
