8df8d4c234a7fe78f76a5d4315e4291432066afa
Anastasia Schmitke <schmitke@mediamarktsaturn.com>
2020-01-24 17:32:22 +0100

    Iteration 4 not finished

--- starter-code/src/Contact.js
+++ starter-code/src/Contact.js
@@ -11,7 +11,7 @@ export default class Contact extends Component {
 
     }
 
-   clickRandom = () => {
+    clickRandom = () => {
         this.setState({
             contacts: [...this.state.contacts, contacts[Math.floor(Math.random() * (contacts.length - 5)) + 5]]
         })
@@ -19,53 +19,63 @@ export default class Contact extends Component {
 
     sortName = () => {
         this.setState({
-            contacts: this.state.contacts.sort((a,b) => {
-             return   a.name.localeCompare(b.name) })
-            
-            }
+            contacts: this.state.contacts.sort((a, b) => {
+                return a.name.localeCompare(b.name)
+            })
+
+        }
         )
     }
 
     sortPop = () => {
         this.setState({
-            contacts: this.state.contacts.sort((x,y) => {
+            contacts: this.state.contacts.sort((x, y) => {
                 return y.popularity - x.popularity
             }),
         })
     }
+
+    deleteContact = contactId => {
+        this.setState({
+            contacts: this.state.contacts.filter(contact => contact.id !== contactId)
+        });
+    };
 
 
     render() {
         console.log(this.state.contacts)
         return (
             <div>
-            <div className="add-random-button"><button onClick={this.clickRandom}>Add Random Contact</button></div><br />
-            <div className="add-random-button"><button onClick={this.sortName}>Sort by name</button></div><br />
-            <div className="add-random-button"><button onClick={this.sortPop}>Sort by popularity</button></div><br />
-            <div className="contacttable">
-                
-                <table >
-                    <thead>
-                        <tr>
-                            <th>Picture</th>
-                            <th>Name</th>
-                            <th>Popularity</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        {this.state.contacts.map(oneP => {
-                            return (
-                                <tr key={oneP.id}>
-                                    <td><img src={oneP.pictureUrl} alt="bild" /> </td>
-                                    <td>{oneP.name}</td>
-                                    <td>{oneP.popularity.toFixed(2)}</td>
-                                </tr>)
-                        })}
+                <div className="buttons">
+                    <div className="add-random-button"><button onClick={this.clickRandom}>Add Random Contact</button></div><br />
+                    <div className="add-random-button"><button onClick={this.sortName}>Sort by name</button></div><br />
+                    <div className="add-random-button"><button onClick={this.sortPop}>Sort by popularity</button></div><br />
+                </div>
+                    <div className="contacttable">
+                    <table >
+                        <thead>
+                            <tr>
+                                <th>Picture</th>
+                                <th>Name</th>
+                                <th>Popularity</th>
+                                <th>Action</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            {this.state.contacts.map(oneP => {
+                                return (
+                                    <tr key={oneP.id} {...oneP} clickToDelete={() => this.deleteContact(oneP.id)}>
+                                        <td><img src={oneP.pictureUrl} alt="bild" /> </td>
+                                        <td>{oneP.name}</td>
+                                        <td>{oneP.popularity.toFixed(2)}</td>
+                                        <td><button onClick={this.clickToDelete}>Delete</button></td>
+                                    </tr>)
+                            })}
 
-                    </tbody>
-                </table>
+                        </tbody>
+                    </table>
+                </div>
             </div>
-        </div>
         )
     }
 }
