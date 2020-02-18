2c289e4625a37ba3f7bb0e6a7689365c0d99d370
Edith-LE <edith.lop.estrada@gmail.com>
2020-02-18 17:37:42 -0600

    done

--- starter-code/src/App.js
+++ starter-code/src/App.js
@@ -1,20 +1,81 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import contacts from './contacts.json'
+
 
 class App extends Component {
+  state = {
+    celebs:[contacts [0], contacts [1], contacts[2], contacts[3], contacts[4]]
+  }
+
+  addCeleb = () =>{
+    const {celebs} = this.state
+    let randomCeleb = contacts [Math.floor(Math.random()* contacts.length)]
+    this.setState({
+      celebs: [...this.state.celebs, randomCeleb]
+    })
+
+  }
+  sortName =() =>{
+    const {celebs} = this.state
+    celebs.sort((a,b)=>{
+      return a.name.localeCompare(b.name)
+    })
+    this.setState(celebs)  
+  }
+
+  sortPopularity =() =>{
+    const {celebs} = this.state
+    celebs.sort((a,b)=>{
+      return b.popularity - a.popularity
+    })
+    this.setState(celebs)  
+  }
+
+  deleteCeleb =(index) =>{
+    const celebsCopy = [...this.state.celebs]
+    celebsCopy.splice(index, 1)
+    this.setState({
+      celebs: celebsCopy
+    })
+
+  }
+
+
+  
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div className="app">
+        <h1>IronContacs</h1>
+        <div className="butt">
+        <button onClick={this.addCeleb}>Add a random celeb</button>
+        <button onClick={this.sortName}>Sort by Name</button> 
+        <button onClick={this.sortPopularity}>Sort by Popularity</button> 
+        </div>
+  
+       <table>
+        <thead>
+          <tr>
+            <th><strong>Picture</strong></th>
+            <th><strong>Name</strong></th>
+            <th><strong>Popularity</strong></th>
+          </tr>
+        </thead>
+        <tbody>
+          {this.state.celebs.map((celeb, i) =>{
+           return(
+            <tr key={i} >
+            <td><img src={celeb.pictureUrl} alt={celeb.name}/></td>
+            <td> {celeb.name} </td>
+            <td> {celeb.popularity} </td>
+            <td><button onClick={()=>this.deleteCeleb (i)}>Delete</button></td>
+          </tr>
+           )
+          })}
+        </tbody>
+      </table>
       </div>
-    );
+    )
   }
 }
 
