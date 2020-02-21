243a9eaab8be942a9ec973500652b272397da077
Tonalli <tonalli@xanum.uam.mx>
2020-02-21 12:14:14 -0600

    Iteration 2

--- starter-code/src/App.js
+++ starter-code/src/App.js
@@ -8,10 +8,21 @@ class App extends Component {
     arreglo: contacts.slice(0,5)
   }
 
+  AddContactHandler = () => {
+    let x0 = 5;
+    let xf = contacts.length;
+    let idx = x0 + Math.floor(Math.random() * (xf - x0));
+    let newContact = contacts[idx];
+    this.setState({
+      arreglo: this.state.arreglo.concat([newContact])
+    });
+  };
+
   render() { 
     return (
       <div className="App">
         <h1>IronContacts</h1>
+        <button onClick={this.AddContactHandler} >Add Random Contact</button>
         <Table data={this.state.arreglo}/>
       </div>
     );
