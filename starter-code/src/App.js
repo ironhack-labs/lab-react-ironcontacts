93acee84171c6de63a07273e37034f7b9528afad
Tonalli <tonalli@xanum.uam.mx>
2020-02-21 11:20:11 -0600

    Add state to App.js

--- starter-code/src/App.js
+++ starter-code/src/App.js
@@ -4,12 +4,15 @@ import contacts from './contacts.json';
 import './App.css';
 
 class App extends Component {
+  state = {
+    arreglo: contacts.slice(0,5)
+  }
+
   render() { 
-    let arreglo = contacts.slice(0,5);
     return (
       <div className="App">
         <h1>IronContacts</h1>
-        <Table data={arreglo}/>
+        <Table data={this.state.arreglo}/>
       </div>
     );
   }
