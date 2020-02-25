87b4af094ab595a32aa42fd855a16400dc8d6460
Stefan Berk <stefan@k2ff.de#>
2020-02-25 16:52:42 +0100

    initial commit

--- starter-code/src/App.js
+++ starter-code/src/App.js
@@ -1,18 +1,31 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import contacts from "./contacts.json"
 
 class App extends Component {
+  state = {
+    contactList: contacts.slice(0,5)
+  };
+
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <div className="CListContainer">
+
+        {/* start of the loop */}
+        {this.state.contactList.map((contact) => {
+          return(
+          <div class="row" key={contact.id}>
+            <div class="img">{contact.pictureUrl}</div>
+            <div class="name">{contact.name}</div>
+            <div class="popularity">{contact.popularity}</div>
+            <div class="action">#</div>
+          </div>
+          )
+        {/* end of the loop */}
+        })}
+        </div>
       </div>
     );
   }
