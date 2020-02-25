4e5262d9acdc967b462deee7549e25881677afe0
Luz Divina Delgado Castañeda <luzdivdelg@gmail.com>
2020-02-25 19:13:57 +0100

    iteration 2 done & iteration 1 updated

--- starter-code/src/components/List.js
+++ starter-code/src/components/List.js
@@ -2,31 +2,34 @@ import React, { Component } from 'react';
 import Card from './Card'
 import contacts from '../contacts.json';
 
-let filteredContacts = contacts.slice(0, 5)
-
 class ActorsList extends Component {
 
     constructor() {
         super()
         this.state = {
-            contacts,
-            filteredContacts
-
+            contacts: contacts.slice(0, 5)
         }
     }
-
+    addContacts = idx => {
+        const newContacts = [...this.state.contacts]
+        let randomContacts = Math.floor(Math.random() * (contacts.length + 1))
+        newContacts.push(contacts[randomContacts])
+        this.setState({ contacts: newContacts })
+    }
     render() {
 
         return (
             <>
+                <button onClick={this.addContacts}>Add Random Contact</button>
                 <table>
                     <tr>
                         <th>Picture</th>
                         <th>Name</th>
                         <th>Popularity</th>
                     </tr>
-                    {this.state.filteredContacts.map((elm, idx) => <Card key={idx} {...elm} />)}
+                    {this.state.contacts.map((elm, idx) => <Card key={idx} {...elm} />)}
                 </table>
+
             </>
         )
     }
