9e4fa79f893e720fbadaac951b0a2ff59f4d27de
rodmartinezmedina <rodmartinezmedina@gmail.com>
2020-02-25 18:44:29 +0100

    sorts working

--- starter-code/src/App.js
+++ starter-code/src/App.js
@@ -5,22 +5,74 @@ import contacts from './contacts.json'
 
 class App extends Component {
   state= {
-    contactsList: contacts.slice(0, 5)
+    contactsList: contacts.slice(0, 5),
   };
+
+  randomContact = () => {
+    const randomNumber = Math.floor(Math.random() * contacts.length);
+    const newRandomContact = contacts.splice(randomNumber, 1);
+    // OPCION 2
+    // const newRandomContact = contacts[randomNumber];
+
+    let updatedList = [...this.state.contactsList, newRandomContact[0]];
+    this.setState({contactsList: updatedList});
+    // escribir aca OPCION 2
+
+    console.log(newRandomContact[0]);
+    console.log(updatedList);
+  };
+
+  sortByName = () => {
+    const sortedContactsList = this.state.contactsList.sort(function (objA, objB) {
+      if (objA.name < objB.name ) {
+        return -1;
+
+      } else if (objA.name > objB.name) {
+        return 1;
+      } else {
+        return 0;
+      }
+    })
+    this.setState({contactsList: sortedContactsList})
+  }
+
+
+  sortByPopularity = () => {
+    const sortedContactsList = this.state.contactsList.sort(function (objA, objB) {
+      if (objA.popularity < objB.popularity ) {
+        return 1;
+
+      } else if (objA.popularity > objB.popularity) {
+        return -1;
+      } else {
+        return 0;
+      }
+    })
+    this.setState({contactsList: sortedContactsList})
+  }
 
 
   render() {
     return (
       <div className="App">
-      <table>
-        {this.state.contactsList.map((contactObj) => {
-          return <tr> { contactObj.name } </tr>
+      <h1>IronContacts</h1>
+      <button onClick= {this.randomContact} >Add random contact</button>
+      <button onClick= {this.sortByName}>Sort by Name</button>
+      <button onClick= {this.sortByPopularity}>Sort by popularity</button>
+
+        {this.state.contactsList.map(contactObj => {
+          return (
+            <div className="actor-card" key={contactObj.id}>
+              <img src={contactObj.pictureUrl} alt=""/>
+              <h1>{contactObj.name}</h1>
+              <h2>{contactObj.popularity}</h2>            
+            </div>
+          );
         })}
-      </table>
-    
+
       </div>
     );
   }
-}
+};
 
-export default App;
+export default App
