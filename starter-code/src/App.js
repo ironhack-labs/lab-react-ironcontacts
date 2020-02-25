c96c1b91c6f1f8585f6fac6a3378881c284377e4
Pablo Carceller <pablocarceller@me.com>
2020-02-25 19:32:37 +0100

    Done

--- starter-code/src/App.js
+++ starter-code/src/App.js
@@ -3,21 +3,92 @@ import "./App.css";
 import contacts from "./contacts.json";
 import Contact from "./contact.js";
 
+let reduceContacts = [...contacts];
+let arrContacts = reduceContacts.slice(0, 5);
+
 class App extends Component {
-  state = { contacts };
+  constructor() {
+    super();
+    this.state = { arrContacts };
+  }
+
+  addRandomContact = () => {
+    let numRandom = Math.floor(Math.random() * contacts.length);
+    arrContacts.push(contacts[numRandom]);
+    this.setState({
+      ...this.state,
+      arrContacts
+    });
+  };
+
+  sortContactsAlph = () => {
+    let sortNames = arrContacts.sort((a, b) => a.name.localeCompare(b.name));
+
+    this.setState({
+      ...this.state,
+      sortNames
+    });
+  };
+
+  sortContactsByPopularity = () => {
+    let sortPopularity = arrContacts.sort(function(a, b) {
+      return b.popularity - a.popularity;
+    });
+    this.setState({
+      ...this.state,
+      sortPopularity
+    });
+  };
+
+  deleteContact(contactID) {
+    console.log(contactID);
+    let newState = { ...this.state };
+    let filteredContacts = newState.arrContacts.filter(
+      contact => contact.id !== contactID
+    );
+    newState.arrContacts = filteredContacts;
+    this.setState(newState);
+  }
+
   render() {
     return (
-      <div className="contacts-grid">
+      <div className="oneContacts">
         <h1>IronContacts</h1>
-        <div className="oneContacts">
-          {this.state.contacts.map(contact => (
-            <Contact
-              picture={contact.pictureUrl}
-              name={contact.name}
-              popularity={contact.popularity}
-            ></Contact>
-          ))}
+        <div>
+          <button onClick={this.addRandomContact} className="random-button">
+            Add random contact
+          </button>
+          <button onClick={this.sortContactsAlph} className="random-button">
+            Sort by name
+          </button>
+          <button
+            onClick={this.sortContactsByPopularity}
+            className="random-button"
+          >
+            Sort by popularity
+          </button>
         </div>
+
+        <table>
+          <thead>
+            <tr>
+              <th>Picture</th>
+              <th>Name</th>
+              <th>Popularity</th>
+            </tr>
+          </thead>
+          <tbody>
+            {this.state.arrContacts.map(contact => (
+              <Contact
+                deleteMe={() => this.deleteContact(contact.id)}
+                key={contact.id}
+                picture={contact.pictureUrl}
+                name={contact.name}
+                popularity={contact.popularity}
+              ></Contact>
+            ))}
+          </tbody>
+        </table>
       </div>
     );
   }
--- starter-code/src/contact.js
+++ starter-code/src/contact.js
@@ -3,11 +3,12 @@ import React from "react";
 export default class Contact extends React.Component {
     render() {
         return (
-            <div className="contact">
-                <img width="200" src={this.props.picture} alt="" />
-                <h1>{this.props.name}</h1>
-                <h2>{this.props.popularity}</h2>
-            </div>
+            <tr className="contact">
+                <td><img width="100" src={this.props.picture} alt="" /></td>
+                <td><h1>{this.props.name}</h1></td>
+                <td><h2>{this.props.popularity}</h2></td>
+                <td><button className="deleteButton" onClick={() => this.props.deleteMe()}>Delete</button></td>
+            </tr>
         );
     }
 }
