5367bde48deab1d09b58527c266fe8ca8e5951de
Luz Divina Delgado Castañeda <luzdivdelg@gmail.com>
2020-02-25 18:39:28 +0100

    iteration 1 updated

--- starter-code/src/components/List.js
+++ starter-code/src/components/List.js
@@ -2,12 +2,16 @@ import React, { Component } from 'react';
 import Card from './Card'
 import contacts from '../contacts.json';
 
+let filteredContacts = contacts.slice(0, 5)
+
 class ActorsList extends Component {
 
     constructor() {
         super()
         this.state = {
-            contacts
+            contacts,
+            filteredContacts
+
         }
     }
 
@@ -21,7 +25,7 @@ class ActorsList extends Component {
                         <th>Name</th>
                         <th>Popularity</th>
                     </tr>
-                    {this.state.contacts.slice(0, 5).map((elm, idx) => <Card key={idx} {...elm} />)}
+                    {this.state.filteredContacts.map((elm, idx) => <Card key={idx} {...elm} />)}
                 </table>
             </>
         )
