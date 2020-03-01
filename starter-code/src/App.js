6df8b722817ceaf00464cca3935d9d7c59019cb5
Daedra <foglzerika@gmail.com>
2020-02-29 20:40:56 -0600

    refactor | refactoring show table!

--- starter-code/src/App.js
+++ starter-code/src/App.js
@@ -1,23 +1,15 @@
 import React, { Component } from 'react';
 import './App.css';
-<<<<<<< HEAD
 import 'bulma/css/bulma.css'
 import contacts from './contacts.json';
 import ContactRow from './components/ContactRow'
 class App extends Component {
   
   state={
-      contactList : contacts.slice(0,5)
+      contactList : contacts.slice(0,5) 
     
   }
   
-
-  displayFiveElements = () =>{
-    const contactsCopy = [...this.state.contactList]
-    this.setState= ({
-            contacts : contactsCopy.slice(0,3)
-    })
-  }
   render() {
     return (
       <div className="App">
@@ -48,24 +40,6 @@ class App extends Component {
         </section>
        
        
-=======
-import List from './components/List'
-import contactList from './contacts.json'
-
-/* let data = dataContacts.slice(0,4);
- */
-class App extends Component {
-
-  state = {
-    contactsList : contactList
-  }
-  
-  render() {
-    return (
-      <div className="App">
-        {/* elementsDisplay = #number the elements we want to display */}
-        <List data={this.state.contactsList} elementsDisplay={4}  />;
->>>>>>> 01f66fe8d81c88803edacc6e49e1b9136aca2921
       </div>
     );
   }
