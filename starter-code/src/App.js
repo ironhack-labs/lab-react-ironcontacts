ae5462fe35a6c2c7eed835bc0cc304fa503aaed8
TemidoRocha <manueltemidorocha@gmail.com>
2020-03-03 15:08:32 +0000

    missing styling

--- starter-code/src/App.js
+++ starter-code/src/App.js
@@ -1,8 +1,34 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import contacts from './contacts.json';
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      list: contacts.slice(0, 5)
+    };
+    this.addContact = this.addContact.bind(this);
+    this.sortContactByName = this.sortContactByName.bind(this);
+    this.sortContactByPopularity = this.sortContactByPopularity.bind(this);
+  }
+
+  addContact() {
+    const contact = contacts[Math.floor(Math.random() * contacts.length)];
+    if (this.state.list.find(contactTry => contactTry.id === contact.id) === undefined) {
+      this.setState(previousState => ({
+        list: [...previousState.list, contact]
+      }));
+    } else this.addContact();
+  }
+
+  sortContactByName = () =>
+    this.setState(previousState => previousState.list.sort((a, b) => a.name.localeCompare(b.name)));
+
+  sortContactByPopularity = () =>
+    this.setState(previousState => previousState.list.sort((a, b) => a.popularity - b.popularity));
+
   render() {
     return (
       <div className="App">
@@ -10,9 +36,25 @@ class App extends Component {
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Welcome to React</h1>
         </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <button onClick={this.addContact}>Add Contact</button>
+        <button onClick={this.sortContactByName}>Sort By Name</button>
+        <button onClick={this.sortContactByPopularity}>Sort By Popularity</button>
+        {this.state.list.map(contact => (
+          <div className="App-intro" key={contact.id}>
+            <div>
+              <p>Picture</p>
+              <img src={contact.pictureUrl} alt={contact.name} />
+            </div>
+            <div>
+              <p>Name:</p>
+              <p>{contact.name}</p>
+            </div>
+            <div>
+              <p>Popularity</p>
+              <p>{contact.popularity}</p>
+            </div>
+          </div>
+        ))}
       </div>
     );
   }
