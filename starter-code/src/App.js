ac918e3858c1c13aa12c5a76028db8bb1c380632
TemidoRocha <manueltemidorocha@gmail.com>
2020-03-03 15:39:05 +0000

    solved

--- starter-code/src/App.js
+++ starter-code/src/App.js
@@ -27,32 +27,47 @@ class App extends Component {
     this.setState(previousState => previousState.list.sort((a, b) => a.name.localeCompare(b.name)));
 
   sortContactByPopularity = () =>
-    this.setState(previousState => previousState.list.sort((a, b) => a.popularity - b.popularity));
+    this.setState(previousState => previousState.list.sort((a, b) => b.popularity - a.popularity));
+
+  removeItem = id =>
+    this.setState(previousState => ({
+      list: previousState.list.filter(item => item.id !== id)
+    }));
 
   render() {
     return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+          <h1 className="App-title">Celebrities Contact List</h1>
         </header>
-        <button onClick={this.addContact}>Add Contact</button>
-        <button onClick={this.sortContactByName}>Sort By Name</button>
-        <button onClick={this.sortContactByPopularity}>Sort By Popularity</button>
+
+        <section className="buttons">
+          <button onClick={this.addContact}>Add Contact</button>
+          <button onClick={this.sortContactByName}>Sort By Name</button>
+          <button onClick={this.sortContactByPopularity}>Sort By Popularity</button>
+        </section>
+
         {this.state.list.map(contact => (
           <div className="App-intro" key={contact.id}>
             <div>
-              <p>Picture</p>
+              <h3>Picture</h3>
               <img src={contact.pictureUrl} alt={contact.name} />
             </div>
-            <div>
-              <p>Name:</p>
-              <p>{contact.name}</p>
+
+            <div className="celebriety">
+              <div>
+                <h3>Name:</h3>
+                <p>{contact.name}</p>
+              </div>
+              <div>
+                <h3>Popularity</h3>
+                <p>{contact.popularity}</p>
+              </div>
             </div>
-            <div>
-              <p>Popularity</p>
-              <p>{contact.popularity}</p>
-            </div>
+            <button className="delete" onClick={() => this.removeItem(contact.id)}>
+              Remove
+            </button>
           </div>
         ))}
       </div>
