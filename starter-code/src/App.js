b1060a68a1a965d0f4e55ec9abf7539c4e653c73
Ana Segarra <anasegarraber@gmail.com>
2020-03-08 21:50:44 +0100

    Add page title

--- starter-code/src/App.js
+++ starter-code/src/App.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import { ContactsList } from './components/ContactsList';
+import { Title } from './components/Title';
 
 const App = () => {
 	return (
 		<div>
+			<Title />
 			<ContactsList />
 		</div>
 	);
