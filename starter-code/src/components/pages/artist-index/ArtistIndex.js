bb53f4897bf5c3de0454c7fdfdc725d00ff8de5e
Ignacio Garcia Barrero <igarciabarrero@outlook.com>
2020-03-08 17:05:50 +0100

    Compledted Iteration 1

--- starter-code/src/components/pages/artist-index/ArtistIndex.js
+++ starter-code/src/components/pages/artist-index/ArtistIndex.js
@@ -5,7 +5,6 @@ import artistsArray from '../../../contacts'
 import { Container, Table } from 'react-bootstrap'
 
 
-
 class ArtistIndex extends Component {
 
   constructor() {
@@ -19,18 +18,22 @@ class ArtistIndex extends Component {
 
       <Container>
 
-        <h1>List of Artists</h1>
-
         <Table >
           <tbody>
+            <tr>
+              <th>Picture</th>
+              <th>Name</th>
+              <th>Porpularity</th>
+            </tr>
             {this.state.artists.map(artist =>
               <tr>
-                <input type="checkbox"></input>
-                <td> <img src={`${artist.pictureUrl}`} alt={artist.name} /> </td>
+                {/* <input type="checkbox"></input> */}
+                <td> <img width="70" height="100" src={`${artist.pictureUrl}`} alt={artist.name} /> </td>
                 <td> <p>{artist.name}</p> </td>
+                <td> <p>{artist.popularity}</p> </td>
 
               </tr>
-            )}
+            ).splice(0, 5)}
           </tbody>
         </Table>
 
