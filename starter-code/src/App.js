042ddb971c7be8578a2c2c01a24497060b492f1c
Luismi Feijoo <luism.feijoo26@gmail.com>
2020-03-08 15:47:49 +0100

    Add the random adding button

--- starter-code/src/App.js
+++ starter-code/src/App.js
@@ -1,16 +1,26 @@
-import React from "react";
+import React, { useState } from "react";
 //import logo from "./logo.svg";
 import "./App.css";
 import { Item } from "./components/item";
 import contacts from "./contacts.json";
 import { Title } from "./components/title";
 
+const n = 5;
+const data = contacts.splice(0, n);
+
 export const App = () => {
-  const n = 5;
-  const displayedContacts = contacts.splice(0, n);
+  const [displayedContacts, setState] = useState(data);
+
+  const addRandom = () => {
+    const newContacts = [...displayedContacts];
+    const randomContact = contacts[Math.floor(Math.random() * contacts.length)];
+    newContacts.push(randomContact);
+    setState(newContacts);
+  };
 
   return (
     <div>
+      <button onClick={() => addRandom()}>Add a random contact</button>
       <Title />
       {displayedContacts.map(e => (
         <Item
