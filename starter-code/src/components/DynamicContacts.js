375c6e8dc86c9186b8446ed9cfb812c41c1a8b58
Gabriela Zapata <thegabyzapata@gmail.com>
2020-03-08 23:20:39 +0100

    Iteration 3 done

--- starter-code/src/components/DynamicContacts.js
+++ starter-code/src/components/DynamicContacts.js
@@ -19,11 +19,50 @@ export default class DynamicContacts extends Component{
         });
     }
 
-    render () {
+    sortByName = () => {
+        function compare(a, b){
+            const contactA = a.name.toUpperCase();
+            const contactB = b.name.toUpperCase();
 
+            let compareContacts = 0;
+            if (contactA > contactB) {
+                compareContacts = 1;
+            } else if (contactA < contactB) {
+                compareContacts = -1;
+            }
+            return compareContacts;
+        }
+
+        this.setState({
+            contacts: [...this.state.contacts.sort(compare)]
+        })
+    }
+
+    sortByPopularity = () => {
+        function compare(a, b){
+            const contactA = a.popularity;
+            const contactB = b.popularity;
+
+            let comparePopularity = 0;
+            if (contactA > contactB) {
+                comparePopularity = 1;
+            } else if (contactA < contactB) {
+                comparePopularity = -1;
+            }
+            return comparePopularity;
+        }
+
+        this.setState({
+            contacts: [...this.state.contacts.sort(compare)]
+        })        
+    }
+
+    render () {
         return (
             <div>
-                <button onClick={this.addRandomContact}>Add Rondom Contact</button>
+                <button onClick={this.addRandomContact}>Add Random Contact</button>
+                <button onClick={this.sortByName}>Sort By Name</button>
+                <button onClick={this.sortByPopularity}>Sort By Popularity</button>
                 <div className="contact-table">
                     <table>
                         <thead>
