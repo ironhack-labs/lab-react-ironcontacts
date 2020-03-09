f3968205a253af79e7089c7e65dfed24fb5d478a
Gonzalo Hernández <gonzalohernandez@icloud.com>
2020-03-09 14:53:47 +0100

    done

--- starter-code/src/App.js
+++ starter-code/src/App.js
@@ -1,21 +1,18 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import contacts from "./contacts.json";
+import ContactsList from "./components/ContactsList";
+import "bootstrap/dist/css/bootstrap.min.css";
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
-  }
-}
+const App = () => (
+  <div className="App">
+    <header className="App-header">
+      <img src={logo} className="App-logo" alt="logo" />
+      <h1 className="App-title">IronContacts</h1>
+    </header>
+    <ContactsList contacts={contacts.splice(0, 5)} />
+  </div>
+);
 
 export default App;
---
+++ starter-code/src/components/ContactsList.js
@@ -0,0 +1,22 @@
+import React from "react";
+import ContactItem from "./ContactItem";
+import Table from "react-bootstrap/Table";
+
+const ContactsList = props => (
+  <Table className="col-md-4 mx-auto mt-4" striped bordered hover>
+    <thead>
+      <tr>
+        <th>Picture</th>
+        <th>Name</th>
+        <th>Popularity</th>
+      </tr>
+    </thead>
+    <tbody>
+      {props.contacts.map((contact, index) => (
+        <ContactItem contact={contact} />
+      ))}
+    </tbody>
+  </Table>
+);
+
+export default ContactsList;
