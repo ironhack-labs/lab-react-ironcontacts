1beeac5fe8a898cf9745f4230a709035b3b52eaa
vctorChamizo <victor.chamizo96@gmail.com>
2020-03-09 17:17:54 +0100

    Add tasklist item

--- starter-code/src/App.js
+++ starter-code/src/App.js
@@ -1,18 +1,15 @@
 import React from "react";
 
-import logo from "./logo.svg";
-import contacts from "./contacts.json";
+import { Header } from "./components/Header";
+import { TaskList } from "./components/TaskList";
 
 import "./App.css";
 
 export const App = () => (
   <div className="App">
-    <header className="App-header">
-      <img src={logo} className="App-logo" alt="logo" />
-      <h1 className="App-title">Welcome to React</h1>
-    </header>
-    <p className="App-intro">
-      To get started, edit <code>src/App.js</code> and save to reload.
-    </p>
+    <Header />
+    <main>
+      <TaskList />
+    </main>
   </div>
 );
--- starter-code/src/index.js
+++ starter-code/src/index.js
@@ -1,10 +1,10 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React from "react";
+import ReactDOM from "react-dom";
 
-import App from './App';
+import App from "./App";
 //import registerServiceWorker from './registerServiceWorker';
 
-import './index.css';
+import "./index.css";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<App />, document.getElementById("root"));
 //registerServiceWorker();
