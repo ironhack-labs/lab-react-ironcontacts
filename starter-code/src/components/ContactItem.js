9f390392ad585a109f70c710cfdb438be8672cf7
J. A. Hernández <joseanher81@gmail.com>
2020-03-10 23:12:12 +0100

    List components created

---
+++ starter-code/src/components/ContactItem.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const ContactItem = () => {
+
+  return(
+    <div>Contact Item cargado</div>
+  );
+}
+
+
+export default ContactItem;
---
+++ starter-code/src/components/ContactList.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import ContactItem from './ContactItem';
+
+const ContactList = () => {
+
+  return(
+    <div>
+      <p>Contact List Cargada</p>
+      <ContactItem />
+    </div>
+  );
+}
+
+
+export default ContactList;
