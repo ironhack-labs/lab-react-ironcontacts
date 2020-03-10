f82da4ceba0118a959d443433d12252d9c577abe
Román Méndez <mendezbeck@gmail.com>
2020-03-10 17:25:21 +0100

    site running on parcel

--- starter-code/src/App.jsx
+++ starter-code/src/App.jsx
@@ -1,21 +1,21 @@
-import React, { Component } from "react";
-import logo from "./logo.svg";
-import "./App.css";
-import contacts from "./contacts.json";
+import React from "react";
+import ReactDOM from "react-dom";
+import logo from "./public/logo.svg";
+import "./public/App.css";
+import contacts from "./public/contacts.json";
 
 console.log(contacts);
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-      </div>
-    );
-  }
-}
+const App = () => (
+  <div className="App">
+    <header className="App-header">
+      <img src={logo} className="App-logo" alt="logo" />
+      <h1 className="App-title">Welcome to React</h1>
+    </header>
+  </div>
+);
 
-export default App;
+document.addEventListener("DOMContentLoaded", () => {
+  document.getElementById("root");
+  ReactDOM.render(<App />, root);
+});
---
+++ starter-code/src/components/Contact.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Contacts = () => {
+  <table>
+    <tr>
+      <td>
+        <img src="{contacts.pictureUrl}" alt="contact image" />
+      </td>
+      <td>{contacts.name}</td>
+      <td>{contacts.popularity}</td>
+    </tr>
+  </table>;
+};
