61da693ba71ca048efc7b032e101ff4a43dbe2e9
Fran <fmjerez@icloud.com>
2020-03-11 08:11:19 +0100

    Iteration_1 Done

--- starter-code/src/App.js
+++ starter-code/src/App.js
@@ -1,21 +1,20 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
-
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
-  }
+import React from "react";
+import "./App.css";
+import Table from "./components/Table";
+function App() {
+  return (
+    <div className="App">
+      <header>
+        <h1 className="App-title">IronContacs</h1>
+        <div className="tHead">
+          <p>Picture</p>
+          <p className="NamePosition">Name</p>
+          <p>Popularity</p>
+        </div>
+      </header>
+      <Table></Table>
+    </div>
+  );
 }
 
 export default App;
