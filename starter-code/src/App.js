ecb9a2facfdd483927ab37fb8c438d0488b7bb7d
Teresa Pelinski <teresapelinski@gmail.com>
2020-03-11 16:23:24 +0100

    function oriented

--- starter-code/src/App.js
+++ starter-code/src/App.js
@@ -1,7 +1,24 @@
-import React, { Component } from 'react';
+//import React, { Component } from 'react';
+import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+//with functions
+const App =  () => {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <h1 className="App-title">Welcome to React</h1>
+      </header>
+      <p className="App-intro">
+        To get started, edit <code>src/App.js</code> and save to reload.
+      </p>
+    </div>
+  );
+};
+
+/* with classes
 class App extends Component {
   render() {
     return (
@@ -16,6 +33,6 @@ class App extends Component {
       </div>
     );
   }
-}
+}*/
 
 export default App;
