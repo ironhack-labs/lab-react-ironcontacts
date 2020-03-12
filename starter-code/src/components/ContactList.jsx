cc74c31eb3edef09293465da0f4229342daa9475
Román Méndez <mendezbeck@gmail.com>
2020-03-12 14:39:46 +0100

    addRandomContact added and recorganized structure

--- starter-code/src/components/ContactList.jsx
+++ starter-code/src/components/ContactList.jsx
@@ -1,26 +1,23 @@
 import React from "react";
-import contactsList from "../public/contacts.json";
 import { ContactItem } from "./ContactItem";
 
-export const ContactList = () => {
-  const random = Math.floor(Math.random() * contactsList.length);
-  const randomItem = contactsList[random];
-  const displayNumber = 5;
-  const displayList = contactsList.splice(0, displayNumber);
+export const ContactList = ({ list }) => {
   return (
-    <div className="contacts-list">
-      <table>
-        <tbody>
-          <tr className="contacts-header">
-            <th>Picture</th>
-            <th className="contact-name">Name</th>
-            <th>Populatiry</th>
-          </tr>
-          {displayList.map((item, i) => (
-            <ContactItem key={i} picture={item.pictureUrl} name={item.name} popularity={Math.round(item.popularity * 100) / 100}></ContactItem>
-          ))}
-        </tbody>
-      </table>
-    </div>
+    <>
+      <div className="contacts-list">
+        <table>
+          <tbody>
+            <tr className="contacts-header">
+              <th>Picture</th>
+              <th className="contact-name">Name</th>
+              <th>Populatiry</th>
+            </tr>
+            {list.map((item, i) => (
+              <ContactItem key={i} picture={item.pictureUrl} name={item.name} popularity={Math.round(item.popularity * 100) / 100}></ContactItem>
+            ))}
+          </tbody>
+        </table>
+      </div>
+    </>
   );
 };
