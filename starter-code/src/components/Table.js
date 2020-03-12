71d9ceacd93fdbbc79b4d6ac61392bde452aa4f3
Fran <fmjerez@icloud.com>
2020-03-12 12:10:28 +0100

    Iteration 3 done

--- starter-code/src/components/Table.js
+++ starter-code/src/components/Table.js
@@ -1,6 +1,8 @@
 import React, { Component } from "react";
 import contactsArray from "../contacts.json";
-import Button from "./RandomButton";
+import RandomButton from "./RandomButton";
+import SortNameButton from "./SortNameButton";
+import SortPopularityButton from "./SortPopularityButton";
 
 class Contacts extends Component {
   constructor() {
@@ -8,7 +10,7 @@ class Contacts extends Component {
     this.state = { contacts: contactsArray.splice(0, 5) };
   }
 
-  handleClick() {
+  randomClick() {
     const min = 0;
     const max = contactsArray.length;
     const rand = Math.floor(Math.random() * (max - min));
@@ -18,13 +20,39 @@ class Contacts extends Component {
       contacts: [...this.state.contacts.splice(0), contactRandom]
     });
   }
+  sortNameClick() {
+    const orderContacts = this.state.contacts.sort(function(a, b) {
+      return a.name > b.name ? 1 : -1;
+    });
+    this.setState({
+      ...this.state,
+      contacts: orderContacts
+    });
+  }
+  sortPopularityClick() {
+    const orderPopularity = this.state.contacts.sort(function(a, b) {
+      return b.popularity - a.popularity;
+    });
+    this.setState({
+      ...this.state,
+      contacts: orderPopularity
+    });
+  }
 
   render() {
     return (
       <div>
-        <Button
+        <RandomButton
           contacts={this.state.contacts}
-          handleClick={this.handleClick.bind(this)}
+          randomClick={this.randomClick.bind(this)}
+        />
+        <SortNameButton
+          contacts={this.state.contacts}
+          sortNameClick={this.sortNameClick.bind(this)}
+        />
+        <SortPopularityButton
+          contacts={this.state.contacts}
+          sortPopularityClick={this.sortPopularityClick.bind(this)}
         />
         <table className="alignContent">
           <tbody>
@@ -48,7 +76,7 @@ class Contacts extends Component {
           </tbody>
         </table>
         <div>
-          <button onClick={this.handleClick.bind(this)}>Click</button>
+          <button onClick={this.randomClick.bind(this)}>Click</button>
         </div>
       </div>
     );
