a0aa419fa9b46fd29bb4e04ec3a92d34ba00180c
J. A. Hernández <joseanher81@gmail.com>
2020-03-12 20:39:15 +0100

    Random button logic

--- starter-code/src/components/RandomButton.js
+++ starter-code/src/components/RandomButton.js
@@ -1,10 +1,11 @@
 import React, {useContext} from 'react';
+import { ContactContext } from '../contexts/ContactContext';
 import Button from 'react-bootstrap/Button';
 
 const RandomButton = () => {
-
+  const {addRandom} = useContext(ContactContext);
   return(
-   <Button variant="primary">Add Random Contact</Button>
+   <Button onClick={() => addRandom()} variant="primary">Add Random Contact</Button>
   );
 }
 
--- starter-code/src/contexts/ContactContext.js
+++ starter-code/src/contexts/ContactContext.js
@@ -7,8 +7,14 @@ const ContactContextProvider = (props) => {
 
   const [contacts, setContacts] = useState(contactsJson.slice(0, 5));
 
+  const addRandom = () => {  
+    const min = Math.ceil(contacts.length);
+    const max = Math.floor(contactsJson.length);
+    setContacts([...contacts, contactsJson[Math.floor(Math.random() * (max - min + 1)) + min]]);
+  }
+
   return(
-    <ContactContext.Provider value={{contacts}}>
+    <ContactContext.Provider value={{contacts, addRandom}}>
       {props.children}
     </ContactContext.Provider>
   );
