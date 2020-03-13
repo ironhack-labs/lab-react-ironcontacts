20f877317f18803f81068868397c95117c9306b8
Ivan Esteban <ivan.estebanisabel@gmail.com>
2020-03-13 17:44:08 +0100

    Header

---
+++ starter-code/src/components/Header.js
@@ -0,0 +1,9 @@
+import React from "react";
+import logo from "../../public/img/logo.svg";
+
+export const Header = () => (
+  <header className="App-header">
+    <img src={logo} className="App-logo" alt="logo" />
+    <h1 className="App-title">IronContacts</h1>
+  </header>
+);
