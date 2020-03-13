e13dd24db080256a69a9b206abf360e7b2a13e22
Borja Domínguez <borja.d.atienza@gmail.com>
2020-03-13 17:06:13 +0100

    Start

---
+++ starter-code/src/app.js
@@ -0,0 +1,13 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+const Famous = () => {
+  <div>
+    <p>HOLAAAAAAA</p>
+  </div>;
+};
+
+document.addEventListener("DOMContentLoaded", () => {
+  const root = document.getElementById("root");
+  ReactDOM.render(<Famous />, root);
+});
