fea268232a9933961d137280b65fbc0dbcb1de8d
Borja Domínguez <borja.d.atienza@gmail.com>
2020-03-14 08:28:41 +0100

    03_Iteration Done

--- starter-code/src/components/FamousList.js
+++ starter-code/src/components/FamousList.js
@@ -17,6 +17,7 @@ function listGenerator() {
 export const FamousList = () => {
   const [lista, setLista] = useState(list);
 
+  // Random
   const random = () => {
     const newList = [...lista];
     let newFamous;
@@ -35,6 +36,34 @@ export const FamousList = () => {
     setLista(newList);
   };
 
+  // Sorts
+  // Name
+
+  const sortName = () => {
+    const newLista = [...lista];
+    let sortNameList = newLista.sort((a, b) => {
+      if (a.name > b.name) {
+        return 1;
+      } else {
+        return -1;
+      }
+    });
+    setLista(sortNameList);
+  };
+  // Popularity
+
+  const sortPopularity = () => {
+    const newLista = [...lista];
+    let sortPopuList = newLista.sort((a, b) => {
+      if (a.popularity > b.popularity) {
+        return -1;
+      } else {
+        return 1;
+      }
+    });
+    setLista(sortPopuList);
+  };
+
   return (
     <div className="table">
       <div className="buttons">
@@ -45,6 +74,22 @@ export const FamousList = () => {
           style={{ padding: "1em", border: "1px solid black" }}
         >
           Add Random Contact
+        </button>
+        <button
+          className="btn"
+          onClick={() => sortName()}
+          type="button"
+          style={{ padding: "1em", border: "1px solid black" }}
+        >
+          Sort by Name
+        </button>
+        <button
+          className="btn"
+          onClick={() => sortPopularity()}
+          type="button"
+          style={{ padding: "1em", border: "1px solid black" }}
+        >
+          Sort by Popularity
         </button>
       </div>
       <table className="listFamous">
@@ -74,22 +119,22 @@ export const FamousList = () => {
   );
 };
 
-// Random Contact
+// // Random Contact
 
-function newContact() {
-  let max = Contacts.length;
-  let contactRandom = Contacts[Math.floor(Math.random() * max)];
-  return contactRandom;
-}
+// function newContact() {
+//   let max = Contacts.length;
+//   let contactRandom = Contacts[Math.floor(Math.random() * max)];
+//   return contactRandom;
+// }
 
-const random = () => {
-  let x = newContact();
-  if (list.includes(x)) {
-    newContact();
-  } else {
-    newFamous = x;
-  }
-};
+// const random = () => {
+//   let x = newContact();
+//   if (list.includes(x)) {
+//     newContact();
+//   } else {
+//     newFamous = x;
+//   }
+// };
 
 // export const Button = () => {
 //   return (
