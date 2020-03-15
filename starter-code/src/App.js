2234a395995408b29e4537c9a297fd9b7969ee04
Evgeny Galitskiy <gha665@gmail.com>
2020-03-14 22:54:42 -0400

    iterations 1 - 4 done

--- starter-code/src/App.js
+++ starter-code/src/App.js
@@ -1,18 +1,58 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+// eslint-disable-next-line
+import actors from "./contacts.json";
+import "./App.css";
+import Contacts from "./components/Contacts/Contacts";
+
+const constActors = [...actors];
 
 class App extends Component {
+  state = {
+    constActors,
+    actors: [...constActors].slice(0, 5)
+  };
+
+  addRandomContact = (e) => {
+    const { actors, constActors } = this.state;
+    const currentActorsIds = [...actors].map(actor => actor.id);
+    const restActors = [...constActors].filter(actor => !currentActorsIds.includes(actor.id));
+    let randomActor = restActors[Math.floor(Math.random() * restActors.length)];
+    console.log(randomActor);
+    const copyActors = [...actors, randomActor];
+    this.setState({
+      actors: copyActors
+    })
+  }
+
+  sortByName = (e) => {
+    const { actors } = this.state;
+    const sortedActors = [...actors].sort((a,b) => a.name > b.name ? 1 : a.name < b.name ? -1 : 0);
+    this.setState({
+      actors: sortedActors
+    })
+  }
+
+  sortByPopularity = (e) => {
+    const { actors } = this.state;
+    const sortedActors = [...actors].sort((a,b) => a.popularity > b.popularity ? 1 : a.popularity < b.popularity ? -1 : 0);
+    this.setState({
+      actors: sortedActors
+    })
+  }
+
+  deleteContact = (e) => {
+    const { actors } = this.state;
+    const filteredActors = [...actors].filter(actor => actor.id !== e.target.id);
+    this.setState({
+      actors: filteredActors
+    })
+  }
+
   render() {
+    const { actors } = this.state;
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <Contacts actors={actors} addRandomContact={this.addRandomContact} sortByName={this.sortByName} sortByPopularity={this.sortByPopularity} deleteContact={this.deleteContact}/>
       </div>
     );
   }
---
+++ starter-code/src/components/contacts.js
@@ -0,0 +1,31 @@
+import React from "react";
+import './Contacts.css';
+
+const Contacts = ({ actors, addRandomContact, sortByName, sortByPopularity, deleteContact }) => {
+  return (
+    <div className="">
+        <h1>IronContacts</h1>
+          <button onClick={addRandomContact}>Add Random Contact</button>
+          <button onClick={sortByName}>Sort By Name</button>
+          <button onClick={sortByPopularity}>Sort By Popularity</button>
+      <ul>
+          <span>Picture</span>
+          <span>Name</span>
+          <span>Popularity</span>
+          <span>Action</span>
+        {actors.map(actor => {
+          return (
+            <div className="contacts" key={actor.id}>
+                <li><img src={actor.pictureUrl} alt="actor "/></li>
+                <li>{actor.name}</li>
+                <li>{actor.popularity.toFixed(2)}</li>
+                <li><button id={actor.id} onClick={deleteContact}>Delete</button></li>
+            </div>
+          )
+        })}
+      </ul>
+    </div>
+  );
+};
+
+export default Contacts;
