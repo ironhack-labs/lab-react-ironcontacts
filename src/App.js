03f65effc8439a1c35aa1ad1377ca2d3fab8c0ef
Gustavo Jordao <gustavo.jordao021@gmail.com>
2020-03-16 19:07:56 -0400

    Finish iterations 1-2

--- src/App.js
+++ src/App.js
@@ -1,18 +1,53 @@
 import React, { Component } from 'react';
 import Contacts from "./contacts.json"
-import ContactList from "./ContactList/ContactList"
-import "./ContactList/ContactList.css"
 import './App.css';
 
 class App extends Component {
-  state = {...Contacts};
+  state = {
+    contacts: [...Contacts.slice(0, 5)]
+  }
+  
+  addRandomActor = () => {
+    const contactsCopy = [...this.state.contacts];
+    while(true) {
+      if (contactsCopy.includes(Contacts[Math.floor(Math.random() * Contacts.length)])) {
+        continue;
+      } else {
+        contactsCopy.push(Contacts[Math.floor(Math.random() * Contacts.length)]);
+        break;
+      }
+    }
+    this.setState({
+      contacts: contactsCopy
+    });
+  };
 
   render() {
     return (
       <div className="App">
         <h1>IronContacts</h1>
-        {console.log(this.state)}
-        <ContactList contact={this.state} />
+        <button onClick={() => this.addRandomActor()}>Add a random contact</button>
+        <div className="list-titles">
+            <table>
+              <thead>
+                <tr>
+                  <th>Picture</th>
+                  <th>Name</th>
+                  <th>Popularity</th>
+                </tr>
+              </thead>
+              <tbody>
+                {this.state.contacts.map((oneActor, index) => {
+                  return (
+                <tr key={index}>
+                  <td><img src={oneActor.pictureUrl} alt="actors" /></td>
+                  <td><p>{oneActor.name}</p></td>
+                  <td><p>{oneActor.popularity}</p></td>
+                </tr>
+                )})}
+              </tbody>
+            </table>
+        </div>
       </div>
     );
   }
