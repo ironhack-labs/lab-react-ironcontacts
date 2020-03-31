d8dfb64d16683aaa5b37c4e8821f51fac73cd384
unai gonzalez <unaigoe91@gmail.com>
2020-03-31 20:28:10 +0200

    BCN-FT-Unai Gonzalez

--- starter-code/src/App.js
+++ starter-code/src/App.js
@@ -1,18 +1,95 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import contacts from "./contacts.json";
+import "./App.css";
+
+//console.log(contacts)
 
 class App extends Component {
+  state = {
+    Celebrities: contacts.splice(0, 5)
+  }
+
+  addRandomContact = () => {
+    let randomContact = Math.floor(Math.random() * contacts.length);
+    let addContact = this.state.Celebrities;
+    addContact.push(contacts[randomContact]);
+
+    this.setState({
+      Celebrities: addContact
+    })
+
+    console.log(typeof addContact)
+
+    return addContact
+  }
+
+  sortContactbyName = () => {
+    let sorted= this.state.Celebrities
+    sorted.sort(function (a, b) {
+      if (b.name > a.name) {
+        return -1;
+      }
+      if (b.name < a.name) {
+        return 1;
+      }
+      return 0;
+    });
+    this.setState({
+      Celebrities : sorted
+    })
+  }
+  
+  sortContactbByPopularity = () => {
+    let sortedByPop= this.state.Celebrities
+    sortedByPop.sort((a,b)=>b.popularity-a.popularity);
+
+    this.setState({
+      Celebrities : sortedByPop
+    })
+  }
+
+
+  deleteContact = (celebId) => {
+    console.log(celebId)
+    const celebritiesCopy = [...this.state.Celebrities];
+    const filtered = celebritiesCopy.filter(celeb => celeb.id !== celebId); 
+
+   
+      this.setState({
+          Celebrities: filtered
+      })
+
+  }
+
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+
+      <div>
+        <h1>Iron contacts</h1>
+        {this.state.Celebrities.map(contacts => {
+          return (
+            <div key={contacts.id}>
+
+              <table width='400px'>
+                <tr>
+                  <th>Picture</th>
+                  <th>Name</th>
+                  <th>Popularity</th>
+                </tr>
+                <tr>
+                  <td><img height='90px' src={contacts.pictureUrl} alt='' /></td>
+                  <td><h3>{contacts.name}</h3></td>
+                  <td><h3>{contacts.popularity}</h3></td>
+                </tr>
+                <button onClick={this.deleteContact}>Delete</button>
+              </table>
+            </div>
+          );
+        })}
+        <button onClick={this.addRandomContact}> Add Random Contact</button>
+        <button onClick={this.sortContactbyName}> Sort by Name</button>
+        <button onClick={this.sortContactbByPopularity}> Sort by Popularity</button>
+
       </div>
     );
   }
