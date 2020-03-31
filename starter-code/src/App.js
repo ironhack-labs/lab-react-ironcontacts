54191e29f5bf120860852ed48f21b0d5daa091c6
Jorge Sb <jorgesanchezblanco1989@gmail.com>
2020-03-31 19:13:33 +0200

    Completed third iteration

--- starter-code/src/App.js
+++ starter-code/src/App.js
@@ -6,55 +6,80 @@ import contacts from "./contacts.json";
 class App extends Component {
   state = {
     contactsInTable: contacts.slice(0, 5),
-    contactOutsideOfTheTable: contacts.slice(5),
+    contactOutsideOfTheTable: contacts.slice(5)
   };
 
-
-  AddRandomContact=()=>{
-    let updatedContactsInTable =[];
-    let remainingContacts = this.state.contactOutsideOfTheTable
-    let randomContact = Math.floor(Math.random()*this.state.contactOutsideOfTheTable.length)
-    console.log(randomContact)
-    updatedContactsInTable = this.state.contactsInTable
-    updatedContactsInTable.push(remainingContacts[randomContact])
-    remainingContacts.splice(randomContact, 1)
+  addRandomContact = () => {
+    let updatedContactsInTable = [];
+    let remainingContacts = this.state.contactOutsideOfTheTable;
+    let randomContact = Math.floor(
+      Math.random() * this.state.contactOutsideOfTheTable.length
+    );
+    updatedContactsInTable = this.state.contactsInTable;
+    updatedContactsInTable.push(remainingContacts[randomContact]);
+    remainingContacts.splice(randomContact, 1);
     this.setState({
       contactsInTable: updatedContactsInTable,
-      contactOutsideOfTheTable:remainingContacts,
+      contactOutsideOfTheTable: remainingContacts
     });
-    console.log(this.state.contactsInTable)
+  };
 
+  sortByPopularity = () => {
+    let sortedContactsInTable = this.state.contactsInTable.sort(function(a, b) {
+      if (a.popularity > b.popularity) {
+        return -1;
+      }
+      if (a.popularity < b.popularity) {
+        return 1;
+      }
+    });
+    this.setState({
+      contactsInTable: sortedContactsInTable
+    });
+  };
 
-  }
+  sortByName = () => {
+    let sortedContactsInTable = this.state.contactsInTable.sort(function(a, b) {
+      if (a.name > b.name) {
+        return 1;
+      }
+      if (a.name < b.name) {
+        return -1;
+      }
+    });
+    this.setState({
+      contactsInTable: sortedContactsInTable
+    });
+  };
 
-
- render()
-   {
+  render() {
     return (
       <div className="App">
-      <h1>Ironcontacts</h1>
-      <button onClick={this.AddRandomContact}>Add a random contact</button>
-        <table className="Table">   
-        <thead>
-          <tr>
-            <th>Picture</th>
-            <th>Name</th>
-            <th>Popularity</th>
-          </tr>
-        </thead>
-        <tbody>
-          {this.state.contactsInTable.map(function(celebrity) {
-            return (
-              <tr key={celebrity.id}>
-                <td>
-                  <img src={celebrity.pictureUrl} alt="" />
-                </td>
-                <td>{celebrity.name}</td>
-                <td>{celebrity.popularity}</td>
-              </tr>
-            );
-          })}
-        </tbody>
+        <h1>Ironcontacts</h1>
+        <button onClick={this.addRandomContact}>Add a random contact</button>
+        <button onClick={this.sortByName}>Sort by name</button>
+        <button onClick={this.sortByPopularity}>Sort by popularity</button>
+        <table className="Table">
+          <thead>
+            <tr>
+              <th>Picture</th>
+              <th>Name</th>
+              <th>Popularity</th>
+            </tr>
+          </thead>
+          <tbody>
+            {this.state.contactsInTable.map(function(celebrity) {
+              return (
+                <tr key={celebrity.id}>
+                  <td>
+                    <img src={celebrity.pictureUrl} alt="" />
+                  </td>
+                  <td>{celebrity.name}</td>
+                  <td>{celebrity.popularity}</td>
+                </tr>
+              );
+            })}
+          </tbody>
         </table>
       </div>
     );
