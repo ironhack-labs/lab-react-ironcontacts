2f5090e9ab04a66a976f4985203c2f4268c7b2d4
Miguel Dominguez Chamorro <mdchamorro7@gmail.com>
2020-03-31 19:38:55 +0200

    Display

---
+++ starter-code/src/components/Header.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+const Header = () => {
+    return(
+    <>
+    <h1>Iron Contacts</h1>
+    <table>
+      <tr>
+        <td><b>Picture</b></td>
+        <td><b>Name</b></td>
+        <td><b>Popularity</b></td>
+      </tr>
+      </table>
+      </>
+    )
+}
+
+export default Header;
