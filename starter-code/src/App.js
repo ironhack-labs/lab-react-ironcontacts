6632db9f7b800e14fc2139c6fc98a6508f004cb2
Jorge Sb <jorgesanchezblanco1989@gmail.com>
2020-03-31 18:50:28 +0200

    Completed second iteration

--- starter-code/src/App.js
+++ starter-code/src/App.js
@@ -2,20 +2,47 @@ import React, { Component } from "react";
 import logo from "./logo.svg";
 import "./App.css";
 import contacts from "./contacts.json";
-let initialContacts = contacts.slice(0, 5);
-
 
 class App extends Component {
-  render() {
+  status = {
+    contactsInTable: contacts.slice(0, 5),
+    contactOutsideOfTheTable: contacts.slice(5),
+  };
+
+
+  AddRandomContact=()=>{
+    let updatedContactsInTable =[];
+    let remainingContacts = this.status.contactOutsideOfTheTable
+    let randomContact = Math.floor(Math.random(remainingContacts.length))
+    updatedContactsInTable = this.status.contactsInTable
+    updatedContactsInTable.push(remainingContacts[randomContact])
+    remainingContacts.splice(randomContact, 1)
+    this.setState({
+      contactsInTable: updatedContactsInTable,
+      contactOutsideOfTheTable:remainingContacts,
+    });
+    console.log(this.status.contactsInTable)
+
+
+  }
+
+
+ render()
+   {
     return (
       <div className="App">
-        <table className="Table">
+      <h1>Ironcontacts</h1>
+      <button onClick={this.AddRandomContact}>Add a random contact</button>
+        <table className="Table">   
+        <thead>
           <tr>
             <th>Picture</th>
             <th>Name</th>
             <th>Popularity</th>
           </tr>
-          {initialContacts.map(function(celebrity) {
+        </thead>
+        <tbody>
+          {this.status.contactsInTable.map(function(celebrity) {
             return (
               <tr key={celebrity.id}>
                 <td>
@@ -26,6 +53,7 @@ class App extends Component {
               </tr>
             );
           })}
+        </tbody>
         </table>
       </div>
     );
