f2a2f9e832844393e403bb8fdea8988f05f34443
Carles Galí Bou <carles_ga@hotmail.com>
2020-04-10 00:15:13 +0200

    Iteration 2 done and all th CSS for the BONUS: Iteration 5

--- starter-code/src/App.js
+++ starter-code/src/App.js
@@ -4,20 +4,43 @@ import ContactTable from './components/ContactTable';
 import './App.css';
 
 class App extends Component {
+  state = {
+    fiveContacts: Contacts.slice(0,5),
+  }
+
+  getRandomIntInclusive(min, max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+  }
+
+  handleClickRandomProfile = () => {
+    this.setState({
+      fiveContacts: [
+        ...this.state.fiveContacts,
+        Contacts[this.getRandomIntInclusive(0, Contacts.length -1)]
+      ]
+    });
+  }
+
   render() {
-    const fiveContacts = Contacts.slice(0,5);
-    console.log(fiveContacts)
     return (
       <div className="App">
+        <div className="buttons-box">
+          <button className="buttons" onClick={ this.handleClickRandomProfile }>Add random profile</button>
+          <button className="buttons" onClick>Sort by name</button>
+          <button className="buttons" onClick>Sort by popularity</button>
+        </div>
+        
         <table className="table-box">
           <tr className="header-table">
             <th>Picture</th>
             <th>Name</th>
             <th>Popularity</th>
           </tr>
-            {fiveContacts.map(profile => (
-              <ContactTable image={profile.pictureUrl} name={profile.name} popularity={profile.popularity} />
-            ))};
+            {this.state.fiveContacts.map(profile => (
+              <ContactTable image={ profile.pictureUrl } name={ profile.name } popularity={ profile.popularity } />
+            ))}
         </table>
       </div>
     );
