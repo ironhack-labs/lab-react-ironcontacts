bb3026e24d0643343f58d188ba97b3a88d2753d1
alejandro <alejimenezgitgithub@gmail.com>
2020-04-12 00:36:14 +0200

    iteration 3 and bonus css

--- starter-code/src/App.js
+++ starter-code/src/App.js
@@ -12,13 +12,11 @@ class App extends Component {
 
   getRandomNum = () => {
     let a = Math.floor(Math.random() * (contacts.length - 0 + 1)) + 0;
-    console.log(a);
     return a;
   }
 
   getRandomContact = () => {
     let b = contacts[this.getRandomNum()];
-    console.log(b);
     return b;
   }
 
@@ -26,26 +24,62 @@ class App extends Component {
     this.setState({
       Showcontacts: [...this.state.Showcontacts, this.getRandomContact()]
     })
-    console.log(this.state.Showcontacts);
+  }
+
+  sortByName = () => {
+    this.setState({
+      Showcontacts: this.sortArrayByName()
+    })
+  }
+
+  hola = () => {
+    console.log("hola");
+  }
+
+  sortArrayByName = () => {
+    return this.state.Showcontacts.sort(function (a,b) {
+      const bandA = a.name.toUpperCase();
+      const bandB = b.name.toUpperCase();
+
+      let comparison = 0;
+      if (bandA > bandB) {
+        comparison = 1;
+      } else if (bandA < bandB) {
+        comparison = -1;
+      }
+      return comparison;
+    })
+  }
+
+  sortArrayByNumber = () => {
+    return  this.state.Showcontacts.sort(function(a, b) {
+      return b.popularity - a.popularity;
+    });
+  }
+
+  sortByPop = () => {
+    this.setState({
+      Showcontacts: this.sortArrayByNumber()
+    })
   }
 
   render() {
     return (
       <div className="App">
+
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+          <h1 className="App-title">IronContactsX</h1>
+          <button onClick={this.addRandomContact} className="mt-30 btn"> Add Random Contact </button>
+          <button onClick={this.sortByName} className="mt-30 btn"> Sort by name </button>
+          <button onClick={this.sortByPop} className="mt-30 btn"> Sort by popularity </button>
         </header>
-        <h1 className="App-intro mt-30 bg-grey">
-          IronContactsX
-        </h1>
 
-        <button onClick={this.addRandomContact} className="mt-30"> Add Random Contact </button>
-
-        <div className="ml-29 inLine mt-30">
-          <div className="with20"> Picture </div>
-          <div className="with20"> Name </div>
-          <div className="with20"> Popularity </div>
+        <div className="ml inLine mt-30">
+          <div className="with20 bg-blue"> Picture </div>
+          <div className="with20 bg-blue"> Name </div>
+          <div className="with20 bg-blue"> Popularity </div>
+          <div className="with20 bg-blue"> Action </div>
         </div>
 
         {this.state.Showcontacts.map((item,index) => {
