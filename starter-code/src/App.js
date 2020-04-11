9bd6d6ad06d23e0fced3c2dd4ae8e51d802120de
Pablolo <pablo_olondriz@hotmail.com>
2020-04-11 18:09:27 +0200

    iteration 1 done

--- starter-code/src/App.js
+++ starter-code/src/App.js
@@ -1,18 +1,47 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import contacts from './contacts.json';
 import './App.css';
 
+let movieStars = contacts.slice(0, 5);
+
 class App extends Component {
+
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <h1 className="homepage-title">IronContacts</h1>
+        <table className="homepage-table">
+          <tr>
+            <th>Picture</th>
+            <th>Name</th>
+            <th>Popularity</th>
+          </tr>
+          <tr>
+            <td><img className="table-img" src={movieStars[0].pictureUrl} alt=""/></td>
+            <td>{movieStars[0].name}</td>
+            <td>{movieStars[0].popularity}</td>
+          </tr>
+          <tr>
+            <td><img className="table-img" src={movieStars[1].pictureUrl} alt=""/></td>
+            <td>{movieStars[1].name}</td>
+            <td>{movieStars[1].popularity}</td>
+          </tr>
+          <tr>
+            <td><img className="table-img" src={movieStars[2].pictureUrl} alt=""/></td>
+            <td>{movieStars[2].name}</td>
+            <td>{movieStars[2].popularity}</td>
+          </tr>
+          <tr>
+            <td><img className="table-img" src={movieStars[3].pictureUrl} alt=""/></td>
+            <td>{movieStars[3].name}</td>
+            <td>{movieStars[3].popularity}</td>
+          </tr>
+          <tr>
+            <td><img className="table-img" src={movieStars[4].pictureUrl} alt=""/></td>
+            <td>{movieStars[4].name}</td>
+            <td>{movieStars[4].popularity}</td>
+          </tr>
+        </table>
       </div>
     );
   }
