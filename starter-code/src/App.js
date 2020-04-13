c4cde7a00ef397bba69c7f7ffb256f105ba9a695
Edu Carmona <dudu.carmona@gmail.com>
2020-04-13 11:39:21 +0200

    Iteration 2 | Add New Random Contacts

--- starter-code/src/App.js
+++ starter-code/src/App.js
@@ -4,14 +4,39 @@ import contacts from './contacts.json';
 import Contact from './components/Contact';
 
 class App extends Component {
+  state = {
+    contacts: contacts.slice(0, 5),
+    ORIGINAL_NUMBER_OF_CONTACTS: contacts.length
+  }
+
+  handleClickNewRandomContact = () => {
+    const restOfContacts = contacts.slice(5, contacts.length);
+    let newRandomContact = undefined;
+    let exist = (newRandomContactId) => {
+      restOfContacts.find(contact => contact.id === newRandomContactId)
+    };
+
+    if (this.state.contacts.length !== this.state.ORIGINAL_NUMBER_OF_CONTACTS) {
+      // New contacts can still be added.
+      do {
+        newRandomContact = restOfContacts[Math.floor(Math.random() * (restOfContacts.length))];
+      } while (exist(newRandomContact.id));
+      
+      this.setState({
+        contacts: [...this.state.contacts, newRandomContact]
+      });
+
+      contacts.splice(contacts.indexOf(newRandomContact), 1);
+    } else {
+      alert('There are no contacts to add');
+    }
+  };
+
   render() {
-    const first5Contacts = contacts.slice(0, 5);
-
-    console.log(first5Contacts);
-
     return (
       <div>
         <h1 className='App-h1'>IronContacts</h1>
+        <button className='App-button' onClick={this.handleClickNewRandomContact}>Add Random Contact</button>
         <table>
           <thead>
             <tr>
@@ -21,7 +46,7 @@ class App extends Component {
             </tr>
           </thead>
           <tbody>
-            {first5Contacts.map(contact => (
+            {this.state.contacts.map(contact => (
               <Contact key={contact.id} pictureUrl={contact.pictureUrl} name={contact.name} popularity={contact.popularity}/>
             ))}
           </tbody>
