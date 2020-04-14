3be352bc5ca12e91e9994f9efb7acd1ae7470d1a
Thibault-d <thibaultdelfaud@gmail.com>
2020-04-14 12:16:26 +0200

    iteration 1

--- starter-code/src/App.js
+++ starter-code/src/App.js
@@ -1,19 +1,35 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import contacts from './contacts.json';
+
 
 class App extends Component {
+  
+  state = {
+    contactArr: contacts.slice(0,5)
+  }
+  
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div class="container">
+        <h1>IronContacts</h1>
+        <section>
+          <header>
+            <div class="col">Picture</div>
+            <div class="col">Name</div>
+            <div class="col">Popularity</div>
+          </header>
+          {this.state.contactArr.map(x => {
+          return(
+            <div class="row">
+              <div class="col"><img src={x.pictureUrl}></img></div>
+              <div class="col">         {x.name}             </div>
+              <div class="col">         {Number(x.popularity.toFixed(2))}       </div>
+            </div>
+          )})}
+        </section>
       </div>
+    
     );
   }
 }
