0aacf1728e796e5b4ba2390c03d01c3cebb7b9e1
Javier Varela <jvarela@strands.com>
2020-04-14 14:49:36 +0200

    iteration 2 random characters

--- src/App.js
+++ src/App.js
@@ -5,8 +5,21 @@ import contacts from './contacts.json';
 
 class App extends Component {
   
+  state = { 
+    contacts: contacts.slice(0, 5)
+  };
+
+  addRandomContact = () => {
+    const contactsCopy = [...this.state.contacts]
+    const remainingContacts = contacts.slice(this.state.contacts.length, contacts.length);
+    const randomContact = remainingContacts[Math.floor(Math.random() * remainingContacts.length)];
+    contactsCopy.push(randomContact);
+    this.setState({
+      contacts: contactsCopy
+    });
+  }
+
   render() {
-    const firstFiveContacts = contacts.slice(0, 5);
     return (
       <div className="App">
         <header className="App-header">
@@ -16,21 +29,26 @@ class App extends Component {
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
+        <button onClick={this.addRandomContact}>Add Random Contact</button>
         <table>
-          <tr>
-            <th>Picture</th>
-            <th>Name</th>
-            <th>Popularity</th>
-          </tr>
-          {
-          firstFiveContacts.map(contact => {
-              return (<tr key={contact.id}>
-                <td><img src={contact.pictureUrl} alt={contact.name}/></td>
-                <td>{contact.name}</td>
-                <td>{contact.popularity}</td>
-              </tr>)
-          })
-          }
+          <thead>
+            <tr>
+              <th>Picture</th>
+              <th>Name</th>
+              <th>Popularity</th>
+            </tr>
+          </thead>
+          <tbody>
+            {
+            this.state.contacts.map(contact => {
+                return (<tr key={contact.id}>
+                  <td><img src={contact.pictureUrl} alt={contact.name}/></td>
+                  <td>{contact.name}</td>
+                  <td>{contact.popularity}</td>
+                </tr>)
+            })
+            }
+          </tbody>
         </table>
       </div>
     );
