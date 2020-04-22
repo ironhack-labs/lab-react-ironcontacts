5c2afa82bbaf4e1b9d1c4c336b176a9f56c5f250
unknown <surge0116@gmail.com>
2020-04-22 10:47:31 -0400

    'done'

--- starter-code/src/App.js
+++ starter-code/src/App.js
@@ -21,55 +21,53 @@ class App extends Component {
         <td>{eachContact.popularity}</td>
         <td><button onClick={()=>this.deleteContact(index)}> Delete </button></td>
       </tr>
-      )
+       )
+     })
+   }
 
-    })
-    
-  }
-
-  deleteContact = (index) => {
-    let contactsShowingCopy = [...this.state.contactsShowing]
-    contactsShowingCopy.splice(index,1)
-    this.setState({constactsShowing:contactsShowingCopy})
-  }
-  
-  
-addRandoContact = () => {
-  if(this.state.remainingContacts.length>-1){
-  let randoNum = Math.floor(Math.random()*this.state.remainingContacts.length)
-  let randomContact = this.state.remainingContacts[randoNum]
-
-  let contactsShowingCopy = [...this.state.contactsShowing]
-  contactsShowingCopy.push(randomContact)
-
-  let remainingContactsCopy = [...this.state.remainingContacts]
-  remainingContactsCopy.splice(randoNum,1)
-
-  this.setState({
-    contactsShowing:contactsShowingCopy,
-    remainingContacts: remainingContactsCopy
-  })
-}}
-
-sortName = () => {
-  let contactsShowingCopySorted = [...this.state.contactsShowing].sort((a,b)=>a.name.localeCompare(b.name))
-  this.setState ({
-    contactsShowing:contactsShowingCopySorted
-  })
-}
-
-sortPop = () => {
-  let contactsShowingCopySorted = [...this.state.contactsShowing].sort((a,b)=>b.popularity-a.popularity)
-  this.setState ({
-    contactsShowing:contactsShowingCopySorted
-  })
-}
-
-
-
-  render() {
-    return (
-      <div className="App">
+   
+   
+   addRandoContact = () => {
+     if(this.state.remainingContacts.length>-1){
+       let randoNum = Math.floor(Math.random()*this.state.remainingContacts.length)
+       let randomContact = this.state.remainingContacts[randoNum]
+       
+       let contactsShowingCopy = [...this.state.contactsShowing]
+       contactsShowingCopy.push(randomContact)
+       
+       let remainingContactsCopy = [...this.state.remainingContacts]
+       remainingContactsCopy.splice(randoNum,1)
+       
+       this.setState({
+         contactsShowing:contactsShowingCopy,
+         remainingContacts: remainingContactsCopy
+        })
+      }}
+      
+      sortName = () => {
+        let contactsShowingCopySorted = [...this.state.contactsShowing].sort((a,b)=>a.name.localeCompare(b.name))
+        this.setState ({
+          contactsShowing:contactsShowingCopySorted
+        })
+      }
+      
+      sortPop = () => {
+        let contactsShowingCopySorted = [...this.state.contactsShowing].sort((a,b)=>b.popularity-a.popularity)
+        this.setState ({
+          contactsShowing:contactsShowingCopySorted
+        })
+      }
+      
+      
+      deleteContact = (index) => {
+        let contactsShowingCopy = [...this.state.contactsShowing]
+        contactsShowingCopy.splice(index,1)
+        this.setState({constactsShowing:contactsShowingCopy})
+      }
+      
+      render() {
+        return (
+          <div className="App">
       <h1>IronContacts</h1>
         <button onClick={this.addRandoContact}> Add Random Contact</button>
         <button onClick={this.sortName}> Sort By Name</button>
