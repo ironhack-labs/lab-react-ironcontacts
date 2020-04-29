2a2d5b5854ee9c23ac7d1a70e55e56a9cbccf47f
rickyalmeidadev <ricky.almeida.dev@gmail.com>
2020-04-29 17:30:02 -0300

    done

--- starter-code/src/App.js
+++ starter-code/src/App.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react'
-import logo from './logo.svg'
 import './App.css'
 import contacts from './contacts.json'
 import TableRow from './components/TableRow'
@@ -11,25 +10,80 @@ class App extends Component {
     this.state = {
       contactsArr: contacts.splice(0, 5),
     }
+
+    this.addRandom = this.addRandom.bind(this)
+    this.sortByName = this.sortByName.bind(this)
+    this.sortByPopularity = this.sortByPopularity.bind(this)
   }
+
+  addRandom() {
+    const randNum = Math.floor(Math.random() * contacts.length)
+    const contactsArrCopy = [...this.state.contactsArr]
+    contactsArrCopy.push(contacts[randNum])
+
+    this.setState({
+      contactsArr: contactsArrCopy,
+    })
+  }
+
+  sortByName() {
+    const contactsArrCopy = [...this.state.contactsArr]
+    contactsArrCopy.sort((a, b) => a.name.localeCompare(b.name))
+
+    this.setState({
+      contactsArr: contactsArrCopy,
+    })
+  }
+
+  sortByPopularity() {
+    const contactsArrCopy = [...this.state.contactsArr]
+    contactsArrCopy.sort((a, b) => b.popularity - a.popularity)
+
+    this.setState({
+      contactsArr: contactsArrCopy,
+    })
+  }
+
+  deleteContact(idx) {
+    console.log('test')
+    const contactsArrCopy = [...this.state.contactsArr]
+
+    contactsArrCopy.splice(idx, 1)
+
+    this.setState({
+      contactsArr: contactsArrCopy,
+    })
+  }
+
   render() {
-    console.log(this.state.contactsArr)
     return (
       <div className="App">
         <h1>IronContacts</h1>
-        <table>
-          <tr>
-            <th>Picture</th>
-            <th>Name</th>
-            <th>Popularity</th>
-          </tr>
-          {this.state.contactsArr.map(contact => (
-            <TableRow
-              picture={contact.pictureUrl}
-              name={contact.name}
-              popularity={contact.popularity}
-            />
-          ))}
+        <div>
+          <button onClick={this.addRandom}>Add random contact</button>
+          <button onClick={this.sortByName}>Sort by name</button>
+          <button onClick={this.sortByPopularity}>Sort by popularity</button>
+        </div>
+        <table className="table-container">
+          <thead>
+            <tr>
+              <th>Picture</th>
+              <th>Name</th>
+              <th>Popularity</th>
+            </tr>
+          </thead>
+          <tbody>
+            {this.state.contactsArr.map((contact, idx) => (
+              <TableRow
+                key={idx}
+                idx={idx}
+                picture={contact.pictureUrl}
+                name={contact.name}
+                popularity={contact.popularity}
+                deleteContact={() => this.deleteContact(idx)}
+              />
+            ))}
+          </tbody>
         </table>
       </div>
     )
--- starter-code/src/components/TableRow.js
+++ starter-code/src/components/TableRow.js
@@ -1,13 +1,20 @@
 import React from 'react'
+import '../App.css'
 
-const TableRow = ({ picture, name, popularity }) => (
-  <tr>
-    <td>
-      <img src={picture} alt={name} style={{ height: '200px' }} />
-    </td>
-    <td>{name}</td>
-    <td>{popularity}</td>
-  </tr>
-)
+const TableRow = ({ picture, name, popularity, deleteContact, idx }) => {
+  const even = { background: '#f3f3f3' }
+  const odd = { background: '#fff' }
+  return (
+    <tr style={idx % 2 === 0 ? even : odd}>
+      <td>
+        <img src={picture} alt={name} style={{ height: '200px' }} />
+      </td>
+      <td>{name}</td>
+      <td>{popularity.toFixed(2)}</td>
+      <td><button style={{background: 'tomato'}} onClick={deleteContact}>Delete</button></td>
+    </tr>
+  )
+
+} 
 
 export default TableRow
