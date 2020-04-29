d876a5cdc622f9c7f7d7b4bec34f0c5db764bef8
Anastasiia Kozlova <anastasiia.sh.v@gmail.com>
2020-04-29 21:53:58 +0200

    done

--- starter-code/src/App.js
+++ starter-code/src/App.js
@@ -1,38 +1,64 @@
-import React, { Component } from 'react';
-import ContactComponent from './components/ContactComponent'
-import './App.css';
-import contacts from './contacts.json';
-
+import React, { Component } from "react";
+import ContactComponent from "./components/ContactComponent";
+import "./App.css";
+import contacts from "./contacts.json";
+import Box from "./components/Box"
 
 class App extends Component {
-
   state = {
-    items: []
+    items: [],
+  };
+  componentDidMount() {
+    this.setState({ items: contacts.slice(0, 5) });
   }
-  componentDidMount(){
-    
-    this.setState({items:contacts.slice(0,5)})
+ 
+  addRandomContact=()=>{
+    const newContact = contacts[Math.floor(Math.random()*contacts.length)]
+    const newArray = [...this.state.items, newContact]
+    //console.log(newArray)
+    this.setState({items: newArray})
   }
 
+  sortByName=()=>{
   
- 
-   
+   const sortedList = [...this.state.items].sort((a,b)=>{
+     
+      if(a.name<b.name){
+        return -1
+      }
+      if(a.name>b.name){
+        return 1
+      }
+      return 0
+    })
+    this.setState({items:sortedList})
+  }
+
+  sortByRate=()=>{
+    const sortedList = [...this.state.items].sort((a,b)=>{
+     return b.popularity-a.popularity
+       
+    })
+    this.setState({items:sortedList})
+  
+  }
+  deleteContact=(index)=>{
+    console.log({index});
+    const newItems=[...this.state.items]
+    newItems.splice(index, 1)
+    this.setState({items:newItems})
+  }
 
   render() {
-    
     return (
-      // <div className="App">
-      //    {this.state.contacts.map( (contact, index) =>  
-      //    <ContactComponent key={index}
-      //    name = {contact.name}
-      //    picture = {contact.pictureUrl}
-      //    popularity = {contact.popularity}/> )
-      //    }
-      //    </div>
-        <div className="App">
-            <ContactComponent contacts = {this.state.items}/> 
-        </div>
-
+      <div className="App">
+        <Box>
+          <button onClick={this.addRandomContact}>Add new contact</button>
+          <button onClick={this.sortByName}>Sort by name</button>
+          <button onClick={this.sortByRate}>Sort by popularity</button>
+        </Box>
+        <ContactComponent contacts={this.state.items} deleteContact={this.deleteContact}/>
+      </div>
     );
   }
 }
--- starter-code/src/components/ContactItem.jsx
+++ starter-code/src/components/ContactItem.jsx
@@ -7,6 +7,7 @@ export default function ContactItem(props) {
                 <td><img className="contact-image"src={props.picture} alt="contact"></img></td>
                 <td>{props.name}</td>
                 <td>{props.popularity}</td>
+                <td><button onClick={props.onDelete}> Delete</button></td>
             </tr> 
         
     )
