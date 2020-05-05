20f7e91b0d3d8bcee41080b05e851fc13e88b849
William Hermozo <william.hermozo@protonmail.com>
2020-05-05 16:56:53 +0200

    done

--- starter-code/src/App.js
+++ starter-code/src/App.js
@@ -1,19 +1,90 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import contacts from './contacts.json';
+
 
 class App extends Component {
+
+  state={
+    renderedContacts: contacts.splice(0,5),
+    data: contacts,
+  }
+
+  renderContacts = () => {
+    return this.state.renderedContacts.map((e,index)=>{
+      return (
+      <tr key={index}>
+        <td>
+          <img src={e.pictureUrl} alt='' height='100px' ></img>
+        </td>
+        <td>
+          {e.name}
+        </td>
+        <td>
+          {e.popularity.toFixed(2)}
+        </td>
+        <td>
+          <button onClick={()=>this.handleDelete(index)}>Delete</button>
+        </td>
+      </tr>
+      )
+    })
+  }
+
+  selectRandomContact = (contactList) => {
+    return contactList[Math.floor(Math.random() * contactList.length)]
+  }
+
+  handleRandom = () => {
+    const newRenderedContacts = [...this.state.renderedContacts]
+    const randomContact = this.selectRandomContact(this.state.data)
+    newRenderedContacts.push(randomContact)
+    this.setState({renderedContacts: newRenderedContacts})
+  }
+
+  handleSortName = () => {
+    const sortedArray = [...this.state.renderedContacts].sort((a,b)=>{
+      return a.name.localeCompare(b.name)
+    })
+    this.setState({renderedContacts: sortedArray})
+  }
+  
+  handleSortPopularity = () => {
+    const sortedArray = [...this.state.renderedContacts].sort((a,b)=>{
+      return b.popularity-a.popularity
+    })
+    this.setState({renderedContacts: sortedArray})
+  }
+
+  handleDelete = (index) => {
+    this.setState({renderedContacts: this.state.renderedContacts.filter((element,contactIndex) => {
+        return contactIndex !== index;
+      }),
+    });
+  }
+
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div className='main'>
+      <div className='cta-buttons'>
+        <button onClick={this.handleRandom}>Add random contact</button>
+        <button onClick={this.handleSortName}>Sort by name</button>
+        <button onClick={this.handleSortPopularity}>Sort by popularity</button>
       </div>
+      <table>
+        <thead>
+          <tr>
+            <th>Picture</th>
+            <th>Name</th>
+            <th>Popularity</th>
+            <th>Action</th>
+          </tr>
+        </thead>
+        <tbody>
+          {this.renderContacts()}
+        </tbody>
+      </table>
+    </div>
     );
   }
 }
