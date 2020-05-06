2f90f05de9d51c26b279c9c69f64c882d0817f84
Olha Danylchenko <danylchenkoolha@gmail.com>
2020-05-06 13:53:26 +0200

    wip

--- starter-code/src/App.js
+++ starter-code/src/App.js
@@ -1,19 +1,76 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import contactsFromJson from "./contacts.json";
+import Page from "./components/Page/Page";
 
 class App extends Component {
+  state = {
+    contacts: contactsFromJson.slice(0, 5),
+    sortedByName: false,
+    sortedByPopularity: false,
+  };
+
+  handleAdd = () => {
+    const contacts = [...this.state.contacts];
+    const restOfContacts = contactsFromJson.slice(5);
+    const randomMovie =
+      restOfContacts[Math.floor(Math.random() * restOfContacts.length)];
+    contacts.push(randomMovie);
+    this.setState({
+      contacts,
+    });
+  };
+
+  sortByName = () => {
+    // const contacts = [...this.state.contacts];
+    // const sorted = contacts.sort((a, b) => (a.name > b.name ? 1 : -1));
+    this.setState({
+      sortedByName: !this.state.sortedByName,
+      sortedByPopularity: false,
+    });
+  };
+
+  sortByPopularity = () => {
+    console.log(this.state.sortedByPopularity);
+    this.setState({
+      sortedByName: false,
+      sortedByPopularity: !this.state.sortedByPopularity,
+    });
+  };
+
   render() {
+    const displayContacts = [...this.state.contacts];
+    if (this.state.sortedByName) {
+      displayContacts.sort((a, b) => (a.name > b.name ? 1 : -1));
+    } else if (this.state.sortedByPopularity) {
+      displayContacts.sort((a, b) => a.popularity - b.popularity);
+    }
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+      <>
+        <h1>IronContacts</h1>
+        <div className="buttons">
+          <button
+            style={{ padding: "1em", marginBottom: "1em" }}
+            onClick={this.handleAdd}
+          >
+            Add random contact
+          </button>
+          <button
+            style={{ padding: "1em", marginBottom: "1em" }}
+            onClick={this.sortByName}
+          >
+            Sort by name
+          </button>
+          <button
+            style={{ padding: "1em", marginBottom: "1em" }}
+            onClick={this.sortByPopularity}
+          >
+            Sort by popularity
+          </button>
+        </div>
+        <Page contacts={displayContacts} />
+      </>
     );
   }
 }
---
+++ starter-code/src/components/Page/Page.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import "./Page.css";
+
+export default function Page(props) {
+  const contacts = props.contacts.map((contact) => {
+    return (
+      <li key={contact.id} className="contact">
+        <img src={contact.pictureUrl} alt="" height="100" />
+        <p>{contact.name}</p>
+        <p>{contact.popularity.toFixed(2)}</p>
+      </li>
+    );
+  });
+  return <ul>{contacts}</ul>;
+}
