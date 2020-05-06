1d90315078867bf5243fe13d0b4a88b70be2656e
Chantel Taki <chanteltaki@gmail.com>
2020-05-06 09:49:29 -0400

    iteration3

--- starter-code/src/App.js
+++ starter-code/src/App.js
@@ -10,6 +10,8 @@ class App extends Component {
       contacts: contacts.slice(0, 5)
     };
     this.addRandom = this.addRandom.bind(this);
+    this.sortByName = this.sortByName.bind(this);
+    this.sortByPopularity = this.sortByPopularity.bind(this);
   }
 
  addRandom() {
@@ -22,6 +24,15 @@ class App extends Component {
     });
   }
 
+ sortByName = () => {
+    this.state.contacts.sort((a, b) => a.name.localeCompare(b.name))
+    this.setState({})
+  }
+
+  sortByPopularity= () => {
+    this.state.contacts.sort((a, b) => b.popularity - a.popularity)
+    this.setState({})
+  }
 
 
 
@@ -31,6 +42,8 @@ class App extends Component {
       <div className="App">
           <h1 className="App-title">Iron Contacts</h1>
           <button onClick={this.addRandom}>Add Random Contact</button>
+          <button onClick={this.sortByName}>Sort By Name</button>
+          <button onClick={this.sortByPopularity}>Sort By Popularity</button>
           <div>
           {contacts.map((contact, index) => {
           return (
@@ -39,8 +52,8 @@ class App extends Component {
                 <th>
                   <img
                     src={contact.pictureUrl}
-                    className="Image-Logo"
-                    alt="logo"
+                    className="Image-pic"
+                    alt="pic"
                   />
                 </th>
 
