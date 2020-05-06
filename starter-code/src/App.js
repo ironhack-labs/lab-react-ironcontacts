13c663438fd6654b409a2ce38cbb0d2e30878403
Emma-Solano <solano.c.emmanuel@gmail.com>
2020-05-05 19:35:59 -0500

    se añade botón y función de addRandom

--- starter-code/src/App.js
+++ starter-code/src/App.js
@@ -1,16 +1,23 @@
 import React, { Component } from 'react'
-import logo from './logo.svg'
 import './App.css'
 import contacts from './contacts.json'
 import IdCard from './components/IdCard'
+import Button from './components/Button'
 
 class App extends Component {
   state = {
     contactsArray: contacts.slice(0, 5),
   }
+  addRandom(){
+    const randomContact = contacts[Math.floor(Math.random()*(contacts.length - 5) - 4)]
+    this.setState({contactsArray: this.state.contactsArray.push(randomContact)})
+  }
   render() {
     return (
       <div>
+      <h1>IronContacts</h1>
+      <Button addRandom={this.state.addRandom}/>
+      <p></p>
         {this.state.contactsArray.map((contact) => (
           <IdCard
             name={contact.name}
---
+++ starter-code/src/components/Button/index.js
@@ -0,0 +1,5 @@
+import React from 'react'
+
+const Button = ({addRandom})=> <button onClick={addRandom}>Add a random contact!</button>
+
+export default Button
