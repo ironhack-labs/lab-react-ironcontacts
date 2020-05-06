3970c4a7465b0a3c46ce1f02381d22d74809954b
Karl Secen <karl.secen@gmail.com>
2020-05-06 13:02:32 +0200

    iteration 3

--- starter-code/src/App.js
+++ starter-code/src/App.js
@@ -6,24 +6,58 @@ import "./App.css";
 const contactList = contacts.splice(0, 5);
 
 class App extends Component {
-  
   state = {
     contact: contactList,
   };
 
+  addRandom = () => {
+    let min = 0;
+    let max = Math.floor(contacts.length);
+
+    let randNum = Math.floor(Math.random() * (max - min + 1)) + min;
+
+    let randContact = () => {
+      //Logic needed to avoid duplicates..
+      return contacts[randNum];
+    };
+
+    this.setState({
+      contact: this.state.contact.concat(randContact()),
+    });
+  };
+
+  sortName = () => {
+    const sorted = this.state.contact.sort((a, b) => {
+      if (a.name < b.name) {
+        return -1;
+      }
+      if (a.name > b.name) {
+        return 1;
+      }
+      return 0;
+    });
+
+    this.setState({
+      contact: sorted,
+    });
+  };
+
   render() {
     const contactItems = this.state.contact.map((contact) => (
-      <tr>
-        <td><img src={contact.pictureUrl} alt=""/></td>
-        <td key={contact.id}>{contact.name}</td>
-        <td key={contact.id}>{contact.popularity}</td>
+      <tr key={contact.id}>
+        <td>
+          <img src={contact.pictureUrl} alt="" />
+        </td>
+        <td>{contact.name}</td>
+        <td>{contact.popularity}</td>
       </tr>
     ));
-
 
     return (
       <div className="contactList">
         <h1>IronContacts</h1>
+        <button onClick={this.addRandom}>Add Random Contact</button>
+        <button onClick={this.sortName}>Sort by name</button>
         <table>
           <thead>
             <tr>
@@ -32,9 +66,7 @@ class App extends Component {
               <th>Popularity</th>
             </tr>
           </thead>
-          <tbody>
-            {contactItems}
-          </tbody>
+          <tbody>{contactItems}</tbody>
         </table>
       </div>
     );
