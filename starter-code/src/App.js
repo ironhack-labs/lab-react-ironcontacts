22d64797b4fe6401742b8eeeb33a9f62f3dd99e5
Phillip Paul <phillippaul@gmx.de>
2020-05-07 20:19:33 +0200

    finished w/o styling

--- starter-code/src/App.js
+++ starter-code/src/App.js
@@ -1,8 +1,54 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+
+// Iteration 1:
+import contacts from "./contacts.json";
+
+// note on button onClick -> NO parenthese or the function will call itself until app crash
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      contactList: contacts.slice(0, 5), //<p>{this.state.contacts[0].name}</p> is possible
+    };
+  }
+
+  addContact = () => {
+    this.setState({
+      contactList: [
+        ...this.state.contactList,
+        contacts[Math.floor(Math.random() * contacts.length - 1)],
+      ],
+    });
+  };
+
+  sortByName = () => {
+    let sortedContactsByName = [...this.state.contactList];
+    sortedContactsByName.sort((a, b) => a.name.localeCompare(b.name));
+    this.setState({
+      contactList: sortedContactsByName,
+    });
+  };
+
+  sortByPop = () => {
+    let sortedContactsByPop = [...this.state.contactList];
+    sortedContactsByPop.sort((a, b) => a.popularity - b.popularity);
+    this.setState({
+      contactList: sortedContactsByPop,
+    });
+  };
+
+  deleteContact = (event) => {
+    let lessContacts = this.state.contactList.filter(
+      (item) => item.id !== event.target.value
+    );
+    this.setState({
+      contactList: lessContacts,
+    });
+  };
+
   render() {
     return (
       <div className="App">
@@ -10,9 +56,39 @@ class App extends Component {
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Welcome to React</h1>
         </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <h2>IronContacts</h2>
+        <div className="buttons">
+          <button onClick={this.addContact}>Add random contact</button>
+          <button onClick={this.sortByName}>Sort by name</button>
+          <button onClick={this.sortByPop}>Sort by popularity</button>
+        </div>
+        <table className="contacts-table">
+          <thead>
+            <tr>
+              <td>Picture</td>
+              <td>Name</td>
+              <td>Popularity</td>
+            </tr>
+          </thead>
+          <tbody>
+            {this.state.contactList.map((item, key) => {
+              return (
+                <tr key={key}>
+                  <td>
+                    <img src={item.pictureUrl} alt="" />
+                  </td>
+                  <td>{item.name}</td>
+                  <td>{item.popularity.toFixed(2)}</td>
+                  <td>
+                    <button onClick={this.deleteContact} value={item.id}>
+                      Delete
+                    </button>
+                  </td>
+                </tr>
+              );
+            })}
+          </tbody>
+        </table>
       </div>
     );
   }
