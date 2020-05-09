4c0cbac7f0f0e12432339cfab36dd5ede9b641e2
ingridpitta <ingridpitta056@gmail.com>
2020-05-09 12:30:33 -0300

    still have to add styles

--- src/App.js
+++ src/App.js
@@ -1,26 +1,114 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
+import contacts from './contacts.json';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      contactsList: []
+    };
+  }
+
+  componentWillMount = () => {
+    this.setState({ contactsList: contacts.slice(0, 5) });
+  };
+
+  populateTable = () => {
+    const { contactsList } = this.state;
+
+    return contactsList.map((actor, index) => {
+      return (
+        <div className="table--row" key={`actor-${index + 1}`}>
+          <img src={actor.pictureUrl} alt={`${actor.name}-img`} />
+          <h4>{actor.name}</h4>
+          <h4>{actor.popularity}</h4>
+          <button
+            id={index}
+            className="btn--delete"
+            onClick={this.deleteContact}
+          >
+            Delete
+          </button>
+        </div>
+      );
+    });
+  };
+
+  addRandomContact = () => {
+    const { contactsList } = this.state;
+
+    contactsList.length < contacts.length &&
+      this.setState({
+        contactsList: [
+          ...contactsList,
+          contacts[
+            parseInt(
+              contactsList.length +
+                Math.random() * (contacts.length - 1 - contactsList.length)
+            )
+          ]
+        ]
+      });
+  };
+
+  sortContactByName = () => {
+    const { contactsList } = this.state;
+
+    const sortedList = contactsList.sort((a, b) =>
+      a.name.localeCompare(b.name)
+    );
+
+    this.setState({ contactsList: sortedList });
+  };
+
+  sortContactByPopularity = () => {
+    const { contactsList } = this.state;
+
+    const sortedList = contactsList.sort((a, b) => b.popularity - a.popularity);
+
+    this.setState({ contactsList: sortedList });
+  };
+
+  deleteContact = e => {
+    const { contactsList } = this.state;
+    const index = e.target.id;
+
+    contactsList.splice(index, 1);
+
+    this.setState({ contactsList });
+  };
+
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <h2>IronContacts</h2>
+        </header>
+        <section className="App-content--container">
+          <div className="btns--table">
+            <button className="btn" onClick={this.addRandomContact}>
+              Add Random Contact
+            </button>
+            <button className="btn" onClick={this.sortContactByName}>
+              Sort by name
+            </button>
+            <button className="btn" onClick={this.sortContactByPopularity}>
+              Sort by popularity
+            </button>
+          </div>
+          <div className="content--table">
+            <h3>Picture</h3>
+            <h3>Name</h3>
+            <h3>Popularity</h3>
+            <h3>Action</h3>
+            {this.populateTable()}
+          </div>
+        </section>
+      </div>
+    );
+  }
 }
 
 export default App;
