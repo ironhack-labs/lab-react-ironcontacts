309b5eb164c384ef14d9b970edd20d0e55c61c6a
YuvalBu <Yuval.Bu@gmail.com>
2020-05-11 11:04:19 +0200

    Add New Random Contacts

--- starter-code/src/App.js
+++ starter-code/src/App.js
@@ -1,21 +1,75 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import contacts from './contacts.json';
+
+// import logo from './logo.svg';
 import './App.css';
 
+function getRandomInt(max) {
+	return Math.floor(Math.random() * Math.floor(max));
+}
+
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
-  }
+	state = {
+		contactsDisplayed: contacts.slice(0, 5)
+	};
+
+	addRandom = () => {
+		let randomCelebrity = contacts[getRandomInt(contacts.length)];
+		this.setState({
+			contactsDisplayed: [ ...this.state.contactsDisplayed, randomCelebrity ]
+		});
+	};
+
+	sortByName = () => {
+		this.setState({
+			contactsDisplayed: [ ...this.state.contactsDisplayed ].sort((a, b) => {
+				return a.name.localeCompare(b.name);
+			})
+		});
+	};
+
+	sortByPopularity = () => {
+		this.setState({
+			contactsDisplayed: [ ...this.state.contactsDisplayed ].sort((a, b) => {
+				return a.popularity - b.popularity;
+			})
+		});
+	};
+
+	render() {
+		return (
+			<div className="App">
+				<h1>Ironhack Contacts</h1>
+				<div className="btn">
+					<button onClick={this.addRandom}>Add random contact</button>
+					<button onClick={this.sortByName}>Sort by name</button>
+					<button onClick={this.sortByPopularity}>Sort by popularity</button>
+				</div>
+				<div className="ironhackTable">
+					<table>
+						<thead>
+							<tr>
+								<th>Picture</th>
+								<th>Name</th>
+								<th>Popularity</th>
+							</tr>
+						</thead>
+						<tbody>
+							{this.state.contactsDisplayed.map((oneCelebrity, index) => (
+								<tr key={index}>
+									<td>
+										<img src={oneCelebrity.pictureUrl} alt="" />
+									</td>
+									<td>{oneCelebrity.name}</td>
+									<td>{oneCelebrity.popularity.toFixed(2)}</td>
+								</tr>
+							))}
+						</tbody>
+					</table>
+				</div>
+			</div>
+		);
+	}
 }
 
 export default App;
