b8d2702950d344146c84394b0d649f32fe851656
m.13ferret@gmail.com <m.13ferret@gmail.com>
2020-05-11 14:49:22 +0200

    incomplete

--- starter-code/src/App.js
+++ starter-code/src/App.js
@@ -1,8 +1,16 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import contacts from "./contacts.json";
+// import Contact from "./components/Contacts";
 
 class App extends Component {
+  constructor() {
+
+  state = {
+    contacts: contacts.slice(0,5)
+  };
+
   render() {
     return (
       <div className="App">
@@ -13,6 +21,9 @@ class App extends Component {
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
+
+        <div className="Card Table"
+          {this.state.contacts.map((co))}
       </div>
     );
   }
---
+++ starter-code/src/components/Card.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+function Card(props) {
+    return (
+        <tr>
+            <td>
+                <img src={props.pictureUrl} alt=""/>
+            </td>
+            <td>{props.name}</td>
+            <td>{props.popularity}</td>
+        </tr>
+    );
+}
+
+export default Card;
