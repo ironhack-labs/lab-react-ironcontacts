3d43f25ef689186a537e490fc0626729fd154ff2
Andre R <andre.rabaca94@gmail.com>
2020-06-01 22:16:07 +0100

    Added done

--- src/App.js
+++ src/App.js
@@ -1,26 +1,57 @@
-import React from 'react';
+import React, { Component, useDebugValue } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import contactsFromJSOM from './contacts.json';
 
-function App() {
-  return (
+
+class App extends Component {
+  state ={
+    firstVisibleContacts: contactsFromJSOM.slice(0,5)
+  }
+  addRandomContact (pictureUrl,name,popularity,id){
+    const addedArray = this.state.firstVisibleContacts
+    addedArray.push({
+      "name": name,
+      "pictureUrl": pictureUrl,
+      "popularity": popularity,
+      "id": id
+    })
+    this.setState({
+      firstVisibleContacts : addedArray
+    }
+    )
+  }
+
+  render (){
+    return(
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <table>
+        <th>
+          Picture
+        </th>
+        <th>
+          Name
+        </th>
+        <th>
+          Popularity
+        </th>
+          {this.state.firstVisibleContacts.map(contact =>{
+            return (
+            <tr>
+              <td><img src={contact.pictureUrl} height="200px"></img></td>
+              <td>{contact.name}</td>
+              <td>{contact.popularity}</td>
+            </tr> )
+          })}
+      </table>
+      <button onClick={()=>this.addRandomContact(
+        "https://image.tmdb.org/t/p/w500/kbWValANhZI8rbWZXximXuMN4UN.jpg",
+        "Margot Robbie",
+        11,
+        "cebolas"
+        )}>Add</button>
     </div>
-  );
+  )};
 }
 
 export default App;
