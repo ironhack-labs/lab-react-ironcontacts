87541cfe1d42e0bda9cfc2f4001bd5831ff27405
LucyJunior <53796420+LucyJunior@users.noreply.github.com>
2020-06-17 10:08:08 +0200

    lab

--- src/App.js
+++ src/App.js
@@ -1,26 +1,86 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component} from 'react';
+//import logo from './logo.svg';
 import './App.css';
+import contacts from './contacts.json';
+import Table from './components/Table';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+
+  state = {
+    celebrity: contacts,
+    chosenCelebs: contacts.slice(0, 5)
+  }
+
+  //adding random celeb to contact list:
+  addCelebrity = () => {
+    const randomCeleb = this.state.celebrity[Math.floor(Math.random() * this.state.celebrity.length)]
+    this.setState({
+      chosenCelebs: this.state.chosenCelebs.concat([randomCeleb])
+    })
+  }
+
+  // sorting list by name (ascending):
+  sortByName = () => {
+    this.setState({
+      // chosenCelebs: this.state.chosenCelebs.sort((a, b) => a.name.localeCompare(b.name))
+      chosenCelebs: this.state.chosenCelebs.sort((a, b) => {
+        if(a.name < b.name){
+        return -1 
+      } else {
+        return 1
+      }
+      })
+
+    })
+  }
+
+  // sorting list by popularity (ascending):
+  sortByPopularity = () => {
+    this.setState({
+      chosenCelebs: this.state.chosenCelebs.sort((a, b) => {
+        return a.popularity - b.popularity
+      })
+    })
+  }
+
+  deleteCeleb = (name) => {
+    let clonedCelebs = JSON.parse(JSON.stringify(this.state.chosenCelebs))
+    let index = clonedCelebs.findIndex((celeb) => {
+      return celeb.name === name;
+    })
+    clonedCelebs.splice(index, 1)
+
+    this.setState({
+      chosenCelebs: clonedCelebs
+    })
+  }
+
+  render() {
+    return (
+      <div>
+        <h1>IronContacts</h1>
+        <button onClick={this.addCelebrity}>Add Random Contact</button>
+        <button onClick={this.sortByName}>Sort by name</button>
+        <button onClick={this.sortByPopularity}>Sort by popularity</button>
+        <table>
+          <tr>
+            <th>Picture</th>
+            <th>Name</th>
+            <th>Popularity</th>
+          </tr>
+        </table>
+        {this.state.chosenCelebs.map((celebrity, index) => {
+          return <Table
+            key={index}
+            pictureUrl={celebrity.pictureUrl}
+            name={celebrity.name}
+            popularity={celebrity.popularity}
+            delete={this.deleteCeleb}
+          />
+        })}
+      </div>
+    )
+  }
 }
 
 export default App;
