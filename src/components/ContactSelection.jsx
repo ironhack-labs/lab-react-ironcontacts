d0f4961fd181ab71046beadd211b4775703fbc95
Aida <aida.loemba@outlook.com>
2020-06-18 22:27:34 +0200

    added sort function

--- src/components/ContactSelection.jsx
+++ src/components/ContactSelection.jsx
@@ -4,7 +4,6 @@ import contacts from '../data/contacts';
 import Contact from './Contact';
 
 export default class Contacts extends Component {
-
     state = {
         contacts: contacts.slice(0, 5)
     }
@@ -16,31 +15,69 @@ export default class Contacts extends Component {
         newSelection.push(randomContact);
     
         this.setState({
-          contacts: newSelection,
+          contacts: newSelection
         });
     }
+
+    sortName = () => {
+        let sortedByName = [...this.state.contacts].sort(function (a, b) {
+            if (a.name > b.name) {
+                return 1;
+            }
+            if (b.name > a.name) {
+                return -1;
+            }
+            return 0;
+        });
+        
+        this.setState({
+          newSelection: sortedByName
+        });
+    }
+
+    sortPopularity = () => {
+        let sortedByPopularity = [...this.state.contacts].sort(function (a, b) {
+            if (a.popularity > b.popularity) {
+                return 1;
+            }
+            if (b.popularity > a.popularity) {
+                return -1;
+            }
+            return 0;
+        });
+        
+        this.setState({
+          newSelection: sortedByPopularity
+        });
+    }
+
+    
     
     render() {
         return (
             <div className="selection">
                 <button onClick={this.addRandom}>Add random contact</button>
+                <button onClick={this.sortName}>Sort by name</button>
+                <button onClick={this.sortPopularity}>Sort by popularity</button>
                 <table>
                     <th>
                         <tr>
-                        <td><h1>Photo</h1></td>
-                        <td><h1>Name</h1></td>
-                        <td><h1>Popularity</h1></td>
+                            <td><h1>Photo</h1></td>
+                            <td><h1>Name</h1></td>
+                            <td><h1>Popularity</h1></td>
                         </tr>
                     </th>
                     <tbody>
-                        {this.state.contacts.map((contact, index) => (
-                            <Contact
-                                key={`${index}-${contact.name}`}
-                                name={contact.name}
-                                pictureUrl={contact.pictureUrl}
-                                popularity={contact.popularity}
-                            />
-                        ))}
+                        {
+                            this.state.contacts.map((contact, index) => (
+                                <Contact
+                                    key={`${index}-${contact.name}`}
+                                    pictureUrl={contact.pictureUrl}
+                                    name={contact.name}
+                                    popularity={contact.popularity}
+                                />
+                            ))
+                        }
                     </tbody>
                 </table>
             </div>
