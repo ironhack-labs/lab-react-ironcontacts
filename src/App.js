53955d3a0e0a7cefb15fc46e148d2bb498819251
Aida <aida.loemba@outlook.com>
2020-06-18 20:51:21 +0200

    removed ActionButtons.jsx - addRamdom picking from the array of 5 and not contacts.json

--- src/App.js
+++ src/App.js
@@ -1,16 +1,13 @@
 import React, { Component } from 'react';
 import './App.css';
 import ContactSelection from './components/ContactSelection'
-import AddRandomBtn from './components/AddRandomBtn';
 
 export default class App extends Component {
-
   render() {
     return (
       <div className="App">
         <h1>A list</h1>
-        <AddRandomBtn />
-        <ContactSelection />
+        <ContactSelection/>
       </div>
     )
   }
--- src/components/ContactSelection.jsx
+++ src/components/ContactSelection.jsx
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import './ContactSelection.css';
 import contacts from '../data/contacts';
 import Contact from './Contact';
 
@@ -8,9 +9,21 @@ export default class Contacts extends Component {
         contacts: contacts.slice(0, 5)
     }
 
+    addRandom = () => {
+        let randomize = Math.floor(Math.random() * this.state.contacts.length);
+        let randomContact = this.state.contacts[randomize];
+        let newSelection = [...this.state.contacts];
+        newSelection.push(randomContact);
+    
+        this.setState({
+          contacts: newSelection,
+        });
+    }
+    
     render() {
         return (
             <div className="selection">
+                <button onClick={this.addRandom}>Add random contact</button>
                 <table>
                     <th>
                         <tr>
