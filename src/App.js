0734659d8f5ba615426a2048a20602e43635e867
Aurore CR <aurorenaturerouge@hotmail.fr>
2020-07-03 15:00:31 +0200

    last

--- src/App.js
+++ src/App.js
@@ -1,6 +1,9 @@
-import React from 'react';
+import React,{Component} from 'react';
+import contacts from './contacts.json';
+
 import logo from './logo.svg';
 import './App.css';
+
 
 function App() {
   return (
@@ -22,5 +25,46 @@ function App() {
     </div>
   );
 }
+
+class App extends Component{
+  state = {
+
+    celeb: contacts,
+
+    newceleb: contacts.slice(0,5)
+  }
+  addContact = () => {
+    let storedCeleb = this.state.celeb[
+      Math.floor(Math.random() * this.state.celeb.length)
+    ];
+    this.setState({
+      newCeleb: this.state.newCeleb.concat([storedCeleb]),
+    });
+  };
+
+  sortName = () => {
+    this.setState({
+      newCeleb: this.state.newCeleb.sort((a, b) => {
+        return a.name > b.name ? -1 : 1;
+      })
+     }
+    )}
+    sortByPopularity = () => {
+      this.setState({
+        chosenCeleb: this.state.chosenCeleb.sort((a, b) => {
+          return a.popularity - b.popularity
+        })
+      })
+    }
+    deleteCeleb = (name) => {
+      let deleteCeleb = [...this.state.newCeleb];
+      let index = deleteCeleb.findIndex((celebrity) => {
+        return celebrity.name === name;
+      });
+    }
+  }
+      
+     
+  
 
 export default App;
