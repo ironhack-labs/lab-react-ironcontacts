fea7fdfdc65540b239c1fa437343b7b98a2d4c70
Marcela Vilas Boas <marcela.vilasboas@gmail.com>
2020-07-12 12:25:28 +0200

    iteration 3

--- src/components/Table.js
+++ src/components/Table.js
@@ -7,9 +7,11 @@ class Table extends Component {
         super(props)
         this.state = {
             allContacts: [...contacts].splice(5),
-            fiveContacts: [...contacts].splice(0,5)
+            fiveContacts: [...contacts].splice(0, 5)
         }
-        this.randomContact = this.randomContact.bind(this)//El valor de this es ignorado cuando la función es llamada con el operador new.
+        this.randomContact = this.randomContact.bind(this)
+        this.sortByName = this.sortByName.bind(this)
+        this.sortByPop = this.sortByPop.bind(this)//El valor de this es ignorado cuando la función es llamada con el operador new.
 
     }
 
@@ -27,39 +29,68 @@ class Table extends Component {
         let index = Math.floor(Math.random() * this.state.allContacts.length);
         let copyFiveContacts = [...this.state.fiveContacts]
         let copyAllContacts = [...this.state.allContacts]
-        copyFiveContacts.push(copyAllContacts[index])
+        const elemToPush = copyAllContacts[index]
+        copyFiveContacts.push(elemToPush)
+        copyAllContacts.splice(index, 1)
+        console.log(index)
+        console.log(copyAllContacts.splice(index, 1))
+        console.log(elemToPush)
+
 
         this.setState({
             allContacts: copyAllContacts,
             fiveContacts: copyFiveContacts
         })
+    }
 
+    sortByName() {
+        let copyToSort = [...this.state.fiveContacts];
+        copyToSort.sort((a, b) => {
+            if(a.name > b.name){
+                return 1;
+            } else if(a.name < b.name){
+                return -1;
+            } else {
+                return 0
+            }
+        });
+        this.setState({
+            fiveContacts: copyToSort,
+        })
+    }
 
+    sortByPop() {
+        let copySortByPop = [...this.state.fiveContacts];
+        copySortByPop.sort((a, b) => a.popularity-b.popularity)
+
+        this.setState({
+            fiveContacts: copySortByPop,
+        })
 
     }
 
     render() {
-        
-        console.log(this.state.allContacts)
-        console.log(this.state.fiveContacts)
-
         return (
-            <div className="tableDiv">
-            <Button
-                addContact={this.randomContact}
-            />
-                <table>
-                    <thead>
-                        <tr>
-                            <th><h2>Picture</h2></th>
-                            <th><h2>Name</h2></th>
-                            <th><h2>Popularity</h2></th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        {this.contactsResult()}
-                    </tbody>
-                </table>
+            <div className="main">
+                <div className="buttonsDiv">
+                    <Button actionBut={this.randomContact} text="Add a random contact"/>
+                    <Button actionBut={this.sortByName} text="Sort by name"/>
+                    <Button actionBut={this.sortByPop} text="Sort by popularity"/>
+                </div>
+                <div className="tableDiv">
+                    <table>
+                        <thead>
+                            <tr>
+                                <th><h2>Picture</h2></th>
+                                <th><h2>Name</h2></th>
+                                <th><h2>Popularity</h2></th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            {this.contactsResult()}
+                        </tbody>
+                    </table>
+                </div>
             </div>
         )
     }
