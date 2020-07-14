f485bd49e53dfd09d79236ac62f6e0f10e498089
Rodrigo <user@Rodrigos-MBP.home>
2020-07-14 21:15:25 +0200

    done

---
+++ src/Components/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+
+import Contacts from './Contacts/Contacts'
+function App() {
+  return (
+    <main>
+
+     
+      <Contacts />
+    </main>
+  );
+}
+
+export default App;
