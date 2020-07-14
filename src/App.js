e0b023db347e10fbc2b71ce74fc4fd0944fd71ef
Míriam Sánchez Alcón <msanchezalcon.msa@gmail.com>
2020-07-14 18:08:46 +0200

    add random

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import React from 'react'
 import './App.css'
 import ContactsList from './components/Contacts/ContactsList'
-import ContactsCard from './components/Contacts/ContactsCard'
 
 
 function App() {
   return (
     <>
       <h1>IronContacts</h1>
+      {/* ¿Cómo se llamaría al botón desde aquí? */}
+      {/* <button onClick={addRandom}>Add random contact</button> */}
+
       <table className="contact-row">
         <thead>
           <tr>
--- src/components/Contacts/ContactsList.js
+++ src/components/Contacts/ContactsList.js
@@ -12,22 +12,38 @@ class ContactsList extends Component {
         }
     }
 
-    deleteContact = id => {
-        const contactsCopy = [...this.state.contactsList]
-        contactsCopy.splice(id, 1)
+
+
+    addRandom = () => {
+        let ramdomPick = Math.round(Math.random() * (contacts.length - 1))
+        let randomContact = contacts[ramdomPick] //from original contacts array
+        let contactsCopy = [...this.state.contactsList] //copy from spliced arr
+        contactsCopy.push(randomContact) //adds random pick from original array into spliced arr
         this.setState({ contactsList: contactsCopy })
     }
+
+
+
+    deleteContact = id => {
+        const contactsCopy = [...this.state.contactsList]
+        contactsCopy.splice(id, 1) //exactly one occurrence
+        this.setState({ contactsList: contactsCopy }) //always set.State after a modification
+    }
+
+
 
     render() {
 
         return (
             <>
+                <button onClick={this.addRandom}>Add random contact</button>
                 {this.state.contactsList.map(elm => <ContactsCard key={elm.id} name={elm.name} pictureUrl={elm.pictureUrl} popularity={elm.popularity} removeContact={this.deleteContact} />)}
-
 
             </>
         )
     }
 }
+
+
 
 export default ContactsList
