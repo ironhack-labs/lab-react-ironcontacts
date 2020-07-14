291c80fb0e5cea4f5021488ab5d7852a96aaec86
Míriam Sánchez Alcón <msanchezalcon.msa@gmail.com>
2020-07-14 14:08:57 +0200

    created project

---
+++ src/components/App.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import logo from './logo.svg'
+import './App.css'
+import ContactsList from './Contacts/ContactsList'
+import ContactsTable from './Contacts/ContactsTable'
+
+
+function App() {
+  return (
+    <>
+      <h1>
+        IronContacts
+      </h1>
+
+    </>
+  )
+}
+
+export default App
