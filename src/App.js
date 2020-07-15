67b960aac602729ae23e270e935ac2a7240dd88f
SiEr0212 <simonertel@gmx.de>
2020-07-15 13:05:40 +0200

    Iteration 1 and 2 done

--- src/App.js
+++ src/App.js
@@ -4,29 +4,25 @@ import contacts from './contacts.json';
 import ContactsList from './components/ContactsList';
 
 const contactsData = contacts.slice(0, 5);
-  
 
 class App extends Component {
   state = {
     contacts: contactsData,
   };
- addContact = () => {
-   const newRandomContact = contacts[Math.floor(Math.random() * contacts.length)];
-   this.setState((state, props) => ({
-    contacts: state.contacts.concat(newRandomContact)
-   }))
- }
+  addContact = () => {
+    const newRandomContact =
+      contacts[Math.floor(Math.random() * contacts.length)];
+    this.setState((state, props) => ({
+      contacts: state.contacts.concat(newRandomContact),
+    }));
+  };
   render() {
     return (
       <div className="App">
         <h1>IronContacts</h1>
         <button onClick={this.addContact}>Add </button>
         <ContactsList contacts={this.state.contacts} />
-        
-        </div>
-
-        
-      
+      </div>
     );
   }
 }
--- src/components/ContactsList.js
+++ src/components/ContactsList.js
@@ -1,28 +1,25 @@
 import React from 'react';
 
-
 export default function ContactsList(props) {
-    const contacts = props.contacts.map(contact => {
-
-    
+  const contacts = props.contacts.map((contact) => {
     return (
-        <div >
+      <div>
         <table>
-        <tr>
+          <tr>
             <th>Picture</th>
             <th>Name</th>
             <th>Popularity</th>
-                       
-        </tr>
-        <tr key={contact.id}>
-          <td><img src={contact.pictureUrl} alt= {contact.name}></img></td>
-          <td>{contact.name}</td>
-          <td>{contact.popularity}</td>
-        </tr>
-          
+          </tr>
+          <tr key={contact.id}>
+            <td>
+              <img src={contact.pictureUrl} alt={contact.name}></img>
+            </td>
+            <td>{contact.name}</td>
+            <td>{contact.popularity}</td>
+          </tr>
         </table>
-        </div>
-        )
-})
-return <div>{contacts}</div>
+      </div>
+    );
+  });
+  return <div>{contacts}</div>;
 }
