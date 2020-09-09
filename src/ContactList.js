7af5072dcea6e7c4a17f8491861d5dfbf9c74963
Esra Ozel <esra.ozel_@hotmail.com>
2020-09-09 14:09:41 +0200

    change the tabel && done without bonus iteration

--- src/ContactList.js
+++ src/ContactList.js
@@ -6,7 +6,7 @@ const ContactList=  props => {
     // const { pictureUrl, name, popularity, id } = contacts;
     return (
         <tr>
-            <td className='img-td'><img src={props.pictureUrl} alt={props.name} /></td>
+            <td><img src={props.pictureUrl} alt={props.name} height="140px"/></td>
             <td>{props.name}</td>
             <td>{props.popularity}</td>
             <td><button onClick={props.clickToDelete}>Delete</button></td>
