1da93a02ea6b668c083733061dc9f2813f8df9ef
Hugo Suastegui <hugo.suastegui.pineda@gamil.com>
2020-09-17 14:47:37 -0500

    Done

--- src/components/Contacts.js
+++ src/components/Contacts.js
@@ -3,14 +3,28 @@ import contacts from '../contacts.json';
 import { Table } from 'reactstrap';
 
 function Contacts() {
-    const [actors, setactors] = useState([])
-    for (let i = 0; 1 < 5; i++){
-        setactors([...actors, contacts[i]])
+    const [actors, setactors] = useState(contacts.filter((el, i) => i < 5))
+
+    const addRandom = () => {
+        let randomNumber = Math.floor(Math.random()*(contacts.length - 6) + 6)
+        setactors([...actors, contacts[randomNumber]])
     }
-    console.log(actors)
+    const sortName = () => {
+        let sortedArray = [...actors].sort((a, b) => a.name.localeCompare(b.name))
+        setactors(sortedArray)
+    }
+    const sortPopularity = () => {
+        let sortedArray = [...actors].sort((a, b) => b.popularity - a.popularity)
+        console.log(sortedArray)
+        setactors(sortedArray)
+    }
+
     return (
         <div>
         <h1>IronContacts</h1>
+        <button onClick={() => addRandom()}>Add Random Contact</button>
+        <button onClick={() => sortName()}>Sort by Name</button>
+        <button onClick={() => sortPopularity()}>Sort by Popularity</button>
             <Table>
                 <thead>
                     <tr>
@@ -20,7 +34,7 @@ function Contacts() {
                     </tr>
                 </thead>
                 <tbody>
-                    {contacts.map((el, index) => (
+                    {actors.map((el, index) => (
                         <tr key={index}>
                         <td><img className='photo' src={el.pictureUrl} alt=""/></td>
                         <td>{el.name}</td>
