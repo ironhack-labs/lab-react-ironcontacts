2bc728772ecfc3d4a9b4cddd6f9da87a411c32fb
Diana <d.thueroff@gmail.com>
2020-09-23 11:53:10 +0200

    first iteration done

--- src/App.js
+++ src/App.js
@@ -1,26 +1,29 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import ContactList from './components/ContactList';
+import contacts from './contacts.json';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+console.log(contacts.slice(0, 4))
+
+  class App extends React.Component{
+  
+    state = {
+      contact: contacts.slice(0, 4)
+    }
+  
+
+    render() {
+      return (
+        <div className='App'>
+        <h1>Contacts</h1>
+        <ContactList contact={this.state.contact}/>
+        
+    
+        </div>
+        
+      )
+    }
+  
 }
 
 export default App;
---
+++ src/components/ContactList.js
@@ -0,0 +1,33 @@
+import React from 'react'
+
+export default function ContactList(props) {
+    console.log(props.contact)
+    const contacts = props.contact.map(person => {
+        return(
+            <table key={person.id}>
+                <tbody>
+                <tr>
+                <th>Picture</th>
+                <th>Name</th>
+                <th>Popularity</th>
+                </tr>
+                <tr>
+                   <td><img src={person.pictureUrl} alt="" width="50"/></td>
+                    <td>{person.name}</td>
+                    <td>{person.popularity}</td>
+
+
+                    
+                </tr>
+                </tbody>
+            </table>
+        )
+
+    })
+    return (
+        <div>
+        <h1>These are your contacts</h1>
+           {contacts} 
+        </div>
+    )
+}
