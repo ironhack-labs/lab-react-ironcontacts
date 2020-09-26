5836547509f83f8df26d57f8421e59161c672b7a
Andrea Astorga <andrea.astorga95@gmail.com>
2020-09-26 13:05:26 +0200

    Iteration 1

--- src/App.js
+++ src/App.js
@@ -1,24 +1,24 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import allContacts from './contacts.json'
+import Contacts from './components/Contact';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1 style={{textAlign: 'center'}}>IronContacts</h1>
+
+        {allContacts.filter((contact, idx) => idx < 5).map((contact, idx) => {
+            return(
+              <Contacts 
+                name={contact.name} 
+                popularity={contact.popularity.toFixed(2)} 
+                picture={contact.pictureUrl} 
+              />
+            )
+          })
+        }
     </div>
   );
 }
---
+++ src/components/Contact.jsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import './Contact.css'
+
+function Contact(props) {
+    console.log(props.popularity)
+    return(
+        <div>
+            <table className="contact-table">
+                <tr>
+                    <th>Picture</th>
+                    <th>Name</th>
+                    <th>Popularity</th>
+                </tr>
+                <tr>
+                    <td><img className="contacts-img" src={props.picture} alt={props.name} /></td>
+                    <td>{props.name}</td>
+                    <td>{props.popularity}</td>
+                </tr>
+            </table>
+        </div>
+
+    )
+}
+
+export default Contact
