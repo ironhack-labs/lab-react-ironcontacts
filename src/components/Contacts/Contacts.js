2a6cd4714f9342ac6e6ab74bb41da13691bfb51b
ariannavalle <61655861+ariannavalle@users.noreply.github.com>
2020-09-26 15:12:05 -0400

    Complete iteration 2

--- src/components/Contacts/Contacts.js
+++ src/components/Contacts/Contacts.js
@@ -2,16 +2,38 @@ import React, { Component } from 'react'
 import contacts from '../../contacts.json';
 
 export default class Contacts extends Component {
+    state = {
+        currentContacts: contacts.slice(0, 5)
+    }
+
+    addContact = newContact => {
+        const currentContactsCopy = [...this.state.currentContacts]
+        currentContactsCopy.push(newContact)
+        this.setState({
+            currentContacts: currentContactsCopy
+        })
+    }
+
+    getRandomContact = () => {
+        return contacts[Math.floor(Math.random() * contacts.length)]
+    }
+
     render() {
-        const fiveContacts = [...contacts].slice(0, 5)
         return (
             <div>
                 <h1>IronContacts</h1>
                 {
-                    fiveContacts.map((contact) => {
-                        return <div key={contact.id}> <img width="50" src={contact.pictureUrl} /> {contact.name} | {contact.popularity.toFixed(2)}</div>
+                    this.state.currentContacts.map((contact) => {
+                        return (
+                            <div key={contact.id}>
+                                <img width="50" src={contact.pictureUrl} />
+                                {contact.name} |
+                                {contact.popularity.toFixed(2)} |
+                            </div>
+                        )
                     })
                 }
+                <button onClick={() => { this.addContact(this.getRandomContact()) }}>Add new random contact</button>
             </div>
         )
     }
