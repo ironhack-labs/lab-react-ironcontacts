c21589efe4dbb711b55399bce7bbcc8460bb2b6b
alexugo <alex.ugodnikov@gmail.com>
2020-09-26 16:44:02 -0400

    Iteration 3

--- src/App.js
+++ src/App.js
@@ -6,21 +6,58 @@ const contactsToDisplay = contacts.slice(0, 5);
 
 export default class App extends Component {
 
-  state = {contactsToDisplay};
+  state = {contactsToDisplay, sortNameAsc: true, sortPopAsc: true};
   addRandomContact = () => {
     const newContact = contacts[Math.round(Math.random() * contacts.length)];
     this.setState({
       contactsToDisplay: [...this.state.contactsToDisplay, newContact]
     });    
   }
+
+  sortByPopularity = () => {
+    this.setState ((prevState) => {
+      if (prevState.sortPopAsc) {
+      prevState.contactsToDisplay.sort((a, b) => {
+          return (a.popularity > b.popularity) ? -1 : (b.popularity > a.popularity) ? 1 : 0
+          })}else{
+          prevState.contactsToDisplay.sort((a, b) => {
+            return (a.popularity < b.popularity) ? -1 : (b.popularity < a.popularity) ? 1 : 0    
+              })
+      }
+      return {contactsToDisplay: prevState.contactsToDisplay, sortPopAsc: !prevState.sortPopAsc}
+    });  
+  }
+
+  sortByName = () => {
+    this.setState ((prevState) => {
+      if (prevState.sortNameAsc) {
+      prevState.contactsToDisplay.sort((a, b) => {
+          return (a.name.toUpperCase() > b.name.toUpperCase()) ? -1 : (b.name.toUpperCase() > a.name.toUpperCase()) ? 1 : 0
+        })}else{
+        prevState.contactsToDisplay.sort((a, b) => {
+            return (a.name.toUpperCase() > b.name.toUpperCase()) ? 1 : (b.name.toUpperCase() > a.name.toUpperCase()) ? -1 : 0      
+            })
+      }
+      return {contactsToDisplay: prevState.contactsToDisplay, sortNameAsc: !prevState.sortNameAsc}
+    });   
+  }  
   
   render() {
   return (
     <div className="App">
     <h1>Iron Contacts</h1>
-    <button onClick={() => this.addRandomContact()}>
-    Add random contact
-    </button>    
+
+    <div className='buttons'>
+      <button onClick={() => this.addRandomContact()}>
+      Add random contact
+      </button>
+      <button onClick={() => this.sortByName()}>
+    Sort by name
+      </button>   
+      <button onClick={() => this.sortByPopularity()}>
+      Sort by popularity
+      </button>  
+    </div>             
     
     <div className="divTable">
     <div className="divTableRow tableHeader">
