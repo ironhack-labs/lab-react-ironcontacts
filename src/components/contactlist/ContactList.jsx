89f99235ebe482d612cac9c17216374ec068af3c
Irma Blanco <irblancofdz@gmail.com>
2020-09-26 13:14:36 +0200

    feat: Iteration 2

--- src/components/contactlist/ContactList.jsx
+++ src/components/contactlist/ContactList.jsx
@@ -2,23 +2,29 @@ import React from 'react';
 import ContactBox from '../contactbox/ContactBox';
 
 class ContactList extends React.Component {
-  state = {};
+  state = {
+    firstContacts: this.props.contacts.slice(0, 5),
+  };
+  addRandomContact = () => {
+    const contactList = this.props.contacts;
+    const random = Math.floor(Math.random() * (contactList.length - 6)) + 6;
+    this.setState({
+      firstContacts: [...this.state.firstContacts, contactList[random]],
+    });
+  };
 
   render() {
     return (
       <div className="FoodList">
-        <input
-          type="text"
-          className="input search-bar mb-2"
-          name="search"
-          placeholder="Search"
-          onChange={this.handleChange}
-          value={this.state.search}
-        />
+        <div class="buttons">
+          <button className="button is-primary" onClick={this.addRandomContact}>
+            Add contact
+          </button>
+        </div>
 
         <div className="columns">
           <div className="column">
-            {this.props.contacts.map((contact, key) => (
+            {this.state.firstContacts.map((contact, key) => (
               <ContactBox key={key} contact={contact} />
             ))}
           </div>
