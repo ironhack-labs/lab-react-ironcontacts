5dbee8c2c2f44fd4e8222abf5b2b7a1833303a5b
Kevin Hernandez <kevin.hernandez001@gmail.com>
2020-09-26 17:15:54 -0400

    used fisher-yates to shuffle contacts to ensure no duplicate contact keys

--- src/App.js
+++ src/App.js
@@ -1,66 +1,65 @@
 import React, { Component } from 'react';
-import contacts from './contacts.json';
+import contactsDB from './contacts.json';
 import './App.css';
+
+console.log(contactsDB.length)
+
+const shuffle = (array) => {
+  var m = array.length, t, i;
+
+  while (m) {
+    i = Math.floor(Math.random() * m--);
+    t = array[m];
+    array[m] = array[i];
+    array[i] = t;
+  }
+
+  return array;
+}
+
+const shuffledcontacts = shuffle(contactsDB);
 
 class App extends Component {
 
   state = {
-    contacts: [
-      {
-        "name": "Idris Elba",
-        "pictureUrl": "https://image.tmdb.org/t/p/w500/d9NkfCwczP0TjgrjpF94jF67SK8.jpg",
-        "popularity": 11.622713,
-        "id": "11731993-0604-4bee-80d5-67ad845d0a38"
-      },
-      {
-        "name": "Johnny Depp",
-        "pictureUrl": "https://image.tmdb.org/t/p/w500/kbWValANhZI8rbWZXximXuMN4UN.jpg",
-        "popularity": 15.656534,
-        "id": "7dad00f7-3949-477d-a7e2-1467fd2cfc06"
-      },
-      {
-        "name": "Monica Bellucci",
-        "pictureUrl": "https://image.tmdb.org/t/p/w500/qlT4904d8oi2NIs28RrgnIZDFZB.jpg",
-        "popularity": 16.096436,
-        "id": "0ad5e441-3084-47a1-9e9b-b917539bba71"
-      },
-      {
-        "name": "Gal Gadot",
-        "pictureUrl": "https://image.tmdb.org/t/p/w500/34kHAyBaBhq2kUrmhM15paEBuuI.jpg",
-        "popularity": 10.049256,
-        "id": "b497e3c4-50bb-4ae2-912f-eb36802c5bc2"
-      },
-      {
-        "name": "Ian McKellen",
-        "pictureUrl": "https://image.tmdb.org/t/p/w500/coWjgMEYJjk2OrNddlXCBm8EIr3.jpg",
-        "popularity": 10.070132,
-        "id": "0067ae32-97b6-4431-898e-eb1c10150abb"
-      },
-    ]
+    contacts: shuffledcontacts.splice(0, 5),
+    sortAlpha: true,
+    sortPop: true
   }
 
   addRandomContact = () => {
-    const randomContact = contacts[Math.floor(Math.random() * contacts.length)];
-    const newContacts = [...this.state.contacts];
-    newContacts.push(randomContact);
-    this.setState({
-      contacts: newContacts
-    })
+    if (shuffledcontacts.length) {
+      const copiedContacts = [...this.state.contacts].concat(shuffledcontacts.pop());
+      console.log(shuffledcontacts.length);
+      this.setState({
+        contacts: copiedContacts
+      });      
+    } else {
+      return
+    }
   }
 
   sortByName = () => {
     const copiedContacts = [...this.state.contacts];
-    copiedContacts.sort((a, b) => (a.name > b.name) ? 1 : ((b.name > a.name) ? -1 : 0));
+    this.state.sortAlpha 
+      ? copiedContacts.sort((a, b) => a.name.localeCompare(b.name)) 
+      : copiedContacts.sort((a, b) => b.name.localeCompare(a.name));
     this.setState({
-      contacts: copiedContacts
+      contacts: copiedContacts,
+      sortAlpha: !this.state.sortAlpha,
+      sortPop: true
     })
   }
 
   sortByPopularity = () => {
     const copiedContacts = [...this.state.contacts];
-    copiedContacts.sort((a, b) => b.popularity - a.popularity);
+    this.state.sortPop 
+      ? copiedContacts.sort((a, b) => b.popularity - a.popularity) 
+      : copiedContacts.sort((a, b) => a.popularity - b.popularity);
     this.setState({
-      contacts: copiedContacts
+      contacts: copiedContacts,
+      sortPop: !this.state.sortPop,
+      sortAlpha: true
     })
   }
 
