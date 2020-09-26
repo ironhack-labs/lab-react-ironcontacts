54f81a8b3503df743f28c3bb1a3d2b0b3c432a25
Old <onelifedesigning@gmail.com>
2020-09-26 13:15:20 +0200

    IT-1 limited

--- src/components/ListContacts.jsx
+++ src/components/ListContacts.jsx
@@ -1,21 +1,31 @@
 import React from 'react';
 import Contact from './Contact';
 
-const ListContacts = (props) => (
-  <table className="table table-hover">
-    <thead className="thead-dark">
-      <tr>
-        <th>Picture</th>
-        <th>Name</th>
-        <th>Popularity</th>
-      </tr>
-    </thead>
-    <tbody>
-      {props.contacts.map((contact) => (
-        <Contact key={contact.id} contact={contact} />
-      ))}
-    </tbody>
-  </table>
-);
+class ListContacts extends React.Component {
+  state = {
+    count: 5,
+  };
+  render() {
+    return (
+      <table className="table table-hover">
+        <thead className="thead-dark">
+          <tr>
+            <th>Picture</th>
+            <th>Name</th>
+            <th>Popularity</th>
+          </tr>
+        </thead>
+        <tbody>
+          {this.props.contacts.map(
+            (contact, index) =>
+              index < this.state.count && (
+                <Contact key={contact.id} contact={contact} />
+              )
+          )}
+        </tbody>
+      </table>
+    );
+  }
+}
 
 export default ListContacts;
