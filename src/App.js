18a254740bc428fd54fd0b60dfe3a1b5b0d5e7cd
alexugo <alex.ugodnikov@gmail.com>
2020-09-26 14:51:17 -0400

    Initial Commit. Iteration 1

--- src/App.js
+++ src/App.js
@@ -1,24 +1,33 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import contacts from './contacts.json';
+
 
 function App() {
   return (
+    
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <h1>Iron Contacts</h1>
+    <div class="divTable">
+
+    <div class="divTableRow tableHeader">
+      <div class="divTableCell">Picture</div>      
+      <div class="divTableCell">Name</div>
+      <div class="divTableCell">Popularity</div>
+      </div>
+
+    {contacts.slice(0,5).map(contact => {
+      return (
+      <div class="divTableRow">
+      <div class="divTableCell"><img src={contact.pictureUrl} style={{width:'100px'}}/></div>      
+      <div class="divTableCell">{contact.name}</div>
+      <div class="divTableCell">{contact.popularity}</div>
+      </div>
+
+      )}
+    )}
+    </div>
     </div>
   );
 }
