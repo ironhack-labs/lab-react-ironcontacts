8a22b8c622e088083ecd09a8d12326619f5255c9
TL Garcia <tomas.bl.garcia@gmail.com>
2020-09-28 12:41:07 +0200

    task: It3 complete

---
+++ src/components/ContactCard.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import '../stylesheets/ContactCard.css';
+
+const ContactCard = (props) => (
+    <article className="ContactCard">
+        <img src={props.contact.pictureUrl} alt="Portrait of celebrity"/>
+        <p>{props.contact.name}</p>
+        <p>{Math.round(props.contact.popularity * 100) / 100}</p>
+    </article>
+)
+
+export default ContactCard
---
+++ src/components/ContactList.jsx
@@ -0,0 +1,48 @@
+import React, { Component } from 'react';
+import ContactCard from './ContactCard';
+import contacts from '../contacts.json';
+
+class ContactList extends Component {
+  state = {
+    contactsToDisplay: contacts.slice(0, 4),
+  };
+
+  loadRandContact = event => {
+    if (this.state.contactsToDisplay.length === contacts.length) {
+      window.alert('There are no more contacts to display');
+      return;
+    }
+
+    let randContact;
+    do {
+      const randIndex = Math.floor(Math.random() * contacts.length);
+      randContact = contacts[randIndex];
+    } while (this.state.contactsToDisplay.includes(randContact));
+
+    this.setState({
+      contactsToDisplay: [randContact, ...this.state.contactsToDisplay],
+    });
+  };
+
+  render() {
+    return (
+      <div className="content-wrapper">
+        <h1>Iron Contacts</h1>
+        <main className="ContactList">
+          <button onClick={this.loadRandContact}>Add Random Contact</button>
+          <div className="ContactList__titles">
+            <h2>Picture</h2>
+            <h2>Name</h2>
+            <h2>Popularity</h2>
+          </div>
+
+          {this.state.contactsToDisplay.map((contact) => (
+            <ContactCard contact={contact} key={contact.id} />
+          ))}
+        </main>
+      </div>
+    );
+  }
+}
+
+export default ContactList;
