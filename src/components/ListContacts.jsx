e1da09a9a31a5880062dbc294feb4432afc87815
Old <onelifedesigning@gmail.com>
2020-09-28 10:14:58 +0200

    IT-2

--- src/components/ListContacts.jsx
+++ src/components/ListContacts.jsx
@@ -3,27 +3,51 @@ import Contact from './Contact';
 
 class ListContacts extends React.Component {
   state = {
-    count: 5,
+    visibleContacts: this.props.contacts.slice(0, 5),
+    hiddeContacts: this.props.contacts.slice(5, this.props.contacts.length),
   };
+
+  randElement = (arr) => {
+    return arr[Math.floor(Math.random() * arr.length)];
+  };
+
+  handleAddRandom = () => {
+    if (this.state.hiddeContacts.length > 0) {
+      const newContact = this.randElement(this.state.hiddeContacts);
+      this.setState((oldState) => {
+        return {
+          visibleContacts: [newContact, ...oldState.visibleContacts],
+          hiddeContacts: oldState.hiddeContacts.filter(
+            (hideContact) => hideContact !== newContact
+          ),
+        };
+      });
+    } else {
+      alert('There are no more contacts to add');
+    }
+  };
+
   render() {
     return (
-      <table className="table table-hover">
-        <thead className="thead-dark">
-          <tr>
-            <th>Picture</th>
-            <th>Name</th>
-            <th>Popularity</th>
-          </tr>
-        </thead>
-        <tbody>
-          {this.props.contacts.map(
-            (contact, index) =>
-              index < this.state.count && (
-                <Contact key={contact.id} contact={contact} />
-              )
-          )}
-        </tbody>
-      </table>
+      <div>
+        <button className="btn btn-primary mb-2" onClick={this.handleAddRandom}>
+          Add Random Contact
+        </button>
+        <table className="table table-hover">
+          <thead className="thead-dark">
+            <tr>
+              <th>Picture</th>
+              <th>Name</th>
+              <th>Popularity</th>
+            </tr>
+          </thead>
+          <tbody>
+            {this.state.visibleContacts.map((contact, index) => (
+              <Contact key={contact.id + index} contact={contact} />
+            ))}
+          </tbody>
+        </table>
+      </div>
     );
   }
 }
