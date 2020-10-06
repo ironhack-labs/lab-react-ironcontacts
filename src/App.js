b1a5ce4714400ca312fa48be42aa6efe74b579e3
Ailie McCorkindale <ailiem134@hotmail.com>
2020-10-06 23:31:14 +0200

    iteration 1 finished

--- src/App.js
+++ src/App.js
@@ -1,26 +1,52 @@
 import React from 'react';
-import logo from './logo.svg';
+//import logo from './logo.svg';
 import './App.css';
+import contacts from './contacts.json';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+class App extends React.Component {
+
+  state = {
+    fiveContacts : contacts.slice(0,5)
+  }
+
+  ContactsArray = ({picture, name, popularity, key}) => {
+    return (
+      <tr key={key}>
+        <td>
+          <img class="celeb-pic" src={picture} alt={name} />
+        </td>
+        <td>{name}</td>
+        <td>{popularity}</td>
+      </tr>
+    )
+  }
+
+
+  render() {
+
+    return (
+      <div className="App">
+      <h1>Iron Contacts</h1>
+        <table id="contact-table">
+        <thead>
+          <tr>
+            <th>Picture</th>
+            <th>Name</th>
+            <th>Popularity</th>
+          </tr>
+        </thead>
+          <tbody>
+            {this.state.fiveContacts.map((contact,i) => 
+              
+                <this.ContactsArray key={i} picture={contact.pictureUrl} name ={contact.name} popularity={contact.popularity.toFixed(2)} />
+              
+            )}
+          </tbody>
+        </table>
+      </div>
+    );
+  }
 }
 
 export default App;
