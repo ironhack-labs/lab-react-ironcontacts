f441bfed2e237154beb4bd88343dff8e7579e281
Rico <rasim.rico@gmail.com>
2020-10-06 17:45:47 +0200

    iteration 2

--- src/App.js
+++ src/App.js
@@ -1,40 +1,52 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './App.css';
 import contacts from './contacts.json';
 
-console.log(contacts.slice(0, 5));
-const firstFive = contacts.slice(0, 5);
+class App extends Component {
+  state = {
+    newContacts: contacts.slice(0, 5),
+  };
 
-function FirstFive() {
-  return (
-    <div className="FirstFive">
-      {firstFive.map((contact) => (
-        <table>
-          <thead>
-            <th>Picture</th>
-            <th>Name</th>
-            <th>Popularity</th>
-          </thead>
-          <tbody>
-            <td>
-              <img src={contact.pictureUrl} alt="pics" />
-            </td>
-            <td> {contact.name} </td>
-            <td> {contact.popularity} </td>
-          </tbody>
-        </table>
-      ))}
-    </div>
-  );
-}
+  addNewContact = () => {
+    const copy = [...contacts];
+    console.log(copy);
+    const randomContact = copy[Math.floor(Math.random() * contacts.length)];
+    this.setState({
+      newContacts: [...this.state.newContacts, randomContact],
+    });
+    console.log(randomContact);
+  };
 
-function App() {
-  return (
-    <div className="App">
-      <h1>Iron Contacts</h1>
-      <FirstFive />
-    </div>
-  );
+  render() {
+    return (
+      <div className="App">
+        <h1>Iron Contacts</h1>
+        <button onClick={() => this.addNewContact()}>Add random contact</button>
+        <div className="FirstFive">
+          <table>
+            <thead>
+              <tr>
+                <th>Picture</th>
+                <th>Name</th>
+                <th>Popularity</th>
+              </tr>
+            </thead>
+            <tbody>
+              {this.state.newContacts.map((contact) => (
+                <tr key={contact.id}>
+                  <td>
+                    <img src={contact.pictureUrl} alt="pics" />
+                  </td>
+                  <td>{contact.name} </td>
+                  <td>{contact.popularity.toFixed(2)} </td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
