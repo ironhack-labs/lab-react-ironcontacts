fe8b86c8870d0a963ff3e869128fda1e815826c1
pheno <nicolas.enault@gmail.com>
2020-10-06 17:57:13 +0200

    pop css

--- src/App.js
+++ src/App.js
@@ -32,7 +32,7 @@ class App extends Component {
 
   sortPop = () => {
     let sortArr = [...this.state.contacts];
-    sortArr.sort((a,b) => a.popularity - b.popularity);
+    sortArr.sort((a,b) => b.popularity - a.popularity);
     this.setState({
       contacts: sortArr,
     });
@@ -50,9 +50,11 @@ class App extends Component {
     return (
       <div>
         <h1>IronContacts</h1>
-        <button onClick={this.add}>Add Random Contact</button>
-        <button onClick={this.sortName}>Sort by name</button>
-        <button onClick={this.sortPop}>Sort by popularity</button>
+        <div className="form-btn">
+        <button className="form-btns" onClick={this.add}>Add Random Contact</button>
+        <button className="form-btns" onClick={this.sortName}>Sort by name</button>
+        <button className="form-btns" onClick={this.sortPop}>Sort by popularity</button>
+        </div>
         <table className="Contacts">
           <thead>
             <tr>
@@ -70,7 +72,7 @@ class App extends Component {
                 </td>
                 <td>{contact.name}</td>
                 <td>{contact.popularity.toFixed(2)}</td>
-                <td> <button onClick={(event) => this.delete(i)}>Delete</button></td>
+                <td><button className="form-btns" onClick={(event) => this.delete(i)}>Delete</button></td>
               </tr>
             ))}
           </tbody>
