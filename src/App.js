2692948e658e8c3bf226854463f11d5bbd2ae2f0
Lisa Poncet <poncet.lisa@gmail.com>
2020-10-06 15:48:21 +0200

    iteration 2 done

--- src/App.js
+++ src/App.js
@@ -1,24 +1,63 @@
 import React from 'react';
-import logo from './logo.svg';
+// import logo from './logo.svg';
+import contacts from './contacts.json';
 import './App.css';
+
+class Contacts extends React.Component {
+  state = {
+    displayedContacts: contacts.slice(0, 5),
+  };
+
+  randomContact = () => {
+    let randomContact = contacts[Math.floor(Math.random() * contacts.length)];
+    console.log(randomContact);
+    let copy = [...this.state.displayedContacts];
+    copy.push(randomContact);
+    this.setState({
+      displayedContacts: copy,
+    });
+  };
+
+  render() {
+    return (
+      <table className="table">
+        <thead>
+          <tr>
+            <th className="th-title" colSpan="3">
+              IronContacts
+            </th>
+          </tr>
+          <tr>
+            <th className="th-title" colSpan="3">
+              <button onClick={this.randomContact}>add a random contact</button>
+            </th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td className="td-title">Picture</td>
+            <td className="td-title">Name</td>
+            <td className="td-title">Popularity</td>
+          </tr>
+          {this.state.displayedContacts.map((contact, i) => (
+            <tr key={i}>
+              <td className="td-content">
+                <img src={contact.pictureUrl} alt="contact-img" />
+              </td>
+              <td className="td-content">{contact.name}</td>
+              <td className="td-content">{contact.popularity.toFixed(2)}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    );
+  }
+}
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Contacts />
     </div>
   );
 }
