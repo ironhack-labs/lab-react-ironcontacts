ee8c5904646de8f3bc20a2a7be9e0b0cf9c62b09
pheno <nicolas.enault@gmail.com>
2020-10-06 16:52:47 +0200

    iteration 1&2 done

--- src/App.js
+++ src/App.js
@@ -1,26 +1,53 @@
-import React from 'react';
+import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import contacts from './contacts.json';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const contactsArr = [...contacts].slice(0, 5);
+
+class App extends Component {
+  state = {
+    contacts: contactsArr,
+  };
+
+  add = () => {
+    let newArr = [...this.state.contacts];
+    newArr.push(
+      contacts[Math.floor(Math.random() * Math.floor(contacts.length))]
+    );
+    this.setState({
+      contacts: newArr,
+    });
+  };
+
+  render() {
+    return (
+      <div>
+        <h1>IronContacts</h1>
+        <button onClick={this.add}>Add Random Contact</button>
+        <table className="Contacts">
+          <thead>
+            <tr>
+              <th>Picture</th>
+              <th>Name</th>
+              <th>Popularity</th>
+            </tr>
+          </thead>
+          <tbody>
+            {this.state.contacts.map((contact) => (
+              <tr>
+                <td>
+                  <img className="pictureUrl" src={contact.pictureUrl} alt="" />
+                </td>
+                <td>{contact.name}</td>
+                <td>{contact.popularity.toFixed(2)}</td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
+    );
+  }
 }
 
 export default App;
