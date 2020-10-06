4f217e8e9a26121a09bafccc63d988ca1f5ff37f
JorellLangmar <jorell.langmar@gmail.com>
2020-10-06 16:54:01 +0200

    Iteration 2

--- src/App.js
+++ src/App.js
@@ -1,25 +1,66 @@
-import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Contact from './contacts.json';
+import React, { Component } from 'react';
 
-function App() {
+const ContactFive = [...Contact].slice(0,5);
+
+class App extends Component {
+
+  state = {
+    AllContact: ContactFive,
+  };
+ 
+addNewStar = () => {
+  let displayContact = [...this.state.AllContact];
+  displayContact.push(Contact[Math.floor(Math.random() * Math.floor(Contact.length))])
+  this.setState({ 
+    AllContact: displayContact
+  })
+  console.log(this.state.AllContact);
+}
+
+
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+        <h1>IronContacts</h1>
+          <button onClick={this.addNewStar}>Add Random Contact</button>
+            <table>
+              <thead>
+                <tr>
+                  <th>Picture</th>
+                  <th>Name</th>
+                  <th>Popularity</th>
+                </tr>
+              </thead>
+              <tbody>
+                {this.state.AllContact.map((person) => {
+                  return (
+                    <Contacts
+                      name={person.name}
+                      popularity={person.popularity}
+                      pictureUrl={person.pictureUrl}/>
+                  );
+                })}
+              </tbody>
+            </table>
+        </header>
+      </div>
+    );
+  }
+}
+
+function Contacts(props) {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <tr>
+      <td>
+        <img style={{height: "20%"}} src={props.pictureUrl} alt="" />
+      </td>
+      <td>{props.name}</td>
+      <td>{props.popularity.toFixed(2)}</td>
+    </tr>
   );
 }
 
