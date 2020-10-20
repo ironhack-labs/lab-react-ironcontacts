5c4f7ecc07b5a2a73b53688107132cf806d456c7
allecramarcella <allecramarcella.dev@gmail.com>
2020-10-20 21:46:56 +0200

    it 1

--- src/App.js
+++ src/App.js
@@ -1,26 +1,49 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
+import contacts from './contacts.json'
+
+
+
+class App extends React.Component {
+
+  firstFiveContacts = contacts.slice(0,5)
+
+  state = {
+    contacts: this.firstFiveContacts
+  }
+
+  render(){
+    return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>IronContacts</h1>
+      <table>
+        <thead>
+          <tr>
+            <th>Picture</th>
+            <th>Name</th> 
+            <th>Popularity</th>
+          </tr>
+        </thead>
+
+        {this.state.contacts.map(contact => {
+          return (
+            <tbody>
+              <tr key={contact.id}>
+                <td > <img src={contact.pictureUrl} alt={contact.name}/> </td>
+                <td >{contact.name}</td>
+                <td >{contact.popularity}</td>
+              </tr>
+            </tbody>
+          )
+        })}
+      </table>
     </div>
-  );
-}
+
+    )
+  }
+  }
+
+
 
 export default App;
