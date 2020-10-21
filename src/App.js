9c4550c341bf81575f8e707e10cf072173d66ca0
anna dodo <anna.dorenskaya@gmail.com>
2020-10-21 21:21:14 +0200

    smth

--- src/App.js
+++ src/App.js
@@ -7,38 +7,53 @@ import contacts from './contacts.json';
 export default class App extends Component {
   state = {
     contacts: contacts,
-    randomContact: []
+    randomContact: [],
+    sortType: 'asc',
   };
 
   addContact = (min, max) => {
-    const randomNum = Math.floor(Math.random() * max) + min  
+    const randomNum = Math.floor(Math.random() * max) + min;
     const randomContact = this.state.contacts[randomNum];
-    
-     this.setState({
-        ...this.state,
-        randomContact: [
-          ...this.state.randomContact,
-          randomContact
-        ]
-     }) 
-  }
-  
-  render() {
 
-    const firstFiveContacts = this.state.contacts.slice(0, 5);
-    /* this.setState({
+    this.setState({
       ...this.state,
-      fiveContacts: firstFiveContacts
-    }) */
-    console.log(this.state)
-    const max = this.state.contacts.length - 1
-    const min = 5
-    console.log(max); 
+      randomContact: [...this.state.randomContact, randomContact],
+    });
+  };
+
+  nameSort = (allActualContacts) => {
+    const sortActual = allActualContacts.sort((a, b) =>
+      a.name > b.name ? 1 : b.name > a.name ? -1 : 0
+    );
+    this.setState({
+      sortActual,
+    });
+    console.log(sortActual)
+  };
+
+
+
+  render() {
+    const firstFiveContacts = this.state.contacts.slice(0, 5);
+    const max = this.state.contacts.length - 1;
+    const min = 5;
+    //
+    const allActualContacts = [
+      ...firstFiveContacts,
+      ...this.state.randomContact,
+    ];
+
     return (
       <div className="App">
         <div>
           <h3>IronContacts</h3>
-          <button onClick={() => this.addContact(min, max)}>Add Random Contact</button>
+          <button onClick={() => this.addContact(min, max)}>
+            Add Random Contact
+          </button>
+          <button onClick={() => this.nameSort(allActualContacts)}>
+            Sort by name
+          </button>
+          <button>Sort by popularity</button>
           <table>
             <thead>
               <tr>
@@ -55,21 +70,19 @@ export default class App extends Component {
                     <img src={contact.pictureUrl} alt="" />
                   </td>
                   <td>{contact.name}</td>
-
                   <td>{contact.popularity}</td>
                 </tr>
               ))}
-              {this.state.randomContact.map(item => (
+              {this.state.randomContact.map((item) => (
                 <tr key={item.id}>
                   <td>
                     <img src={item.pictureUrl} alt="" />
                   </td>
                   <td>{item.name}</td>
-
                   <td>{item.popularity}</td>
                 </tr>
-              )
-              )}
+              ))}
+              {/* {this.state.sortActual.map((contact) => contact.name)} */}
             </tbody>
           </table>
         </div>
