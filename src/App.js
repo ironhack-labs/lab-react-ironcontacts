b061deb38e8ab0afeaa2079c759a94ba8256b524
isisabreu <isisrcabreu@gmail.com>
2020-10-30 00:19:52 -0300

    done until partially until interaction 2

--- src/App.js
+++ src/App.js
@@ -2,25 +2,49 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+import contacts from './contacts.json';
+import { render } from '@testing-library/react';
+
+class App extends React.Component{
+  state = {
+    contacts: contacts.slice(0,5),
+  };
+
+
+displayContacts = () => {
+  const contactsJSX = this.state.contacts.map((contact) => {
+    return (
+      <div>
+      <table>
+      <thead>
+      <th>Name: {contact.name}</th>
+      <th>Picture URL: {contact.pictureUrl}</th>
+      <th>Popularity: {contact.popularity}</th>
+      <th>id: {contact.id}</th>
+      </thead>
+      </table>
     </div>
-  );
+    );
+  });
+  return contactsJSX;
+};
+
+addRandomContact = () =>{
+  const index = Math.floor(Math.random()*contacts.length);
+  const randomContact = contacts[index]; 
+  this.state.contacts.push(randomContact);
+  this.setState({contacts: this.state.contacts });
+
+};
+
+render() {
+  return(
+    <div className="App">
+        {this.displayContacts()}
+        <button onClick={this.addRandomContact}>Add Random Content</button>
+    </div>
+  )
+};
 }
 
 export default App;
