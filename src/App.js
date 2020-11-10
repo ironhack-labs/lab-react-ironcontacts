d20d88fd653b8d8aceeaa70a5c5d83c5fabe6ef1
Alberto <a.p.r.13287@gmail.com>
2020-11-10 18:47:36 +0100

    falta el bonus

--- src/App.js
+++ src/App.js
@@ -1,26 +1,91 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
+import contacts from './contacts.json';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
 
+class App extends Component {
+  state = {
+    contactsFive: contacts.slice(0, 5)
+  }
+
+  actorRandom = () => {
+    const copyContacts = [...this.state.contactsFive]
+    const random = contacts[Math.floor(Math.random() * contacts.length)];
+    if (!copyContacts.includes(random)) {
+      copyContacts.push(random);
+    }
+    this.setState({
+      contactsFive: copyContacts
+    })
+  }
+
+  sortName = () => {
+    const copyFive = [...this.state.contactsFive]
+    copyFive.sort(function(a, b){
+      if(a.name < b.name) { return -1; }
+      if(a.name > b.name) { return 1; }
+      return 0;
+  })
+  
+    this.setState({
+      contactsFive: copyFive
+    })
+  }
+
+  sortPopularity = () => {
+    const copyFive = [...this.state.contactsFive]
+    copyFive.sort(function(a, b){
+      if(a.popularity > b.popularity) { return -1; }
+      if(a.popularity < b.popularity) { return 1; }
+      return 0;
+    })
+  
+    this.setState({
+      contactsFive: copyFive
+    })
+  }
+  
+  deleteButton (id) {
+    
+      let filtered = this.state.contactsFive.filter(contact=> contact.id !== id)
+      this.setState({contactsFive:[...filtered]})
+    
+  }
+
+
+
+render() {
+    return (
+      <div className="App">
+        <h1>IronContacts</h1>
+        <button onClick={() => this.actorRandom()}>
+          Add Random Contact</button>
+        <button onClick={() => this.sortName()}>
+          Sort by name</button>
+        <button onClick={() => this.sortPopularity()}>
+          Sort by popularity</button>
+        <table>
+          <thead>
+            <tr>
+              <th>Picture</th>
+              <th>Name</th>
+              <th>Popularity</th>
+              <th>Action</th>
+            </tr>
+          </thead>
+          <tbody>
+            {this.state.contactsFive.map(oneActor => 
+             <tr key={oneActor.id}>
+                <td><img src={oneActor.pictureUrl} /></td>
+                <td>{oneActor.name}</td>
+                <td>{oneActor.popularity}</td>
+                <td><button onClick={() => this.deleteButton(oneActor.id)}>Delete</button></td>
+            </tr>
+            )}
+          </tbody>
+        </table>
+      </div>
+    )
+  }
+}
 export default App;
