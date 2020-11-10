21996ebdbdc9052f8c093e18b52d92c9233108a5
Oscar M <empanaisimo@gmail.com>
2020-11-10 16:43:22 +0100

    Iteration 2

--- src/App.js
+++ src/App.js
@@ -1,36 +1,53 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {Component} from 'react';
 import './App.css';
 import contacts from './contacts';
 
-function App() {
+class App extends Component {
 
-  var filteredContacts;
+  initContacts = [];
+  initContacts = contacts.filter((contact, idx) => idx < 5);
 
-  return (
-     <div className="App">
-      <header className="App-header">
-      <h1>IronContacts</h1>
-      <table>
-        <tr>
-          <th>Picture</th>
-          <th>Name</th>
-          <th>Popularity</th>
-        </tr>
-        {filteredContacts = contacts.map(contact => 
-          <tr>
-            <td><img src={contact.pictureUrl} alt={contact.name} width={'55px'} height={'85px'}/></td>
-            <td><p>{contact.name}</p></td>
-            <td><p>{contact.popularity}</p></td>
-          </tr>
-          )}
-      </table> 
+  state = {
+    contacts: this.initContacts
+  }
 
-      
+  addRandomContact = () =>{
+    const rndContact = contacts[Math.floor(Math.random()*contacts.length)]
+    const newContacts = [...this.state.contacts, rndContact]
+    this.setState({contacts: newContacts})
+  }
 
-      </header>
-    </div>
-  );
+
+
+  // displayedContacts = 
+
+  render(){
+
+    return (
+      <div className="App">
+       <header className="App-header">
+       <h1>IronContacts</h1>
+       <button onClick={()=>this.addRandomContact()}>Add Random Contact</button>
+       <table>
+         <tr>
+           <th>Picture</th>
+           <th>Name</th>
+           <th>Popularity</th>
+         </tr>
+         {this.state.contacts.map(contact => 
+           <tr>
+             <td><img src={contact.pictureUrl} alt={contact.name} width={'55px'} height={'85px'}/></td>
+             <td><p>{contact.name}</p></td>
+             <td><p>{contact.popularity}</p></td>
+           </tr>
+           )} 
+        </table> 
+       </header>
+     </div>
+   );
+
+  }
+
 }
 
 export default App;
