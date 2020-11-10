92122cd872f2dc48f1d8a005bad00862cce5f6f6
Cristina Castro <cristinacastro87@gmail.com>
2020-11-10 17:44:13 +0100

    done

--- src/App.js
+++ src/App.js
@@ -1,26 +1,100 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
+import contacts from './contacts.json';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+class App extends Component {
+  state = {
+    firstFiveContacts: contacts.slice(0, 5),
+  }
+  
+  selectRandom = () => {
+    const contactsCopy =  [...this.state.firstFiveContacts]
+    const newRandom = contacts[Math.floor(Math.random()*contacts.length)];
+    if(!contactsCopy.includes(newRandom)){
+      contactsCopy.push(newRandom)
+    }
+    this.setState({
+      firstFiveContacts: contactsCopy
+    })
+  }
+
+  sortByName = () => {
+    const nameSortedArr = [...this.state.firstFiveContacts].sort((a,b) => {
+      if (a.name > b.name){
+        return 1;
+      } else if (a.name < b.name){
+        return -1
+      } else {
+        return 0
+      }
+    });
+    this.setState({
+      firstFiveContacts: nameSortedArr
+    })
+  }
+
+  sortByPopularity = () => {
+    const popSortedArr = [...this.state.firstFiveContacts].sort((a,b) => {
+      if (a.popularity > b.popularity){
+        return 1;
+      } else if (a.popularity < b.popularity){
+        return -1
+      } else {
+        return 0
+      }
+    });
+    this.setState({
+      firstFiveContacts: popSortedArr
+    })
+  }
+
+  deleteContact = (contactIndex) => {
+    const contactsCopy = [...this.state.firstFiveContacts]
+    const filtered = contactsCopy.filter(oneContact => oneContact.id !== contactIndex)
+
+    this.setState({
+      firstFiveContacts: filtered
+    })
+  }
+
+
+  
+  render() {
+    return (
+      <div className="App">
+        <h1>IconContacts</h1>
+        <button onClick={this.selectRandom}>Add a random contact</button>
+        <button onClick={this.sortByName}>Sort by name</button>
+        <button onClick={this.sortByPopularity}>Sort by popularity</button>
+        <table>
+          <thead>
+            <tr>
+              <th>Picture</th>
+              <th>Name</th>
+              <th>Popularity</th>
+              <th>Delete</th>
+
+            </tr>
+          </thead>
+          <tbody>
+            {this.state.firstFiveContacts.map((oneContact, index) => {
+              return (
+                <tr key={index} {...oneContact}>
+                  <td>
+                    <img src={oneContact.pictureUrl} alt="" width="60" />
+                  </td>
+                  <td>{oneContact.name}</td>
+                  <td>{Math.round(oneContact.popularity * 100) / 100}</td>
+                  <td><button onClick={() => this.deleteContact(oneContact.id)}>Delete</button></td>
+                </tr>
+              );
+            })}
+          </tbody>
+        </table>
+      </div>
+    );
+  }
 }
 
 export default App;
