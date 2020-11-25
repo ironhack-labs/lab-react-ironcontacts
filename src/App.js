73e79d15053b39ac23c453d2065123f12e864f75
Ivan Rubio <ivanrubioangeles@gmail.com>
2020-11-24 20:32:23 -0600

    otra tabla

--- src/App.js
+++ src/App.js
@@ -7,25 +7,7 @@ import Table from './components/Table';
 function App() {
   return (
     <div className="App">
-      <div>
-        <table class="egt">
-          <tr>
-            <td>Celda 1</td>
-
-            <td>Celda 2</td>
-
-            <td>Celda 3</td>
-          </tr>
-
-          <tr>
-            <td>Celda 4</td>
-
-            <td>Celda 5</td>
-
-            <td>Celda 6</td>
-          </tr>
-        </table>
-      </div>
+      <Table></Table>
     </div>
   );
 }
