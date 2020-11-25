f5f1e3c05aa373ff3a791bedb9b75e15dd0bf982
Rich Alossi <alossix@gmail.com>
2020-11-25 18:21:16 +0100

    Iteration 2 done

--- src/App.js
+++ src/App.js
@@ -3,14 +3,22 @@ import './App.css';
 import contacts from './contacts.json';
 
 function App() {
-  const [contactState, setContactState] = useState();
-  let contactsFive = contacts.slice(0, 5);
+  const [contactState, setContactState] = useState([]);
+  const contactsNew = [...contactState];
+  const contactsFive = contacts.slice(0, 5);
+
+  const randomContactHandler = () => {
+    let randomContact = contacts[Math.floor(Math.random() * contacts.length)];
+    contactsNew.push(randomContact);
+    setContactState(contactsNew);
+  };
 
   return (
     <div className="App">
       <table>
         <th>
           <tr>IronContacts</tr>
+          <button onClick={randomContactHandler}>Add Random Contact</button>
           <tr>
             <td className="contactsTable-data">Picture</td>
             <td className="contactsTable-data">Name</td>
@@ -20,7 +28,18 @@ function App() {
             return (
               <div className="contactsTable">
                 <td className="contactsTable-Img">
-                  <img src={contact.pictureUrl}></img>
+                  <img src={contact.pictureUrl} alt={contact.name}></img>
+                </td>
+                <td className="contactsTable-data">{contact.name}</td>
+                <td className="contactsTable-data">{contact.popularity}</td>
+              </div>
+            );
+          })}
+          {contactState.map((contact) => {
+            return (
+              <div className="contactsTable">
+                <td className="contactsTable-Img">
+                  <img src={contact.pictureUrl} alt={contact.name}></img>
                 </td>
                 <td className="contactsTable-data">{contact.name}</td>
                 <td className="contactsTable-data">{contact.popularity}</td>
