59679179824efa2c697384866915624315c86dc6
Nina van den Braak <ninavandenbraak@gmail.com>
2020-11-25 12:20:28 +0100

    improved iteration 1 and made 2

--- src/App.js
+++ src/App.js
@@ -1,12 +1,52 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
-import FiveContacts from './components/fivecontacts';
+import Contacts from './contacts';
+
+function ShowContacts() {
+  const [contacts, setContacts] = useState(Contacts.splice(0, 5));
+
+  const randomContactHandler = () => {
+    const contactCopy = [...contacts];
+    const getRandomContact = () =>
+      Contacts[
+        Math.floor(
+          Math.random() * (Contacts.length - (contacts.length - 1)) +
+            (contacts.length - 1)
+        )
+      ];
+    contactCopy.push(getRandomContact());
+
+    setContacts(contactCopy);
+  };
+
+  return (
+    <div>
+      <button onClick={randomContactHandler}>Add Random Contact</button>
+      <table>
+        <tr>
+          <th>Picture</th>
+          <th>Name</th>
+          <th>Popularity</th>
+        </tr>
+        {contacts.map((item) => (
+          <tr key={item.id}>
+            <td>
+              <img src={item.pictureUrl} alt={item.name} />
+            </td>
+            <td>{item.name}</td>
+            <td>{item.popularity}</td>
+          </tr>
+        ))}
+      </table>
+    </div>
+  );
+}
 
 function App() {
   return (
     <div className="App">
       <h1>IronContacts</h1>
-      <FiveContacts />
+      <ShowContacts />
     </div>
   );
 }
