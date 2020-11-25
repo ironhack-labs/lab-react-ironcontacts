e9a753d55f6e4b620b3046768322a16e09867dc2
Ivan Rubio <ivanrubioangeles@gmail.com>
2020-11-24 20:47:24 -0600

    objects

--- src/App.js
+++ src/App.js
@@ -3,6 +3,12 @@ import logo from './logo.svg';
 import './App.css';
 import contacts from './contacts.json';
 import Table from './components/Table';
+let contactsObj = { ...contacts };
+
+Object.entries(contactsObj).map(([key, value]) => {
+  let contact = value;
+  console.log(value);
+});
 
 function App() {
   return (
