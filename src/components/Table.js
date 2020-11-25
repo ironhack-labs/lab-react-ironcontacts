7f55870dd6464373970ba64ed95451ca0276dd0a
Ivan Rubio <ivanrubioangeles@gmail.com>
2020-11-24 18:05:05 -0600

    1

---
+++ src/components/Table.js
@@ -0,0 +1,6 @@
+import React from 'react';
+
+const Table = '';
+//Display that array of 5 contacts in a <table> and display the picture, name, and popularity of each contact.
+
+export default Table;
