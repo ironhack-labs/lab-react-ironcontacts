76bf6a7a34ffb9b0d1990fa1a9e4b64d306c7e4e
Ivan Rubio <ivanrubioangeles@gmail.com>
2020-11-24 21:36:44 -0600

    a table

--- src/App.js
+++ src/App.js
@@ -1,19 +1,43 @@
 import React from 'react';
+import ReactDOM from 'react-dom';
 import logo from './logo.svg';
 import './App.css';
-import contacts from './contacts.json';
-import Table from './components/Table';
-let contactsObj = { ...contacts };
+import dataSource from './contacts.json';
+import 'antd/dist/antd.css'; // or 'antd/dist/antd.less'
 
-Object.entries(contactsObj).map(([key, value]) => {
-  let contact = value;
-  console.log(value);
-});
+import { Table } from 'antd';
+const columns = [
+  {
+    title: 'Name',
+    dataIndex: 'name',
+    width: 150,
+  },
+  {
+    title: 'pictureUrl',
+    dataIndex: 'pictureUrl',
+    width: 150,
+  },
+  {
+    title: 'popularity',
+    dataIndex: 'popularity',
+  },
+  {
+    title: 'id',
+    dataIndex: 'id',
+  },
+];
+
+const data = dataSource;
 
 function App() {
   return (
     <div className="App">
-      <Table></Table>
+      <Table
+        columns={columns}
+        dataSource={data}
+        pagination={{ pageSize: 50 }}
+        scroll={{ y: 240 }}
+      />
     </div>
   );
 }
