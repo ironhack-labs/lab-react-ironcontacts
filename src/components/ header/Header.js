7e0a13e3d29347a23a4da64b346e1daa945dce39
ElfGod <albertogx@icloud.com>
2020-11-25 00:40:23 -0500

    first commit

---
+++ src/components/ header/Header.js
@@ -0,0 +1,13 @@
+
+import React from 'react'
+import './Header.css'
+
+class Header extends Compenent {
+    return (
+        <div className="header">
+            <h1 >IronContacts</h1>
+        </div>
+    )
+}
+
+export default Header
