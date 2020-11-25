07e348996dc1f845948dbc9cb6b3b2b63074509a
Maria Reyna <mereynag@gmail.com>
2020-11-25 11:09:18 -0400

    avances

--- src/App.js
+++ src/App.js
@@ -4,15 +4,30 @@ import './App.css';
 import contacts from './contacts.json'
 
 function FirstFive(){
-    const firstFive = contacts.slice(0, 5)
-    const [contacts, setContacts] = useState(firstFive)
+    // const firstFive = contacts.slice(0, 5)
+    const [contacts, setContacts] = useState(contacts.slice(0, 5))
 
     return (
       <table>
-
+        <tr>
+          <th>Picture</th>
+          <th>Name</th>
+          <th>Popularity</th>
+        </tr>
+        {contacts.map(contact => <Table key={contact.id} {...contact} name={contact.name} popularity={contact.popularity}/>        
       </table>
     )
 
+}
+
+function Table({name, popularity, pictureUrl}){
+  return (
+    <tr>
+        <td><img src={pictureUrl}/></td>
+        <td>{name}</td>
+        <td>{popularity}</td>
+    </tr>
+  )
 }
 
 
