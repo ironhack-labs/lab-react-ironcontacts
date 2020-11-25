039f58516cd919420a0f18c0a89e1099753bfa89
KarenRoth <karenrothc@gmail.com>
2020-11-25 11:39:07 -0300

    problems

--- src/App.js
+++ src/App.js
@@ -1,24 +1,50 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useState} from 'react';
 import './App.css';
+import Contacts from './contacts.json';
+import styled from 'styled-components';
+// import showContacts from './components/showContacts'
+
+let arrContacts = [...Contacts]
+let lengthContacts = arrContacts.length
 
 function App() {
+    let [ show, setShow ] = useState(Contacts.slice(0,5))
+  //   const randomContact = () => {
+  //    let randomContactPos = Math.floor(Math.random()*(lengthContacts-5)+5)
+  //    arrContacts.push(arrContacts[randomContactPos])
+  //    setShow(show=arrContacts)
+  //  }
+
+   const ContactStyled = styled.article`
+  width: 500px;
+  height: auto;
+  img {
+    width: 100px;
+    height: auto;
+  }
+`;
+  
+ const showContacts = ({ name, pictureUrl, popularity}) => {
+  return (
+   <ContactStyled>
+    <img src={pictureUrl} alt="picture" />
+    <h2>{name}</h2>
+    <b>{popularity}</b>
+    </ContactStyled>
+      );
+}
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     
+      {/* <button onClick={randomContact}>Add Random Contact</button> */}
+      {showContacts}
+      {show.map(contact=> <showContacts 
+         key={contact.id}
+           {...contact}
+          />
+        )}
     </div>
   );
 }
