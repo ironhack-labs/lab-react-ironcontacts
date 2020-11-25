f5eb131cfe2df6c84385ef0a723a12e2d9fb1be4
Maria Reyna <mereynag@gmail.com>
2020-11-25 10:53:02 -0400

    primeros avances

--- src/App.js
+++ src/App.js
@@ -1,24 +1,28 @@
-import React from 'react';
+import React, {useState} from 'react';
 import logo from './logo.svg';
 import './App.css';
+import contacts from './contacts.json'
+
+function FirstFive(){
+    const firstFive = contacts.slice(0, 5)
+    const [contacts, setContacts] = useState(firstFive)
+
+    return (
+      <table>
+
+      </table>
+    )
+
+}
+
+
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     <h1>IronContacts</h1>
+     <FirstFive/>
     </div>
   );
 }
