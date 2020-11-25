2aca763054ec5bcaf8e8e4dac42a347b02c8d8a2
Roy-Arne Brussel <29131401+Roybrussel@users.noreply.github.com>
2020-11-25 15:38:36 +0100

    Adjusted iteration 1.

--- src/App.js
+++ src/App.js
@@ -3,31 +3,35 @@ import './App.css';
 import Contacts from './contacts.json';
 
 function App() {
+  const listOfContacts = Contacts.slice(0, 5);
   return (
     <div className="App">
-      <table>
-        <thead>
-          <tr>
-            <th>Picture</th>
-            <th>Name</th>
-            <th>Popularity</th>
-          </tr>
-        </thead>
-        {Contacts.map((contact) => (
-          <tr>
-            <td>
-              <img src={contact.pictureUrl} alt=""></img>
-            </td>
-            <td>
-              <p>{contact.name}</p>
-            </td>
-            <td>
-              <p>{contact.popularity.toFixed(2)}</p>
-            </td>
-          </tr>
-        ))}
-        ;
-      </table>
+      <div>
+        <h1>IronContacts</h1>
+        <button>Add Random Contact</button>
+        <table>
+          <thead>
+            <tr>
+              <th>Picture</th>
+              <th>Name</th>
+              <th>Popularity</th>
+            </tr>
+          </thead>
+          {listOfContacts.map((contact) => (
+            <tr>
+              <td>
+                <img src={contact.pictureUrl} alt=""></img>
+              </td>
+              <td>
+                <p>{contact.name}</p>
+              </td>
+              <td>
+                <p>{contact.popularity.toFixed(2)}</p>
+              </td>
+            </tr>
+          ))}
+        </table>
+      </div>
     </div>
   );
 }
