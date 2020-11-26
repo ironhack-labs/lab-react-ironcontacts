a35ad58e1f53c0531792862ab8726f9a5b51e1a3
Abbie <abbie@summers.gg>
2020-11-26 12:39:10 +0100

    Iteration 1-4 complete

--- src/App.js
+++ src/App.js
@@ -1,26 +1,78 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import contacts from './contacts.json';
+class App extends React.Component {
+  state = {
+    shortList: contacts.slice(0, 5),
+  };
+  addRandom() {
+    const contactsCopy = [...this.state.shortList];
+    const newRandom = contacts[Math.floor(Math.random() * contacts.length)]
+    contactsCopy.push(newRandom);
+    this.setState({
+      shortList: contactsCopy
+    })
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  };
+
+  sortByName() {
+    const sortedList = this.state.shortList
+      .sort((a, b) => a.name.localeCompare(b.name))
+
+    this.setState({
+      shortList: sortedList
+    })
+  };
+
+  sortByPopularity() {
+    const newSortedList = this.state.shortList
+      .sort((a, b) => a.popularity - b.popularity)
+
+    this.setState({
+      shortList: newSortedList
+    })
+  };
+
+  delete(contactId) {
+    const updatedList = this.state.shortList
+    const contactIndex = updatedList.findIndex((item) => item.id === contactId)
+    updatedList.splice(contactIndex, 1)
+    console.log(updatedList)
+    this.setState({
+      shortList: updatedList
+    })
+
+  };
+
+  render() {
+    return (
+      <div className="App">
+        <h1>IronContacts</h1>
+        <button onClick={() => this.addRandom()}>Add Random</button>   <button onClick={() => this.sortByName()}>Sort by name</button>
+        <button onClick={() => this.sortByPopularity()}>Sort by popularity</button>
+        <div className="table-wrapper">
+          <table>
+            <tr>
+              <th>Picture</th>
+              <th>Name</th>
+              <th>Popularity</th>
+              <th>Action</th>
+            </tr>
+            {this.state.shortList.map((item) => {
+              return (
+                <tr key={item.id} {...item}>
+                  <td><img src={item.pictureUrl} alt="" height="150" /></td>
+                  <td>{item.name}</td>
+                  <td>{Math.round(item.popularity * 100) / 100}</td>
+                  <td><button onClick={() => this.delete(item.id)} className="delete-btn">Delete</button></td>
+                </tr>
+              )
+            })}
+          </table>
+        </div>
+
+      </div>
+    )
+  };
 }
-
 export default App;
