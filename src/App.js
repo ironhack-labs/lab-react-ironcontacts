8c191eb78302eb7d42b06a31e8af1a28a2755aec
Maya Ausset <mayaausset@gmail.com>
2020-11-26 19:54:20 +0100

    iteration 2 failed, moving on...

--- src/App.js
+++ src/App.js
@@ -1,19 +1,29 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
 import contactsSeed from './contacts.json';
 import Contact from './components/contact'
 
 function App() {
   const firstContacts = contactsSeed.slice(0,5);
-  //const [contacts, setContacts] = useState(firstContacts);
+  const [contacts, setContacts] = useState(firstContacts);
 
+  const addRandomContact = () => {
+    const contactsCopy = [...contacts];
+    const randomContact = contactsSeed[Math.floor(Math.random() * contactsSeed.length)];
+    const newRandom = contactsCopy.push(randomContact);
+    console.log(`RANDOM CONTACT IS ${randomContact}, contact is ${contacts}  NEW RANDOM IS ${newRandom}`);
+    //! does not work... // -- > setContacts(firstContacts, newRandom);
+  };
 
   return (
     <div className="App">
     <h1 className="title">IronContacts</h1>
+    <div>
+    <button onClick={addRandomContact()} className="random-btn">Add a Random Contact</button>
+    </div>
     <div className="contact-div">
-    {firstContacts.map((contact) => 
-      <Contact key={contact.id} {...contact}/>
+    {firstContacts.map((contact, index) => 
+      <Contact key={index} {...contact}/>
     )}
     </div>
     </div>
--- src/components/contact.js
+++ src/components/contact.js
@@ -4,11 +4,13 @@ const Contact = (props)  => {
     return (
         <div className="contact-div">
         <table>
+            <thead>
             <tr>
             <th>Picture</th>
             <th>Name</th>
             <th>Popularity</th>
             </tr>
+            </thead>
             <tr>
             <td><img src={props.pictureUrl} alt={props.name} width="150px"/></td>
             <td><h3>{props.name}</h3></td>
