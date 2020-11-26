75cae72365ecee64a6315afb0fc242206bfc1cdd
Ceyda Ulubas <ceyda.ulubas@gmail.com>
2020-11-26 09:37:16 +0300

    Iteration 1

--- src/App.js
+++ src/App.js
@@ -1,26 +1,15 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState} from 'react'
 import './App.css';
+import contacts from './contacts.json'
 
-function App() {
+const App = () => {
+  const fiveContacts = contacts.slice(0,5);
+  const [contactsState, setcontactsState] =useState (fiveContacts);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
 
-export default App;
+  )
+
+};
+
+export default App
