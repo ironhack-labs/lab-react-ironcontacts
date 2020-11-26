76e629ee8b1f78ee0c91b351a0adaae8350d9206
Maya Ausset <mayaausset@gmail.com>
2020-11-26 20:12:02 +0100

    none of the logic works, getting error for rendering on localhost

--- src/App.js
+++ src/App.js
@@ -11,8 +11,24 @@ function App() {
     const contactsCopy = [...contacts];
     const randomContact = contactsSeed[Math.floor(Math.random() * contactsSeed.length)];
     const newRandom = contactsCopy.push(randomContact);
-    console.log(`RANDOM CONTACT IS ${randomContact}, contact is ${contacts}  NEW RANDOM IS ${newRandom}`);
+    //console.log(`RANDOM CONTACT IS ${randomContact}, contact is ${contacts}  NEW RANDOM IS ${newRandom}`);
     //! does not work... // -- > setContacts(firstContacts, newRandom);
+  };
+
+  const sortByName = () => {
+    const contactsCopy = [...contacts];
+    const sortedContacts = contactsCopy.sort((a, b) => {
+    return b.name-a.name } );
+    //console.log(`SORTED CONTACT ${sortedContacts}`);
+    //! does not work... // -- > setContacts(sortedContacts);
+  };
+
+  const sortByPopularity = () => {
+    const contactsCopy = [...contacts];
+    const sortedContactsPop = contactsCopy.sort((a, b) => {
+    return b.popularity-a.popularity } );
+    //console.log(`SORTED Popularity ${sortedContactsPop}`);
+    //! does not work... // -- > setContacts(sortedContactsPop);
   };
 
   return (
@@ -20,6 +36,8 @@ function App() {
     <h1 className="title">IronContacts</h1>
     <div>
     <button onClick={addRandomContact()} className="random-btn">Add a Random Contact</button>
+    <button onClick={sortByName()} className="random-btn">Sort By Name</button>
+    <button onClick={sortByPopularity()} className="random-btn">Sort By Popularity</button> 
     </div>
     <div className="contact-div">
     {firstContacts.map((contact, index) => 
