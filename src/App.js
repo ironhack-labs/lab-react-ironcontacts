f41801ee36bb16f3da96541a680039d115cb9fdd
Maya Ausset <mayaausset@gmail.com>
2020-11-27 14:24:51 +0100

    .

--- src/App.js
+++ src/App.js
@@ -1,43 +1,50 @@
-import React, { useState } from 'react';
+import React from 'react';
+//import React, { useState } from 'react';
 import './App.css';
 import contactsSeed from './contacts.json';
-import Contact from './components/contact'
+import Contact from './components/contact';
+const firstContacts = contactsSeed.slice(0,5);
 
 function App() {
-  const firstContacts = contactsSeed.slice(0,5);
-  const [contacts, setContacts] = useState(firstContacts);
+  
+  //const [contacts, setContacts] = useState(firstContacts);
 
-  const addRandomContact = () => {
+  /* const addRandomContact = () => {
     const contactsCopy = [...contacts];
-    const randomContact = contactsSeed[Math.floor(Math.random() * contactsSeed.length)];
+    let randomContact = "";
+
+  do {
+    randomContact = contactsSeed[Math.floor(Math.random() * contactsSeed.length)]
+  }while (contacts.some((actor) => actor.id === randomContact.id))
+
     const newRandom = contactsCopy.push(randomContact);
     //console.log(`RANDOM CONTACT IS ${randomContact}, contact is ${contacts}  NEW RANDOM IS ${newRandom}`);
-    //! does not work... // -- > setContacts(firstContacts, newRandom);
-  };
+    //! does not work ... --> // setContacts(contactsCopy, newRandom);
+  }; */
 
-  const sortByName = () => {
+ /*  const sortByName = () => {
     const contactsCopy = [...contacts];
     const sortedContacts = contactsCopy.sort((a, b) => {
     return b.name-a.name } );
     //console.log(`SORTED CONTACT ${sortedContacts}`);
-    //! does not work... // -- > setContacts(sortedContacts);
+    //! does not work ... --> // setContacts(sortedContacts);
   };
-
-  const sortByPopularity = () => {
+ */
+/*   const sortByPopularity = () => {
     const contactsCopy = [...contacts];
     const sortedContactsPop = contactsCopy.sort((a, b) => {
     return b.popularity-a.popularity } );
     //console.log(`SORTED Popularity ${sortedContactsPop}`);
     //! does not work... // -- > setContacts(sortedContactsPop);
-  };
+  }; */
 
   return (
     <div className="App">
     <h1 className="title">IronContacts</h1>
     <div>
-    <button onClick={addRandomContact()} className="random-btn">Add a Random Contact</button>
+    {/* <button onClick={addRandomContact()} className="random-btn">Add a Random Contact</button> 
     <button onClick={sortByName()} className="random-btn">Sort By Name</button>
-    <button onClick={sortByPopularity()} className="random-btn">Sort By Popularity</button> 
+    <button onClick={sortByPopularity()} className="random-btn">Sort By Popularity</button>  */}
     </div>
     <div className="contact-div">
     {firstContacts.map((contact, index) => 
