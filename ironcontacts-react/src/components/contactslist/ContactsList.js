effb8adbaa9e2f785091953ad8705bed1a15efd1
Paricia Muñoz <patrimdm96@gmail.com>
2020-12-01 17:51:20 +0000

    [WEB-DEV DE] Patricia Muñoz de Dios

--- ironcontacts-react/src/components/contactslist/ContactsList.js
+++ ironcontacts-react/src/components/contactslist/ContactsList.js
@@ -7,28 +7,28 @@ class ContactsList extends Component {
   constructor() {
     super()
     this.state = {
-      movies: [contacts[0], contacts[1], contacts[2], contacts[3], contacts[4]]
+      contactList: [contacts[0], contacts[1], contacts[2], contacts[3], contacts[4]]
     }
   }
     
     addRandomContact = () => {
-        const newMoviesArr = [...this.state.movies]
-        const idx = Math.floor(Math.random() * (contacts.length - this.state.movies.length) + this.state.movies.length);
-        newMoviesArr.push(contacts[idx])
-        this.setState({ movies: newMoviesArr })
+        const newContactList = [...this.state.contactList]
+        const idx = Math.floor(Math.random() * (contacts.length - this.state.contactList.length) + this.state.contactList.length);
+        newContactList.push(contacts[idx])
+        this.setState({ contactList: newContactList })
     }
 
     sortName = () => {
-        const sortedArr = this.state.movies.sort((a, b) => (a.name > b.name) ? 1 : -1)
-        this.setState({ movies: sortedArr })
+        const sortedList = this.state.contactList.sort((a, b) => (a.name > b.name) ? 1 : -1)
+        this.setState({ contactList: sortedList })
     }
     
     sortPopularity = () => {
-        const sortedArr = this.state.movies.sort((a, b) => (a.popularity > b.popularity) ? -1 : 1)
-        this.setState({ movies: sortedArr })
+        const sortedList = this.state.contactList.sort((a, b) => (a.popularity > b.popularity) ? -1 : 1)
+        this.setState({ contactList: sortedList })
     }
 
-    deleteContact = elmId => this.setState({movies: this.state.movies.filter(elm => elm.id != elmId)})
+    deleteContact = elmId => this.setState({contactList: this.state.contactList.filter(elm => elm.id != elmId)})
     
   render() {
     return (
@@ -41,7 +41,7 @@ class ContactsList extends Component {
             <table>
                 <TableHead head1="Picture" head2="Name" head3="Popularity" head4="Action" />
                 <tbody>
-                    {this.state.movies.map(elm => <TableRow key={elm.id} deleteContact={() => this.deleteContact(elm.id)} {...elm} />)}
+                    {this.state.contactList.map(elm => <TableRow key={elm.id} deleteContact={() => this.deleteContact(elm.id)} {...elm} />)}
                 </tbody>
             </table>
         </section>
