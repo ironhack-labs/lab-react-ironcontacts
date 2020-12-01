7f55edf5834c22919759d3e6ec86ab142f353601
sarastackhouse <sarapalacios7@gmail.com>
2020-12-01 17:10:21 +0100

    Iteration 2 done

--- src/App.js
+++ src/App.js
@@ -1,24 +1,38 @@
 import React from 'react';
 // import logo from './logo.svg';
-// import './App.css';
+import './App.css';
 import contacts from './contacts.json';
 
-const firstFiveContactsArr = contacts.slice(1, 6);
-
-const roundedPopularity=(_number)=>Math.round((_number)*100)/100
-
-firstFiveContactsArr.forEach(roundedPopularity)
 
 class App extends React.Component {
   state = {
-    contacts: firstFiveContactsArr
+    contacts: contacts.slice(0, 5),
+  };
+
+
+  sliceArr = (_contacts) => {
+    return _contacts.slice(0, 5);
+  };
+
+  roundedPopularity = (_number) => {
+   return Math.round(_number * 100) / 100;
+  }
+
+
+  newRandomContacts = (_contacts) => {
+    _contacts.sort(() => {
+      return 0.5 - Math.random();
+    });
+    this.setState({ contacts: this.sliceArr(contacts) });
   };
 
   render() {
-   
     return (
       <div className="App">
         <h1>IronContacts</h1>
+        <button onClick={() => this.newRandomContacts(contacts)}>
+          Add Random Contact
+        </button>
         <tr>
           <td>Picture</td>
           <td>Name</td>
@@ -29,41 +43,39 @@ class App extends React.Component {
             <img src={contacts[0].pictureUrl} alt={contacts[0].name} />
           </td>
           <td>{contacts[0].name}</td>
-          <td>{roundedPopularity(contacts[0].popularity)}</td>
+          <td>{this.roundedPopularity(contacts[0].popularity)}</td>
         </tr>
         <tr>
           <td>
             <img src={contacts[1].pictureUrl} alt={contacts[0].name} />
           </td>
           <td>{contacts[1].name}</td>
-          <td>{roundedPopularity(contacts[1].popularity)}</td>
+          <td>{this.roundedPopularity(contacts[1].popularity)}</td>
         </tr>
         <tr>
           <td>
             <img src={contacts[2].pictureUrl} alt={contacts[0].name} />
           </td>
           <td>{contacts[2].name}</td>
-          <td>{roundedPopularity(contacts[2].popularity)}</td>
+          <td>{this.roundedPopularity(contacts[2].popularity)}</td>
         </tr>
         <tr>
           <td>
             <img src={contacts[3].pictureUrl} alt={contacts[0].name} />
           </td>
           <td>{contacts[3].name}</td>
-          <td>{roundedPopularity(contacts[3].popularity)}</td>
+          <td>{this.roundedPopularity(contacts[3].popularity)}</td>
         </tr>
         <tr>
           <td>
             <img src={contacts[4].pictureUrl} alt={contacts[0].name} />
           </td>
           <td>{contacts[4].name}</td>
-          <td>${roundedPopularity(contacts[4].popularity)}</td>
+          <td>{this.roundedPopularity(contacts[4].popularity)}</td>
         </tr>
       </div>
     );
   }
 }
-
-
 
 export default App;
