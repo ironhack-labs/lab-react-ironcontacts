db09a0533259de5b8fc398e7f7718c4e662167fd
Emma Craig <emma.ryal@gmail.com>
2020-12-01 20:00:35 +0100

    1 and 2 done plus the bones of 3 and 4

--- src/App.js
+++ src/App.js
@@ -1,26 +1,83 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import contacts from './contacts.json';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  state = {
+    contacts: contacts.slice(0,5)
+  };
+handleRandom = () => {
+const randomIndex = Math.floor((contacts.length) * (Math.random()));
+const randomContact = contacts[randomIndex];
+this.setState ({contacts: [...this.state.contacts, randomContact]});
+return 
+}
+
+handleSortByPop = () => {
+  const sortedPop = this.state.contacts.sort(function(x, y){
+    return x-y;
+  })
+  this.setState ({contacts: [...sortedPop]})
+
+}
+
+handleSortByName = () => {
+  const sortedName = [...this.state.contacts].sort();
+  console.log("sorted name array: ",sortedName);
+  this.setState ({contacts: [...sortedName]})
+
+return 
+}
+
+handleDeleteContact = (id) => {
+  const filteredContacts = this.state.contacts.filter((contactObj) => {
+    if (contactObj.id !== id) { 
+      return true;
+    }
+    else {  
+      return false;
+    }
+  })
+
+  this.setState( { contacts: filteredContacts } )
+}
+
+
+
+
+
+
+  render() {
+    return (
+      <div className="App">
+        <button onClick = {this.handleRandom}>Add Random Contact</button>
+        <button onClick = {this.handleSortByPop}>Sort By Popularity</button>
+        <button onClick = {this.handleSortByName}>Sort By Name</button>
+        <table>
+          <tr>
+            <th>Picture</th>
+            <th>Name</th>
+            <th>Popularity</th>
+            <th>Action</th>
+          </tr>
+          {this.state.contacts.map((contactObj) => {
+            return (
+              <tr>
+                <td>
+                  <img src={contactObj.pictureUrl} alt="photo" />
+                </td>
+                <td>{contactObj.name}</td>
+                <td>{contactObj.popularity}</td>
+                <td><button onClick = {this.handleDeleteContact} id = {contactObj.id}>Delete</button></td>
+              </tr>
+            );
+          })}
+         
+        </table>
+      </div>
+    );
+  }
 }
 
 export default App;
