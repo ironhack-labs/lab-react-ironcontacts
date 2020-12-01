f23badf8dd71b43d59d7befa8c9f78c7a639ceab
Teresa Torrente <mttorrente.o@gmail.com>
2020-12-01 21:40:06 +0100

    done

---
+++ lab-react-ironcontacts/src/components/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import ContactList from './contacts/ContactsList'
+
+function App() {
+  return (
+    <main>
+      <nav className="navbar"><h1>IronContacts</h1></nav>
+       <ContactList/>
+    </main>
+  );
+}
+
+export default App;
---
+++ lab-react-ironcontacts/src/components/contacts/ContactsList.js
@@ -0,0 +1,86 @@
+import React, { Component } from 'react'
+import contactsList from '../../contacts.json'
+import ContactCard from './ContactCard'
+
+import './ContactList.css'
+
+class ContactList extends Component {
+
+    constructor() {
+        super()
+        this.state = {
+            contactsList: contactsList.slice(0, 5)
+        }
+    }
+
+
+    addRandomContact = () => {
+        const randomContact = Math.floor(Math.random() * contactsList.length)
+        this.state.contactsList.push(contactsList[randomContact])
+
+
+         this.setState({
+            contactsList: this.state.contactsList
+        })
+
+    }
+
+
+    sortByName = () => {
+        this.setState({
+            contactsList: this.state.contactsList.sort(function(a, b){
+               return a.name.localeCompare(b.name);
+                        })
+                    })
+
+    }
+
+
+    sortByPopularity = () => {
+        this.setState({
+            contactsList: this.state.contactsList.sort(function(a, b){
+               return b.popularity - a.popularity;
+                        })
+                    })
+
+    }
+    
+
+    removeContact = contactIdToDelete => {
+        this.setState({
+            contactsList: this.state.contactsList.filter(elm => elm.id !== contactIdToDelete)
+        })
+    }
+
+
+    render() {
+
+
+        return (
+            <section className="ironContacts">
+                <div className="btn">
+                <button onClick={this.addRandomContact}>Add Random Contact</button>
+                <button onClick={this.sortByName}>Sort by name</button>
+                <button onClick={this.sortByPopularity}>Sort by popularity</button>
+                    </div>
+              <table className="card">
+            <thead>
+                <tr>
+                    <th>Picture</th>
+                    <th>Name</th>
+                    <th>Popularity</th>
+                    <th>Action</th>
+
+                </tr>
+
+        </thead>
+        <tbody>
+                {this.state.contactsList.map(elm => <ContactCard key={elm.id} deleteContact={() => this.removeContact(elm.id)} name={elm.name} pictureUrl={elm.pictureUrl} popularity={elm.popularity} />)}
+                </tbody>
+                </table>
+                </section>
+        )
+    }
+}
+
+export default ContactList 
