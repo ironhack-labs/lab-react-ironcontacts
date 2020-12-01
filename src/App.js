fce4de5621f23cf3c43fab797279b9d207926450
sarastackhouse <sarapalacios7@gmail.com>
2020-12-01 21:00:07 +0100

    Done with styles

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
-// import logo from './logo.svg';
 import './App.css';
+import 'bulma/css/bulma.css';
 import contacts from './contacts.json';
 
 class App extends React.Component {
@@ -42,46 +42,61 @@ class App extends React.Component {
   render() {
     return (
       <div className="App">
-        <h1>IronContacts</h1>
-        <button onClick={() => this.newRandomContact()}>
-          Add Random Contact
-        </button>
-        <button onClick={() => this.sortByName()}>Sort by name</button>
-        <button onClick={() => this.sortByPopularity()}>
-          Sort by popularity
-        </button>
-        <table>
-          <thead>
-            <tr>
-              <th>Picture</th>
-              <th>Name</th>
-              <th>Popularity</th>
-              <th>Action</th>
-            </tr>
-          </thead>
-          <tbody>
-            {this.state.contacts.map((contact) => {
-              return (
-                <tr key={contact.id}>
-                  <td>
-                    <img src={contact.pictureUrl} alt={contact.name}></img>
-                  </td>
-                  <td>{contact.name}</td>
-                  <td>{contact.popularity.toFixed(2)}</td>
-                  <td>
-                    <button
-                      onClick={() => {
-                        this.deleteContact(contact.id);
-                      }}
-                    >
-                      Delete
-                    </button>
-                  </td>
-                </tr>
-              );
-            })}
-          </tbody>
-        </table>
+        <div class="title">
+          <h1>IronContacts</h1>
+        </div>
+        <div class="buttons">
+          <button
+            class="button is-dark"
+            onClick={() => this.newRandomContact()}
+          >
+            Add Random Contact
+          </button>
+          <button class="button is-dark" onClick={() => this.sortByName()}>
+            Sort by name
+          </button>
+          <button
+            class="button is-dark"
+            onClick={() => this.sortByPopularity()}
+          >
+            Sort by popularity
+          </button>
+        </div>
+        <div class="table">
+          <table>
+            <thead>
+              <tr>
+                <th>Picture</th>
+                <th>Name</th>
+                <th>Popularity</th>
+                <th>Action</th>
+              </tr>
+            </thead>
+            <tbody>
+              {this.state.contacts.map((contact) => {
+                return (
+                  <tr key={contact.id}>
+                    <td>
+                      <img src={contact.pictureUrl} alt={contact.name}></img>
+                    </td>
+                    <td>{contact.name}</td>
+                    <td>{contact.popularity.toFixed(2)}</td>
+                    <td>
+                      <button
+                        class="button is-rounded button is-small button is-black"
+                        onClick={() => {
+                          this.deleteContact(contact.id);
+                        }}
+                      >
+                        Delete
+                      </button>
+                    </td>
+                  </tr>
+                );
+              })}
+            </tbody>
+          </table>
+        </div>
       </div>
     );
   }
