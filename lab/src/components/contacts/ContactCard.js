8dc33f81077bbb54a6c584174fef26594de146bb
Isabel Perez <isabel.perez.ruiz25@gmail.com>
2020-12-01 20:07:14 +0100

    done

---
+++ lab/src/components/contacts/ContactCard.js
@@ -0,0 +1,21 @@
+import React from 'react'
+
+import './ContactCard.css'
+
+const ContactCard = ({name ,pictureUrl, popularity, deleteContact}) => {
+
+
+    return (
+   
+            <tr className="contact">
+                <td><img src={pictureUrl} className="picture" alt="contact "/></td>
+                <td>{name}</td>
+            <td>{popularity}</td>
+            <td><button onClick={deleteContact} className="delete-btn">Delete contact</button></td>
+
+            </tr>  
+        
+    )
+}
+
+export default ContactCard
