acd375cc32b95d480560edc4a3daf97b6e3f35a4
Juan Miguel Sansinenea <juanmi.sansinenea@gmail.com>
2020-12-01 22:09:58 +0100

    map list iteration limited to 5 actors now

--- src/App.js
+++ src/App.js
@@ -1,34 +1,12 @@
 import React from 'react';
-//import logo from './logo.svg';
 import './App.css';
 import contacts from './contacts.json';
+let contactsArr = [{}];
+for (let i=0; i<5; i++){
+  contactsArr[i] = ({pictureUrl: contacts[i].pictureUrl, name: contacts[i].name, popularity: contacts[i].popularity} )
+  console.log("contactsArr", contactsArr)
+}
 
-// function App() {
-//   return (
-//     <div className="App">
-//       <header className="App-header">
-//         <img src={logo} className="App-logo" alt="logo" />
-//         <p>
-//           Edit <code>src/App.js</code> and save to reload.
-//         </p>
-//         <a
-//           className="App-link"
-//           href="https://reactjs.org"
-//           target="_blank"
-//           rel="noopener noreferrer"
-//         >
-//           Learn React
-//         </a>
-//       </header>
-//       <table>
-//         <th>Picture</th><th>Name</th><th>Popularity</th>
-//         <tr>
-//           <td></td>
-//         </tr>
-//       </table>
-//     </div>
-//   );
-// }
 
 const App = () => {
   return (
@@ -41,14 +19,14 @@ const App = () => {
         </tr>
       </thead>
       <tbody>
-        {contacts.map((el, i) => {
+        {contactsArr.map((el, i) => {
           return (
-            <tr key={contacts[i].id}>
+            <tr key={contactsArr[i].id}>
               <td>
-                <img src={contacts[i].pictureUrl} />
+                <img src={contactsArr[i].pictureUrl} />
               </td>
-              <td>{contacts[i].name}</td>
-              <td>{contacts[i].popularity}</td>
+              <td>{contactsArr[i].name}</td>
+              <td>{contactsArr[i].popularity}</td>
             </tr>
           );
         })}
