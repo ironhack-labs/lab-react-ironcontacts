0907c0ca4dca2ee122a0ad22150b7fc7b459e38c
Christian Ares <christianaresvillan@gmail.com>
2020-12-01 13:39:04 +0100

    Done!

--- src/App.js
+++ src/App.js
@@ -1,26 +1,86 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import './App.css';
+import contacts from './contacts.json';
+
+class App extends React.Component {
+  
+  
+    state = {
+      contactsList: contacts.slice(0, 5)
+  }
+
+  randomContact = ()=>{
+    const randomIndex = Math.floor(Math.random()*(contacts.length))
+    const newContact = contacts[randomIndex]
+    const copyOfContactsList = [...this.state.contactsList]
+    copyOfContactsList.unshift(newContact)
+    this.setState({contactsList: copyOfContactsList})
+
+  }
+
+  sortByNameContact = ()=>{
+
+    const copyOfContactsList = [...this.state.contactsList]
+    copyOfContactsList.sort((item1, item2)=>{
+      if (item1.name > item2.name) {
+        return 1;
+      } else if (item1.name < item2.name) {
+        return -1;
+      } 
+      return 0;
+    })
+
+    this.setState({contactsList: copyOfContactsList})
+  }
+
+  sortByPopularity = () => {
+    const copyOfContactsList = [...this.state.contactsList]
+    copyOfContactsList.sort((item1, item2)=>{
+      if (item1.popularity > item2.popularity) { return -1; }
+      else if (item1.popularity < item2.popularity) { return 1; }
+      else if (item1.popularity === item2.popularity) { return 0; } 
+    })
+    this.setState({contactsList: copyOfContactsList})
+  }
+
+  deleteContact = (contactId) => {
+    const copyOfContactsList = [...this.state.contactsList]
+    const deletedContact = copyOfContactsList.filter((item)=>{
+      return item.id !== contactId
+    })
+
+    this.setState({contactsList: deletedContact})
+  }
+
+  render(){
+      return (
+      <div className="App">
+        <h1>Iron Contacts</h1>
+        <button onClick={this.randomContact}>Add Random Contact</button>
+        <button onClick={this.sortByNameContact}>Sort by name</button>
+        <button onClick={this.sortByPopularity}>Sort by popularity</button>
+        <table> 
+          <tr>
+            <th>Picture</th>
+            <th>Name</th>
+            <th>Popularity</th>
+            <th>Action</th>
+          </tr>
+          {this.state.contactsList.map((contact)=>{
+            return(
+              <tr key={contact.id}>
+                <td> <img src={contact.pictureUrl}/> </td>
+                <td>{contact.name}</td>
+                <td>{contact.popularity.toFixed(2)}</td>
+                <td><button onClick={()=>this.deleteContact(contact.id)}>Delete</button></td>
+              </tr>
+              )
+          })}
+        </table>
+      </div>
+    ); 
+  }
 }
 
 export default App;
