9854e1c37333b19a3f4b817e4f9e732bff9664b5
Facundo Gomez <gomezvera.f@gmail.com>
2020-12-02 13:00:39 +0100

    all exercises except bonus done

--- src/App.js
+++ src/App.js
@@ -1,26 +1,91 @@
-import React from 'react';
+import React, {Component} from 'react';
 import logo from './logo.svg';
 import './App.css';
+import contacts from './contacts.json'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+export default class App extends Component {
+
+  state = {
+    list: contacts.slice(0,5)
+  }
+  
+  randomHandler = (event) => {
+    console.log('click 1');
+    console.log(this.state.list, this.state.limit);
+
+    const randNumber = () => {
+      let rand = Math.round(Math.random()*contacts.length)
+      return rand
+    }
+
+    
+    let number = randNumber()
+    do {
+      number = randNumber()
+    }
+    while (this.state.list.includes(contacts[number]));
+    let newVal = contacts[number]
+    let newArr = [...this.state.list,newVal]
+    this.setState({list: newArr})
+
+  }
+  sortByPopularity = () => {
+    let sortedList = [...this.state.list].sort((a,b) => {
+      return b.popularity-a.popularity
+    })
+    // console.log(sortedList)
+    this.setState({list: sortedList})
+  }
+
+  sortByName = () => {
+    let sortedList = [...this.state.list].sort((a,b) => {
+      return a.name.localeCompare(b.name)
+    })
+    // console.log(sortedList)
+    this.setState({list: sortedList})
+  }
+
+  deleteOne = (event) => {
+    console.log(event)
+    let id = event
+    const newList = [...this.state.list]
+    let solution = newList.filter((item) => item.id !== id)
+    this.setState({list: solution})
+  }
+
+
+  render() {
+    return (
+      <div className="App">
+        <button onClick={this.randomHandler}>Add Random Contact </button>
+        <button onClick={this.sortByName}>Sort by name</button>
+        <button onClick={this.sortByPopularity}>Sort by popularity</button>
+          <table>
+          <thead>
+            <tr>
+             <th>Picture</th>
+             <th>Name</th>
+            <th>Popularity</th>
+            <th>Action</th>
+            </tr>
+          </thead>
+          <tbody>
+          {this.state.list.map(contact => {
+            return (
+              <tr key={contact.id}>
+              <td><img src={contact.pictureUrl}></img></td>
+              <td>{contact.name}</td>
+              <td>{contact.popularity}</td>
+              <td><button id={contact.id}onClick={() => this.deleteOne(contact.id)}>Delete</button></td>
+              </tr>
+            )
+          })}
+          </tbody>
+          </table>
+          
+      </div>
+    );
+  }
 }
 
-export default App;
