1855a456f7ac0b78a36674ebbd6ba09d4f8d991c
Marcel Niebylski <mnblski@gmail.com>
2020-12-02 17:39:23 +0000

    It 1 -4 done - an error appears, need to ask on padlet

--- src/App.js
+++ src/App.js
@@ -1,26 +1,84 @@
-import React from 'react';
+import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import contacts from './contacts.json';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Table />
     </div>
   );
+}
+
+class Table extends Component {
+  state = {
+    allContacts: contacts.slice(0, 5),
+  };
+
+  addContact = () => {
+    let randomContact = contacts[Math.floor(Math.random() * contacts.length)];
+    let updatedContactsAdd = this.state.allContacts.push(randomContact);
+    this.setState({ allContacts: updatedContactsAdd });
+  };
+
+  removeContact = () => {
+    let updatedContactsRemove = this.state.allContacts.pop();
+    this.setState({ allContacts: updatedContactsRemove });
+  };
+
+  sortByName = () => {
+    this.setState({
+      allContacts: this.state.allContacts.sort((a, b) =>
+        a.name > b.name ? 1 : b.name > a.name ? -1 : 0
+      ),
+    });
+  };
+
+  sortByPopularity = () => {
+    this.setState({
+      allContacts: this.state.allContacts.sort((a, b) =>
+        a.popularity < b.popularity ? 1 : b.popularity < a.popularity ? -1 : 0
+      ),
+    });
+  };
+
+  render() {
+    return (
+      <div className="contacts">
+        <div>
+          <h1>IronContacts</h1>
+          <button onClick={this.addContact}>Add Random contact</button>
+          <button onClick={this.sortByName}>Sort By Name</button>
+          <button onClick={this.sortByPopularity}>Sort By Popularity</button>
+          <button onClick={this.removeContact}>Remove Contact</button>
+        </div>
+
+        <table>
+          <thead>
+            <tr>
+              <th>Picture</th>
+              <th>Name</th>
+              <th>Popularity</th>
+            </tr>
+          </thead>
+          <tbody>
+            {this.state.allContacts.map((contact) => {
+              return (
+                <tr key={contact.name} className="contacts2">
+                  <td>
+                    <img className="profPic" src={contact.pictureUrl} />
+                  </td>
+                  <td>{contact.name}</td>
+                  <td>{contact.popularity}</td>
+                </tr>
+              );
+            })}
+          </tbody>
+        </table>
+      </div>
+    );
+  }
 }
 
 export default App;
