30ad0e53c85dee855a329b9dde358325516b6e96
ararita <munjizavesna@gmail.com>
2020-12-02 11:43:52 +0100

    Iteration 1 done

--- src/App.js
+++ src/App.js
@@ -1,26 +1,57 @@
-import React from 'react';
+import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import contacts from './contacts.json';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  state = {
+    contacts: contacts.slice(0, 5),
+  };
+
+  // displayContacts = () => {
+  //   const showContacts = this.state.contacts.map((contact) => {
+  //     console.log('contact', contact);
+  //     console.log('id', contact.id);
+
+  //     return <tr key={contact.id}></tr>;
+  //   });
+  // };
+
+  render() {
+    console.log(this.state.contacts);
+    return (
+      <>
+        <h1>IronContacts</h1>
+
+        <div className="table-wrap">
+          <table>
+            <thead>
+              <tr>
+                <th>Picture</th>
+                <th>Name</th>
+                <th>Popularity</th>
+              </tr>
+            </thead>
+            <tbody>
+              {this.state.contacts.map((contact) => (
+                <tr key={contact.id}>
+                  <td>
+                    <img src={contact.pictureUrl} width="80px"></img>
+                  </td>
+                  <td>
+                    <p>{contact.name}</p>
+                  </td>
+                  <td>
+                    <p>{contact.popularity.toFixed(2)}</p>
+                  </td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+        </div>
+      </>
+    );
+  }
 }
 
 export default App;
