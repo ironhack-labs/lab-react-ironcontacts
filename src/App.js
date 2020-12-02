ff3d71c3a684968a0622e9de48e05d6908c6e53a
Jan <jan.leon.voelker@gmail.com>
2020-12-02 12:21:13 +0100

    iteration 1 done

--- src/App.js
+++ src/App.js
@@ -1,26 +1,54 @@
-import React from 'react';
+import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import contacts from './contacts.json';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default class App extends Component {
+  state = {
+    contactList: contacts.slice(0, 5),
+  };
+  // state = {
+  //   counter: 0,
+  //   picture: this.props.imgs[0],
+  // };
+  // nextPicture = () => {
+  //   if (this.state.counter > 3) {
+  //     this.setState({
+  //       counter: this.state.counter - 4,
+  //     });
+  //   }
+  //   this.setState({
+  //     counter: this.state.counter + 1,
+  //     picture: this.props.imgs[this.state.counter],
+  //   });
+  // };
+  render() {
+    console.log(this.state.contactList);
+    return (
+      <div className="App">
+        <table>
+          <thead>
+            <tr>
+              <th>Picture</th>
+              <th>Name</th>
+              <th>Popularity</th>
+            </tr>
+          </thead>
+          <tbody>
+            {this.state.contactList.map((contact) => {
+              return (
+                <tr key={contact.id}>
+                  <td>
+                    <img src={contact.pictureUrl} height="120" alt="" />
+                  </td>
+                  <td>{contact.name}</td>
+                  <td>{contact.popularity}</td>
+                </tr>
+              );
+            })}
+          </tbody>
+        </table>
+      </div>
+    );
+  }
 }
-
-export default App;
