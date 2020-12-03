fee7c13666c12d797122d9d2f8887e87182f0b43
Marcel Niebylski <mnblski@gmail.com>
2020-12-03 09:02:38 +0000

    Bugs fixed, all done

--- src/App.js
+++ src/App.js
@@ -18,28 +18,44 @@ class Table extends Component {
 
   addContact = () => {
     let randomContact = contacts[Math.floor(Math.random() * contacts.length)];
-    let updatedContactsAdd = this.state.allContacts.push(randomContact);
+
+    const updatedContactsAdd = [...this.state.allContacts, randomContact];
+
     this.setState({ allContacts: updatedContactsAdd });
   };
 
-  removeContact = () => {
-    let updatedContactsRemove = this.state.allContacts.pop();
-    this.setState({ allContacts: updatedContactsRemove });
+  removeContact = (name) => {
+    const copyOfContacts = [...this.state.allContacts];
+
+    const theActorIndex = copyOfContacts.findIndex(
+      (actor) => actor.name === name
+    );
+
+    copyOfContacts.splice(theActorIndex, 1);
+
+    this.setState({ allContacts: copyOfContacts });
   };
 
   sortByName = () => {
+    const copyOfContacts = [...this.state.allContacts];
+
+    const sortedByName = copyOfContacts.sort((a, b) =>
+      a.name > b.name ? 1 : b.name > a.name ? -1 : 0
+    );
     this.setState({
-      allContacts: this.state.allContacts.sort((a, b) =>
-        a.name > b.name ? 1 : b.name > a.name ? -1 : 0
-      ),
+      allContacts: sortedByName,
     });
   };
 
   sortByPopularity = () => {
+    const copyOfContacts = [...this.state.allContacts];
+
+    const sortedByPop = copyOfContacts.sort((a, b) =>
+      a.popularity < b.popularity ? 1 : b.popularity < a.popularity ? -1 : 0
+    );
+
     this.setState({
-      allContacts: this.state.allContacts.sort((a, b) =>
-        a.popularity < b.popularity ? 1 : b.popularity < a.popularity ? -1 : 0
-      ),
+      allContacts: sortedByPop,
     });
   };
 
@@ -51,7 +67,7 @@ class Table extends Component {
           <button onClick={this.addContact}>Add Random contact</button>
           <button onClick={this.sortByName}>Sort By Name</button>
           <button onClick={this.sortByPopularity}>Sort By Popularity</button>
-          <button onClick={this.removeContact}>Remove Contact</button>
+          {/* <button onClick={this.removeContact}>Remove Contact</button> */}
         </div>
 
         <table>
@@ -65,7 +81,11 @@ class Table extends Component {
           <tbody>
             {this.state.allContacts.map((contact) => {
               return (
-                <tr key={contact.name} className="contacts2">
+                <tr
+                  key={contact.name}
+                  onClick={() => this.removeContact(contact.name)}
+                  className="contacts2"
+                >
                   <td>
                     <img className="profPic" src={contact.pictureUrl} />
                   </td>
