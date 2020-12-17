931e3728dc2254e29b02e4ef33a8d7581f90e3b6
srtagalicia <srtagalicia@gmail.com>
2020-12-17 17:50:06 +0100

    lab done

--- src/components/Card.js
+++ src/components/Card.js
@@ -6,20 +6,18 @@ import DeleteButton from './DeleteButton';
 function Card(props) {
     const { name, pictureUrl, popularity, id } = props.contact;
 
-    
-
     return (
         <div className="card-div">
             <div className="delete-button-div">
-                <DeleteButton handleClick={this.deleteContact} />
+                <DeleteButton handleClick={() => {props.deleteContact(id)}} />
             </div>
             
             <tr id={id}>
                 <td><img src={pictureUrl} alt="contact img" width="100" height="150" /></td>
                 <td>{name}</td>
-            <td>{popularity}</td>
+                <td>{popularity}</td>
             
-        </tr>
+            </tr>
 
         </div>
         
