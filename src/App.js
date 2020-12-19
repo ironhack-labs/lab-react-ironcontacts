2881c223a2733fa9eea5e6e0b7cd5ce7e45618b7
Alicja Slupska <ala.slupska@gmail.com>
2020-12-19 17:17:33 +0100

    iterations 1-4 done

--- src/App.js
+++ src/App.js
@@ -1,13 +1,87 @@
 import React from 'react';
 import './App.css';
+import contacts from './contacts.json';
+
 
 class App extends React.Component {
+  state = {
+    contacts: contacts.slice(0, 5)
+  }
+
+
+  clickHandler = () => {
+
+    let randomContact = contacts[Math.floor(Math.random() * contacts.length)];
+    const newContacts = this.state.contacts.concat(randomContact)
+
+    this.setState({
+      contacts: newContacts
+    })
+  }
+
+  sortByNameHandler = () => {
+    const sortedContacts = [...this.state.contacts].sort((a, b) => (a.name > b.name) ? 1 : -1)
+    this.setState({
+      contacts: sortedContacts
+    })
+  }
+
+  sortByPopHandler = () => {
+    const sortedContacts = [...this.state.contacts].sort((a, b) => (a.popularity < b.popularity) ? 1 : -1)
+    this.setState({
+      contacts: sortedContacts
+    })
+  }
+
+
+  deleteHandler = (id) => {
+
+    this.setState({
+      contacts: this.state.contacts.filter(contact => contact.id !== id)
+    })
+  }
+
+
+
   render() {
+    
+    const mappingContacts = (contact) => {
+      return (
+        <tr key={contact.id}>
+          <td><img src={contact.pictureUrl}></img></td>
+          <td>{contact.name}</td>
+          <td>{contact.popularity}</td>
+          <td><button onClick={() => this.deleteHandler(contact.id)}>Delete</button></td>
+        </tr>
+      )
+    }
+
+    const myContacts = this.state.contacts.map(mappingContacts)
+
     return (
-      <div className="App" >
+      <div>
+        <h1>Iron Contacts</h1>
+
+        <button onClick={this.clickHandler}>Add Random Contact</button>
+        <button onClick={this.sortByNameHandler}>Sort by Name</button>
+        <button onClick={this.sortByPopHandler}>Sort by Popularity</button>
+
+        <table>
+          <tr>
+            <th>
+              <b>Picture</b>
+            </th>
+            <th>
+              <b>Name</b>
+            </th>
+            <th>
+              <b>Popularity</b>
+            </th>
+          </tr>
+          {myContacts}
+        </table>
       </div>
     );
   }
 }
-
 export default App;
