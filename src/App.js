837aa22629946e994a733d8825c4ef164388bcb9
Alicja Slupska <ala.slupska@gmail.com>
2020-12-19 18:59:22 +0100

    basic styles added

--- src/App.js
+++ src/App.js
@@ -6,82 +6,92 @@ import contacts from './contacts.json';
 class App extends React.Component {
   state = {
     contacts: contacts.slice(0, 5)
-  }
+  };
 
 
   clickHandler = () => {
 
-    let randomContact = contacts[Math.floor(Math.random() * contacts.length)];
-    const newContacts = this.state.contacts.concat(randomContact)
+    const randomContact = contacts[Math.floor(Math.random() * contacts.length)];
+    const newContacts = this.state.contacts.concat(randomContact);
 
     this.setState({
       contacts: newContacts
-    })
-  }
+    });
+  };
 
   sortByNameHandler = () => {
-    const sortedContacts = [...this.state.contacts].sort((a, b) => (a.name > b.name) ? 1 : -1)
+    const sortedContacts = [...this.state.contacts].sort((a, b) => (a.name > b.name) ? 1 : -1);
     this.setState({
       contacts: sortedContacts
-    })
-  }
+    });
+  };
 
   sortByPopHandler = () => {
-    const sortedContacts = [...this.state.contacts].sort((a, b) => (a.popularity < b.popularity) ? 1 : -1)
+    const sortedContacts = [...this.state.contacts].sort((a, b) => (a.popularity < b.popularity) ? 1 : -1);
     this.setState({
       contacts: sortedContacts
-    })
-  }
+    });
+  };
 
 
   deleteHandler = (id) => {
 
     this.setState({
       contacts: this.state.contacts.filter(contact => contact.id !== id)
-    })
-  }
+    });
+  };
 
 
 
   render() {
-    
+
     const mappingContacts = (contact) => {
       return (
         <tr key={contact.id}>
-          <td><img src={contact.pictureUrl}></img></td>
+          <td><img src={contact.pictureUrl} alt="celebrity"></img></td>
           <td>{contact.name}</td>
           <td>{contact.popularity}</td>
-          <td><button onClick={() => this.deleteHandler(contact.id)}>Delete</button></td>
+          <td id="last-table-data"><button onClick={() => this.deleteHandler(contact.id)} className="btn btn-danger">Delete</button></td>
         </tr>
-      )
-    }
+      );
+    };
 
-    const myContacts = this.state.contacts.map(mappingContacts)
+    const myContacts = this.state.contacts.map(mappingContacts);
 
     return (
-      <div>
-        <h1>Iron Contacts</h1>
+      <div className="main-container">
 
-        <button onClick={this.clickHandler}>Add Random Contact</button>
-        <button onClick={this.sortByNameHandler}>Sort by Name</button>
-        <button onClick={this.sortByPopHandler}>Sort by Popularity</button>
+        <h1>Popular Celebrities</h1>
 
-        <table>
-          <tr>
-            <th>
-              <b>Picture</b>
-            </th>
-            <th>
-              <b>Name</b>
-            </th>
-            <th>
-              <b>Popularity</b>
-            </th>
-          </tr>
-          {myContacts}
-        </table>
+        <div className="buttons-container">
+          <button onClick={this.clickHandler} className="btn btn-primary">Add Random Contact</button>
+          <button onClick={this.sortByNameHandler} className="btn btn-secondary">Sort by Name</button>
+          <button onClick={this.sortByPopHandler} className="btn btn-light">Sort by Popularity</button>
+        </div>
+
+        <div>
+          <table>
+            <thead>
+              <tr>
+                <th>
+                  <h4>Picture</h4>
+                </th>
+                <th>
+                  <h4>Name</h4>
+                </th>
+                <th>
+                  <h4>Popularity</h4>
+                </th>
+              </tr>
+            </thead>
+            <tbody>
+              {myContacts}
+            </tbody>
+          </table>
+        </div>
       </div>
     );
-  }
+  };
 }
+
 export default App;
