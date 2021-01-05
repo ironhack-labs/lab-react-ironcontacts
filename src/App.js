825a02f2341c0e720ae41e2eaab61337ed1b129d
Edouard Pfauwadel <edouard.pfauwadel@gmail.com>
2021-01-05 16:51:28 +0100

    done no styling yet

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ class App extends React.Component {
     contacts: contacts.slice(0, 5),
   };
 
-  addRandom = (contact, event) => {
+  addRandom = () => {
     const copy = [...this.state.contacts];
     const randomContact = contacts[Math.floor(Math.random() * contacts.length)];
     copy.push(randomContact);
@@ -18,21 +18,48 @@ class App extends React.Component {
     });
   }
 
+  sortName = () => {
+    const copy = [...this.state.contacts];
+    copy.sort((a, b) => (a.name > b.name) ? 1 : (a.name === b.name) - 1)
+
+    this.setState({
+      contacts: copy
+    })
+  }
+
+  sortPopularity = () => {
+    const copy = [...this.state.contacts];
+    copy.sort((a, b) => (a.popularity < b.popularity) ? 1 : (a.popularity === b.popularity) - 1)
+
+    this.setState({
+      contacts: copy
+    })
+  }
+
+  removeContact = (contact) => {
+    this.setState({
+      contacts: this.state.contacts.filter((p) => p.id !== contact.id)
+    })
+  }
 
   render() {
     return (
       <div className="App" >
         <h1> IRONCONTACTS </h1>
         <button onClick={this.addRandom}>Add random</button>
+        <button onClick={this.sortName}>Sort Names</button>
+        <button onClick={this.sortPopularity}>Sort Popularity</button>
         {this.state.contacts.map((contact => {
           return (
-            <div key={contact.id}>
+            <div key={`contacts ${contact.id}`}>
               <td>
                 <img src={contact.pictureUrl} />
               </td>
               <td> Name: {contact.name}</td>
-              <td> Popularity: {contact.popularity}</td>
+              <td> Popularity: {contact.popularity.toFixed(2)}</td>
+              <button onClick={() => this.removeContact(contact)}>delete</button>
             </div>
+
           )
         }))}
       </div >
