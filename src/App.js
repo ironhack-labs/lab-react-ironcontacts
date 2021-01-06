e67f8979894d614723d2c09495b474e3ac8b696a
Menhaj safi <minhaj.sahil01@gmail.com>
2021-01-06 08:53:39 +0100

    lab is done except style

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import React from 'react';
 import './App.css';
+import Contacts from './Contacts.js';
 
 class App extends React.Component {
   render() {
     return (
-      <div className="App" >
+      <div className="App">
+        <h1>IronContacts</h1>
+        <Contacts />
       </div>
     );
   }
 }
-
 export default App;
