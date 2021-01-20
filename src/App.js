623e97ea56caf2a323e08ed01c2274852cf1d413
Dirk Kelderman <dirkkelderman@gmail.com>
2021-01-20 14:47:29 +0100

    iteration 1

--- src/App.js
+++ src/App.js
@@ -1,13 +1,41 @@
 import React from 'react';
 import './App.css';
+import contacts from './contacts.json';
+
+
 
 class App extends React.Component {
+
+
   render() {
+
+    const movieContacts = contacts.slice(0,5).map(value => {
+      return(
+    
+        <tr>
+          <td><img src={value.pictureUrl}/></td>
+          <td>{value.name}</td>
+          <td>{Math.round(value.popularity * 100) / 100}</td>
+        </tr>
+
+      ) 
+    })
+
     return (
       <div className="App" >
+      <h1>IronContacts</h1>
+        <div className="Contacts">
+          <tr>
+            <th>Picture</th>
+            <th>Name</th>
+            <th>Popularity</th>
+          </tr>
+          {movieContacts}
+        </div>
       </div>
     );
   }
 }
 
 export default App;
+
