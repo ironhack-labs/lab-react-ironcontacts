6693a00b8078b0dd0ee87e97a1516261b53087c1
ZuzanaAK <kubatovazuzana@gmail.com>
2021-01-21 16:51:12 +0100

    iteration 3 done, again problem with how to place bottons properly

--- src/App.js
+++ src/App.js
@@ -9,51 +9,80 @@ class App extends React.Component {
  
   ButtonRandom = () => {
  
-      let clonedCeleb = [...this.state.celebrities]
+      let clonedCeleb1 = [...this.state.celebrities]
  
       let randomCel = contacts[Math.floor(Math.random()*contacts.length)]
-      clonedCeleb.push(randomCel)
+      clonedCeleb1.push(randomCel)
  
       this.setState({
-        celebrities: clonedCeleb
+        celebrities: clonedCeleb1
       })
   }
  
- 
- 
- 
+  SortedByName = () => {
+
+    let clonedCeleb2 = [...this.state.celebrities]
+
+    clonedCeleb2.sort((a,b) =>{
+      if (a.name<b.name){
+        return -1
+      } else if (a.name>b.name) {
+        return 1
+      } else {
+        return 0
+      }
+    })
+
+    this.setState({
+      celebrities: clonedCeleb2
+    })
+  }
+
+  SortedByPopularity = () => {
+
+    let clonedCeleb3 = [...this.state.celebrities]
+
+    clonedCeleb3.sort((a,b) =>{
+      return (b.popularity-a.popularity)
+    })
+
+    this.setState({
+      celebrities: clonedCeleb3
+    })
+  }
+
   render() {
     return (
- 
-      
-    
-    
+
+
     this.state.celebrities.map(elem => { 
       return(
-      <React.Fragment>
-        <button onClick={this.ButtonRandom}>Click here</button>
-      
-      <table>
-          <thead>
-            <tr>
-              <th>picture</th>
-              <th>name</th>
-              <th>popularity</th>
-            </tr>
-          </thead>
-        <tbody>
-          <tr>
-            <td>
-              <div className="TableImage">
-                <img src={elem.pictureUrl}/>
-              </div>
-            </td>
-            <td>{elem.name}</td>
-            <td>{elem.popularity}</td>
-          </tr>
-        </tbody>
-      </table>
-    </React.Fragment> 
+          <React.Fragment>
+              <button onClick={this.ButtonRandom}>Click here</button>
+              <button onClick={this.SortedByName}>SortByName</button> 
+              <button onClick={this.SortedByPopularity}>SortByPopularity</button>  
+            
+            <table>
+                <thead>
+                  <tr>
+                    <th>picture</th>
+                    <th>name</th>
+                    <th>popularity</th>
+                  </tr>
+                </thead>
+              <tbody>
+                <tr>
+                  <td>
+                    <div className="TableImage">
+                      <img src={elem.pictureUrl}/>
+                    </div>
+                  </td>
+                  <td>{elem.name}</td>
+                  <td>{elem.popularity}</td>
+                </tr>
+              </tbody>
+            </table>
+          </React.Fragment> 
       )
       })
     )     
