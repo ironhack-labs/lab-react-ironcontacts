d59adbc8326615535835011e13f362662a1eb583
ZuzanaAK <kubatovazuzana@gmail.com>
2021-01-22 14:24:28 +0100

    completely done

--- src/App.js
+++ src/App.js
@@ -61,61 +61,55 @@ class App extends React.Component {
     this.setState({
       celebrities: clonedCeleb4
     })
-
   }
 
- 
 
   render() {
     const UpdatedList = (elem) => {
       return (
-        <div className = "DeleteButton">
-          <button onClick={elem.clickToDelete}>Delete</button>
-        </div> 
-  
+        <div>
+          <table>
+            <tbody>
+              <tr>
+                <td>
+                  <div className="TableImage">
+                    <img src={elem.pictureUrl}/>
+                  </div>
+                </td>
+                <td>{elem.name}</td>
+                <td>{elem.popularity}</td>
+                <td className = "DeleteButton">
+                  <button onClick={elem.clickToDelete}>Delete</button>
+                </td>
+              </tr>
+            </tbody>
+          </table>
+      </div>
       ) 
     }
 
-    const deleteFun = this.state.celebrities.map( elem => {
-      return (
-            <UpdatedList key={elem.id} 
-            clickToDelete={()=> this.DeleteButton(elem.id)} {...elem}/>
-            )
-          }); 
-
-  const table =      
-        <table>
-          <thead>
-            <tr>
-              <th>picture</th>
-              <th>name</th>
-              <th>popularity</th>
-              <th>action</th>
-              
-            </tr>
-          </thead>
-        </table>
 
     const contactList = this.state.celebrities.map(elem => { 
       return(
           <React.Fragment>
-            <table>
-              <tbody>
-                <tr>
-                  <td>
-                    <div className="TableImage">
-                      <img src={elem.pictureUrl}/>
-                    </div>
-                  </td>
-                  <td>{elem.name}</td>
-                  <td>{elem.popularity}</td>
-                </tr>
-              </tbody>
-            </table>
+            <UpdatedList key={elem.id} 
+            clickToDelete={()=> this.DeleteButton(elem.id)} {...elem}/>
           </React.Fragment> 
-          
-      )
-      })
+        )
+    })
+
+      const table =      
+      <table>
+        <thead>
+          <tr>
+            <th>picture</th>
+            <th>name</th>
+            <th>popularity</th>
+            <th>action</th>
+            
+          </tr>
+        </thead>
+        </table>
 
     return (
       <React.Fragment>
@@ -126,9 +120,10 @@ class App extends React.Component {
           <button onClick={this.SortedByName}>Sort By Name</button> 
           <button onClick={this.SortedByPopularity}>Sort By Popularity</button>
         </div>
+        <tbody>
         {table}
         {contactList}
-        <td>{deleteFun}</td>
+        </tbody>
       </div>
       </React.Fragment>
     )     
