6a28484ae14c07e797970fa5e85f3112cd893a88
Thrashskin <jmontegonz@gmail.com>
2021-01-23 22:55:21 +0100

    Done

--- src/App.js
+++ src/App.js
@@ -12,12 +12,25 @@ class App extends React.Component {
     }
   }
 
+  deleteContact(id) {
+    
+    let contactsClone = JSON.parse(JSON.stringify(this.state.contactsList))
+
+    let filteredList = contactsClone.filter(contact => contact.id !== id)
+
+    this.setState({
+      contactsList: filteredList
+    })
+
+  }
+
   displayContact(contact) {
     return (
       <tr key={contact.id}>
         <td><img src={contact.pictureUrl} alt={contact.name} className ="contactPicture"/></td>
         <td>{contact.name}</td>
         <td>{contact.popularity}</td>
+        <td><button onClick={() => this.deleteContact(contact.id)}>Delete</button></td>
       </tr>
     );
   }
@@ -49,17 +62,63 @@ class App extends React.Component {
     }
   }
 
+  sortByName(contactsArray) {
+
+    let contactsClone = JSON.parse(JSON.stringify(this.state.contactsList));
+
+    let sortedContacts = contactsClone.sort((a, b) => {
+      if (a.name < b.name) {
+        return -1;
+      }
+      else if (a.name > b.name) {
+        return 1;
+      } else {
+        return 0;
+      }
+    })
+
+    this.setState({
+      contactsList: sortedContacts
+    })
+  }
+
+  sortByPopularity() {
+
+    let contactsClone = JSON.parse(JSON.stringify(this.state.contactsList));
+
+    let sortedContacts = contactsClone.sort((a, b) => {
+      if (a.popularity < b.popularity) {
+        return 1;
+      }
+      else if (a.popularity > b.popularity) {
+        return -1;
+      } else {
+        return 0;
+      }
+    })
+
+    this.setState({
+      contactsList: sortedContacts
+    })
+  }
+
+
   render() {
     return (
       <div className="App" >
         <h1>IRONHACK CONTACTS</h1>
-        <button onClick={() => this.addRandomContact(contacts)} >Add Random Contact</button>
+        <div>
+          <button onClick={() => this.addRandomContact(contacts)} >Add Random Contact</button>
+          <button onClick={() => this.sortByName(contacts)} >Sort by name</button>
+          <button onClick={() => this.sortByPopularity(contacts)} >Sort by popularity</button>
+        </div>
         <table className="contacts-table">
           <thead>
             <tr>
               <th>Picture</th>
               <th>Name</th>
               <th>Popularity</th>
+              <th>Action</th>
             </tr>
           </thead>
           <tbody>
