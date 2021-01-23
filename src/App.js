f509b8b44c8d18c92ec6dc9bf93ffb52b8db5f3d
Thrashskin <jmontegonz@gmail.com>
2021-01-23 20:34:20 +0100

    Iteration 1 complete

--- src/App.js
+++ src/App.js
@@ -1,10 +1,33 @@
 import React from 'react';
 import './App.css';
+import contacts from './contacts.json'
+
+
+const firstFive = contacts.slice(0,5).map((contact) => {
+  return (
+    <tr>
+      <td><img src={contact.pictureUrl} alt={contact.name} className ="contactPicture"/></td>
+      <td>{contact.name}</td>
+      <td>{contact.popularity}</td>
+    </tr>
+  );
+});
+
 
 class App extends React.Component {
+
   render() {
     return (
       <div className="App" >
+        <h1>IRONHACK CONTACTS</h1>
+        <table className="contacts-table">
+          <tr>
+            <th>Picture</th>
+            <th>Name</th>
+            <th>Popularity</th>
+          </tr>
+          {firstFive}
+        </table>
       </div>
     );
   }
