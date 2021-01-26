0049067cd5932c7caf8014fca227d310e2a05d64
Ashley Vermaas <ashleyvermaas1@gmail.com>
2021-01-26 11:09:34 +0100

    complete iteration 1

--- src/App.js
+++ src/App.js
@@ -1,10 +1,33 @@
 import React from 'react';
 import './App.css';
+import contacts from './contacts.json';
+
+const fiveContacts = contacts.slice(0, 5)
 
 class App extends React.Component {
+  state = {
+    fiveContacts
+  }
+
   render() {
+    const contactsList = this.state.fiveContacts.map(contact => {
+      return <React.Fragment>
+        <tr>
+          <td><img src={contact.pictureUrl} /></td>
+          <td>{contact.name}</td>
+          <td>{contact.popularity}</td>
+        </tr>
+      </React.Fragment>
+    })
     return (
       <div className="App" >
+        <h1>IronContacts</h1>
+        <table>
+          <th>Picture</th>
+          <th>Name</th>
+          <th>Popularity</th>
+          <React.Fragment>{contactsList}</React.Fragment>
+        </table>
       </div>
     );
   }
