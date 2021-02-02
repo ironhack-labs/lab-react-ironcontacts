1408ca0975a6cfd731ef265f1ad67e5d719d0c65
ThSchweers <th.schweers@googlemail.com>
2021-02-02 08:46:42 +0100

    [Remote WD PT 280121] Thomas Schweers

--- src/App.js
+++ src/App.js
@@ -1,13 +1,76 @@
 import React from 'react';
 import './App.css';
+import contacts from './contacts.json';
 
-class App extends React.Component {
+export default class App extends React.Component {
+
+  state = {
+    contacts : contacts.slice(0, 10),
+  }
+
+  addRandomContact = () => {
+    let randomContact = contacts[Math.floor(Math.random() * contacts.length)] 
+    this.setState(() => {
+     return {contacts : [...this.state.contacts, randomContact]}
+    })
+  }
+
+  sortByName = () => {
+    let alphaName = [...this.state.contacts].sort((a, b) => a.name.localeCompare(b.name))
+    this.setState(() => {
+      return {contacts: alphaName}
+    })
+  }
+
+  sortByPopularity = () => {
+    let mostPopular = [...this.state.contacts].sort((a, b) => b.popularity - a.popularity)
+    this.setState(() => {
+      return {contacts: mostPopular}
+    })
+  }
+
+  removeContact = (index) => {
+    const copy = [...this.state.contacts];
+    copy.splice(index, 1);
+    this.setState({
+      contacts: copy,
+    });
+  }
   render() {
     return (
       <div className="App" >
-      </div>
-    );
-  }
-}
+      <h3>IronContacts</h3>
+          <button onClick={() => this.addRandomContact()}>Add Random actor</button>
+          <button onClick={() => this.sortByPopularity()} >Sort by popularity</button>
+          <button onClick={() => this.sortByName()}>Sort by name</button>
 
-export default App;
+
+        <table>
+        <thead>
+        <tr>
+          <th>Picture</th>
+          <th>Name</th>
+          <th>Popularity</th>
+        </tr>
+        </thead>
+
+        <tbody>
+        { this.state.contacts.map((contact) => {
+
+          return (
+
+            <tr key={contact.id} style={{backgroundColor: 'lightGrey'}}>
+             <td><img style={{width: '120px', height: '150px'}} src={contact.pictureUrl} alt=""/></td>
+             <td>{contact.name}</td>
+             <td>{contact.popularity.toFixed(2)}</td>
+             <button onClick={() => this.removeContact()}>Delete</button>
+            </tr>  
+          )
+        }     
+         )}
+        </tbody>
+        </table>
+        </div>
+    );	   
+  }	  
+}	
