d2584263f3bcf1a323103e469045a74b65d11775
carmen valencia <carmenpilar1990@gmail.com>
2021-02-10 20:10:23 +0100

    Iteration 1

--- src/App.js
+++ src/App.js
@@ -1,10 +1,37 @@
 import React from 'react';
+import contacts from './contacts.json';
 import './App.css';
 
 class App extends React.Component {
+  state={
+    contacts: contacts.slice(0, 5),
+  };
+
   render() {
     return (
       <div className="App" >
+      <h1>IronContacts</h1>
+
+<table className="table">
+          <thead>
+            <td>Picture</td>
+            <td>Name</td>
+            <td>Popularity</td>
+          </thead>
+         </table> 
+         <tbody>
+            {this.state.contacts.map((contact, index) => {
+              return (
+                <tr key={index}>
+                  <td>
+                    <img src={contact.pictureUrl} alt=""></img>
+                  </td>
+                  <td>{contact.name}</td>
+                  <td>{contact.popularity.toFixed(2)}</td>
+                </tr>
+              );
+            })};
+                </tbody>  
       </div>
     );
   }
