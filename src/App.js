f2037cd924a4d8c7e2b0156dae24594bd3eeaf95
TomasAldea <tomas_aldea@DESKTOP-58B551K.localdomain>
2021-02-11 17:59:06 +0100

    [WD-PT-BCN-0920] - AldeaTomas

--- src/App.js
+++ src/App.js
@@ -2,10 +2,12 @@ import React from 'react';
 import './App.css';
 import IronContacts from "../src/Components/IronContacts/IronContacts"
 
+
 class App extends React.Component {
   render() {
     return (
       <div className="App" >
+        <h1>Iron Contacts</h1>
         <IronContacts></IronContacts>
       </div>
     );
--- src/Components/IronContacts/IronContacts.js
+++ src/Components/IronContacts/IronContacts.js
@@ -2,31 +2,66 @@ import React from 'react';
 import contacts from '../../contacts.json';
 import "./IronContacts.css"
 
+
+
 export default function IronContacts() {
-  const [fiveFirst, setFiveFirst] = React.useState(contacts.slice(0, 5));
+const [fiveFirst, setFiveFirst] = React.useState(contacts.slice(0, 5));
+
+
+// add random function
+var randomIndex = contacts[Math.floor(Math.random() * contacts.length)];
+const buttonAdd = () => setFiveFirst(state => state.concat(randomIndex))
+// delete function
+const buttonDelete = (contactId) => setFiveFirst(state => state.filter(contact => contactId !== contact.id))
+
+// callback to sort by key
+function dynamicSort(property) {
+  var sortOrder = 1;
+  if(property[0] === "-") {
+      sortOrder = -1;
+      property = property.substr(1);
+  }
+  return function (a,b) {
+      var result = (a[property] > b[property]) ? -1 : (a[property] < b[property]) ? 1 : 0;
+      return result * sortOrder;
+  }
+}
+// sort by rate function
+const buttonSortRate = () => setFiveFirst(fiveFirst => [...fiveFirst].sort(dynamicSort("popularity")))
+// sort by name function
+const buttonSortName = () => setFiveFirst(fiveFirst => [...fiveFirst].sort(dynamicSort("name")))
 
   return (
+    <div className="table">
+      <div className="button-div">
+      <button className="button-marging" onClick={buttonAdd}>Add Random</button>
+      <button className="button-marging" onClick={buttonSortRate}>Sort by Rate</button>
+      <button className="button-marging" onClick={buttonSortName}>Sort by Name</button>
+      </div>
     <table className="inline-table">
       <thead>
         <tr>
           <th>Picture</th>
           <th>Name</th>
           <th>Popularity</th>
+          <th>Delete</th>
         </tr>
       </thead>
       <tbody>
         {fiveFirst.map(function (i) {
           return (
-            <tr>
+            <tr key={i.id}>
               <td>
-                <img src={i.pictureUrl} width="70" height="100" />
+                <img src={i.pictureUrl} alt="contact face" width="70" height="100" />
               </td>
-              <td key={i.name}>{i.name}</td>
-              <td key={i.popularity}>{Math.round(i.popularity)}</td>
+              <td>{i.name}</td>
+              <td>{Math.round(i.popularity)}</td>
+              <td><button onClick={() => buttonDelete(i.id)}>Delete</button></td>
             </tr>
           );
         })}
       </tbody>
     </table>
+    </div>
   );
 }
