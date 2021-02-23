b42b9439f4b8d8e00f062688197bb9456730628c
honghainguyen777 <n.h.hai2015@gmail.com>
2021-02-23 10:27:45 +0100

    iteration 1

--- src/App.js
+++ src/App.js
@@ -1,23 +1,46 @@
 import logo from './logo.svg';
 import './App.css';
+import contacts from './contacts.json';
+
+
+const ContactGenerator = ({name, pictureUrl, popularity}) => {
+  return (
+  <tr>
+      <td><img src={pictureUrl} alt={name} className="profile-img"/></td>
+      <td>{name}</td>
+      <td>{popularity}</td>
+    </tr>
+  )
+};
+
+const ContactList = (props) => {
+  return props.contacts.map(contact => {
+    return (<ContactGenerator key={contact.id} {...contact} />);
+  });
+};
+
+
 
 function App() {
+  const first5Contacts = contacts.slice(0, 10);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="contacts">
+        <h1>IronContacts</h1>
+        <table>
+          <thead>
+            <tr>
+              <th>Picture</th>
+              <th>Name</th>
+              <th>Popularity</th>
+            </tr>
+          </thead>
+          <tbody>
+            <ContactList contacts={first5Contacts} />
+          </tbody>
+        </table>
+      </div>
     </div>
   );
 }
