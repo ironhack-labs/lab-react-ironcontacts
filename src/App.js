39a43c59ea9cdf76821194fc9d45f65ccef60794
Sebastian Montoya <seb246@gmail.com>
2021-02-23 14:58:02 -0500

    done without styling

--- src/App.js
+++ src/App.js
@@ -1,41 +1,56 @@
 import contacts from './contacts.json'
 import React from 'react';
 
+
 let onlyFive = contacts.filter((contact, index) => index < 5);
 let otherContacts = contacts.filter((contact,index) => index >= 5);
 
 class App extends React.Component {
   state ={
-    onlyFive,
-    otherContacts
+    firstFive: onlyFive,
+    otherContacts: otherContacts
+  }
+
+  sortName(){
+    let copyFive = [...this.state.firstFive];
+    copyFive.sort((a,b) => a.name.localeCompare(b.name));
+    this.setState({firstFive: copyFive })
   }
 
   addRandom(randomC){
-    let copyOfOthers = [...onlyFive];
+    let copyOfOthers = [...this.state.firstFive];
     copyOfOthers.unshift(randomC);
-    this.setState({ onlyFive: copyOfOthers })
+    this.setState({ firstFive: copyOfOthers })
     
   }
 
   showContacts(){
-    return this.state.onlyFive.map((contact, index) => {
+    return this.state.firstFive.map((contact, index) => {
 
     return(
       <tr key={contact.id}>
         <td><img style={{width:'10vw'}} src={contact.pictureUrl} alt={contact.name}/></td>
         <td>{contact.name}</td>
         <td>{contact.popularity.toFixed(2)}</td>
+        <button onClick={(er) => this.deleteContact(contact, index)}>Delete</button>
       </tr>
     )
     
   })
   }
 
+  deleteContact(contact, index){
+    let copyContacts = [...this.state.firstFive];
+    copyContacts.splice(index, 1);
+    this.setState({firstFive: copyContacts});
+  }
+
 
   render(){
     return (
-      <div className="App">
+      <div className="App1">
         <button onClick={() => this.addRandom(this.state.otherContacts[Math.floor(Math.random() * this.state.otherContacts.length)])}>Add Random</button>
+        <button onClick={() => this.sortName()}>Sort by Name</button>
         <table>
           <thead>
             <tr>
