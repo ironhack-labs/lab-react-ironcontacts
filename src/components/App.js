8d0b11939e026325767bedb377381001305d527b
Barbara Diaz <barbarad91@gmail.com>
2021-02-23 15:55:56 +0100

    Display table with first 5 contacts

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,22 @@
 import "./App.css"
 
+import contacts from "../contacts.json"
+import Table from "./Table"
+
 function App() {
-  return <h2>Hola</h2>
+  const twoContacts = [
+    contacts[0],
+    contacts[1],
+    contacts[2],
+    contacts[3],
+    contacts[4],
+  ]
+
+  return (
+    <>
+      <Table className="contacts-table" contacts={twoContacts} />
+    </>
+  )
 }
 
 export default App
---
+++ src/components/Table.js
@@ -0,0 +1,26 @@
+import "./Table.css"
+
+const Table = ({ className, contacts }) => (
+  <table className={className}>
+    <thead>
+      <tr>
+        <th>Picture</th>
+        <th>Name</th>
+        <th>Popularity</th>
+      </tr>
+    </thead>
+    <tbody>
+      {contacts.map(contact => (
+        <tr key={contact.id}>
+          <td>
+            <img src={contact.pictureUrl} alt={contact.name}></img>
+          </td>
+          <td>{contact.name}</td>
+          <td>{contact.popularity}</td>
+        </tr>
+      ))}
+    </tbody>
+  </table>
+)
+
+export default Table
