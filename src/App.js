03e755e35294c04d1431a2b6b19be2af7f44e81e
Michal Raman <raman_michael@hotmail.com>
2021-02-24 12:54:48 +0100

    finished iteration 3

--- src/App.js
+++ src/App.js
@@ -1,25 +1,67 @@
-import logo from './logo.svg';
 import './App.css';
+import contacts from "./contacts.json";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+import React, { Component } from 'react'
+
+export default class App extends Component {
+  state = {
+    contactsArr : contacts.slice(0,5)
+  }
+
+  addContact = () => {
+    const uniqueContacts = contacts.filter(contact => {
+      return !this.state.contactsArr.includes(contact)
+    })
+    console.log(uniqueContacts)
+    if(uniqueContacts.length > 0) {
+      const randomContact = uniqueContacts[Math.floor(Math.random()*uniqueContacts.length)]
+      this.setState({
+        contactsArr : [...this.state.contactsArr, randomContact]
+      })
+    }
+  }
+
+  filterNameContact = () => {
+    this.setState({
+      contactsArr : [...this.state.contactsArr].sort((a,b) =>(a.name > b.name)?1:((b.name>a.name)?-1:0))
+    })
+  }
+
+  filterPopContact = () => {
+    this.setState({
+      contactsArr : [...this.state.contactsArr].sort((a,b) =>(b.popularity > a.popularity)?1:((a.popularity>b.popularity)?-1:0))
+    })
+  }
+
+  render() {
+    return (
+      <div className="App">
+      <h1 id="title">IronContacts</h1>
+      <div>
+        <button className="addButton" onClick={this.addContact}>Add Random Contact</button>
+        <button className="addButton" onClick={this.filterNameContact}>Sort By Name</button>
+        <button className="addButton" onClick={this.filterPopContact}>Sort By Popularity</button>
+      </div>
+      <table>
+      <thead>
+        <tr>
+          <td>Picture</td>
+          <td>Name</td>
+          <td>Popularity</td>
+        </tr>
+      </thead>
+      <tbody>
+        {this.state.contactsArr.map(item => (
+            <tr key={item.id}>
+              <td><img id="picture" src={item.pictureUrl} alt="director"/></td>
+              <td><p>{item.name}</p></td>
+              <td><p>{item.popularity}</p></td>
+            </tr>
+            ))}
+      </tbody>
+      </table>
     </div>
-  );
+    )
+  }
 }
 
-export default App;
