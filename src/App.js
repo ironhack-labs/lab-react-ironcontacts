cf1237136e0dd2ebf9afc3d8f3f514450541c48f
Daniela Alonzo <monsterjunior@outlook.es>
2021-02-25 00:05:26 -0600

    done

--- src/App.js
+++ src/App.js
@@ -1,25 +1,69 @@
-import logo from './logo.svg';
+import React, {useState} from 'react'
 import './App.css';
+import contacts from "./contacts.json";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const [contact, setContacts]= useState(contacts.slice(0, 5))
+console.log(contact)
+const random = (event) => {
+  event.preventDefault()
+  let newRandomContact = contacts[Math.floor(Math.random()* contacts.length)]
+  setContacts([...contact,newRandomContact])
 }
 
+const sortAlph = (event) => {
+  event.preventDefault()
+  let sortedByName= contact.sort(function(a, b){
+    if(a.name < b.name) { return -1; }
+    if(a.name > b.name) { return 1; }
+    return 0;
+})
+  setContacts([...sortedByName])
+}
+
+const sortPop = (event) => {
+  event.preventDefault()
+  let sortedByPop= contact.sort(function(a, b){
+  return b.popularity - a.popularity;
+})
+  setContacts([...sortedByPop])
+}
+
+const deleteArtist = (id) => {
+
+  // event.preventDefault()
+  const listaArtista = contact.filter((element) => {
+    return element.id !== id
+  })
+  setContacts([...listaArtista])
+}
+
+  return (
+    <>
+    <button onClick={random}>Add Random Contact</button>
+    <button onClick={sortAlph}>Sort by Name</button>
+    <button onClick={sortPop}>Sort by Popularity</button>
+    <table>
+    <tr>
+      <th>Picture</th>
+      <th>Name</th>
+      <th>Popularity</th>
+      <th>Action</th>
+    </tr>
+        {
+          contact.map((e, id)=>{
+            return(
+            <tr key={id}>
+              <td><img style={{width:90}} src={e.pictureUrl}/></td>
+              <td>{e.name}</td>
+              <td>{e.popularity}</td>
+              <td><button onClick={() => deleteArtist(e.id)}>Eliminar</button></td>
+            </tr>
+            )
+          })
+      }
+    </table>
+    </>
+  );
+}
 export default App;
