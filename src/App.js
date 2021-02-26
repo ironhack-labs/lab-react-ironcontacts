da8752c46ac38664c885d1cabbd617dadebe97db
RFeldman84 <rachel.feldman84@gmail.com>
2021-02-25 21:07:49 -0500

    iteration 1

--- src/App.js
+++ src/App.js
@@ -1,25 +1,68 @@
-import logo from './logo.svg';
 import './App.css';
+import React from 'react'
+import contacts from "./contacts.json";
 
-function App() {
+// function App() {
+//   return (
+//     <div className="App">
+    
+//     </div>
+//   );
+// }
+
+// export default App;
+
+
+const contact5 = contacts.filter((contact, i) => i < 5)
+
+class App extends React.Component {
+  
+state = {
+    contact5
+  }
+
+render(){
+
+const {contact5} = this.state 
+const firstFive = contact5.map((c,i)=> {
+  return <tr key={c.id}>
+  <td ><img width='100vh' src={c.pictureUrl} alt=""/></td>
+  <td ><h4>{c.name}</h4></td>
+  <td >{c.popularity.toFixed(2)}</td>
+ </tr> 
+  
+})
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+  < >
+    <div className='btns'>
+      <button onClick={this.pickRandom}>Add Random Contact</button>
+      <button>Sort by Name</button>
+      <button>Sort by Popularity</button>
     </div>
+
+    <table > 
+    <caption ><h1> IronContacts</h1></caption> 
+    <thead>
+        <tr>
+          <th><h3>Picture</h3></th>
+          <th ><h3>Name</h3></th>
+          <th ><h3>Popularity</h3></th>
+        </tr>
+      </thead>
+     <tbody>
+        {firstFive}
+     </tbody>
+    </table>
+    
+
+    
+    </>
   );
-}
+  }
+
+};
+
 
 export default App;
