da8d75bf8c781167e6eeb8113d1c22341f174704
Kiko Quesada <franciscoquesadacabello89@gmail.com>
2021-03-04 20:34:51 +0100

    feat: Iteration 3 done

--- src/components/contact-list/ContactList.js
+++ src/components/contact-list/ContactList.js
@@ -2,36 +2,60 @@
 import { Component } from 'react'
 import contacts from '../../contacts.json';
 
-const firstFive = contacts.slice(0, 5);
 class ContactList extends Component {
 
     state = {
-        randomContact: undefined
+        randomContact: undefined,
+        restOfContacts: contacts.slice(5, contacts.length),
+        displayedContacts: contacts.slice(0, 5)
     }
 
 
     addRandom = () => {
-        const restOfContacts = contacts.slice(5, contacts.length);
+        const { restOfContacts, displayedContacts } = this.state;
         const randomNumber = Math.round(Math.random() * (restOfContacts.length - 1));
         const randomContact = restOfContacts[randomNumber];
         restOfContacts.splice(randomNumber, 1)
+        displayedContacts.push(randomContact)
         this.setState({
-            randomContact
+            randomContact,
+            restOfContacts
         })
+
+    }
+
+    handleSort = (sortBy) => {
+        function sortByPopularity(a, b) {
+            return b.popularity - a.popularity
+        }
+        if (sortBy === 'name') {
+            this.setState({
+                displayedContacts: this.state.displayedContacts.sort((a, b) => a.name.localeCompare(b.name))
+            })
+        } else if (sortBy === 'popularity') {
+            this.setState({
+                displayedContacts: this.state.displayedContacts.sort(sortByPopularity)
+            })
+        }
     }
 
 
 
     render() {
         const { className } = this.props;
-        if (typeof this.state.randomContact !== 'undefined') {
-            console.log(this.state.randomContact);
-            firstFive.push(this.state.randomContact)
-            this.state.randomContact = undefined;
-        }
+        const { displayedContacts } = this.state;
         return (
             <div>
                 <button onClick={() => this.addRandom()} className="btn btn-primary">Add random Contact</button>
+                <div className="dropdown">
+                    <button className="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuButton1" data-bs-toggle="dropdown" aria-expanded="false">
+                        Dropdown button
+                    </button>
+                    <ul className="dropdown-menu" aria-labelledby="dropdownMenuButton1">
+                        <li><a className="dropdown-item" onClick={() => this.handleSort('name')}>Sort by Name</a></li>
+                        <li><a className="dropdown-item" onClick={() => this.handleSort('popularity')}>Sort by Popularity</a></li>
+                    </ul>
+                </div>
                 <table className={`table ${className}`}>
                     <thead>
                         <tr>
@@ -41,10 +65,10 @@ class ContactList extends Component {
                         </tr>
                     </thead>
                     <tbody>
-                        {firstFive.map(contact => (
+                        {displayedContacts.map(contact => (
                             <tr key={contact.id}>
                                 <td>{contact.name}</td>
-                                <td><img style={{width:"70px"}} src={contact.pictureUrl} /></td>
+                                <td><img style={{ width: "70px" }} src={contact.pictureUrl} /></td>
                                 <td>{contact.popularity}</td>
                             </tr>
                         ))}
