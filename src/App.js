145d3dda86bb10b7f00842255d5699e9c0a8677c
JuanBerrondo <bega2000@protonmail.com>
2021-03-04 20:30:39 +0100

    done

--- src/App.js
+++ src/App.js
@@ -1,25 +1,93 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import contacts from "./contacts.json";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+class App extends React.Component {
+  state = {
+    initial: contacts.slice(0, 5),
+  };
+  randomUser = () => {
+    this.state.initial.push(
+      contacts[Math.floor(Math.random() * contacts.length)]
+    );
+    this.setState(this.state.initial);
+  };
+  sortByName = () => {
+    this.state.initial.sort((a, b) => {
+      return a.name < b.name ? -1 : a.name > b.name ? 1 : 0;
+    });
+    this.setState(this.state.initial);
+  };
+  sortByPopularity = () => {
+    this.state.initial.sort((a, b) => {
+      return a.popularity > b.popularity
+        ? -1
+        : a.popularity < b.popularity
+        ? 1
+        : 0;
+    });
+    this.setState(this.state.initial);
+  };
+  deleteUser = (id) => {
+    const identifyUser = this.state.initial.findIndex((user) => user.id === id);
+    this.state.initial.splice(identifyUser, 1);
+    this.setState(this.state.initial);
+  };
+  render() {
+    return (
+      <div className="App">
+        <button
+          type="button"
+          className="btn btn-success m-2"
+          onClick={() => this.randomUser()}
         >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+          Add random user
+        </button>
+        <button
+          type="button"
+          className="btn btn-primary m-2"
+          onClick={() => this.sortByName()}
+        >
+          Sort by name
+        </button>
+        <button
+          type="button"
+          className="btn btn-primary m-2"
+          onClick={() => this.sortByPopularity()}
+        >
+          Sort by popularity
+        </button>
+        <table>
+          <tr>
+            <th>Picture</th>
+            <th>Name</th>
+            <th>Popularity</th>
+          </tr>
+          {this.state.initial.sort().map((contact) => (
+            <tr>
+              <td>
+                <img
+                  className="col-md-4"
+                  src={contact.pictureUrl}
+                  alt="avatar"
+                />
+              </td>
+              <td>{contact.name}</td>
+              <td>{contact.popularity}</td>
+              <td>
+                <button
+                  type="button"
+                  className="btn btn-danger"
+                  onClick={() => this.deleteUser(contact.id)}
+                >
+                  Delete
+                </button>
+              </td>
+            </tr>
+          ))}
+        </table>
+      </div>
+    );
+  }
 }
-
 export default App;
