60b2cc69c347ce5c54ee7991579b847795ed7b99
Ana <anaurrig@gmail.com>
2021-03-05 18:53:06 +0100

    feat: acabado

--- src/components/contact-list/ContactList.jsx
+++ src/components/contact-list/ContactList.jsx
@@ -1,60 +1,76 @@
 import React from 'react'
 import Contact from '../contacts/Contact'
-import contacts from '../../contacts.json'
+import contactsData from '../../contacts.json'
 import Button from '../button/Button';
 
-const contactsFive = contacts.slice(0,5);
+const contacts = contactsData.slice(0, 5);
 
-class ContactList extends React.Component{
-      
+class ContactList extends React.Component {
+
     state = {
-        // contacts : contactsFive . Equivale a lo de abajo
-        contactsFive ,
-    } ;
-    
-    
+        contacts,
+    };
 
     addContact = () => {
-        const index = Math.floor(Math.random() * contacts.length);
-        const item = contacts[index]
-        if (contactsFive.some(e => e === item)) {
-            const index = Math.floor(Math.random() * contacts.length);
-            const item = contacts[index]
-        } else {
+        if (this.state.contacts.length < contactsData.length){
+            var index = 0;
+            var item = undefined;
+    
+            do {
+                index = Math.floor(Math.random() * contactsData.length);
+                item = contactsData[index];
+            } while (this.state.contacts.some(e => e === item) )
+    
+                
             this.setState((prev) => ({
-                contactsFive: [item, ...prev.contactsFive]
+                contacts: [item, ...prev.contacts]
             }))
+
+        } else {
+            alert("No hay más contactos que añadir")
         }
     }
-    
+
     sortByName = () => {
         this.setState((state) => ({
-            contactsFive: state.contactsFive.sort((a, b) => (a.name.localeCompare(b.name)))}
+            contacts: state.contacts.sort((a, b) => (a.name.localeCompare(b.name)))
+        }
         ))
     }
 
     sortByPopularity = () => {
         this.setState((state) => ({
-            contactsFive: state.contactsFive.sort((a, b) => (b.popularity - a.popularity))}
+            contacts: state.contacts.sort((a, b) => (b.popularity - a.popularity))
+        }
         ))
     }
-    render(){
+
+    deleteContact = (id) => {
+        this.setState((prev) => {
+            return {
+                contacts: prev.contacts.filter(e => e.id !== id)
+            }
+        })
+    }
+
+    render() {
+
 
         return (
-            <>
+            <div>
                 <div>
-                    <Button label = "Add Random Contact" 
-                            onClick={ this.addContact } ></Button>
+                    <Button label="Add Random Contact"
+                        onClick={this.addContact} ></Button>
                 </div>
                 <div>
-                    <Button label = "Sort by name" 
-                           onClick={() => { this.sortByName(this.state) }} ></Button>
+                    <Button label="Sort by name"
+                        onClick={() => { this.sortByName(this.state) }} ></Button>
                 </div>
                 <div>
-                    <Button label = "Sort by popularity" 
-                           onClick={() => { this.sortByPopularity(this.state) }} ></Button>
+                    <Button label="Sort by popularity"
+                        onClick={() => { this.sortByPopularity(this.state) }} ></Button>
                 </div>
-                <table className="table">
+                <table className="table text-center">
                     <thead>
                         <tr>
                             <th scope="col">Picture</th>
@@ -63,19 +79,21 @@ class ContactList extends React.Component{
                         </tr>
                     </thead>
                     <tbody>
-                        {this.state.contactsFive.map((contact, i) => (
+                        {this.state.contacts.map((contact, i) => (
                             <div key={i}>
-                                <Contact contact={contact}>  </Contact>
+                                <Contact
+                                    contact={contact}
+                                    deleteContact={this.deleteContact}>  </Contact>
                             </div>
                         )
                         )}
-    
+
                     </tbody>
                 </table>
-            </>
-    
+            </div>
+
         )
     }
-    
+
 }
 export default ContactList
--- src/components/contacts/Contact.jsx
+++ src/components/contacts/Contact.jsx
@@ -1,11 +1,18 @@
-function Contact({contact}) {
-    return(
-  <tr>
+import Button from "../button/Button"
+
+
+function Contact({ contact, deleteContact }) {
+  return (
+    <tr>
       <th scope="col"></th>
-      <th scope="col" > <img src={contact.pictureUrl} width="60" alt=""/>  </th>
+      <th scope="col" > <img src={contact.pictureUrl} width="60" alt="" />  </th>
       <th scope="col">{contact.name}</th>
       <th scope="col">{contact.popularity}</th>
+      <div>
+        <Button label="Delete"
+          onClick={() => { deleteContact(contact.id) }} ></Button>
+      </div>
     </tr>
-    )
+  )
 }
 export default Contact
