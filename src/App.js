5d720063cbf34aa9fa6a11f9f86f1f915962839d
digomattar21 <digomattar@gmail.com>
2021-03-13 15:41:02 -0300

    done without styling

--- src/App.js
+++ src/App.js
@@ -1,25 +1,78 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import contacts from "./contacts.json";
+import React, { Component } from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export class App extends Component {
+  state = {
+    contacts: contacts.slice(0, 5),
+    allContacts: contacts
+  };
+
+  handleRandomClick = ()=>{
+    let randContact = this.state.allContacts[Math.floor(Math.random()*this.state.allContacts.length)];
+    const stateCopy = [...this.state.contacts]
+    stateCopy.push(randContact)
+    this.setState({contacts: stateCopy})
+  }
+
+  handleNameSort = ()=>{
+    const stateCopy = [...this.state.contacts]
+    const sorted = stateCopy.sort(function(a, b){
+      if(a.name < b.name) { return -1; }
+      if(a.name > b.name) { return 1; }
+      return 0;
+  })
+    this.setState({contacts: sorted})
+  }
+
+  handlePopSort = () =>{
+    const stateCopy = [...this.state.contacts];
+    const sorted = stateCopy.sort(function(a, b){
+      if(a.popularity < b.popularity) { return 1; }
+      if(a.popularity > b.popularity) { return -1; }
+      return 0;
+  })
+    this.setState({contacts: sorted})
+  }
+
+  handleDeleteClick = (id)=>{
+    const stateCopy = [...this.state.contacts];
+    const indexToRemove = stateCopy.findIndex(contact=>contact.id === id);
+    stateCopy.splice(indexToRemove, 1)
+    this.setState({contacts:stateCopy})
+  }
+
+  render() {
+    return (
+      <>
+      <div>
+      <button onClick={this.handleRandomClick}>Add Random Contact</button>
+      <button onClick={this.handleNameSort}>Sort by name</button>
+      <button onClick={this.handlePopSort}>Sort by popularity</button>
+      </div>
+      <table>
+        <thead>
+          <tr>
+            <th>Picture</th>
+            <th>Name</th>
+            <th>Popularity</th>
+          </tr>
+        </thead>
+        <tbody>
+          {this.state.contacts.map((contact) => {
+           return ( <tr>
+              <td><img src={contact.pictureUrl} alt="img" style={{height:'100px'}}/></td>
+              <td>{contact.name}</td>
+              <td>{contact.popularity}</td>
+              <td><button onClick={()=>this.handleDeleteClick(contact.id)}>Delete</button></td>
+              </tr>)
+          })}
+        </tbody>
+      </table>
+      </>
+    );
+  }
 }
 
 export default App;
