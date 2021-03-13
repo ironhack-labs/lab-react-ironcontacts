5b608c03a86ee3f707df5f5ddc31203939355854
eduardoLemes <eduardo_lsf@outlook.com>
2021-03-13 17:37:22 -0300

    done 1 and 2 iterations

--- src/App.js
+++ src/App.js
@@ -1,25 +1,59 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import contacts from "./contacts.json";
+import  './App.css'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      data: contacts.slice(0, 5),
+    };
+  }
+  randomContacts = () => {
+    let randomCont = contacts[Math.floor(Math.random() * contacts.length)];
+    const dataCopy = [...this.state.data];
+    dataCopy.push(randomCont);
+    this.setState({
+      data: dataCopy,
+    });
+  };
+
+  render() {
+    return (
+      <div>
+        <button onClick={() => this.randomContacts()}>ADD Random Contacts</button>
+        {this.state.data.map((element) => {
+          return (
+            <div>
+              <h1>Iron contacts</h1>
+              <table>
+                <tr>
+                  <th>Picture</th>
+                  <th>Name</th>
+                  <th>Popularity</th>
+                </tr>
+                <tr>
+                  <td>
+                    <img className="image" src={element.pictureUrl} />
+                  </td>
+                  <td>{element.name}</td>
+                  <td>{element.popularity}</td>
+                </tr>
+              </table>
+            </div>
+          );
+        })}
+      </div>
+    );
+  }
 }
+
+// function App() {
+//   return (
+//     <div className="App">
+
+//     </div>
+//   );
+// }
 
 export default App;
