911fca6e0fb8d666337b423e2ab9d8f30d242cc5
Anna Lefour <anna.lefour@laposte.net>
2021-03-16 18:28:44 +0100

    It 1..

--- src/App.js
+++ src/App.js
@@ -1,23 +1,45 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import contacts from "./contacts.json";
+
+import React, { PureComponent } from "react";
+
+export class Contacts extends PureComponent {
+  state = {
+    fiveContacts: contacts.slice(0, 5),
+  };
+
+  render() {
+    // console.log(contacts)
+    return (
+      <div>
+        <table id="contacts">
+          <thead>
+            <th>Picture</th>
+            <th>Name</th>
+            <th>Popularity</th>
+          </thead>
+          {this.state.fiveContacts.map((contact) => (
+            <tbody key={contact.id}>
+              <tr>
+                <td>
+                  <img src={contact.pictureUrl} alt=""></img>
+                </td>
+                <td>{contact.name}</td>
+                <td>{contact.popularity}</td>
+              </tr>
+            </tbody>
+          ))}
+        </table>
+      </div>
+    );
+  }
+}
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Contacts />
     </div>
   );
 }
