216b54557ea210dec0e854eaacefc0a027bfe676
Mathieu Jacob <mathieu.jacob@hotmail.fr>
2021-03-16 20:01:45 +0100

    done with css style

--- src/App.js
+++ src/App.js
@@ -1,25 +1,91 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import contacts from "./contacts.json";
+import React, { Component } from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  state = {
+    contacts: contacts.slice(0, 5),
+  };
+
+  randomUser = () => {
+    this.state.contacts.push(
+      contacts[Math.floor(Math.random() * contacts.length)]
+    );
+    this.setState(this.state.contacts);
+    // {Math.floor(Math.random() * (this.props.max - this.props.min) + this.props.min)}
+  };
+
+  sortByName = () => {
+    this.state.contacts.sort((first, next) => {
+      if (first.name < next.name) {
+        return -1;
+      } else if (first.name > next.name) {
+        return 0;
+      }
+    });
+
+    this.setState(this.state.contacts);
+  };
+
+  sortByPopularity = () => {
+    this.state.contacts.sort((first, next) => {
+      if (first.popularity > next.popularity) {
+        return -1;
+      } else if (first.popularity < next.popularity) {
+        return 0;
+      }
+    });
+
+    this.setState(this.state.contacts);
+  };
+
+  removeContact(contactToRemoveIndex) {
+    const copyContacts = [...this.state.contacts];
+
+    copyContacts.splice(contactToRemoveIndex, 1);
+
+    this.setState({ contacts: copyContacts });
+  }
+
+  render() {
+    return (
+      <div>
+      <nav>
+        <button class= "top-button" onClick={this.randomUser}>Click To Add</button>
+        <button class= "top-button" onClick={this.sortByName}>By Name</button>
+        <button class= "top-button" onClick={this.sortByPopularity}>By Popularity</button>
+        </nav>
+        <body>
+        <table>
+          <tr>
+            <th>Picture</th>
+            <th>Name</th>
+            <th>Popularity</th>
+            <th>Action</th>
+          </tr>
+          {this.state.contacts.map((person, index) => {
+            return (
+              <tr key={person.id}>
+                <td>
+                  <img className="picture" src={person.pictureUrl} alt="" />
+                </td>
+                <td class="name">{person.name}</td>
+                <td class="name">{person.popularity}</td>
+                <td>
+                  <button class= "delete-button" onClick={() => this.removeContact(index)}>
+                    delete
+                  </button>
+                </td>
+             
+              </tr>
+            );
+          })}
+        </table>
+        </body>
+      </div>
+    );
+  }
 }
 
 export default App;
