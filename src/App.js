1c798465caa1c1727adeaed4a4872f00b69c7e5c
Mohammed Adib <adibmohammed95@gmail.com>
2021-03-16 23:46:21 +0100

    Lab finished without Bonus of Styling

--- src/App.js
+++ src/App.js
@@ -1,23 +1,156 @@
 import logo from './logo.svg';
 import './App.css';
 
+import contacts from './contacts.json'
+
+import React, { Component } from 'react'
+
+
+
+class Contacts extends Component {
+
+state = {
+  fiveContacts: contacts.slice(0,5),
+  sortType: 'a',
+  sortPop: 0
+  
+};
+
+addRandomContact = () => {
+
+  const randomPick = contacts[Math.floor(Math.random() * contacts.length)]
+ 
+  this.setState({fiveContacts: [randomPick, ...this.state.fiveContacts]})
+
+
+}
+
+removeActor(actor){
+  
+const spreadContacts = [...this.state.fiveContacts];
+
+spreadContacts.splice(actor, 1);
+
+this.setState({fiveContacts: spreadContacts})
+
+}
+
+sortAlphabetically = () => {
+
+  // for(let i = 0; i<this.state.fiveContacts.length; i++){
+  //   const newfive = this.state.fiveContacts
+  //   if(a.newfive[i].name < b.newfive[i].name){
+  //     return -1;
+  //   }else if (a.newfive[i].name > b.newfive[i].name) {
+  //     return 1
+  //   }
+  // }
+ 
+
+  //   if(a.this.state.fiveContacts < b.this.state.fiveContacts){
+  //     return -1
+  //   }else if(a.this.state.fiveContacts > b.this.state.fiveContacts){
+  //     return 1
+  //   }
+
+  // }
+
+ 
+  this.state.fiveContacts.sort((a, b) => {
+    const isSorted = (this.state.sortType === 'a') ? 1 : -1;
+    return isSorted * a.name.localeCompare(b.name)
+  })
+
+  this.setState({sortType: this.state.sortType})
+
+
+}
+
+sortByPopularity = () => {
+
+
+ 
+  this.state.fiveContacts.sort((a, b) => {
+    const isSorted = (this.state.sortType === 0) ? 1 : -1;
+    return isSorted * a.name.localeCompare(b.name)
+  })
+
+  this.setState({sortPop: this.state.sortPop})
+
+
+}
+
+
+
+
+
+  render() {
+
+    
+
+    return (
+      <div>
+        <h1>IronContacts</h1>
+
+<button onClick={this.addRandomContact}>Add Contact</button>
+<button onClick={this.sortAlphabetically}>Sort By Name</button>
+<button onClick={this.sortByPopularity}>Sort By popularity</button>
+
+
+
+        <table>
+           <thead>
+             <tr>
+               <th>Picture</th>
+               <th>Name</th>
+               <th>Popularity</th>
+               <th>Delete</th>
+             </tr>
+             
+           </thead>
+        {this.state.fiveContacts.map((contact, index) => (
+          <tbody key={contact.id}>
+            <tr >
+            <td>
+                <img src={contact.pictureUrl} style={{width:"80px"}} />
+              </td>
+              <td>
+                {contact.name}
+              </td>
+             
+              <td>
+                {contact.popularity.toFixed(2)}
+              </td>
+              <td>
+              <button onClick={() => this.removeActor(index)}>Remove Actor</button>
+              </td>
+            
+              
+            </tr>
+
+
+          </tbody> 
+          
+        ))}
+        </table>
+      </div>
+    )
+  }
+}
+
+
+
+
+
+
+
+
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      MFs
+      <Contacts/>
+      
     </div>
   );
 }
