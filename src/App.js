ae820a2e1378f0d9a7d5b66d1289d8ba9bf006b1
eduardoLemes <eduardo_lsf@outlook.com>
2021-03-15 22:12:30 -0300

    done

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from "react";
 import contacts from "./contacts.json";
-import  './App.css'
+import "./App.css";
 
 class App extends React.Component {
   constructor(props) {
@@ -9,6 +9,7 @@ class App extends React.Component {
       data: contacts.slice(0, 5),
     };
   }
+
   randomContacts = () => {
     let randomCont = contacts[Math.floor(Math.random() * contacts.length)];
     const dataCopy = [...this.state.data];
@@ -18,42 +19,57 @@ class App extends React.Component {
     });
   };
 
+  sortName = () => {
+    const dataCopy = [...this.state.data];
+    dataCopy.sort((a, b) => {
+      if (a.name < b.name) {
+        return -1;
+      }
+      if (a.name > b.name) {
+        return 1;
+      }
+      return 0;
+    });
+    this.setState({ data: dataCopy });
+  };
+
   render() {
     return (
       <div>
-        <button onClick={() => this.randomContacts()}>ADD Random Contacts</button>
-        {this.state.data.map((element) => {
-          return (
-            <div>
-              <h1>Iron contacts</h1>
-              <table>
+        <button onClick={() => this.randomContacts()}>
+          ADD Random Contacts
+        </button>
+        <button onClick={() => this.sortName()}>Sort by name</button>
+        <div>
+          <h1>Iron contacts</h1>
+          <div>
+            <table>
+              <thead>
                 <tr>
                   <th>Picture</th>
                   <th>Name</th>
                   <th>Popularity</th>
                 </tr>
-                <tr>
-                  <td>
-                    <img className="image" src={element.pictureUrl} />
-                  </td>
-                  <td>{element.name}</td>
-                  <td>{element.popularity}</td>
-                </tr>
-              </table>
-            </div>
-          );
-        })}
+              </thead>
+              <tbody>
+                {this.state.data.map((element) => {
+                  return (
+                    <tr key={element.id}>
+                      <td>
+                        <img className="image" src={element.pictureUrl} />
+                      </td>
+                      <td>{element.name}</td>
+                      <td>{element.popularity}</td>
+                    </tr>
+                  );
+                })}
+              </tbody>
+            </table>
+          </div>
+        </div>
       </div>
     );
   }
 }
-
-// function App() {
-//   return (
-//     <div className="App">
-
-//     </div>
-//   );
-// }
 
 export default App;
