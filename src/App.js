e661b9c42b0d3e18e9288d5fcc5c0616d46ced93
Romain B <romain.bnd@gmail.com>
2021-03-16 18:09:15 +0100

    Done all iterations

--- src/App.js
+++ src/App.js
@@ -1,25 +1,80 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import contactsJSON from "./contacts.json";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import React, { Component } from "react";
+
+class App extends Component {
+  state = {
+    contact: contactsJSON.slice(0, 5),
+  };
+
+  randomIndex() {
+    return Math.floor(Math.random() * contactsJSON.length);
+  }
+
+  handleRandomActor = () => {
+    const newRandomActor = contactsJSON[this.randomIndex()];
+    const newList = [newRandomActor, ...this.state.contact];
+    this.setState({ contact: newList }, () => console.log(newList));
+  };
+  handleSortByName = () => {
+    const sortedArr = [...this.state.contact].sort((a, b) =>
+      b.name < a.name ? 1 : -1
+    );
+    this.setState({ contact: sortedArr });
+  };
+
+  handleSortByPopularity = () => {
+    const sortedArr = [...this.state.contact].sort(
+      (a, b) => b.popularity - a.popularity
+    );
+    this.setState({ contact: sortedArr });
+  };
+
+  handeDeleteActor = (id) => {
+    const copyActors = this.state.contact.filter((actor) => actor.id !== id);
+    this.setState({ contact: copyActors });
+  };
+
+  render() {
+    return (
+      <div className="Container">
+        <button onClick={this.handleRandomActor}>Random Actor</button>
+        <button onClick={this.handleSortByName}>Sort by name</button>
+        <button onClick={this.handleSortByPopularity}>
+          Sort by popularity
+        </button>
+        <table>
+          <thead>
+            <tr>
+              <td>Picture</td>
+              <td>Name</td>
+              <td>Popularity</td>
+              <td>Delete</td>
+            </tr>
+          </thead>
+          <tbody>
+            {this.state.contact.map((actor) => (
+              <tr key={actor.id}>
+                <td>
+                  {" "}
+                  <img src={actor.pictureUrl} alt="portraits" />{" "}
+                </td>
+                <td>{actor.name}</td>
+                <td>{actor.popularity.toFixed(2)}</td>
+                <td>
+                  <button onClick={() => this.handeDeleteActor(actor.id)}>
+                    Delete
+                  </button>
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
+    );
+  }
 }
 
 export default App;
