6bb0c6e8cbd88f37971545af6ecd4e734c5c1080
Victoria V <virlan.victoria@gmail.com>
2021-03-17 08:44:21 +0100

    iteration 1-4

--- src/Components/Contact.jsx
+++ src/Components/Contact.jsx
@@ -5,30 +5,69 @@ class Contacts extends Component {
     state= {
         contacts: contactsJSON.slice(0,5),
     };
-    handleRandomActor () {
+    handleRandomActor =() =>{
         const copyContacts = [...this.state.contacts];
+        
 
-       let randomActor = copyContacts => copyContacts[Math.floor(Math.random()* copyContacts.length)];
-      
+       let addrandomContact = copyContacts => copyContacts[Math.floor(Math.random()* this.copyContacts.length)];
+       copyContacts.push(addrandomContact);
+       this.setState({contacts:copyContacts});
     }
+
+    sortByName =()=>{
+        const copyContacts = [...this.state.contacts];
+        let sortedNames = copyContacts => copyContacts.sort((a,b)=> a.name.localeCompare(b.name))
+        
+
+        this.setState({contacts:copyContacts});
+        return sortedNames;
+    }
+    sortByPopularity =()=>{
+        const copyContacts = [...this.state.contacts];
+        let sortedPopularity = copyContacts => copyContacts.sort((a,b)=> a.popularity.localeCompare(b.popularity))
+        
+
+        this.setState({contacts:copyContacts});
+   
+    }
+    handleRemoveContact(contactToRemoveIndex){
+        const copyContacts = [...this.state.contacts];
+        copyContacts.splice(contactToRemoveIndex,1);
+        this.setState({contacts:copyContacts});
+    }
+
+
 
     render (){
         return (
             <div>
+                <div className ="btns">
+        <div className="button">
+        <button onClick={this.handleRandomActor}>Add Random Contact</button>
+        </div>
+        <div className="button">
+        <button onClick={this.sortByName}>Sort by Name</button>
+        </div>
+        <div className="button">
+        <button onClick={this.sortByPopularity}>Sort by Popularity</button>
+        </div>
+        </div>
                 <table>
                  <thead>
                      <tr>
                          <th>Picture</th>
                          <th>Name</th>
                          <th>Popularity</th>
+                         <th>Action</th>
                      </tr>
                  </thead>
                 {this.state.contacts.map((contacts)=> (
              <tbody>
                  <tr className ="table">
             <td><img className ="image" src = {contacts.pictureUrl}/></td>
-            <td class ="table">{contacts.name}</td>
-            <td class ="table">{Math.round(contacts.popularity *100)/100}</td>
+            <td className ="table">{contacts.name}</td>
+            <td className ="table">{Math.round(contacts.popularity *100)/100}</td>
+            <td className ="table"><button onClick={this.state.handleRemoveContact}>Delete</button></td>
             
                  </tr>
              </tbody>
