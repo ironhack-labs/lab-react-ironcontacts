50fac14f73042c51bfc5ca96d22578a77e709150
maudfabrello <ma.fabrello@gmail.com>
2021-03-17 09:02:59 +0100

    to be continued

--- src/App.js
+++ src/App.js
@@ -1,25 +1,69 @@
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
+import contacts from './contacts.json';
+import React, { Component } from "react";
+
+class Contacts extends Component {
+  state = {
+    allcontacts: contacts.slice(0, 5),
+  };
+
+
+render() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <div>
+      <table id="contacts">
+        <thead>
+          <th>Picture</th>
+          <th>Name</th>
+          <th>Popularity</th>
+          </thead>
+          {this.state.allContacts.map((contact) => (
+          <tbody key={contact.id}>
+            <tr>
+              <td>
+                <img src={contact.pictureUrl} alt=""></img>
+                </td>
+                <td>{contact.name}</td>
+                <td>{contact.popularity}</td>
+              </tr>
+            </tbody>
+          ))}
+        </table>
+      </div>
+    );
+  }
+}
+    
+
+/*
+  function App() {
+    return (
+      <div className="App">
+  
+        {contacts
+          .map((contact) => {
+            return (
+              <table>
+                <th>
+                  <img className="actorImg" src={contact.pictureUrl} img />{" "}
+                </th>
+                <th className="contact">{contact.name}</th>
+                <th>{contact.popularity}</th>
+              </table>
+            );
+          })
+          .slice(0, 5)}
+      </div>
+  
+    );
+  }
+  
+  export default App; */
+
+  
 }
 
 export default App;
+
