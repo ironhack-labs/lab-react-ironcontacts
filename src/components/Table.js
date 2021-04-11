7b1571dccb5961e44a0fd23c80871280697f14de
karuvsentor <karuvsentor@gmail.com>
2021-04-11 18:07:06 +0200

    it 2 done

--- src/components/Table.js
+++ src/components/Table.js
@@ -6,9 +6,22 @@ export default function Table() {
     let [contacts, setContact] = useState(Contacts.slice(0, 5))
 
 
+   let addContact = () => {
+        const newRandomContact = Contacts[Math.floor(Math.random() * (Contacts.length -1) + 5)]
+
+        setContact([
+            ...contacts,
+            newRandomContact
+        ])
+    }
+
+
+
+
     return (
         <div className="">
             <h1 className="title"> IronContacts</h1>
+                <button onClick={() => addContact()}> Add Random Contact</button>
             <table className="table">
                 <tr>
                     <th>Picture</th>
@@ -19,7 +32,7 @@ export default function Table() {
                 {contacts.map((e, id) => {
                     return (
                         <tr key={id}>
-                            <td><img width="100px" src={e.pictureUrl} /></td>
+                            <td><img width="95px" src={e.pictureUrl} /></td>
                             <td>{e.name}</td>
                             <td>{(e.popularity).toFixed(2)}</td>
                         </tr>
