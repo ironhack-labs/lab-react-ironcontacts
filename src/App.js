7263ab67902151ef669f7540d2c5aff0e398616f
antonicasasayasg <antoni.casasayas1@gmail.com>
2021-05-04 14:35:46 +0200

    iteration 1 done

--- src/App.js
+++ src/App.js
@@ -1,25 +1,45 @@
 import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import contacts from "./contacts.json";
 
 function App() {
+  
+  const displayActors = () => {
+    return contacts.map((contact) => {
+
+      return (
+        <div className="main">
+         
+          <table>
+            <thead>
+              <tr>
+                <th colspan="1">Picture</th>
+                <th colspan="1">Name</th>
+                <th colspan="1">Popularity</th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr>
+                <td>
+                  <img src={contact.pictureUrl}></img>
+                </td>
+                <td>{contact.name}</td>
+                <td>{contact.popularity}</td>
+              </tr>
+            </tbody>
+          </table>
+          
+        </div>
+      );
+    })
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>IronContacts</h1>
+      {displayActors()}
     </div>
   );
 }
 
 export default App;
+
