59544280b395bf30089c562b73866bbf33a483cf
antonicasasayasg <antoni.casasayas1@gmail.com>
2021-05-04 17:54:59 +0200

    iteration 4 done

--- src/App.js
+++ src/App.js
@@ -11,16 +11,13 @@ class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      arrayCopy: contacts.slice(0, 5)
-    }
+      arrayCopy: contacts.slice(0, 5),
+    };
   }
   displayActors() {
-    
-    
     return this.state.arrayCopy.map((contact, index) => {
       return (
         <div className="main">
-         
           <table>
             <thead>
               <tr>
@@ -36,38 +33,60 @@ class App extends React.Component {
                 </td>
                 <td>{contact.name}</td>
                 <td>{contact.popularity}</td>
+                <td>
+                  <button onClick={() => this.removeContact(contact.id)}>Delete</button>
+                </td>
               </tr>
             </tbody>
           </table>
-          
         </div>
       );
-    })
+    });
   }
 
   addContact() {
-   
     let randomContact = Math.floor(Math.random() * contacts.length);
-    let newArray = [... this.state.arrayCopy]
+    let newArray = [...this.state.arrayCopy];
     newArray.push(contacts[randomContact]);
-  
-    this.setState({ arrayCopy: newArray })
-  }
-    
-  
 
+    this.setState({ arrayCopy: newArray });
+  }
+
+  sortByPop() {
+    let newArray = [...this.state.arrayCopy];
+    newArray.sort(
+      (contact1, contact2) => contact2.popularity - contact1.popularity
+    );
+    this.setState({ arrayCopy: newArray });
+  }
+
+  sortByName() {
+    let newArray = [...this.state.arrayCopy];
+    newArray.sort((a, b) => a.name.localeCompare(b.name));
+      
+    this.setState({ arrayCopy: newArray });
+  }
+  
+  removeContact(id) {
+    
+
+    let filteredArray = this.state.arrayCopy.filter((contact) => contact.id !== id);
+    this.setState({arrayCopy: filteredArray })
+
+  }
   render() {
     return (
       <div className="App">
         <h1>IronContacts</h1>
         <button onClick={() => this.addContact()}>Add Random Contact</button>
+        <button onClick={() => this.sortByPop()}>Sort by popularity</button>
+        <button onClick={() => this.sortByName()}>Sort by name</button>
+        
+
         {this.displayActors()}
-      
       </div>
     );
   }
-
-  
 }
 
 
