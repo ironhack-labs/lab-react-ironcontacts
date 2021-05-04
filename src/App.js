059738ffeab5475029cd615b015aee41b6c45f0a
Sisco Falcó <siscofalcop@gmail.com>
2021-05-04 16:37:34 +0200

    Changes done

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import './App.css';
-import Table from './components/Table';
 import ContactList from './components/contactList'
 
 function App() {
   console.log("hola");
+  for(let i = 0; i < 5; i++){
+    
+  }
   return (
     <div className="appDiv">
       <ContactList />
