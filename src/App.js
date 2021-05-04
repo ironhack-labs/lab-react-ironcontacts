9a619d650382ed31fd4e9f793f8345e0b79d734c
Silinde87 <paurodriguezmolina@gmail.com>
2021-05-04 14:53:03 +0200

    Iteration 3

--- src/App.js
+++ src/App.js
@@ -1,52 +1,70 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 import contacts from "./contacts.json";
-import React from 'react';
+import React from "react";
 
 class App extends React.Component {
-  constructor (props) {
-    super(props) 
-    this.state = {
-      contactsList : []
-    }
-    for (let i = 0; i<5; i++){
-      this.state.contactsList.push(contacts[i])
-    } 
-    console.log(this.state.contactsList)
+	constructor(props) {
+		super(props);
+		this.state = {
+			contactsList: [],
+		};
+    //Iteration 1. Adding 5 initial movies to state.contactList
+		for (let i = 0; i < 5; i++) {
+			this.state.contactsList.push(contacts[i]);
+		}
+	}
+
+  //Iteration 2. This add's a new contact.
+	addContact() {
+		let newContact = contacts[Math.floor(Math.random() * contacts.length)];
+		let newContactsList = [...this.state.contactsList];
+		newContactsList.push(newContact);
+		this.setState({ contactsList: newContactsList });
+	}
+
+  //Iteration 3. Sorts
+  sortByPopularity(){
+    let arrayCopy = [...this.state.contactsList];
+    this.setState({contactsList: arrayCopy.sort((contact1, contact2) => contact1.popularity - contact2.popularity)});  
   }
-  addContact(){
-    let newContact = contacts[Math.floor(Math.random()*contacts.length)]
-    let newContactsList = [...this.state.contactsList]
-    newContactsList.push(newContact)
-    console.log(newContactsList)
-    this.setState({contactsList: newContactsList})
+  sortByName(){
+    let arrayCopy = [...this.state.contactsList];
+    this.setState({contactsList: arrayCopy.sort((contact1, contact2) => contact1.name.localeCompare(contact2.name))});
   }
-  render () {
-    return (
-      <div>
-        <button onClick={() => this.addContact()}>Add Random Contact</button>
-        <table>
-          <thead>
-            <tr>
-              <th>Picture</th>
-              <th>Name</th>
-              <th>Popularity</th>
-            </tr>
-          </thead>
-          <tbody>
-            {this.state.contactsList.map((contact, index) => {
-              return (
-                <tr key={contact.id+index}>
-                  <td><img src={contact.pictureUrl}></img></td>
-                  <td>{contact.name}</td>
-                  <td>{contact.popularity}</td>
-                </tr>
-              )
-            })}
-          </tbody>
-        </table>
-      </div>
-    );
-  }
+
+  //Iteration 4. Remove contact
+
+	render() {
+		return (
+			<div>
+				<button onClick={() => this.addContact()}>Add Random Contact</button>
+        <button onClick={() => this.sortByName()}>Sort by name</button>
+        <button onClick={() => this.sortByPopularity()}>Sort by popularity</button>
+				<table>
+					<thead>
+						<tr>
+							<th>Picture</th>
+							<th>Name</th>
+							<th>Popularity</th>
+						</tr>
+					</thead>
+					<tbody>
+						{this.state.contactsList.map((contact, index) => {
+							return (
+								<tr key={contact.id + index}>
+									<td>
+										<img src={contact.pictureUrl}></img>
+									</td>
+									<td>{contact.name}</td>
+									<td>{contact.popularity}</td>
+								</tr>
+							);
+						})}
+					</tbody>
+				</table>
+			</div>
+		);
+	}
 }
 export default App;
