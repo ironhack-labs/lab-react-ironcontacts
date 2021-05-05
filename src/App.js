8a908fb8c93dbe67c2674a41b258532453a57b3c
Raquel Rodriguez <srtademaciel@gmail.com>
2021-05-05 16:09:17 +0200

    iteration 1 and 2

--- src/App.js
+++ src/App.js
@@ -1,25 +1,47 @@
-import logo from './logo.svg';
-import './App.css';
+import contacts from "./contacts.json";
+import React from "react";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  state = {
+    fiveFirstContacts: contacts.slice(0, 5)
+  }
+
+  randomContact = () => {
+    const random = contacts[Math.floor(Math.random() * contacts.length-5)+5]
+    const fiveFirstContactsCopy = [...this.state.fiveFirstContacts]
+    console.log(fiveFirstContactsCopy)
+   fiveFirstContactsCopy.push(random)
+    this.setState({...this.state, fiveFirstContacts: fiveFirstContactsCopy})
+  }
+
+
+  render() {
+    const contactList = this.state.fiveFirstContacts.map((contact, index) => (
+      <tr key={index}>
+        <td ><img src={contact.pictureUrl} alt={contact.name} className='contactImg'/></td>
+        <td>{contact.name}</td>
+        <td>{contact.popularity.toFixed(2)}</td>
+      </tr>
+      ));  
+
+    return (
+      <div className="App">
+        <div className='Contacts-table'>
+        <h1>IronContacts</h1>
+        <button onClick={this.randomContact}>Add Random Contact</button>
+          <table>
+            <tr>
+              <th>Picture</th>
+              <th>Name</th>
+              <th>Popularity</th>
+            </tr>
+            {contactList}
+          </table>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
