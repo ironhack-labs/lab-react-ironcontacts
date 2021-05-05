566bc56b1b81c34d04b5e18f5f16a3acfb1e7459
Antonio Vitale <vitalb2b@Antonios-MacBook-Air.local>
2021-05-04 22:38:37 -0300

    changes

--- src/App.js
+++ src/App.js
@@ -1,25 +1,45 @@
 import logo from './logo.svg';
 import './App.css';
+import contacts from './contacts.json';
 
 function App() {
+
+  const DisplayFiveContacts = (contacts) => {
+
+    let list = [];
+
+    for( let i =0; i <= 4; i++){
+
+      list.push(  
+        <tr> 
+            <td> <img src={contacts[i].pictureUrl}/> </td>
+            <td>{contacts[i].name} </td>
+            <td>{contacts[i].popularity}  </td>
+        </tr> 
+
+       )
+    }
+    return list;
+
+  }
+  
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      
+      <table className="">
+        <tr>
+          <th>Picture </th>
+          <th>Name </th>
+          <th>Popularity </th>
+          
+        </tr>
+
+      {DisplayFiveContacts(contacts)}
+      </table>
+      
     </div>
   );
 }
+
 
 export default App;
