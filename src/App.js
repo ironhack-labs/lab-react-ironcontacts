22284a562b0ea9a0c6d774d74fa9ea165da24c38
alescofet <alex.escofet96@gmail.com>
2021-05-05 16:24:45 +0200

    Iteration 1 and 2 done

--- src/App.js
+++ src/App.js
@@ -1,25 +1,49 @@
 import logo from './logo.svg';
 import './App.css';
+import contacts from "./contacts.json";
+import React from 'react';
 
-function App() {
-  return (
+
+class App extends React.Component {
+  state={
+    contactList: contacts.slice(0,5)
+  }
+  render(){
+    const fiveContacts=this.state.contactList.map((contact,index)=>(
+     <tr key={index}>
+       <td> <img src={contact.pictureUrl} alt=""/> </td>
+       <td>{contact.name}</td>
+       <td>{contact.popularity.toFixed(2)}</td>
+     </tr>
+    ))
+    const addRandom=(()=>{
+      let num = Math.floor(Math.random()*(contacts.length-6)+6)
+      let copyOfState={...this.state}
+      copyOfState.contactList.push(contacts[num])  
+      this.setState(copyOfState)
+    })
+     return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <table className="actors">
+            <thead>
+            <tr>
+              <th>Picture</th>
+              <th>Name</th>
+              <th>Popularity</th>
+            </tr>
+            </thead>
+              <tbody>
+                {fiveContacts}
+              </tbody>
+            
+          </table>
       </header>
+      <button onClick={()=>{addRandom()}}>Add Random Actor</button>
     </div>
   );
+  }
+ 
 }
 
 export default App;
