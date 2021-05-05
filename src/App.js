2c133ac47e3523f3c7c2d0ce2f58f98b07a2b493
Katarina Orlovska <k.orlovska@posteo.de>
2021-05-05 13:37:34 +0200

    the slice/setState of remove still to fix

--- src/App.js
+++ src/App.js
@@ -1,25 +1,76 @@
 import logo from './logo.svg';
 import './App.css';
+import contacts from './contacts.json'
+import React, { Component } from 'react'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+
+  state = {
+    currentContacts: contacts.slice(0,5)
+  }
+
+  addRandomContact = () => { 
+    const random = Math.floor(Math.random()*contacts.length)
+    const randomContact = contacts[random]
+
+    this.setState((state) =>  ({ 
+      currentContacts: [...state.currentContacts, randomContact]
+    })) 
+  }
+
+  sortAlphabetically = () => { 
+    //console.log("hello");
+    this.setState((state) => ({
+      currentContacts: state.currentContacts.sort( (a,b) => a.name.localeCompare(b.name))
+    }))}
+  
+  sortPopularity = () => { 
+    console.log("hello");
+    this.setState((state) => ({
+    currentContacts: state.currentContacts.sort( (a,b) => b.popularity - a.popularity)
+  }))}
+
+  removeContact = (id) => {
+    const index = this.state.currentContacts.findIndex(contact => contact.id===id);
+      this.state.currentContacts.splice(index, 1);
+      this.setState( (state) => ({currentContacts: state.currentContacts})
+      )
+  }
+
+  render () {
+    //console.log(this.state.currentContacts);
+
+    const mappedContacts = this.state.currentContacts.map( contact => (
+      <tr key={contact.id}>
+        <td><img src={contact.pictureUrl} height="100px"/></td>
+        <td>{contact.name}</td>
+        <td>{contact.popularity}</td>
+        <td><button onClick={() => this.removeContact(contact.id)}>Remove contact</button></td>
+      </tr>
+     ))
+
+    return (
+      <>
+        <button onClick={this.addRandomContact}>Add new contact</button>
+        <button onClick={this.sortAlphabetically}>Sort alphabetically</button>
+        <button onClick={this.sortPopularity}>Sort by popularity</button>
+        <table>
+          <thead>
+              <tr>
+                <td>Picture</td>
+                <td>Name</td>
+                <td>Popularity</td>
+              </tr>
+          </thead>
+          <tbody>
+            {mappedContacts}
+          </tbody>
+        </table>
+      </>
+    )
+  }
 }
 
 export default App;
+
+ 
