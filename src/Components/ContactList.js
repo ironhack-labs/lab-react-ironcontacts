f5fac0d1ea77e843d6fea87c3b67f7030c9e357f
Alkin Maystorov <alkin971@gmail.com>
2021-05-05 11:01:46 +0200

    Iteration 2

--- src/Components/ContactList.js
+++ src/Components/ContactList.js
@@ -3,26 +3,34 @@ import Contacts from "../contacts.json";
 import "./ContactList.css";
 
 export default function ContactList() {
-  let [contacts] = useState(Contacts.slice(0, 5));
+  let [contacts, setContact] = useState(Contacts.slice(0, 5));
+
+  let addContact = () => {
+    const newRandomContact =
+      Contacts[Math.floor(Math.random() * (Contacts.length - 1) + 5)];
+
+    setContact([...contacts, newRandomContact]);
+  };
 
   return (
     <div className="">
       <h1 className="title"> IronContacts</h1>
       <table className="table">
+        <button onClick={() => addContact()}> Add Random Contact</button>
         <tr>
           <th>Picture</th>
           <th>Name</th>
           <th>Popularity</th>
         </tr>
 
-        {contacts.map((e, id) => {
+        {contacts.map((contact, id) => {
           return (
             <tr key={id}>
               <td>
-                <img width="80px" src={e.pictureUrl} />
+                <img width="80px" src={contact.pictureUrl} />
               </td>
-              <td>{e.name}</td>
-              <td>{e.popularity.toFixed(2)}</td>
+              <td>{contact.name}</td>
+              <td>{contact.popularity.toFixed(2)}</td>
             </tr>
           );
         })}
