3cfd1c2fe2ae792a5b98df66dfabb30a3e05f08a
Steffen Leppin <leppinsteffen@gmail.com>
2021-05-05 21:04:48 +0200

    done

--- src/App.js
+++ src/App.js
@@ -1,25 +1,80 @@
-import logo from './logo.svg';
 import './App.css';
+import contacts from "./contacts.json";
+import React from 'react'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+let arrayOfFive = contacts.slice(0, 5);
+
+class App extends React.Component {
+  state = {
+    Actors: arrayOfFive
+  }
+
+  addRandom = (props) => {
+    const randomActor = contacts[Math.floor(Math.random() * contacts.length)]
+    const ActorsCopy = [...this.state.Actors]
+    this.setState(() => {
+      return { Actors: [...ActorsCopy, randomActor] }
+    })
+  }
+
+  sortByName = (props) => {
+    const sortedNameArray = this.state.Actors.sort((a, b) => {
+      return a.name.localeCompare(b.name)
+    })
+    this.setState({
+      Actors: sortedNameArray
+    })
+  }
+
+  sortByPop = (props) => {
+    const sortedPopArray = this.state.Actors.sort((a, b) => {
+      return b.popularity - a.popularity
+    })
+    this.setState({
+      Actors: sortedPopArray
+    })
+  }
+
+  delete = (id) => {
+    const newArray = this.state.Actors.filter((item) => item.id !== id);
+    this.setState((state) => {
+      return { Actors: newArray }
+    })
+  }
+
+  render() {
+    const Actors = this.state.Actors.map(actor => {
+      return (
+        <tr key={ actor.id }>
+          <td><img src={ actor.pictureUrl } alt="" /></td>
+          <td>{ actor.name }</td>
+          <td>{ actor.popularity.toFixed(2) }</td>
+          <td><button onClick={ () => { this.delete(actor.id) } }>remove</button></td>
+        </tr>
+      )
+    })
+    return (
+      <div className="container" >
+        <h1>IronContacts</h1>
+        <button onClick={ this.addRandom }>add random actor</button>
+        <button onClick={ this.sortByName }>sort by name</button>
+        <button onClick={ this.sortByPop }>sort by popularity</button>
+        <table>
+          <thead>
+            <tr>
+              <th>Picture</th>
+              <th>Name</th>
+              <th>Popularity</th>
+              <td>Action</td>
+            </tr>
+          </thead>
+          <tbody>
+            { Actors }
+          </tbody>
+        </table>
+      </div>
+    );
+  }
 }
 
 export default App;
