342ab92438b0a1f7bd5fbb7d599aeff553298026
Antonio Vitale <vitalb2b@Antonios-MacBook-Air.local>
2021-05-06 22:19:28 -0300

    changes

--- src/App.js
+++ src/App.js
@@ -1,31 +1,76 @@
+
+import React from 'react'; 
 import logo from './logo.svg';
 import './App.css';
-import contacts from './contacts.json';
+import contactsJson from './contacts.json';
+
+const contactsSlice = contactsJson.splice(0,5);
 
 function App() {
 
-  const DisplayFiveContacts = (contacts) => {
+  const [contacts, setContacts] = React.useState(contactsSlice) 
+  const DisplayFiveContacts = () => {
 
-    let list = [];
+        return contacts.map(contact => {
+          
+          return (
+            <tr> 
+                <td> <img src={contact.pictureUrl} alt="teste" />  </td>
+                <td>{contact.name} </td>
+                <td>{contact.popularity}  </td>
 
-    for( let i =0; i <= 4; i++){
+                <td><button onClick={() => remove(contact.id)}>Delete</button> </td>
+            </tr>
+          )
 
-      list.push(  
-        <tr> 
-            <td> <img src={contacts[i].pictureUrl}/> </td>
-            <td>{contacts[i].name} </td>
-            <td>{contacts[i].popularity}  </td>
-        </tr> 
-
-       )
-    }
-    return list;
-
+        })
+         
   }
-  
+
+
+  const remove = (id) =>{
+
+    const contactIndex = contacts.findIndex((element) =>{return element.id === id});
+    const newArray = [...contacts];
+    newArray.splice(contactIndex, 1);
+
+    console.log(contacts)
+    setContacts(newArray);
+  }
+
+  const randomContact = () =>{
+
+      const randomIndex =  Math.floor(Math.random() * contactsJson.length);
+      const chosenContact = contactsJson[randomIndex];
+      setContacts([...contacts, chosenContact]);
+  }
+
+
+  const sortByName = () =>{
+
+    const newArray = [...contacts];
+    newArray.sort((a,b) =>{ return a.name.localeCompare(b.name)} );
+    
+    setContacts(newArray);
+
+ }
+
+ const sortByPop = () =>{
+
+  const newArray = [...contacts];
+  newArray.sort((a,b) =>{ return b.popularity - a.popularity} );
+  setContacts(newArray);
+
+}
+
   return (
     <div className="App">
       
+      <button onClick={randomContact}>Add random contact </button>
+      <button onClick={sortByName}>Sort by name </button>
+      <button onClick={sortByPop}>Sort by popularity </button>
+
+
       <table className="">
         <tr>
           <th>Picture </th>
@@ -34,12 +79,11 @@ function App() {
           
         </tr>
 
-      {DisplayFiveContacts(contacts)}
+      {DisplayFiveContacts()}
       </table>
       
     </div>
   );
 }
-
 
 export default App;
