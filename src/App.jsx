15ba493e09340e8005c504f8b09f75947e9f9488
Aleksandra Pietraszek <ola.pietraszek@gmail.com>
2021-05-08 16:51:27 +0200

    iteration 3

--- src/App.jsx
+++ src/App.jsx
@@ -7,31 +7,47 @@ let firstFive = contacts.slice(0, 5);
 
 function App() {
   const [contactsArr, setContactsArr] = React.useState(firstFive);
+
   function addRandomContact() {
     const randomContact = contacts.slice(5)[
       Math.floor(Math.random() * (contacts.length - 5))
     ];
-    const newArr = firstFive.push(randomContact);
+    const newArr = [...firstFive, randomContact];
     setContactsArr(newArr);
   }
+
+  function sortByName() {
+    const newArr = [...contactsArr].sort((a, b) => {
+      return a.name.localeCompare(b.name);
+    });
+    setContactsArr(newArr);
+  }
+
+  function sortByPopularity() {
+    const newArr = [...contactsArr].sort((a, b) => b.popularity - a.popularity);
+    setContactsArr(newArr);
+  }
+
   return (
     <div className="App">
       <h1>IronContacts</h1>
-      <button onClick={addRandomContact}>Add Random Contacts</button>
+      <button onClick={addRandomContact}>Add random contact</button>
+      <button onClick={sortByName}>Sort by name</button>
+      <button onClick={sortByPopularity}>Sort by popularity</button>
       <table>
         <tr>
           <th>Picture</th>
           <th>Name</th>
           <th>Popularity</th>
         </tr>
-        {firstFive.map((person, index) => {
+        {contactsArr.map((person, index) => {
           return (
             <tr>
               <td>
                 <img src={person.pictureUrl}></img>
               </td>
               <td>{person.name}</td>
-              <td>{person.popularity}</td>
+              <td>{person.popularity.toFixed(2)}</td>
             </tr>
           );
         })}
