2a8e7c59388da34141ea1a65d65b84e0583445c3
Tomsylva <tomsylvanz@gmail.com>
2021-05-08 15:49:03 +0200

    iteration 1 complete

--- src/App.js
+++ src/App.js
@@ -1,9 +1,30 @@
 import "./App.css";
+import contacts from "./contacts.json";
 
 function App() {
   return (
     <div className="App">
-      <p>Oidaaaa Spozi</p>
+      <p>Ironcontacts</p>
+      <table>
+        <tr>
+          <th>PICTURE</th>
+          <th>NAME</th>
+          <th>POPULARITY</th>
+        </tr>
+
+        {contacts.map((contact) => {
+          const { pictureUrl, name, popularity } = contact;
+          return (
+            <tr>
+              <td>
+                <img src={pictureUrl} alt="image" style={{ height: "80px" }} />
+              </td>
+              <td>{name}</td>
+              <td>{popularity.toFixed(2)}</td>
+            </tr>
+          );
+        })}
+      </table>
     </div>
   );
 }
