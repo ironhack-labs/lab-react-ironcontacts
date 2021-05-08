8d38ce83c653be1cc60dc9996e6f51fc43b11e96
frankie.eckersley-carr <frankie.eckersley-carr@outlook.com>
2021-05-08 19:05:34 +0200

    complete iterrations

--- src/App.js
+++ src/App.js
@@ -1,25 +1,95 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+
+import contacts from "./contacts.json";
 
 function App() {
+  const [contactsArr, setContactsArr] = React.useState(contacts.slice(0, 5));
+
+  function addRandomContact() {
+    const randomContact = [
+      contacts.slice(6, contacts.length)[
+        Math.floor(Math.random() * contacts.length)
+      ],
+    ];
+    const newArr = contactsArr.concat(randomContact);
+    // const newArr = [...contactsArr, randomContact] would also work! NP random contact should no longer be in an array then
+    setContactsArr(newArr);
+  }
+
+  function sortName() {
+    const newArr = [...contactsArr].sort((a, b) => {
+      return a.name.localeCompare(b.name);
+    });
+    setContactsArr(newArr);
+  }
+
+  function sortPopularity() {
+    const newArr = [...contactsArr].sort((a, b) => {
+      return a.popularity - b.popularity;
+    });
+    setContactsArr(newArr);
+  }
+
+  function deleteContact(id) {
+    const newArr = contactsArr.filter((contact) => contact.id !== id);
+    setContactsArr(newArr);
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>IronContacts</h1>
+      <button onClick={addRandomContact}>Add Random Contact</button>
+      <button onClick={sortName}>Sort by name</button>
+      <button onClick={sortPopularity}>Sort by popularity</button>
+
+      <table>
+        <thead>
+          <tr>
+            <th>Picture</th>
+            <th>Name</th>
+            <th>Popularity</th>
+          </tr>
+        </thead>
+        <tbody>
+          {contactsArr.map((contact) => {
+            return (
+              <tr key={contact.id}>
+                <td>{contact.name}</td>
+                <td>
+                  <img src={contact.pictureUrl} width="100px" />
+                </td>
+                <td>{contact.popularity.toFixed(2)}</td>
+                <td>
+                  <button onClick={() => deleteContact(contact.id)}>
+                    Delete
+                  </button>
+                </td>
+              </tr>
+            );
+          })}
+        </tbody>
+      </table>
     </div>
   );
 }
+
+// function Contact(props) {
+//   const { name, pictureUrl, popularity, id, deleteContact } = props;
+//   console.log(deleteContact);
+//   return (
+//     <tr>
+//       <td>{name}</td>
+//       <td>
+//         <img src={pictureUrl} width="100px" />
+//       </td>
+//       <td>{popularity.toFixed(2)}</td>
+//       <td>
+//         <button onClick={deleteContact(id)}>Delete</button>
+//       </td>
+//     </tr>
+//   );
+// }
 
 export default App;
