71c957dca6e3bf7503f2b2e874772c10523d949d
Gizem <gizemwanka@gmail.com>
2021-05-08 16:33:46 +0200

    add iteration 1

--- src/App.js
+++ src/App.js
@@ -1,25 +1,41 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import contacts from "./contacts.json";
+import React from "react";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    // console.log(contacts);
+
+    const [contact, setContact] = React.useState(contacts.slice(0, 5));
+
+    return (
+        <div className="App">
+            <h1>IronContacts</h1>
+            <table>
+                <tbody>
+                    <tr>
+                        <th>Picture</th>
+                        <th>Name</th>
+                        <th>Popularity</th>
+                    </tr>
+
+                    {contacts.map((contact) => {
+                        return (
+                            <tr key={contact.id}>
+                                <td>
+                                    <img
+                                        width="70px"
+                                        src={contact.pictureUrl}
+                                    />
+                                </td>
+                                <td>{contact.name}</td>
+                                <td>{contact.popularity.toFixed(2)}</td>
+                            </tr>
+                        );
+                    })}
+                </tbody>
+            </table>
+        </div>
+    );
 }
 
 export default App;
