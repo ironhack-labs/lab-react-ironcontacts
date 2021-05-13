c0a9055ec6a09472666961ef50b7ca6fe94f8318
Aleksandra Porada <aleksandra.porada92@gmail.com>
2021-05-13 09:54:42 +0200

    Struggle

--- src/App.js
+++ src/App.js
@@ -1,23 +1,74 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import contacts from "./contacts.json";
 
 function App() {
+  const newArr = contacts.slice(0, 5);
+  const [contactsArr, setContactsArr] = React.useState(contacts.slice(0, 5));
+
+  function presentCelebrity(props) {
+    return (
+      <tr>
+        <td>
+          <img
+            src={props.pictureUrl}
+            alt={props.name}
+            style={{ height: "150px" }}
+          ></img>
+        </td>
+        <td>{props.name}</td>
+        <td>{props.popularity.toFixed(2)}</td>
+        <td>
+          <button onClick={() => props.deleteContact(props.index)}>
+            Delete
+          </button>
+        </td>
+      </tr>
+    );
+  }
+
+  function addRandomContact() {
+    const randomIndex = Math.floor(Math.random() * contacts.length);
+
+    setContactsArr([contacts[randomIndex], ...contactsArr]);
+  }
+
+  function deleteContact(index) {
+    const newArr = [...contactsArr].filter((contact, idx) => idx !== index);
+    setContactsArr(newArr);
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Contacts</h1>
+      <button style={{ marginBottom: "50px" }} onClick={addRandomContact}>
+        Add Random Contact
+      </button>
+      <table>
+        <thead>
+          <tr>
+            <th>Picture</th>
+            <th>Name</th>
+            <th>Popularity</th>
+          </tr>
+        </thead>
+        <tbody>
+          {contactsArr.map((contact, index) => {
+            function deleteThisContact() {
+              deleteContact(index);
+            }
+            return (
+              <presentCelebrity
+                {...contact}
+                index={index}
+                key={`${contact.id} – ${index}`}
+                deleteContact={deleteContact}
+              ></presentCelebrity>
+            );
+          })}
+        </tbody>
+      </table>
     </div>
   );
 }
