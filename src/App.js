cf1acb8cc5438e23da069bb499a7a877c8d318f1
Mario P. Cardenas <15961362+Mario-paul@users.noreply.github.com>
2021-06-29 16:34:12 -0400

    iteration 1 to 3 complete

--- src/App.js
+++ src/App.js
@@ -1,23 +1,82 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import contacts from "./contacts.json";
+import React, { useState } from "react";
 
 function App() {
+  let [list, setList] = useState(contacts.splice(0, 5));
+  let [rest, setRest] = useState(contacts);
+
+  function fillTable() {
+    return list.map((contact, i) => {
+      //console.log(contact);
+      return (
+        <tr key={i}>
+          <td>
+            <img
+              src={contact.pictureUrl}
+              style={{ width: "6vw" }}
+              alt="contact"
+            ></img>
+          </td>
+          <td>{contact.name}?</td>
+          <td>{contact.popularity.toFixed(2)}</td>
+          <td><button >Delete</button></td>
+        </tr>
+      );
+    });
+  }
+
+  function randomContact() {
+    let newRest = [...rest];
+    let newList = [...list];
+    let newContact = newRest[Math.floor(Math.random() * newRest.length)];
+    newList.push(newContact);
+    newRest.splice(newRest.indexOf(newContact), 1);
+    setList(newList);
+    setRest(newRest);
+  }
+
+  function sortPopular() {
+    let popularList = [...list];
+    popularList.sort((a, b) => b.popularity - a.popularity);
+    setList(popularList);
+  }
+
+  function sortName() {
+    let nameList = [...list];
+    nameList.sort(function (a, b) {
+      if (a.name < b.name) {
+          return -1;
+      }
+      if (b.name < a.name) {
+          return 1;
+      }
+      return 0;
+  });
+    setList(nameList)
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>IronContacts</h1>
+      <button onClick={randomContact}> Add random contact</button>
+      <button onClick={sortName}> Sort by name ▲</button>
+      <button onClick={sortPopular}> Sort by popularity ▼</button>
+
+      <div style={{ display: "flex", justifyContent: "center" }}>
+        <table>
+          <thead>
+            <tr>
+              <th>Picture</th>
+              <th>Name</th>
+              <th>Popularity</th>
+              <th>Action</th>
+            </tr>
+          </thead>
+          <tbody>{fillTable()}</tbody>
+        </table>
+      </div>
     </div>
   );
 }
