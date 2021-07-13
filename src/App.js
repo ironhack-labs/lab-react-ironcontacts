fd6edeb60e0e55d371830c5632d1d8a59b4292e3
George DS Brooks <george@gdsbrooks.com>
2021-07-13 15:54:21 +0100

    i3 sort passes sortType parameter to single sort method

--- src/App.js
+++ src/App.js
@@ -12,10 +12,27 @@ function App() {
     const randomContact = contacts[(Math.floor(Math.random() * contacts.length))]
     updateContacts([...myContacts, randomContact])
   }
+
+  const handleSort = (sortType) => {
+    const sortedContacts = JSON.parse(JSON.stringify(myContacts))
+    sortedContacts.sort((a,b) => {
+      if (a[sortType] > b[sortType]) { 
+        return 1 
+      } else if (a[sortType] < b[sortType]) { 
+        return -1 
+      } else {
+        return 0
+      }
+  })
+  updateContacts(sortedContacts)
+}
   return (
     <> 
     <h1>IronContacts</h1>
     <button onClick={handleAddRandom}>Add Random Contact</button>
+    <button onClick={() => handleSort('name')}>Sort By Name</button>
+    <button onClick={() => handleSort('popularity')}>Sort By Popularity</button>
+
     <table class="table">
       <thead>
         <tr>
@@ -27,7 +44,7 @@ function App() {
         myContacts.map((contact) => {
         return (
           <>
-          <tr>
+          <tr key={contact.id}>
           <th><img height="50px" src={contact.pictureUrl} alt={contact.name} /></th>
           <th> {contact.name}</th>
           <th> {contact.popularity}</th>
