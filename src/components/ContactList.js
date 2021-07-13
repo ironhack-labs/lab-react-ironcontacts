03d871863b5c7f72ef74c3110f650fab71d08e20
Nicholas Zanotto <nicholas.zanotto@hotmail.com>
2021-07-14 00:27:03 +0200

    [WEB-MAD DE] Nicholas

--- src/components/ContactList.js
+++ src/components/ContactList.js
@@ -6,57 +6,40 @@ class ContactList extends Component {
 
     constructor() {
         super()
-        this.state = {
-            contacts: contacts.slice(0, 5)
-        }
+        this.state = {contacts: contacts.slice(0, 5)}
     }
-
-
     addContact = () => {
         const random = contacts[Math.floor(Math.random() * contacts.length)]
         const List = [...this.state.contacts]
         List.push(random)
         this.setState({
-            contacts: List
-        })
+            contacts: List})
     }
-
-
     sortByName = () => {
         const sortListName = this.state.contacts.sort((a, b) => a.name.localeCompare(b.name))
         this.setState({
-            contacts: sortListName
-        })
+            contacts: sortListName})
     }
-
-
     sortByPopularity = () => {
         const sortListPopularity = this.state.contacts.sort((a, b) => b.popularity - a.popularity)
         this.setState({
-            contacts: sortListPopularity
-        })
+            contacts: sortListPopularity})
     }
-
-
     deleteContact = contactId => {
         this.setState({
             contacts: this.state.contacts.filter(elm => elm.id !== contactId)
         })
     }
-
-
     render() {
 
         return (
-            <>
-                <button onClick={this.addContact}>Add Random Contact</button>
-                <button onClick={this.sortByName}>Sort by Name</button>
-                <button onClick={this.sortByPopularity}>Sort by Popularity</button>
-                {
-                    this.state.contacts.map((elm) => <table className='Table'key={elm.id}> <img src={elm.pictureUrl} alt="picture" style={{ width: '10%' }} />{elm.name} {elm.popularity.toFixed(2)} <button onClick={ () => this.deleteContact(elm.id) }>Delete</button></table>)
-                }
+        <>
+        <button onClick={this.addContact}>Add Random Contat</button>
+        <button onClick={this.sortByName}>Sort by Name</button>
+        <button onClick={this.sortByPopularity}>Sort by Populartiy</button>
+        {this.state.contacts.map((elm) => <table className='table'key={elm.id}> <img src={elm.pictureUrl} alt="" style={{ width:'10%'}} />{elm.name} {elm.popularity.toFixed(2)} <button onClick={ () => this.deleteContact(elm.id) }>Delete</button></table>)}
 
-            </>
+        </>
         )
     }
 }
