a37347ba1bd90ea77bf0c7e33d1635a9daaf6682
Bitterbaitbite <guillechernandez@gmail.com>
2021-07-13 16:55:07 +0200

    Only styles left (and incoming!)

--- src/App.js
+++ src/App.js
@@ -1,25 +1,93 @@
 import logo from './logo.svg';
 import './App.css';
+import contactList from './contacts.json'
+import Button from './Button';
+import { Component } from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+
+  constructor() {
+    super();
+
+    this.state = {
+      limitedList: contactList.filter((el, i) => i < 5)
+    }
+  }
+
+  addContact = () => {
+    let newContact;
+    const newList = [...this.state.limitedList];
+
+    do {
+      newContact = this.getRandomContact();
+    } while(newList.includes(newContact) && newList.length < contactList.length);
+
+    newList.length < contactList.length && newList.push(newContact);
+
+    this.setState({
+      limitedList: [...newList]
+    })
+  }
+
+  sortName = () => {
+    const newList = [...this.state.limitedList];    
+
+    this.setState({
+      limitedList: newList.sort((a, b) => a.name.localeCompare(b.name))
+    })
+  }
+
+  sortPopularity = () => {
+    const newList = [...this.state.limitedList];
+
+    this.setState({
+      limitedList: newList.sort((a, b) => b.popularity - a.popularity)
+    })
+  }
+
+  deleteContact = (id) => {
+    this.setState({
+      limitedList: this.state.limitedList.filter((el) => el.id !== id)
+    })
+  }
+
+  getRandomContact = () => {
+    let i = Math.floor(Math.random() * contactList.length);
+    return contactList[i];
+  }
+
+  render = () =>
+   (
+      <>
+        <h1>IronContacts</h1>
+
+        <button onClick={this.addContact}>Add Random Contact</button>
+        <button onClick={this.sortName}>Sort by name</button>
+        <button onClick={this.sortPopularity}>Sort by popularity</button>
+
+        <table>
+          <thead>
+            <tr>
+              <th>Picture</th>
+              <th>Name</th>
+              <th>Popularity</th>
+              <th>Action</th>
+            </tr>
+          </thead>
+
+          <tbody>
+          {this.state.limitedList.map((el) =>
+            <tr key={el.id}>
+              <td><img src={el.pictureUrl} alt={el.name} /></td>
+              <td>{el.name}</td>
+              <td>{Math.round(el.popularity)}</td>
+              <td><button onClick={() => this.deleteContact(el.id)}>Delete</button></td>
+            </tr>
+          )}
+          </tbody>
+        </table>
+      </>
+    )
 }
 
 export default App;
---
+++ src/Button.js
@@ -0,0 +1,8 @@
+import { Component } from "react";
+import contactList from './contacts.json'
+
+class Button extends Component {
+
+}
+
+export default Button;
