e6312b92e7094c3e90fbc5e801aa6fbee532ce75
Enrique Donaire <donaire.q2@gmail.com>
2021-07-13 17:04:39 +0200

    Lab-IronContact-Done

--- src/App.js
+++ src/App.js
@@ -1,23 +1,68 @@
-import logo from './logo.svg';
 import './App.css';
+import myContacts from './contacts.json';
+import React, { useState } from 'react';
 
 function App() {
+  const [ Celebrities, setCelebrities ] = useState(myContacts.slice(0, 5));
+
+  const addnewCeleb = () => {
+    let newCelebIndex = Math.floor(Math.random() * myContacts.length);
+    let newCeleb = myContacts[newCelebIndex];
+    setCelebrities([...Celebrities, newCeleb])
+  }
+
+  const sortByName = () => {
+    const celebArr = JSON.parse(JSON.stringify(Celebrities));
+    celebArr.sort((a, b) => {
+      return a.name > b.name ? 1 : a.name < b.name ? -1 : 0; 
+    })
+    setCelebrities(celebArr)
+  }
+
+  const sortByPopularity = () => {
+    const celebArr = JSON.parse(JSON.stringify(Celebrities));
+    celebArr.sort((a, b) => b.popularity - a.popularity);
+    setCelebrities(celebArr);
+  }
+
+  const deleteVip = index => {
+    let filteredCelebs = Celebrities.filter((person, idx) => index !== idx);
+    setCelebrities(filteredCelebs);
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>IronContacts</h1>
+      <div className="button-container">
+        <button id="one" onClick={addnewCeleb}>Add a new Celebrity</button>
+        <button id="two" onClick={sortByName}>Sort by name</button>
+        <button id="three" onClick={sortByPopularity}>Sort by popularity</button>
+      </div>
+      <br/>
+      <br/>
+      <table>
+        <thead>
+          <tr>
+            <th id="myPicture">Picture</th>
+            <th id="myName">Name</th>
+            <th id="myPop">Popularity</th>
+          </tr>
+        </thead>
+        {Celebrities.map((person, index) => {
+          return (
+            <tbody id="thePicture" key={index}>
+              <tr>
+                <td><img src={person.pictureUrl}></img></td>
+                <td>{person.name}</td>
+                <td>{person.popularity}</td>
+                <td><button id="four" onClick={() => deleteVip(index)} >Delete</button></td>
+              </tr>
+            </tbody>
+          )
+        })}
+      </table>
+    </div>
     </div>
   );
 }
