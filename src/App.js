1dc1a388547d6818175e19994bc01459705814b8
alexandreoliv <alexandrebo@gmail.com>
2021-07-14 12:42:03 +0200

    add iteration 1

--- src/App.js
+++ src/App.js
@@ -1,25 +1,32 @@
 import logo from './logo.svg';
 import './App.css';
+import contactsData from './contacts.json';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  	let state = {
+		  contacts: [contactsData[0], contactsData[1], contactsData[2], contactsData[3], contactsData[4]]
+  	}
+
+	return (
+		<div className="App">
+		<header className="App-header">
+			<table>
+				<tr>
+					<th>Picture</th>
+					<th>Name</th>
+					<th>Popularity</th>
+				</tr>
+				{state.contacts.map(contact => (
+				<tr key={contact.id}>
+					<td>{<img src={contact.pictureUrl} alt="Profile" height="100px"></img>}</td>
+					<td>{contact.name}</td>
+					<td>{contact.popularity}</td>
+				</tr>
+				))}
+			</table>
+		</header>
+		</div>
+	);
 }
 
 export default App;
