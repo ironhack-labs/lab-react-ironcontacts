ab7a1e846f17e0fa768955eac94b87e66612d25b
Pedro Garrett <pmcgarrett@gmail.com>
2021-07-14 17:36:29 +0100

    done

---
+++ src/Card.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import "./Card.css";
+
+function Card({ name, pictureUrl, popularity, id, removeContact }) {
+    return (
+        <div className="card">
+            <div className="content">
+                <img src={pictureUrl} alt={pictureUrl} />
+                <h3>{name}</h3>
+                <h3>{popularity.toFixed(2)}</h3>
+                {/* <button onClick={removeContact(id)}>Delete</button> */}
+            </div>
+        </div>
+    );
+}
+
+export default Card
