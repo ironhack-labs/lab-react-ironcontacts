e619c414654c2ac746d0e6a8a3c745de680cb410
Evaniomne <edmoura@leroymerlin.com.br>
2021-07-18 17:16:31 -0300

    emndone

--- src/App.js
+++ src/App.js
@@ -7,18 +7,18 @@ import './ContactList.css';
 class App extends React.Component {
 
   state = {
-    contactsList: contacts.slice(0,5)
+    contactsList: contacts.slice(0, 5)
   }
 
   randomContact = () => {
     let randomNumber = Math.floor(Math.random() * contacts.length)
     this.setState((state) => ({
-    contactsList: [...state.contactsList, contacts[randomNumber]]
+      contactsList: [...state.contactsList, contacts[randomNumber]]
     }));
   }
 
   sortAlphabet = () => {
-    let sorted = this.state.contactsList.sort(function(a, b) {
+    let sorted = this.state.contactsList.sort(function (a, b) {
       let nameA = a.name;
       let nameB = b.name;
       if (nameA < nameB) return -1;
@@ -27,13 +27,13 @@ class App extends React.Component {
     });
 
     this.setState((state) => ({
-    contactsList: sorted
+      contactsList: sorted
 
     }));
   }
 
   sortPopular = () => {
-    let sorted = this.state.contactsList.sort(function(a, b) {
+    let sorted = this.state.contactsList.sort(function (a, b) {
       let nameA = a.popularity;
       let nameB = b.popularity;
       if (nameA < nameB) return 1;
@@ -42,57 +42,61 @@ class App extends React.Component {
     });
 
     this.setState((state) => ({
-    contactsList: sorted
+      contactsList: sorted
 
     }));
   }
 
   // delete = (id) => {      
   //  }
-  deleteCelebrity = (id) =>{
-    this.setState((state)=>{
-        return {
-            contacts: state.contactsList.filter((contact)=>{
-                return contact.id !== id
-            })
-        }
+  deleteCeleb = (id) => {
+    const newContacts = [...this.state.contacts];
+    const index = newContacts.findIndex(contact => contact.id === id);    
+    newContacts.splice(index, 1);
+    this.setState({
+    contacts: newContacts 
     })
+    //   return {
+    //     contacts: state.contactsList.filter((contact) => {
+    //       return contact.id !== id
+    //     })
+    //   }
+    // })
   }
 
 
 
 
 
-   renderAllInitialsContacts = () => {
-    const output = this.state.contactsList.map((contact) => { 
-      let popularitySpliced = String(contact.popularity).slice(0,5)
-      return ( 
+  renderAllInitialsContacts = () => {
+    const output = this.state.contactsList.map((contact) => {
+      let popularitySpliced = String(contact.popularity).slice(0, 5)
+      return (
 
-        
+
         <tr key={contact.id}>
-          <td><img src={contact.pictureUrl} alt="celeb"/></td>
+          <td><img src={contact.pictureUrl} alt="celeb" /></td>
           <td>{contact.name}</td>
           <td>{popularitySpliced}</td>
-          <td><button onClick={this.deleteCelebrity}> Delete</button></td>
+          <td><button onClick={this.deleteCeleb}> Delete</button></td>
         </tr>
-        
-      )   
+
+      )
     })
     return output
   }
 
   render() {
-    return  (   
-      
-      
-            <div className="App">
+    return (
+
+      <div className="App">
         <header className="App-header">
           <h1 className="Celeb">IronContacts</h1>
           <th1 className="th1">
-          <button onClick={this.randomContact}> Add Random Contact</button>
-          <button onClick={this.sortAlphabet}> Sort Alphabeticaly</button>
-          <button onClick={this.sortPopular}> Sort by Popularity</button>
-          </th1>          
+            <button onClick={this.randomContact}> Add Random Contact</button>
+            <button onClick={this.sortAlphabet}> Sort Alphabeticaly</button>
+            <button onClick={this.sortPopular}> Sort by Popularity</button>
+          </th1>
           <table>
             <thead>
               <tr>
