6af9fa44a32be75bb5548d8b098cc087ce400bc0
Debora Seo <deborah_seo@hotmail.com>
2021-07-18 21:55:48 -0300

    done

--- src/App.js
+++ src/App.js
@@ -1,25 +1,72 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import contacts from "./contacts.json";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+class App extends React.Component {
+  state = {
+    contactOriginal: contacts,
+    contactsArr: contacts.splice(0,5)
+  }
+
+  handleAddNewContact = () => {
+    const newArray = [...this.state.contactsArr];
+    const randomContact = this.state.contactOriginal[Math.floor(Math.random()*this.state.contactOriginal.length)];
+    newArray.push(randomContact);
+    this.setState ({
+      contactsArr: newArray
+    });
+  };
+
+  handleSortByName = () => {
+    const arrayCopy = [...this.state.contactsArr];
+    arrayCopy.sort((a, b) => {
+      return a.name < b.name ? -1 : 1;
+    });
+    this.setState ({
+      contactsArr: arrayCopy
+    });
+  };
+
+  handleDelete = (id) => {
+    const arrCopy = [...this.state.contactsArr];
+    const index = arrCopy.findIndex((el) => el.id === id);
+    arrCopy.splice(index, 1);
+    this.setState ({
+      contactsArr: arrCopy
+    })
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <h1>IronContacts</h1>
+        <button onClick={()=>{this.handleAddNewContact()}}>Add Random Contact</button>
+        <button onClick={()=>{this.handleSortByName()}}>Sort by Name</button>
+        <button>Sort by Popularity</button>
+        <table>
+          <thead>
+            <tr>
+              <th>Picture</th>
+              <th>Name</th>
+              <th>Popularity</th>
+            </tr>  
+          </thead>
+          <tbody>
+             {this.state.contactsArr.map((contact) => <>
+             <tr key={contact.id}>
+               <td><img id="stars" src={contact.pictureUrl} alt=''/></td>
+               <td>{contact.name}</td>
+               <td>{contact.popularity}</td>
+               <button id="delete-btn" onClick={() => {this.handleDelete(contact.id)}}>Delete</button>
+             </tr>
+             
+             </>)}     
+          </tbody>
+        </table>
+      </div>
+    )
+  }
 }
 
 export default App;
