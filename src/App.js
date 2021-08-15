d1747aab62c5711b4e3c4135b30dfcc113c0dadc
Cesar Carreras <cesar.carreras95@gmail.com>
2021-08-14 21:25:29 -0500

    Iteration 1, 2, 3, 4

--- src/App.js
+++ src/App.js
@@ -1,23 +1,61 @@
-import logo from './logo.svg';
+import {useState} from 'react'
 import './App.css';
+import contactsData from "./contacts.json";
+
 
 function App() {
+
+  const firstFive = contactsData.slice(0, 5)
+  const [contacts, setContacts] = useState(firstFive)
+
+  const handleAddRandom = () => {
+    const randContact = contacts[Math.floor(Math.random() * contacts.length -1)]
+
+    const newContacts = [...contacts, randContact]
+    setContacts(newContacts)
+  }
+
+  const handleSort = () => {
+    let newContactsArray = [...contacts]
+    newContactsArray.sort((a,b) => {
+      if(a.name < b.name) return -1
+      if(a.name > b.name) return 1
+      return 0;
+    })
+    console.log(newContactsArray)
+    setContacts(newContactsArray)
+  }
+
+  const handleDelete = (id) => {
+    const updatedArr = contactsData.filter(contact => contact.id !== id)
+    setContacts(updatedArr)
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <h1>IronContacts</h1>
+        <section className="table">
+        <button onClick={handleAddRandom}>Add Random Contact</button>
+        <button onClick={handleSort}>Sort by Name</button>
+        <table>
+          <tr>
+            <th>Picture</th>
+            <th>Name</th>
+            <th>Popularity</th>
+            <th>Action</th>
+          </tr>
+
+          {contacts.map (contact => (
+                <tr key={contact.id}>
+                <td ><img key={contact.pictureUrl} src={contact.pictureUrl} width="50px" alt="profile pic"/></td>
+                <td key={contact.name} >{contact.name}</td>
+                <td key={contact.popularity} >{contact.popularity.toFixed(2)}</td>
+                <button onClick={handleDelete}>Delete</button>
+                </tr>
+                ))}
+
+        </table>
+      </section>
     </div>
   );
 }
