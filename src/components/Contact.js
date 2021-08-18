3b7ec61ac4b4f36b6539376967975686c3e2673e
Hongshan <zhou.hongshan636@gmail.com>
2021-08-18 21:52:36 +0200

    all done

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -2,15 +2,18 @@ import React from "react";
 
 class Contact extends React.Component {
     render(){
-        return(
-            <tr>
-                <th>
-                    <img src={this.props.pictureUrl} alt={this.props.name} />
-                </th>
-                <th>{this.props.name}</th>
-                <th>{this.props.popularity.toFixed(2)}</th>
-            </tr>
-        )
+        return (
+          <tr>
+            <th>
+              <img src={this.props.pictureUrl} alt={this.props.name} />
+            </th>
+            <th>{this.props.name}</th>
+            <th>{this.props.popularity.toFixed(2)}</th>
+            <th>
+              <button onClick={this.props.clickToDelete}>Delete</button>
+            </th>
+          </tr>
+        );
     }
 }
 
--- src/components/ContactsTable.js
+++ src/components/ContactsTable.js
@@ -3,45 +3,82 @@ import allProducersContacts from "../contacts.json";
 import Contact from "./Contact";
 
 class ContactsTable extends React.Component {
-    state = {
-contacts : allProducersContacts.slice(0,5)
-    }
+  state = {
+    contacts: allProducersContacts.slice(0, 5),
+  };
 
-    randomContactHandler = () => {
-       let randomContact =
-         allProducersContacts[
-           Math.floor(Math.random() * allProducersContacts.length)
-         ];
-        let contactsCopy = [...this.state.contacts] 
-        contactsCopy.push(randomContact)
-        this.setState ({
-             contacts: contactsCopy
-        })    
+  randomContactHandler = () => {
+    let randomContact =
+      allProducersContacts[
+        Math.floor(Math.random() * allProducersContacts.length)
+      ];
+    let contactsCopy = [...this.state.contacts];
+    contactsCopy.push(randomContact);
+    this.setState({
+      contacts: contactsCopy,
+    });
+  };
+
+  sortContacts = (method) => {
+    if (method === "name") {
+      return this.setState({
+        contacts: this.state.contacts.sort((a, b) =>
+          a.name.localeCompare(b.name)
+        ),
+      });
+    } else {
+      return this.setState({
+        contacts: this.state.contacts.sort(
+          (a, b) => b.popularity - a.popularity
+        ),
+      });
     }
-    render(){
-        return (
-          <div>
-            <table>
-              <thead>
-                <tr>
-                  <th className="title">IronContacts</th>
-                </tr>
-                <button className='randomBtn' onClick={this.randomContactHandler}>Add Random Contact</button>
-                <tr className="rowTitle">
-                  <th>Picture</th>
-                  <th>Name</th>
-                  <th>Popularity</th>
-                </tr>
-              </thead>
-              <tbody>
-                {this.state.contacts.map((contact) => {
-                  return <Contact {...contact} />;
-                })}
-              </tbody>
-            </table>
-          </div>
-        );
-    }
+  };
+
+  deleteContact = id => {
+    this.setState((prevState) => {
+      return {
+        contacts: prevState.contacts.filter((contact) => contact.id !== id),
+      };
+    });
+  }
+
+  render() {
+    return (
+      <div>
+        <table>
+          <thead>
+            <tr>
+              <th className="title">IronContacts</th>
+            </tr>
+            <tr className="btns">
+              <button onClick={this.randomContactHandler}>
+                Add Random Contact
+              </button>
+              <button onClick={()=>this.sortContacts('name')}>Sort by name</button>
+              <button onClick={()=>this.sortContacts('popularity')}>Sort by popularity</button>
+            </tr>
+            <tr className="rowTitle">
+              <th>Picture</th>
+              <th>Name</th>
+              <th>Popularity</th>
+              <th>Action</th>
+            </tr>
+          </thead>
+          <tbody>
+            {this.state.contacts.map((contact) => {
+              return (
+                <Contact
+                  {...contact}
+                  clickToDelete={() => this.deleteContact(contact.id)}
+                />
+              );
+            })}
+          </tbody>
+        </table>
+      </div>
+    );
+  }
 }
 
-export default ContactsTable
+export default ContactsTable;
