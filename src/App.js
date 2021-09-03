79091769d1d7b736f74ac93e96e32c73b08059c1
Andrei <andrei_1002@hotmail.com>
2021-09-03 18:12:20 +0200

    added table to display first 5 contacts (iteration 1)

--- src/App.js
+++ src/App.js
@@ -1,25 +1,55 @@
-import logo from './logo.svg';
+import { Component } from 'react';
+import contacts from './contacts.json';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const firstContacts = contacts.splice(0, 5);
 
-export default App;
+export default class App extends Component{
+
+  state = {
+    contacts: firstContacts
+  };
+
+  render() {
+
+    const contactsList = this.state.contacts.map((contact) => {
+      return(
+        <tr key={contact.id}>
+          <td>
+            <img src={contact.pictureUrl} alt={contact.name} style={{width:100, objectFit:'cover'}} />
+          </td>
+          <td>
+            {contact.name}
+          </td>
+          <td>
+            {contact.popularity}
+          </td>
+        </tr>
+      )
+    });
+
+    return (
+      <div className="App">
+        <h1>IronContacts</h1>
+        <table>
+          <thead>
+            <tr>
+              <th>
+                Picture
+              </th>
+              <th>
+                Name
+              </th>
+              <th>
+                Popularity
+              </th>
+            </tr>
+          </thead>
+          <tbody>
+            {contactsList}
+          </tbody>
+        </table>
+      </div>
+    )
+  };
+}
