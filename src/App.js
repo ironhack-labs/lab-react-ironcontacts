2d6ec6afa918c5334b61bd1c06548c6fb1bd428f
Paloma Borro <palomabs93@gmail.com>
2021-09-03 16:55:48 +0200

    iteración 2 con estilos

--- src/App.js
+++ src/App.js
@@ -8,31 +8,45 @@ export default class App extends React.Component {
     contacts: contacts.slice(0, 5)
   }
   
+  onRandomContact = () => {
+    let randomContact =
+      contacts[Math.floor(Math.random() * contacts.length)];
+
+    let contactsCopy = [...this.state.contacts];
+
+    contactsCopy.push(randomContact);
+
+    this.setState({
+      contacts: contactsCopy
+    });
+  };
+
   render() {
   const contact = this.props;
   return (
     <div className="App">
       <h1>Iron Contacts</h1>
-        <table>
-          <thead className='title'>
-            <tr>
-              <th>Picture</th>
-              <th>Name</th>
-              <th>Popularity</th>
-            </tr>
-          </thead>
-          <tbody>
-            {this.state.contacts.map(contact => (
+        <button onClick={this.onRandomContact}>Add Random Contact</button>
+          <table>
+            <thead className='title'>
               <tr>
-                <th>
-                  <img src={contact.pictureUrl} alt={contact.name} />
-                </th>
-                <th>{contact.name}</th>
-                <th>{contact.popularity}</th>
+                <th>Picture</th>
+                <th>Name</th>
+                <th>Popularity</th>
               </tr>
-            ))}
-          </tbody>
-        </table>
+            </thead>
+            <tbody>
+              {this.state.contacts.map(contact => (
+                <tr>
+                  <th>
+                    <img src={contact.pictureUrl} alt={contact.name} />
+                  </th>
+                  <th>{contact.name}</th>
+                  <th>{contact.popularity}</th>
+                </tr>
+              ))}
+            </tbody>
+          </table>
     </div>
   );
   }
