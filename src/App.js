45341959692e2bf3c1abc20f38344aa293bc5b35
Andrei <andrei_1002@hotmail.com>
2021-09-04 13:50:21 +0200

    added addRandomContact button (iteration 2)

--- src/App.js
+++ src/App.js
@@ -10,19 +10,33 @@ export default class App extends Component{
     contacts: firstContacts
   };
 
+  addRandomContact = () => {
+    let newRandomContact = '';
+
+    const randomIndex = Math.floor(Math.random() * contacts.length);
+
+    newRandomContact = contacts[randomIndex];
+
+    this.setState(state => {
+      return {
+        contacts: [...state.contacts, newRandomContact]
+      }
+    })
+  };
+
   render() {
 
     const contactsList = this.state.contacts.map((contact) => {
       return(
         <tr key={contact.id}>
           <td>
-            <img src={contact.pictureUrl} alt={contact.name} style={{width:100, objectFit:'cover'}} />
+            <img src={contact.pictureUrl} alt={contact.name} style={{width:100, objectFit:'cover', height:120}} />
           </td>
           <td>
             {contact.name}
           </td>
           <td>
-            {contact.popularity}
+            {contact.popularity.toFixed(2)}
           </td>
         </tr>
       )
@@ -31,6 +45,7 @@ export default class App extends Component{
     return (
       <div className="App">
         <h1>IronContacts</h1>
+        <button onClick={this.addRandomContact}> Add Random Contact</button>
         <table>
           <thead>
             <tr>
