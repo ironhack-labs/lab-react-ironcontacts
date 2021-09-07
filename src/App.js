242a19b3a970c808ed289a30a76bb4c97b1a85b2
andrekleine <andrekleine@gmail.com>
2021-09-07 11:40:11 -0300

    Iteration 2

--- src/App.js
+++ src/App.js
@@ -1,27 +1,46 @@
-import React from "react";
+import React, {useState} from "react";
 import "./App.css";
 import TableRow from "./Components/TableRow/TableRow";
 import Contacts from "./contacts.json";
 
 const firstFive = Contacts.slice(0, 5);
+const largerArr = Contacts.slice(5);
 
-function App() {
-  const [counter, setCounter] = React.useState(firstFive);
+const App = () => {
+  const AddRandomContact = () => {
+    const [items, setItems] = useState(firstFive);
 
+    const randomContact = largerArr[Math.floor(Math.random() * largerArr.length)];
+
+    const addItem = () => {
+        setItems([... items, randomContact])
+    }
+    
+    return (
+        <div>
+            <button onClick={addItem}>Add Contacts</button>
+            <table>
+        <tbody>
+        {items.map((element) => {
+          return (
+            <TableRow 
+              pictureUrl={element.pictureUrl}
+              name={element.name}
+              popularity={element.popularity.toFixed(2)}
+            />
+          );
+          })}        
+        </tbody>
+      </table>      
+        </div>
+    )
+  };
+  
   return (
-    <table>
-      <tbody>
-      {firstFive.map((element) => {
-        return (
-          <TableRow 
-            pictureUrl={element.pictureUrl}
-            name={element.name}
-            popularity={element.popularity.toFixed(2)}
-          />
-        );
-        })}        
-      </tbody>
-    </table>
+    <div>
+      <AddRandomContact />      
+    </div>    
   );
 }
+
 export default App;
