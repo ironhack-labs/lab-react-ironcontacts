4a72655e79ef5aeb790b2d275c2b495a2d252afb
jbaker825 <jbaker825@gmail.com>
2021-09-08 15:28:49 -0500

    iteration 2

--- src/App.js
+++ src/App.js
@@ -1,41 +1,50 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 import contacts from "./contacts.json";
-
-let actors = contacts.slice(0, 5)
-
-const ShowTable = () => {
-  let list = actors.map(actor => {
-    return (
-      <tr>
-        <td><img src={actor.pictureUrl} height = "50px"/></td>
-        <td>{actor.name}</td>
-        <td>{actor.popularity}</td>
-      </tr>
-    )
-  })
-  return list
-}
+import { useState } from "react";
 
 function App() {
+  let [actors, setActors] = useState(contacts.slice(0, 5));
+
+  function randomContact() {
+    let randomNumber = Math.floor(Math.random() * (contacts.length - 1));
+    let newContact = contacts[randomNumber];
+    contacts.splice(randomNumber, 1);
+
+    let copyOfActors = [...actors];
+    copyOfActors.push(newContact);
+    setActors(copyOfActors);
+  }
+
+  const ShowTable = () => {
+    let list = actors.map((actor) => {
+      return (
+        <tr>
+          <td>
+            <img src={actor.pictureUrl} height="50px" />
+          </td>
+          <td>{actor.name}</td>
+          <td>{actor.popularity}</td>
+        </tr>
+      );
+    });
+    return list;
+  };
+
   return (
     <div className="App">
       <h1>IronContacts</h1>
+      <button onClick={randomContact}> Add Random Contact</button>
       <table>
         <tr>
           <th>picutre</th>
           <th>name</th>
           <th>popularity</th>
         </tr>
-        <ShowTable/>
+        <ShowTable />
       </table>
     </div>
   );
-}
-
-function selectRandomContact(data) {
-  let randomNumber = Math.floor(Math.random() * (data.length + 1));
-  return data[randomNumber];
 }
 
 export default App;
