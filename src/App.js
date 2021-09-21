50b77d0194dc1e0e38b86f10ee8274bd9c7a00c8
Jesse Vermeulen <jessevermeulen123@gmail.com>
2021-09-21 18:22:45 +0200

    WIP: lab

--- src/App.js
+++ src/App.js
@@ -1,23 +1,18 @@
-import logo from './logo.svg';
 import './App.css';
+import contacts from "./contacts.json";
 
 function App() {
+  let ourContacts = contacts.map(
+    contact => <div>
+      <img src={contact.pictureUrl} alt="" />
+      {contact.name}
+
+      {contact.popularity}
+    </div>
+  )
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {ourContacts}
     </div>
   );
 }
