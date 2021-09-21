1d947875932ad7c5d940406821d6faea36063303
Laura <lauradecc@gmail.com>
2021-09-21 15:36:41 +0200

    iterations finished

--- src/App.js
+++ src/App.js
@@ -24,12 +24,45 @@ class App extends React.Component {
   }
 
 
+  sortByName = () => {
+
+    const contactsCopy = [...this.state.contacts];
+
+    this.setState({
+      ...this.state,
+      contacts: contactsCopy.sort((contact1, contact2) => contact1.name.localeCompare(contact2.name))
+    })
+  }
+
+
+  sortByPopularity = () => {
+
+    const contactsCopy = [...this.state.contacts];
+
+    this.setState({
+      ...this.state,
+      contacts: contactsCopy.sort((contact1, contact2) => contact2.popularity - contact1.popularity)
+    })
+  }
+
+
+  deleteContact = (id) => {
+
+    this.setState({
+      ...this.state,
+      contacts: this.state.contacts.filter(contact => contact.id !== id)
+    })
+  }
+
+
   render() {
     return (
       <div className="App">
         <h1>IronContacts</h1>
         <br />
         <button onClick={() => this.addRandomContact()}>Add Random Contact</button>
+        <button onClick={() => this.sortByName()}>Sort By Name</button>
+        <button onClick={() => this.sortByPopularity()}>Sort By Popularity</button>
         <br />
         <table>
           <thead>
@@ -43,10 +76,11 @@ class App extends React.Component {
             {
               this.state.contacts.map(contact => {
                 return (
-                  <tr>
+                  <tr key={contact.id}>
                     <td><img src={contact.pictureUrl} alt={contact.name} /></td>
                     <td>{contact.name}</td>
                     <td>{contact.popularity.toFixed(2)}</td>
+                    <td><button onClick={() => this.deleteContact(contact.id)}>Delete</button></td>
                   </tr>
                 )
               })
