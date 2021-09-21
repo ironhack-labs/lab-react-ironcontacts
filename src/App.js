a24be5c827118f192d31ba187a92631659860596
Vanessa Zagone <vanessa.zgn@gmail.com>
2021-09-21 18:05:21 +0200

    Compulsory iterations done!

--- src/App.js
+++ src/App.js
@@ -1,25 +1,95 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import contacts from "./contacts.json";
 
-function App() {
-  return (
+
+class App extends React.Component {
+
+  constructor(){
+    super();
+      this.state = {
+        remainingContacts: contacts.slice(5, contacts.length-1),
+        initialContacts: contacts.slice(0,5)
+        }
+    };
+    
+
+
+    addRandomContact() {
+      const originalContacts = [...this.state.remainingContacts]
+      let i = Math.floor(Math.random() * originalContacts.length)
+      
+      const shownContacts = [...this.state.initialContacts]
+      shownContacts.push(originalContacts[i])
+      originalContacts.splice(i, 1)
+      console.log(originalContacts)
+
+      this.setState({
+        ...this.state,
+        initialContacts: shownContacts,
+        remainingContacts: originalContacts.length > 0 ? originalContacts : contacts
+      })
+
+    };
+
+
+    sortByName() {
+      const shownContacts = [...this.state.initialContacts].sort((contact1, contact2) => contact1.name.localeCompare(contact2.name))
+  
+      this.setState({
+        ...this.state,
+        initialContacts: shownContacts
+      })
+    }
+
+    sortByPopularity() {
+      const shownContacts = [...this.state.initialContacts].sort((contact1, contact2) => contact2.popularity - contact1.popularity)
+  
+      this.setState({
+        ...this.state,
+        initialContacts: shownContacts
+      })
+    }
+
+
+
+    removeContact = (id) => {
+      const shownContacts = [...this.state.initialContacts].filter(contact => contact.id !== id)
+      this.setState({
+        ...this.state,
+        initialContacts: shownContacts
+      })
+    }
+  
+
+
+
+   
+
+  render() {
+
+   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     <button onClick={() => this.addRandomContact()}>Add contacts</button>
+     <button onClick={() => this.sortByName()}>Sort by name</button>
+     <button onClick={() => this.sortByPopularity()}>Sort by popularity</button>
+     
+        <table>
+          { this.state.initialContacts.map((contact, idx) => {
+            return <tr key={contact.id + idx}>
+                      <td><img src={contact.pictureUrl} className='img-actor' alt={contact.name} /></td> 
+                      <td><h2>{contact.name}</h2></td> 
+                      <td><h4>{contact.popularity.toFixed(2)}</h4></td>
+                      <td><button onClick={() => this.removeContact(contact.id)}>Remove</button></td>   
+                   </tr>
+          })}
+        </table>   
     </div>
-  );
+   )
+  }
 }
 
 export default App;
+
+
+
