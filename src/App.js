02fba18694e317bb54487dff2c24b847d6775539
simonmihalynagy <nagysimonmihaly@gmail.com>
2021-09-23 05:47:41 +0200

    iteration 2 done

--- src/App.js
+++ src/App.js
@@ -1,60 +1,55 @@
-
 import React from "react";
-import './App.css';
+import "./App.css";
 import contacts from "./contacts.json";
 
-const firstFiveContacts = contacts.slice(0,5);
+let firstFiveContacts = contacts.slice(0, 5);
 
+//let randomContact = contacts[Math.floor(Math.random() * contacts.length)];
 
-
-class App extends React.Component{
-
-
-
-
-
+class App extends React.Component {
   state = {
-    firstFiveContacts
+    ironContacts: firstFiveContacts,
+  };
+
+  addRandomContact = () => {
+    let newContacts = this.state.ironContacts.slice();
+
+    let randomContact = contacts[Math.floor(Math.random() * contacts.length)];
+
+    newContacts.push(randomContact);
+
+    this.setState({ ironContacts: newContacts });
+  };
+
+  render() {
+    return (
+      <div>
+        <h2>IronContacts</h2>
+        <table>
+          <tr>
+            <th>picture</th>
+            <th>name</th>
+            <th>popularity</th>
+          </tr>
+          {this.state.ironContacts.map((contact) => {
+            return (
+              <tr key={contact.id}>
+                <img
+                  style={{ width: "50px", height: "70px" }}
+                  src={contact.pictureUrl}
+                  alt=""
+                />
+
+                <th>{contact.name}</th>
+                <th>{contact.popularity}</th>
+              </tr>
+            );
+          })}
+        </table>
+        <button onClick={this.addRandomContact}>Add random contact</button>;
+      </div>
+    );
   }
-
-  render(){
-    console.log(firstFiveContacts)
-    return(
-      
-      <table>
-      
-
-      <h2>IronContacts</h2>
-        
-      <tr>
-        <th>picture</th>
-        <th>name</th>
-        <th>popularity</th>
-      </tr>
-      
-        {this.state.firstFiveContacts.map((contact)=>{
-          return (
-            
-            <tr key={contact.id}>
-              
-
-              <img style={{width: "50px", height: "70px"}} src={contact.pictureUrl} alt="" />
-              
-
-              <th>{contact.name}</th>
-              <th>{contact.popularity}</th>
-            </tr>
-            
-          
-          )
-          
-        })}
-      
-      
-    </table>
-  )}
-
-  
 }
 
 export default App;
