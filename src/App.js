a00ad26951464d6208a2cab6f1da27873ad437ab
Lexadiaz120 <daura38@gmail.com>
2021-09-24 08:21:01 +0200

    update contacts

--- src/App.js
+++ src/App.js
@@ -1,25 +1,68 @@
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import './App.css';
+import contacts from "./contacts.json";
+import React from "react";
+
+class App extends React.Component { 
+
+  constructor(){
+    super() 
+    this.state = { 
+      remainingContacts: contacts.slice(5, contacts.length - 1),
+      initialContacts: contacts.slice(0, 5)
+    } 
+    
+  } 
+
+
+  addRandomContact = () =>{
+    const originContacts = [...this.state.remainingContacts];
+    let i = Math.floor(Math.random() * originContacts.length);
+
+    const showContacts = [...this.state.initialContacts]; 
+    showContacts.push(originContacts[i]);
+    originContacts.splice(i , 1); 
+    
+
+    this.setState({
+      ...this.state,
+      initialContacts: showContacts,
+      remainingContacts: originContacts.length > 0 ? originContacts : contacts
+    })
+  }
+ 
+  render() {
+
+    return (
+      <div className="list-of-contacts">
+        <h2>IronContacts</h2>
+        <table>
+          <tbody>
+            <tr className="spacer">
+              <th>Picture</th>
+              <th>Name</th>
+              <th>Popularity</th>
+            </tr>
+            {this.state.initialContacts.map((contact, idx) => {
+              return (
+                <tr key = {contact.id  + idx}>
+                  <td><img src={`${contact.pictureUrl}`} alt='pic'></img> </td>
+                  <td> <p className="name">{`${contact.name}`}</p> </td>
+                  <td> <p className="popularity">{`${contact.popularity}`}</p></td> 
+                </tr>
+
+              )
+            })}
+
+          </tbody>
+        </table> 
+        <div className="buttons-container">
+          <button onClick={() => this.addRandomContact()}>Add contacts</button>
+        </div>
+
+      </div>
+    )
+  }
 }
 
-export default App;
+ export default App;
