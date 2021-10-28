ce30398555b293e5eeb8620c67e402b5f08aeb73
Daniel td <danieltd@gmail.com>
2021-10-28 13:28:35 +0200

    iteration 2 done

--- src/App.js
+++ src/App.js
@@ -3,40 +3,34 @@ import "./App.css";
 import { Fragment } from "react";
 import contacts from "./contacts.json";
 
-function ListComponents() {
+function App() {
   const [listContacts, setListContacts] = useState(contacts.slice(0, 5));
 
-  function AddNewContact() {
-    const newContact = {
-      contacts,
-    };
+  console.log(listContacts);
 
-    const newArr = [...contacts];
+  function addContact() {
+    const newContact = contacts[Math.floor(Math.random() * contacts.length)];
+    console.log(newContact);
+    const newArr = [...listContacts];
     newArr.unshift(newContact);
-
     setListContacts(newArr);
   }
 
   return (
     <div>
-      <button onClick={AddNewContact}>Add Random Contact</button>
+      <button onClick={addContact}>Add new contact</button>
       {listContacts.map((contact, index) => {
         return (
           <Fragment key={index}>
             <table>
-              <tbody>
-                <tr>
-                  Picture
-                  <br />
-                  <img
-                    style={{ width: "10%" }}
-                    src={contact.pictureUrl}
-                    alt=""
-                  />
-                </tr>
-                <tr>name: {contact.name}</tr>
-                <tr>Popularity{contact.popularity}</tr>
-              </tbody>
+              <tr>
+                {" "}
+                Picture
+                <br />
+                <img style={{ width: "10%" }} src={contact.pictureUrl} alt="" />
+              </tr>
+              <tr>name: {contact.name}</tr>
+              <tr>Popularity{contact.popularity}</tr>
             </table>
           </Fragment>
         );
@@ -44,13 +38,58 @@ function ListComponents() {
     </div>
   );
 }
-function App() {
-  return (
-    <div>
-      <ListComponents />
-    </div>
-  );
-}
+
+// function ListComponents() {
+//   const [listContacts, setListContacts] = useState(contacts.slice(0, 5));
+
+//   // Math.floor.random on the contacts array
+//   // Push it to the list contacts array
+
+//   function AddNewContact() {
+//     const newContact = {
+//       contacts,
+//     };
+
+//     const newArr = [...contacts];
+//     newArr.unshift(newContact);
+
+//     setListContacts(newArr);
+//   }
+
+//   return (
+//     <div>
+//       <button onClick={AddNewContact}>Add Random Contact</button>
+//       {listContacts.map((contact, index) => {
+//         return (
+//           <Fragment key={index}>
+//             <table>
+//               <tbody>
+//                 <tr>
+//                   Picture
+//                   <br />
+//                   <img
+//                     style={{ width: "10%" }}
+//                     src={contact.pictureUrl}
+//                     alt=""
+//                   />
+//                 </tr>
+//                 <tr>name: {contact.name}</tr>
+//                 <tr>Popularity{contact.popularity}</tr>
+//               </tbody>
+//             </table>
+//           </Fragment>
+//         );
+//       })}
+//     </div>
+//   );
+// }
+// function App() {
+//   return (
+//     <div>
+//       <ListComponents />
+//     </div>
+//   );
+// }
 
 export default App;
 
