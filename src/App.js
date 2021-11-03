6e51c7f0b1843d79c66a1f19b595fe0c0255f928
Dominika Lipka <dominika.lipka96@gmail.com>
2021-11-03 18:34:03 +0100

    Iteration 2 done

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
-import React from 'react';
-import './App.css';
-import contacts from './contacts.json'
+import React from "react";
+import "./App.css";
+import contacts from "./contacts.json";
 
-const firstFiveContacts = contacts.slice(0,5)
+const firstFiveContacts = contacts.slice(0, 5);
 console.log(firstFiveContacts);
+
+function getRandomContact() {
+  return contacts[Math.ceil(Math.random()*contacts.length)];
+};
 
 class Contact extends React.Component {
   render() {
@@ -19,26 +23,44 @@ class Contact extends React.Component {
   }
 }
 
-function App() {
-  return (
-    <div className="container">
-      <h1>IronContacts</h1>
-      <table>
-        <thead>
-          <tr>
-            <th>Picture</th>
-            <th>Name</th>
-            <th>Popularity</th>
-          </tr>
-        </thead>
-        <tbody>
-          {firstFiveContacts.map((contact) => {
-            return <Contact key={contact.id} {...contact} />;
-          })}
-        </tbody>
-      </table>
-    </div>
-  );
+class App extends React.Component {
+  state = {
+    contactsDisplayed: firstFiveContacts
+  };
+
+  addRandomContact = () => {
+    this.setState((prevState, props) => {
+      let randomContact = getRandomContact();
+      return {
+        contactsDisplayed: [...prevState.contactsDisplayed, randomContact],
+      };
+    })
+  }
+
+  render() {
+    return (
+      <div className="container">
+        <h1>IronContacts</h1>
+        <div className="buttons">
+          <button onClick={this.addRandomContact}>Add Random Contact</button>
+        </div>
+        <table>
+          <thead>
+            <tr>
+              <th>Picture</th>
+              <th>Name</th>
+              <th>Popularity</th>
+            </tr>
+          </thead>
+          <tbody>
+            {this.state.contactsDisplayed.map((contact) => {
+              return <Contact key={contact.id} {...contact} />;
+            })}
+          </tbody>
+        </table>
+      </div>
+    );
+  }
 }
 
 export default App;
