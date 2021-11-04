7c5c03afa1b781b7468737d9016503ac73ef5b88
Kati Fe <k-felgitsch@hotmail.com>
2021-11-04 23:43:48 +0000

    Iteration 1 + 2 + 3 done

--- src/App.js
+++ src/App.js
@@ -8,16 +8,18 @@ const fiveContacts = contacts.slice(0, 5);
 console.log(fiveContacts);
 console.log(fiveContacts[3]);
 
-randomActor = () => {
-  const newArr = this.state.initialActors
-  console.log("please logg")
-  let actorRandom = getActorRandomly();
-  console.log("pleasse")
-  this.setState((prevState, props)=> {
-    return {initialActors: prevState.initialActors.push(randomActor)}
+// randomActor = () => {
+//   const newArr = this.state.initialContacts
 
-  })
-}
+//   let actorRandom = getActorRandomly();
+//   console.log("pleasse")
+//   this.setState((prevState, props) => {
+//     return { initialContacts: prevState.initialContacts.push(randomActor) }
+
+//   })
+// }
+
+
 
 class Contact extends React.Component {
 
@@ -25,7 +27,7 @@ class Contact extends React.Component {
     return (
       <tr key="contact.id">
         <td>
-          <img src={this.props.pictureURl} alt="" />
+          <img className="Picture" src={this.props.pictureUrl} alt="" />
         </td>
         <td>{this.props.name}</td>
         <td>{this.props.popularity}</td>
@@ -35,28 +37,75 @@ class Contact extends React.Component {
 }
 
 class App extends React.Component {
- 
+  constructor(props) {
+    super(props);
+    this.state = {
+      initialContacts: fiveContacts
+    }
+  }
+
+  addRandomContact = () => {
+    const newContact= contacts[Math.floor(Math.random()*contacts.length)]  
+    this.setState((prevState, props)=>{
+    const newList =[...prevState.initialContacts, newContact]
+    return {initialContacts: newList}
+    })
+   
+  }
+
+  sortbyName = ()=> {
+    
+    this.setState( (prevState,props) =>{
+      const listOfNames = prevState.initialContacts.sort(function(a,b){
+        return a.name.localeCompare(b.name)
+      });
+       
+      return ({initialContacts: listOfNames})
+    }
+    ) 
+  }
+
+  sortbyPopularity = ()=>{
+    this.setState( (prevState, props)=>{
+      const listOfPopularity = prevState.initialContacts.sort(function(a,b){
+        return a.popularity - b.popularity
+      });
+      return ({initialContacts: listOfPopularity})
+    })
+  }
 
   render() {
     return (
       <div className="container">
         <h1>Iron Contacts</h1>
+        <button onClick={this.addRandomContact}>Add random contact</button>
+        <button onClick={this.sortbyName}>Sort the table by name</button>
+        <button onClick={this.sortbyPopularity}>Sort the table by Popularity</button>
         <table>
           <thead>
-           <tr>
-            <th>Picture</th>
-            <th>Name</th>
-            <th>Popularity</th>
+            <tr>
+              <th>Picture</th>
+              <th>Name</th>
+              <th>Popularity</th>
             </tr>
-           </thead>
+          </thead>
+
+
+
           <tbody>
-          {/* <h1>hello</h1> */  use map method!!!! }
-            <Contact                                
-            name={fiveContacts.name}
-            image={fiveContacts.imageUrl}
-            popularity={fiveContacts.popularity}
-            />
+
+            {this.state.initialContacts.map((contact) => {
+              return <Contact
+                name={contact.name}
+                pictureUrl={contact.pictureUrl}
+                popularity={contact.popularity}
+              />
+            })}
+
+
           </tbody>
+
+
         </table>
 
       </div>
