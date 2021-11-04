81cf5ac59c11da40b5c2c9244e7eed9c9da18a2c
CodingEinhornDeu <88826323+CodingEinhornDeu@users.noreply.github.com>
2021-11-04 01:00:31 +0100

    iteration 1 and random contact

--- src/App.js
+++ src/App.js
@@ -1,23 +1,52 @@
-import logo from './logo.svg';
 import './App.css';
+import contacts from "./contacts.json";
+
+const arrFromContact = contacts.slice(0, 5);
+
+const pickRandom = () => {
+  const randomContact = contacts[Math.floor(Math.random() * contacts.length)];
+  arrFromContact.push(randomContact);
+  console.log(arrFromContact)
+}
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="tContainer">
+      <h2>Ironhack Contacts</h2>
+
+      <button onClick={pickRandom} className="random-btn">Add Random Contact</button>
+      <table>
+        <tr className="table-header">
+          <th>Picture</th>
+          <th>Name</th>
+          <th>Popularity</th>
+        </tr>
+        <tr>
+          <td><img className="imgClass" src={arrFromContact[0].pictureUrl} alt="image1" /></td>
+          <td>{arrFromContact[0].name}</td>
+          <td>{arrFromContact[0].popularity}</td>
+        </tr>
+        <tr>
+          <td><img className="imgClass" src={arrFromContact[1].pictureUrl} alt="image1" /></td>
+          <td>{arrFromContact[1].name}</td>
+          <td>{arrFromContact[1].popularity}</td>
+        </tr>
+        <tr>
+          <td><img className="imgClass" src={arrFromContact[2].pictureUrl} alt="image1" /></td>
+          <td>{arrFromContact[2].name}</td>
+          <td>{arrFromContact[2].popularity}</td>
+        </tr>
+        <tr>
+          <td><img className="imgClass" src={arrFromContact[3].pictureUrl} alt="image1" /></td>
+          <td>{arrFromContact[3].name}</td>
+          <td>{arrFromContact[3].popularity}</td>
+        </tr>
+        <tr>
+          <td><img className="imgClass" src={arrFromContact[4].pictureUrl} alt="image1" /></td>
+          <td>{arrFromContact[4].name}</td>
+          <td>{arrFromContact[4].popularity}</td>
+        </tr>
+      </table>
     </div>
   );
 }
