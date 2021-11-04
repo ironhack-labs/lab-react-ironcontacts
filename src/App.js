cb33d0657b922746eee94c4660cf469517a0c2bc
jeannette.valencia <jfatima.valencia@gmail.com>
2021-11-04 23:09:44 +0100

    Iteration 2 Random Contact

--- src/App.js
+++ src/App.js
@@ -1,19 +1,61 @@
-
-import Main from "./Main"
 import './App.css';
+import contacts from "./contacts.json";
+import React from "react";
 
+let newArr = contacts.slice(0, 5)
 
-function App (){
-  return (
-    <>
-      <div className="App">
-        <h1>IronContacts</h1>
-        <div>
-          <Main/>
-        </div>
+function renderRandomContact (){
+  return contacts[Math.ceil(Math.random()*contacts.length)];
+}
+class Contact extends React.Component{
+  render(){
+    return(
+      <div>
+        <tr className="contact-details">
+          <td><img src={this.props.pictureUrl} alt="actor"></img></td>
+          <td>{this.props.name}</td>
+          <td>{this.props.popularity}</td>
+        </tr>
       </div>
-    </>
-  )
+    )
+  }
+}
+
+class App extends React.Component{
+  state = {
+    newListContacts: newArr,
+  }
+  createRandomContact = ()=>{
+    this.setState((prevState, props)=>{
+      let randomContact =renderRandomContact();
+      console.log(randomContact)
+      return {newListContacts : [randomContact, ...prevState.newListContacts]}
+    })
+  }
+  render(){
+    return (
+      <>
+        <div className="App">
+          <h1>IronContacts</h1>
+          <button onClick={this.createRandomContact}>Add Random Contact</button>
+          <table>
+            <thead>
+              <tr>
+                  <th>Picture</th>
+                  <th>Name</th>
+                  <th>Popularity</th>
+              </tr>
+            </thead>
+            <tbody>
+              {this.state.newListContacts.map((contact)=>{
+                return <Contact key ={contact.id} pictureUrl={contact.pictureUrl} name={contact.name} popularity={contact.popularity}/>
+              })}
+            </tbody>
+          </table>
+        </div>
+      </>
+    )
+  }
 }
 
 export default  App;
