a81a5d1e063c4f8a0c8d91c3fadf2bc2b5106429
Aintzane Morales <aintzanemorales1992@gmail.com>
2021-11-05 09:46:12 +0100

    Iteration 3 done

--- src/App.js
+++ src/App.js
@@ -3,8 +3,13 @@ import contacts from "./contacts.json";
 import React from "react";
 
 const initialContacts = contacts.slice(0,5)
-
 // console.log(initialContacts)
+function getActorRandomly(){
+  return contacts[Math.floor(Math.random()*contacts.length)]
+}
+// console.log(getRandomActor(contacts))
+
+
 class Actors extends React.Component {
    render() {
     return (
@@ -17,16 +22,12 @@ class Actors extends React.Component {
   }
 }
 
-function getActorRandomly(){
-  return contacts[Math.floor(Math.random()*contacts.length)]
-}
-// console.log(getRandomActor(contacts))
-
 class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
       initialActors: initialContacts
+
     };
   }
 
@@ -34,18 +35,42 @@ class App extends React.Component {
     this.setState((prevState) => {
       let actorRandom = getActorRandomly();
       const newListActors = [...prevState.initialActors, actorRandom]
+      // console.log(newListActors)
       return {initialActors: newListActors}
-    })    
+    })
+  }
+
+
+  sortName = () => {
+
+    this.setState((prevState, props) => {
+      const objectOfNames = prevState.initialActors.sort((a,b) => {
+        return a.name.localeCompare(b.name)
+        
+      });
+      return {initialActors: objectOfNames}
+      
+    })
   }
   
+    
+  sortByPopularity = () => {
+    this.setState((prevState, props) => {
+      const popularityList = prevState.initialActors.sort((a, b) => {
+        return a.popularity- b.popularity
+      });
+      return {initialActors: popularityList}
+    })
+  }
+
   render() {
     return (
       <div className="container">
         <div className="table">
           <h1>IronContacts</h1>
           <button onClick={this.randomActor}>Add Random Contact</button>
-          <button onClick={this.sortName}>sort the table by name</button>
-          <button >sort by popularity</button>
+          <button onClick={this.sortName}>Sort the table by name</button>
+          <button onClick={this.sortByPopularity}>sort by popularity</button>
           <table>
           <tr>
             <th>Picture</th>
@@ -55,6 +80,7 @@ class App extends React.Component {
 
           <tbody>
             {this.state.initialActors.map((actor) => {
+              // console.log({...actor})
               return <Actors key={actor.id} {...actor} />
             })}
           </tbody>
