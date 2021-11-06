08717b22afdf0532d0b778fea01a96adccf8ea93
jeannette.valencia <jfatima.valencia@gmail.com>
2021-11-06 11:42:53 +0100

    Iteration 4 done

--- src/App.js
+++ src/App.js
@@ -15,6 +15,7 @@ class Contact extends React.Component{
           <td><img src={this.props.pictureUrl} alt="actor"></img></td>
           <td>{this.props.name}</td>
           <td>{this.props.popularity}</td>
+          <td><button onClick={this.props.deleteButton}>DELETE</button></td>
         </tr>
       </div>
     )
@@ -36,8 +37,9 @@ class App extends React.Component{
   sortByName = ()=>{
     this.setState((prevState, props)=>{
       const sortedList = prevState.newListContacts.sort((a,b)=>{
-        return a.name - b.name
+        return a.name.localeCompare(b.name)
       })
+      console.log (sortedList)
       return {newListContacts: sortedList}
     })
   }
@@ -45,11 +47,21 @@ class App extends React.Component{
   sortByPopularity = ()=>{
     this.setState((prevState, props)=>{
       const sortedList = prevState.newListContacts.sort((a,b)=>{
-        return a.popularity > b.popularity
+        return a.popularity > b.popularity ? 1 : b.popularity > a.popularity ? -1 : 0
       })
       return {newListContacts: sortedList}
     })
   }
+
+  handleDeleteContact(contactId){
+    this.setState((prevState, prps)=>{
+      const newList = prevState.newListContacts.filter((item)=>{
+        return item.id != contactId
+      })
+      return {newListContacts : newList}
+    })
+  }
+
   render(){
     return (
       <>
@@ -70,7 +82,13 @@ class App extends React.Component{
             </thead>
             <tbody>
               {this.state.newListContacts.map((contact)=>{
-                return <Contact key ={contact.id} pictureUrl={contact.pictureUrl} name={contact.name} popularity={contact.popularity}/>
+                return <Contact
+                key ={contact.id}
+                pictureUrl={contact.pictureUrl}
+                name={contact.name}
+                popularity={contact.popularity}
+                deleteButton = {()=>this.handleDeleteContact(contact.id)}
+                />
               })}
             </tbody>
           </table>
