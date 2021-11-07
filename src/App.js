77e3f8cdae670a669958f23e235016284375b979
Geoffrey Laville <laville.geoffrey@gmail.com>
2021-11-07 18:50:32 +0100

    iteration 1

--- src/App.js
+++ src/App.js
@@ -1,25 +1,58 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import contacts from "./contacts.json";
 
-function App() {
+let smallArr = contacts.slice(0, 5);
+console.log("1st console log", smallArr);
+
+function Contacts(props) {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <tr>
+      <td>
+        <img className="picture" src={props.pictureUrl} alt="profil" />
+      </td>
+      <td>{props.name}</td>
+      <td>{props.popularity}</td>
+    </tr>
   );
+}
+
+class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      contacts: smallArr,
+    };
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <h1>IronContacts</h1>
+        <table className="table">
+          <thead>
+            <tr>
+              <td><h2>Picture</h2></td>
+              <td><h2>Name</h2></td>
+              <td><h2>Popularity</h2></td>
+            </tr>
+          </thead>
+          <tbody>
+            {this.state.contacts.map((elm) => {
+              return (
+                <Contacts
+                  key={elm.id}
+                  pictureUrl={elm.pictureUrl}
+                  name={elm.name}
+                  popularity={elm.popularity}
+                />
+              );
+            })}
+          </tbody>
+        </table>
+      </div>
+    );
+  }
 }
 
 export default App;
