cface654bb7701d89c6212ef095f9805303a3410
Analia Lopez <analialopezrodriguez@gmail.com>
2021-11-30 18:52:23 +0100

    Lab

--- src/App.js
+++ src/App.js
@@ -1,25 +1,81 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import contacts from "./contacts.json";
 
-function App() {
+let contactsCopy = contacts.slice(0,5)
+class App extends React.Component {
+
+  constructor(){
+    super()
+
+    this.state = {
+      fiveContacts: contactsCopy
+    }
+  }
+
+
+  addRamdom(){
+    let newContacts = this.state.fiveContacts
+    newContacts.push(contacts[Math.ceil(Math.random()*contacts.length)])
+
+    this.setState({
+      fiveContacts: newContacts
+    })
+  }
+
+  sortByName = () => {
+    contactsCopy.sort((a,b) => a.name.localeCompare(b.name) )
+    this.setState({
+      fiveContacts: contactsCopy
+    })
+  }
+    sortByPopularity = () => {
+      contactsCopy.sort((a,b) => b.popularity - a.popularity)
+      this.setState({
+        fiveContacts: contactsCopy
+      })
+
+  }
+
+  
+
+  render(){
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+    <h1> IronContacts</h1>
+    <button onClick={() => this.addRamdom()}> Add ramdom contacts</button>
+    <button onClick={() => this.sortByName()}> Sort by id</button>
+    <button onClick={() => this.sortByPopularity()}> Sort by popularity</button>
+    <table class="table">
+        <thead>
+          <tr>
+            <th scope="col">#</th>
+            <th scope="col">Picture</th>
+            <th scope="col">Name</th>
+            <th scope="col">Pupularity</th>
+          </tr>
+        </thead>
+
+        <tbody>
+        {this.state.fiveContacts.map(eachContact => {
+          return(
+          <tr>
+            <th scope="row"></th>
+            <td><img src={(eachContact.pictureUrl)}/></td>
+            <td>{eachContact.name}</td>  
+            <td>{eachContact.popularity.toFixed(2)}</td>
+          </tr>
+          )
+            }
+          )
+        }
+        </tbody>
+    </table>
+    
     </div>
-  );
+  )
+  }
 }
+
 
 export default App;
