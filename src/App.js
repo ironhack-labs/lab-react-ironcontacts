691466a9b57afbdee41e1bc3ad46cec1960ba775
jmsd <sanchezdiezma@gmail.com>
2021-11-30 18:31:00 +0100

    lab done

--- src/App.js
+++ src/App.js
@@ -1,25 +1,59 @@
-import logo from './logo.svg';
+import { Component } from 'react';
 import './App.css';
+import contacts from "./contacts.json";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+class App extends Component {
+  constructor() {
+    super()
+    this.state = {
+      contactsFiltered: []
+    }
+  }
+
+  //OLVIDAR HASTA MAÑANA
+  componentDidMount = () => this.setState({
+    contactsFiltered: contacts.splice(0, 5)
+  })
+
+  addRandom = () => {
+    const newArr = [...this.state.contactsFiltered]
+    // calcular indice aleatorio y pasarle del array contacts el elemento con ese indice
+    newArr.push()
+    this.setState({ contactsFiltered: newArr })
+  }
+  
+  render() {
+    return (
+      <div>
+      <button onClick={this.addRandom}>Add random</button>
+        <table>
+      <thead>
+            <tr>
+              <th>Picture</th>
+              <th>Name</th>
+              <th>Popularity</th>
+            </tr>
+              
+      </thead>
+      <tbody>
+            {this.state.contactsFiltered.map(actor => {
+        return (
+          
+          <tr>
+            <td><img src={actor.pictureUrl}/></td>
+            <td>{actor.name}</td>
+            <td>{actor.popularity}</td>
+          </tr>
+          
+        )
+      })}
+    
+      </tbody>
+      </table>
+      </div>
+    );
+  }
 }
 
 export default App;
