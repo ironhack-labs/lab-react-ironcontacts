614b687378f119ec230211b00215f236703b8775
Dulce Fabiola <dulcefabiola.rm@gmail.com>
2021-12-09 19:47:06 -0600

    sort

--- src/components/Contacts.js
+++ src/components/Contacts.js
@@ -12,6 +12,28 @@ const Contacts = () => {
     setAddContact([...addContact, contacts[random]]);
   };
 
+  //Creamos una nueva data
+  const [sortContact, setSortContact] = useState(addContact);
+  //Funciones de sort
+  const sortByName = () => {
+    console.log("name");
+    //ordenar alfabeticamente DESC
+    setSortContact([
+      sortContact
+        .sort((a, b) => {
+          if (a.name < b.name) return 1;
+          if (a.name > b.name) return -1;
+          if (a.name === 0) return 0;
+        })
+        .reverse(),
+    ]);
+  };
+
+  const sortByPopularity = () => {
+    console.log("popularity");
+    setSortContact([sortContact.sort((a, b) => b.popularity - a.popularity)]);
+  };
+
   return (
     <>
       {/* <h1>Add new Contact</h1>
@@ -20,14 +42,17 @@ const Contacts = () => {
       <h1>Change Contacts</h1>
       <button onClick={() => generateRandomContact()}>Random Contacts</button>
 
-      {addContact.map(({ name, pictureUrl, popularity, id }) => {
+      <h1>Sort</h1>
+      <button onClick={() => sortByName()}>Sort by Name</button>
+      <button onClick={() => sortByPopularity()}>Sort by Popularity</button>
+
+      {addContact.map(({ name, pictureUrl, popularity, id }, index) => {
         return (
           <Contact
-            key={id}
+            key={index}
             name={name}
             pictureUrl={pictureUrl}
             popularity={popularity}
-            id={id}
           />
         );
       })}
