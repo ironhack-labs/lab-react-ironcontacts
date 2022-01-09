7b5f8eac713cd4483a4f62b6ec34aa8e67d96349
karlajaramillo <karlav.jaramillo@gmail.com>
2022-01-09 17:18:54 +0100

    Iteration 1 - display 5 contacts - done

--- src/App.js
+++ src/App.js
@@ -7,35 +7,32 @@ function App() {
   console.log(firstContacts)
   
   return (
-    <div className="App">
-    <h1>IronContacts</h1>
+    <div className="app-wrapper">
+      <h1>IronContacts Hello</h1>
+      <button class="random-contact" onClick={(e)=> console.log(e.target, 'hello')}>+ Add Random Contact</button>
 
-    {/* Iteration 1 - table of contacts */}
+      {/* Iteration 1 - table of contacts */}
       <div className="contacts-table">
-      <table>
-       <thead>
-          <th>Picture</th>
-          <th>Name</th>
-          <th>Popularity</th>
-        </thead>
-        <tbody>
-        {firstContacts.map(contact => {
-          return(
-            <tr key={contact.id}>
-              <td><img src={contact.pictureUrl} width={50} alt="" /></td>
-              <td>{contact.name}</td>
-              <td>{contact.popularity}</td>
-            </tr>
-            )  
-        })
-      }
-      </tbody>
-      </table>
-  
- 
-
-
-        
+        <table>
+          <thead>
+              <th>Picture</th>
+              <th>Name</th>
+              <th>Popularity</th>
+            </thead>
+            <tbody>
+            {console.log(firstContacts)}
+            {firstContacts.map(contact => {
+              return(
+                <tr key={contact.id}>
+                  <td><img src={contact.pictureUrl} width={50} alt="" /></td>
+                  <td>{contact.name}</td>
+                  <td>{contact.popularity}</td>
+                </tr>
+                )  
+              })
+            }
+          </tbody>
+        </table>  
       </div>
     </div>
   );
