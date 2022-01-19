e2940c4b3e2a1c08354b217d410d3cf27dacaf97
Michael Seitz <michael.s.seitz@gmail.com>
2022-01-19 22:03:49 +0100

    iteration 1 ... i guess

--- src/App.js
+++ src/App.js
@@ -1,23 +1,47 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import contacts from "./contacts.json";
+import "./App.css";
+
+// to get only the first 5 contacts from contacts
+const initialContacts = contacts.slice(0, 5);
 
 function App() {
+  const [contactSlate, setContactSlate] = React.useState(initialContacts);
+  // preparing table elements
+  const contactList = () =>
+    contactSlate.map((contact) => {
+      return (
+        <tr key={contact.id}>
+          <td>
+            <img style={imgStyle} src={contact.pictureUrl} alt={contact.name} />
+          </td>
+          <td>{contact.name}</td>
+          <td>{contact.popularity}</td>
+        </tr>
+      );
+    });
+
+  // styling for the table
+  const tableStyle = {
+    borderCollapse: "collapse",
+    width: "80%",
+    margin: "0 10%",
+  };
+  const imgStyle = {
+    height: "150px",
+  };
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <table style={tableStyle}>
+        <thead>
+          <tr>
+            <th>Picture</th>
+            <th>Name</th>
+            <th>Popularity</th>
+          </tr>
+        </thead>
+        <tbody>{contactList()}</tbody>
+      </table>
     </div>
   );
 }
