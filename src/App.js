da22e437b9d93f756dc882b8553b7cd96347ee21
phimanim <aliker.imane@gmail.com>
2022-01-20 16:57:56 +0100

    it 1

--- src/App.js
+++ src/App.js
@@ -1,23 +1,33 @@
 import logo from './logo.svg';
 import './App.css';
+import contacts from "./contacts.json";
 
 function App() {
+  const slicedArray = contacts.slice(0, 4);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      Iron Contacts
+      <table>
+    <thead>
+        <tr>
+            <th>Name</th>
+            <th>Photo</th>
+            <th>Popularity</th>
+        </tr>
+    </thead>
+    <tbody>
+   
+    {slicedArray.map((c)=>(
+        <tr>
+        <td> <img src={c.pictureUrl}></img>  </td>
+        <td> {c.name} </td>
+        <td> {Math.round(c.popularity * 100) / 100}</td>
+      </tr>
+
+      ))}
+        
+    </tbody>
+</table>
     </div>
   );
 }
