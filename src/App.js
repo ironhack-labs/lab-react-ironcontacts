a1e8ba8be2a709465cfe4385ce11ef2e1372d517
Michael Seitz <michael.s.seitz@gmail.com>
2022-01-20 21:16:00 +0100

    iteration 3

--- src/App.js
+++ src/App.js
@@ -4,45 +4,27 @@ import "./App.css";
 
 function App() {
   // to get only the first 5 contacts from contacts as initial state
-  const initialContacts = contactsData.slice(0, 5).map((contact) => (
-    <tr key={contact.id}>
-      <td>
-        <img
-          style={{ height: "150px" }}
-          src={contact.pictureUrl}
-          alt={contact.name}
-        />
-      </td>
-      <td>{contact.name}</td>
-      <td>{contact.popularity}</td>
-    </tr>
-  ));
+  const initialContacts = contactsData.slice(0, 5);
   //setting up the contact state
   const [contacts, setContacts] = React.useState(initialContacts);
   const addRandomActor = () => {
     // random actor suchen
     const randomActor =
       contactsData[Math.floor(Math.random() * contactsData.length)];
-    const randomActorElement = (
-      <tr key={randomActor.id}>
-        <td>
-          <img
-            style={{ height: "150px" }}
-            src={randomActor.pictureUrl}
-            alt={randomActor.name}
-          />
-        </td>
-        <td>{randomActor.name}</td>
-        <td>{randomActor.popularity}</td>
-      </tr>
-    );
     // durch set Contacts dem state hinzufügen
-    setContacts([randomActorElement, ...contacts]);
+    setContacts([randomActor, ...contacts]);
+  };
+  const sortNames = () => {
+    // const sortedContacts = contacts.sort((a,b) => a.name-b.name);
+    const sortedContacts = contacts.sort((a, b) => a.name.localeCompare(b.name));
+    setContacts([...sortedContacts]);
   };
 
   return (
     <div className="App">
       <button onClick={addRandomActor}>Add Random Actor</button>
+      <button onClick={sortNames}>Sort by Names</button>
+      {/* <button onClick={sortPop}>Sort by Popularity</button> */}
       <table style={{ width: "90%" }}>
         <thead>
           <tr>
@@ -51,7 +33,21 @@ function App() {
             <th>Popularity</th>
           </tr>
         </thead>
-        <tbody>{contacts}</tbody>
+        <tbody>
+          {contacts.map((contact) => (
+            <tr key={contact.id}>
+              <td>
+                <img
+                  style={{ height: "150px" }}
+                  src={contact.pictureUrl}
+                  alt={contact.name}
+                />
+              </td>
+              <td>{contact.name}</td>
+              <td>{contact.popularity}</td>
+            </tr>
+          ))}
+        </tbody>
       </table>
     </div>
   );
