ab8f5e32c080d37ef7dcf9ffe20d1c26d63807bf
Oswaldo <oswaldoavilesib@gmail.com>
2022-02-17 09:29:27 -0600

    Iteration 3 done

--- src/App.js
+++ src/App.js
@@ -1,16 +1,25 @@
 import { useState } from "react";
 import contactsData from "./contacts.json";
-import './App.css'
+import "./App.css";
 
-console.log(contactsData);
+console.log("LENGTH",contactsData.length);
 
 function App() {
   const [contacts, setContacts] = useState(contactsData.slice(0, 5));
 
+  const handleClick = () => {
+    const randomNumber = Math.floor(Math.random() * contacts.length)
+    // console.log("RANDOMNUMBER",randomNumber)
+    console.log("RADOM CONTACT",contacts[randomNumber])
+    setContacts([...contacts,contactsData[randomNumber]])
+
+  }
   console.log("CONTACTS", contacts);
 
   return (
     <div className="App">
+    <h1>IronContacts</h1>
+      <button onClick={handleClick}>Add Random Contact</button>
       <table>
         <thead>
           <tr>
@@ -22,10 +31,16 @@ function App() {
           </tr>
         </thead>
         <tbody className="table-body">
-          {contacts.map((contact) => {
+          {contacts.map((contact,index) => {
             return (
-              <tr>
-              <td><img className="profile-pic" src={contact.pictureUrl} alt=""/></td>
+              <tr key={index}>
+                <td>
+                  <img
+                    className="profile-pic"
+                    src={contact.pictureUrl}
+                    alt=""
+                  />
+                </td>
                 <td>{contact.name}</td>
                 <td>{contact.popularity}</td>
                 <td>{contact.wonOscar ? <span>🏆</span> : null}</td>
