f963170adcd75c5770f6beba9a80a70826debbcd
stephan2311 <96991576+stephan2311@users.noreply.github.com>
2022-02-22 19:42:00 +0100

    Iteration 4 & 5 done

--- src/App.js
+++ src/App.js
@@ -17,11 +17,23 @@ function App() {
     setContacts([...contacts])
   }
 
+  const sortByPopularity = () => {
+    contacts.sort((a,b) => a.popularity < b.popularity ? 1 : -1);
+    setContacts([...contacts])
+  }
+
+  const sortByName = () => {
+    contacts.sort((a,b) => a.name.localeCompare(b.name));
+    setContacts([...contacts])
+  }
+
   return (
     <div className="App">
       <header className="App-header">
         <h1>Ironcontacts</h1>
         <button onClick={addRandomContact}>Add Random Contact</button>
+        <button onClick={sortByPopularity}>Sort by Popularity</button>
+        <button onClick={sortByName}>Sort by Name</button>
         <table>
           <thead>
             <tr>
