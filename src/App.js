fa33ef7ad4301985690d8af94802153a891e2077
stephan2311 <96991576+stephan2311@users.noreply.github.com>
2022-02-23 06:19:37 +0100

    Some Styling added

--- src/App.js
+++ src/App.js
@@ -18,33 +18,46 @@ function App() {
   }
 
   const sortByPopularity = () => {
-    contacts.sort((a,b) => a.popularity < b.popularity ? 1 : -1);
+    contacts.sort((a, b) => a.popularity < b.popularity ? 1 : -1);
     setContacts([...contacts])
   }
 
   const sortByName = () => {
-    contacts.sort((a,b) => a.name.localeCompare(b.name));
+    contacts.sort((a, b) => a.name.localeCompare(b.name));
     setContacts([...contacts])
   }
+
+  // const removeContact = (e) => {
+  //     const array = [...contacts];
+  //     const index = array.indexOf(e)
+  //     if (index !== -1) {
+  //       array.splice(index, 1);
+  //       setContacts({contacts: array});
+  //     }
+  //   };
+
 
   return (
     <div className="App">
       <header className="App-header">
         <h1>Ironcontacts</h1>
-        <button onClick={addRandomContact}>Add Random Contact</button>
-        <button onClick={sortByPopularity}>Sort by Popularity</button>
-        <button onClick={sortByName}>Sort by Name</button>
-        <table>
-          <thead>
+        <div className='Control'>
+          <button onClick={addRandomContact}>Add Random Contact</button>
+          <button onClick={sortByPopularity}>Sort by Popularity</button>
+          <button onClick={sortByName}>Sort by Name</button>
+        </div>
+        <table className='Table'>
+          <thead className='TableHeader'>
             <tr>
-              <th>Picture</th>
+              <th></th>
               <th>Name</th>
               <th>Popularity</th>
               <th>Won Oscar</th>
               <th>Won Emmy</th>
+              <th>Actions</th>
             </tr>
           </thead>
-          <tbody>
+          <tbody className='TableBody'>
             {contacts.map(element => {
               return (
                 <tr>
@@ -53,6 +66,7 @@ function App() {
                   <td>{element.popularity.toFixed(2)}</td>
                   <td>{element.wonOscar ? '🏆' : ''}</td>
                   <td>{element.wonEmmy ? '🏆' : ''}</td>
+                  {/* <td><button onClick={() => removeContact(element.id)}>Delete</button></td> */}
                 </tr>
               )
             })}
