ff9c4184586301b4b85f8eb6c3aa2e0ca7bc08fa
joaonacho <joaonacho@gmail.com>
2022-03-15 21:32:14 +0000

    iteration 1

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useState } from "react/cjs/react.production.min";
+import newContacts from "./contacts.json";
 
 function App() {
+  // const [contacts, setContacts] = useState([...newContacts.splice(0, 5)]);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <>
+        {/* <ul>
+          {contacts.map((contact) => {
+            return <li>{contact.name}</li>;
+          })}
+        </ul> */}
+      </>
     </div>
   );
 }
