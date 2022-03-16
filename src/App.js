81ca79b7ee6399bff0494c3098a816976c38d62c
joaonacho <joaonacho@gmail.com>
2022-03-16 13:03:27 +0000

    iteration 3 done

--- src/App.js
+++ src/App.js
@@ -1,19 +1,53 @@
 import "./App.css";
-import { useState } from "react/cjs/react.production.min";
-import newContacts from "./contacts.json";
+import { useState } from "react";
+const newContacts = require("./contacts.json");
 
 function App() {
-  // const [contacts, setContacts] = useState([...newContacts.splice(0, 5)]);
+  const [contacts, setContacts] = useState(newContacts.slice(0, 5));
 
   return (
     <div className="App">
-      <>
-        {/* <ul>
-          {contacts.map((contact) => {
-            return <li>{contact.name}</li>;
+      <h1>IronContacts</h1>
+      <button
+        onClick={() =>
+          setContacts([
+            ...contacts,
+            newContacts[Math.floor(Math.random() * newContacts.length) + 5],
+          ])
+        }
+      >
+        Add random contact
+      </button>
+      <table>
+        <thead>
+          <tr>
+            <th>Image</th>
+            <th>Name</th>
+            <th>Popularity</th>
+            <th>Won an Oscar</th>
+            <th>Won an Emmy</th>
+          </tr>
+        </thead>
+        <tbody>
+          {contacts.map((contact, index) => {
+            return (
+              <tr key={index}>
+                <td>
+                  <img
+                    src={contact.pictureUrl}
+                    alt="contact-img"
+                    style={{ width: "100px" }}
+                  />
+                </td>
+                <td>{contact.name}</td>
+                <td>{contact.popularity}</td>
+                <td>{contact.wonOscar ? "🏆" : ""}</td>
+                <td>{contact.wonEmmy ? "🏆" : ""}</td>
+              </tr>
+            );
           })}
-        </ul> */}
-      </>
+        </tbody>
+      </table>
     </div>
   );
 }
