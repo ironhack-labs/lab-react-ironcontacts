2c2c9ae6fa8a57f86f3da94cb0ca5b679eb53d74
Vitor Afonso <vittokm@hotmail.com>
2022-03-17 16:23:20 +0000

    iteration 3 done

--- src/App.js
+++ src/App.js
@@ -4,9 +4,33 @@ import contactsData from './contacts.json';
 import {useState} from 'react';
 
 function App() {
-
+  //state1
   let [contacts, setContacts] = useState([contactsData[0], contactsData[1], contactsData[2], contactsData[3], contactsData[4]]);
 
+  let restOfContacts = [];
+  for(let i = 5; i < contactsData.length;i++) {
+    restOfContacts.push(contactsData[i]);
+  }
+  //state2
+  let [newContacts, setNewContacts] = useState(restOfContacts);
+
+
+  const addRandomContact = () => {
+    
+    let randomContact = newContacts[Math.floor(Math.random() * newContacts.length)];
+    
+    setContacts([...contacts, randomContact]);
+
+    setNewContacts(newContacts.filter(item => item.id !== randomContact.id));
+    
+    /* console.log('index to remove =>',newContacts.indexOf(randomContact));
+    console.log('name =>',randomContact.name);
+    console.log('rest of contacts after adding to contacts =>',newContacts);
+    console.log('new contacts  =>',contacts); */
+
+    
+    
+  };
 
   return (
     <div className="App">
@@ -31,9 +55,11 @@ function App() {
                 <td>{contact.wonEmmy && <span>🏆</span>}</td>
               </tr>
             )
-          })}      
+          })}     
+          
         </tbody>
       </table>
+      <button onClick={addRandomContact}>Add Random Contact</button> 
     </div>
   );
 }
