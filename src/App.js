15962d61c39ca5050af334b76cf643a9108d0c65
Corentin Aesch <corentin.aesch@gmail.com>
2022-03-17 19:30:42 +0000

    iteration 4 done

--- src/App.js
+++ src/App.js
@@ -1,23 +1,51 @@
-import logo from './logo.svg';
+import contactsJSON from "./contacts.json";
 import './App.css';
+import { useState } from "react";
+
 
 function App() {
+
+  const [contacts, setContacts] = useState(contactsJSON.slice(0,5))
+
+
+  const addNewContact = () => {
+    let newContactIndex = Math.floor(Math.random() * contactsJSON.length);
+    setContacts([...contacts, contactsJSON[newContactIndex]])
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <button onClick={addNewContact}>Add Contact</button>
+      <button onClick={() => setContacts(contacts.name.sort())}>Sort by name</button>
+      <button onClick={() => setContacts(contacts.popularity.sort((a,b) => a - b))}>Sort by popularity</button>
+
+      <table>
+        <thead>
+          <tr>
+            <th>IronContacts</th>
+          </tr>
+          <tr>
+            <th>Name</th>
+            <th>Popularity</th>
+            <th>Won Emmy</th>
+            <th>Won Oscar</th>
+          </tr>
+        </thead>
+        {contacts.map((contact) => {
+          return (
+            <>
+              <tbody>
+                <tr>
+                  <td>{contact.name}</td>
+                  <td>{contact.popularity.toFixed(2)}</td>
+                  <td>{contact.wonEmmy ? "🏆" : ""}</td>
+                  <td>{contact.wonOscar ? "🏆" : ""}</td>
+                </tr>
+              </tbody>
+            </>
+          )
+        })}
+      </table>
     </div>
   );
 }
