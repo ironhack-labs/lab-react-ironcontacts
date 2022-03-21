c64ff3e7b319802747584e72e9ca788f95166f9e
Arturo Vivar <arthas1237@hotmail.com>
2022-03-21 14:57:58 +0100

    added list

--- src/App.js
+++ src/App.js
@@ -1,11 +1,33 @@
 import logo from './logo.svg';
+import React from 'react'
 import './App.css';
-import contacts from "./contacts.json";
+import contactsJson from "./contacts.json";
 
 
 function App() {
+
+  //cogemos los 5 primeros contactos
+    const startContacts = contactsJson.slice(0, 5);
+
+  // probando el useState de react, tambien podriamos poner arriba { useState }
+    const [contacts, setContacts] = React.useState(startContacts);
+
+    
+
+
+
   return (
     <div className="App">
+     <table>
+       <h1>IronContacts</h1>
+       {contacts.map((contact) => (
+
+        <p>{contact.name}</p> 
+
+       ))}
+       
+       </table> 
+      
       
     </div>
   );
