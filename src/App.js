7df2f77e31d8cf35e780b75e9f3763b46dff8865
Robert Cabrera <cabrera.rch@gmail.com>
2022-03-22 16:10:59 +0100

    Iteration 4 done

--- src/App.js
+++ src/App.js
@@ -5,31 +5,62 @@ import contacts from './contacts.json'; // Total: 52
 class App extends Component {
   state =  {
     contacts: [...contacts].splice(0, 5), // first 5
-    remainingContacts: [...contacts].splice(5, contacts.length) // other 47
+    remainingContacts: [...contacts].splice(5, contacts.length), // other 47
+    sort: ''
   }
 
   randomContact = () => {
     const { contacts, remainingContacts } = this.state;
     const randomIndex = Math.floor(Math.random() * (remainingContacts.length - contacts.length + 1) + contacts.length);
 
-
-    this.setState( {
+    this.setState({
         contacts: this.state.contacts.concat(remainingContacts[randomIndex])
     })
+  }
 
+  handleSortBy = (event) => {
+    const { name } = event.target;
+    console.log(`the name of envent.targer is ${name}`)
+
+    this.setState(prevState => {
+      return {
+        sort: prevState.sort === name ? '' : name
+      }
+    })
+  }
+
+  sortContacts = () => {
+    const { contacts, sort } = this.state;
+
+    if (sort === 'name') {
+      return contacts.sort((a, b) => {
+        if (a.name < b.name) return -1
+        if (b.name < a.name) return 1
+        return 0
+      })
+    }
+
+    if (sort === 'popularity') {
+      return contacts.sort((a, b) => {
+        if (a.popularity < b.popularity) return 1
+        if (b.popularity < a.popularity) return -1
+        return 0
+      })
+    }
   }
 
 
   render() {
     const { contacts } = this.state;
-    console.log(contacts)
-
+    const contactsSort = this.sortContacts();
 
     return (
       <div className="app wrapper">
   
         <h1>IronContacts</h1>
-        <button onClick={this.randomContact}>Add Random Contact</button>
+        <button onClick={this.randomContact}>Add Random Contact</button>        
+        <button name="name" onClick={this.handleSortBy}>Sort by name</button>
+        <button name="popularity" onClick={this.handleSortBy}>Sort by popularity</button>
   
         <div className="table">
           <table>
