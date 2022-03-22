8998d120d5aec73155fdf9449e0df57157013a3a
Maria Gallego <mariagestionredes@gmail.com>
2022-03-22 13:15:10 +0100

    iteration 2 done

--- src/App.js
+++ src/App.js
@@ -4,30 +4,58 @@ import contacts from "./contacts.json";
 
 class App extends Component {
   state = {
-    contacts: contacts,
+    contacts: contacts, //tried to contacts.slice here but didn´t work 👀👀
   };
+  addRandomContact = () => {
+    const remainingContacts = this.state.contacts.slice(5);
+    const random = remainingContacts[Math.floor(Math.random() * remainingContacts.length)];
+
+    this.setState({
+      contacts: [...this.state.contacts, random],
+    });
+  };
+
   render() {
     const { contacts } = this.state;
     const justFiveContacts = contacts.slice(0, 5);
-    console.log(justFiveContacts)
+
     return (
       <div className="App">
         <div className="container">
-        <h1>Ironcontacts</h1>
-          <table className='table'>
-            <tr>
-              <th>Pic</th>
-              <th>Name</th>
-              <th>Popularity</th>
-            </tr>
+          <h1>Ironcontacts</h1>
+          <button
+            name="random"
+            className="RandomButton"
+            onClick={() => this.addRandomContact()}
+          >
+            Add Random Contact
+          </button>
+          <table className="table">
+            <tbody>
+              <tr>
+                <th>Pic</th>
+                <th>Name</th>
+                <th>Popularity</th>
+                <th>Oscars</th>
+                <th>Emmys</th>
+              </tr>
+            </tbody>
             {justFiveContacts.map((elements, key) => {
               return (
                 <tr key={key}>
-                  <td><img className='ContactsPics' src={elements.pictureUrl} alt=''/></td>
+                  <td>
+                    <img
+                      className="ContactsPics"
+                      src={elements.pictureUrl}
+                      alt=""
+                    />
+                  </td>
                   <td>{elements.name}</td>
                   <td>{elements.popularity.toFixed(2)}</td>
+                  <td>{elements.wonOscar ? "🏆" : ""}</td>
+                  <td>{elements.wonEmmy}</td>
                 </tr>
-              )
+              );
             })}
           </table>
         </div>
