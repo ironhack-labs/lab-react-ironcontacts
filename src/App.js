90848cd0e0d62e7c577572979fbca679821ef27e
Eduardo Francos Margareto <efmargareto@hotmail.com>
2022-03-23 00:46:03 +0100

    iteration 2,3,4 finished

--- src/App.js
+++ src/App.js
@@ -1,19 +1,57 @@
-import contacts from './contacts.json'
+import contactsJson from './contacts.json'
 import "./App.css";
 import { Component } from 'react';
 
 class App extends Component {
 
   state = {
-    contacts: contacts.slice(0,5)
+    allContacts: contactsJson.slice(5, contactsJson.length),
+    contacts: contactsJson.slice(0,5),
+    shortContacts: ''
+  }
+
+  getRamdonContact = () => {
+    const { allContacts, contacts } = this.state
+
+    const newContact = allContacts[Math.floor(Math.random() * allContacts.length)]
+    contacts.push(newContact)
+
+    this.setState({
+      allContacts: allContacts.filter(contact => contact.id !== newContact.id),
+      contacts: contacts
+    })
+  }
+
+  handSorty = (event) => {
+    const { contacts } = this.state
+    const shortByAttr = event.target.name
+
+    let shortElemns;
+    if(shortByAttr === 'name') {
+      shortElemns = contacts.sort(function(a, b){
+        if(a[shortByAttr] < b[shortByAttr]) { return -1 }
+        if(a[shortByAttr] > b[shortByAttr]) { return 1 }
+        return 0;
+      })
+    } else {
+      shortElemns = contacts.sort((a, b) => b[shortByAttr] - a[shortByAttr]);
+    }
+
+    if(!shortElemns) shortElemns = contacts
+
+    this.setState({
+      contacts: shortElemns
+    })
   }
 
   
   render() {
-    console.log(this.state.contacts)
     return (
       <div className="App container">
         <h1 className='m-4'>ironcontacts</h1>
+        <button className="btn btn-primary m-4" onClick={this.getRamdonContact}>Add Ramdon Contact</button>
+        <button className="btn btn-success m-4" name='popularity' onClick={this.handSorty}>Sort By Popularity</button>
+        <button className="btn btn-success m-4" name='name' onClick={this.handSorty}>Sort By Name</button>
         <table className="table">
           <thead>
             <tr>
@@ -32,7 +70,7 @@ class App extends Component {
                   <tr>
                     <td className='ironcontacts-img' style={{backgroundImage: `url(${pictureUrl})`}}></td>
                     <td>{name}</td>
-                    <td>{popularity.toFixed(2)}</td>
+                    <td>{popularity && popularity.toFixed(2)}</td>
                     <td>{wonOscar ? (
                           <span className='iconWin'></span>
                         ) : (
