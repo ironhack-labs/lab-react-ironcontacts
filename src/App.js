56a64958c6d952b824c6c01f0c0b57a379ba81e1
Vicente Garcia <vicentegarciaayllon@gmail.com>
2022-03-22 16:38:56 +0100

    finish iterations 2 and 3

--- src/App.js
+++ src/App.js
@@ -6,6 +6,18 @@ class App extends Component {
   state = {
     contacts: contacts.filter((el, index) => index < 5)
   }
+  
+  addRandomContact() {
+    const newContact = contacts[Math.floor(Math.random() * (contacts.length - 5 + 1) + 5)];
+
+    this.setState(prevState => {
+      if (!prevState.contacts.includes(newContact)){
+        return {
+          contacts: prevState.contacts.concat(newContact)
+        }
+      }
+    })
+  }
 
 
   render(){
@@ -13,6 +25,12 @@ class App extends Component {
     return (
       <div className="App">
         <h2>IronContacts</h2>
+        <button
+          className="add-contact"
+          onClick={() => this.addRandomContact()}
+        >
+          Add Random Contact
+        </button>
         <div className="actors-table container">
           <table>
             <thead>
@@ -20,6 +38,8 @@ class App extends Component {
                 <th>Picture</th>
                 <th>Name</th>
                 <th>Popularity</th>
+                <th>Won Oscar</th>
+                <th>Won Emmy</th>
               </tr>
             </thead>
             <tbody>
@@ -30,6 +50,8 @@ class App extends Component {
                       <th><img src={contact.pictureUrl} alt="" /></th>
                       <th>{contact.name}</th>
                       <th>{contact.popularity.toFixed(2)}</th>
+                      <th>{contact.wonOscar && '🏆'}</th>
+                      <th>{contact.wonEmmy && '🏆'}</th>
                     </tr>
                   )
                 })
