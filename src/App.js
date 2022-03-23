29730d94e90a0caf8caecd247876c8a86287c043
Oscar Sangrador <oscarsangrador@gmail.com>
2022-03-23 16:19:38 +0100

    iteration 4-5. done.

--- src/App.js
+++ src/App.js
@@ -8,7 +8,8 @@ class App extends Component {
 
     state = {
       firstContacts: [...contacts].splice(0,5),
-      otherContacts: [...contacts].splice(5, contacts.length)
+      otherContacts: [...contacts].splice(5, contacts.length),
+      sort: " "
     }
 
     addRandom = () => {
@@ -18,19 +19,70 @@ class App extends Component {
       console.log(contactRandom)
 
       this.setState({
-        firstContacts: [...firstContacts, contactRandom]
+        firstContacts: [contactRandom, ...firstContacts]
       })
       console.log(firstContacts)
     }
 
+    handleSortBy = (event) => {
+      const { name } = event.target
+  
+      this.setState(prevState => {
+        return {
+          sort: prevState.sort === name ? '' : name
+        }
+      })
+    }
+
+    sortContacts = () => {
+      const { firstContacts, sort } = this.state
+
+      if (sort === 'name') {
+        return firstContacts.sort((a, b) => {
+          if (a.name < b.name) return 1
+          if (b.name < a.name) return -1
+          return 0
+        })
+      }
+  
+      if (sort === 'popularity') {
+        return firstContacts.sort((a, b) => {
+          if (a.popularity < b.popularity) return 1
+          if (b.popularity < a.popularity) return -1
+          return 0
+        })
+      }
+    }
+
+    deleteContacts = (id) => {
+      const {firstContacts } = this.state
+
+      this.setState({ firstContacts: firstContacts.filter(contact => contact.id !== id) })
+    }
+
   render(){
+    const { firstContacts } = this.state;
+    this.sortContacts()
+    
     return (
       <div className="App">
     <h3>IronContacts</h3>
-      <button
-        onClick={this.addRandom}>
-      Add Random Contact
-      </button>
+      <div className="buttons">
+        <button
+          onClick={this.addRandom}>
+        Add Random Contact
+        </button>
+        <button
+          onClick={this.handleSortBy}
+          name="name">
+        Sort by name
+        </button>
+        <button
+          onClick={this.handleSortBy}
+          name="popularity">
+        Sort by popularity
+        </button>
+      </div>
     
     <table className="table size-tb">
     <thead>
@@ -38,9 +90,9 @@ class App extends Component {
     <th scope="col">Picture</th>
     <th scope="col">Name</th>
     <th scope="col">Popularity</th>
-    <th scope="col">Won Oscar</th>
-    <th scope="col">Won Emmy</th>
-    {/* <th scope="col"></th> */}
+    <th scope="col">Won an Oscar</th>
+    <th scope="col">Won an Emmy</th>
+    <th scope="col"></th>
     </tr>
     </thead>
       { this.state.firstContacts.map(({ pictureUrl, name, popularity, wonOscar, wonEmmy, id }) => {
@@ -64,6 +116,13 @@ class App extends Component {
                 </td>
                 <td>
                 {wonEmmy && "🏆"}
+                </td>
+                <td>
+                  <button
+                  className="btn btn-danger"
+                  key={id}
+                  onClick={() => this.deleteContacts(id)}>
+                  Delete</button>
                 </td>
               </tr>
               </tbody>
