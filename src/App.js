3fd1f8fe9de9f77f7388224f9dc2119ce3e18878
Alejandra Lopez <alejandralopez91@gmail.com>
2022-03-23 21:01:06 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -6,16 +6,23 @@ class App extends Component {
   state = {
     contacts: completeContactList.slice(0, 5),
   };
+  addRandomContact=()=>{
+  const { contacts } = this.state
+  contacts.push([])
+  }
     render() {
       const { contacts } = this.state;
       return (
         <div className="App">
           <header>
+            <button onClick={()=>this.addRandomContact()}>Add Random Contact</button>
             <table className="table-title">
               <tr className="separate">
                 <th>Picture</th>
                 <th>Name</th>
                 <th>Popularity</th>
+                <th>Won an Oscar</th>
+                <th>Won an Emmy</th>
               </tr>
             </table>
           </header>
@@ -28,6 +35,8 @@ class App extends Component {
                   </td>
                   <td>{item.name}</td>
                   <td>{item.popularity}</td>
+                  <td>{item.wonOscar ? "🏆" : ""}</td>
+                  <td>{item.wonEmmy ? "🏆" : ""}</td>
                 </tr>
               </table>
             );
