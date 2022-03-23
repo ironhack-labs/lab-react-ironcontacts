13bc2319fff0a95de96290d902f9c8fd0e4da236
Pablo Mateos <pmateos805@gmail.com>
2022-03-23 12:58:26 +0100

    Iteration 1

--- src/App.js
+++ src/App.js
@@ -1,25 +1,62 @@
-import logo from './logo.svg';
-import './App.css';
+// src/App.js
+import "./App.css";
+import contacts from "./contacts.json"
+import React, { Component } from 'react'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  state = {
+    contacts:[]
+  }
+
+  fiveContacts(){
+    let newArr = []
+
+    for( let i = 0; i <= 4; i ++){
+      newArr.push( contacts[i])
+    }
+
+    this.setState({
+      contacts: newArr
+    })
+  }
+
+  componentDidMount(){
+    this.fiveContacts()
+  }
+
+
+  render(){
+    return (
+      <div className="App">
+        <h1>Ironcontacts</h1>
+        <table>
+          <thead>
+          <tr>
+            <th>Picture</th>
+            <th>Name</th>
+            <th>Popularity</th>
+          </tr>
+          </thead>
+
+          <tbody>
+            {this.state.contacts.map((contact, index) => {
+              return(
+              <tr key={contact.id}>
+                  <td>
+                  <div className="picture-row">
+                    <img className="celbPicture" src={contact.pictureUrl} alt="famous person" />
+                  </div>
+                  </td>
+                  <td>{contact.name}</td>
+                  <td>{(contact.popularity).toFixed(2)}</td>
+              </tr>
+              )
+            })}
+          </tbody>
+        </table>
+
+      </div>
+    )
+  }
 }
-
 export default App;
