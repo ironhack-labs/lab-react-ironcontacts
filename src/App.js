52eb57ac1c6455d1c5ff405a97f5879ca9072410
Oscar Sangrador <oscarsangrador@gmail.com>
2022-03-23 10:34:02 +0100

    iteration 1-2

--- src/App.js
+++ src/App.js
@@ -1,59 +1,61 @@
-import data from './contacts.json';
+import React, {Component} from 'react';
+import contacts from './contacts.json';
 import './App.css';
 
-const contacts = [
-  {
-    "name": "Idris Elba",
-    "pictureUrl": "https://image.tmdb.org/t/p/w500/d9NkfCwczP0TjgrjpF94jF67SK8.jpg",
-    "popularity": 11.622713,
-    "id": "11731993-0604-4bee-80d5-67ad845d0a38",
-    "wonOscar": false,
-    "wonEmmy": false
-  },
-  {
-    "name": "Ben Affleck",
-    "pictureUrl": "https://image.tmdb.org/t/p/w500/cPuPt6mYJ83DjvO3hbjNGug6Fbi.jpg",
-    "popularity": 9.157077,
-    "id": "1599707e-5f49-4529-b920-db3831419b04",
-    "wonOscar": true,
-    "wonEmmy": false
-  },
-  {
-    "name": "James McAvoy",
-    "pictureUrl": "https://image.tmdb.org/t/p/w500/oPIfGm3mf4lbmO5pWwMvfTt5BM1.jpg",
-    "popularity": 9.098376,
-    "id": "fef2ac16-36df-486d-8d69-41f1bafa8101",
-    "wonOscar": false,
-    "wonEmmy": false
-  },
-  {
-    "name": "Robin Wright",
-    "pictureUrl": "https://image.tmdb.org/t/p/w500/cke0NNZP4lHRtOethRy2XGSOp3E.jpg",
-    "popularity": 8.802542,
-    "id": "5133d421-dc81-4e3a-81fa-57816da7ce60",
-    "wonOscar": false,
-    "wonEmmy": false
-  },
-  {
-    "name": "Hugh Jackman",
-    "pictureUrl": "https://image.tmdb.org/t/p/w500/oOqun0BhA1rLXOi7Q1WdvXAkmW.jpg",
-    "popularity": 8.58347,
-    "id": "1144413a-4d60-45e4-a51e-ec9ad321d835",
-    "wonOscar": false,
-    "wonEmmy": true
-  },
-]
+class App extends Component {
 
-function App() {
-  return (
-    <div className="App">
+    state = {
+      firstContacts: [...contacts].splice(0,5),
+      otherContacts: [...contacts].splice[5, contacts.length]
+    }
+
+  render(){
+    return (
+      <div className="App">
+    <h3>IronContacts</h3>
+    
+    <table class="table size-tb">
+    <thead>
+    <tr className='me-3'>
+    <th scope="col">Piture</th>
+    <th scope="col">Name</th>
+    <th scope="col">popularity</th>
+    <th scope="col">Won Oscar</th>
+    <th scope="col">Won Emmy</th>
+    {/* <th scope="col"></th> */}
+    </tr>
+    </thead>
       {
-        contacts.map((contact) => {
-          return ()
+        this.state.firstContacts.map(({ pictureUrl, name, popularity, wonOscar, wonEmmy }) => {
+          return (
+            <tbody>
+              <tr>
+                <td>
+                <img 
+                  className='contact-img'
+                  src={pictureUrl}
+                  alt=""
+                />
+                </td>
+                <td>{name}</td>
+                <td>{popularity}</td>
+
+                <td>
+                {wonOscar && "🏆"}
+                </td>
+                <td>
+                {wonEmmy && "🏆"}
+                </td>
+              </tr>
+              </tbody>
+          )
         })
       }
+   </table>
     </div>
-  );
+    );
+  }
 }
+
 
 export default App;
