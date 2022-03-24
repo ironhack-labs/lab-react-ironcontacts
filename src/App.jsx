a50e156307668bdd31b659e2d68c596c496b9eaa
Sergio Villabrille <sergiovillabrille@hotmail.com>
2022-03-24 23:30:21 +0100

    iterations 1,2,3,4,5 done

--- src/App.jsx
+++ src/App.jsx
@@ -8,14 +8,54 @@ import "./App.css";
 class App extends Component {
 
   state = {
-    contacts: contacts.filter((e,index) => index < 5)
+    contacts: contacts.filter((e,index) => index < 5),
+    sort: ''
   }
+
+  
+  addRandomContact = () => {
+
+    const randomContact = contacts[Math.floor(Math.random() * (contacts.length - 5 + 1) + 5)];
+
+    this.setState(prevState => {
+
+      if (!prevState.contacts.includes(randomContact)){
+
+        return {
+
+          contacts: prevState.contacts.concat(randomContact)
+        };
+      };
+    });
+  }
+
+   sortByName = () => {
+      const sortContactsByName = this.state.contacts.sort((a,b) => a.name.localeCompare(b.name))
+
+      this.setState({
+        contacts: sortContactsByName
+      })
+  }
+
+  sortByPopularity = () => {
+
+    let sortContactsbyPopularity = this.state.contacts.sort((a,b) => b.popularity - (a.popularity))
+    this.setState({
+      contacts: sortContactsbyPopularity
+    })
+  }
+
 
   render() {
 
     return (
       <div className="App">
         <h1>IronContacts</h1>
+        <div>
+          <button onClick={() => this.addRandomContact()}>Add random contact</button>
+          <button onClick={() => this.sortByName()}>Sort by name</button>
+          <button onClick={() => this.sortByPopularity()}>Sort by popularity</button>
+        </div>
         <table>
             <thead>
               <tr>
@@ -24,6 +64,7 @@ class App extends Component {
                 <th>Popularity</th>
                 <th>Won Oscar</th>
                 <th>Won Emmy</th>
+                <th>Actions</th>
               </tr>
             </thead>
             <tbody>
@@ -36,6 +77,7 @@ class App extends Component {
                       <th>{contact.popularity.toFixed(2)}</th>
                       <th>{contact.wonOscar && '🏆'}</th>
                       <th>{contact.wonEmmy && '🏆'}</th>
+                      <th><button>Delete</button></th>
                     </tr>
                   )
                 })
