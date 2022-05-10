2dfc92b8a6e4a4e5651f3b2e1441151f15b5dbd9
Jawid <41361157+infjay@users.noreply.github.com>
2022-05-10 22:10:50 +0200

    solved till random button

--- src/App.js
+++ src/App.js
@@ -1,25 +1,36 @@
 // src/App.js
 import "./App.css";
-import contacsArr from "./contacts.json";
+import contactsArr from "./contacts.json";
+import { useState } from 'react'
 
 function App() {
-  console.log();
-  let firstFiveContacts = [
-    contacsArr[0],
-    contacsArr[1],
-    contacsArr[2],
-    contacsArr[3],
-    contacsArr[4],
-  ];
+
+  let firstFiveContacts = contactsArr.slice(0,5);
+
+  let randomContacts = Math.floor(Math.random() * otherContacts.length);
+  
+  const [contacts, setContacts] = useState(firstFiveContacts);
+
+  let otherContacts = contactsArr.filter(eachCeleb => {
+    return !contacts.includes(eachCeleb)
+  }) ;
+
+
+  let addRandomContact = ()=> {
+  
+  }
 
   return (
     <div className="App">
       <h2> List of Contacts </h2>
+      <button >Add Random</button>
       <table>
         <tr>
           <th>Picture</th>
           <th>Name</th>
           <th>Popularity</th>
+          <th>wonOscar</th>
+          <th>wonEmmy</th>
         </tr>
 
         {firstFiveContacts.map((element) => {
@@ -28,6 +39,12 @@ function App() {
               <td><img className="image" src={element.pictureUrl}></img></td>
               <td>{element.name}</td>
               <td>{element.popularity}</td>
+              {element.wonOscar && <td>🏆</td>}
+              {!element.wonOscar && <td></td>}
+              {element.wonEmmy && <td>🏆</td>}
+              {!element.wonEmmy && <td></td>}
+              
+              
             </tr>
           );
         })}
