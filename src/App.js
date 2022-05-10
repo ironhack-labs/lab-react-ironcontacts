58202a8d8b860a3d4f980e54ff6a26cb912cb40f
Louise <louisegabriellecc@gmail.com>
2022-05-10 20:02:44 +0100

    Taking a break at It4

--- src/App.js
+++ src/App.js
@@ -1,23 +1,53 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import contactsArr from "./contacts.json";
+import { useState } from "react";
 
 function App() {
+  let firstFive = contactsArr.slice(0, 5);
+  const [contacts, setContacts] = useState(firstFive);
+
+  const addRandomContact = () => {
+    let filterContacts = contactsArr.filter((people) => {
+      return !contacts.includes(people);
+    });
+
+    let randomContact =
+      filterContacts[Math.floor(Math.random() * filterContacts.length)];
+
+    setContacts(contacts.concat(randomContact));
+  };
+
+  const sortContacts = () => {
+  let sortedContacts = [...contacts]
+  sortedContacts.sort((a, b) => (a.name > b.name) ? 1 : (b.name > a.name) ? -1: 0 );
+  console.log(sortedContacts)
+   }
+
+  console.log(contacts)
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>IronContacts</h1>
+      <button onClick={addRandomContact}>Add A Random Contact</button>
+      <button onClick={sortContacts}>Sort by Name</button>
+
+      <h2 className="contact-row">
+        <span>Picture</span> <span>Name</span> <span>Popularity</span>{" "}
+        <span>Won Oscar</span> <span>Won Emmy</span>
+      </h2>
+      <div className="table">
+        {contacts.map((contact) => {
+          return (
+            <div className="contact-row" key={contact.id}>
+              <img src={contact.pictureUrl} alt={contact.name}></img>
+              <span> {contact.name}</span>
+              <span> {contact.popularity}</span>
+              <span> {contact.wonOscar && "🏆"}</span>
+              <span>{contact.wonEmmy && "🏆"}</span>
+            </div>
+          );
+        })}
+      </div>
     </div>
   );
 }
