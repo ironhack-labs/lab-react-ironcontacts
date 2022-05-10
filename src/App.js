3ccf45e866d2d55ef5dc02d79502a7a8c423e140
jidekickpush <jeandenisdrane@gmail.com>
2022-05-10 20:27:43 +0200

    Iteration 1 done

--- src/App.js
+++ src/App.js
@@ -1,23 +1,29 @@
-import logo from './logo.svg';
 import './App.css';
+import contactsArr from "./contacts.json";
 
 function App() {
+  const fiveContacts = contactsArr.slice(0, 5);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <table>
+        <thead>
+          <tr>
+            <th>Picture</th>
+            <th>Name</th>
+            <th>Popularity</th>
+          </tr>
+        </thead>
+        <tbody>
+          {fiveContacts.map((contact)=>(
+            <tr key={contact.id}>
+              <td>{<img src={contact.pictureUrl} alt=""></img>}</td>
+              <td>{contact.name}</td>
+              <td>{contact.popularity.toFixed(2)}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
     </div>
   );
 }
