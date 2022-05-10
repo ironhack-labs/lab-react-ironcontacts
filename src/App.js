f03973eac7c4e1c302fd808f48c54e0ea70972e9
Ivo Silva <silvaivosoares@gmail.com>
2022-05-10 20:19:39 +0100

    Completed iterations 1-2-3

--- src/App.js
+++ src/App.js
@@ -1,23 +1,54 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import contactsArr from "./contacts.json";
+import { useState } from "react";
+
+// Storing the first 5 contacts from the contacts.json
+const contacts = contactsArr.slice(0, 5);
+const remainingContacts = contactsArr.slice(6, contactsArr.length);
+
+
 
 function App() {
+  const [contact, setContact] = useState(contacts);
+
+//Iteration 3, add new Random Contacts
+function getRandomContact() {
+  const newContact = remainingContacts.splice(Math.floor(Math.random()*(remainingContacts.length - 1)), 1);
+  const newList = contact.concat(newContact);
+  setContact(newList);
+}
+
+//Iteration 4, Sort Contacts by Name and Popularity
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <button onClick={getRandomContact}>Add Random Contact</button>
+
+      <table>
+        <thead>
+          <tr>
+            <th>Picture</th>
+            <th>Name</th>
+            <th>Popularity</th>
+            <th>Won an Oscar</th>
+            <th>Won an Emmy</th>
+          </tr>
+        </thead>
+        <tbody>
+          {contact.map(contact => {
+            return(
+              <tr key={contact.id}>
+                <td><img src={contact.pictureUrl} alt={contact.name}></img></td>
+                <td><span>{contact.name}</span></td>
+                <td><span>{Math.round(contact.popularity * 100) / 100}</span></td>
+                <td><span>{contact.wonOscar ? <img id="trophyIcon" src="../trophy.png" alt="Trophy Icon"></img> : ""}</span></td>
+                <td><span>{contact.name}</span></td>
+              </tr>
+            )
+          })}
+        </tbody>
+      </table>
     </div>
   );
 }
