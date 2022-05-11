490ec742089c2b1de0efcfc6cc416d79bd64ffa8
fevi <foevaroma@gmail.com>
2022-05-11 08:26:36 +0200

    Iterations 1-3 and 6 completed, 4-5 don't work

--- src/App.js
+++ src/App.js
@@ -1,24 +1,100 @@
-import logo from './logo.svg';
+
+import { useState } from 'react';
+import contactsArr from "./contacts.json";
+
 import './App.css';
 
+
+
 function App() {
+  let fiveContacts = contactsArr.slice(0,5);
+
+  const [contacts, setContacts] = useState(fiveContacts);
+
+  const addRandomContact = () => {
+  if(!contactsArr.length){ return false;}
+
+    const randomContact = Math.floor(Math.random() * (contactsArr.length));
+    const newRandomContact = [...contacts,contactsArr[randomContact]]
+   contactsArr.splice(randomContact,1);
+    setContacts(newRandomContact);
+  }
+
+
+  const sortByName = () => {
+    let selectedContacts = [...contacts];
+
+    selectedContacts.sort((a,b) =>(a.name > b.name) ? 1 : ((b.name > a.name) ? -1 : 0))
+
+    setContacts(selectedContacts);
+  }
+
+
+
+  const sortByPopularity = () => {
+
+    const selectedContacts = [...contacts.sort((a, b) => { 
+      if(a.popularity > b.popularity) {return -1;} 
+      if(a.popularity < b.popularity) {return 1;} 
+      return 0}),];
+    setContacts(selectedContacts)
+
+
+  }
+
+
+
+  const deleteContact = contactId =>{
+    const newContacts = contacts.filter( element => element.id !== contactId);
+    setContacts(newContacts);
+  }
+
   return (
+
+    <main className="container">
+      <div className='wrapper'>
+    <section className="buttons">
+        <button onClick={() => addRandomContact()} className="btn">
+        { contactsArr.length ? 'Add Random Contact' : 'No contacts'}
+        </button>   
+        <button onClick={sortByName}>Sort By Name</button>
+      <button  onClick={sortByPopularity}>Sort By Popularity</button>
+
+
+
+</section>
+
+
+
+
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+ <table className="contact-table">
+    <thead>
+      <tr>
+        <th>Picture</th>
+        <th>Name</th>
+        <th>Popularity</th>
+        <th>Won an Oscar</th>
+        <th>Won an Emmy</th>
+        <th>Delete</th>
+      </tr>
+      </thead>
+      <tbody>
+      {fiveContacts.map(contact=>{
+        return <tr key={contact.id}>
+          <td><img src={contact.pictureUrl} className="contact-picture" alt={contact.name}></img></td>
+          <td>{ contact.name }</td>
+          <td>{ contact.popularity.toFixed(2) }</td>
+          <td>{ contact.wonOscar && "🏆" }</td>
+          <td>{ contact.wonEmmy && "🏆" }</td>
+          <td><button onClick={deleteContact}  key={contact.id}>Delete</button></td>
+        </tr>
+      })}
+      </tbody>
+    </table>
     </div>
+    </div>
+    </main>
   );
 }
 
