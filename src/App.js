6fb429a04e12112a6fc37d9fea43359cd20f3f63
Josipa314 <101945658+josipa314@users.noreply.github.com>
2022-05-11 12:10:56 +0200

    only 2 iter

--- src/App.js
+++ src/App.js
@@ -1,25 +1,47 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import contactsArr from "./contacts.json";
+import { useState } from "react";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
 
+  let fiveContacts = contactsArr.slice(0, 5);
+  console.log(fiveContacts);
+
+  const [contacts, setContacts] = useState(fiveContacts);
+
+
+  return <div className="App">
+    <table>
+      <thead>
+        <tr>
+          <th>PICTURE</th>
+          <th>NAME</th>
+          <th>POPULARITY</th>
+          <th>OSCAR</th>
+          <th>EMMY</th>
+        </tr>
+      </thead>
+      <tbody>
+        
+          {fiveContacts.map((element) => {
+            return (
+            <tr>
+                <td> <img src={element.pictureUrl} alt="picture" width="75" height="85"/> </td>
+                <td> {element.name} </td>
+                <td> {element.popularity} </td>
+                <td> {element.wonOscar && <i class="fas fa-trophy"></i>} 
+                     </td>
+                <td> {element.wonEmmy && <i class="fas fa-trophy"></i> } 
+                       </td>
+                 
+ 
+            </tr>
+            );
+          })};
+        
+      </tbody>
+    </table>
+  </div>
+}
 export default App;
+
