c61e8150745468617de721bf4853bf5c3ab63afb
AnaPinho1990 <anagonpinho@gmail.com>
2022-05-24 17:55:13 +0100

    done without bonus

--- src/App.js
+++ src/App.js
@@ -1,25 +1,76 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import contact from "./contacts.json";
+import React, { useState } from "react";
 
 function App() {
+  const [contactList, setContactList] = useState(contact.slice(0, 5)); // usar o usestate para controlar a dinamica - hook
+
+  const newContacts = contact.slice(6);
+
+  const randomContact = () => {
+    const newContact = contact[Math.floor(Math.random() * newContacts.length)];
+    setContactList([newContact, ...contactList]);
+  };
+
+  const sortButton = () => {
+    setContactList([
+      ...contactList.sort((x, y) => x.name.localeCompare(y.name)),
+    ]);
+  };
+  const sortPop = () => {
+    setContactList([
+      ...contactList.sort((x, y) => y.popularity - x.popularity),
+    ]);
+  };
+
+  const deleteContact = (elementId) => {
+    const filteredContact = contactList.filter((element) => {
+      return element.id !== elementId;
+    });
+
+    setContactList(filteredContact);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <h1>IronContacts</h1>
+      <button onClick={() => randomContact()}>Add Random Contact</button>
+      <button onClick={() => sortButton()}>Sort by name</button>
+      <button onClick={() => sortPop()}>Sort by popularity</button>
+      <table>
+        <thead>
+          <th>Picture</th>
+          <th>Name</th>
+          <th>Popularity</th>
+          <th>Won an Oscar</th>
+          <th>Won an Emmy</th>
+        </thead>
+        <tbody>
+          {contactList.map((element) => {
+            return (
+              <tr>
+                <td>
+                  <img src={element.pictureUrl} alt="" width={100}></img>
+                </td>
+                <td>{element.name}</td>
+                <td>{element.popularity.toFixed(2)}</td>
+                <td>{element.wonOscar ? <p>🏆</p> : <p>X</p>}</td>
+                <td>{element.wonEmmy ? <p>🌟</p> : <p>X</p>}</td>
+                <td>
+                  {" "}
+                  <button
+                    onClick={() => deleteContact(element.id)}
+                    className="btn-delete"
+                  >
+                    Delete
+                  </button>
+                </td>
+              </tr>
+            );
+          })}
+        </tbody>
+      </table>
+    </>
   );
 }
-
 export default App;
