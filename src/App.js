2b327daea05a339a28ddf017dc082aac74fb814c
Duarte Alves <duartemalves12@gmail.com>
2022-05-24 17:36:06 +0100

    done

--- src/App.js
+++ src/App.js
@@ -1,23 +1,90 @@
-import logo from './logo.svg';
-import './App.css';
-
+import "./App.css";
+import { useState } from "react";
+import ContactData from "./contacts.json";
 function App() {
+  const [contacts, setContacts] = useState(ContactData.slice(0, 5));
+
+  const randomContacts = () => {
+    let random = ContactData[Math.floor(Math.random() * ContactData.length)];
+    console.log(random);
+    setContacts([random, ...contacts]);
+  };
+  const sortPopularity = () => {
+    let sort = [...contacts].sort((a, b) => b.popularity - a.popularity);
+    setContacts(sort);
+  };
+  const sortNames = () => {
+    let sort = [...contacts].sort((a, b) => a.name.localeCompare(b.name));
+    setContacts(sort);
+  };
+
+  const deleteContact = (contactId) => {
+    const filteredActor = contacts.filter((contact) => {
+      return contact.id !== contactId;
+    });
+    setContacts(filteredActor);
+  };
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>IronContacts</h1>
+      <button onClick={() => sortNames()}>Sort by name</button>
+      <button onClick={() => sortPopularity()}>Sort by popularity</button>
+      <button onClick={() => randomContacts()}>Add a random contact</button>
+      <table>
+        <tr>
+          <th>Picture</th>
+          <th>Name</th>
+          <th>Popularity</th>
+          <th>Won Oscar</th>
+          <th>Won Emmy</th>
+          <th>Actions</th>
+        </tr>
+
+        <tbody>
+          {contacts.map((el) => {
+            return (
+              <tr>
+                <td>
+                  <img src={el.pictureUrl} alt="" />
+                </td>
+                <td>
+                  <p>{el.name}</p>
+                </td>
+                <td>
+                  <p>{el.popularity}</p>
+                </td>
+                <td>
+                  <p>
+                    {el.wonOscar ? (
+                      <img
+                        src="https://github.githubassets.com/images/icons/emoji/unicode/1f3c6.png"
+                        alt="trophy image"
+                      />
+                    ) : (
+                      true
+                    )}
+                  </p>
+                </td>
+                <td>
+                  <p>
+                    {el.wonEmmy ? (
+                      <img
+                        src="https://github.githubassets.com/images/icons/emoji/unicode/1f3c6.png"
+                        alt="trophy image"
+                      />
+                    ) : (
+                      true
+                    )}
+                  </p>
+                </td>
+                <td>
+                  <button onClick={() => deleteContact(el.id)}>Delete</button>
+                </td>
+              </tr>
+            );
+          })}
+        </tbody>
+      </table>
     </div>
   );
 }
