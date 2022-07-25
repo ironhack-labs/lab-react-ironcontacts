a1b2820da926907c92573546356c9757c9e1f736
hleonn <edhleon2001@hotmail.com>
2022-07-24 21:49:54 -0500

    :)

--- src/App.js
+++ src/App.js
@@ -1,23 +1,55 @@
-import logo from './logo.svg';
 import './App.css';
+import React, {useState} from 'react'
+import contacts from './contacts.json'
 
+const firstFive = contacts.slice(0,5) //returns selected array elements as a new array
 function App() {
+  const [contacts, setContacts] = useState(firstFive)
+  const addContact=()=>{
+    let randomIndex = Math.floor(
+      Math.random()*contacts.length)
+      let randomContact=contacts [randomIndex]
+      setContacts([randomContact, ...contacs])
+    }
+    
+  
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>IRON-CONTACS</h1>
+        <button>onClick = {addContact} Add Random Contact </button> {/* iteration 3 & line 8*/}
+        <button>onClick = {()=>setContacts()} > Sort by popularity </button>{/*iteration 4*/}
+        <button>onClick = {()=>setContacts()} > Sort by name </button>{/*iteration 4*/}
+      <br></br>{/*line break*/}
+        <table>
+          <thead>
+            <tr>
+              <th>Picture</th>
+              <th>Name</th>
+              <th>Popularity</th>
+              <th>Won<br></br>Oscar </th>
+              <th>Won<br></br>Emmy </th>
+              <th>Actions</th>
+            </tr>
+          </thead>
+          
+          <tbody>
+            {contacts.map(element =>{
+              return (
+                <tr>
+                  <td><img className="celi" src={element.pictureUrl} alt=""/></td> {/*Check*/}
+                    <td>{element.name}</td>
+                    <td>{element.popularity}</td>
+                    <td>{element.wonOscar ? '🏆' : null}</td> {/*iteration 2*/}
+                    <td>{element.wonEmmy ? '🏆' : null}</td> {/*iteration 2*/}
+                    <button onclick={()=>setContacts(contacts._id)}
+                    className="btn-delete">DELETE</button> {/*iteration 5*/}
+
+                </tr>
+              )
+            })}
+          </tbody>
+        </table>
+      
     </div>
   );
 }
