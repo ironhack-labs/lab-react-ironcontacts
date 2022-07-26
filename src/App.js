de023f5879f4d2ec885ceaf5b7018b9d9ecd275a
Lara Rockenstein <lara.rockenstein@live.de>
2022-07-26 19:07:22 +0200

    iteration 4

--- src/App.js
+++ src/App.js
@@ -14,17 +14,43 @@ function App() {
 
   const addRandomContact = () => {
     const randomContact =  allContacts[Math.floor(Math.random()*allContacts.length)];
-    console.log(randomContact)
+    // console.log(randomContact)
     setContacts((prevList) => {
       const prevListCopy = [...prevList];
       prevListCopy.push(randomContact);
-      console.log(prevListCopy);
+      // console.log(prevListCopy);
       return prevListCopy;
     })
     // const remainingContacts = allContacts.slice()
   }
+
+  const sortByName = () => {
+    setContacts((prevList) => {
+      const prevListCopy = [...prevList];
+      prevListCopy.sort(function(a,b) {
+        return a.name.localeCompare(b.name)
+      })
+      return prevListCopy;
+    })
+  }
+
+  const sortByPopularity = () => {
+    setContacts((prevList) => {
+      const prevListCopy = [...prevList];
+      prevListCopy.sort(function(a,b) {
+        console.log(a.popularity)
+        console.log(b.popularity)
+        console.log(a.popularity - b.popularity)
+        return b.popularity - a.popularity;
+      })
+      return prevListCopy;
+    })
+  }
   
   return <div className="App">
+
+    <button onClick={sortByName}>Sort by name</button>
+    <button onClick={sortByPopularity}>Sort by popularity</button>
 
     <table>
       <tr>
