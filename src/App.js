c0c35fb5e88d05ef136917c542b80f59f118ca8c
“Alexandre <coma.alexandre@gmail.com>
2022-07-26 20:15:30 +0200

    lab not finished, stuck on iteration 4

--- src/App.js
+++ src/App.js
@@ -1,23 +1,60 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
+import contacts from "./contacts.json";
 
 function App() {
+
+  const [contactsArr, setContact] = useState(contacts.slice(0, 5));
+  const addRandomContact = () => {
+    const randomContact = contacts[Math.floor(Math.random() * contacts.length)];
+    setContact((prevContacts) => {
+      const newContacts = [...prevContacts, randomContact]
+      return newContacts
+    })
+  }
+
+  const [contactsAlphabetical, setAlphabetically]= useState(contactsArr)
+  setAlphabetically ( (prevValue) => {
+    const copy = [...prevValue]
+    copy.sort((a, b) => a.name.localeCompare(b.name))
+    return copy;
+});
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>Iron Contacts</h1>
+        <button onClick={addRandomContact}>Add random contact</button>
+        <button onClick={setAlphabetically}>Sort by Name</button>
+        <button>Sort by Popularity</button>
+        <table>
+          <tr>
+            <th>Picture</th>
+            <th>Name</th>
+            <th>Popularity</th>
+            <th>Won Oscar</th>
+            <th>Won Emmy</th>
+          </tr>
+          {contactsArr.map((contact) => {
+            return (
+              <div>
+                <tr key={contact.id}>
+                  <td>
+                    <img src={contact.pictureUrl} alt="" />
+                  </td>
+                  <td>{contact.name}</td>
+                  <td>{contact.popularity}</td>
+                  {contact.wonOscar
+                    ? <td>🏆</td>
+                    : <td></td>}
+                </tr>
+              </div>
+            )
+          })}
+        </table>
       </header>
+      <div className="Main">
+      </div>
     </div>
   );
 }
