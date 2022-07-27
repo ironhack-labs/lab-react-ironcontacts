13bfbd674a8f557f5e4580090e299c46a0620052
Darren Iyoyo <darreniyoyo@gmail.com>
2022-07-27 09:09:12 +0200

    done

--- src/App.js
+++ src/App.js
@@ -1,25 +1,58 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import contactsArray from "./contacts.json"
+import { useState } from "react";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+    const [contacts, setContacts] = useState(contactsArray.slice(0, 5));
+
+    const deleteContact = (contactId) => {
+        setContacts( (prevContacts) => {
+            const newList = prevContacts.filter( (element) => {
+                return element.id !== contactId;
+            });
+            return newList;
+        });
+    }
+
+  //   const addContact = (contactId) => {
+  //     const randomContact = contactObj[Math.floor(Math.random()*items.length)];
+  //     setContacts( (prevContacts) => {
+  //         const newList = prevContacts.filter( (element) => {
+  //             return element.id !== contactId;
+  //         });
+  //         return newList;
+  //     });
+  // }
+
+
+    return (
+        <div className="Main">
+
+            {contacts.map((contactObj) => {
+                return (
+                    <div key={contactObj.id} className="contact">
+                        <h2>Name {contactObj.name}</h2>
+                        <h3>Popularity {contactObj.popularity}</h3>
+                        
+                        <>
+                        { contactObj.pictureUrl 
+                            ? <img src={contactObj.pictureUrl} alt={contactObj.name} />
+                            : <p>Sorry, no image</p>
+                        }
+                        </>
+                        <button onClick={() => { deleteContact(contactObj.id) }}>Delete</button>
+                        {/* <button onClick={() => { addContact(contactObj.id) }}>Add Random Contact</button> */}
+              
+                        <h3>Won Oscar: {contactObj.wonOscar && "🏆"}</h3>
+                        <h3>Won Emmy: {contactObj.wonEmmy && "🏆" }</h3>
+                      
+                    </div>
+                )
+            })}
+
+        </div>
+    );
 }
 
 export default App;
