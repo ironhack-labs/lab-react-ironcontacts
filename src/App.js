a9ca061ecd0731e7e5695ef5deea87b0a0b76948
jackletran <jack.letran@moebel.de>
2022-07-27 09:06:41 +0200

    iteration 1, 2, 5 - tried 3 - didn't finish 4

--- src/App.js
+++ src/App.js
@@ -1,25 +1,80 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import contactsArray from "./contacts.json";
+import { useState } from "react";
+
+const contacts = [
+  contactsArray[0],
+  contactsArray[1],
+  contactsArray[2],
+  contactsArray[3],
+  contactsArray[4],
+];
 
 function App() {
+  const [actors, setActors] = useState(contacts);
+
+  const deleteContact = (contactId) => {
+    setActors((prevActors) => {
+      const newList = prevActors.filter((element) => {
+        return element.id !== contactId;
+      });
+      return newList;
+    });
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <button
+        onClick={() => {
+          setActors((prevActors) => {
+            let randomIndex = Math.floor(Math.random() * contactsArray.length); // random index
+            let randomContact = contactsArray[randomIndex]; // pick a random from whole list
+            prevActors.push(randomContact); // push to newList
+            prevActors = actors;
+            console.log(actors);
+            console.log(prevActors);
+            return actors;
+          });
+        }}
+      >
+        Add random contact
+      </button>{" "}
+      <table>
+        <tbody>
+          <tr>
+            <th>Picture</th>
+            <th>Name</th>
+            <th>Popularity</th>
+            <th>Won an Oscar</th>
+            <th>Won an Emmy</th>
+          </tr>
+          {actors.map((actor) => {
+            let wonOscar = actor.wonOscar ? <td>Oscar</td> : <td></td>;
+            let wonEmmy = actor.wonEmmy ? <td>Emmy</td> : <td></td>;
+            return (
+              <tr key={actor.name}>
+                <td>
+                  <img src={actor.pictureUrl} alt={actor.name} />
+                </td>
+                <td>{actor.name}</td>
+                <td>{actor.popularity}</td>
+                {wonOscar}
+                {wonEmmy}
+                <td>
+                  <button
+                    onClick={() => {
+                      deleteContact(actor.id);
+                    }}
+                  >
+                    Delete
+                  </button>
+                </td>
+              </tr>
+            );
+          })}
+        </tbody>
+      </table>
     </div>
   );
 }
-
 export default App;
