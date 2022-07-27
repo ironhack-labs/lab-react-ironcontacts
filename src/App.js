c7ccee2b8b060ae89e196de4d16fdf0fdf2b60f4
Nic Solitom <nicsolitom@gmail.com>
2022-07-27 08:40:25 +0200

    Nic Solitom - incomplete, only started iteration 3

--- src/App.js
+++ src/App.js
@@ -1,23 +1,76 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import "./App.css";
+import contactsArr from "./contacts.json";
 
 function App() {
+  const fiveContacts = [...contactsArr].slice(0, 5);
+  const [contacts, setContacts] = useState(fiveContacts);
+
+  const listItems = contacts.map((contact) => {
+    const roundedPopularity = Math.round(contact.popularity * 100) / 100;
+    return (
+      <tr>
+        <td>
+          <img
+            className="contact-img"
+            src={contact.pictureUrl}
+            alt="Contact profile"
+          />
+        </td>
+        <td>{contact.name}</td>
+        <td>{roundedPopularity}</td>
+        {contact.wonOscar ? <td>🏆</td> : <td></td>}
+        {contact.wonEmmy ? <td>🏆</td> : <td></td>}
+      </tr>
+    );
+  });
+
+
+/// NOT READY!
+  function addRandomContact() {
+    let randomNum = Math.floor(Math.random() * contactsArr.length);   
+    setContacts((prevState) => {
+      let returnArr = [];
+      return prevState.push(contactsArr[randomNum]);
+    });
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="header">
+        <h1>IronContacts</h1>
+        <button onClick={() => addRandomContact()}>Add Random Contact</button>
+      </div>
+
+      <table className="contacts-table">
+        <tbody>
+          <tr>
+            <td>
+              <strong>Picture</strong>
+            </td>
+            <td>
+              <strong>Name</strong>
+            </td>
+            <td>
+              <strong>Popularity</strong>
+            </td>
+            <td>
+              <strong>
+                Won
+                <br />
+                Oscar
+              </strong>
+            </td>
+            <td>
+              <strong>
+                Won
+                <br />
+                Emmy
+              </strong>
+            </td>
+          </tr>
+          {listItems}
+        </tbody>
+      </table>
     </div>
   );
 }
