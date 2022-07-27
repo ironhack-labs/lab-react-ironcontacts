5b330beed3252424cc9c3197711c28fab2b8c5b7
Stephanie-Tovar <stephanie.anne.tovar@gmail.com>
2022-07-27 08:17:25 +0200

    avoid repetition in iteration 3

--- src/App.js
+++ src/App.js
@@ -10,10 +10,16 @@ function App() {
   const remainingContacts = contactsArray.slice(5);
 
   const addRandomContact = () => {
-    const randomContact = remainingContacts[Math.floor(Math.random() * remainingContacts.length)]
-    const copy = [...contacts]
-    copy.push(randomContact)
-    setContacts(copy)
+    let randomContact = remainingContacts[Math.floor(Math.random() * remainingContacts.length)]
+    let copy = [...contacts]
+    let index = copy.findIndex((element) => element.id === randomContact.id)
+
+    if (index === -1) {
+      copy.push(randomContact)
+      setContacts(copy)
+    } else {
+      setContacts(contacts)
+    }
   }
 
   const sortByPopularity = () => {
@@ -37,10 +43,12 @@ function App() {
     })
   }
 
+  console.log(contacts)
+
   return (
     <div className='App'>
       <h1>IronContacts</h1>
-      <button onClick={addRandomContact}>Add random contact</button>
+      <button onClick={() => addRandomContact()}>Add random contact</button>
       <button onClick={sortByPopularity}>Sort by popularity</button>
       <button onClick={sortByName}>Sort by name</button>
       <table className="table">
