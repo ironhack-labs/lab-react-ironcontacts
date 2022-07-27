ce6b4e49783e9249b5242bb5242ebaf956676022
Tobias Conrad <max.tobias.conrad@gmail.com>
2022-07-27 08:51:22 +0200

    lab done

--- src/App.js
+++ src/App.js
@@ -1,23 +1,91 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import "./App.css";
+import contactsArr from "./contacts.json";
+
+
 
 function App() {
+  
+  const [contacts, setContacts] = useState(contactsArr.slice(0, 5))
+
+
+  function addRandom() {
+    let clonedArray
+    setContacts((prevContacts)=>{
+      let contact = contactsArr[Math.floor(Math.random()*contactsArr.length)] 
+        //need to check if contact.id === element.id of prevContacts exists -> if yes search another one, if no add to clonedArray (couldn´t make that work sadly)
+        clonedArray = [...prevContacts, contact]
+        return clonedArray 
+      })
+    console.log(clonedArray)
+  }
+
+  function sortByName() {
+    setContacts((prevContacts) => {
+      const copy = [...prevContacts]
+      copy.sort( (a,b) => {
+        let fa = a.name
+        let fb = b.name
+
+    if (fa < fb) {
+        return -1
+    }
+    if (fa > fb) {
+        return 1
+    }
+    return 0
+       } )
+       return copy
+    })
+  }
+
+  function sortByPopularity() {
+    setContacts((prevContacts) => {
+      const copy = [...prevContacts]
+      copy.sort((a, b) => {
+        return b.popularity - a.popularity
+      })
+      return copy
+    })
+  }
+
+  function deleteContact(id) {
+    setContacts((prevContacts) => {
+      let newArr = prevContacts.filter((contactObj) => {
+        return contactObj.id !== id 
+      })
+      return newArr
+    })
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App" >
+      <button onClick={addRandom}>Add Random Contact</button>
+      <button onClick={sortByName}>sort by name</button>
+      <button onClick={sortByPopularity}>sort by popularity</button>
+      {contacts.map((contactObj) => {
+        return (
+          <table key={contactObj.id}>
+            <tr>
+              <th>Picture</th>
+              <th>Name</th>
+              <th>Popularity</th>
+              <th>Won Emmy</th>
+              <th>Won Oscar</th>
+              <th>Actions</th>
+            </tr>
+            <td>
+              <img src={contactObj.pictureUrl} alt={contactObj.name} />
+            </td>
+            <td>{contactObj.name}</td>
+            <td>{contactObj.popularity}</td>
+            {contactObj.wonEmmy && <td>🏆</td> }
+            {contactObj.wonOscar && <td>🏆</td>}
+            <td><button onClick={() => deleteContact(contactObj.id)}>Delete</button></td>
+          </table>
+          
+        )
+      })}
     </div>
   );
 }
