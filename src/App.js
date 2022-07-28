3d0b7d6180cf2c8f257d1778f8ff47284011c20d
Natalia Buzila <nataliabuzila@gmail.com>
2022-07-28 19:15:53 +0300

    Iteration 1, 2, 5

--- src/App.js
+++ src/App.js
@@ -1,25 +1,49 @@
 import logo from './logo.svg';
 import './App.css';
+import contactsData from './contacts.json'
+import { useState } from 'react';
+
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  const [contacts, setContacts] = useState(contactsData.slice(0,5));
+
+  const deleteContact = contactID => {
+    const filteredContacts = contacts.filter(contact => {
+      return contact.id !== contactID
+    });
+    setContacts(filteredContacts);
+  }
+
+      return (
+        <div className="App">
+          <h2>IronContacts</h2>
+          <button>Add Random Contact</button>
+          <table>
+            <tr>
+              <th>Picture</th>
+              <th>Name</th>
+              <th>Popularity</th>
+              <th>Won an Oscar</th>
+              <th>Won an Emmy</th>
+              <th>Delete button</th>
+            </tr>
+            { contacts.map(contact => {
+              return (
+                <tr>
+                  <td><img src={contact.pictureUrl} alt={contact.name}/></td>
+                  <td>{contact.name}</td>
+                  <td>{contact.popularity}</td>
+                  {contact.wonOscar && <td>🏆</td>}
+                  {contact.wonEmmy && <td>🏆</td>}
+                  <td><button onClick={() => deleteContact(contact.id)}>Delete </button></td>
+                </tr>
+              )
+            })
+
+            }
+          </table>
+        </div>
+      );
 }
 
 export default App;
