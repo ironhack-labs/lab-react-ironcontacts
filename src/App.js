0c3efe84359b2fb4de9a0e5ff16ce44460a00a69
Evgeniia Riaguzova <aglaya_2004@mail.ru>
2022-07-28 12:53:03 +0200

    commented out iterations with the problems, but still would like to get the review about them

--- src/App.js
+++ src/App.js
@@ -1,25 +1,57 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import contacts from "./contacts.json";
+import React, { useState } from 'react';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+  const [contactsArr, setContactsArr] = useState(contacts);
+  const firstFiveContacts = contacts.slice(0, 5)
+  const restContacts = contacts.slice(5)
+  let additionalContact = restContacts[Math.floor(Math.random()*restContacts.length)];
+  /*const clickToAdd = (additionalContact.index) => {
+    firstFiveContacts.push(additionalContact.index);
+    setContactsArr(firstFiveContacts)};*/
 
+  return ( <div className="App">
+  <h2>IronContacts</h2>
+{/*
+ <button onClick={() => clickToAdd(additionalContact.index)} className="addRandomContact">
+  Add Random Contact 
+      </button> 
+     
+  <button onClick={() => 
+      firstFiveContacts.sort()
+       setContactsArr(firstFiveContacts)} 
+       className="btn-sortByName">
+       Sort by popylarity </button>   
+       
+       */} 
+
+   <table>
+        <tr>
+            <th className="pic">Picture</th>
+             <th className="name">Name</th>
+             <th className="popularity">Popularity</th>  
+             <th className="wonOscar">Won an Oscar</th>
+             <th className="wonEmmy">Won an Emmy</th>
+          </tr>
+          </table>
+          
+  {firstFiveContacts.map((contact, index) => (
+    <table>
+          <tr  key={ index}>
+             <td className="pic"> <img className="profilePic" src={contact.pictureUrl}/></td>
+             <td className="name">{contact.name}</td>
+             <td className="popularity">{contact.popularity}</td>  
+             {contact.wonOscar ? <td className="wonOscar">🏆</td>  : <td className="wonOscar"> not yet</td> }
+             {contact.wonEmmy ? <td className="wonEmmy">🏆</td>  : <td className="wonEmmy"> not yet</td> }
+
+             
+             
+          </tr>
+        </table>
+        
+      ))}
+  </div>
+  )
+}
 export default App;
