9b6424c8183c73ddbb03f7a23af9740fd27ec1e2
zarahsara <sarahabbasi25@aol.com>
2022-07-28 16:43:47 +0100

    done

--- src/App.js
+++ src/App.js
@@ -1,8 +1,35 @@
+import { useState } from "react";
 import logo from './logo.svg';
 import './App.css';
+import contacts from "./contacts.json";
+
 
 function App() {
-  return (
+  
+  
+  
+    contacts.map((contacts, index) => (
+    <li key={index}>{element}</li>
+  ))
+  <td>
+         <td>celebImg' src={eachContact.pictureUrl} alt='celebrity photo' />
+         </td>
+          <td>{eachContact.name}</td>
+          <td>{eachContact.popularity.toFixed(2)}</td>
+          <td>
+            
+          </td>
+        </tr>
+    }
+
+        contacts.sort(function(a, b){
+          if(a.name < b.name) { return -1; }
+          if(a.popularity > b.popularity) { return 1; }
+          return 0;
+      })
+  
+   
+   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
@@ -15,11 +42,15 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          IronContacts
         </a>
       </header>
     </div>
   );
 }
+
+
+
+
 
 export default App;
