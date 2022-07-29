9bf9b0d580f7c3ea2dbf52a08891d2d524be5a08
Vale Quiroga <valentinqramos@outlook.com>
2022-07-29 13:27:03 -0500

    Iteration 2

--- src/App.js
+++ src/App.js
@@ -3,9 +3,10 @@ import {useState} from 'react'
 import './App.css';
 
 function App() {
-  const contactsFiltered = contactsData.slice(0,5)
+  const contactsFiltered = contactsData.slice(0,25)
   const [contacts , setContacts] = useState(contactsFiltered)
-  
+  console.log(contacts)
+
   return (
     <div className="App">
       <h1>IronContacts</h1>
@@ -15,14 +16,18 @@ function App() {
             <th>Picture</th>
             <th>Name</th>
             <th>Popularity</th>
+            <th>Won Oscar</th>
+            <th>Won Emmy</th>
           </tr>
         </thead>
         <tbody>
-          {contacts.map((contact ,  index) => (
-            <tr key={index} >
+          {contacts.map((contact) => (
+            <tr key={contact.id} >
               <td> <img src={contact.pictureUrl} alt="contact" width="100" /> </td>
               <td> {contact.name} </td>
               <td> {contact.popularity.toFixed(2)} </td>
+              <td> {contact.wonOscar ? '🏆' : null} </td>
+              <td> {contact.wonEmmy ? '🏆' : null} </td>
             </tr>
           ))}
         </tbody>
