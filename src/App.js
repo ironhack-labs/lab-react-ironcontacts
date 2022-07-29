f6dc9796b9126f56eb1b9a67ed76b36ad6623cf4
Natalia Buzila <nataliabuzila@gmail.com>
2022-07-29 21:11:25 +0300

    Iterations 3 and 4

--- src/App.js
+++ src/App.js
@@ -5,19 +5,79 @@ import { useState } from 'react';
 
 
 function App() {
+
   const [contacts, setContacts] = useState(contactsData.slice(0,5));
 
+  //V1
   const deleteContact = contactID => {
-    const filteredContacts = contacts.filter(contact => {
-      return contact.id !== contactID
-    });
+    const filteredContacts = contacts.filter(contact =>  contact.id !== contactID);
     setContacts(filteredContacts);
+  }
+
+  //V2
+  // Q: dont understand line 11: what is the difference between "const deleteContact = contactID =>{}" and "deleteContact(contactID) {}"?
+  // const deleteContact = contactID => {
+  //   const filteredContacts = contacts.filter(contact => {
+  //     return contact.id !== contactID
+  //   });
+  //   setContacts(filteredContacts);
+  // }
+
+  //V3
+  // 2Q:
+  //  1. how does the return work in the function for filter() method, doesnt it stop at the first check?
+  //  2. how does setContacts function know what to take for prevContacts variable. We didnt give it any value aniwhere.
+  // function deleteContact2 (contactID) {
+  //   setContacts((prevContacts) =>{
+  //     let filteredContacts2 = prevContacts.filter((contact) => {
+  //       return contactID !== contact.id
+  //     })
+  //     return filteredContacts2;
+  //   })
+  // }
+
+  function addRandom() {
+    setContacts((prevContacts) =>{
+      let copyArr = [...prevContacts]
+      let randomContact = contactsData[Math.floor(Math.random()*contactsData.length)]
+      let duplicateContact = false;
+      copyArr.forEach(element => {
+        if(element.id === randomContact.id) duplicateContact = true;
+     });
+     if (!duplicateContact) copyArr.push(randomContact);
+      return copyArr;
+    })
+  }
+
+  function sortByName () {
+    setContacts((prevContacts) =>{
+      let copyArr = [...prevContacts]
+      copyArr.sort((a,b) => {
+        const nameA = a.name.toUpperCase();
+        const nameB = b.name.toUpperCase();
+        if(nameA<nameB) return -1;
+        if(nameA>nameB) return 1;
+        return 0;
+      })
+      return copyArr
+    })
+  }
+
+  function sortByPopularity () {
+    setContacts((prevContacts) =>{
+      let copyArr = [...prevContacts]
+      copyArr.sort((a,b) => b.popularity-a.popularity)
+      return copyArr
+    })
+ 
   }
 
       return (
         <div className="App">
           <h2>IronContacts</h2>
-          <button>Add Random Contact</button>
+          <button onClick={addRandom}>Add Random Contact</button>
+          <button onClick={sortByName}>Sort by Name</button>
+          <button onClick={sortByPopularity}>Sort by Popularity</button>
           <table>
             <tr>
               <th>Picture</th>
@@ -25,7 +85,7 @@ function App() {
               <th>Popularity</th>
               <th>Won an Oscar</th>
               <th>Won an Emmy</th>
-              <th>Delete button</th>
+              <th>Actions</th>
             </tr>
             { contacts.map(contact => {
               return (
@@ -39,7 +99,6 @@ function App() {
                 </tr>
               )
             })
-
             }
           </table>
         </div>
