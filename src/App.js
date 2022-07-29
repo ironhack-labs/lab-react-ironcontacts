38443b34193dfdc0bde12c7cf6b5460bf61e99f8
Arturo Barrantes <arturo.barrantesv@pucp.pe>
2022-07-29 17:03:43 -0500

    iteration2 done

--- src/App.js
+++ src/App.js
@@ -4,38 +4,45 @@ import { useState } from "react";
 import dataContacts from "./contacts.json";
 
 function App() {
+  const firstSixContacts = dataContacts.filter((contact, idx) => idx < 6);
 
-  const firstFiveContacts = dataContacts.filter((contact,idx)=> idx<5)
-  
-
-  const [contacts, useContacts] = useState(firstFiveContacts);
+  const [contacts, useContacts] = useState(firstSixContacts);
 
   console.log(contacts);
 
-  return <div className="App">
-
-<table>
-  <tr>
-    <th>Picture</th>
-    <th>Name</th>
-    <th>Popularity</th>
-  </tr>
-{contacts.map(contact =>
-{return (
-  <tr>
-    <td>
-    <img src={contact.pictureUrl} alt="contacts[0].name" height={200}/>
-    
-    </td>
-    <td>{contact.name}</td>
-    <td>{contact.popularity}</td>
-  </tr>
-)})}
- 
-
-
-</table>
-
-  </div>;
+  return (
+    <div className="App">
+      <table>
+        <thead>
+          <tr>
+            <th>Picture</th>
+            <th>Name</th>
+            <th>Popularity</th>
+            <th>Won Oscar</th>
+            <th>Won Emmy</th>
+          </tr>
+        </thead>
+        <tbody>
+          {contacts.map((contact) => {
+            return (
+              <tr key={contact.id}>
+                <td>
+                  <img
+                    src={contact.pictureUrl}
+                    alt="contacts[0].name"
+                    height={200}
+                  />
+                </td>
+                <td>{contact.name}</td>
+                <td>{contact.popularity}</td>
+                {contact.wonOscar ? <td>🏆</td> : <td></td>}
+                {contact.wonEmmy ? <td>🏆</td> : <td></td>}
+              </tr>
+            );
+          })}
+        </tbody>
+      </table>
+    </div>
+  );
 }
 export default App;
