8bd096650b481f8e07690d4bc8f05be1366a8e92
Azdhy Galvan <azdhy16@gmail.com>
2022-07-30 00:37:34 -0500

    iteration 1y 2

--- src/App.js
+++ src/App.js
@@ -1,23 +1,88 @@
 import logo from './logo.svg';
 import './App.css';
+import contactsData  from "./contacts.json"
+import { useState } from 'react';
+
 
 function App() {
+
+const [contacts,serContacts] = useState(contactsData)
+
+const nameRandom = Math.floor(Math.random()* (contacts[0].name - contacts[50].name) + contacts[0].name)
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+      <div>
+      <div>
+        <button>Add Random Contact</button>
+      </div>
+
+        <table>
+          <tr>
+              <td>Picture</td>
+              <td>Name</td>
+              <td>Popularity</td>
+              <td>Won Emy</td>
+              <td>Won Oscar</td>
+              
+          </tr>
+
+          <tr>
+            <td><img src={contacts[0].pictureUrl} width={100} height={150}></img></td>
+            <td>{contacts[0].name}</td>
+            <td>{contacts[0].popularity.toFixed(2)}</td>
+            {contacts[0].wonEmmy ? <td>🏆</td> : <td> ❌</td>}
+            {contacts[0].wonOscar ? <td>🏆</td> : <td> ❌</td>}
+          </tr>
+          <tr>
+            <td><img src={contacts[1].pictureUrl} width={100} height={150}></img></td>
+            <td>{contacts[1].name}</td>
+            <td>{contacts[1].popularity.toFixed(2)}</td>
+            {contacts[1].wonEmmy ? <td>🏆</td> : <td> ❌</td>}
+            {contacts[1].wonOscar ? <td>🏆</td> : <td> ❌</td>}
+          </tr>
+          <tr>
+            <td><img src={contacts[2].pictureUrl} width={100} height={150}></img></td>
+            <td>{contacts[2].name}</td>
+            <td>{contacts[2].popularity.toFixed(2)}</td>
+            {contacts[2].wonEmmy ? <td>🏆</td> : <td> ❌</td>}
+            {contacts[2].wonOscar ? <td>🏆</td> : <td> ❌</td>}
+          </tr>
+          <tr>
+            <td><img src={contacts[3].pictureUrl} width={100} height={150}></img></td>
+            <td>{contacts[3].name}</td>
+            <td>{contacts[3].popularity.toFixed(2)}</td>
+            {contacts[3].wonEmmy ? <td>🏆</td> : <td> ❌</td>}
+            {contacts[3].wonOscar ? <td>🏆</td> : <td> ❌</td>}
+          </tr>
+          <tr>
+            <td><img src={contacts[4].pictureUrl} width={100} height={150}></img></td>
+            <td>{contacts[4].name}</td>
+            <td>{contacts[4].popularity.toFixed(2)}</td>
+            {contacts[4].wonEmmy ? <td>🏆</td> : <td> ❌</td>}
+            {contacts[4].wonOscar ? <td>🏆</td> : <td> ❌</td>}
+          </tr>
+          <tr>
+            <td><img src={contacts[5].pictureUrl} width={100} height={150}></img></td>
+            <td>{contacts[5].name}</td>
+            <td>{contacts[5].popularity.toFixed(2)}</td>
+            {contacts[5].wonEmmy ? <td>🏆</td> : <td> ❌</td>}
+            {contacts[5].wonOscar ? <td>🏆</td> : <td> ❌</td>}
+          </tr>
+
+
+
+
+        </table>
+      </div>
+
+
+
+
+
+     
     </div>
   );
 }
