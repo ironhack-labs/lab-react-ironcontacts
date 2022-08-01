664044b386121e87b550cd83a4323413ec71cc6b
ana luz martinez <almadigital.mx@gmail.com>
2022-07-31 22:54:40 -0700

    iteration 3 done

--- src/App.js
+++ src/App.js
@@ -8,10 +8,27 @@ function App() {
   const fiveContacts = contactList.slice(0,5)
   //set contacts state
   const [contacts,setContacts]=useState(fiveContacts)
+//add random contact to list (and avoid the current contacts)
+  const addRandomContact = () => {
+    const randomContact =  contactList[Math.floor(Math.random()*contactList.length)];
+    //.find returns the first element in the provided array that satisfies the provided testing function
+    const found = contacts.find(element => element.id=== randomContact.id);
+    if (found === undefined) {
+      setContacts((prevList) => {
+        const prevListCopy = [...prevList];
+        prevListCopy.push(randomContact);
+        return prevListCopy;
+      })
+    }
+    else {
+      addRandomContact();
+    }
+  }
 
   return <div className="App">
     <h1>IronContacts</h1>
 <table className="contact-table">
+  <button onClick={addRandomContact}>Add Random Contact</button>
   <thead>
   <tr>
     <th><h2>Picture</h2></th>
