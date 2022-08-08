f5512c8295a9684049ce5ca20cbfe9212ffe26dd
emmanuelsolis <emma.ruso@outlook.com>
2022-08-07 22:19:14 -0500

    iteration 1 & 2

--- src/App.js
+++ src/App.js
@@ -1,25 +1,74 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
+import contactsArray from './contacts.json';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+  const fiveContacts = contactsArray.slice(0, 5);
+  const [contacts, setContacts] = useState(fiveContacts);
+  const listContacts = contacts.map(contact => {
+    const roundedPopularity = Math.round(contact.popularity *100)/100;
+    return (
+      <tr>
+        <td>
+          <img 
+          src={contact.pictureUrl} 
+          alt="Contact profile"
+         className="contact-img" 
+         width={100} height={100} 
+         />
+        </td>
+        <td>{contact.name}</td>
+        <td>{roundedPopularity}</td>
+        {contact.wonOscar ? <td>🏆</td> : <td></td>}
+        {contact.wonEmmy ? <td>🏆</td> : <td></td>}
+      </tr>
+    )
+  } )
+  function addRandomContact() {
+    let randomContact = contactsArray[Math.floor(Math.random() * contactsArray.length)];
+    let founded = contacts.find(element => element.id === randomContact.id);
+    if (founded === undefined) {
+    setContacts(prevState => {
+      let prevList = [prevState];
+      return prevList.push(randomContact)
+    } )
+  }else {
+    addRandomContact();
+  }
+  }
+   
+    return (
+      <div className="App">
+        <div className="header">
+          <h1>IronContacts</h1>
+          <button onClick={() => addRandomContact()}>Add Random Contact</button>
+        </div>
+        <table className="contacts-table">
+          <tbody>
+            <tr>
+              <td>
+                <strong>Picture</strong>
+              </td>
+              <td>
+                <strong>Name</strong>
+              </td>
+              <td>
+                <strong>Popularity</strong>
+              </td>
+              <td>
+                <strong>Won <br/>Oscar</strong>
+              </td>
+              <td>
+                <strong>Won <br/>Emmy</strong>
+              </td>
+            </tr>
+            {listContacts}
+          </tbody>
+        </table>
+      </div>
+    )
+  }
+ 
+
 
 export default App;
