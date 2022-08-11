9f6f4596743ba8cdd797fbb082d14879d2aae490
Renata Inojosa <inojosa.renata@gmail.com>
2022-08-11 14:08:25 -0300

    iteration 1 done

--- src/App.js
+++ src/App.js
@@ -1,23 +1,46 @@
-import logo from './logo.svg';
 import './App.css';
+import contacts from "./contacts.json";
+
+const contactsArr = contacts.slice(0, 5); 
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <h1> IronContacts </h1>
+    <table className="table-list">
+      <tr>
+        <th>Picture</th>
+        <th>Name</th>
+        <th>Popularity</th>
+      </tr>
+      <tr>
+        <td><img className="contacts-img" src={contactsArr[0].pictureUrl} alt="img"/></td>
+        <td>{contactsArr[0].name}</td>
+        <td>{contactsArr[0].popularity.toFixed(2)}</td>
+     </tr>
+     <tr>
+        <td><img className="contacts-img" src={contactsArr[1].pictureUrl}alt="img"/></td>
+        <td>{contactsArr[1].name}</td>
+        <td>{contactsArr[1].popularity.toFixed(2)}</td>
+     </tr>
+     <tr>
+        <td><img className="contacts-img" src={contactsArr[2].pictureUrl}alt="img"/></td>
+        <td>{contactsArr[2].name}</td>
+        <td>{contactsArr[2].popularity.toFixed(2)}</td>
+     </tr>
+     <tr>
+        <td><img className="contacts-img" src={contactsArr[3].pictureUrl}alt="img"/></td>
+        <td>{contactsArr[3].name}</td>
+        <td>{contactsArr[3].popularity.toFixed(2)}</td>
+     </tr>
+     <tr>
+        <td><img className="contacts-img" src={contactsArr[4].pictureUrl}alt="img"/></td>
+        <td>{contactsArr[4].name}</td>
+        <td>{contactsArr[4].popularity.toFixed(2)}</td>
+     </tr>
+
+    </table>
+ 
     </div>
   );
 }
