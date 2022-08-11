ba3619def292ca269b9fb5254d0b4a3a93644b1b
Renata Inojosa <inojosa.renata@gmail.com>
2022-08-11 15:06:08 -0300

    doing iteration 3

--- src/App.js
+++ src/App.js
@@ -1,20 +1,18 @@
 import './App.css';
+import React, { useState } from 'react'; 
 import contacts from "./contacts.json";
 
 const contactsArr = contacts.slice(0, 5); 
 
-const wonOscar = contactsArr.map((artist) => {
-  return artist.wonOscar && '🏆'
-});
-
-const wonEmmy = contactsArr.map((artist) => {
-  return artist.wonEmmy && '🏆'
-});
 
 function App() {
+
+  const [random, setRandom] = useState();
+
   return (
     <div className="App">
     <h1> IronContacts </h1>
+    <button>Add Random Contact</button>
     <table className="table-list">
       <tr>
         <th>Picture</th>
@@ -27,36 +25,36 @@ function App() {
         <td><img className="contacts-img" src={contactsArr[0].pictureUrl} alt="img"/></td>
         <td>{contactsArr[0].name}</td>
         <td>{contactsArr[0].popularity.toFixed(2)}</td>
-        <td>{wonOscar}</td>
-        <td>{wonEmmy}</td>
+        <td>{contactsArr[0].wonOscar && '🏆'}</td>
+        <td>{contactsArr[0].wonEmmy && '🏆'}</td>
      </tr>
      <tr>
         <td><img className="contacts-img" src={contactsArr[1].pictureUrl}alt="img"/></td>
         <td>{contactsArr[1].name}</td>
         <td>{contactsArr[1].popularity.toFixed(2)}</td>
-        <td>{wonOscar}</td>
-        <td>{wonEmmy}</td>
+        <td>{contactsArr[1].wonOscar && '🏆'}</td>
+        <td>{contactsArr[1].wonEmmy && '🏆'}</td>
      </tr>
      <tr>
         <td><img className="contacts-img" src={contactsArr[2].pictureUrl}alt="img"/></td>
         <td>{contactsArr[2].name}</td>
         <td>{contactsArr[2].popularity.toFixed(2)}</td>
-        <td>{wonOscar}</td>
-        <td>{wonEmmy}</td>
+        <td>{contactsArr[2].wonOscar && '🏆'}</td>
+        <td>{contactsArr[2].wonEmmy && '🏆'}</td>
      </tr>
      <tr>
         <td><img className="contacts-img" src={contactsArr[3].pictureUrl}alt="img"/></td>
         <td>{contactsArr[3].name}</td>
         <td>{contactsArr[3].popularity.toFixed(2)}</td>
-        <td>{wonOscar}</td>
-        <td>{wonEmmy}</td>
+        <td>{contactsArr[3].wonOscar && '🏆'}</td>
+        <td>{contactsArr[3].wonEmmy && '🏆'}</td>
      </tr>
      <tr>
         <td><img className="contacts-img" src={contactsArr[4].pictureUrl}alt="img"/></td>
         <td>{contactsArr[4].name}</td>
         <td>{contactsArr[4].popularity.toFixed(2)}</td>
-        <td>{wonOscar}</td>
-        <td>{wonEmmy}</td>
+        <td>{contactsArr[4].wonOscar && '🏆'}</td>
+        <td>{contactsArr[4].wonEmmy && '🏆'}</td>
      </tr>
 
     </table>
