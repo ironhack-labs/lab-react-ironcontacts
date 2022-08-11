f30f4c5cb56a568ce32c11e7d4e9c99cbcda835b
Renata Inojosa <inojosa.renata@gmail.com>
2022-08-11 14:22:00 -0300

    iteration 2 done

--- src/App.js
+++ src/App.js
@@ -3,6 +3,14 @@ import contacts from "./contacts.json";
 
 const contactsArr = contacts.slice(0, 5); 
 
+const wonOscar = contactsArr.map((artist) => {
+  return artist.wonOscar && '🏆'
+});
+
+const wonEmmy = contactsArr.map((artist) => {
+  return artist.wonEmmy && '🏆'
+});
+
 function App() {
   return (
     <div className="App">
@@ -12,31 +20,43 @@ function App() {
         <th>Picture</th>
         <th>Name</th>
         <th>Popularity</th>
+        <th>Won Oscar</th>
+        <th>Won Emmy</th>
       </tr>
       <tr>
         <td><img className="contacts-img" src={contactsArr[0].pictureUrl} alt="img"/></td>
         <td>{contactsArr[0].name}</td>
         <td>{contactsArr[0].popularity.toFixed(2)}</td>
+        <td>{wonOscar}</td>
+        <td>{wonEmmy}</td>
      </tr>
      <tr>
         <td><img className="contacts-img" src={contactsArr[1].pictureUrl}alt="img"/></td>
         <td>{contactsArr[1].name}</td>
         <td>{contactsArr[1].popularity.toFixed(2)}</td>
+        <td>{wonOscar}</td>
+        <td>{wonEmmy}</td>
      </tr>
      <tr>
         <td><img className="contacts-img" src={contactsArr[2].pictureUrl}alt="img"/></td>
         <td>{contactsArr[2].name}</td>
         <td>{contactsArr[2].popularity.toFixed(2)}</td>
+        <td>{wonOscar}</td>
+        <td>{wonEmmy}</td>
      </tr>
      <tr>
         <td><img className="contacts-img" src={contactsArr[3].pictureUrl}alt="img"/></td>
         <td>{contactsArr[3].name}</td>
         <td>{contactsArr[3].popularity.toFixed(2)}</td>
+        <td>{wonOscar}</td>
+        <td>{wonEmmy}</td>
      </tr>
      <tr>
         <td><img className="contacts-img" src={contactsArr[4].pictureUrl}alt="img"/></td>
         <td>{contactsArr[4].name}</td>
         <td>{contactsArr[4].popularity.toFixed(2)}</td>
+        <td>{wonOscar}</td>
+        <td>{wonEmmy}</td>
      </tr>
 
     </table>
