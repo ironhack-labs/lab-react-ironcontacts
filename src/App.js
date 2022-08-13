38998f77c9af350a9df901c3c85771b083256e03
emmanuelsolis <emma.ruso@outlook.com>
2022-08-12 19:09:17 -0500

    adding a New Random Contact iteration 3

--- src/App.js
+++ src/App.js
@@ -6,69 +6,63 @@ function App() {
   const fiveContacts = contactsArray.slice(0, 5);
   const [contacts, setContacts] = useState(fiveContacts);
   const listContacts = contacts.map(contact => {
-    const roundedPopularity = Math.round(contact.popularity *100)/100;
-    return (
-      <tr>
-        <td>
-          <img 
-          src={contact.pictureUrl} 
-          alt="Contact profile"
-         className="contact-img" 
-         width={100} height={100} 
-         />
-        </td>
-        <td>{contact.name}</td>
-        <td>{roundedPopularity}</td>
-        {contact.wonOscar ? <td>🏆</td> : <td></td>}
-        {contact.wonEmmy ? <td>🏆</td> : <td></td>}
-      </tr>
-    )
-  } )
-  function addRandomContact() {
-    let randomContact = contactsArray[Math.floor(Math.random() * contactsArray.length)];
-    let founded = contacts.find(element => element.id === randomContact.id);
-    if (founded === undefined) {
-    setContacts(prevState => {
-      let prevList = [prevState];
-      return prevList.push(randomContact)
+    const roundedPopularity = Math.round(contact.popularity * 100) / 100;
+      return (
+        <tr>
+          <td>
+            <img
+              src={contact.pictureUrl}
+              alt="Contact profile"
+              className="contact-img"
+              width={100} height={100}
+            />
+          </td>
+          <td>{contact.name}</td>
+          <td>{roundedPopularity}</td>
+          {contact.wonOscar ? <td>🏆</td> : <td></td>}
+          {contact.wonEmmy ? <td>🏆</td> : <td></td>}
+        </tr>
+      )
     } )
-  }else {
-    addRandomContact();
-  }
-  }
-   
-    return (
-      <div className="App">
-        <div className="header">
-          <h1>IronContacts</h1>
-          <button onClick={() => addRandomContact()}>Add Random Contact</button>
-        </div>
-        <table className="contacts-table">
-          <tbody>
-            <tr>
-              <td>
-                <strong>Picture</strong>
-              </td>
-              <td>
-                <strong>Name</strong>
-              </td>
-              <td>
-                <strong>Popularity</strong>
-              </td>
-              <td>
-                <strong>Won <br/>Oscar</strong>
-              </td>
-              <td>
-                <strong>Won <br/>Emmy</strong>
-              </td>
-            </tr>
-            {listContacts}
-          </tbody>
-        </table>
+    const handleRandom = () => {
+      setContacts((prevState) => [
+        ...prevState, contactsArray[
+        Math.floor(Math.random() * (prevState.length - 0 + 1) + 0)
+        ]])
+        };
+  
+  return (
+    <div className="App">
+      <div className="header">
+        <h1>IronContacts</h1>
+        <button onClick={() => handleRandom()}>Add Random Contact</button>
       </div>
-    )
-  }
- 
+      <table className="contacts-table">
+        <tbody>
+          <tr>
+            <td>
+              <strong>Picture</strong>
+            </td>
+            <td>
+              <strong>Name</strong>
+            </td>
+            <td>
+              <strong>Popularity</strong>
+            </td>
+            <td>
+              <strong>Won <br />Oscar</strong>
+            </td>
+            <td>
+              <strong>Won <br />Emmy</strong>
+            </td>
+          </tr>
+          {listContacts}
+        </tbody>
+      </table>
+    </div>
+  )
+}
+
 
 
 export default App;
