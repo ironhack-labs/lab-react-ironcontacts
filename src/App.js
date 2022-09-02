d5309efb7a1ab84602cca816fba08aa853fb691b
CrisArranz <c.arranz.arevalo@gmail.com>
2022-09-02 06:21:06 +0200

    Refactoring variable names

--- src/App.js
+++ src/App.js
@@ -1,39 +1,39 @@
-import contacts from './contacts.json';
+import contactsData from './contacts.json';
 import Contact from './components/contact/Contact';
 import { useState } from 'react';
 
 import './App.css';
 
 function App() {
-  const dataContact = contacts.filter((contact, index) => index < 5);
+  const filterContact = contactsData.filter((contact, index) => index < 5);
 
-  const [contactsData, setContactsData] = useState(dataContact);
+  const [contacts, setContactsData] = useState(filterContact);
 
-  const handleContact = () => {
-    const pending = contacts.filter(c => !contactsData.includes(c));
-    const random  = pending[Math.floor(Math.random() * pending.length)]
-    if (random) {
-      setContactsData([random, ...contactsData])
+  const handleAddContact = () => {
+    const pendingContacts = contactsData.filter(c => !contacts.includes(c));
+    const randomContact  = pendingContacts[Math.floor(Math.random() * pendingContacts.length)]
+    if (randomContact) {
+      setContactsData([randomContact, ...contacts])
     }
   }
 
   const handleSortByPopularity = () => {
-    setContactsData([...contactsData.sort((a, b) => a.popularity - b.popularity)])
+    setContactsData([...contacts.sort((a, b) => a.popularity - b.popularity)])
   }
 
   const handleSortByName = () => {
-    setContactsData([...contactsData.sort((a, b) => a.name - b.name)])
+    setContactsData([...contacts.sort((a, b) => a.name - b.name)])
   }
 
   const handleDelete = (contact) => {
-    setContactsData(contactsData.filter(c => c.id !== contact.id ))
+    setContactsData(contacts.filter(c => c.id !== contact.id ))
   }
 
   return (
     <div className="app d-flex justify-content-center align-items-center flex-column text-center">
       <h2>IronContacts</h2>
       <div className="d-flex mb-3">
-        <button className="btn btn-primary mx-1" onClick={handleContact}>Add new Contact</button>
+        <button className="btn btn-primary mx-1" onClick={handleAddContact}>Add new Contact</button>
         <button className="btn btn-primary mx-1" onClick={handleSortByPopularity}>Sort by popularity</button>
         <button className="btn btn-primary mx-1" onClick={handleSortByName}>Sort by name</button>
       </div>
@@ -49,7 +49,7 @@ function App() {
             </tr>
           </thead>
           <tbody>
-            {contactsData.map(contact => <Contact key={contact.id} contact={contact} onDelete={() => handleDelete(contact)}/>)}
+            {contacts.map(contact => <Contact key={contact.id} contact={contact} onDelete={() => handleDelete(contact)}/>)}
           </tbody>
       </table>
     </div>
