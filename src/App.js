9465297a0cfc01fa753e3e93e9708453b423b7c9
Numan Torres <numantorresr@gmail.com>
2022-09-20 15:30:33 +0200

    almost done

--- src/App.js
+++ src/App.js
@@ -1,25 +1,55 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import "./App.css";
+import array from "../src/contacts.json";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
 
+  const fiveContacts = array.slice(0, 5)
+
+  const [initActors, setInitActors] = useState(fiveContacts);
+
+  const addActor = () => {
+    const newActors = [...initActors];
+    newActors.push(array[9]);
+    console.log(newActors)
+    setInitActors(newActors);
+  }
+
+
+  return (
+    <div className="App table-artists">
+      <table className="">
+        <thead className="head-table">
+          <tr>
+            <th>Picture</th>
+            <th>Name</th>
+            <th>Popularity</th>
+            <th>Won Oscar</th>
+            <th>Won Emmy</th>
+          </tr>
+        </thead>
+        <tbody className="body-table">
+          {initActors.map((actor) =>
+            <tr key={actor.name}>
+              <td>
+                <img src={actor.pictureUrl} alt="" />
+              </td>
+              <td>{actor.name} </td>
+              <td>{actor.popularity} </td>
+              <td>{actor.wonOscar ? '🏆' : '💀'}</td>
+              <td>{actor.wonEmmy ? '🏆' : '👋🏻'}</td>
+
+              {/* 
+                {
+                actor.wonOscar ? <td>🏆</td> : <td>🤥</td>
+              }
+
+ */}
+            </tr>
+          )}
+        </tbody>
+      </table>
+      <button onClick={addActor} >Add Actor</button>
+    </div>);
+}
 export default App;
