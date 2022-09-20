eba48c098d34df86563fe20ca07c0b73749cccab
David Pardo <davidpardomartin@gmail.com>
2022-09-20 15:13:34 +0200

    done

--- src/App.js
+++ src/App.js
@@ -1,25 +1,77 @@
 import logo from './logo.svg';
 import './App.css';
+import contacts from '../src/contacts.json'
+import { useState } from "react"
+
 
 function App() {
+  const fiveContacts = contacts.slice(0, 5)
+
+  const [initActors, setInitActors] = useState(fiveContacts)
+
+  const addActor = () => {
+    const newActors = [...initActors]
+    newActors.push(contacts[9])
+    setInitActors(newActors)
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App table-artist">
+      <table>
+        <thead className='head-table'>
+          <tr>
+            <th>Picture</th>
+            <th>Name</th>
+            <th>Popularity</th>
+            <th>WonOscar</th>
+            <th>WonEmmy</th>
+          </tr>
+        </thead>
+        <tbody className='body-table'>
+          {initActors.map((actor) => <tr key={actor.name}>
+            <td><img src={actor.pictureUrl} alt="" /></td>
+            <td>{actor.name}</td>
+            <td>{actor.popularity}</td>
+            {
+              actor.wonOscar ? <td>🏆</td> : <td>💩</td>
+            }
+            {
+              actor.wonEmmy ? <td>🏆</td> : <td>💩</td>
+            }
+          </tr>)}
+          {/* <td>{actor.wonOscar ? '🏆' : '💩'}</td> ------------> Segunda forma más elegante!!!!!!
+          <td>{actor.wonEmmy ? '🏆' : '💩'}</td> */}
+        </tbody>
+      </table>
+      <button onClick={addActor}>Add Actor</button>
     </div>
   );
+
 }
 
 export default App;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
