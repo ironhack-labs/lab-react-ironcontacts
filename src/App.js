4a96dfcec62a2a8961048f0c8b1ec854c7c5ae36
Carlos Vallejo <carlosvallejo.webmaster@gmail.com>
2022-09-20 15:49:26 +0200

    done

--- src/App.js
+++ src/App.js
@@ -1,25 +1,53 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react"
+import "./App.css";
+import contacts from "./contacts.json"
+
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+  const actors = contacts.slice(0, 6)
 
+  const [initContact, setContact] = useState(actors)
+
+  const addContact = () => {
+    const newContacts = [...initContact];
+    const randomNumber = Math.floor(Math.random() * (contacts.length - 6)) + 6
+    if (contacts[randomNumber]) {
+      newContacts.unshift(contacts[randomNumber]);
+      contacts.splice(randomNumber, 1);
+      setContact(newContacts);
+    }
+  }
+
+  return (<div className="App App-header">
+
+    <h1>IronContacts</h1>
+
+    <button onClick={addContact}>Add Random Contact</button>
+
+    <table className="">
+      <thead>
+        <tr>
+          <th>Picture</th>
+          <th>Name</th>
+          <th>Popularity</th>
+          <th>Won Oscar</th>
+          <th>Won Emmy</th>
+        </tr>
+      </thead>
+      <tbody>
+        {initContact.map((actor) =>
+          < tr >
+            <td><img className="img-actor" src={actor.pictureUrl} /></td>
+            <td>{actor.name}</td>
+            <td>{actor.popularity}</td>
+            <td>{actor.wonOscar ? '🏆' : '👎'}</td>
+            <td>{actor.wonEmmy ? '🏆' : '👎'}</td>
+          </tr>
+
+        )}
+      </tbody>
+    </table>
+  </div >
+  )
+}
 export default App;
