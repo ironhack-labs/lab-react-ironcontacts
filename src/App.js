62d9b5f4926396d0595146dc3116e8a261510953
tiffany_chang <changyunhsuan.fr@gmail.com>
2022-09-20 21:35:42 +0200

    finish iteration 2

--- src/App.js
+++ src/App.js
@@ -1,23 +1,67 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useState } from "react";
+import contacts from "./contacts.json";
 
 function App() {
+  const initialList = contacts.slice(0, 5);
+  const [contactList, setContactList] = useState(initialList);
+
+  function addRandomContact() {
+    console.log("add");
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>IronContacts</h1>
+      <button onClick={addRandomContact}>Add Random Contact</button>
+      <table>
+        <thead>
+          <tr>
+            <th>
+              <h2>Picture</h2>
+            </th>
+            <th>
+              <h2>Name</h2>
+            </th>
+            <th>
+              <h2>Popularity</h2>
+            </th>
+            <th>
+              <h2>Won an Oscar</h2>
+            </th>
+            <th>
+              <h2>Won an Emmy</h2>
+            </th>
+          </tr>
+        </thead>
+        <tbody>
+          {contactList.map((contact) => {
+            return (
+              <tr key={contact.id}>
+                <td>
+                  <img
+                    width="150px"
+                    src={contact.pictureUrl}
+                    alt="contact_img"
+                  ></img>
+                </td>
+                <td>
+                  <h3>{contact.name}</h3>
+                </td>
+                <td>
+                  <h3>{contact.popularity}</h3>
+                </td>
+                <td>
+                  <h3>{contact.wonOscar && "🏆"}</h3>
+                </td>
+                <td>
+                  <h3>{contact.wonEmmy && "🏆"}</h3>
+                </td>
+              </tr>
+            );
+          })}
+        </tbody>
+      </table>
     </div>
   );
 }
