6c91d674e946a5614112bc130c5148dc3a5e9f8f
Kira Soba <kira.soba@gmx.de>
2022-09-20 21:56:00 +0200

    Iteration 3: done

--- src/App.js
+++ src/App.js
@@ -3,8 +3,9 @@ import "./App.css";
 import contactsArr from "./contacts.json";
 import { useState } from "react";
 
-console.log(contactsArr);
-console.log(contactsArr.slice(0, 5));
+// console.log(contactsArr);
+// console.log(contactsArr.slice(0, 5));
+// console.log(contactsArr[47]);
 
 function App() {
   const [contacts, setContacts] = useState(contactsArr.slice(0, 6));
@@ -12,29 +13,54 @@ function App() {
   return (
     <div className="App">
       <h1>IronContacts</h1>
+      <button
+        onClick={() => {
+          setContacts([...contacts, pickRandomContact(contacts)]);
+        }}
+      >
+        Add random contact
+      </button>
       <table>
-        <tr>
-          <th>Picture</th>
-          <th>Name</th>
-          <th>Popularity</th>
-          <th>Won an Oscar</th>
-          <th>Won an Emmy</th>
-        </tr>
-
-        {contacts.map((contact) => (
+        <thead>
           <tr>
-            <td>
-              <img style={{ height: "200px" }} src={contact.pictureUrl}></img>
-            </td>
-            <td>{contact.name}</td>
-            <td>{contact.popularity.toFixed(2)}</td>
-            <td>{contact.wonOscar ? "🏆" : ""}</td>
-            <td>{contact.wonEmmy ? "🏆" : ""}</td>
+            <th>Picture</th>
+            <th>Name</th>
+            <th>Popularity</th>
+            <th>Won an Oscar</th>
+            <th>Won an Emmy</th>
           </tr>
-        ))}
+        </thead>
+        <tbody>
+          {contacts.map((contact) => {
+            return (
+              <tr key={contact.id}>
+                <td>
+                  <img
+                    style={{ height: "200px" }}
+                    src={contact.pictureUrl}
+                  ></img>
+                </td>
+                <td>{contact.name}</td>
+                <td>{contact.popularity.toFixed(2)}</td>
+                <td>{contact.wonOscar ? "🏆" : ""}</td>
+                <td>{contact.wonEmmy ? "🏆" : ""}</td>
+              </tr>
+            );
+          })}
+        </tbody>
       </table>
     </div>
   );
+}
+
+function pickRandomContact(arr) {
+  // console.log("Button clicked");
+  const randomNum = Math.floor(
+    Math.random() * (contactsArr.length - arr.length - 1) + arr.length - 1
+  );
+  // console.log("Random Number: ", randomNum);
+  return contactsArr[randomNum];
+  // console.log("Random Contact: ", randomContact);
 }
 
 export default App;
