4418bef8443ee62c72a909c88981bd79517b9f80
Tony5300 <tony_tolen@ziggo.nl>
2022-09-21 14:35:17 +0200

    iteration 3 done

--- src/App.js
+++ src/App.js
@@ -5,16 +5,38 @@ import contacts from "./contacts.json";
 function App() {
   const maxContacts = contacts.slice(0, 5);
   const [movieContact, setMovieContact] = useState(maxContacts);
+  const contactsCopy = [...contacts];
   console.log(maxContacts);
+
+  // const randomActor = contacts[Math.floor(Math.random() * contacts.length)];
+  const getRandomActor = () => {
+    const randomActor = Math.floor(Math.random() * contactsCopy.length);
+    const newContact = contactsCopy.splice(randomActor, 1);
+    const updatedContacts = [...movieContact, ...newContact];
+    setMovieContact(updatedContacts);
+  };
+
+  console.log("Random Actor:", contacts);
+
   return (
     <div className="App">
       <h1>IronContacts</h1>
+      <button
+        // onClick={() => {
+        //   setMovieContact([...movieContact, getRandomActor(contacts)]);
+        // }}
+        onClick={getRandomActor}
+      >
+        Add Random Contact
+      </button>
       <table>
         <tbody>
           <tr>
             <th>Picture</th>
             <th>Name</th>
             <th>Popularity</th>
+            <th>Won Oscar</th>
+            <th>Won Emmy</th>
           </tr>
           {movieContact.map((actor) => {
             return (
@@ -27,17 +49,12 @@ function App() {
                   />
                 </td>
                 <td>{actor.name}</td>
-                <td>{actor.popularity}</td>
+                <td> {Number(actor.popularity).toFixed(2)}</td>
+                {actor.wonEmmy ? <td>🏆</td> : ""}
+                {actor.wonOscar ? <td>🏆</td> : ""}
               </tr>
             );
           })}
-          {/* <tr>
-            <td>
-              <img src={movieContact[0].pictureUrl} alt="actor-img" />
-            </td>
-            <td>Math Damon</td>
-            <td>9.50</td>
-          </tr> */}
         </tbody>
       </table>
     </div>
