00ede1db1217bd594ee47a29ca7aa443ec1dfdad
Daiane Reina <daiane.veras.reina@gmail.com>
2022-09-22 19:31:16 +0100

    done: Iteration 1 | Display 5 Contacts

--- src/App.js
+++ src/App.js
@@ -1,23 +1,50 @@
-import logo from './logo.svg';
-import './App.css';
+// import logo from "./logo.svg";
+import { useState } from "react";
+import "./App.css";
+import contactsData from "./contacts.json";
 
 function App() {
+  const allContacts = [...contactsData];
+  // console.log("Contacts:", allContacts);
+
+  const fiveContacts = allContacts.slice(0, 5);
+  // console.log("Contacts:", fiveContacts);
+
+  const [contacts, setcontact] = useState(fiveContacts);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>IronContacts</h1>
+      <div className="tableContacts">
+        <table className="contacts">
+          <thead style={{ fontWeight: 500 }}>
+            <tr>
+              <th>Picture</th>
+              <th>Name</th>
+              <th>Popularity</th>
+            </tr>
+          </thead>
+          <tbody>
+            {contacts.map((contact) => {
+              return (
+                <tr key={contact.id} className="contact">
+                  <th>
+                    <img
+                      src={contact.pictureUrl}
+                      alt={contact.name}
+                      style={{ width: 70 }}
+                    />
+                  </th>
+                  <th style={{ fontWeight: 200 }}> {contact.name}</th>
+                  <th style={{ fontWeight: 200 }}>
+                    {contact.popularity.toFixed(2)}{" "}
+                  </th>
+                </tr>
+              );
+            })}
+          </tbody>
+        </table>
+      </div>
     </div>
   );
 }
