dfdfb7930e8863388f0fbf1cf843843b931382cc
Guillermo Cubells <guillermocubells@gmail.com>
2022-09-22 21:46:05 +0200

    feature: third iteration,rendering not unique random contacts

--- src/App.js
+++ src/App.js
@@ -2,11 +2,33 @@ import "./App.css";
 import contactList from "./contacts.json";
 import { useState } from "react";
 
+function getRandomContact(initList, endList) {
+  const rand = Math.floor(Math.random() * (endList - initList) + initList);
+  return rand;
+}
+
 function App() {
-  const [contacts, setContacts] = useState(contactList.slice(0, 8));
+  const [contacts, setContacts] = useState(contactList.slice(0, 5));
+  // const [otherContacts, setOtherContacts] = useState(contactList.slice(5));
+
+  // const randomContact = () => {
+  //   setOtherContacts(Math.floor(Math.random() * otherContacts.length));
+  //   setContacts([...contacts, setOtherContacts]);
+  // };
+
+  const randomContact = () => {
+    setContacts([
+      ...contacts,
+      contactList[getRandomContact(0, contactList.length)],
+    ]);
+  };
+
   return (
     <div className="App">
       <h1>Iron Contacts</h1>
+      <div>
+        <button onClick={randomContact}> Add Random Contact</button>
+      </div>
       <table>
         <thead>
           <tr>
@@ -20,11 +42,12 @@ function App() {
         <tbody>
           {contacts.map((contact) => {
             return (
-              <tr>
+              <tr key={contact.id}>
                 <td>
                   <img
                     style={{ height: "80px" }}
                     src={contact.pictureUrl}
+                    alt="hallo"
                   ></img>
                 </td>
                 <td> {contact.name}</td>
