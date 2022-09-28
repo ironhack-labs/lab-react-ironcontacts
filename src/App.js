21f8072f9e84451a691d7700462c7edc36f0e777
Maxime Erkens <mhmserkens@gmail.com>
2022-09-28 16:57:52 +0200

    iteration 3

--- src/App.js
+++ src/App.js
@@ -4,11 +4,34 @@ import contacts from "./contacts.json";
 
 function App() {
   const [contactList, setContactList] = useState(contacts.slice(0, 5));
-  //console.log(contactList);
+  //console.log(contactList.length);
+  //maak lijst met overige contacten
+  const remainingContactsList = contacts.filter(
+    (el) => !contactList.includes(el)
+  );
+
+  //console.log(remainingContactsList.length);
+
+  const addContact = () => {
+    // kies willekerig contact uit lijst met overige
+    const newContact =
+      remainingContactsList[Math.floor(Math.random() * contacts.length)];
+    console.log(typeof newContact);
+    //voeg toe aan contactList
+    //const newContactsList = contactList.push(newContact);
+    const newContactsList = [...contactList, newContact];
+
+    console.log(newContactsList);
+    setContactList(newContactsList);
+  };
+
+  //addContact();
+
   return (
     <div className="App">
       <div>
         <h1>IronContacts</h1>
+        <button onClick={addContact}>Add Random Contact</button>
         {contactList.map((contact) => {
           return (
             <table key={contact.id} {...contact}>
@@ -16,6 +39,8 @@ function App() {
                 <th>Picture</th>
                 <th>Name</th>
                 <th>Popularity</th>
+                <th>Won Oscar</th>
+                <th>Won Emmy</th>
               </tr>
               <tr>
                 <td>
@@ -23,6 +48,16 @@ function App() {
                 </td>
                 <td>{contact.name}</td>
                 <td>{contact.popularity}</td>
+                <td>
+                  {contact.wonOscar && (
+                    <img src="./3d-fluency-trophy.png" alt="Oscar" />
+                  )}
+                </td>
+                <td>
+                  {contact.wonEmmy && (
+                    <img src="./3d-fluency-trophy.png" alt="Emmy" />
+                  )}
+                </td>
               </tr>
             </table>
           );
