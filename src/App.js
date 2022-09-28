b8d51843124e12e8c485e618f4f901d72ce1dfa4
PelayoPAR <pelayopar@gmail.com>
2022-09-29 01:16:54 +0200

    up to iteration 3

--- src/App.js
+++ src/App.js
@@ -1,23 +1,54 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import contactList from "./contacts.json";
+import { useState } from "react";
 
 function App() {
+  const [contacts, setContacts] = useState([]);
+  
+
+  function getRandomContact(contactList) {
+    const randomIndex = Math.floor(Math.random() * contactList.length)
+    return contactList.splice(randomIndex, 1)[0]
+  }
+  // console.log(contacts)
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <h1>IronContacts</h1>
+     <button onClick={()=> {
+        setContacts([...contacts, getRandomContact(contactList)])
+      }}>Add Random Contact</button>
+      <table >
+        <thead>
+          <tr>
+            <th><b>Picture</b></th>
+            <th><b>Name</b></th>
+            <th><b>Popularity</b></th>
+            <th><b>Won an Oscar</b></th>
+            <th><b>Won an Emmy</b></th>
+          </tr>
+        </thead>
+        <tbody>
+          {contacts.map((contact, index) => {
+            {/* console.log(contact) */}
+            return (      
+              <tr key={index}>
+                <td><img style={{width: "80px"}} src={contact.pictureUrl}></img></td>
+                <td>{contact.name}</td>
+                <td>{contact.popularity.toFixed(2)}</td>
+                <td>{contact.wonOscar 
+                  ? "🏆"
+                  : null
+                }</td>
+                <td>{contact.wonEmmy 
+                  ? "🏆"
+                  : null
+                }</td>
+              </tr>
+            );
+         })}
+        </tbody>
+      </table>
+      {/* {console.log(contacts)} */}
     </div>
   );
 }
