f795b7536ffed704a30ae8ce0cfa1cda8d2baa62
Elvan Ipeker <eipekerdogan@hotmail.com>
2022-09-30 21:22:45 +0200

    not finished yet

--- src/App.js
+++ src/App.js
@@ -1,23 +1,63 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import contactsArray from "./contacts.json";
+import { useState } from "react";
 
 function App() {
+  const [contacts, setContacts] = useState(contactsArray.slice(0, 5));
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <table>
+        <thead>
+          <tr>
+            <td>
+              <h2>Picture</h2>
+            </td>
+            <td>
+              <h2>Name</h2>
+            </td>
+            <td>
+              <h2>Popularity</h2>
+            </td>
+            <td>
+              <h2>
+                Won <br /> Oscar
+              </h2>
+            </td>
+            <td>
+              <h2>
+                Won <br /> Emmy
+              </h2>
+            </td>
+            <td>
+              <h2>Actions</h2>
+            </td>
+          </tr>
+        </thead>
+
+        <tbody>
+          {contacts.map((contact) => {
+            return (
+              <tr>
+                <td>
+                  <img src={contact.pictureUrl} />
+                </td>
+                <td>
+                  <p>{contact.name}</p>
+                </td>
+                <td>
+                  {Math.round((contact.popularity + Number.EPSILON) * 100) /
+                    100}
+                </td>
+                <td></td>
+                <td></td>
+                <td></td>
+              </tr>
+            );
+          })}
+        </tbody>
+      </table>
     </div>
   );
 }
