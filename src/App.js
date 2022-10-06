7378c8df9415f3696d597f3a584ccaeba528bdfe
Lena Araújo <lenaaraujo92@gmail.com>
2022-10-06 13:39:25 -0300

    started lab

--- src/App.js
+++ src/App.js
@@ -1,23 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import contacts from "./contacts.json";
+import React, { useState } from "react";
 
 function App() {
+  const [contactsArr, setContactsArr] = useState(contacts.slice(0, 5));
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>IronContacts</h1>
+      <table>
+        <thead>
+          <tr>
+            <th>Picture</th>
+            <th>Name</th>
+            <th>Popularity</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>
+              <img src={contactsArr[0].pictureUrl} alt="Contact" />
+            </td>
+            <td>{contactsArr[0].name}</td>
+            <td>{contactsArr[0].popularity}</td>
+          </tr>
+        </tbody>
+      </table>
     </div>
   );
 }
