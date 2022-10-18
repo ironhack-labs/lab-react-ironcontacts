536fa435766fd34929e405a1c40ce1f609cebf2f
Jose Piras <giuseppe3piras@gmail.com>
2022-10-18 21:56:52 +0200

    Stuck in Iteration 3

--- src/App.js
+++ src/App.js
@@ -12,66 +12,80 @@ function App() {
 
   const [contacts, setContacts] = useState(firstFive)
 
-  console.log(firstFive)
+  const addRandom = () => {
 
+    const randomContact = contactsFromJSON[Math.floor(Math.random() * contactsFromJSON.length)];
 
-
-  return (
-
-
-    <div className="App">
-
-      <table>
-        <tbody>
-          <tr>
-            <th>picture</th>
-            <th>name</th>
-            <th>popularity</th>
-            <th>Won an Oscar</th>
-            <th>Won an Emmy</th>
-          </tr>
-          <tr>
-            <td> <img src={firstFive[0].picture} alt="picture"></img>  </td>
-            <td>{firstFive[0].name}</td>
-            <td>{firstFive[0].popularity}</td>
-            {firstFive[0].wonOscar ? <td>🏆</td> : <td></td>}
-            {firstFive[0].wonEmmy ? <td>🏆</td> : <td></td>}
-
-          </tr>
-          <tr>
-            <td> <img src={firstFive[1].picture} alt="picture"></img>  </td>
-            <td>{firstFive[1].name}</td>
-            <td>{firstFive[1].popularity}</td>
-            {firstFive[1].wonOscar ? <td>🏆</td> : <td></td>}
-            {firstFive[1].wonEmmy ? <td>🏆</td> : <td></td>}
-          </tr>
-          <tr>
-            <td> <img src={firstFive[2].picture} alt="picture"></img>  </td>
-            <td>{firstFive[2].name}</td>
-            <td>{firstFive[2].popularity}</td>
-            {firstFive[2].wonOscar ? <td>🏆</td> : <td></td>}
-            {firstFive[2].wonEmmy ? <td>🏆</td> : <td></td>}
-          </tr>
-          <tr>
-            <td> <img src={firstFive[3].picture} alt="picture"></img>  </td>
-            <td>{firstFive[3].name}</td>
-            <td>{firstFive[3].popularity}</td>
-            {firstFive[3].wonOscar ? <td>🏆</td> : <td></td>}
-            {firstFive[3].wonEmmy ? <td>🏆</td> : <td></td>}
-          </tr>
-          <tr>
-            <td> <img src={firstFive[4].picture} alt="picture"></img>  </td>
-            <td>{firstFive[4].name}</td>
-            <td>{firstFive[4].popularity}</td>
-            {firstFive[4].wonOscar ? <td>🏆</td> : <td></td>}
-            {firstFive[4].wonEmmy ? <td>🏆</td> : <td></td>}
-          </tr>
-        </tbody>
-
-      </table>
-
-    </div>
-  );
+    if (randomContact !== firstFive) {
+      firstFive.push(randomContact)
+    };
+  }
 }
+
+
+console.log(randomContact)
+
+
+
+return (
+
+
+  <div className="App">
+
+    <table>
+      <tbody>
+        <tr>
+          <th>picture</th>
+          <th>name</th>
+          <th>popularity</th>
+          <th>Won an Oscar</th>
+          <th>Won an Emmy</th>
+        </tr>
+        <tr>
+          <td> <img src={firstFive[0].picture} alt="picture"></img>  </td>
+          <td>{firstFive[0].name}</td>
+          <td>{firstFive[0].popularity}</td>
+          {firstFive[0].wonOscar ? <td>🏆</td> : <td></td>}
+          {firstFive[0].wonEmmy ? <td>🏆</td> : <td></td>}
+
+        </tr>
+        <tr>
+          <td> <img src={firstFive[1].picture} alt="picture"></img>  </td>
+          <td>{firstFive[1].name}</td>
+          <td>{firstFive[1].popularity}</td>
+          {firstFive[1].wonOscar ? <td>🏆</td> : <td></td>}
+          {firstFive[1].wonEmmy ? <td>🏆</td> : <td></td>}
+        </tr>
+        <tr>
+          <td> <img src={firstFive[2].picture} alt="picture"></img>  </td>
+          <td>{firstFive[2].name}</td>
+          <td>{firstFive[2].popularity}</td>
+          {firstFive[2].wonOscar ? <td>🏆</td> : <td></td>}
+          {firstFive[2].wonEmmy ? <td>🏆</td> : <td></td>}
+        </tr>
+        <tr>
+          <td> <img src={firstFive[3].picture} alt="picture"></img>  </td>
+          <td>{firstFive[3].name}</td>
+          <td>{firstFive[3].popularity}</td>
+          {firstFive[3].wonOscar ? <td>🏆</td> : <td></td>}
+          {firstFive[3].wonEmmy ? <td>🏆</td> : <td></td>}
+        </tr>
+        <tr>
+          <td> <img src={firstFive[4].picture} alt="picture"></img>  </td>
+          <td>{firstFive[4].name}</td>
+          <td>{firstFive[4].popularity}</td>
+          {firstFive[4].wonOscar ? <td>🏆</td> : <td></td>}
+          {firstFive[4].wonEmmy ? <td>🏆</td> : <td></td>}
+        </tr>
+      </tbody>
+
+    </table>
+
+
+    <button onClick={addRandom}>Random</button>
+
+  </div>
+);
+
 
 export default App;
