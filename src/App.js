5112a9dbb696ed97b08427ca59f42eccf652a6d5
Liz Manresa <manresa.liz@gmail.com>
2022-10-18 23:48:50 +0200

    solve iteration 1 and 2

--- src/App.js
+++ src/App.js
@@ -1,25 +1,52 @@
-import logo from './logo.svg';
+
 import './App.css';
+import contactsFromJSON from './contacts.json'
+import { useState } from "react";
 
 function App() {
+
+  let firstFive = contactsFromJSON.slice(0, 5)
+  const [contacts, setContacts] = useState(firstFive)
+  const title = "IronContacts";
+
+  
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div  className="App">
+        <h1>{title}</h1>
+        <button>Add Random Contact</button>
+        <button>Sort by popularity</button>
+        <button>Sort by name</button>
+        <table>
+          <thead>
+            <tr>
+              <th>Picture</th>
+              <th>Name</th>
+              <th>Popularity</th>
+              <th>Won Oscar</th>
+              <th>Won Emmy</th>
+            </tr>
+          </thead>
+          <tbody>
+          {contacts.map( contactElement => {
+          
+            return( <tr>
+              <td><img className="picture" src={contactElement.pictureUrl}/></td>
+              <td>{contactElement.name}</td>
+              <td>{Math.round(contactElement.popularity * 100)/100}</td>
+              <td>{contactElement.wonOscar ? '🏆' : null}</td>
+              <td>{contactElement.wonEmmy ? '🏆' : null}</td>
+            </tr>)   
+              }
+            )
+          }
+          </tbody>
+        </table>                          
     </div>
   );
 }
 
 export default App;
+
+
+                          
