ef55d17c9f6f6164426fcc22cd8db64b53278b1c
Marc Logan <marclogan90@gmail.com>
2022-10-19 09:08:03 +0200

    got to iteration 2

--- src/App.js
+++ src/App.js
@@ -1,25 +1,58 @@
-import logo from './logo.svg';
+//import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
+import contacts from './contacts.json'
+
+
 
 function App() {
+  let newArrInic = contacts.slice(0,5)
+
+  const [newArr,setNewArr] = useState(newArrInic)
+
+  function addRandom(){
+    let numRandom = (Math.floor(Math.random() * contacts.length));
+    let contactRandom = contacts[numRandom];
+    const arrExtra = [...newArr, contactRandom];
+    setNewArr(arrExtra);
+  }
+
+  const contactInfo = newArrInic.map((contact) => {
+    return (
+
+      <tr>
+        <td><img src={contact.pictureUrl} width='60px' /></td>
+        <td>{contact.name}</td>
+        <td>{(contact.popularity).toFixed(2)}</td>
+        <td>{contact.wonOscar}</td>
+        <td>{contact.wonEmmy}</td>
+      </tr>
+
+    )
+  })
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+      <h1>Ironcontacts</h1>
+      <div>
+        <button onClick={addRandom}>Add Random</button>
+      </div>
+
+      <h1>Ironcontacts</h1>
+      <table>
+        <thead>
+          <tr>
+            <th>Picture</th>
+            <th>Name</th>
+            <th>Popularity</th>
+            <th>Won An Oscar</th>
+            <th>Won An Emmy</th>
+          </tr>
+        </thead>
+        <tbody>{contactInfo}</tbody>
+      </table>
     </div>
-  );
+  )
 }
 
 export default App;
