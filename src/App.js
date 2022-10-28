fc21f382cf5758b9dd7145d3220321d4a11e74ed
hvkst <bernd@hvkst.de>
2022-10-29 00:01:12 +0200

    Iteration IV - added sorting in both directions

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ const initialState = allContacts.slice(0, 5);
 
 function App() {
   const [contacts, setContacts] = useState(initialState);
+  const [sortingDirection, setSortingDirection] = useState(false);
 
   function randomContact(input) {
     let randomContact = input[Math.floor(Math.random() * input.length)];
@@ -16,15 +17,49 @@ function App() {
     return randomContact;
   }
 
-  function handleClick() {
+  // Get a random contact
+  function handleClickRandom() {
     setContacts((current) => [...current, randomContact(allContacts)]);
   }
+
+  // Sort by popularity
+  const toggleSortingPop = () => {
+    // 👇️ passed function to setState
+    setSortingDirection((current) => !current);
+  };
+
+  function handleClickSortPop() {
+    const contactsPopSort = [...contacts];
+    contactsPopSort.sort((a, b) => {
+      return sortingDirection ? b.popularity - a.popularity : a.popularity - b.popularity;
+    });
+    setContacts(contactsPopSort);
+    toggleSortingPop();
+  }
+
+  // Sort by name
+  function handleClickSortName() {
+    const contactsNameSort = [...contacts];
+    contactsNameSort.sort((a, b) => {
+      return sortingDirection ? a.name.localeCompare(b.name) : b.name.localeCompare(a.name);
+    });
+    setContacts(contactsNameSort);
+    toggleSortingPop();
+  }
+
+  const buttonStyle = { padding: '5px', margin: '0 5px' };
 
   return (
     <div className="App">
       <h1>IronContacts</h1>
-      <button onClick={handleClick} style={{ padding: '10px' }}>
+      <button onClick={handleClickRandom} style={buttonStyle}>
         Add random Contact
+      </button>
+      <button onClick={handleClickSortPop} style={buttonStyle}>
+        Sort by popularity
+      </button>
+      <button onClick={handleClickSortName} style={buttonStyle}>
+        Sort by name
       </button>
       <table>
         <tbody>
