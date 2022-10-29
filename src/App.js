0bb78f403205997a58f27841cce743f8250a00c2
hvkst <bernd@hvkst.de>
2022-10-29 16:32:55 +0200

    done without styling

--- src/App.js
+++ src/App.js
@@ -1,69 +1,74 @@
-// import contacts from './contacts.json';
+import allContacts from './contacts.json';
 import { useState } from 'react';
 import './App.css';
 
-let allContacts = require('./contacts.json');
 const initialState = allContacts.slice(0, 5);
 
 function App() {
   const [contacts, setContacts] = useState(initialState);
   const [sortingDirection, setSortingDirection] = useState(false);
 
-  function randomContact(input) {
+  // Make sure contact is not there yet
+  const randomContact = (input) => {
     let randomContact = input[Math.floor(Math.random() * input.length)];
     while (contacts.includes(randomContact)) {
       randomContact = input[Math.floor(Math.random() * input.length)];
     }
     return randomContact;
-  }
+  };
 
   // Get a random contact
-  function handleClickRandom() {
-    setContacts((current) => [...current, randomContact(allContacts)]);
-  }
+  const getRandomContact = () => {
+    if (contacts.length < 52) {
+      setContacts((current) => [...current, randomContact(allContacts)]);
+    }
+  };
+
   // Toggle sorting direction
   const toggleSortingDirection = () => {
     setSortingDirection((current) => !current);
   };
+
   // Sort by popularity
-  function handleClickSortPop() {
+  const sortByPopularity = () => {
     const contactsPopSort = [...contacts];
     contactsPopSort.sort((a, b) => {
       return sortingDirection ? b.popularity - a.popularity : a.popularity - b.popularity;
     });
     setContacts(contactsPopSort);
     toggleSortingDirection();
-  }
+  };
 
   // Sort by name
-  function handleClickSortName() {
+  const sortByName = () => {
     const contactsNameSort = [...contacts];
     contactsNameSort.sort((a, b) => {
       return sortingDirection ? a.name.localeCompare(b.name) : b.name.localeCompare(a.name);
     });
     setContacts(contactsNameSort);
     toggleSortingDirection();
-  }
+  };
+
   // Delete Contact
-  function handleClickDelete(id) {
+  const handleClickDelete = (id) => {
     const contactToDelete = contacts.findIndex((obj) => obj.id === id);
     const contactsMinusOne = [...contacts];
     contactsMinusOne.splice(contactToDelete, 1);
     setContacts(contactsMinusOne);
-  }
+  };
 
   const buttonStyle = { padding: '5px', margin: '0 5px' };
 
   return (
     <div className="App">
       <h1>IronContacts</h1>
-      <button onClick={handleClickRandom} style={buttonStyle}>
+      <button onClick={getRandomContact} style={buttonStyle}>
         Add random Contact
       </button>
-      <button onClick={handleClickSortPop} style={buttonStyle}>
+      <button onClick={sortByPopularity} style={buttonStyle}>
         Sort by popularity
       </button>
-      <button onClick={handleClickSortName} style={buttonStyle}>
+      <button onClick={sortByName} style={buttonStyle}>
         Sort by name
       </button>
       <table>
@@ -96,7 +101,7 @@ function App() {
                 </td>
                 <td>
                   <button
-                    onClick={(e) => {
+                    onClick={() => {
                       handleClickDelete(contact.id);
                     }}
                   >
