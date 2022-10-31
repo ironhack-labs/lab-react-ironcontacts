06ac939cf7ba7071d02e33c5cf872874df491478
Patrick-Groot <patrick@candis.io>
2022-10-31 17:21:24 +0100

    Un-refactor, but finish lab

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import { useState } from "react";
 import "./App.css";
-import ContactTable from "./components/ContactTable";
 import contactsData from "./contacts.json";
 
 const startingContacts = contactsData.slice(0, 5);
@@ -32,8 +31,7 @@ function App() {
     const contactsByName = [...contactArray];
     console.log("contacts to sort: ", contactsByName);
     contactsByName.sort((a, b) => {
-      if (a.name.toLowerCase() < b.name.toLowerCase()) return -1;
-      return 1;
+      return a.name.toLowerCase() < b.name.toLowerCase() ? -1 : 1;
     });
     setContactArray(contactsByName);
   }
@@ -41,10 +39,17 @@ function App() {
   function sortByPopularity() {
     const contactsByPopularity = [...contactArray];
     contactsByPopularity.sort((a, b) => {
-      if (parseFloat(a.popularity) > parseFloat(b.popularity)) return -1;
-      return 1;
+      return parseFloat(a.popularity) > parseFloat(b.popularity) ? -1 : 1;
     });
     setContactArray(contactsByPopularity);
+  }
+
+  function deleteContact(id) {
+    const contactsDel = [...contactArray];
+    const indexToDelete = contactsDel.findIndex((elem) => 
+      elem.id === id);
+      contactsDel.splice(indexToDelete, 1);
+    setContactArray(contactsDel);
   }
 
   return (
@@ -55,7 +60,42 @@ function App() {
         <button onClick={sortByName}>Sort by Name</button>
         <button onClick={sortByPopularity}>Sort by Popularity</button>
       </div>
-      <ContactTable contactArray={[contactArray]} />
+      <table className="contactsTable">
+        <thead>
+          <tr>
+            <th>Picture</th>
+            <th>Name</th>
+            <th>Popularity</th>
+            <th>Won Oscar</th>
+            <th>Won Emmy</th>
+            <th>Action</th>
+          </tr>
+        </thead>
+        <tbody>
+          {contactArray.map((elem) => {
+            elem.popularity = parseFloat(elem.popularity).toFixed(2);
+            return (
+              <tr key={elem.id}>
+                <td>
+                  <img
+                    className="profilePicture"
+                    src={elem.pictureUrl}
+                    alt={`Profile of ${elem.name}`}
+                  />
+                </td>
+                <td>{elem.name}</td>
+                <td>{elem.popularity}</td>
+                {elem.wonOscar ? <td>🏆</td> : <td></td>}
+                {elem.wonEmmy ? <td>🏆</td> : <td></td>}
+                <td>
+                  <button onClick={() => {
+                      deleteContact(elem.id)}}>Delete</button>
+                </td>                
+              </tr>              
+            );
+          })}
+        </tbody>
+      </table>
     </div>
   );
 }
