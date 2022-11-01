dff75ef1b0c893517c40befe92829f38e481f168
yebrai <ivansevv@gmail.com>
2022-11-01 18:40:52 +0100

    all done

--- src/App.js
+++ src/App.js
@@ -1,23 +1,89 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import contacts from "./contacts.json";
+import { useState } from "react";
 
 function App() {
+  const firstFiveFamous = contacts.slice(0, 5);
+  const [list, setList] = useState(firstFiveFamous);
+
+  const addRandomContact = () => {
+    const randomIndex = Math.floor(Math.random() * contacts.length);
+    const randomContact = contacts[randomIndex];
+    let copyContacts = [...list];
+    console.log(copyContacts);
+    !copyContacts.includes(randomContact) && copyContacts.push(randomContact);
+    setList(copyContacts);
+  };
+
+  const sortByName = () => {
+    const copyContacts = [...list];
+
+    copyContacts.sort((elem1, elem2) => {
+      return elem1.name > elem2.name ? 1 : -1;
+    });
+
+    setList(copyContacts);
+  };
+
+  const sortByPopularity = () => {
+    const copyContacts = [...list];
+
+    copyContacts.sort((elem1, elem2) => {
+      return elem2.popularity - elem1.popularity;
+    });
+
+    setList(copyContacts);
+  };
+
+  const deleteContact = (contactId) => {
+    console.log(contactId);
+    const filteredList = list.filter((eachElem) => eachElem.id === contactId ? false : true
+    );
+
+    setList(filteredList);
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h2>IronContacts</h2>
+      <button onClick={addRandomContact}>Add random Contact</button>
+      <button onClick={sortByName} style={{ margin: "15px" }}>
+        Sort by Name
+      </button>
+      <button onClick={sortByPopularity}>Sort by Popularity</button>
+      <table style={{ margin: "0 auto 50px" }}>
+        <thead>
+          <tr>
+            <th>Picture</th>
+            <th>Name</th>
+            <th>Popularity</th>
+            <th style={{ padding: "20px" }}>Won an Oscar</th>
+            <th style={{ padding: "20px" }}>Won and Emmy</th>
+            <th>Actions</th>
+          </tr>
+        </thead>
+
+        <tbody>
+          {list.map((eachFamous) => {
+            return (
+              <tr key={eachFamous.id}>
+                <td>
+                  <img src={eachFamous.pictureUrl} alt="" width="50px" />
+                </td>
+                <td>{eachFamous.name}</td>
+                <td>{eachFamous.popularity.toFixed(2)}</td>
+                <td>{eachFamous.wonOscar ? "🏆" : ""}</td>
+                {/* ternario */}
+                <td>{eachFamous.wonEmmy && "🏆"}</td>
+                {/* Cortocircuito */}
+                <td>
+                  <button onClick={() => deleteContact(eachFamous.id)}>Delete</button>
+                </td>
+              </tr>
+            );
+          })}
+        </tbody>
+      </table>
     </div>
   );
 }
