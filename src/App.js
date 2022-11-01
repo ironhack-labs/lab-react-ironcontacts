cbeba4ff4078e40733156b388afbd7b3bdb28aad
MoniIgnacio <moniignacio02@gmail.com>
2022-11-01 21:59:25 +0400

    add It 6 done

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import contacts from "./contacts.json";
 import "./App.css";
 import { useState } from "react";
+
+
 function App() {
   let initialPersons = contacts.slice(0, 5);
   const [list, setList] = useState(initialPersons);
@@ -16,32 +18,52 @@ function App() {
 
   const sortByPop = () => {
     const cloneContacts = [...list];
-    cloneContacts.sort((elem1, elem2) => elem1.popularity > elem2.popularity ? -1 : 1);
+    cloneContacts.sort((elem1, elem2) =>
+      elem1.popularity > elem2.popularity ? -1 : 1
+    );
 
     setList(cloneContacts);
   };
 
   const sortByName = () => {
     const cloneContacts = JSON.parse(JSON.stringify(list));
-    cloneContacts.sort((elem1, elem2) => elem1.name > elem2.name ? 1 : -1);
+    cloneContacts.sort((elem1, elem2) => (elem1.name > elem2.name ? 1 : -1));
 
     setList(cloneContacts);
   };
 
   const deleteContact = (contactId) => {
-    const filteredList = list.filter((eachPerson) => eachPerson.id === contactId ? false : true);
+    const filteredList = list.filter((eachPerson) =>
+      eachPerson.id === contactId ? false : true
+    );
 
     setList(filteredList);
+  };
+
+  const buttonStyle = {
+    backgroundColor: "red",
+    opacity: "0.6",
+    marginBottom: "50px",
+    padding: '20px',
+    borderRadius: '20px'
   };
 
   return (
     <div className="App">
       <div>
         <h3>IronContacts</h3>
-        <button onClick={addContact}>Add Random Contact</button>
-        <button onClick={sortByPop}>Sort by popularity</button>
-        <button onClick={sortByName}>Sort by name</button>
-        <table>
+        <div>
+          <button onClick={addContact} style={buttonStyle}>
+            Add Random Contact
+          </button>
+          <button onClick={sortByPop} style={buttonStyle}>
+            Sort by popularity
+          </button>
+          <button onClick={sortByName} style={buttonStyle}>
+            Sort by name
+          </button>
+        </div>
+        <table style={{ margin: "0 auto", width: "100vh", padding: "20px" }}>
           <thead>
             <tr>
               <th>Picture</th>
@@ -68,7 +90,13 @@ function App() {
                   <td>{eachPerson.wonOscar === true ? "🏆" : ""}</td>
                   <td>{eachPerson.wonEmmy === true ? "🏆" : ""}</td>
                   <td>
-                    <button onClick={() => {deleteContact(eachPerson.id)}}>Delete</button>
+                    <button
+                      onClick={() => {
+                        deleteContact(eachPerson.id);
+                      }}
+                    >
+                      Delete
+                    </button>
                   </td>
                 </tr>
               </tbody>
