c06ba071b05c45bea7d2dd92fdef85c99a5bca05
victor moreno <oliverio89@gmail.com>
2022-11-29 17:03:32 +0100

    semidone

--- src/App.js
+++ src/App.js
@@ -1,23 +1,50 @@
-import logo from './logo.svg';
+import { useState } from 'react'
 import './App.css';
+import contacts from "./contacts.json";
 
 function App() {
+
+
+  const selectedContacts = contacts.splice(0, 5)
+
+  const [contactsData, setContactsData] = useState(selectedContacts)
+  const addConctacts = () => {
+    const randomContacts = contacts[5]
+    const contactsCopy = [...contactsData]
+    contactsCopy.unshift(randomContacts)
+    setContactsData(contactsCopy)
+  }
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1> IronContacts </h1>
+      <button onClick={addConctacts}>Añadir Contacto</button>
+
+      <table>
+        <thead>
+          <tr>
+            <th> Picture</th>
+            <th> Name</th>
+            <th> Popularity</th>
+            <th> Won Oscar</th>
+            <th> Won Emmy</th>
+          </tr>
+        </thead>
+        <tbody>
+
+          {contactsData.map(elm => {
+            return (
+              <tr>
+                <td><img src={elm.pictureUrl}></img></td>
+                <td><h3>{elm.name}</h3></td>
+                <td> {elm.popularity}</td>
+                <td> {elm.wonOscar ? <p>🏆</p> : <p></p>}</td>
+                <td> {elm.wonEmmy ? <p>🏆</p> : <p></p>}</td>
+              </tr>)
+          })}
+        </tbody>
+      </table>
     </div>
   );
 }
