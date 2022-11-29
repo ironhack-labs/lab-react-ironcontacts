27e936aeaafa0701e241b1962552634e7add057e
sijincodes <sijinmathews03@gmail.com>
2022-11-29 21:00:58 +0100

    iteration4

--- src/App.js
+++ src/App.js
@@ -1,51 +1,55 @@
 // src/App.js
 import "./App.css";
 import contacts from "./contacts.json";
-import { useState } from 'react';
-
- const showContacts = contacts.slice(0,5);
+import { useState } from "react";
 
 function App() {
-  //console.log(ShowContacts)
-  const [contact,setContact] = useState(showContacts);
-  function addContact(){
-    const randomNumber=  Math.floor(Math.random() * contacts.length) + 6 //from 6 to length of array
-    console.log(randomNumber)
-     const newContacts   = [...contact,contacts[randomNumber]];
+  const showContacts = contacts.slice(0, 5);
+  const [contact, setContact] = useState(showContacts);
+
+  function addContact() {
+    const randomNumber = Math.floor(Math.random() * contacts.length) + 6; //from 6 to length of array
+    console.log(randomNumber);
+    const newContacts = [...contact, contacts[randomNumber]];
     setContact(newContacts);
   }
+  function sortByName() {
+
+    setContact([...contact].sort((a, b) => a.name.localeCompare(b.name)));
+  }
+  function sortByPopularity(){
+    setContact([...contact].sort((a,b)=>b.popularity-a.popularity))
+  }
   return (
-    
     <div className="App">
-       <button onClick={addContact}>Add random contact</button>
+      <button onClick={addContact}>Add random contact</button>
+      <button onClick={sortByName}>Sort By Name</button>
+      <button onClick={sortByPopularity}>Sort By Popularity</button>
       <h2>IronContacts</h2>
       <table>
-      <thead>
-  <tr>
-    <th>Picture</th>
-    <th>Name</th>
-    <th>Popularity</th>
-    <th>Won An Oscar</th>
-    <th>Won An Emmy</th>
-  </tr>
-  </thead>
-  <tbody>
-       {contact.map(elm => 
-        
-        <tr key={elm.id} >
-   
-    <td><img src={elm.pictureUrl} alt="img" /></td>
-    <td>{elm.name}</td>
-    <td>{elm.popularity}</td>
-    {elm.wonOscar ?<td>🏆</td>: <td></td>}
-    {elm.wonEmmy ? <td>🏆</td>: <td></td>}
-    </tr>
-  )}
-  </tbody>
-</table>
-      
-      
-    
+        <thead>
+          <tr>
+            <th>Picture</th>
+            <th>Name</th>
+            <th>Popularity</th>
+            <th>Won An Oscar</th>
+            <th>Won An Emmy</th>
+          </tr>
+        </thead>
+        <tbody>
+          {contact.map((elm) => (
+            <tr key={elm.id}>
+              <td>
+                <img src={elm.pictureUrl} alt="img" />
+              </td>
+              <td>{elm.name}</td>
+              <td>{elm.popularity}</td>
+              {elm.wonOscar ? <td>🏆</td> : <td></td>}
+              {elm.wonEmmy ? <td>🏆</td> : <td></td>}
+            </tr>
+          ))}
+        </tbody>
+      </table>
     </div>
   );
 }
