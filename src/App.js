f4bd4920f390e907353c0cdce6c7210bab67fda0
Daniela <danielacardona2203@gmail.com>
2022-12-04 23:13:45 +0100

    done

--- src/App.js
+++ src/App.js
@@ -1,98 +1,71 @@
-import logo from './logo.svg';
-import './App.css';
-import { useState } from 'react';
+import "./App.css";
+import { useState } from "react";
 import contacts from "./contacts.json";
 
 function App() {
-  const [contact, setContact] = useState(contacts.slice(0,5));
+  let data = [...contacts];
+  let fiveContacts = data.slice(0, 5);
+  let newArray = data.slice(6);
+  const [celebs, setCelebs] = useState(fiveContacts);
 
   function addRandom() {
-    let randomNr = Math.floor(Math.random()*contacts.length);
-
-    let randomContact = contacts[randomNr];
-    console.log("random", contacts[randomNr].name)
-
-    let listedContacts = [];
-    contact.forEach(el => {
-      listedContacts.push(el.name);
-    })
-    console.log("array",listedContacts );
-
-    if ( listedContacts.indexOf(contacts[randomNr].name) === -1 ) {
-      setContact([...contact, randomContact]);
-    }
+    let randomContact = newArray[Math.floor(Math.random() * newArray.length)];
+    setCelebs([...celebs, randomContact]);
   }
 
   function sortPopularity() {
-    let sorted = [...contact].sort((a, b) => {
-      return b.popularity - a.popularity;
-    })
-    setContact(sorted)
+    let populatirySorted = [...celebs].sort((a, b) =>
+      a.popularity < b.popularity ? -1 : 1
+    );
+    setCelebs(populatirySorted);
   }
 
-  function sortName() {    
-    let sorted = [...contact].sort((a, b) => {
-      return a.name > b.name ? 1 : -1;
-    });
-    setContact(sorted)  
+  function sortName() {
+    let nameSorted = [...celebs].sort((a, b) => (a.name < b.name ? -1 : 1));
+    setCelebs(nameSorted);
   }
 
-  const clickToDelete = contactId => {
-    console.log(contactId)
-    const filteredContact = [...contact].filter(contact => {
-      return contact.id !== contactId;
+  function deleteCeleb(id) {
+    const deletedArray = [...celebs].filter((celeb) => {
+      return celeb.id !== id;
     });
-
-    setContact(filteredContact);
+    setCelebs(deletedArray);
   }
 
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-      <h1>IronContact</h1>
-      <div className="btn-buttons">
+      <h1>Iron Contacts</h1>
+      <div>
         <button onClick={addRandom}>Add Random Contact</button>
-        <button onClick={sortPopularity}>Sort by popularity</button>
-        <button onClick={sortName}>Sort by name</button>
+        <button onClick={sortPopularity}>Sorte by Populatiry</button>
+        <button onClick={sortName}>Sorte by Name</button>
       </div>
-
       <table>
         <thead>
           <tr>
             <th>Picture</th>
             <th>Name</th>
-            <th>Popularity</th>
+            <th>Populatiry</th>
             <th>Won an Oscar</th>
             <th>Won an Emmy</th>
-            <th>Actions</th>
+            <th>Action</th>
           </tr>
         </thead>
         <tbody>
-        {/* {contact.length < 5 ? } */}
-        {contact.map(contact => (
-          <tr key={contact.id}>
-            <td><img src={contact.pictureUrl} alt={contact.name}/></td>
-            <td>{contact.name}</td>
-            <td>{contact.popularity}</td>
-            {contact.wonOscar ? <td>🏆</td> : <td></td> }
-            {contact.wonEmmy ? <td>🌟</td> : <td></td> }
-
-            <td><button onClick={() => clickToDelete(contact.id)} className="delete-btn">Delete</button></td>
-          </tr>
-        ))}
+          {celebs.map((contact) => (
+            <tr key={contact.id}>
+              <td>
+                <img src={contact.pictureUrl} alt="" />
+              </td>
+              <td>{contact.name}</td>
+              <td>{contact.popularity}</td>
+              {contact.wonOscar ? <td>🏆</td> : <td></td>}
+              {contact.wonEmmy ? <td>🏆</td> : <td></td>}
+              <td>
+                <button onClick={() => deleteCeleb(contact.id)}>Delete</button>
+              </td>
+            </tr>
+          ))}
         </tbody>
       </table>
     </div>
