b0ce5fa82ad57eb6597a3b3f0fa9635725f16d7f
Mevine Teelokee <mevine.teelokee@gmail.com>
2023-01-05 05:49:30 +0100

    iteration 2 completed

--- src/App.js
+++ src/App.js
@@ -1,27 +1,35 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 import contacts from "./contacts.json";
 
 function App() {
-  let contact = contacts.splice(0,5)
-  console.log(contact)
+  let contact = contacts.splice(0, 5);
+  console.log(contact);
   return (
     <div className="App">
-     <table>
-      <tr>
-        <th>Picture</th>
-        <th>Name</th>
-        <th>Popularity</th>
-      </tr>
-      {contact.map((e) => {
-        console.log(e)
-        return <tr>
-        <td><img className='pic' src={e.pictureUrl} alt="" /></td>
-        <td>{e.name}</td>
-        <td>{e.popularity}</td>
-      </tr>
-      })}
-     </table>
+      <table>
+        <tr>
+          <th>Picture</th>
+          <th>Name</th>
+          <th>Popularity</th>
+          <th>Won Oscar</th>
+          <th>Won Emmy</th>
+        </tr>
+        {contact.map((e) => {
+          return (
+            <tr>
+              <td>
+                <img className="pic" src={e.pictureUrl} alt="" />
+              </td>
+              <td>{e.name}</td>
+              <td>{e.popularity}</td>
+              {e.wonOscar === true && <td> 🏆</td>}
+
+              {e.wonEmmy=== true && <td> 🏆</td>}
+            </tr>
+          );
+        })}
+      </table>
     </div>
   );
 }
