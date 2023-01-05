44c37e0ed7b7e029c16123c16ae3d827e9291c3d
Mevine Teelokee <mevine.teelokee@gmail.com>
2023-01-05 05:42:20 +0100

    iteration 1 completed

--- src/App.js
+++ src/App.js
@@ -1,23 +1,27 @@
 import logo from './logo.svg';
 import './App.css';
+import contacts from "./contacts.json";
 
 function App() {
+  let contact = contacts.splice(0,5)
+  console.log(contact)
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     <table>
+      <tr>
+        <th>Picture</th>
+        <th>Name</th>
+        <th>Popularity</th>
+      </tr>
+      {contact.map((e) => {
+        console.log(e)
+        return <tr>
+        <td><img className='pic' src={e.pictureUrl} alt="" /></td>
+        <td>{e.name}</td>
+        <td>{e.popularity}</td>
+      </tr>
+      })}
+     </table>
     </div>
   );
 }
