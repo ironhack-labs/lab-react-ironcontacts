b8928df032bd6dad8e0f69235e254c6c8af46c19
Paulfresnel <paul.fresnel1@gmail.com>
2023-01-07 16:39:53 +0100

    First Iterations Done

--- src/App.js
+++ src/App.js
@@ -1,23 +1,53 @@
 import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
+import ContactsArray from './contacts.json'
 
 function App() {
+  const copyOfContactsArray = [...ContactsArray]
+  const slicedCopiedArray = copyOfContactsArray.slice(0,5)
+  const restOfArray = copyOfContactsArray.slice(5)
+  const [contacts, setContacts] = useState(slicedCopiedArray)
+
+  const addRandomContact = () =>{
+    console.log(contacts)
+    let randomNumber = Math.floor(Math.random()* restOfArray.length)
+    console.log(randomNumber)
+    let elementToAdd = restOfArray[randomNumber]
+    console.log(elementToAdd)
+    let copyOfSlicedArray = [...slicedCopiedArray] 
+    copyOfSlicedArray.push(elementToAdd)
+    console.log(copyOfSlicedArray)
+    setContacts(copyOfSlicedArray)
+  }
+
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <button onClick={addRandomContact}>Add a Random Contact</button>
+      <table>
+        <thead>
+          <tr>
+            <td>Picture</td>
+            <td>Name</td>
+            <td>Popularity</td>
+            <td>Won an Oscar?</td>
+            <td>Won an Emmy?</td>
+          </tr>
+        </thead>
+        <tbody>
+          {contacts.map(contact=>{
+            return <tr>
+              <td><img className="small" src={contact.pictureUrl}></img></td>
+              <td>{contact.name}</td>
+              <td>{contact.popularity}</td>
+              {contact.wonOscar && <td>🏆</td>}
+              {contact.wonEmmy && <td>✨</td>}
+            </tr>
+          })}
+        </tbody>
+      </table>
     </div>
   );
 }
