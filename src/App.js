8eec5058f95e9749f73b4f2330a3ea04679c95ea
Anil Ozsoy <anilozsoy@gmail.com>
2023-01-09 17:42:25 +0100

    Iteration 3 | Add New Random Contacts

--- src/App.js
+++ src/App.js
@@ -3,13 +3,32 @@ import { useState } from "react";
 import allContacts from "./contacts.json";
 
 function App() {
-  //First 5 element of an array
-  const firstFive = allContacts.slice(0, 5);
-  //
-  const [contacts, setContact] = useState(firstFive);
+  //Listed Contacts
+  const listedContacts = allContacts.slice(0, 5);
+
+  //State
+  const [contacts, setContacts] = useState(listedContacts);
+
+  //Unlisted Contacts
+  const unListedContacts = allContacts.slice(5, allContacts.length);
+
+  const addRandomContact = () => {
+    //Random contact from unlisted contacts
+    let randomContactIndex = Math.floor(
+      Math.random() * unListedContacts.length
+    );
+    // New Contact
+    let newContact = unListedContacts.splice(randomContactIndex, 1);
+
+    console.log(contacts);
+    console.log(newContact[0]);
+    setContacts([...contacts, newContact[0]]);
+  };
 
   return (
     <div className="App">
+      <h1>IronContacts</h1>
+      <button onClick={addRandomContact}>Add Random Contact</button>
       <table>
         <tbody>
           <tr>
