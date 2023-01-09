4d4dfb311abf05b1c96e1971d6b8980c58a6b5b1
Paulfresnel <paul.fresnel1@gmail.com>
2023-01-09 15:40:08 +0100

    Lab finished, no styling yet

--- src/App.js
+++ src/App.js
@@ -1,31 +1,73 @@
-import logo from './logo.svg';
 import { useState } from 'react';
 import './App.css';
 import ContactsArray from './contacts.json'
 
+const copyOfContactsArray = [...ContactsArray];
+let slicedCopiedArray = copyOfContactsArray.slice(0,5);
+const restOfArray = copyOfContactsArray.slice(5);
+
+
 function App() {
-  const copyOfContactsArray = [...ContactsArray]
-  const slicedCopiedArray = copyOfContactsArray.slice(0,5)
-  const restOfArray = copyOfContactsArray.slice(5)
+  
   const [contacts, setContacts] = useState(slicedCopiedArray)
 
   const addRandomContact = () =>{
-    console.log(contacts)
     let randomNumber = Math.floor(Math.random()* restOfArray.length)
     console.log(randomNumber)
+    console.log(restOfArray.length)
     let elementToAdd = restOfArray[randomNumber]
+        let copyOfSlicedArray = [...contacts] 
+
     console.log(elementToAdd)
-    let copyOfSlicedArray = [...slicedCopiedArray] 
     copyOfSlicedArray.push(elementToAdd)
+    slicedCopiedArray = copyOfSlicedArray
+    restOfArray.splice(randomNumber, 1)
     console.log(copyOfSlicedArray)
     setContacts(copyOfSlicedArray)
   }
 
+  const popularitySort = ()=>{
+    let sortedContacts = contacts.sort((a,b)=>{
+      return b.popularity - a.popularity
+    })
+    setContacts(sortedContacts)
+  }
+
+  const nameSort = ()=>{
+    let sortedContactsByName = contacts.sort((a,b)=>{
+      if (a.name < b.name){
+        return -1;
+      } else if (a.name > b.name){
+        return 1;
+      } else {
+        return 0
+      }   
+     })
+    console.log(sortedContactsByName)
+    setContacts(sortedContactsByName)
+  }
+
+  const deleteContact = (event)=>{
+    let target = event.target
+      console.log(target)
+    let contactId = target.value
+      console.log(contactId)
+    let newContactsArray = contacts.filter(contact=>{
+      return contact.id !== contactId
+    })
+    setContacts(newContactsArray)
+    /* let rowElement = target.parentElement.parentElement
+    rowElement.style.display = "none"
+    console.log(rowElement) */
+  }
 
 
   return (
     <div className="App">
     <button onClick={addRandomContact}>Add a Random Contact</button>
+    <button onClick={popularitySort}>Sort by Popularity</button>
+        <button onClick={nameSort}>Sort by Name</button>
+
       <table>
         <thead>
           <tr>
@@ -34,6 +76,7 @@ function App() {
             <td>Popularity</td>
             <td>Won an Oscar?</td>
             <td>Won an Emmy?</td>
+            <td> </td>
           </tr>
         </thead>
         <tbody>
@@ -42,9 +85,11 @@ function App() {
               <td><img className="small" src={contact.pictureUrl}></img></td>
               <td>{contact.name}</td>
               <td>{contact.popularity}</td>
-              {contact.wonOscar && <td>🏆</td>}
-              {contact.wonEmmy && <td>✨</td>}
+              {contact.wonOscar ? <td>🏆</td> : <td> </td>}
+              {contact.wonEmmy ? <td>✨</td> : <td> </td>}
+              <td><button value={contact.id}onClick={deleteContact}>Delete Contact</button></td>
             </tr>
+            
           })}
         </tbody>
       </table>
