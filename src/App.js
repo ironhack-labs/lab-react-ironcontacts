3bc9a4ad9971d2e59758892f0f7156c358997ca1
Thalita dos Reis <dosreistha@gmail.com>
2023-01-17 18:10:20 +0100

    iterations 1 to 3 done

--- src/App.js
+++ src/App.js
@@ -1,23 +1,56 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState } from "react";
+import contactsJson from "./contacts.json";
 
 function App() {
+  
+  // listing contacts: iteration 1 ++
+  const listedContacts = contactsJson.slice(0, 5)
+  const [contacts, setContact] = useState(listedContacts);
+
+  // contacts not listed
+  const contactsNotListed = contactsJson.slice(5, contactsJson.length)
+
+  const addRandomContact = () => {
+    // add random contact from contacts that are not listed
+    let randomContact = Math.floor(Math.random() * contactsNotListed.length) 
+
+    let newContact = contactsNotListed.splice(randomContact, 1)
+
+    setContact([...contacts, newContact[0]]);
+  }
+
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>IronContacts</h1>
+
+      <button onClick={addRandomContact}>Add Random Contact</button>
+
+      <table>
+        <tbody>
+          <tr>
+            <th>Picture</th>
+            <th>Name</th>
+            <th>Populatiry</th>
+            <th>Won Oscar</th>
+            <th>Won Emmy</th>
+          </tr>
+
+          {contacts.map((contact) => {
+            return (
+              <tr key={contact.id}>
+                <td key={contact.id}><img src={contact.pictureUrl} alt="" width={100}/></td>
+                <td>{contact.name}</td>
+                <td>{(Math.ceil(contact.popularity) / 2).toFixed(2)}</td>
+                <td>{contact.wonOscar ? "🏆" : ""}</td>
+                <td>{contact.wonEmmy ? "\u2B50" : ""}</td>
+              </tr>
+            );
+          })}
+        </tbody>
+      </table>
     </div>
   );
 }
