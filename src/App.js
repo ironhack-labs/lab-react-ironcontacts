b7de37a2c8df443324c75c6f01f6ff9cd71af7ed
Laura Bährs <laura.baehrs@googlemail.com>
2023-01-18 00:58:58 +0100

    finish iteration 3

--- src/App.js
+++ src/App.js
@@ -1,14 +1,31 @@
 import contacts from "./contacts.json";
 import "./App.css";
-//import { useState } from "react";
+import { useState } from "react";
 
 function App() {
   const firstFiveContacts = contacts.slice(0, 5);
-  //const [contactsArray, setContactsArray] = useState(firstFiveContacts);
+  const [contactsArray, setContactsArray] = useState(firstFiveContacts);
+
+  const createRandomContacts = () => {
+    //calc the new list of contacts
+
+    const randomContact =
+      contacts[Math.floor(Math.random() * (contacts.length - 6))];
+
+    //update state
+    setContactsArray([...contactsArray, randomContact]);
+  };
 
   return (
     <div className="App">
       <h1>Ironhack Contacts</h1>
+      <button
+        onClick={() => {
+          createRandomContacts();
+        }}
+      >
+        Add Random Contact
+      </button>
       <table>
         <thead>
           <tr>
@@ -18,11 +35,15 @@ function App() {
           </tr>
         </thead>
         <tbody>
-          {firstFiveContacts.map((contactDetails) => {
+          {contactsArray.map((contactDetails) => {
             return (
               <tr>
                 <td>
-                  <img className="profilePicture" src={contactDetails.pictureUrl} alt="contact"></img>
+                  <img
+                    className="profilePicture"
+                    src={contactDetails.pictureUrl}
+                    alt="contact"
+                  ></img>
                 </td>
                 <td>
                   <p>{contactDetails.name}</p>
