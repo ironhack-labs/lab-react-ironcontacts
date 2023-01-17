e7ba0eb38f2b4b70f1bb264c948e480a59557ffc
SarahK-coder <117637388+SarahK-coder@users.noreply.github.com>
2023-01-17 18:16:37 +0100

    Iteration 3 complete

--- src/App.js
+++ src/App.js
@@ -5,17 +5,32 @@ import getContacts from './contacts.json';
 function App() {
   // Iteration 1
   const fiveContacts = getContacts.slice(0,5);
-  console.log(fiveContacts);
+  //console.log(fiveContacts);
   const [contacts, setContacts] = useState(fiveContacts);
+  // Iteration 3
+  const remainingContacts = getContacts.slice(5, getContacts.length);
+  //console.log(remainingContacts);
+
+  const addRandomContact = () => {
+    let randomRemainingContacts = Math.floor(Math.random() * remainingContacts.length);
+    let getRandomContact = remainingContacts.splice(randomRemainingContacts, 1);
+    console.log(randomRemainingContacts);
+    console.log(getRandomContact[0]);
+    setContacts([...contacts, getRandomContact[0]]);
+  }
 
   return (
     <div className="App">
+      {/* Iteration 3 */}
+      <button onClick={addRandomContact} className="btn">Add Random Contact</button>
+      {/* Iteration 1 */}
       <table>
         <thead>
           <tr>
             <th>Picture</th>
             <th>Name</th>
             <th>Popularity</th>
+            {/* Iteration 2 */}
             <th>Won an Oscar</th>
             <th>Won an Emmy</th>
           </tr>
@@ -32,6 +47,7 @@ function App() {
                 </td>
                 <td>{contact.name}</td>
                 <td>{contact.popularity}</td>
+                {/* Iteration 2 */}
                 <td>
                   {contact.wonOscar
                   ? '🏆'
