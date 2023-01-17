b3512bccfd43d8ca059f21f40b2d5b618b458aba
Thalita dos Reis <dosreistha@gmail.com>
2023-01-17 19:34:29 +0100

    iteration 4 and 5 done

--- src/App.js
+++ src/App.js
@@ -20,13 +20,31 @@ function App() {
     setContact([...contacts, newContact[0]]);
   }
 
+  // sorting by name
+  const sortByName = () => {setContact([...contacts].sort((a, b) => a.name.localeCompare(b.name)))}
 
+  // sorting by popularity
+  const sortByPopularity = () => {setContact([...contacts].sort((a, b) => b.popularity - a.popularity))}
+
+  // removing contacts
+  const deleteContact = (id) => {
+    const updatedContacts = [...contacts].filter(contact => contact.id !== id);
+    setContact(updatedContacts);
+  }
+
+  let titleMessage = contacts.length > 0 
+        ? <p>Number of IronContacts: {contacts.length}</p> 
+        : <p>There are no more IronContacts to be displayed</p>
 
   return (
     <div className="App">
       <h1>IronContacts</h1>
 
+      {titleMessage}
+
       <button onClick={addRandomContact}>Add Random Contact</button>
+      <button onClick={sortByPopularity}>Sort by popularity</button>
+      <button onClick={sortByName}>Sort by name</button>
 
       <table>
         <tbody>
@@ -36,6 +54,7 @@ function App() {
             <th>Populatiry</th>
             <th>Won Oscar</th>
             <th>Won Emmy</th>
+            <th>Action</th>
           </tr>
 
           {contacts.map((contact) => {
@@ -46,6 +65,7 @@ function App() {
                 <td>{(Math.ceil(contact.popularity) / 2).toFixed(2)}</td>
                 <td>{contact.wonOscar ? "🏆" : ""}</td>
                 <td>{contact.wonEmmy ? "\u2B50" : ""}</td>
+                <td><button onClick={ () => deleteContact(contact.id) }>Delete</button></td>
               </tr>
             );
           })}
@@ -53,6 +73,7 @@ function App() {
       </table>
     </div>
   );
+
 }
 
 export default App;
