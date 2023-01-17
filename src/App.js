3c80da847f67ebe265528cd0ea9b150f589ca141
Rumeli Paul <rumelipaul@gmail.com>
2023-01-17 20:18:57 +0100

    added styling

--- src/App.js
+++ src/App.js
@@ -7,89 +7,109 @@ function App() {
   const [listOfContacts, setListOfContacts] = useState(firstFive);
   //console.log(firstFive);
 
-const addContact = () =>{
+  const addContact = () => {
+    const newList = contactsFromJson.filter((contact) => {
+      return !listOfContacts.includes(contact);
+    });
+    const item = newList[Math.floor(Math.random() * newList.length)];
+
+    const newContact = [...listOfContacts];
+    newContact.push(item);
+
+  setListOfContacts(newContact);
 
 
-const newList = contactsFromJson.filter((contact)=>{
-return !listOfContacts.includes(contact)
-})
-const item = newList[Math.floor(Math.random()*newList.length)];
+    
+  };
+  ///////////////// Sort by Name ///////////////////////
 
-const newContact = [...listOfContacts];
-newContact.push(item);
+  const sortByName = () => {
+    const copyOfContacts = [...listOfContacts];
+    copyOfContacts.sort((a, b) => a.name.localeCompare(b.name));
+    setListOfContacts(copyOfContacts);
+  };
 
-setListOfContacts(newContact);
+  ////////////////// SOrt by pop /////////////////////////////
+  const sortByPop = () => {
+    const copyPop = [...listOfContacts];
+    copyPop.sort((a, b) => b.popularity - a.popularity);
+    setListOfContacts(copyPop);
+  };
+  /////////////// Delete Contact /////////////////////////////
+
+  const deleteContact = (id) => {
+    const newListOfContacts = listOfContacts.filter((contact) => {
+      return contact.id !== id;
+    });
+    setListOfContacts(newListOfContacts);
+  };
+  let titleMessage;
+  if (listOfContacts.length > 0) {
+    titleMessage = <h2> The current number of Contacts :{listOfContacts.length}</h2>;
+  } else {
+    titleMessage = <h2> Sorry!!! no Contacts </h2>;
+  }
 
 
- 
-}
-///////////////// Sort by Name ///////////////////////
-
-const sortByName = () =>{
-const copyOfContacts = [...listOfContacts];
-copyOfContacts.sort((a, b) => a.name.localeCompare(b.name));
-setListOfContacts(copyOfContacts);
-
-}
-
-////////////////// SOrt by pop /////////////////////////////
-const sortByPop = () =>{
-  const copyPop = [...listOfContacts];
-  copyPop.sort((a, b)=> b.popularity-a.popularity);
- setListOfContacts(copyPop);
-}
-/////////////// Delete Contact /////////////////////////////
-
-const deleteContact = (id) => {
-
-  const newListOfContacts = listOfContacts.filter((contact) => {
-    return contact.id !== id;
-  });
-  setListOfContacts(newListOfContacts);
- };
 
   return (
     <div className="App">
-      <h1>Contacts :</h1>
+    <h1>Contacts :</h1>
+    {titleMessage}
+    <h2> The total number of Contacts :{contactsFromJson.length}</h2>;
 
-      <div>
-        <button onClick={addContact} >Add Contact</button>
+    {listOfContacts.length === contactsFromJson.length && <h3> Sorry, cannot add any more !!</h3>}
+    
+  
+      <div className="Navbar">
+        
+      {listOfContacts.length !== contactsFromJson.length &&  <button onClick={addContact}>Add Random Contact</button> }
+       
+         
+       
+     
+          <button onClick={sortByName}>Sort By Name</button>
+     
+      
+          <button onClick={sortByPop}>Sort By Popularity</button>
+        
       </div>
-      <div>
-        <button onClick={sortByName} >Sort By name</button>
-      </div>
-      <div>
-        <button onClick={sortByPop} >Sort By Popularity</button>
-      </div>
-
-      <table>
+<div className="container">
+      <table className="Contact-table">
         <tr>
           <th>Picture</th>
           <th>Name</th>
           <th>Popularity</th>
-        </tr>
-      </table>
-      {listOfContacts.map((contactDetails) => {
-        return (
-          <div className="Contact">
-            <table className="Contact-table">
-              <tr>
-                <td>
-                  <img src={contactDetails.pictureUrl} alt="" />
-                </td>
-                <td>{contactDetails.name}</td>
-                <td> {contactDetails.popularity}</td>
-                <td>{ contactDetails.wonOscar && '🏆' }</td>
-                <td>{ contactDetails.wonEmmy && '🏆' }</td>
-                <td><button onClick={() => {
-                deleteContact(contactDetails.id);
-              }}>Delete</button></td>
-              </tr>
-            </table>
-          </div>
-        );
-      })}
+          <th>Won Oscar</th>
+          <th>Won Emmy </th>
+          <th>Actions </th>
 
+        </tr>
+
+        {listOfContacts.map((contactDetails) => {
+          return (
+            <tr>
+              <td>
+                <img src={contactDetails.pictureUrl} alt="" />
+              </td>
+              <td>{contactDetails.name}</td>
+              <td> {contactDetails.popularity}</td>
+              <td>{contactDetails.wonOscar && "🏆"}</td>
+              <td>{contactDetails.wonEmmy && "🏆"}</td>
+              <td>
+                <button className="delete-btn"
+                  onClick={() => {
+                    deleteContact(contactDetails.id);
+                  }}
+                >
+                  Delete
+                </button>
+              </td>
+            </tr>
+          );
+        })}
+      </table>
+      </div>
     </div>
   );
 }
