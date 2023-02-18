a4c66bf918ecc7e668304d0aef0ff9a66bc4dd28
Nadine Machmeier <info@nadinemachmeier.com>
2023-02-18 15:35:08 +0000

    iteration 1 done

--- src/App.js
+++ src/App.js
@@ -1,23 +1,39 @@
-import logo from './logo.svg';
-import './App.css';
+// import logo from './logo.svg';
+import contactsMovie from "./contacts.json";
+import { useState } from "react";
+
+import "./App.css";
 
 function App() {
+  const fiveContacts = contactsMovie.slice(0, 5);
+  const [contacts, setContacts] = useState(fiveContacts);
+  console.log("5 contacts:" + fiveContacts);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h2>Iron Contacts</h2>
+
+      <tr>
+        <th>Picture</th>
+        <th>Name</th>
+        <th>Popularity</th>
+      </tr>
+
+      {fiveContacts.map((contacts) => {
+        return (
+          <div key={contacts.id}>
+            <table>
+              <tr>
+                <td>
+                  <img src={contacts.pictureUrl} alt="image" />
+                </td>
+                <td>{contacts.name}</td>
+                <td>{contacts.popularity}</td>
+              </tr>
+            </table>
+          </div>
+        );
+      })}
     </div>
   );
 }
