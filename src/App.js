12fff3ea2c3c68d36ac839dce11f93d9dabda046
CameronStClair95 <cameronaitcheson@gmail.com>
2023-02-18 15:47:05 +0000

    stuck

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
   const [contactList, updateContactList] = useState(contacts.slice(0,5));
 console.log(contactList)
 const newContactList = contactList.map((contact) => {
-  return(
+return(
     <tr>
             <td> 
               <img className='profilePicture' src={contact.pictureUrl} alt= 'profilePicture' /> 
@@ -20,9 +20,28 @@ const newContactList = contactList.map((contact) => {
 
   )
 })
+
+// updateContactList(newContactList)
+
+function addRandomContact(){
+
+  const remainingContacts = contacts.filter(
+  (contact) => {
+    return contact.id !== newContactList.id
+  }
+  )
+  const randomContact = contacts[Math.floor(Math.random() * remainingContacts.length)];
+  newContactList.push(randomContact)
+  
+console.log(newContactList)
+
+  // updateContactList(randomContact)
+
+}
   return (
     <div className="App">
       <h1>Iron Contact</h1>
+      <button onClick={addRandomContact()}> Add Random Contact</button>
       <table>
         <thead>
           <tr>
