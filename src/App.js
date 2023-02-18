26435d03a0b4760463a0a5d1dd8a8b98d0241e3c
valentine974 <valentine.bathany@hotmail.fr>
2023-02-18 17:54:28 +0100

    solved lab

--- src/App.js
+++ src/App.js
@@ -3,8 +3,52 @@ import contacts from "./contacts.json";
 import { useState } from "react";
 
 function App() {
-  const [contactList, updateContactList] = useState(contacts.slice(0,5));
-console.log(contactList)
+  const [contactList, updateContactList] = useState(contacts.slice(0,5)); 
+
+  
+
+  function addRandomContact(){
+
+    const remainingContacts = contacts.filter(
+    (contact) => {
+      return contact.id !== newContactList.id
+    }
+    )
+    const randomContact = contacts[Math.floor(Math.random() * remainingContacts.length)];
+    // updateContactList(contactList.push(randomContact)) 
+
+    const displayRandomContact =[...contactList,randomContact]
+ 
+    updateContactList(displayRandomContact) 
+   
+  
+  }
+
+  function sortPopularity (){
+    const sortedPopContacts = contactList.slice().sort((a,b)=>{
+      return b.popularity-a.popularity
+    }) 
+    updateContactList(sortedPopContacts)
+    console.log(contactList)
+  }
+
+  function sortName (){
+    const sortedNameContacts = contactList.slice().sort((a,b)=>{
+      return a.name.localeCompare(b.name)
+    })
+    console.log(sortedNameContacts)
+
+    updateContactList(sortedNameContacts)
+  }
+
+  function deleteContact(contactId) {
+    const deleteContactList = contactList.slice().filter((contact)=>{
+      return contact.id !== contactId
+    })
+    updateContactList(deleteContactList)
+
+  }
+  
 const newContactList = contactList.map((contact) => {
 return(
     <tr>
@@ -12,42 +56,33 @@ return(
               <img className='profilePicture' src={contact.pictureUrl} alt= 'profilePicture' /> 
             </td>
             <td> {contact.name}  </td>
-            <td> {contact.popularity} </td>
+            <td> {contact.popularity} </td> 
+            <td>{contact.wonOscar ? <span role="img" aria-label="trophy">🏆</span> : null}</td>
+            <td>{contact.wonEmmy ? <span role="img" aria-label="trophy">🏆</span> : null}</td>
+    
             <td>
-            <button> Delete</button>
+            <button onClick={()=>deleteContact(contact.id)}> Delete</button>
             </td>
           </tr>
 
   )
 })
 
-// updateContactList(newContactList)
 
-function addRandomContact(){
-
-  const remainingContacts = contacts.filter(
-  (contact) => {
-    return contact.id !== newContactList.id
-  }
-  )
-  const randomContact = contacts[Math.floor(Math.random() * remainingContacts.length)];
-  newContactList.push(randomContact)
-  
-console.log(newContactList)
-
-  // updateContactList(randomContact)
-
-}
   return (
     <div className="App">
       <h1>Iron Contact</h1>
-      <button onClick={addRandomContact()}> Add Random Contact</button>
+      <button onClick={()=>addRandomContact()}> Add Random Contact</button>
+      <button onClick={()=>sortPopularity()}> Sort by popularity</button>
+      <button onClick={()=>sortName()}> Sort by name</button>
       <table>
         <thead>
           <tr>
             <th  >Picture</th>
             <th>Name</th>
             <th>Popularity</th>
+            <th>Won Oscar</th>
+            <th>Won Emmy</th>
           </tr>
         </thead>
         <tbody>
