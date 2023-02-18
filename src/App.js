0307131f49808747469e1574e29060c231c30f83
artweise <art2wsxweise7ujm@gmail.com>
2023-02-18 17:41:00 +0200

    1 uteration completed

--- src/App.js
+++ src/App.js
@@ -1,23 +1,34 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import "./App.css";
+import contacts from "./contacts.json";
 
 function App() {
+  const [contactsData, setContacts] = useState(contacts.slice(0, 5));
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>IronContacts</h1>
+      <div className="table">
+        <table>
+          <thead>
+            <th>Picture</th>
+            <th>Name</th>
+            <th>Popularity</th>
+          </thead>
+          <tbody>
+            {contactsData.map((contact) => {
+              return (
+                <tr key={contact.id}>
+                  <td>
+                    <img src={contact.pictureUrl} alt="" />
+                  </td>
+                  <td>{contact.name}</td>
+                  <td>{contact.popularity}</td>
+                </tr>
+              );
+            })}
+          </tbody>
+        </table>
+      </div>
     </div>
   );
 }
