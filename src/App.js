97d5b836132b5fd6a5c761b55cdbaa97ada34e7a
Dapato2 <108306801+Dapato2@users.noreply.github.com>
2023-02-17 19:12:42 -0600

    iteration 3

--- src/App.js
+++ src/App.js
@@ -1,25 +1,64 @@
-import logo from './logo.svg';
 import './App.css';
-
+import { useState } from 'react';
+import  contacts from   './contacts.json'
 function App() {
+
+  const conFive = contacts.slice(0,5)
+
+  const [contactInfo, setContactInfo] = useState(conFive)
+
+  const addContact = () => {
+    const random = contacts[Math.floor(Math.random()* contacts.length)]
+    const newList = [...contactInfo]
+    newList.push(random)
+    setContactInfo(newList)
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div>
+      <table>
+    <thead>
+        <tr>
+            <th colspan="2">Contacts</th>
+        </tr>
+    </thead>
+    
+        <tr>
+            <td>Name</td>
+            <td>Picture</td>
+            <td>Popularity</td>
+            <td>Won an oscar</td>
+            <td>Won an Emmy</td>
+        </tr>
+    <tbody>
+    {
+              contactInfo.map((contact, contact_index) => (
+                <tr key={contact_index}>
+                  <td><img src={contact.pictureUrl} height="180" width="120"/></td>
+                  <td>{contact.name}</td>
+                  <td>{contact.popularity.toFixed(2)}</td>
+                  <td>
+                  {contact.wonOscar && <p>🏆</p>}
+                  {!contact.wonOscar && <p>No</p>}
+                  </td>
+                  <td>
+                  {contact.wonEmmy && <p>🏆</p>}
+                  {!contact.wonEmmy && <p>No</p>}
+                  </td>
+                  <td>
+              
+                  </td>
+                </tr>
+              ))
+            }
+    </tbody>
+</table>
+      </div>
+      <button onClick={addContact}>ADD</button>
     </div>
   );
 }
 
 export default App;
+
