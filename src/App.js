d48d43f6cfb865cdbd1ebb2cfa557e46c30d92f6
AzizaHalder <aziza.halder@sap.com>
2023-02-18 18:23:19 +0100

    Begin iteration 3

--- src/App.js
+++ src/App.js
@@ -4,9 +4,36 @@ import contacts from "./contacts.json";
 
 function App() {
   const [contactsData, setContacts] = useState(contacts.slice(0, 5));
+
+  // function random() {
+  const getRandom = (newContact) => {
+    const newArray = [...contacts];
+    const randomIndex = Math.floor(Math.random() * newArray.length);
+    const randomContact = contacts[randomIndex];
+    newArray.slice(0, 5);
+    // newArray.push(randomContact);
+    // newArray.splice(0, 5, randomIndex);
+    // newArray.slice(0, 6);
+    console.log("NewArray", newArray);
+    console.log("Random Contact", randomContact);
+    // setContacts(newArray.splice(5, 0, randomIndex));
+    setContacts([randomContact]);
+  };
+
+  // }
+
+  // function getRandom() {
+  //   const randomContact = contacts[Math.floor(Math.random()) * contacts.length];
+  //   console.log(randomContact);
+  //   return randomContact;
+
+  //   setContacts(randomContact);
+  // }
+
   return (
     <div className="App">
       <h1>IronContacts</h1>
+      <button onClick={getRandom}>Add Random Contact</button>
       <div className="table">
         <table>
           <thead>
