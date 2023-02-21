6810348c5a9e2a97ed462cfe89d68aaea2bb03aa
HenrikH <henrik.hellem@live.com>
2023-02-21 16:08:42 +0100

    Iteration 3 plus fancy btn

--- src/App.js
+++ src/App.js
@@ -1,9 +1,22 @@
 import "./App.css";
-import contacts from "./contacts.json";
+import React, { useState } from "react";
+import contactsDataJSON from "./contacts.json";
 
 function App() {
+  let fiveFirstContacts = contactsDataJSON.slice(0, 5);
+  const [contacts, setContacts] = useState(fiveFirstContacts);
+
+
+  const addRandomContact = () => {
+    const notDisplayedContacts = contactsDataJSON.filter((item => !contacts.includes(item)))
+    const randomContact = notDisplayedContacts[Math.floor(Math.random()*notDisplayedContacts.length)]
+
+    setContacts([...contacts, randomContact])
+  };
+
   return (
     <div className="App">
+      <button className="glow-on-hover" onClick={addRandomContact}>Add Contact</button>
       <table data-table-theme="dark zebra">
         <thead>
           <tr>
