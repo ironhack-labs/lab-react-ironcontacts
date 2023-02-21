2faa1be24be356154ae3794855eeef2f3c1954ed
Indrajeet Patil <patilindrajeet.science@gmail.com>
2023-02-21 15:16:14 +0100

    sorting feature

--- src/App.js
+++ src/App.js
@@ -4,13 +4,7 @@ import ContactList from "./components/ContactList";
 function App() {
   return (
     <div className="App">
-      <h1>IronContacts</h1>
 
-      <div className="buttons">
-        <button>Add Random Contact</button>
-        <button>Sort by Name</button>
-        <button>Sort by Popularity</button>
-      </div>
 
       <ContactList />
     </div>
--- src/components/ContactList.js
+++ src/components/ContactList.js
@@ -1,34 +1,58 @@
 import { useState } from "react";
-import contacts from "../contacts.json";
+import contactsFromJSON from "../contacts.json";
 import ContactRow from "../components/ContactRow";
 
-let contactsCopy = [...contacts];
+function ContactList() {
+  const [contacts, setContacts] = useState(contactsFromJSON);
 
-function contactList() {
+  const sortContacts = () => {
+    let sortedContacts = [...contacts];
+    sortedContacts.sort((a, b) => {
+      if (a.name < b.name) {
+        return -1;
+      } else if (a.name > b.name) {
+        return 1;
+      } else {
+        return 0;
+      }
+    });
+    setContacts(sortedContacts);
+  };
+
   return (
-    <table>
-      <thead>
-        <tr>
-          <th>Picture</th>
-          <th>Name</th>
-          <th>Popularity</th>
-          <th>Won Oscar</th>
-          <th>Won Emmy</th>
-          <th>Actions</th>
-        </tr>
-      </thead>
-      <tbody>
-        {contactsCopy.map((contact) => {
-          return (
-            <ContactRow
-              key={contact.id}
-              contact={contact}
-            />
-          );
-        })}
-      </tbody>
-    </table>
+    <div className="ContactsTable">
+      <h1>IronContacts</h1>
+
+      <div className="buttons">
+        <button>Add Random Contact</button>
+        <button onClick={sortContacts}>Sort by Name</button>
+        <button>Sort by Popularity</button>
+      </div>
+
+      <table>
+        <thead>
+          <tr>
+            <th>Picture</th>
+            <th>Name</th>
+            <th>Popularity</th>
+            <th>Won Oscar</th>
+            <th>Won Emmy</th>
+            <th>Actions</th>
+          </tr>
+        </thead>
+        <tbody>
+          {contacts.map((contact) => {
+            return (
+              <ContactRow
+                key={contact.id}
+                contact={contact}
+              />
+            );
+          })}
+        </tbody>
+      </table>
+    </div>
   );
 }
 
-export default contactList;
+export default ContactList;
