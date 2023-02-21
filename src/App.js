9d838031a49b29738de19e3ef494e19546ec13a4
Indrajeet Patil <patilindrajeet.science@gmail.com>
2023-02-21 14:28:14 +0100

    initial list

--- src/App.js
+++ src/App.js
@@ -1,23 +1,41 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import "./App.css";
+import contacts from "./contacts.json";
+import ContactRow from "./ContactRow";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>IronContacts</h1>
+
+      <div className="buttons">
+        <button>Add Random Contact</button>
+        <button>Sort by Name</button>
+        <button>Sort by Popularity</button>
+      </div>
+
+      <table>
+        <thead>
+          <tr>
+            <th>Picture</th>
+            <th>Name</th>
+            <th>Popularity</th>
+            <th>Won Oscar</th>
+            <th>Won Emmy</th>
+            <th>Actions</th>
+          </tr>
+        </thead>
+        <tbody>
+          {contacts.map((contact) => {
+            return (
+              <ContactRow
+                key={contact._id}
+                contact={contact}
+              />
+            );
+          })}
+        </tbody>
+      </table>
     </div>
   );
 }
---
+++ src/ContactRow.js
@@ -0,0 +1,23 @@
+function ContactRow(props) {
+  const { contact } = props;
+
+  return (
+    <tr>
+      <td>
+        <img
+          src={contact.pictureUrl}
+          alt={contact.name}
+        ></img>
+      </td>
+      <td>{contact.name}</td>
+      <td>{contact.popularity.toLocaleString("en-US", { maximumFractionDigits: 2 })}</td>
+      <td>{contact.wonOscar ? "🏆" : ""}</td>
+      <td>{contact.wonEmmy ? "🌟" : ""}</td>
+      <td>
+        <button>Delete</button>
+      </td>
+    </tr>
+  );
+}
+
+export default ContactRow;
