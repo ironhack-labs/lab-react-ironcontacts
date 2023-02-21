145a339f3d7cafe0418e6a60279d3d7e1b3c51ea
Tobias Krauss <tobias.tk.krauss@gmail.com>
2023-02-21 17:19:50 +0100

    iterations 4 and 5

--- src/App.js
+++ src/App.js
@@ -1,33 +1,95 @@
 import './App.css';
 import contactsJSON from "./contacts.json"
+import { useState } from "react";
 
 function App() {
-  const contacts = contactsJSON.slice(28, 33)
+  const [contacts, setContacts] = useState(contactsJSON.slice(0, 5) )
+  const [order, setOrder] = useState("DSC")
+  
+ /*  const randomContact = contactsJSON[Math.floor(Math.random() * contactsJSON.length)]
+  const addRandomContact = (contact) => {
+        if (randomContact.id !== contactId)
+    }
+    setContacts(randomContact.id)
+} */
+
+
+  const sorting = (col) => {
+    if(order === "DSC"){
+      const sorted = [...contacts].sort((a,b)=>
+      a[col].toLowerCase() < b[col].toLowerCase() ? 1 : -1
+      );
+      setContacts(sorted)
+      setOrder("ASC")
+    }
+      if(order === "ASC"){
+        const sorted = [...contacts].sort((a,b)=>
+        a[col].toLowerCase() > b[col].toLowerCase() ? 1 : -1
+        );
+        setContacts(sorted)
+        setOrder("DSC")  
+    }
+  }
+
+  const popSorting = (col) => {
+    if(order === "DSC"){
+      const sorted = [...contacts].sort((a,b)=>
+      a[col] < b[col] ? 1 : -1
+      );
+      setContacts(sorted)
+      setOrder("ASC")
+    }
+      if(order === "ASC"){
+        const sorted = [...contacts].sort((a,b)=>
+        a[col] > b[col] ? 1 : -1
+        );
+        setContacts(sorted)
+        setOrder("DSC")  
+    }
+  }
+
+
+  const deleteContact = (contactId) => {
+    const filteredContacts = contacts.filter(contact => {
+        return contact.id !== contactId
+    })
+    setContacts(filteredContacts)
+}
 
   return (
     <div className="App">
       <h1>Ironcontacts</h1>
+
+      <button onClick={() => sorting("name")}>Sort by Name</button>
+      <button onClick={() => popSorting("popularity")}>Sort by Popularity</button>
+
       <div className='table'>
       <table className='table-outside'>
+        <thead>
         <tr>
           <th>Picture</th>
           <th>Name</th>
           <th>Popularity</th>
           <th>Won Oscar</th>
           <th>Won Emmy</th>
+          <th>Actions</th>
         </tr>
+        </thead>
       </table>
       {contacts.map(contact => {
         return (
 
           <table className='table-inside'>
-            <tr>
+            <tbody>
+            <tr key={contacts.id}>
               <td> <img src={contact.pictureUrl} alt="Picture" /></td>
               <td>{contact.name}</td>
               <td>{(contact.popularity).toFixed(2)}</td>
               <td>{contact.wonOscar === true && <p>🏆</p>}</td>
               <td>{contact.wonEmmy === true && <p>🏆</p>}</td>
+              <td><button onClick={() => deleteContact(contact.id)} className="btn-delete">Delete</button></td>
             </tr>
+            </tbody>
 
           </table>)
 
