1d80b948271f4bebf2ae41c25004c29bee6c2db1
Pauline <sisal.dynamo-0r@icloud.com>
2023-02-21 23:50:45 +0100

    iteration 1 - 3

--- src/App.js
+++ src/App.js
@@ -1,7 +1,50 @@
 import "./App.css";
+import contactsList from "./contacts.json";
+import { useState } from "react";
 
 function App() {
+  const [contacts, setContacts] = useState(contactsList.slice(0, 5));
 
-  return <div className="App"></div>;
+  function addRandomContact() {
+    const newContact = contactsList[Math.floor(Math.random() * contactsList.length)]
+    if (!contacts.includes(newContact)) {
+      setContacts([newContact, ...contacts]);
+    }
+  }
+  
+  return (
+    <div className="App">
+      <h1>Ironcontacts</h1>
+      <button onClick={addRandomContact}>Add random contact</button>
+      <div>
+      <table>
+        <thead>
+        <tr>
+          <th>Picture</th>
+          <th>Name</th>
+          <th>Popularity</th>
+          <th>won Oscar</th>
+          <th>won Emmy</th>
+        </tr>
+        </thead>
+        <tbody>
+          {contacts.map((contacts) => {
+            return (
+              <tr key={contacts.id} className="table">
+                <td>
+                  <img src={contacts.pictureUrl} alt={contacts.name} />
+                </td>
+                <td>{contacts.name}</td>
+                <td>{contacts.popularity}</td>
+                <td>{contacts.wonOscar ? "🏆" : ""}</td>
+                <td>{contacts.wonEmmy ? "🏆" : ""}</td>
+              </tr>
+            );
+          })}
+          </tbody>
+      </table>
+      </div>
+    </div>
+  );
 }
 export default App;
