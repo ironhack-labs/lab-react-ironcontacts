6286656ff3a4213fe8c2641fc3740b1dc1da0824
HenrikH <henrik.hellem@live.com>
2023-02-21 14:51:30 +0100

    Add CSS to make table readable, correct tables

--- src/App.js
+++ src/App.js
@@ -4,22 +4,26 @@ import contacts from "./contacts.json";
 function App() {
   return (
     <div className="App">
-      <table>
-        <tr>
-          <th>Picture</th>
-          <th>Name</th>
-          <th>Popularity</th>
-          <th>Have won an Oscar</th>
-          <th>Have won an Emmy</th>
-        </tr>
+      <table data-table-theme="dark zebra">
+        <thead>
+          <tr>
+            <th>Picture</th>
+            <th>Name</th>
+            <th>Popularity</th>
+            <th>Have won an Oscar</th>
+            <th>Have won an Emmy</th>
+          </tr>
+        </thead>
         {contacts.map((contact) => {
           return (
             <tr>
-              <th><img src={contact.pictureUrl} alt="" /></th>
+              <th>
+                <img src={contact.pictureUrl} alt="" />
+              </th>
               <th>{contact.name}</th>
               <th>{contact.popularity}</th>
-              <th>{(contact.wonOscar) ? "🏆" : null}</th>
-              <th>{(contact.wonEmmy) ? "🏆" : null}</th>
+              <th>{contact.wonOscar ? "🏆" : null}</th>
+              <th>{contact.wonEmmy ? "🏆" : null}</th>
             </tr>
           );
         })}
