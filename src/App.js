a58750ea54a493fbc580e55373e9895fbeea19c1
ArturoRubin <116040655+ArturoRubin@users.noreply.github.com>
2023-02-27 21:07:39 -0600

    finished lab

--- src/App.js
+++ src/App.js
@@ -1,25 +1,10 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+
+import "antd/dist/reset.css";
+import Celebrities from "./components/Celebrities";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  return <Celebrities />;
 }
 
 export default App;
