4949ade5ba15f8c02389ab538ccc91d0f675dc9d
romi <romi.varnier@gmail.com>
2023-02-28 17:55:31 +0100

    Iteration 5 | Remove Contacts

--- src/App.js
+++ src/App.js
@@ -2,21 +2,57 @@ import './index'
 import React, { useState } from 'react'
 import contactsDB from './contacts.json'
 // import Container from '../src/components/Container'
+const unusedContacts = contactsDB.slice(5)
 
 const App = () => {
   const [contacts, setContacts] = useState(contactsDB.slice(0, 5))
   const handleAddContact = () => {
-    const randomContact =
-      contactsDB[Math.floor(Math.random() * contactsDB.length)]
+    if (unusedContacts.length === 0) return
+
+    const randomContact = unusedContacts.splice(
+      Math.floor(Math.random() * unusedContacts.length),
+      1
+    )[0] //もしも[0]が無かったらrandomContactは[{…}]なのでそのままアクセスできない
+
+    // console.log(randomContact)
     const copy = [...contacts]
     copy.push(randomContact)
     setContacts(copy)
   }
+  const handleSortPopularity = () => {
+    const copy = [...contacts]
+    copy.sort((a, b) => b.popularity - a.popularity)
+    setContacts(copy)
+  }
+  const handleSortName = (contact) => {
+    const copy = [...contacts]
+    copy.sort((a, b) => {
+      a = a.name.toLowerCase()
+      b = b.name.toLowerCase()
+      if (a < b) {
+        return -1
+      } else if (a > b) {
+        return 1
+      }
+      return 0
+    })
+    setContacts(copy)
+  }
+
+  const handleDelete = (id) => {
+    const newList = contacts.filter((contact) => contact.id !== id)
+    setContacts(newList)
+  }
+
   return (
     <section>
-      <div class="title">
+      <div className="title">
         <h1>IronContacts</h1>
-        <button onClick={handleAddContact}>Add Random Contact</button>
+        <div className="button">
+          <button onClick={handleAddContact}>Add id Contact</button>
+          <button onClick={handleSortPopularity}>Sort by popularity</button>
+          <button onClick={handleSortName}>Sort by name</button>
+        </div>
       </div>
       <table>
         <thead>
@@ -36,31 +72,32 @@ const App = () => {
             <th>
               <h2>Won Emmy</h2>
             </th>
+            <th>
+              <h2>Actions</h2>
+            </th>
           </tr>
         </thead>
-        {contacts.map((contact) => {
-          return (
-            <tbody>
-              <tr>
+        <tbody>
+          {contacts.map((contact) => {
+            return (
+              // deleteボタンの直前の親につける
+              <tr key={contact.id}>
                 <td>
                   <img src={contact.pictureUrl} alt={contact.name} />
                 </td>
+                <td>{contact.name}</td>
+                <td>{Math.floor(contact.popularity * 100) / 100}</td>
+                <td>{contact.wonOscar && '🏆'}</td>
+                <td>{contact.wonEmmy && '🏆'}</td>
                 <td>
-                  <p>{contact.name}</p>
-                </td>
-                <td>
-                  <p>{Math.floor(contact.popularity * 100) / 100}</p>
-                </td>
-                <td>
-                  <p>{contact.wonOscar && '🏆'}</p>
-                </td>
-                <td>
-                  <p>{contact.wonEmmy && '🏆'}</p>
+                  <button onClick={() => handleDelete(contact.id)}>
+                    Delete
+                  </button>
                 </td>
               </tr>
-            </tbody>
-          )
-        })}
+            )
+          })}
+        </tbody>
       </table>
     </section>
   )
