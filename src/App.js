785d152003af4f49200f6f5508a77959a2867f5d
Cristina Chaparro <cricbau@gmail.com>
2023-02-28 16:57:21 +0100

    iteration 1 done

--- src/App.js
+++ src/App.js
@@ -1,23 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import contactData from "./contacts.json";
+
+const contacts = contactData.slice(0, 5);
+console.log(contacts);
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {contacts.map((eachContact) => {
+        return (
+          <div className="card">
+            <img
+              src={eachContact.pictureUrl}
+              alt={eachContact.name}
+              height={"350px"}
+            />
+            <br />
+            <h3>{eachContact.name}</h3>
+            <p>Popularity: {eachContact.popularity}</p>
+          </div>
+        );
+      })}
     </div>
   );
 }
