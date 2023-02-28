f221504c360094598920faccc102199b9ebd633a
Mehdi Eddira <m.eddira75@outlook.fr>
2023-02-28 18:09:03 +0100

    lab done

--- src/App.js
+++ src/App.js
@@ -1,23 +1,79 @@
-import logo from './logo.svg';
-import './App.css';
+// src/App.js
+import Allcontacts from "./contacts.json";
+
+import "./App.css";
+import { useState } from "react";
 
 function App() {
+  const [contacts, setContacts] = useState(Allcontacts.slice(0, 5));
+
+  const handleAddContact = () => {
+    const randomContact =
+      Allcontacts[Math.floor(Math.random() * Allcontacts.length)];
+    const copy = [...Allcontacts];
+
+    copy.push(randomContact);
+    setContacts(copy);
+  };
+
+  const handleSortContactName = () => {
+    const copy = [...Allcontacts];
+    copy.sort((a, b) => (a.name > b.name ? -1 : 1));
+    setContacts(copy);
+  };
+
+  const handleSortContactPopularity = () => {
+    const copy = [...Allcontacts];
+    copy.sort((a, b) => b.popularity - a.popularity);
+    setContacts(copy);
+  };
+
+  const handleDelete = (id) => {
+    const newList = contacts.filter((contact) => contact.id !== id);
+    setContacts(newList);
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <table>
+        <div class="buttons">
+          <button onClick={handleAddContact}>Add Random contact</button>
+          <button onClick={handleSortContactName}>Sort by name</button>
+          <button onClick={handleSortContactPopularity}>
+            Sort by popularity
+          </button>
+        </div>
+        <thead>
+          <tr>
+            <th>Picture</th>
+            <th>Name</th>
+            <th>Popularity</th>
+            <th>Won an Oscar</th>
+            <th>Won an Emmy</th>
+            <th>Actions</th>
+          </tr>
+        </thead>
+        <tbody>
+          {contacts.map((contact) => {
+            return (
+              <tr>
+                <td>
+                  <img src={contact.pictureUrl} alt="{contact.name}"></img>
+                </td>
+                <td>{contact.name}</td>
+                <td>{contact.popularity}</td>
+                <td>{contact.wonOscar && "🏆"}</td>
+                <td>{contact.wonEmmy && "🏆"}</td>
+                <td>
+                  <button onClick={() => handleDelete(contact.id)}>
+                    Delete
+                  </button>
+                </td>
+              </tr>
+            );
+          })}
+        </tbody>
+      </table>
     </div>
   );
 }
