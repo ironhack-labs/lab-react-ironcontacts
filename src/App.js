d8cc2986641d019e61c9013087e3c71acbdf1166
Carlos Sosa <csosa@simpletg.com>
2023-03-04 16:28:23 -0500

    iteration 4 and 5 - done

--- src/App.js
+++ src/App.js
@@ -27,6 +27,34 @@ function App() {
     setActorsList(newContactList);
   };
 
+  const sortTable = (field) => {
+    const actors = [...actorsList];
+
+    actors.sort((a, b) => {
+      if (field === 'popularity') return b.popularity - a.popularity;
+      if (field === 'name') {
+        const nameA = a.name.toUpperCase();
+        const nameB = b.name.toUpperCase();
+        if (nameA < nameB) return -1;
+        if (nameA > nameB) return 1;
+      }
+
+      return 0;
+    });
+
+    setActorsList(actors);
+  };
+
+  const deleteContact = (id) => {
+    const contacts = [...actorsList];
+
+    const contactIndex = contacts.findIndex((el) => el.id === id);
+
+    contacts.splice(contactIndex, 1);
+
+    setActorsList(contacts);
+  };
+
   const tableRows = actorsList.map((contact) => {
     return (
       <tr key={contact.id}>
@@ -34,7 +62,7 @@ function App() {
           <img src={contact.pictureUrl} alt="" style={{ height: '140px' }} />
         </td>
         <td>{contact.name}</td>
-        <td>{contact.popularity}</td>
+        <td>{contact.popularity.toFixed(2)}</td>
         <td>
           {contact.wonOscar && (
             <img src="trophy.png" alt="" style={{ height: '40px' }} />
@@ -45,6 +73,15 @@ function App() {
             <img src="trophy.png" alt="" style={{ height: '40px' }} />
           )}
         </td>
+        <td>
+          <button
+            onClick={() => {
+              deleteContact(contact.id);
+            }}
+          >
+            Delete
+          </button>
+        </td>
       </tr>
     );
   });
@@ -52,6 +89,20 @@ function App() {
   return (
     <div>
       <button onClick={addActor}>Get Random Actor</button>
+      <button
+        onClick={() => {
+          sortTable('popularity');
+        }}
+      >
+        Sort by popularity
+      </button>
+      <button
+        onClick={() => {
+          sortTable('name');
+        }}
+      >
+        Sort by name
+      </button>
       <table>
         <thead>
           <tr>
@@ -60,6 +111,7 @@ function App() {
             <th>Popularity</th>
             <th>Won Oscar</th>
             <th>Won Emmy</th>
+            <th>Actions</th>
           </tr>
         </thead>
         <tbody>{tableRows}</tbody>
