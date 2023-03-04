6f1af2bf09479cab0664e503c99e2c39aafe965d
Carlos Sosa <csosa@simpletg.com>
2023-03-04 15:51:22 -0500

    iteration 3 - done

--- src/App.js
+++ src/App.js
@@ -2,46 +2,67 @@ import './App.css';
 import contactData from './contacts.json';
 import React, { useState } from 'react';
 
+const contactList = contactData.splice(0, 5);
 function App() {
-  const [contact] = useState(() => {
-    const contactTable = contactData
-      .map((contact) => {
-        return (
-          <tr key={contact.id}>
-            <td>
-              <img src={contact.pictureUrl} alt="" Style="height:190px;" />
-            </td>
-            <td>{contact.name}</td>
-            <td>{contact.popularity}</td>
-            <td>
-              {contact.wonOscar && (
-                <img src="trophy.png" alt="" Style="height:40px" />
-              )}
-            </td>
-            <td>
-              {contact.wonEmmy && (
-                <img src="trophy.png" alt="" Style="height:40px" />
-              )}{' '}
-            </td>
-          </tr>
-        );
-      })
-      .slice(0, 5);
+  const [actorsList, setActorsList] = useState(contactList);
+  console.log({ actorsList });
+  const randomIndex = (max) => {
+    return Math.random() * (max - 0) + 0;
+  };
 
-    return contactTable;
+  const getRandomActor = () => {
+    const randomContact = contactData.splice(
+      randomIndex(contactData.length - 1),
+      1
+    );
+    // contactList.push(randomContact);
+    return randomContact[0];
+  };
+
+  const addActor = () => {
+    const newContactList = [...actorsList];
+    const newContact = getRandomActor();
+
+    newContactList.push(newContact);
+    setActorsList(newContactList);
+  };
+
+  const tableRows = actorsList.map((contact) => {
+    return (
+      <tr key={contact.id}>
+        <td>
+          <img src={contact.pictureUrl} alt="" style={{ height: '140px' }} />
+        </td>
+        <td>{contact.name}</td>
+        <td>{contact.popularity}</td>
+        <td>
+          {contact.wonOscar && (
+            <img src="trophy.png" alt="" style={{ height: '40px' }} />
+          )}
+        </td>
+        <td>
+          {contact.wonEmmy && (
+            <img src="trophy.png" alt="" style={{ height: '40px' }} />
+          )}
+        </td>
+      </tr>
+    );
   });
 
   return (
     <div>
+      <button onClick={addActor}>Get Random Actor</button>
       <table>
         <thead>
-          <th>Picture</th>
-          <th>Name</th>
-          <th>Popularity</th>
-          <th>Won Oscar</th>
-          <th>Won Emmy</th>
+          <tr>
+            <th>Picture</th>
+            <th>Name</th>
+            <th>Popularity</th>
+            <th>Won Oscar</th>
+            <th>Won Emmy</th>
+          </tr>
         </thead>
-        <tbody>{contact}</tbody>
+        <tbody>{tableRows}</tbody>
       </table>
     </div>
   );
