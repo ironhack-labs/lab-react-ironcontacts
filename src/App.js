80e736db53331c7dce92d311d30efc6408e9cd0b
Joao Victor Souto <jheibol1212@icloud.com>
2023-03-07 22:00:54 -0300

    lab finalzado

--- src/App.js
+++ src/App.js
@@ -1,13 +1,55 @@
 import "./App.css";
 import contacts from "./contacts.json";
 import ListaAtores from "./components/ListaAtores";
+import { useState } from 'react';
+// import AddAtor from "./components/AddAtor";
 
 
 function App() {
-  const lista = contacts.splice(0,5);
-  
+
+  const [lista, setLista] = useState(contacts.slice(0,5));
+  function addAtor(){
+    let atorSelecionado = contacts[Math.floor(Math.random() * contacts.length)]
+
+    const verificarDuplicidade = lista.find(ator => {
+      return ator.name === atorSelecionado.name;
+    });
+    if(!verificarDuplicidade){
+      const copy = [...lista]
+      copy.push(atorSelecionado)
+      setLista(copy)
+    }
+  }
+
+  function classificarPopularidade(){
+    const copy = [...lista]
+    copy.sort(function(a,b) {
+      return a.popularity > b.popularity ? -1 : a.popularity > b.popularity ? 1 : 0;
+    })
+    setLista(copy)
+  }
+  function classificarNome(){
+    const copy = [...lista]
+    copy.sort(function(a,b) {
+      return a.name < b.name ? -1 : a.name > b.name ? 1 : 0;
+    })
+    setLista(copy)
+  }
+  function deletar(id){
+    const copy = [...lista]
+    const deleteAtor = copy.filter(ator => {
+      return ator.id !== id;
+    });
+    setLista(deleteAtor)
+  }
+
+
+
   return <div className="App">
-  <ListaAtores lista={lista}/> 
+  <button onClick={addAtor}>Adiocionar Ator Aleatorio</button>
+  <button onClick={classificarPopularidade}>Classificar por Popularidade</button>
+  <button onClick={classificarNome}>Classificar por Nome</button>
+  <ListaAtores lista={lista} deletar={deletar}/> 
   </div>;
   
  
--- src/components/ListaAtores.js
+++ src/components/ListaAtores.js
@@ -1,4 +1,4 @@
-function ListaAtores({ lista }){
+function ListaAtores({ lista, deletar }){
     
     return (
     <table>
@@ -9,17 +9,20 @@ function ListaAtores({ lista }){
                 <th>Popularity</th>
                 <th>Won Oscar</th>
                 <th>Won Emmy</th>
+                <th>Delete</th>
             </tr>
         </thead>
         <tbody>
-        {lista.splice(0,5).map((ator, index) => {
-            console.log(ator.name)
-        return <tr key={index}>
+        {lista.map((ator) => {
+            
+        return <tr key={ator.id}>
+        
                     <td><img src={ator.pictureUrl} alt='Foto Ator'/></td>
                     <td>{ator.name}</td>
                     <td>{ator.popularity.toFixed(2)}</td>
-                    <td></td>
-                    <td></td>
+                    <td>{ator.wonOscar && <p>🏆</p>}</td>
+                    <td>{ator.wonEmmy && <p>🏆</p>}</td>
+                    <td><button onClick={() => deletar(ator.id)}>Deletar</button></td>
                 </tr>
         })}
         </tbody>
