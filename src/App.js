75597df459e8d70aa357038d53af9f093194b5a9
KARLALOPEZWDFTJAN2023 <karlacarbajalguillots@gmail.com>
2023-03-10 00:10:11 -0600

    avance lab

--- src/App.js
+++ src/App.js
@@ -1,23 +1,80 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import "./App.css";
+import contactsData from "./contacts.json"
 
 function App() {
+
+  const [contacts, setContacts] = useState(contactsData.slice(0, 5))
+
+  const addRandomContact = ()=>{
+    const randomContact = contactsData[Math.floor(Math.random()* contactsData.length)];
+
+    if(contacts.length === contactsData.length) return console.log("You cannot add more contacts");
+    if(contacts.some((contact)=>contact.id === randomContact.id)){
+      addRandomContact();
+    }else{
+      setContacts(contacts=>[...contacts, randomContact]);
+    }
+  }
+
+  const sortByPopularity = ()=>{
+    const sortedContacts = contacts.slice().sort((ele1, ele2)=>{
+      return ele2.popularity - ele1.popularity;
+    })
+    setContacts(sortedContacts);
+  }
+
+  const sortByName = ()=>{
+    const sortedContacts = contacts.slice().sort((ele1, ele2)=>{
+      if(ele1.name < ele2.name) {return -1}
+      if(ele1.name > ele2.name) {return 1}
+      return 0;
+    })
+    setContacts(sortedContacts);
+  }
+
+  const clickToDelete = (contactId)=>{
+    const filteredContacts = contacts.filter((contact)=>{
+      return contact.id !== contactId
+    })
+    setContacts(filteredContacts)
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>IronContacts</h1>
+      <nav className="navbar">
+        <button onClick={addRandomContact}>Add random contact</button>
+        <button onClick={sortByPopularity}>Sort by popularity</button>
+        <button onClick={sortByName}>Sort by name</button>
+      </nav>
+
+      <table className="styled-table">
+        <thead>
+          <tr>
+            <th>Picture</th>
+            <th>Name</th>
+            <th>Popularity</th>
+            <th>Won an Oscar</th>
+            <th>Won an Emmy</th>
+            <th>Actions</th>
+          </tr>
+        </thead>
+        <tbody>
+        {contacts.map((contact)=>{
+          return (
+            <tr key={contact.id}>
+              <td><img src={contact.pictureUrl} alt="contact-img"></img></td>
+              <td>{contact.name}</td>
+              <td>{contact.popularity}</td>
+              <td>{contact.wonOscar && "🏆"}</td>
+              <td>{contact.wonEmmy && "🏆"}</td>
+              <td><button className="deleteBtn" onClick={() => clickToDelete(contact.id)}>Delete 🗑</button></td>
+            </tr>
+          )
+        })}
+        </tbody>
+      </table>
     </div>
   );
 }
