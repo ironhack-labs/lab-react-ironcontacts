3fa3b8279f00d851808389cc13ee57cc69ae26cf
ManuelDs28 <ktireds2803@gmail.com>
2023-03-13 03:59:36 +0100

    adding some styles

--- src/components/Contacts/index.js
+++ src/components/Contacts/index.js
@@ -11,7 +11,7 @@ const ContactsTable = () => {
     if (remainingContacts.length > 0) {
       const randomIndex = Math.floor(Math.random() * remainingContacts.length);
       const randomContact = remainingContacts[randomIndex];
-      setContactsState([randomContact, ...contactsState ]);
+      setContactsState([randomContact, ...contactsState]);
       setRemainingContacts(
         remainingContacts.filter((_, i) => i !== randomIndex)
       );
@@ -19,27 +19,35 @@ const ContactsTable = () => {
   };
 
   const sortByName = () => {
-    setContactsState([...contactsState].sort((a, b) => a.name.localeCompare(b.name)));
+    setContactsState(
+      [...contactsState].sort((a, b) => a.name.localeCompare(b.name))
+    );
   };
-  
+
   const sortByPopularity = () => {
-    setContactsState([...contactsState].sort((a, b) => b.popularity - a.popularity));
+    setContactsState(
+      [...contactsState].sort((a, b) => b.popularity - a.popularity)
+    );
   };
   const deleteContact = (id) => {
     setContactsState(contactsState.filter((contact) => contact.id !== id));
-    setRemainingContacts([...remainingContacts, contactsState.find((contact) => contact.id === id)]);
+    setRemainingContacts([
+      ...remainingContacts,
+      contactsState.find((contact) => contact.id === id),
+    ]);
   };
 
   return (
     <div className="ContactsTable">
-    <div className="ButtonsContainer">
-      <button onClick={addRandomContact}>Add Random Contact</button>
-      <button onClick={sortByPopularity}>Sort By Popularity</button>
-      <button onClick={sortByName}>Sort By Name</button>
-    </div>
-      <table>
+      <div className="ButtonsContainer">
+        <button onClick={addRandomContact}>Add Random Contact</button>
+        <button onClick={sortByPopularity}>Sort By Popularity</button>
+        <button onClick={sortByName}>Sort By Name</button>
+      </div>
+<br></br>
+      <table className="table">
         <thead>
-          <tr>
+          <tr className="titles">
             <th>Picture</th>
             <th>Name</th>
             <th>Popularity</th>
@@ -47,17 +55,25 @@ const ContactsTable = () => {
             <th>Won Emmy</th>
           </tr>
         </thead>
+        <br></br>
         <tbody>
           {contactsState.map((contact) => (
             <tr key={contact.name}>
               <td>
                 <img src={contact.pictureUrl} alt={contact.name} />
+                </td>
+                <td>{contact.name}</td>
+                <td>{contact.popularity.toFixed(1)}</td>
+                <td>{contact.wonOscar ? "🏆" : "🔴"}</td>
+                <td>{contact.wonEmmy ? "🌟" : "🔴"}</td>
+                <td>
+                <button
+                  className="delete-btn"
+                  onClick={() => deleteContact(contact.id)}
+                >
+                  Delete
+                </button>
               </td>
-              <td>{contact.name}</td>
-              <td>{contact.popularity.toFixed(1)}</td>
-              <td>{contact.wonOscar ? "🏆" : "🔴"}</td>
-              <td>{contact.wonEmmy ? "🌟" : "🔴"}</td>
-              <td><button onClick={() => deleteContact(contact.id)}>Delete</button></td>
             </tr>
           ))}
         </tbody>
