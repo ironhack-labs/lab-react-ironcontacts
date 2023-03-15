4719a8d4792b12495d4c385c81195a16fdda6cb6
pianattero <mariapianattero@gmail.com>
2023-03-15 18:22:45 +0100

    feat: it 3 done

---
+++ src/components/Buttons/Buttons.jsx
@@ -0,0 +1,5 @@
+const Button = ({ onClick, text }) => {
+  return <button onClick={onClick}>{text}</button>;
+};
+
+export default Button;
--- src/components/ContactsTable/ContactsTable.jsx
+++ src/components/ContactsTable/ContactsTable.jsx
@@ -1,26 +1,47 @@
+import React from "react";
 import ContactsItem from "../ContactsItem/ContactsItem";
+import Button from "../Buttons/Buttons";
 import "./ContactsTable.css";
 import contacts from "../../contacts.json";
 
-const ContactsTable = () => {
-  return (
-    <table className="ContactsTable">
-      <thead>
-        <tr>
-          <th>Picture</th>
-          <th>Name</th>
-          <th>Popularity</th>
-          <th>Won an Oscar</th>
-          <th>Won an Emmy</th>
-        </tr>
-      </thead>
-      <tbody>
-        {contacts.map((contact) => {
-          return <ContactsItem {...contact} key={contact.id} />;
-        })}
-      </tbody>
-    </table>
-  );
-};
+class ContactsTable extends React.Component {
+  state = {
+    contacts: contacts.splice(0, 5),
+  };
+
+  addRandomContact = () => {
+    const randomContact = contacts.splice(0, 1);
+
+    this.setState((prevState) => ({
+      contacts: [randomContact[0], ...prevState.contacts],
+    }));
+  };
+
+  render() {
+    return (
+      <>
+        <Button onClick={this.addRandomContact} text="Add Random Contact" />
+
+        <table className="ContactsTable">
+          {console.log(contacts)}
+          <thead>
+            <tr>
+              <th>Picture</th>
+              <th>Name</th>
+              <th>Popularity</th>
+              <th>Won an Oscar</th>
+              <th>Won an Emmy</th>
+            </tr>
+          </thead>
+          <tbody>
+            {this.state.contacts.map((contact) => (
+              <ContactsItem {...contact} key={contact.id} />
+            ))}
+          </tbody>
+        </table>
+      </>
+    );
+  }
+}
 
 export default ContactsTable;
