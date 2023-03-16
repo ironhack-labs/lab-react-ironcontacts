b9118bc0b30c589ce69bc0a9f7d6b7fecd4817ba
Edson <eddyao1990@gmail.com>
2023-03-16 01:34:29 +0100

    done with error

--- src/App.js
+++ src/App.js
@@ -1,23 +1,59 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState } from "react";
+import contacts from "./contacts.json";
 
 function App() {
-  return (
+  let fiveContact = contacts.slice(0, 5)
+  const [data, setData]= useState(fiveContact)
+
+  function addContact() {
+    const randomContact = contacts[Math.floor(contacts.lenght * Math.random())];
+    setData([randomContact, ...data])
+  }
+
+  function sortByName() {
+    const sortedName = [...data].sort((a,b) => a.name.localeCompare(b.name));
+    setData(sortedName)
+  }
+
+  function sortByPopularity() {
+    const sortedPopularity = [...data].sort((a,b) => b.popularity > a.popularity ? 1 : -1)
+    setData(sortedPopularity);
+
+   
+  }
+    return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h2>IronContact</h2>
+      <button onClick={addContact}>Add Random Contact</button>
+      <button onClick={sortByName}>Sort by name</button>
+      <button onClick={sortByPopularity}>Sort by popularity</button> 
+      <table>
+
+      <tr>
+          <th>Picture</th>
+          <th>Name</th>
+          <th>Popularity</th>
+          <th>Won an Oscar</th>
+          <th>Won an Emmy</th>
+          <th>Actions</th>
+        </tr>
+
+        {data.map((Data) => (
+          <tr key={Data._id}>
+            <td><img src={Data.pictureUrl} alt="profilepic" /></td>
+            <td>{Data.name}</td>
+            <td>{Data.popularity.toFixed(2)}</td>
+            <td>{Data.wonOscar && <p>🏆</p>}</td>
+            <td>{Data.wonEmmy && <p>🏆</p>}</td>
+          </tr>
+     
+     )
+     )
+     }
+
+
+      </table>
     </div>
   );
 }
