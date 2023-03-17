bfc0bc0da5cc7d8e1174ced78e7f2d9ab5d1f007
Leandro Pereyra <leandro-pereyra@hotmail.es>
2023-03-17 19:02:02 +0100

    Iteration 3

--- src/components/table/Table.jsx
+++ src/components/table/Table.jsx
@@ -9,23 +9,43 @@ const Table = () => {
     setContacts(contactsJSON.slice(0, 5));
   }, []);
 
+  const handleAddRandom = () => {
+    const notRepeatedArr = [];
+
+    contactsJSON.forEach((contactJSON) => {
+      const findObject = contacts.some(
+        (contactState) => contactJSON === contactState
+      );
+
+      if (!findObject) notRepeatedArr.push(contactJSON);
+    });
+
+    const randomContact =
+      notRepeatedArr[Math.floor(Math.random() * notRepeatedArr.length)];
+
+    if (randomContact) setContacts((prev) => [...prev, randomContact]);
+  };
+
   return (
-    <table className="table-container">
-      <thead>
-        <tr>
-          <th>Image</th>
-          <th>name</th>
-          <th>popularity</th>
-          <th>Won Oscar</th>
-          <th>Won Emmy</th>
-        </tr>
-      </thead>
-      <tbody>
-        {contacts.map((contact) => (
-          <Row key={contact.id} contact={contact} />
-        ))}
-      </tbody>
-    </table>
+    <>
+      <button onClick={handleAddRandom}>Add Random Contact</button>
+      <table className="table-container">
+        <thead>
+          <tr>
+            <th>Image</th>
+            <th>name</th>
+            <th>popularity</th>
+            <th>Won Oscar</th>
+            <th>Won Emmy</th>
+          </tr>
+        </thead>
+        <tbody>
+          {contacts.map((contact) => (
+            <Row key={contact.id} contact={contact} />
+          ))}
+        </tbody>
+      </table>
+    </>
   );
 };
 
