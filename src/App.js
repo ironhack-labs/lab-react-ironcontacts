dff8a55427290a7e15ef2f43819df3ffc15ea172
Paloma Gladine <paloma.gladine@gmail.com>
2023-03-17 21:59:01 +0100

    Iteration 3 | Add New Random Contacts

--- src/App.js
+++ src/App.js
@@ -1,15 +1,13 @@
 import './App.css';
 import ContactList from './components/contacts/ContactList';
-import contactsJson from './contacts.json'
 
-const contacts = contactsJson.slice(0, 5)
 
 function App() {
   return (
     <div className="App">
-     <h1 className='font-extrabold text-5xl text-start mb-2 ml-2'>IronContacts</h1>
-      <ContactList contacts={contacts} />
-     
+      <h1 className='font-extrabold text-5xl text-start mb-2 ml-2'>IronContacts</h1>
+      <ContactList />
+
     </div>
   )
 }
--- src/components/contacts/ContactList.jsx
+++ src/components/contacts/ContactList.jsx
@@ -1,23 +1,50 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import ContactItem from './ContactItem'
+import contactsJson from '../../contacts.json'
 
 
-export default function ContactList({ contacts }) {
+
+export default function ContactList() {
+
+  const [contacts, setContacts] = useState([])
+  const [remainContacts, setRemainContacts] = useState([])
+
+  useEffect(() => {
+    setContacts(contactsJson.slice(0, 5))
+    setRemainContacts(contactsJson.slice(5))
+  }, [])
+
+
+  const handleAddClick = () => {
+    if (remainContacts.length !== 0) {
+      const randomNum = Math.floor(Math.random() * (remainContacts.length))
+      setContacts((prev) => [
+        ...prev,
+        remainContacts[randomNum]
+      ])
+      setRemainContacts((prev) => prev.filter((contact) => contact.id !== remainContacts[randomNum].id))
+      console.log(remainContacts);
+    }
+  }
+
 
   return (
-    <table className='table-auto mx-7'>
-      <thead>
-        <tr>
-          <th className='w-16'>Picture</th>
-          <th className='w-16'>Name</th>
-          <th className='w-16'>Popularity</th>
-          <th className='w-16'>Won Oscar</th>
-          <th className='w-16'>Won Emmy</th>
-        </tr>
-      </thead>
-      <tbody className='text-center'>
-        {contacts.map((contact) => (<ContactItem key={contact.id} {...contact} />))}
-      </tbody>
-    </table>
+    <>
+      <button className='border border-black py-1 px-5 bg-slate-200' onClick={handleAddClick}>Add Random Contact</button>
+      <table className='table-auto mx-7'>
+        <thead>
+          <tr>
+            <th className='w-16'>Picture</th>
+            <th className='w-16'>Name</th>
+            <th className='w-16'>Popularity</th>
+            <th className='w-16'>Won Oscar</th>
+            <th className='w-16'>Won Emmy</th>
+          </tr>
+        </thead>
+        <tbody className='text-center'>
+          {contacts.map((contact) => (<ContactItem key={contact.id} {...contact} />))}
+        </tbody>
+      </table>
+    </>
   )
 }
