3abf6680fbc73d4945fc4c90803f93f5236254e0
David Reguart <reguartdavid@gmail.com>
2023-03-17 23:19:43 +0100

    Iteration 1 => Done

--- src/App.js
+++ src/App.js
@@ -1,23 +1,49 @@
-import logo from './logo.svg';
 import './App.css';
+import contacts from "./contacts.json";
 
 function App() {
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App bg-gradient-to-bl from-blue-700 via-blue-800 to-gray-900 text-white overflow-x-hidden font-[Poppins]">
+      <div className="w-screen flex items-center flex-col mb-10">
+        <img src="../images/man_user.png" alt="User" className="w-1/6" />
+        <h1 className="font-bold text-5xl w-1/5 border-b-4 py-3 hover:border-8 hover:py-4 hover:rounded-full hover:border-white hover:shadow-indigo-300 hover:shadow-lg hover:cursor-pointer hover:bg-slate-400 hover:bg-opacity-10 transition ease-in-out hover:scale-110">IronContacts</h1>
+      </div>
+
+      <div className="flex justify-center">
+        <table className="w-2/4 text-sm text-gray-500 text-center">
+          <thead className="text-xs text-gray-400 uppercase bg-gray-700">
+            <tr>
+              <th scope="col" className="px-6 py-3">
+                Picture
+              </th>
+              <th scope="col" className="px-6 py-3">
+                Name
+              </th>
+              <th scope="col" className="px-6 py-3">
+                Popularity
+              </th>
+            </tr>
+          </thead>
+          <tbody>
+            {contacts.slice(0, 5).map((contact) =>
+              <tr key={contact.id} className="bg-gray-800 hover:bg-gray-600">
+                <th scope="row" className="flex items-center justify-center px-6 py-4 text-gray-900 whitespace-nowrap">
+                  <img className="w-20 h-30 rounded-lg" src={contact.pictureUrl} alt={`${contact.name}`} />
+                </th>
+                <td className="px-6 py-4 font-medium text-white text-xl">
+                  {contact.name}
+                </td>
+                <td className="px-6 py-4 font-medium text-base">
+                  {contact.popularity.toFixed(2)}
+                </td>
+              </tr>
+            )}
+          </tbody>
+        </table>
+      </div>
+
     </div>
   );
 }
