d73b54787b905ca141ae0afb482d752b0d497c72
ficapp2.0 <ficapp2.0@gmail.com>
2023-03-18 02:59:15 +0100

    feat: iteration 3

--- src/components/ContactList.js
+++ src/components/ContactList.js
@@ -4,21 +4,32 @@ import contactsDB from '../contacts.json'
 
 function ContactList() {
   const [contacts, setContacts] = useState(contactsDB.slice(0, 5))
+  const onClickAddRandom = () => {
+    setContacts((prevs) => {
+      const remains = contactsDB.filter(contact => prevs.every(prev => prev.id !== contact.id))
+      return [...prevs, remains[Math.floor(Math.random() * (remains.length - 1))]]
+    })
+  }
+
+
   return (
-    <table className='table'>
-      <thead>
-        <tr>
-          <th scope="col">Picture</th>
-          <th scope="col">Name</th>
-          <th scope="col">Popularity</th>
-          <th scope="col">Won Oscar</th>
-          <th scope="col">Won Emmy</th>
-        </tr>
-      </thead>
-      <tbody>
-        {contacts.map((contact) => <Contact {...contact} />)}
-      </tbody>
-    </table>
+    <>
+      <button className='btn btn-outline-primary mb-3' onClick={onClickAddRandom}>Add random contact</button>
+      <table className='table'>
+        <thead>
+          <tr>
+            <th scope="col">Picture</th>
+            <th scope="col">Name</th>
+            <th scope="col">Popularity</th>
+            <th scope="col">Won Oscar</th>
+            <th scope="col">Won Emmy</th>
+          </tr>
+        </thead>
+        <tbody>
+          {contacts.map((contact) => <Contact key={contact.id} {...contact} />)}
+        </tbody>
+      </table>
+    </>
   )
 }
 
