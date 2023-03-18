ba3209f244dc8135277e488c1b5531baf12a402f
David Reguart <reguartdavid@gmail.com>
2023-03-18 19:08:08 +0100

    Lab solved + Bonus Iteration with Tailwind

--- src/App.js
+++ src/App.js
@@ -42,6 +42,10 @@ function App() {
     setContacts([...contacts]);
   }
 
+  const handleDeleteContact = (id) => {
+    setContacts((prev) => prev.filter((contact) => contact.id !== id));
+  }
+
   return (
     <div className="App bg-gradient-to-bl from-blue-700 via-blue-800 to-gray-900 text-white overflow-x-hidden font-[Poppins]">
       <div className="w-screen flex items-center flex-col mb-10">
@@ -51,35 +55,35 @@ function App() {
 
       <div className="flex justify-center">
         <div className="w-1/2 flex justify-around">
-          <button class="relative inline-flex items-center justify-center px-6 py-3 mb-8 overflow-hidden font-bold text-white rounded-md shadow-2xl group" onClick={handleAddRandomContact}>
-            <span class="absolute inset-0 w-full h-full transition duration-300 ease-out opacity-0 bg-gradient-to-br from-pink-600 via-purple-700 to-blue-400 group-hover:opacity-100"></span>
-            <span class="absolute top-0 left-0 w-full bg-gradient-to-b from-white to-transparent opacity-5 h-1/3"></span>
-            <span class="absolute bottom-0 left-0 w-full h-1/3 bg-gradient-to-t from-white to-transparent opacity-5"></span>
-            <span class="absolute bottom-0 left-0 w-4 h-full bg-gradient-to-r from-white to-transparent opacity-5"></span>
-            <span class="absolute bottom-0 right-0 w-4 h-full bg-gradient-to-l from-white to-transparent opacity-5"></span>
-            <span class="absolute inset-0 w-full h-full border border-white rounded-md opacity-10"></span>
-            <span class="absolute w-0 h-0 transition-all duration-300 ease-out bg-white rounded-full group-hover:w-56 group-hover:h-56 opacity-5"></span>
-            <span class="relative">Add Random Contact</span>
+          <button className="relative inline-flex items-center justify-center px-6 py-3 mb-8 overflow-hidden font-bold text-white rounded-md shadow-2xl group" onClick={handleAddRandomContact}>
+            <span className="absolute inset-0 w-full h-full transition duration-300 ease-out opacity-0 bg-gradient-to-br from-pink-600 via-purple-700 to-blue-400 group-hover:opacity-100"></span>
+            <span className="absolute top-0 left-0 w-full bg-gradient-to-b from-white to-transparent opacity-5 h-1/3"></span>
+            <span className="absolute bottom-0 left-0 w-full h-1/3 bg-gradient-to-t from-white to-transparent opacity-5"></span>
+            <span className="absolute bottom-0 left-0 w-4 h-full bg-gradient-to-r from-white to-transparent opacity-5"></span>
+            <span className="absolute bottom-0 right-0 w-4 h-full bg-gradient-to-l from-white to-transparent opacity-5"></span>
+            <span className="absolute inset-0 w-full h-full border border-white rounded-md opacity-10"></span>
+            <span className="absolute w-0 h-0 transition-all duration-300 ease-out bg-white rounded-full group-hover:w-56 group-hover:h-56 opacity-5"></span>
+            <span className="relative">Add Random Contact</span>
           </button>
-          <button class="relative inline-flex items-center justify-center px-6 py-3 mb-8 overflow-hidden font-bold text-white rounded-md shadow-2xl group" onClick={handleSortbyPopularity}>
-            <span class="absolute inset-0 w-full h-full transition duration-300 ease-out opacity-0 bg-gradient-to-br from-pink-600 via-purple-700 to-blue-400 group-hover:opacity-100"></span>
-            <span class="absolute top-0 left-0 w-full bg-gradient-to-b from-white to-transparent opacity-5 h-1/3"></span>
-            <span class="absolute bottom-0 left-0 w-full h-1/3 bg-gradient-to-t from-white to-transparent opacity-5"></span>
-            <span class="absolute bottom-0 left-0 w-4 h-full bg-gradient-to-r from-white to-transparent opacity-5"></span>
-            <span class="absolute bottom-0 right-0 w-4 h-full bg-gradient-to-l from-white to-transparent opacity-5"></span>
-            <span class="absolute inset-0 w-full h-full border border-white rounded-md opacity-10"></span>
-            <span class="absolute w-0 h-0 transition-all duration-300 ease-out bg-white rounded-full group-hover:w-56 group-hover:h-56 opacity-5"></span>
-            <span class="relative">Sort by Popularity</span>
+          <button className="relative inline-flex items-center justify-center px-6 py-3 mb-8 overflow-hidden font-bold text-white rounded-md shadow-2xl group" onClick={handleSortbyPopularity}>
+            <span className="absolute inset-0 w-full h-full transition duration-300 ease-out opacity-0 bg-gradient-to-br from-pink-600 via-purple-700 to-blue-400 group-hover:opacity-100"></span>
+            <span className="absolute top-0 left-0 w-full bg-gradient-to-b from-white to-transparent opacity-5 h-1/3"></span>
+            <span className="absolute bottom-0 left-0 w-full h-1/3 bg-gradient-to-t from-white to-transparent opacity-5"></span>
+            <span className="absolute bottom-0 left-0 w-4 h-full bg-gradient-to-r from-white to-transparent opacity-5"></span>
+            <span className="absolute bottom-0 right-0 w-4 h-full bg-gradient-to-l from-white to-transparent opacity-5"></span>
+            <span className="absolute inset-0 w-full h-full border border-white rounded-md opacity-10"></span>
+            <span className="absolute w-0 h-0 transition-all duration-300 ease-out bg-white rounded-full group-hover:w-56 group-hover:h-56 opacity-5"></span>
+            <span className="relative">Sort by Popularity</span>
           </button>
-          <button class="relative inline-flex items-center justify-center px-6 py-3 mb-8 overflow-hidden font-bold text-white rounded-md shadow-2xl group" onClick={handleSortbyName}>
-            <span class="absolute inset-0 w-full h-full transition duration-300 ease-out opacity-0 bg-gradient-to-br from-pink-600 via-purple-700 to-blue-400 group-hover:opacity-100"></span>
-            <span class="absolute top-0 left-0 w-full bg-gradient-to-b from-white to-transparent opacity-5 h-1/3"></span>
-            <span class="absolute bottom-0 left-0 w-full h-1/3 bg-gradient-to-t from-white to-transparent opacity-5"></span>
-            <span class="absolute bottom-0 left-0 w-4 h-full bg-gradient-to-r from-white to-transparent opacity-5"></span>
-            <span class="absolute bottom-0 right-0 w-4 h-full bg-gradient-to-l from-white to-transparent opacity-5"></span>
-            <span class="absolute inset-0 w-full h-full border border-white rounded-md opacity-10"></span>
-            <span class="absolute w-0 h-0 transition-all duration-300 ease-out bg-white rounded-full group-hover:w-56 group-hover:h-56 opacity-5"></span>
-            <span class="relative">Sort by Name</span>
+          <button className="relative inline-flex items-center justify-center px-6 py-3 mb-8 overflow-hidden font-bold text-white rounded-md shadow-2xl group" onClick={handleSortbyName}>
+            <span className="absolute inset-0 w-full h-full transition duration-300 ease-out opacity-0 bg-gradient-to-br from-pink-600 via-purple-700 to-blue-400 group-hover:opacity-100"></span>
+            <span className="absolute top-0 left-0 w-full bg-gradient-to-b from-white to-transparent opacity-5 h-1/3"></span>
+            <span className="absolute bottom-0 left-0 w-full h-1/3 bg-gradient-to-t from-white to-transparent opacity-5"></span>
+            <span className="absolute bottom-0 left-0 w-4 h-full bg-gradient-to-r from-white to-transparent opacity-5"></span>
+            <span className="absolute bottom-0 right-0 w-4 h-full bg-gradient-to-l from-white to-transparent opacity-5"></span>
+            <span className="absolute inset-0 w-full h-full border border-white rounded-md opacity-10"></span>
+            <span className="absolute w-0 h-0 transition-all duration-300 ease-out bg-white rounded-full group-hover:w-56 group-hover:h-56 opacity-5"></span>
+            <span className="relative">Sort by Name</span>
           </button>
         </div>
       </div>
@@ -103,6 +107,9 @@ function App() {
               <th scope="col" className="px-6 py-3">
                 Won Emmy
               </th>
+              <th scope="col" className="px-6 py-3">
+                Actions
+              </th>
             </tr>
           </thead>
           <tbody>
@@ -122,6 +129,15 @@ function App() {
                 </td>
                 <td className="px-6 py-4 font-medium text-3xl">
                   {wonEmmy(contact)}
+                </td>
+                <td className="px-6 py-4 font-light">
+                  <button className="relative inline-flex items-center justify-start px-6 py-3 overflow-hidden font-medium transition-all bg-red-500 rounded-xl group" onClick={() => handleDeleteContact(contact.id)} >
+                    <span className="absolute top-0 right-0 inline-block w-4 h-4 transition-all duration-500 ease-in-out bg-red-700 rounded group-hover:-mr-4 group-hover:-mt-4">
+                      <span className="absolute top-0 right-0 w-5 h-5 rotate-45 translate-x-1/2 -translate-y-1/2 bg-white"></span>
+                    </span>
+                    <span className="absolute bottom-0 left-0 w-full h-full transition-all duration-500 ease-in-out delay-200 -translate-x-full translate-y-full bg-red-600 rounded-xl group-hover:mb-11 group-hover:translate-x-0"></span>
+                    <span className="relative w-full text-left text-white transition-colors duration-200 ease-in-out group-hover:text-white">Delete</span>
+                  </button>
                 </td>
               </tr>
             )}
