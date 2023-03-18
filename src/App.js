ba643009262b2c8fd36ceae577aef39d8a9d7134
elena992 <elena.fernandez1992@gmail.com>
2023-03-18 07:08:30 +0100

    in progress

--- src/App.js
+++ src/App.js
@@ -1,25 +1,37 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import "./App.css";
+import contacts from "./contacts.json";
 
 function App() {
+  const filterContacts = contacts.slice(0, 5)
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <header>
+      <h1>IronContacts</h1>
+    </header>
+      <table>
+        <tr>
+          <th>Picture</th>
+          <th>Name</th>
+          <th>Popularity</th>
+          <th>Won Oscar</th>
+          <th>Won Emmy</th>
+        </tr>
+        {filterContacts.map((contact) => (
+          <tr key={contact.id}>
+            <td>
+              <img className="contact-image" src={contact.pictureUrl} alt={contact.name} />
+            </td>
+            <td>{contact.name}</td>
+            <td>{contact.popularity.toFixed(2)}</td>
+            <td>{contact.wonOscar && <p>🏆</p>}</td>
+            <td>{contact.wonEmmy && <p>🏆</p>}</td>
+          </tr>
+        ))}
+      </table>
     </div>
   );
-}
-
-export default App;
+        }
+      
+ export default App;
