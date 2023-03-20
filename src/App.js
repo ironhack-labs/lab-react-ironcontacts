9d19f7c89b4d2b5218ca8fd86ebcd0e364470a3d
Albert Garcia <gpalbertgp@outlook.es>
2023-03-20 12:40:40 +0100

    Done

--- src/App.js
+++ src/App.js
@@ -1,23 +1,72 @@
-import logo from './logo.svg';
+import React, {useEffect, useState} from 'react';
 import './App.css';
+import ContactList from './components/ContactList/ContactList';
+import contactsJson from "./contacts.json";
 
 function App() {
+
+  const [contacts, setContacts] = useState([contactsJson[0], contactsJson[1], contactsJson[2], contactsJson[3], contactsJson[4]])
+
+  const handleRandomContact = () => {
+      setContacts((prevContacts) => {
+      let randomContact
+      
+      do {
+        randomContact = contactsJson[Math.floor(Math.random() * contactsJson.length)]
+      } while (prevContacts.includes(randomContact))
+      console.log(randomContact)
+
+      return [randomContact, ...prevContacts]
+      })   
+  }
+
+  const handleSortpopularity = () => {
+    setContacts((prevContacts) => {
+      const sortedContacts = [...prevContacts].sort((a, b) =>  { return b.popularity - a.popularity })
+      return sortedContacts;
+    })
+  }
+
+  const handleSortName = () => {
+    setContacts((prevContacts) => {
+      const sortedContacts = [...prevContacts].sort((a, b) => { return a.name > b.name ? 1 : -1})
+      return sortedContacts;
+    })
+  }
+
+  const handleDeleteContact = (id) => {
+    setContacts((prevContacts) => {
+      console.log(`Deleting ${id}`)
+      return prevContacts.filter(contact => contact.id !== id)
+      
+    })
+  }
+ 
+  useEffect(() => {
+    setContacts(contacts)
+  },[contacts])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='d-flex flex-column align-items-center'>
+      <h1 className='mt-5'>IronContacts</h1>
+      <div>
+        <button className='m-3' onClick={handleRandomContact}>Add Random Contact</button>
+        <button className='m-3' onClick={handleSortpopularity}>Sort by popularity</button>
+        <button className='m-3' onClick={handleSortName}>Sort by name</button>
+      </div>
+      <table width='500'>
+      <tbody>
+        <tr>
+          <th className='px-3'>Picture</th>
+          <th className='px-3'>Name</th>
+          <th className='px-3'>Popularity</th>
+          <th className='px-3'>Won Oscar</th>
+          <th className='px-3'>Won Emmy</th>
+          <th className='px-3'>Actions</th>
+        </tr>
+        {contacts?.map((contact) => <ContactList key={contact.id} {...contact} onClickDelete={() => handleDeleteContact(contact.id)}/>)}
+      </tbody>
+      </table> 
     </div>
   );
 }
---
+++ src/components/ContactList/ContactList.js
@@ -0,0 +1,29 @@
+import React from 'react'
+
+function ContactList({ name, pictureUrl, popularity, wonOscar, wonEmmy, onClickDelete}) {
+
+  let roundPopularity = (Math.round(popularity * 100) / 100).toFixed(2)
+  let hasOscar = ''
+  let hasEmmy = ''
+
+  if (wonOscar) {
+    hasOscar = '🏆'
+  }
+
+  if (wonEmmy) {
+    hasEmmy = '🏆'
+  }
+  
+  return (
+    <tr>
+      <td><img src={pictureUrl} alt={name} height='150' /></td>
+      <td className='px-3'>{name}</td>
+      <td className='px-3'>{roundPopularity}</td>
+      <td className='px-3'>{hasOscar}</td>
+      <td className='px-3'>{hasEmmy}</td>
+      <td className='px-3'><button onClick={onClickDelete}>Delete</button></td>
+    </tr>
+  )
+}
+
+export default ContactList
