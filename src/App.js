bcef6e7ab04fd8dc81e2b8ee748c4f04d75e40cf
Joao Vitor <joao-vitor.fede@hpe.com>
2023-03-20 22:44:57 -0300

    Iteration 2 done

--- src/App.js
+++ src/App.js
@@ -1,35 +1,44 @@
-import './App.css';
-import contactsArray from './contacts.json';
-import { useState } from 'react';
-
+import "./App.css";
+import contactsArray from "./contacts.json";
+import { useState } from "react";
 
 function App() {
   const firstFiveContacts = contactsArray.slice(0, 5);
-  const [contacts , setContacts ] = useState(firstFiveContacts);
-  
+  const [contacts, setContacts] = useState(firstFiveContacts);
+
   return (
     <div className="App">
-    <h1>IronContacts</h1>
-      <table> 
+      <h1>IronContacts</h1>
+      <table>
         <thead>
-         <tr>
-          <th>Picture</th>
-          <th>Name</th>
-          <th>Popularity</th>
-        </tr>
-       </thead>
+          <tr>
+            <th>Picture</th>
+            <th>Name</th>
+            <th>Popularity</th>
+            <th>Won Oscar</th>
+            <th>Won Emmy</th>
+          </tr>
+        </thead>
       </table>
       <tbody>
-      {contacts.map(contact => {
+        {contacts.map((contact) => {
           let imgContent;
-          imgContent = <img className ="picture" src={contact.pictureUrl} alt={contact.name} />
+          imgContent = (
+            <img
+              className="picture"
+              src={contact.pictureUrl}
+              alt={contact.name}
+            />
+          );
           return (
             <tr key={contact.id}>
               <td>{imgContent}</td>
               <td>{contact.name}</td>
               <td>{contact.popularity.toFixed(2)}</td>
+              <td>{contact.wonOscar && <p>🏆</p>}</td>
+              <td>{contact.wonEmmy && <p>🏆</p>}</td>
             </tr>
-          )
+          );
         })}
       </tbody>
     </div>
