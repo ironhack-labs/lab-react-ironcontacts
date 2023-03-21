06c6bd68b5d943549ca12ad76dec07c9a32070b5
Joao Vitor <joao-vitor.fede@hpe.com>
2023-03-20 22:35:33 -0300

    Iteration 1 done

--- src/App.js
+++ src/App.js
@@ -1,23 +1,37 @@
-import logo from './logo.svg';
 import './App.css';
+import contactsArray from './contacts.json';
+import { useState } from 'react';
+
 
 function App() {
+  const firstFiveContacts = contactsArray.slice(0, 5);
+  const [contacts , setContacts ] = useState(firstFiveContacts);
+  
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <h1>IronContacts</h1>
+      <table> 
+        <thead>
+         <tr>
+          <th>Picture</th>
+          <th>Name</th>
+          <th>Popularity</th>
+        </tr>
+       </thead>
+      </table>
+      <tbody>
+      {contacts.map(contact => {
+          let imgContent;
+          imgContent = <img className ="picture" src={contact.pictureUrl} alt={contact.name} />
+          return (
+            <tr key={contact.id}>
+              <td>{imgContent}</td>
+              <td>{contact.name}</td>
+              <td>{contact.popularity.toFixed(2)}</td>
+            </tr>
+          )
+        })}
+      </tbody>
     </div>
   );
 }
