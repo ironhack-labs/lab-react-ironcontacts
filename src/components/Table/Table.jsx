7ebb6d0818532db9803fbde9435188e57fb80242
Victor Lopez <victorlpz2301@gmail.com>
2023-03-22 11:41:27 +0100

    Iteration 2 Done

--- src/components/Table/Table.jsx
+++ src/components/Table/Table.jsx
@@ -2,18 +2,20 @@ import React from 'react'
 import Contact from '../Contacts/Contact'
 import contacts from "../../contacts.json"
 
-const contactsArray = contacts.slice(0, 5)
+const contactsArray = contacts.slice(0, 10)
 
 function Table() {
   return (
     <div>
-      <h1 className="mt-3 fs-bold">IronContacts</h1>
+      <h1 className="mt-3 mb-3 fw-bold">IronContacts</h1>
       <table className="">
         <thead>
           <tr>
             <th>Picture</th>
             <th>Name</th>
             <th>Popularity</th>
+            <th>Won Oscar</th>
+            <th>Won Emmy</th>
           </tr>
         </thead>
         <tbody>{contactsArray.map(contact => <Contact contact={contact} key={contact.id}/> )}</tbody>
