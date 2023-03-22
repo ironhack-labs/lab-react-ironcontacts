2dff2e1137dd7ba463eab0d13644287e2c0828a7
Ana Gutiérrez Ruiz <ana.gutierrez.ruiz@Anas-iMac.home>
2023-03-22 11:47:23 +0100

    iteration 2

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -3,9 +3,11 @@ import React from 'react';
 function Contact({ contact }) {
   return (
     <tr>
-      <td><img src={contact.pictureUrl} alt="Contact Pic" /></td>
-      <td>{contact.name}</td>
-      <td>{contact.popularity.toFixed(2)}</td>
+      <td className="me-2"><img src={contact.pictureUrl} alt="Contact Pic" style={{width: 60}} /></td>
+      <td className="me-2">{contact.name}</td>
+      <td className="me-2">{contact.popularity.toFixed(2)}</td>
+      <td className="me-2">{contact.wonOscar ? "🏆" : ""}</td>
+      <td className="me-2">{contact.wonEmmy ? "🏆" : ""}</td>
     </tr>
   )
 }
--- src/components/ContactsTable.jsx
+++ src/components/ContactsTable.jsx
@@ -9,9 +9,16 @@ function ContactsTable() {
 
   return (
     <div>
-      <h1>IronContacts</h1>
-      <table>
+      <h1 className="m-3 mb-3 fw-bold">IronContacts</h1>
+      <table className="m-3">
         <thead>
+        <tr>
+          <th>Picture</th>
+          <th>Name</th>
+          <th>Popularity</th>
+          <th>Oscar</th>
+          <th>Emmy</th>
+        </tr>
         </thead>
           <tbody>
             {contactsArray.map(contact => <Contact contact={contact} key={contact.id} />)}
