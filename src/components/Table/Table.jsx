e6aca760afad47d350a6b2cda7d6090d9363529b
Victor Lopez <victorlpz2301@gmail.com>
2023-03-23 11:23:18 +0100

    Iteration 4 Done

--- src/components/Table/Table.jsx
+++ src/components/Table/Table.jsx
@@ -2,36 +2,47 @@ import React, { useState } from 'react'
 import Contact from '../Contacts/Contact'
 import contactsData from "../../contacts.json"
 
-const restContacts = contactsData.map( x => x );
+const restContacts = contactsData.map(x => x);
 
-for(let i = 0; i < 5; i++){
+for (let i = 0; i < 5; i++) {
   restContacts.shift();
 }
 
 function Table() {
-  const contactsArray = contactsData.slice(0,5);
+  const contactsArray = contactsData.slice(0, 5);
   const [contacts, setContacts] = useState(contactsArray);
-  
-  
+
+
 
   function handleAddRandomContact() {
-    if(restContacts.length !== 0) {
-      const index = (Math.floor(Math.random()*restContacts.length))
+    if (restContacts.length !== 0) {
+      const index = (Math.floor(Math.random() * restContacts.length))
       setContacts([...new Set([restContacts[index], ...contacts])])
-    
+
       restContacts.splice(index, 1);
     } else {
       return
     }
   }
-  
 
- 
+  function handleSortByPopularity() {
+    setContacts([...contacts].sort((a, b) => b.popularity - a.popularity))
+  }
+
+
+  function handleSortByName() {
+    setContacts([...contacts].sort((a, b) => a.name.localeCompare(b.name)))
+  }
+
+
+
 
   return (
     <div>
       <h1 className="mt-3 mb-3 fw-bold">IronContacts</h1>
       <button onClick={handleAddRandomContact}>Add Random Contact</button>
+      <button onClick={handleSortByPopularity}>Sort by Popularity</button>
+      <button onClick={handleSortByName}>Sort by Name</button>
       <table className="m-3">
         <thead>
           <tr>
