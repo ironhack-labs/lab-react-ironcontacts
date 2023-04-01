18ceb824b1eaa80b1b3ff09a2f8215b44c50a965
BodyYvesEL <bodysoftwareengineer@gmail.com>
2023-04-01 16:44:21 +0200

    Iteration 1

--- src/App.js
+++ src/App.js
@@ -1,9 +1,40 @@
 import logo from './logo.svg';
 import './App.css';
+import contactsJSON from "./contacts.json";
+import React, { useState } from 'react';
+
 
 function App() {
+  const [contacts, setContacts] = useState(contactsJSON.slice(0, 5));
+
   return (
-    <div className="App"></div>
+    <div className="App">
+      <h1>IronContacts</h1>
+      <table>
+        <thead>
+          <tr>
+            <th>Picture</th>
+            <th>Name</th>
+            <th>Popularity</th>
+            <th>Won an Oscar</th>
+            <th>Won an Emmy</th>
+          </tr>
+        </thead>
+        <tbody>
+          {contacts.map((contact) => (
+            <tr key={contact.id}>
+              <td>
+                <img src={contact.pictureUrl} alt={contact.name} width="100" />
+              </td>
+              <td>{contact.name}</td>
+              <td>{contact.popularity.toFixed(2)}</td>
+              <td>{contact.wonOscar ? '🏆' : ''}</td>
+              <td>{contact.wonEmmy ? '🏆' : ''}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
   );
 }
 
