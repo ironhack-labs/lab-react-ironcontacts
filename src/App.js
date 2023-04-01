79607042123af599b48edc9da476da549c6fd4bd
Azienda-Creativa <116496155+Azienda-Creativa@users.noreply.github.com>
2023-04-01 17:32:53 +0200

    WD PT RMT NOV22 Alessandro Iadarola
    iteration nr 1-2

--- src/App.js
+++ src/App.js
@@ -1,25 +1,43 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg"
+import "./App.css"
+import { useState } from "react"
+import contacts from "./contacts.json"
 
 function App() {
+  const [thisContact, setContact] = useState(contacts.slice(0, 10))
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <table>
+        <thead>
+          <tr>
+            <th>Picture</th>
+            <th>Name</th>
+            <th>Popularity</th>
+            <th>Won Oscar</th>
+            <th>Won Emmy</th>
+            <th>Actions</th>
+          </tr>
+        </thead>
+        <tbody>
+          {thisContact.map((contact) => (
+            <tr key={contact.id}>
+              <td>
+                <img
+                  src={contact.pictureUrl}
+                  alt={contact.name}
+                />
+              </td>
+              <td>{contact.name}</td>
+              <td>{contact.popularity.toFixed(2)}</td>
+              <td>{contact.wonOscar && "🏆"}</td>
+              <td>{contact.wonEmmy && "🏆"}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
