be37b1ee9777c37ec8ea8455c7d7a5fad82be42a
Wilkins Morales <wilkinscom@gmail.com>
2023-04-01 17:57:16 +0200

    iteration 3

--- src/App.js
+++ src/App.js
@@ -1,11 +1,23 @@
 import "./App.css";
 import contactsData from "./contacts.json";
-import { useState } from "react";
+import React, { useState } from "react";
 
 function App() {
-  const [contacts, setContacts] = useState(contactsData.slice(0, 5));
+  const [contacts, setContacts] = useState([...contactsData].slice(0, 5));
+  const randomActor = () => {
+    const filteredContacts = contactsData.filter(
+      (contact) => !contacts.includes(contact)
+    );
+    const randomContact =
+      filteredContacts[Math.floor(Math.random() * filteredContacts.length)];
+    setContacts([...contacts, randomContact]);
+    console.log("Added contact: " + randomContact.name);
+  };
+
   return (
     <div className="App">
+      <h2>IronContacts</h2>
+      <button onClick={randomActor}>Add Random Contact</button>
       <table>
         <thead>
           <tr>
