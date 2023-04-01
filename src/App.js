00f2306870e5ba4e7b3661175066dd30379c80cd
Johannes Chorzempa <jchorzempa@posteo.org>
2023-04-01 16:13:17 +0200

    Add iteration 1 w/ styles + prettier

--- src/App.js
+++ src/App.js
@@ -1,25 +1,38 @@
-import logo from './logo.svg';
+// src/App.js
 import './App.css';
+import contacts from './contacts.json';
+import { useState } from 'react';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	const [ironContacts, setIronContacts] = useState([contacts[0], contacts[1], contacts[2], contacts[3], contacts[4]]);
+
+	console.log(ironContacts);
+
+	return (
+		<div className='App'>
+			<table>
+				<thead>
+					<tr>
+						<th>Picture</th>
+						<th>Name</th>
+						<th>Popularity</th>
+					</tr>
+				</thead>
+
+				{ironContacts.map((contact) => (
+					<tbody key={contact.id}>
+						<tr>
+							<th>
+								<img className='portrait' src={contact.pictureUrl} alt=' celebrity' />
+							</th>
+							<td>{contact.name}</td>
+							<td>{contact.popularity}</td>
+						</tr>
+					</tbody>
+				))}
+			</table>
+		</div>
+	);
 }
 
 export default App;
