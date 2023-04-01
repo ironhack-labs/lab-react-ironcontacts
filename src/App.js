2907a6b042ce43168ae9c8ba755238f9e784df3b
Wilkins Morales <wilkinscom@gmail.com>
2023-04-01 16:31:57 +0200

    Iteration 1

--- src/App.js
+++ src/App.js
@@ -1,8 +1,37 @@
-import logo from "./logo.svg";
 import "./App.css";
+import contactsData from "./contacts.json";
+import { useState } from "react";
 
 function App() {
-  return <div className="App"></div>;
+  const [contacts, setContacts] = useState(contactsData.slice(0, 5));
+  return (
+    <div className="App">
+      <table>
+        <thead>
+          <tr>
+            <th>Picture</th>
+            <th>Name</th>
+            <th>Popularity</th>
+          </tr>
+        </thead>
+        <tbody>
+          {contacts.map((contact, id) => (
+            <tr key={id}>
+              <td>
+                <img
+                  src={contact.pictureUrl}
+                  alt={contact.name}
+                  style={{ width: "50px" }}
+                />
+              </td>
+              <td>{contact.name}</td>
+              <td>{contact.popularity.toFixed(2)}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
+  );
 }
 
 export default App;
