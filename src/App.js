a4cc3e8ddbb3ef30fd585e91e2de017ee7228b32
Johannes Chorzempa <jchorzempa@posteo.org>
2023-04-01 16:57:57 +0200

    Solved lab

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 // src/App.js
 import './App.css';
 import contacts from './contacts.json';
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 
 function App() {
 	const limitedContacts = contacts.slice(0, 5);
@@ -19,34 +19,61 @@ function App() {
 		setIronContacts([...ironContacts, contacts[random]]);
 	}
 
+	function sortByName() {
+		const sorted = ironContacts.sort((a, b) => a.name.localeCompare(b.name));
+		setIronContacts([...sorted]);
+	}
+
+	function sortByPopularity() {
+		const sortedByPopularity = ironContacts.sort((a, b) => b.popularity - a.popularity);
+		setIronContacts([...sortedByPopularity]);
+	}
+
+	function deleteContact(id) {
+		const filteredContacts = ironContacts.filter((contact) => contact.id !== id);
+		setIronContacts([...filteredContacts]);
+	}
+
+	const [color, setColor] = useState('none');
+
 	return (
 		<div className='App'>
+			<div className='buttons'>
+				<button onClick={addRandomContact}> Add Random Contact</button>
+				<button onClick={sortByName}> Sort Contacts by Name</button>
+				<button onClick={sortByPopularity}> Sort Contacts by Popularity</button>
+			</div>
 			<table>
 				<thead>
-					<tr>
+					<tr className='headers'>
 						<th>Picture</th>
 						<th>Name</th>
 						<th>Popularity</th>
 						<th>Won Oscar</th>
 						<th>Won Emmy</th>
+						<th>Actions</th>
 					</tr>
 				</thead>
 
 				{ironContacts.map((contact) => (
-					<tbody key={contact.id}>
+					<tbody className={color} key={contact.id}>
 						<tr>
 							<th>
 								<img className='portrait' src={contact.pictureUrl} alt=' celebrity' />
 							</th>
 							<td>{contact.name}</td>
-							<td>{contact.popularity}</td>
-							<td>{contact.popularity ? '🏆' : ''}</td>
-							<td>{contact.wonEmmy ? '🏆' : ''}</td>
+							<td>{Number(contact.popularity).toFixed(2)}</td>
+							<td>{contact.wonOscar ? '🏆' : 'X'}</td>
+							<td>{contact.wonEmmy ? '🌟' : 'X'}</td>
+							<td>
+								<button className='delete' onClick={() => deleteContact(contact.id)}>
+									Delete
+								</button>
+							</td>
 						</tr>
 					</tbody>
 				))}
 			</table>
-			<button onClick={addRandomContact}> Add Random Contact</button>
 		</div>
 	);
 }
