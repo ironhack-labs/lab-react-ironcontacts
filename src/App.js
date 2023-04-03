5db45277327a0a6d2cad00dc4d9d187e397bdef9
Wilkins Morales <wilkinscom@gmail.com>
2023-04-03 22:32:37 +0200

    order popularity

--- src/App.js
+++ src/App.js
@@ -31,7 +31,16 @@ function App() {
   };
 
   const orderPopularity = () => {
-    console.log("hello");
+    const sortedPopular = [...contacts].sort(function (a, b) {
+      if (a.popularity > b.popularity) {
+        return -1;
+      }
+      if (a.popularity < b.popularity) {
+        return 1;
+      }
+      return 0;
+    });
+    setContacts(sortedPopular);
   };
 
   const deleteContact = (id) => {
