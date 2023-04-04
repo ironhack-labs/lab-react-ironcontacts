7d65b7975f66f5d386f39c2413d50ceab8005802
Yona <118460222+yosoyyona@users.noreply.github.com>
2023-04-04 16:17:25 +0200

    Iteration 3 - done

--- src/App.js
+++ src/App.js
@@ -3,24 +3,39 @@ import {useState} from 'react'
 import contactsData from './contacts.json'
 
 function App() {
-
-  const [contacts, setContacts] = useState(contactsData.slice(0,4))
-
+  
+  const [contacts, setContacts] = useState(contactsData.slice(0,5))
+  
+  const addRandomContact = () => {
+    const filteredContacts = contactsData.filter(contact => !contacts.includes(contact))
+    const randomNumber = Math.floor(Math.random()*filteredContacts.length)
+    
+    setContacts([...contacts, filteredContacts[randomNumber]])
+  }
+  
   return (
     <div className="App">
       <h1>IronContacts</h1>
+
+      <button onClick={addRandomContact}>Add Random Contact</button>
+
       <table>
-        <tr>
-          <th>Picture</th>
-          <th>Name</th>
-          <th>Popularity</th>
-          <th>Won an Oscar</th>
-          <th>Won an Emmy</th>
-        </tr>
-          {contacts.map(contact => {
+        <thead>
+          <tr>
+            <th>Picture</th>
+            <th>Name</th>
+            <th>Popularity</th>
+            <th>Won an Oscar</th>
+            <th>Won an Emmy</th>
+          </tr>
+        </thead>
+        <tbody>
+          {contacts.map((contact) => {
             return (
-              <tr>
-                <td><img src={contact.pictureUrl} /></td>
+              <tr key={contact.id}>
+                <td>
+                  <img src={contact.pictureUrl} alt={contact.name} />
+                </td>
                 <td>{contact.name}</td>
                 <td>{contact.popularity.toFixed(2)}</td>
                 <td>{contact.wonOscar? <p>🏆</p>:<p>✖️</p>}</td>
@@ -28,6 +43,7 @@ function App() {
               </tr>
             )
           })}
+        </tbody>
       </table>
     </div>
   );
