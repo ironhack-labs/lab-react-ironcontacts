63e99e58261c83e773ed30c17c90509a3f008a44
TunaErkmen <98965068+TunaErkmen@users.noreply.github.com>
2023-04-04 18:46:00 +0200

    Iteration 5 done and added styling as bonus

--- src/App.js
+++ src/App.js
@@ -28,30 +28,39 @@ const randomContacts = () => {
     setContactsArr(sortedContacts);
   };
 
+
+  const deleteContact = (contactId) => {
+    const filteredContacts = contactsArr.filter((contactObj) => {
+      return contactObj.id !== contactId;
+    });
+    setContactsArr(filteredContacts);
+  };
 return (
     <div className="App">
       <h1>IronContacts</h1>
       <button onClick={randomContacts}>Add Random Contact</button>
       <button onClick={sortByPopularity}>Sort by popularity</button>
       <button onClick={sortByName}>Sort by name</button>
-      <table  >
+      <table>
       <thead>
-      <tr>
+      <tr >
               <th>Picture </th>
               <th> Name</th>
               <th> Popularity </th>
               <th> Won an Oscar </th>
               <th>Won an Emmy </th>
+              <th>Actions</th>
             </tr>
             </thead>
             <tbody>
+
       {contactsArr.map((contactObj) => {
         return (
        
            
             <tr key={contactObj.id}>
               <td>
-                <img src={contactObj.pictureUrl} alt={contactObj.name} className="image" />
+                <img src={contactObj.pictureUrl} alt={contactObj.name} />
               </td>
               <td>
                 <p>{contactObj.name}</p>
@@ -62,9 +71,13 @@ return (
               <td>
                    { contactObj.wonOscar ? "🏆"  : " "}
               </td>
-              { contactObj.wonEmmy ? "🏆"  : " "}
+             
               <td>
+              { contactObj.wonEmmy ? "🏆"  : " "}
+              </td>
 
+              <td>
+                <button onClick={() => deleteContact(contactObj.id)}>Delete</button>
               </td>
             </tr>
         );
