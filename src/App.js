a44f9eb77c77e6825e560d0f361dbd34226b746f
ABenat1988 <ale.benat@gmail.com>
2023-04-04 21:47:11 +0200

    iteration3

--- src/App.js
+++ src/App.js
@@ -1,25 +1,55 @@
-import logo from './logo.svg';
-import './App.css';
+// src/App.js
+import "./App.css";
+import { useState } from "react";
+import contactsFromJson from "./contacts.json";
+
+
 
 function App() {
+  const [contacts, setContacts] = useState(contactsFromJson.slice(0, 5));
+  
+  const addRandomContact = () =>{
+    const indexRandom = Math.floor(Math.random() * (contactsFromJson.length - 0 + 1) + 0);
+    const contactRandomCheck = contacts.find(element => element.id === contactsFromJson[indexRandom].id);
+    if(!contactRandomCheck){
+      return setContacts(previousArray => [...previousArray, contactsFromJson[indexRandom]]);
+    } else{
+      return
+    }
+    
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Iron Contacts</h1>
+      <div>
+        <button onClick={ () => {addRandomContact()} }>Add Random Contact</button>
+      </div>
+      <table>
+        <thead>
+          <tr>
+            <th>Picture</th>
+            <th>Name</th>
+            <th>Popularity</th>
+            <th>Won <br/>Oscar</th>
+            <th>Won <br/>Emmy</th>
+          </tr>
+        </thead>
+        <tbody>
+          {contacts.map((contactObj) => {
+            return (
+              <tr key={contactObj.id}>
+                <td><img src={contactObj.pictureUrl} alt={contactObj.name} /></td>
+                <td>{contactObj.name}</td>
+                <td>{contactObj.popularity}</td>
+                {contactObj.wonOscar && <td>🏆</td>}
+                {contactObj.wonEmmy && <td>🏆</td>}
+              </tr>
+            );
+          })}
+        </tbody>
+      </table>
     </div>
   );
 }
-
 export default App;
