c388de211f2cc50d3ce9b1710f53815382252e1a
Michael <mickmhtong@gmail.com>
2023-04-05 01:41:05 +0800

    fix: Iteration 1

--- src/App.js
+++ src/App.js
@@ -16,21 +16,19 @@ function App() {
             <th>Won Emmy</th>
           </tr>
         </thead>
-        {contacts.map((contact) => {
-          return (
-            <tbody key={contact.id}>
-              <tr>
-                <td>
-                  <img src={contact.pictureUrl} alt="" />
-                </td>
-                <td>{contact.name}</td>
-                <td>{contact.popularity}</td>
-                {contact.wonOscar && <td>🏆</td>}
-                {contact.wonEmmy && <td>🏆</td>}
-              </tr>
-            </tbody>
-          );
-        })}
+        {contacts.slice(0, 5).map((contact) => (
+          <tbody key={contact.id}>
+            <tr>
+              <td>
+                <img src={contact.pictureUrl} alt="" />
+              </td>
+              <td>{contact.name}</td>
+              <td>{contact.popularity}</td>
+              {contact.wonOscar && <td>🏆</td>}
+              {contact.wonEmmy && <td>🏆</td>}
+            </tr>
+          </tbody>
+        ))}
       </table>
     </div>
   );
