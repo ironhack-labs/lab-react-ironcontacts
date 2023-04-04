2565a5561b3e8b64f417ed1df89b7fb0b6fa226e
brea-practice <ihk.gitcomm@gmail.com>
2023-04-04 16:13:18 +0200

    Iteration 4

--- src/App.js
+++ src/App.js
@@ -5,8 +5,39 @@ import contactsData from "./contacts.json";
 function App() {
 
   const [contacts, setContacts] = useState(contactsData.slice(0, 5))
+
+  const [restOfContacts, setRestOfContacts] = useState(contactsData.slice(5))
+
+  function addRandomContact() {
+    const i = Math.floor(Math.random() * restOfContacts.length)
+    setContacts([...contacts, restOfContacts[i]])
+
+    const newRest = [...restOfContacts]
+    newRest.splice(i, 1)
+    console.log(newRest)
+    setRestOfContacts(newRest)
+  }
+
+  function orderPopularity() {
+    const sorted = contacts.slice().sort((a, b) => a.popularity < b.popularity)
+    setContacts(sorted)
+  }
+
+  function orderAlphabetically() {
+    const sorted = contacts.slice().sort((a, b) => a.name.localeCompare(b.name))
+    setContacts(sorted)
+  }
+
   return <div className="App">
+
   <h1>IronContacts</h1>
+
+  <div id="buttonDiv">
+    <button onClick={addRandomContact}>Add Random Contact</button>
+    <button onClick={orderPopularity}>Sort by popularity</button>
+    <button onClick={orderAlphabetically}>Sort by name</button>
+  </div>
+
     <table>
       <thead>
         <tr>
@@ -31,6 +62,7 @@ function App() {
         )
       })}
     </table>
+
   </div>;
 }
 export default App;
