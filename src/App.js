6241cd200ceef74e2133d4b28096edf92e5c5d92
Pim Blom <pim.blom@icloud.com>
2023-04-04 22:53:02 +0200

    iteration 5 done, iterations 3,4blocked

--- src/App.js
+++ src/App.js
@@ -1,25 +1,67 @@
-import logo from './logo.svg';
-import './App.css';
+// src/App.js
+import { useState } from "react";
+import "./App.css";
+import contactsFromJson from "./contacts.json"
+
+
 
 function App() {
+  const [contacts, setContacts] = useState(contactsFromJson.slice(0, 5));
+  //console.log(contacts)
+  
+  const addRandomContact = ()=>{
+    const randomIndex = Math.floor(Math.random() * contactsFromJson.length)
+    //console.log(randomIndex)
+    const randomContact = contactsFromJson[randomIndex]
+  }
+  //push(randomContact)
+  //const sortAlphabetically = ()=>{
+
+
+  const deleteContact = (contactId)=>{
+    const newList = contacts.filter((filteredContacts)=>{
+      if (filteredContacts.id === contactId){
+        return false 
+      } else {
+        return true
+      }
+    })
+    setContacts(newList)
+  }
+  
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <button onClick={()=>{addRandomContact()}}> add random contact</button>
+      
+      <table>
+            <tr>
+              <th> Picture</th>
+              <th> Name</th>
+              <th> Popularity</th>
+              <th> Won an Oscar </th>
+              <th> Won an Emmy </th>
+            </tr>
+            
+            
+      {contacts.map((contactObj) => {
+        
+        return (
+          <>
+            <tr>
+              <td> <img src={contactObj.pictureUrl}/> </td>
+              <td> {contactObj.name}</td>
+              <td> {contactObj.popularity}</td>
+              <td> {contactObj.wonOscar && <p>🏆</p> }</td>
+              <td> {contactObj.wonEmmy && <p>🏆</p> }</td>
+            </tr>
+            <button onClick={()=>{deleteContact(contactObj.id)}}> delete</button>
+            </>
+        );
+      })}
+      </table>
+      
     </div>
   );
 }
-
 export default App;
