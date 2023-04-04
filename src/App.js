b75d45aa8074f4f9e8044d851143a60ba1a5b52c
Mattie Barton <maitlandbarton@gmail.com>
2023-04-04 18:25:31 +0200

    completed through iteration 3

--- src/App.js
+++ src/App.js
@@ -1,23 +1,59 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState } from "react";
+import contactsFromJson from './contacts.json';
 
 function App() {
+
+  const [contacts, setContacts] = useState(contactsFromJson.slice(0,5));
+  const [restOfContacts, setRestOfContacts] = useState(contactsFromJson.slice(5))
+
+  const addRandomContact = () => {
+    const randomIndex = Math.floor(Math.random() * restOfContacts.length);
+
+    const randomContact = restOfContacts[randomIndex];
+    const remainingContacts = restOfContacts.filter((contact, index) => index !== randomIndex);
+  
+    setContacts([...contacts, randomContact]);
+    setRestOfContacts(remainingContacts);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App"> 
+    <h1>Ironcontacts!!</h1>
+
+    <button onClick = {addRandomContact}>Add Random Contact!</button>
+
+    <hr/>
+      <table>
+      <thead>
+        <tr>
+          <th>Picture</th>
+          <th>Name</th>
+          <th>Popularity</th>
+          <th>wonOscar</th>
+          <th>wonEmmy</th>
+        </tr>
+      </thead>
+      <tbody>
+
+      {contacts.map( contactObj => {
+        return(
+          <tr>
+          <td>{contactObj.pictureUrl
+                ? <img src={contactObj.pictureUrl} alt={contactObj.name} /> 
+                : <img src="https://dummyimage.com/182x268/aaaaaa/000000" />}
+          </td>
+          <td>{contactObj.name}</td>
+          <td>{contactObj.popularity}</td>
+          <td>{contactObj.wonOscar ? '🏆' : ' '}</td>
+          <td>{contactObj.wonEmmy ? '🏆' : ' '}</td>
+          </tr>
+        )
+      })}
+
+
+      </tbody>
+    </table>
     </div>
   );
 }
