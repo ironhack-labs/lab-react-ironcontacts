caeae6596026b133b119b940dfa417a9b04a33f1
andrehcarvalho6 <andre.hcarvalho1@gmail.com>
2023-04-04 22:39:19 +0100

    lab done

--- src/App.js
+++ src/App.js
@@ -1,23 +1,54 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import contacts from "./contacts.json";
+import React, { useState } from "react";
 
 function App() {
+  const [contact, setContact] = useState(contacts.slice(0, 5));
+  const restOfContacts = contacts.slice(5);
+  const addContact = () => {
+    const randomId = Math.floor(Math.random() * restOfContacts.length);
+    const randomContact = restOfContacts[randomId];
+    setContact((oldContacts) => [...oldContacts, randomContact]);
+  };
+
+  const deleteContact = (id) => {
+    const updatedContacts = contact.filter((contact) => contact.id !== id);
+    setContact(updatedContacts);
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Contacts List</h1>
+      <button onClick={addContact}>Add Random Contact</button>
+      <table>
+        <thead>
+          <tr>
+            <th>Picture</th>
+            <th>Name</th>
+            <th>Popularity</th>
+            <th>Won an Oscar</th>
+            <th>Won an Emmy</th>
+          </tr>
+        </thead>
+        <tbody>
+          {contact.map((contact) => (
+            <tr key={contact.id}>
+              <td>
+                <img src={contact.pictureUrl} alt={contact.name} />
+              </td>
+              <td>{contact.name}</td>
+              <td>{contact.popularity.toFixed(2)}</td>
+              <td>{contact.wonOscar ? "🏆" : ""}</td>
+              <td>{contact.wonEmmy ? "🏆" : ""}</td>
+              <td>
+                <button onClick={() => deleteContact(contact.id)}>
+                  Delete
+                </button>
+              </td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
     </div>
   );
 }
