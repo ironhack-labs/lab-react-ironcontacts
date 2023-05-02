91f9a37af7ec5624810319a9c5996de98a45afab
hussain <muhammadhussain170117@gmail.com>
2023-05-02 17:16:22 +0200

    Completed

--- src/App.js
+++ src/App.js
@@ -1,23 +1,99 @@
-import logo from './logo.svg';
-import './App.css';
+// src/App.js
+import "./App.css";
+import allContact from "./contacts.json";
+import { useState } from "react";
 
 function App() {
+  const [contacts, setContacts] = useState(allContact.slice(0, 5));
+
+  function randomContact() {
+    let array = [...allContact];
+    for (let i = 0; i < contacts.length; i++) {
+      for (let j = 0; j < allContact.length; j++) {
+        if (contacts[i].id === allContact[j].id) {
+          array.splice(j, 1);
+        }
+      }
+    }
+    const random = Math.floor(Math.random() * array.length);
+
+    const arr = [...contacts];
+    arr.push(array[random]);
+    setContacts(arr);
+  }
+
+  function sortByName() {
+    let copy = [...contacts];
+    // const sorted=copy.name.join()
+    const sorted=copy.sort((a,b)=>{
+      if(a.name>b.name){
+        return 1
+      }
+      else{
+        return -1
+      }
+    })
+    console.log(sorted)
+    setContacts(sorted)
+  }
+
+  function sortByPopularity() {
+    let copy = [...contacts];
+    const sorted=copy.sort((a,b)=>{
+      if(a.popularity<b.popularity){
+        return 1
+      }
+      else{
+        return -1
+      }
+    })
+    console.log(sorted)
+    setContacts(sorted)
+  }
+
+  function deleteActor(id){
+    const newArr=[]
+    contacts.forEach((data)=>{
+      if(!(data.id===id)){
+        newArr.push(data)
+      }
+    })
+    setContacts(newArr)
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="list">
+      <h2>IronContacts</h2>
+      <table>
+        <thead>
+          <tr>
+            <th>picture</th>
+            <th>name</th>
+            <th>popularity</th>
+            <th>Won an Oscar</th>
+            <th>Won an Emmy</th>
+          </tr>
+        </thead>
+        <tbody>
+          {contacts.map((data, index) => {
+            return (
+              <tr>
+                <td>
+                  <img src={data.pictureUrl} alt="" className="imgSmall" />
+                </td>
+                <td>{data.name}</td>
+                <td>{data.popularity}</td>
+                <td>{data.wonOscar ? <p>trophy</p> : <p>no trophy</p>}</td>
+                <td>{data.wonEmmy ? <p>trophy</p> : <p>no trophy</p>}</td>
+                <td><button onClick={()=>deleteActor(data.id)}>Delete</button></td>
+              </tr>
+            );
+          })}
+        </tbody>
+      </table>
+      <button onClick={() => randomContact()}>Add Random Contact</button>
+      <button onClick={() => sortByName()}>Sort By Name</button>
+      <button onClick={() => sortByPopularity()}>Sort By Popularity</button>
     </div>
   );
 }
