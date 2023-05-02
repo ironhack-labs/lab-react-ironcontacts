2d739160c227ddccf21e8a3d8c7f687da2e02b1a
Silvia Flare <silviaguizan@gmail.com>
2023-05-02 15:06:37 +0200

    iteration 2 and 3

--- src/App.js
+++ src/App.js
@@ -3,17 +3,38 @@ import contacts from "./contacts.json";
 import { useState } from "react";
 
 function App() {
-  const [contactsArr, setContactsArr] = useState(contacts.slice(0, 5));
+  const [contactsArr, setContactsArr] = useState(contacts.slice(0, 6));
+  console.log(contactsArr);
+
+  function addRandomContact() {
+    const contactsOn = contactsArr.map((contact) => contact.id);
+    // console.log("Contact on the screen: " + contactsOn);
+
+    const contactsOut = contacts.filter(
+      (contact) => !contactsOn.includes(contact.id)
+    );
+    // console.log("Contact not on the screen: ", contactsOut);
+
+    const randomIndex = Math.floor(Math.random() * contactsOut.length);
+    const randomContact = contactsOut[randomIndex];
+
+    setContactsArr([...contactsArr, randomContact]);
+  }
 
   return (
     <div className="App">
       <h1>Ironontacts</h1>
+
+      <button onClick={addRandomContact}>Add random contact</button>
+
       <table>
         <thead>
           <tr>
             <th>Picture</th>
             <th>Name</th>
             <th>Popularity</th>
+            <th>Won Oscar</th>
+            <th>Won Emmy</th>
           </tr>
         </thead>
 
@@ -31,6 +52,8 @@ function App() {
                 </td>
                 <td>{contact.name}</td>
                 <td>{contact.popularity.toFixed(2)}</td>
+                <td>{contact.wonOscar ? <p>🏆</p> : <p></p>} </td>
+                <td>{contact.wonwonEmmy ? <p>🏆</p> : <p></p>} </td>
               </tr>
             );
           })}
