b42aa293b2a9bfd125cd638e3b59a9ece68aac16
gunnar-miklis <gunnar.miklis@gmail.com>
2023-05-02 19:36:53 +0200

    Done. But iteration 5 needs some updates.

--- src/App.js
+++ src/App.js
@@ -1,25 +1,87 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
+import allContacts from './contacts.json';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	// Iteration 1
+	const [visibleContacts, setVisibleContacts] = useState( allContacts.slice( 0, 6 ) );
+
+	// Iteration 3
+	const addRandomContact = () => {
+		// FIXME: attempting to avoid duplicates
+		console.log( 'allContacts.length :>> ', allContacts.length );
+		const remainingContacts = [];
+		allContacts.filter( ( contact ) => {
+			visibleContacts.forEach( ( visibleContact ) => {
+				if ( visibleContact.id !== contact.id ) {
+					return true;
+				} else {
+					return false;
+				}
+			} );
+		} );
+		console.log( 'remainingContacts.length :>> ', remainingContacts.length );
+		const randomCharacter = remainingContacts[Math.floor( Math.random() * remainingContacts.length )];
+		if ( remainingContacts.length > 0 ) {
+			setVisibleContacts( [...visibleContacts, randomCharacter] );
+		}
+	};
+
+	// Iteration 4
+	const sortByName = () => {
+		const copy = [...visibleContacts];
+		copy.sort( ( a, b ) => a.name.localeCompare( b.name ) );
+		setVisibleContacts( copy );
+	};
+	const sortByPopularity = () => {
+		const copy = [...visibleContacts];
+		copy.sort( ( a, b ) => b.popularity - a.popularity );
+		setVisibleContacts( copy );
+	};
+
+	// Iteration 5
+	const deleteContact = ( deleteThisId ) => {
+		const filtered = visibleContacts.filter( ( contact ) => contact.id !== deleteThisId );
+		return setVisibleContacts( filtered );
+	};
+
+	return (
+		<div className="App">
+			<h1>IronContacts</h1>
+			<div className='actions'>
+				<button onClick={addRandomContact}>Random Contact</button>
+				<button onClick={sortByName}>Sort by Name</button>
+				<button onClick={sortByPopularity}>Sort by Popularity</button>
+			</div>
+			<table>
+				<thead>
+					<tr>
+						<th>Picture</th>
+						<th>Name</th>
+						<th>Popularity</th>
+						<th>Won an Oscar</th>
+						<th>Won an Emmy</th>
+						<th>Remove</th>
+					</tr>
+				</thead>
+				<tbody>
+					{ visibleContacts.map( ( contact ) => {
+						return (
+							<tr key={contact.id}>
+								<td><img src={contact.pictureUrl} alt='photo' /></td>
+								<td>{contact.name}</td>
+								<td>{( contact.popularity ).toFixed( 2 )}</td>
+								{ contact.wonOscar ? <td>🏆</td> : <td></td>}
+								{ contact.wonEmmy ? <td>🏆</td> : <td></td>}
+								<td>
+									<button onClick={() => deleteContact( contact.id )}>✖️</button>
+								</td>
+							</tr> );
+					} )}
+				</tbody>
+			</table>
+		</div>
+	);
 }
 
 export default App;
