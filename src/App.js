cf23b4d4f61fdf476524ea27ba74dea74626823a
Nikolas <nihe@posteo.de>
2023-05-02 18:52:57 +0200

    [ber-wd-ft-mar23] Nikolas

--- src/App.js
+++ src/App.js
@@ -1,25 +1,50 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useState } from "react"
+import contactsData from "./contacts.json"
+
 
 function App() {
+
+  const [contacts, setContacts] = useState(contactsData.slice(0,6)) 
+
+  console.log(contacts.pictureUrl)
+
+  const handleClick = () => {
+    for (i = 0; i < setContacts.length; i++) {
+      if (!contacts[i]) {
+        setContacts.push(contacts[i])
+      }
+    }
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <h1>IronContacts</h1>
+    <button onClick={handleClick}>Add random contact</button>
+      <table>
+        <thead>
+          <tr>
+            <th>Picture</th>
+            <th>Name</th>
+            <th>Popularity</th>
+            <th>Won Oscar</th>
+            <th>Won Emmy</th>
+          </tr>
+        </thead>
+        <tbody>
+          { contacts.map((contact) => {
+            return ( 
+              <tr key={contact.id}>
+                <td><img src={contact.pictureUrl} alt={contact.name}/></td>
+                <td>{contact.name}</td>
+                <td>{contact.popularity.toFixed(2)}</td>
+                <td>{contact.wonOscar === true ? "🏆": null}</td>
+              </tr>
+            )
+          })}
+        </tbody>
+    </table>
     </div>
-  );
+  )
 }
-
 export default App;
