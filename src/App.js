5d4d5675cb6a0fc3314a564a5bd49095b18511cc
panagiotisliordos <takislio21@yahoo.gr>
2023-05-02 15:31:51 +0200

    done

--- src/App.js
+++ src/App.js
@@ -1,25 +1,105 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
+import contacts from "./contacts.json";
+import contactsData from "./contacts.json";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [firstFiveContacts, setFirstFiveContacts] = useState(
+        contacts.slice(0, 5)
+    );
+
+    // const [contactsList, setContactsList] = useState(contacts);
+    const [remainingContacts, setRemainingContacts] = useState(
+        contactsData.slice(5)
+    );
+
+    const addRandomContact = () => {
+        if (remainingContacts.length > 0) {
+            const randomIndex = Math.floor(
+                Math.random() * remainingContacts.length
+            );
+            const randomContact = remainingContacts[randomIndex];
+            setFirstFiveContacts([...firstFiveContacts, randomContact]);
+        }
+    };
+    const sortByName = () => {
+        const sortedContacts = [...firstFiveContacts].sort((a, b) => {
+            if (a.name < b.name) {
+                return -1;
+            }
+            if (a.name > b.name) {
+                return 1;
+            }
+            return 0;
+        });
+        setFirstFiveContacts(sortedContacts);
+    };
+
+    const sortByPopularity = () => {
+        const sortedContacts = [...firstFiveContacts].sort(
+            (a, b) => b.popularity - a.popularity
+        );
+        setFirstFiveContacts(sortedContacts);
+    };
+
+    const deleteContact = (id) => {
+        setFirstFiveContacts(
+            firstFiveContacts.filter((contact) => contact.id !== id)
+        );
+        setRemainingContacts([
+            ...remainingContacts,
+            firstFiveContacts.find((contact) => contact.id === id),
+        ]);
+    };
+
+    return (
+        <div className="App">
+            <h1 className="contact-list-title">My Contacts</h1>
+            <div className="buttons">
+                <button onClick={addRandomContact}>Add Random Contact</button>
+                <button onClick={sortByName}>Sort by Name</button>
+                <button onClick={sortByPopularity}>Sort by Popularity</button>
+            </div>
+
+            <table>
+                <thead>
+                    <tr>
+                        <th>Picture</th>
+                        <th>Name</th>
+                        <th>Popularity</th>
+                        <th>Won an Oscar</th>
+                        <th>Won an Emmy</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {firstFiveContacts.map((contact) => (
+                        <tr key={contact.id}>
+                            <td>
+                                <img
+                                    src={contact.pictureUrl}
+                                    alt={contact.name}
+                                    height="100"
+                                />
+                            </td>
+                            <td>{contact.name}</td>
+                            <td>{contact.popularity.toFixed(2)}</td>
+                            <td>
+                                {contact.wonOscar ? <span> 🏆</span> : null}
+                            </td>
+                            <td>{contact.wonEmmy ? <span> 🏆</span> : null}</td>
+                            <td>
+                                <button
+                                    onClick={() => deleteContact(contact.id)}
+                                >
+                                    Delete
+                                </button>
+                            </td>
+                        </tr>
+                    ))}
+                </tbody>
+            </table>
+        </div>
+    );
 }
 
 export default App;
