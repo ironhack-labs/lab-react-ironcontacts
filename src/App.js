a52a0e71237de6d4d5042a3e1009bf0d7c8c776a
HeimLi <127536715+HeimLi@users.noreply.github.com>
2023-05-02 14:45:47 +0200

    Iteration3

--- src/App.js
+++ src/App.js
@@ -5,8 +5,17 @@ import { useState } from 'react';
 
 function App() {
   const [myContacts, setMyContacts] = useState([contacts[0], contacts[1], contacts[2], contacts[3], contacts[4]]);
+  function addContact() {
+    let ranIndex = Math.floor(Math.random() * ((myContacts.length -1)-5+1)+5)
+    let newContact = contacts[ranIndex]
+    let newArray = [...myContacts, newContact]
+    setMyContacts(newArray)
+  }
   return (
   <div className="App">
+
+<h1>Iron Contacts</h1>
+ <button onClick={addContact}> Add Random Contact </button>
 
 <table>
  <thead>
@@ -14,6 +23,8 @@ function App() {
     <th>Picture</th>
     <th>Name</th>
     <th>Popularity</th>
+    <th>Won an Oscar</th>
+    <th>Won an Emmy</th>
   </tr>
  </thead>
  <tbody>
@@ -23,6 +34,12 @@ function App() {
       <td><img src={contact.pictureUrl} alt="Image" height={200}/></td>
       <td>{contact.name}</td>
       <td>{contact.popularity}</td>
+      <td>
+        {contact.wonOscar === true ? "🏆" : null}
+      </td>
+      <td>
+        {contact.wonEmmy === true ? "🏆" : null} 
+      </td>
     </tr>
     )
   })}
