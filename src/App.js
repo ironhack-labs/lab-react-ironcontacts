a3edabdae82c3a5bd94b86b78049ce4baa4f56d9
CrisRamosLazaro <crislr@hotmail.com>
2023-05-23 16:57:59 +0200

    lab done till iteration 3

--- src/App.js
+++ src/App.js
@@ -1,22 +1,77 @@
-import logo from './logo.svg';
+import { useState } from 'react'
 import './App.css';
+import contacts from './contacts.json'
 
 function App() {
+
+  const contactsB = contacts.slice(0, 5)
+  // const contactsRest = contacts.slice(5, contacts.lenght)
+
+  const [contactsArray, setContactsArray] = useState(contactsB)
+  const [nameArray, setName] = useState(contactsArray)
+  const [popularArray, setPopularity] = useState(contactsArray)
+
+  const addRandom = () => {
+
+    const randomIndex = Math.floor(Math.random() * contacts.length)
+
+    const contactsCopy = [...contactsArray]
+    const randomActor = contacts[randomIndex]
+
+    contactsCopy.push(randomActor)
+    setContactsArray(contactsCopy)
+
+  }
+
+  const sortByName = () => {
+
+    const contactsCopy = [...contactsArray]
+    contactsCopy.sort((a, b) => {
+
+    })
+    setName(contactsCopy)
+
+  }
+
   return (
+
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+
+        <>
+          <table>
+            <thead>
+              <tr>
+                <th>Picture</th>
+                <th>Name</th>
+                <th>Popularity</th>
+                <th>Won an Oscar</th>
+                <th>Won an Emmy</th>
+              </tr>
+            </thead>
+            <tbody>
+
+              {contactsArray.map(elm => {
+                return (
+                  <tr key={elm.id}>
+                    <td><img className="actor" src={elm.pictureUrl} alt={elm.name} /></td>
+                    <td>{elm.name}</td>
+                    <td>{elm.popularity}</td>
+                    {elm.wonOscar ? <td>🏆</td> : ' '}
+                    {elm.wonEmmy ? <td>🏆</td> : ' '}
+                  </tr>
+                )
+              })}
+            </tbody>
+          </table>
+
+          <button onClick={addRandom}>Add Random Contact</button>
+          <button onClick={sortByName}>Sort by name</button>
+        </>
+
+
+
+
       </header>
     </div>
   );
