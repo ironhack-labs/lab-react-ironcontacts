cf6811e15421a983f5fe6c388cca8c8f0cc1fe50
José Antonio Escudero García <jaeg90@gmail.com>
2023-05-24 20:53:44 +0200

    Iteration 1 done

--- src/App.js
+++ src/App.js
@@ -1,11 +1,47 @@
 import "./App.css";
 import contacts from "./contacts.json";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 function App() {
   const arrayInit = Math.floor(Math.random() * (contacts.length - 5));
-  const [celebrities, setCelebrities] = useState(contacts);
-  return <div className="App"></div>;
+  const [celebrities, setCelebrities] = useState([]);
+
+  useEffect(() => {
+    setCelebrities(contacts);
+  }, []);
+  const selectCelebrities = celebrities.slice(arrayInit, arrayInit + 5);
+
+  return (
+    <div className="App">
+      <h1>IronContacts</h1>
+      <table>
+        <thead>
+          <tr>
+            <th>Picture</th>
+            <th>Name</th>
+            <th>Popularity</th>
+          </tr>
+        </thead>
+        <tbody>
+          {selectCelebrities.map((cel) => {
+            return (
+              <tr>
+                <th>
+                  <img src={cel.pictureUrl} alt="Profile" />
+                </th>
+                <th>
+                  <p>{cel.name}</p>
+                </th>
+                <th>
+                  <p>{cel.popularity}</p>
+                </th>
+              </tr>
+            );
+          })}
+        </tbody>
+      </table>
+    </div>
+  );
 }
 
 export default App;
