1978b58222e69fa7ac49d7f044e1084643fa4635
José Antonio Escudero García <jaeg90@gmail.com>
2023-05-24 13:24:48 +0200

    working on the lab

--- src/App.js
+++ src/App.js
@@ -1,25 +1,11 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import contacts from "./contacts.json";
+import { useState } from "react";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  const arrayInit = Math.floor(Math.random() * (contacts.length - 5));
+  const [celebrities, setCelebrities] = useState(contacts);
+  return <div className="App"></div>;
 }
 
 export default App;
