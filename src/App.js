91b52d63a004085fa6ad9c01368756de6d6daebe
olgam <olgamartyniuk88@gmail.com>
2023-05-27 20:20:01 +0200

    busy with it 3

--- src/App.js
+++ src/App.js
@@ -1,39 +1,84 @@
-import logo from "./logo.svg";
-import "./App.css";
+import logo from './logo.svg';
+import './App.css';
 import contacts from "./contacts.json";
-import React, { useState } from "react";
+import React, { useState } from 'react';
 
 function App() {
   const [fiveContacts, setFiveContacts] = useState(contacts.slice(0, 5));
-  console.log(fiveContacts);
+  
+
+  const generatingRemainingContacts = ((contacts) => {
+    for (let i = 0; i <= 5; i++) {
+      contacts.shift();
+
+    }
+    return;
+    
+  });
+ console.log(generatingRemainingContacts(contacts));
+  const remainingContacts = generatingRemainingContacts(contacts);
+ 
+
+  const newContactIndex = Math.floor(Math.random() * remainingContacts.length);
+  const newContact = remainingContacts[newContactIndex];
+ 
+
+
+  const hasOscar = (elem) => {
+    if (elem) {
+      return <p>🏆</p>;
+    }
+  }
+  const hasEmmy = (elem) => {
+    if (elem) {
+      return <p>🏆</p>;
+    } 
+  }
 
   return (
     <div className="App">
+      {/* <button onClick={randomContact}>Add random contact</button> */}
+      
+
+
+      
       <table>
-        <thead>
-          <th colspan="3">IronContacts</th>
+        <thead> 
+         
+          <th colSpan="3">IronContacts</th>
           <tr>
             <th>Picture</th>
             <th>Name</th>
             <th>Popularity</th>
+            <th>Won Oscar</th>
+            <th>Won Emmy</th>
+
           </tr>
+
         </thead>
         <tbody>
-          {fiveContacts.map((contact) => {
-            return (
-              <tr>
-                {" "}
-                <td>
-                  {" "}
-                  <img src={contact.pictureUrl} />{" "}
-                </td>
-                <td>{contact.name}</td>
-                <td>{contact.popularity} </td>
-              </tr>
-            );
-          })}
+          
+          
+             {fiveContacts.map(contact => {
+               return <tr> <td>  <img alt="pic" src={contact.pictureUrl}/> </td>
+                 <td>{contact.name}</td>
+                 <td>{contact.popularity} </td>
+                 <td> {hasOscar(contact.wonOscar)}   </td>
+                 <td>{hasEmmy(contact.wonEmmy)}</td>
+               </tr>
+                 ;
+             })}
+          
+          
+          
+          
+
         </tbody>
+
+      
+     
       </table>
+     
     </div>
   );
 }
