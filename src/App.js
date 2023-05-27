147813b5423e80823c82e09c449839932b1962d8
Wiebke Lueneburg <git@fraulueneburg.de>
2023-05-27 16:12:32 +0200

    solved iteration 1–4

--- src/App.js
+++ src/App.js
@@ -1,25 +1,122 @@
-import logo from './logo.svg';
+// src/App.js
 import './App.css';
+import contacts from './contacts.json';
+import { useState } from 'react';
+
+const firstFiveContacts = contacts.slice(0, 5);
+const contactList = contacts.slice(5);
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+	const [contactsArr, updateContactsArr] = useState(firstFiveContacts);
+	const [possibleContacts, setPossibleContacts] = useState(contactList);
 
+	// add contact
+	const addRandomContact = () => {
+		const possibleContactsCopy = [...possibleContacts];
+		const randomIndex = Math.floor(Math.random() * possibleContacts.length);
+		const randomContact = possibleContactsCopy[randomIndex];
+		possibleContactsCopy.splice(randomIndex, 1);
+		setPossibleContacts(possibleContactsCopy);
+
+		updateContactsArr([...contactsArr, randomContact]);
+	};
+
+	// sort
+
+	const sortByName = () => {
+		const contactsToSort = [...contactsArr];
+		contactsToSort.sort(contactsToSort.name);
+
+		function sortStuff(a, b) {
+			const attr1 = a.name;
+			const attr2 = b.name;
+
+			let comparison = 0;
+
+			if (attr1 > attr2) {
+				comparison = 1;
+			} else if (attr1 < attr2) {
+				comparison = -1;
+			}
+			return comparison;
+		}
+		contactsToSort.sort(sortStuff);
+		updateContactsArr(contactsToSort);
+	};
+
+	const sortByPopularity = () => {
+		const contactsToSort = [...contactsArr];
+		contactsToSort.sort(contactsToSort.popularity);
+
+		function sortStuff(a, b) {
+			const attr1 = a.popularity;
+			const attr2 = b.popularity;
+
+			let comparison = 0;
+
+			if (attr1 > attr2) {
+				comparison = 1;
+			} else if (attr1 < attr2) {
+				comparison = -1;
+			}
+			return comparison;
+		}
+		contactsToSort.sort(sortStuff);
+		updateContactsArr(contactsToSort);
+	};
+
+	return (
+		<div className="App">
+			<h1>IronContacts</h1>
+			<button
+				onClick={(event) => {
+					addRandomContact();
+				}}>
+				Add Random Contact
+			</button>
+			<table>
+				<thead>
+					<tr>
+						<th>Picture</th>
+						<th>
+							Name
+							<button
+								onClick={(event) => {
+									sortByName();
+								}}>
+								Sort
+							</button>
+						</th>
+						<th>
+							Popularity
+							<button
+								onClick={(event) => {
+									sortByPopularity();
+								}}>
+								Sort
+							</button>
+						</th>
+						<th>Won an Oscar</th>
+						<th>Won an Emmy</th>
+					</tr>
+				</thead>
+				<tbody>
+					{contactsArr.map(({ name, pictureUrl, popularity, id, wonOscar, wonEmmy }) => {
+						return (
+							<tr key={id}>
+								<td>
+									<img width="200" src={pictureUrl} alt={name} />
+								</td>
+								<td>{name}</td>
+								<td>{popularity}</td>
+								<td>{wonOscar ? '🏆' : null}</td>
+								<td>{wonEmmy ? '🏆' : null}</td>
+							</tr>
+						);
+					})}
+				</tbody>
+			</table>
+		</div>
+	);
+}
 export default App;
