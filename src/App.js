c60134fb14674c112ed51805d8089800d2636797
Natassa <112552395+natko22@users.noreply.github.com>
2023-05-27 16:55:14 +0200

    done with iteration 3

--- src/App.js
+++ src/App.js
@@ -1,23 +1,60 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import contacts from "./contacts.json";
+import { useState } from "react";
 
 function App() {
+  const [allContacts, setAllContacts] = useState(contacts.slice(0, 5));
+  console.log(allContacts);
+
+  const remainingContacts = contacts.slice(5, contacts.length);
+
+  const addRandomContact = () => {
+    const oneRandomIndex = Math.floor(Math.random() * remainingContacts.length);
+    const oneRandomCeleb = remainingContacts[oneRandomIndex];
+    setAllContacts([
+      ...allContacts,
+      {
+        pictureUrl: oneRandomCeleb.pictureUrl,
+        name: oneRandomCeleb.name,
+        popularity: oneRandomCeleb.popularity,
+        wonOscar: oneRandomCeleb.wonOscar,
+        wonEmmy: oneRandomCeleb.wonEmmy,
+      },
+    ]);
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <ul>
+        <h1>IronContacts</h1>
+        <button onClick={addRandomContact}>Add Random Contact</button>
+        <table>
+          <thead>
+            <tr>
+              <th>Picture</th>
+              <th>Name</th>
+              <th>Popularity</th>
+              <th>Won an Oscar</th>
+              <th>Won an Emmy</th>
+            </tr>
+            {allContacts.map((element) => {
+              return (
+                <>
+                  <tr>
+                    <td>
+                      {" "}
+                      <img src={element.pictureUrl} alt={element.name} />
+                    </td>
+                    <td>{element.name}</td> <td>{element.popularity}</td>
+                    <td> {element.wonOscar ? "🏆" : null} </td>
+                    <td> {element.wonEmmy ? "🏆" : null} </td>
+                  </tr>
+                </>
+              );
+            })}
+          </thead>
+        </table>
+      </ul>
     </div>
   );
 }
