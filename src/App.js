e7d883a759cf34b3a184b6d02e2d1dc4c7c693c0
MaaX59 <Maax.strandberg@gmail.com>
2023-05-28 17:23:13 +0200

    Iteration 5 delete button works but add contact does not

--- src/App.js
+++ src/App.js
@@ -4,8 +4,24 @@ import contacts from "./contacts.json";
 import React, { useState } from 'react';
 
 function App() {
-  const [fiveContacts, setFiveContacts] = useState(contacts);
-  // maybe create new array of 5 contacts then render that array
+  const [contactsList, setContacts] = useState(contacts);
+
+  //  create array of 5 contacts 
+  // const fiveContacts = contactsList.slice(5, 10);
+  // console.log(fiveContacts);
+  
+
+  //delete function
+const deleteContact = id => {
+    const filteredContacts = contactsList.filter(contact => {
+      return contact.id !== id;
+    })
+    setContacts(filteredContacts);
+  };
+
+  // const addContact = 
+
+
 
   // const generatingRemainingContacts = ((contacts) => {
   //   for (let i = 0; i <= 5; i++) {
@@ -16,35 +32,22 @@ function App() {
 //   const newContactIndex = Math.floor(Math.random() * remainingContacts.length);
 //   const newContact = remainingContacts[newContactIndex];  
   
-  const deleteContact = id => {
-    const filteredContacts = contacts.filter(contact => {
-      return contact.id !== id;
-    })
-    setFiveContacts(filteredContacts);
-  };
-
+  
 
   const hasOscar = (elem) => {
     if (elem) {
       return <p>🏆</p>;
-    }
-  }
+    }}
   const hasEmmy = (elem) => {
     if (elem) {
       return <p>🏆</p>;
-    } 
-  }
+    }}
 
   return (
     <div className="App">
       <button>Add random contact</button>
-      
-
-
-      
       <table>
         <thead> 
-         
           <th colSpan="5">IronContacts</th>
           <tr>
             <th>Picture</th>
@@ -52,36 +55,21 @@ function App() {
             <th>Popularity</th>
             <th>Won Oscar</th>
             <th>Won Emmy</th>
-
           </tr>
-
         </thead>
         <tbody>
-          
-          
-             {fiveContacts.map(contact => {
+             {contactsList.map(contact => {
                return <tr key={contact.id}>  
                <td>  <img alt="pic" src={contact.pictureUrl} /> </td>
                  <td>{contact.name}</td>
                  <td>{contact.popularity} </td>
                  <td> {hasOscar(contact.wonOscar)}   </td>
                  <td>{hasEmmy(contact.wonEmmy)}</td>
-                 <td><button onClick={ () => deleteContact(contact.id)} >Delete</button></td>
-                   
-               </tr>
-                 ;
+                 <td><button onClick={ () => deleteContact(contact.id)} >Delete</button></td> 
+               </tr>;
              })}
-          
-          
-          
-          
-
         </tbody>
-
-      
-     
       </table>
-     
     </div>
   );
 }
