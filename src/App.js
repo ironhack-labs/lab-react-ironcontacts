fee23c259c306d961e957b271f052fab2efa0b11
“Zuzana <zuzana@getyourguide.com>
2023-05-28 13:36:34 +0200

    Iteration 4 - sorting by name

--- src/App.js
+++ src/App.js
@@ -3,19 +3,17 @@ import contacts from "./contacts.json";
 import { useState } from "react";
 
 function App() {
-  const [allContacts, setAllContacts] = useState(contacts.slice(0, 5));
-  console.log(allContacts);
-
-  const remainingContacts = contacts.slice(5, contacts.length);
+  const slidedContacts = contacts.slice(0, 5);
+  const [allContacts, setAllContacts] = useState(slidedContacts);
 
   const addRandomContact = () => {
+    const remainingContacts = contacts.slice(5, contacts.length);
     const oneRandomIndex = Math.floor(Math.random() * remainingContacts.length);
     const oneRandomCeleb = remainingContacts[oneRandomIndex];
-    setAllContacts([
-      ...allContacts,
+    setAllContacts([...allContacts,
       {
-        pictureUrl: oneRandomCeleb.pictureUrl,
         name: oneRandomCeleb.name,
+        pictureUrl: oneRandomCeleb.pictureUrl,
         popularity: oneRandomCeleb.popularity,
         wonOscar: oneRandomCeleb.wonOscar,
         wonEmmy: oneRandomCeleb.wonEmmy,
@@ -23,11 +21,24 @@ function App() {
     ]);
   };
 
+  // const sortbyPopularity = () => {
+  //   const orderedContactsByPop = allContacts.sort();
+  //   setAllContacts(...allContacts,orderedContactsByPop);
+  // };
+
+  const sortbyName = () => {
+    const SortedByName = [...allContacts].sort((a,b)=> a.name > b.name ? 1 : a.name < b.name ? -1 : 0);
+    setAllContacts(SortedByName);
+  };
+
   return (
     <div className="App">
       <ul>
         <h1>IronContacts</h1>
         <button onClick={addRandomContact}>Add Random Contact</button>
+        {console.log(allContacts)};
+        {/* <button onClick={sortbyPopularity}>Sort by popularity</button> */}
+        <button onClick={sortbyName}>Sort by name</button> 
         <table>
           <thead>
             <tr>
@@ -42,7 +53,7 @@ function App() {
                 <>
                   <tr>
                     <td>
-                      {" "}
+                     
                       <img src={element.pictureUrl} alt={element.name} />
                     </td>
                     <td>{element.name}</td> <td>{element.popularity}</td>
