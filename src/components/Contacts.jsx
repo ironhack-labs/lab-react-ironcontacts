f4aeb889730d736bb37a49bb7c4d026cb7c91b52
Javier Vivas <javier.vivas@icloud.com>
2023-05-30 22:48:52 +0200

    Lab Done

--- src/components/Contacts.jsx
+++ src/components/Contacts.jsx
@@ -9,12 +9,41 @@ const addContact = (contactsArr) => {
   const randomContact = dataContacts[Math.floor(Math.random() * dataContacts.length)]
   const newContactsArr = [...contactsArr, randomContact]
   setContactsArr(newContactsArr)
-}
+  }
+  
+const sortByPopularity = () => {
+    setContactsArr(prevList => {
+    return [...prevList].sort((a,b)=> b.popularity - a.popularity)
+  })
+  }
+
+const sortByName = () => {
+  setContactsArr(prevList => {
+    return [...prevList].sort((a, b) => {
+      if (a.name < b.name) {
+        return -1;
+      }
+      if (a.name > b.name) {
+        return 1;
+      }
+      return 0;
+    });
+  });
+  };
+  
+  
+const deleteContact = contactName => {
+    if (contactName) {
+      setContactsArr(contactsArr.filter(contact=>contact.name !==contactName))
+    }
+  }
 
   return (
     <div>
       <h1>IronContacts</h1>
-    <button onClick={(event) => addContact(contactsArr)}>Add Random Contact</button> 
+      <button className="btn-top" onClick={(event) => addContact(contactsArr)}>Add Random Contact</button> 
+      <button className="btn-top" onClick={(event) => sortByPopularity(contactsArr)}>Sort by Popularity</button>
+      <button className="btn-top" onClick={(event) => sortByName(contactsArr)}>Sort by Name</button>
       <table>
         <thead>
           <tr>
@@ -23,12 +52,14 @@ const addContact = (contactsArr) => {
             <th>POPULARITY</th>
             <th>WON OSCAR</th>
             <th>WON EMMY</th>
+            <th></th>
+            
           </tr>
         </thead>
         <tbody>
           {contactsArr.map(contact => {
             return (
-              <tr>
+              <tr className="content-contact">
                 <td>
                   <img className="img-contact" src={contact.pictureUrl} alt={`foto de ${contact.name}`} />
                 </td>
@@ -39,10 +70,14 @@ const addContact = (contactsArr) => {
                   {contact.popularity.toFixed(2)}
                 </td>
                 <td>
-                  {contact.wonOscar ===true?"🏆":""}
+                  {contact.wonOscar ===true?"🏆":"-"}
                 </td>
                                 <td>
-                  {contact.wonEmmy ===true?"🌟":""}
+                  {contact.wonEmmy ===true?"🌟":"-"}
+                </td>
+
+                <td>
+                  <button className="btn-delete" onClick={() => deleteContact(contact.name)}>Delete</button>
                 </td>
               
               </tr>
