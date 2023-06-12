2a17ea775715a5cedd9b32baff4baf3963909f61
singa23 <singabe-xd@hotmail.fr>
2023-06-12 21:29:56 +0200

    itération 1

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,34 @@
+import React from "react";
 import "./App.css";
+import contacts from "./contacts.json";
 
 function App() {
+  const [contactList, setContactList] = React.useState(contacts.slice(0,5));
   return (
     <div className="App">
-      <h1>LAB | React IronContacts</h1>
+      <h1>IronContacts</h1>
+      <table>
+        <thead>
+          <tr>
+            <th>Picture</th>
+            <th>Name</th>
+            <th>Popularity</th>
+          </tr>
+        </thead>
+        <tbody>
+          {contactList.map((contact) => {
+            return (
+              <tr key={contact.id}>
+                <td><img src={contact.pictureUrl} /></td>
+                <td>{contact.name}</td>
+                <td>{contact.popularity}</td>
+              </tr>
+            )
+          })}
+        </tbody>
+      </table>
     </div>
+
   );
 }
 
