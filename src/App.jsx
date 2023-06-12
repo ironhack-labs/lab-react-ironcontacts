7ca86cbb1e5a9ac8f4d4e366229f237cc2e68961
Rayane <alirayane14@gmail.com>
2023-06-12 21:13:35 +0200

    iteration 1

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,34 @@
+import React from "react";
 import "./App.css";
+import Contacts from "./contacts.json";
 
 function App() {
+  const [contacts, setContacts] = React.useState(Contacts.slice(0, 5));
   return (
     <div className="App">
       <h1>LAB | React IronContacts</h1>
+      <table>
+        <thead>
+          <tr>
+            <th>Picture</th>
+            <th>Name</th>
+            <th>Popularity</th>
+          </tr>
+        </thead>
+        <tbody>
+          {contacts.map((contact) => {
+            return (
+              <tr>
+                <td>
+                  <img src={contact.pictureUrl} alt="" />
+                </td>
+                <td>{contact.name}</td>
+                <td>{contact.popularity.toFixed(2)}</td>
+              </tr>
+            );
+          })}
+        </tbody>
+      </table>
     </div>
   );
 }
