b4173d1c103bd0c9b12ecab34a5006c629babd1d
Cedric Aim <cedric.aim@gmail.com>
2023-06-14 15:44:19 +0200

    iteration 4-6

--- src/App.jsx
+++ src/App.jsx
@@ -16,36 +16,58 @@ function App() {
       contactsFiltered[Math.floor(Math.random() * contactsFiltered.length)],
     ]);
   }
+
+  function sortByPopularity() {
+    const contactsSorted = [...contacts].sort((a, b) => b.popularity - a.popularity);
+    setContacts(contactsSorted);
+  }
+
+  function sortByName() {
+    const contactsSorted = [...contacts].sort((a, b) => a.name.localeCompare(b.name));
+    setContacts(contactsSorted);
+  }
+
+  function removeContact(id) {
+    setContacts(contacts.filter((contact) => contact.id !== id));
+  }
   return (
     <div className="App">
       <h1>LAB | React IronContacts</h1>
       {contactsDataFiltered.length > 1 && (
         <button onClick={addRandomContact}>Add Random Contact</button>
       )}
-      <table>
-        <thead>
-          <tr>
-            <th>Picture</th>
-            <th>Name</th>
-            <th>Popularity</th>
-            <th>Won Oscar</th>
-            <th>Won Emmy</th>
-          </tr>
-        </thead>
-        <tbody>
-          {contacts.map((contact) => (
-            <tr key={contact.id}>
-              <td>
-                <img className="contact__img" src={contact.pictureUrl} alt="actor" />
-              </td>
-              <td>{contact.name}</td>
-              <td>{Math.round(contact.popularity * 100) / 100}</td>
-              <td>{contact.wonOscar && <span>🏆</span>}</td>
-              <td>{contact.wonEmmy && <span>🏆</span>}</td>
+      <button onClick={sortByPopularity}>Sort by popularity</button>
+      <button onClick={sortByName}>Sort by name</button>
+      {contacts.length > 0 && (
+        <table>
+          <thead>
+            <tr>
+              <th>Picture</th>
+              <th>Name</th>
+              <th>Popularity</th>
+              <th>Won Oscar</th>
+              <th>Won Emmy</th>
+              <th>Actions</th>
             </tr>
-          ))}
-        </tbody>
-      </table>
+          </thead>
+          <tbody>
+            {contacts.map((contact) => (
+              <tr key={contact.id}>
+                <td>
+                  <img className="contact__img" src={contact.pictureUrl} alt="actor" />
+                </td>
+                <td>{contact.name}</td>
+                <td>{Math.round(contact.popularity * 100) / 100}</td>
+                <td>{contact.wonOscar && <span>🏆</span>}</td>
+                <td>{contact.wonEmmy && <span>🌟</span>}</td>
+                <td>
+                  <button onClick={() => removeContact(contact.id)}>Delete</button>
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      )}
     </div>
   );
 }
