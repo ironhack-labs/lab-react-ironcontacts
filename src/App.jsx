4d282358fa94425b23d507e1abc55992b2621204
byref17 <124827023+byref17@users.noreply.github.com>
2023-06-14 19:51:41 +0200

    iteration 3 end

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,38 @@
 import "./App.css";
 import contacts from "./contacts.json";
-//import { useState } from "react";
+import { useState } from "react";
 
-//const [contactsInfo, setContacts] = useState(contacts)
 
-const firstIteration =[contacts[0],contacts[1],contacts[2], contacts[3], contacts[4]]
-console.log("first Iteration content =",firstIteration)
+// const firstIteration = [contacts[0], contacts[1], contacts[2], contacts[3], contacts[4]]
+// console.log("first Iteration content =", firstIteration)
 
 
 function App() {
-console.log("first Iteration content =",firstIteration)
+
+  const firstIteration = contacts.slice(0, 5);
+
+  const [contactsInfo, setContacts] = useState(firstIteration);
+
+  console.log("contacts info", contactsInfo)
+
+  console.log("first Iteration content =", firstIteration)
+
+  function addRandomContact() {
+    const remainingContacts = contacts.filter(contact => contactsInfo.indexOf(contact) === -1)
+
+    let num = Math.floor(Math.random() * remainingContacts.length);
+
+    const contactsCopy = contactsInfo.slice()
+
+    contactsCopy.push(remainingContacts[num])
+
+    setContacts(contactsCopy)
+  }
+
   return (
     <div className="App">
       <h1>LAB | React IronContacts</h1>
+      <button onClick={function () { addRandomContact() }}>ADD Random Contact</button>
       <table className="actorsTable">
         <thead>
           <tr>
@@ -24,15 +44,15 @@ console.log("first Iteration content =",firstIteration)
           </tr>
         </thead>
         <tbody>
-          {firstIteration.map((el) => {
-            return(
+          {contactsInfo.map((el) => {
+            return (
               <tr key={el.id}>
-                <td className="picture"><img src={el.pictureUrl} alt="picture"/></td>
+                <td className="picture"><img src={el.pictureUrl} alt="picture" /></td>
                 <td className="widthOne"><h2>{el.name}</h2></td>
-                <td className="widthOne"><p>{el.popularity}</p></td>
+                <td className="widthOne"><p>{el.popularity.toFixed(2)}</p></td>
                 <td className="widthTwo">{el.wonOscar && <p>🏆</p>}</td>
                 <td className="widthTwo">{el.wonEmmy && <p>🏆</p>}</td>
-              </tr>    
+              </tr>
             )
           })}
         </tbody>
