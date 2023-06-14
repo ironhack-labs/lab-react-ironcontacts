48db552f6a909181610ca5779d01c37938a53622
rach <rachelpytse@gmail.com>
2023-06-14 19:41:30 +0200

    finished iteration3

--- src/App.jsx
+++ src/App.jsx
@@ -5,21 +5,39 @@ import "./App.css";
 
 const contactsCopy = contacts.slice();
 const fiveContacts = contactsCopy.slice(0, 5);
-const restContacts = contactsCopy.slice(6);
 
 function App(props) {
   console.log(fiveContacts);
-  const [contacts, setContacts] = React.useState(fiveContacts);
+  const [contactList, setContacts] = React.useState(fiveContacts);
   const trophy = "🏆";
-
-  function addContact(id) {
-    const randomContact = contacts[Math.floor(Math.random() * contacts.length)];
-  }
 
   return (
     <div className="App">
       <h1>LAB | React IronContacts</h1>
-      {contacts.map(function (contact) {
+      <button
+        onClick={function () {
+          console.log("je fonctionne");
+
+          // 1. tirer au sort un contact (qui n'est pas deja dans les `contacts`)
+
+          // creer une liste qui contient seulement les contacts non-presents dans le state
+          const filteredContacts = contacts.filter(
+            (contact) => !contactList.find((c) => c.id === contact.id)
+          );
+          console.log("filteredContacts", filteredContacts); // 47
+
+          const randomContact =
+            filteredContacts[
+              Math.floor(Math.random() * filteredContacts.length)
+            ];
+
+          // 2. maj le state (setContacts) a un tableau de 6 contacts
+          setContacts([...fiveContacts, randomContact]);
+        }}
+      >
+        Add random contact
+      </button>
+      {contactList.map(function (contact) {
         return (
           <table key={contact.id}>
             <thead>
