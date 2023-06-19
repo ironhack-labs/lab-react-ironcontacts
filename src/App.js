eeaa060944b2d920354d8b6c23029f3b1bff0452
Philippe Miossec <pmiossec@gmail.com>
2023-06-19 17:23:28 +0200

    Iteration 1: Display 5 Contacts

--- src/App.js
+++ src/App.js
@@ -1,8 +1,34 @@
+import { useState } from 'react';
 import './App.css';
 
+import allContacts from './contacts.json'
+
 function App() {
+  const [contacts, updateContacts] = useState(allContacts.slice(0, 5));
   return (
-    <div className="App"></div>
+    <div className="App">
+
+    <h1>IronContacts</h1>
+
+    <table>
+      <thead>
+        <tr>
+          <td>Picture</td>
+          <td>Name</td>
+          <td>Popularity</td>
+        </tr>
+      </thead>
+    <tbody>
+    { contacts.map(c => {
+      return <tr>
+      <td><img src={c.pictureUrl} alt={c.name} />  </td>
+      <td>{c.name}</td>
+      <td>{c.popularity.toFixed(2)}</td>
+      </tr>
+    })}
+    </tbody>
+    </table>
+    </div>
   );
 }
 
