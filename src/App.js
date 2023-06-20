58a9d132bb81fa7e3b5ab5e6745ec1c2f657d030
Paula Mateo <anapaula.mateog@gmail.com>
2023-06-20 22:23:29 +0200

    done till iteration  3

--- src/App.js
+++ src/App.js
@@ -1,23 +1,51 @@
-import logo from './logo.svg';
+//import logo from './logo.svg';
 import './App.css';
+import contactsData from "./contacts.json";
+import {useState} from "react";
+
 
 function App() {
+
+  let fiveContacts=contactsData.slice(0,5);
+  const [contacts, setContacts]= useState(fiveContacts);
+  const randomContact =() => {
+    //calculate size of the array contacts
+    const sizeArray=contacts.reduce( sum => sum+1,0);
+    
+    //random index value
+    const randomIndex=Math.floor(Math.random()*sizeArray);
+    //random item
+    const item=contacts[randomIndex];
+
+    //new list with a random contact
+    const newList =[...contacts];
+    newList.push(contacts[item]);
+  }
+  
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <h1>IronContacts</h1>
+    <p><button onClick={() => {randomContact(contacts.id)}}>Add Random Contact</button></p>
+     <table>
+      <tr>
+        <th>Picture</th>
+        <th>Name</th>
+        <th>Popularity</th>
+        <th>Won an Oscar</th>
+        <th>Won an Emmy</th>
+      </tr>
+      {fiveContacts.map((element)=> (
+        <tr>
+        <th><img src={element.pictureUrl}/></th>
+        <th>{element.name}</th>
+        <th>{element.popularity}</th>
+        {element.wonOscar ? <th>🏆</th>: <th></th>}
+        {element.wonEmmy ? <th>🏆</th> : <th></th>}
+      </tr>
+      ))}
+      
+     </table> 
     </div>
   );
 }
