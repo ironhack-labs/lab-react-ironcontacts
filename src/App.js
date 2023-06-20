b4f40c6b490e8af5837ba424d1340924d0cc1877
Lina Zerkly <zerkly.lina@gmail.com>
2023-06-20 21:33:58 +0200

    Iteration 2 done

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import "./App.css";
 import contactsData from "./contacts.json";
 import { useState } from "react";
 
-const contacts = contactsData.slice(0, 5);
+const contacts = contactsData.slice(0, 6);
 
 
 function App() {
@@ -20,18 +20,27 @@ function App() {
             <th>Picture</th>
             <th>Name</th>
             <th>Popularity</th>
+            <th>Won an Oscar</th>
+            <th>Won an Emmy</th>
+            <th></th>
           </tr>
     {contactsToDisplay.map((contactsObj) => {
         return (
+          <div>
           <tr key={contactsObj.id}>
-            <td><img src={contactsObj.pictureUrl} class="image" alt="" /></td>
-            <td>{contactsObj.name}</td>
-            <td>{contactsObj.popularity}</td>
+            <td><img src={contactsObj.pictureUrl} alt="" /></td> <br />
+            <td>{contactsObj.name}</td> <br />
+            <td>{contactsObj.popularity}</td> <br />
+            <td>{contactsObj.wonOscar === true && <p>🏆</p>}</td> <br />
+            <td>{contactsObj.wonEmmy === true && <p>Yes</p>}</td>
+            <td></td>
           </tr>
+          </div>
         );
       })}
       </tbody>
       </table>
+      <button>Add Contact</button>
     </div>
   );
 }
