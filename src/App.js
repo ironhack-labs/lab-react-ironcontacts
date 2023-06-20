22b88678d90baac823bd76008c21688d1145b194
Dok <pierredok@outlook.fr>
2023-06-20 19:20:46 +0200

    Iteration 1/2/3 done

--- src/App.js
+++ src/App.js
@@ -1,25 +1,73 @@
-import logo from './logo.svg';
 import './App.css';
+import contacts from "./contacts.json"
+import { useState } from 'react';
+
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  
+
+  const [contactInfo, setContacts] = useState(contacts.slice(0,5))
+
+  const randomArr = [...contacts].filter(contact => !contactInfo.includes(contact))
+
+  
+  console.log(randomArr);
+
+
+  const addRandomContact = () => {  
+    console.log( randomArr);
+    const intRandom = Math.floor (Math.random() * randomArr.length);
+    const addContact = randomArr[intRandom];
+
+    setContacts(([...contactInfo, addContact]));
+  }
+
+
+
+
+
+  return(
+    <div className='App'>
+    <h1>IronContact</h1>
+    <button onClick={addRandomContact}>Add Random Contact</button>  
+              <table>
+              <thead>
+                <tr>
+                  <th>Pictures</th>
+                  <th>Name</th>
+                  <th>Popularity</th>
+                  <th>Won Oscar</th>
+                  <th>Won Emmy</th>
+
+                </tr>
+              </thead>
+              <tbody>
+                
+                {contactInfo.map((celebrity, index) => {
+                  return(
+                      <tr key={index}>
+                        <td><img src={celebrity.pictureUrl}/></td>
+                        <td>{celebrity.name}</td>
+                        <td>{Math.floor( celebrity.popularity * 100 )/100}</td> 
+                        {celebrity.wonOscar
+                        ? <td>🏆</td>
+                        : <td></td>}
+                        
+                        {celebrity.wonEmmy
+                        ? <td>🏆</td>
+                        : <td></td>}
+
+                        <button>Delete</button>
+                      </tr> 
+                  )
+                  })}
+              </tbody>
+          </table>
+        
+        
+        
+  </div>
+  )
 }
 
 export default App;
