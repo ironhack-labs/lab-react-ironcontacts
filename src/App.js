31acad71e97b7b6d5d38ed0b65593634646a209e
nunojsmonteiro <nunojsmonteiro@gmail.com>
2023-06-21 00:03:47 +0100

    it1, 2 e 5

--- src/App.js
+++ src/App.js
@@ -1,25 +1,64 @@
-import logo from './logo.svg';
 import './App.css';
+import contactsData from './contacts.json';
+import { useState } from "react";
 
 function App() {
+
+  const [contactsToDisplay, setContactsToDisplay] = useState(contactsData)
+
+  let fiveContacts = contactsData.slice(0,5)
+  
+
+  const deleteContact = (contactId) => {
+   
+    // contactsToDisplay.push(); // NEVER MODIFY STATE DIRECTLY !
+
+    const newList = contactsToDisplay.filter( (element) => {
+        return element.id !== contactId;
+    });
+
+    setContactsToDisplay(newList);
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+    {fiveContacts.map((contactsObj) => {
+      return(
+        <div className="contactsdetails">
+          <table>
+              <th>Picture</th> 
+              <th>Name</th>
+              <th>Popularity</th>
+              <th>Won Oscar</th>
+              <th>Won Emmy</th>
+              <th>Delete Contact</th>
+            <tbody>
+              <td className="Photos"><img src={contactsObj.pictureUrl} alt="picture"/></td>
+              <td>{contactsObj.name}</td>
+              <td>{contactsObj.popularity}</td>
+              
+              <td>{ contactsObj.wonOscar === true 
+                ? "🏆"
+                : "Maybe next year 😉"}</td>
+
+              <td>{ contactsObj.wonEmmy === true 
+                ? "🏆"
+                : "Maybe next year 😉"}</td>
+
+              <td>
+                <button onClick={ () => {deleteContact(contactsObj.id)}}>Delete this contact</button>
+              </td>
+
+            
+            </tbody>
+          </table>
+        </div>
+      )
+    })}
     </div>
   );
 }
+
 
 export default App;
