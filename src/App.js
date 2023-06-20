77c6db6a191d927a071f2dafbd6873033003a08e
Chiara888 <carolin@family-klose.de>
2023-06-20 20:31:51 +0200

    iteration 1-3.5

--- src/App.js
+++ src/App.js
@@ -1,25 +1,60 @@
-import logo from './logo.svg';
-import './App.css';
+// src/App.js
+
+import React, { useState } from 'react';
+
+import contacts from "./contacts.json";
+
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+  const [contactsToDisplay, setcontactsToDisplay] = useState(contacts.slice(0, 5));
+
+  const remainingContacts = contacts.slice(5)
+  // const addRandomContact = (() => {
+  //  // I would use math random and math floor but it was not working :( 
+  // })
+
+  return <div className="App">
+
+    <button>Add random contact</button>
+    {/* here is onClick missing but the whole function was not working */}
+
+    <table>
+      <thead>
+        <tr>
+          <th>Picture</th>
+          <th>Name</th>
+          <th>Popularity</th>
+          <th>Won Oscar</th>
+          <th>Won Emmy</th>
+        </tr>
+      </thead>
+      <tbody>
+        {contactsToDisplay.map((contact) => (
+          <tr key={contact.id}>
+            <td>
+              <img src={contact.pictureUrl} alt={contact.name} height="100" />
+            </td>
+            <td>
+              <p>{contact.name}</p>
+            </td>
+            <td>
+              <p>{contact.popularity}</p>
+            </td>
+            <td>
+              <p>{contact.wonOscar ? '🏆' : null}</p>
+            </td>
+            <td>
+            <p>{contact.wonEmmy ? '🏆' : null}</p>
+            </td>
+          </tr>
+        ))}
+      </tbody>
+    </table>
+  </div>;
 }
+
+
+
 
 export default App;
