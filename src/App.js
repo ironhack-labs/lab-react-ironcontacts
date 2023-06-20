c802f8c3e8d5fdaa4a1915ef82870abcec520cbb
gtildis <gretil_15@hotmail.com>
2023-06-20 18:07:37 +0200

    Iteration 1 | Display 5 Contacts ///Iteration 2 | Conditionally Display Awards Info

--- src/App.js
+++ src/App.js
@@ -1,25 +1,57 @@
-import logo from './logo.svg';
-import './App.css';
+/** @format */
+
+import { useState } from "react";
+import "./App.css";
+import contactsData from "./contacts.json";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	let firstFive = contactsData.slice(0, 5);
+	const [contacts, setContacts] = useState(firstFive);
+
+	return (
+		<div className="App">
+			<h1>IronContacts</h1>
+
+			<table>
+				<thead>
+					<tr>
+						<th>Picture</th>
+						<th>Name</th>
+						<th>Popularity</th>
+						<th>Won Oscar</th>
+						<th>Won Emmy</th>
+					</tr>
+				</thead>
+				<tbody>
+					{contacts.map((e) => {
+						return (
+							<tr className="details-tr">
+								<td>
+									<img src={e.pictureUrl} />
+								</td>
+								<td>{e.name}</td>
+								<td>{e.popularity.toFixed(2)}</td>
+								{e.wonOscar ? (
+									<td>
+										<p>🏆</p>
+									</td>
+								) : (
+									<td></td>
+								)}
+								{e.wonEmmy ? (
+									<td>
+										<p>🏆</p>
+									</td>
+								) : (
+									<td></td>
+								)}
+							</tr>
+						);
+					})}
+				</tbody>
+			</table>
+		</div>
+	);
 }
 
 export default App;
