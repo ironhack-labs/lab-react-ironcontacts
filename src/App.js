f7fb18d625308613a2fe003273b8338813327194
Joao Faria <joaorafaria@gmail.com>
2023-06-20 21:51:07 +0100

    iteration 3 and 4

--- src/App.js
+++ src/App.js
@@ -6,14 +6,47 @@ function App() {
   let firstFive = contactsData.slice(0, 5);
   const [contacts, setContacts] = useState(firstFive);
 
+  const [remainingContacts, setRemainingContacts] = useState(
+    contactsData.slice(contacts.length, contactsData.length)
+  );
+
+  function randomCeleb() {
+    let randomNum = Math.random() * remainingContacts.length;
+    let randomCelebrity = remainingContacts[randomNum];
+
+    setContacts([...contacts, randomCelebrity]);
+  }
+
+  function sortAlpha() {
+    const newArr = [...contacts];
+    newArr.sort((a, b) => {
+      if (a.name < b.name) {
+        return -1;
+      }
+      if (a.name > b.name) {
+        return 1;
+      }
+      return 0;
+    });
+    setContacts([...newArr]);
+  }
+  function sortPopularity() {
+    const newSortedArr = [...contacts];
+    newSortedArr.sort((a, b) => {
+      return b.popularity - a.popularity;
+    });
+
+    setContacts([...newSortedArr]);
+  }
+
   return (
     <div className="App">
       <div className="App">
         <h1>Iron Contacts</h1>
         <div className="buttons">
-          {/* <button onClick={randomCeleb}>Add Random Contact</button>
+          <button onClick={randomCeleb}>Add Random Contact</button>
           <button onClick={sortAlpha}>Sort by Name</button>
-          <button onClick={sortPopularity}>Sort by Popularity</button> */}
+          <button onClick={sortPopularity}>Sort by Popularity</button>
         </div>
         <table>
           <thead>
@@ -34,6 +67,10 @@ function App() {
                   </td>
                   <td>{celebrity.name}</td>
                   <td>{celebrity.popularity}</td>
+                  {celebrity.wonOscar && <td>🏆</td>}
+                  {!celebrity.wonOscar && <td></td>}
+                  {celebrity.wonEmmy && <td>🏆</td>}
+                  {!celebrity.wonEmmy && <td></td>}
                 </tr>
               );
             })}
