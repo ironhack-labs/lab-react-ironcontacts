7bc408e28643c47bdf6069289fb504acfd177ce2
Joseph Segbefia <elorma.segbefia@gmail.com>
2023-07-09 23:26:27 +0200

    Complete 3rd Iteration

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,44 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import "./App.css";
 import contactsData from "./contacts.json";
 
 function App() {
-  const [contacts, setContacts] = useState(contactsData.slice(0, 5));
+  const [contacts, setContacts] = useState([]);
+
+  useEffect(() => {
+    const filteredActors = contactsData.filter((actor) =>
+      [
+        "Matt Damon",
+        "David Harbour",
+        "Ansel Elgort",
+        "Maria Bello",
+        "Ryan Reynolds"
+      ].includes(actor.name)
+    );
+
+    setContacts(filteredActors);
+  }, []);
+
+  function addRandomContact() {
+    const newContacts = [...contacts];
+    const randomIndex = Math.floor(Math.random() * contactsData.length);
+    const randomContact = contactsData[randomIndex];
+    newContacts.push(randomContact);
+    setContacts(newContacts);
+  }
   return (
     <div className="App">
       <h1>LAB | React IronContacts</h1>
+
+      <button onClick={addRandomContact}>Add Random Contact</button>
 
       <table>
         <thead>
           <th>Picture</th>
           <th>Name</th>
           <th>Popularity</th>
+          <th>Won Oscar</th>
+          <th>Won Emmy</th>
         </thead>
         <tbody>
           {contacts.map((contact) => (
@@ -22,6 +48,7 @@ function App() {
               </td>
               <td>{contact.name}</td>
               <td>{contact.popularity.toFixed(2)}</td>
+              <td>{contact.wonOscar ? "🏆" : ""}</td>
             </tr>
           ))}
         </tbody>
