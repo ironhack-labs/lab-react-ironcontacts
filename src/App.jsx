255466fdbbd60bcf67a84633b3ea3c19d7317b31
Andy Cockroft <acockroft1994@gmail.com>
2023-07-11 15:19:16 +0200

    iteration one complete

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,34 @@
 import "./App.css";
+import Contacts from "./contacts.json";
+import { useState } from "react";
 
 function App() {
+  const fiveContacts = Contacts.slice(0, 5);
+  const [firstFiveData, setFirstFiveData] = useState(fiveContacts);
+
   return (
     <div className="App">
-      <h1>LAB | React IronContacts</h1>
+      <h1>IronContacts</h1>
+      {firstFiveData.map((Contact) => {
+        return (
+          <table>
+            <thead>
+              <th>Picture</th>
+              <th>Name</th>
+              <th>Popularity</th>
+            </thead>
+            <tbody key={Contact.id}>
+              <tr>
+                <th>
+                  <img style={{ width: 100 }} src={Contact.pictureUrl}></img>
+                </th>
+                <th>{Contact.name}</th>
+                <th>{Contact.popularity.toFixed(2)}</th>
+              </tr>
+            </tbody>
+          </table>
+        );
+      })}
     </div>
   );
 }
