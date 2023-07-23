7af4cbc980fb696aa83ff7f6dfd123781995af1c
Bernard Cardoso <be1.0@hotmail.com>
2023-07-23 21:45:35 +0100

    1, 2, and 3 Iteration Done!

--- src/App.js
+++ src/App.js
@@ -1,23 +1,60 @@
-import logo from './logo.svg';
+import React from 'react';
+import { useState } from 'react';
+import contacts from './contacts.json';
 import './App.css';
 
 function App() {
+  const [contactsList, setContactsList] = useState(contacts.slice(30, 35));
+  const remainingContacts = contacts.slice(0, 30).concat(contacts.slice(35));
+  const getRemainingContacts = () => {
+    const addedContactIds = contactsList.map((contact) => contact.id);
+    return contacts.filter((contact) => !addedContactIds.includes(contact.id));
+  };
+
+  const getRandomContact = () => {
+    const remainingContacts = getRemainingContacts();
+    const randomIndex = Math.floor(Math.random() * remainingContacts.length);
+    return remainingContacts[randomIndex];
+  };
+
+  const handleAddRandomContact = () => {
+    const remainingContacts = getRemainingContacts();
+
+    if (remainingContacts.length === 0) {
+      alert('No more contacts to add!');
+      return;
+    }
+    const randomContact = getRandomContact();
+    setContactsList((prevContacts) => [...prevContacts, randomContact]);
+  };
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Contacts List</h1>
+      <button onClick={handleAddRandomContact}>Add Random Contact</button>
+    <table>
+      <thead>
+        <tr>
+          <th>Picture</th>
+          <th>Name</th>
+          <th>Popularity</th>
+          <th>Oscar</th>
+          <th>Emmy</th>
+        </tr>
+      </thead>
+      <tbody>
+        {contactsList.map((contact) => (
+          <tr key={contact.id}>
+            <td>
+              <img src={contact.pictureUrl} alt={contact.name} width="100" />
+            </td>
+            <td>{contact.name}</td>
+            <td>{contact.popularity}</td>
+            <td>{contact.wonOscar ? '🏆' : ''}</td>
+            <td>{contact.wonEmmy ? '🏆' : ''}</td>
+          </tr>
+        ))}
+      </tbody>
+    </table>
     </div>
   );
 }
