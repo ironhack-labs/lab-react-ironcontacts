a7930927893bd9d0590fe1ac1e8759bf96c480e0
Walter Bernal <wltrbm@gmail.com>
2023-07-24 13:17:38 -0700

    Iteration 1

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
-
+import contactsData from './contacts.json'
+import { useState } from 'react'
+import ContactList from "./components/ContactList"
 
 function App() {
+  const [contacts, setContacts] = useState(contactsData.slice(0, 5))
+  
+  console.log(contacts)
+
   return (
     <div className="App">
-
+      <ContactList contacts={contacts}/>
     </div>
   )
 }
