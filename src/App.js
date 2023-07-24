87f471f864e1e75bd4fd2cf36a35dd0f5329654f
Patricia <marie.lou.trips@gmail.com>
2023-07-24 12:42:28 +0100

    iteration 3 done

--- src/App.js
+++ src/App.js
@@ -1,29 +1,46 @@
 import "./App.css";
-import contacts from "./contacts.json";
+import contactsList from "./contacts.json";
 import React, { useState } from 'react';
 
 function App() {
 
-  const [fiveContacts] = useState(contacts.slice(0, 5));
+
+  const [fiveContacts] = useState(contactsList.slice(0, 5));
+  const [contacts, updateContacts] = useState(fiveContacts);
+
+  const addContact = () => {
+    let contactStart = contactsList.slice(5);
+    let randomContact = Math.floor(Math.random() * contactStart.length);
+    const newContact = contactStart[randomContact];
+    const updatedContacts = [...contacts, newContact];
+    updateContacts(updatedContacts);
+  };
+
 
   return <div className="App">
+ 
+  <button onClick={addContact}>Add Random Contact</button>
     <table>
         <thead>
           <tr>
-          
             <th>Picture</th>
             <th>Name</th>
             <th>Popularity</th>
+            <th>Won an Oscar  </th>
+            <th>  Won an Emmy</th>
           </tr>
         </thead>
         <tbody>
-          {fiveContacts.map((contact) => (
+          {contacts.map((contact) => (
             <tr key={contact.id}>
               <td>
                 <img src={contact.pictureUrl} alt={contact.name} style={{ width: '100px', height: 'auto' }} />
               </td>
               <td>{contact.name}</td>
               <td>{contact.popularity}</td>
+
+              <td>{contact.wonOscar ? "🏆" : null}</td>
+              <td>{contact.wonEmmy ? "🏆" : null}</td>
             </tr>
           ))}
         </tbody>
