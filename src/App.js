eb179654afd5b0a384b3fa42164e369668a03e92
Andres Franco <andresfranla@gmail.com>
2023-07-25 07:44:08 -0600

    Iteration 4 done

--- src/App.js
+++ src/App.js
@@ -3,11 +3,28 @@ import './App.css';
 import contactsData from "./contacts.json"
 import ContactList from './components/ContactList';
 import AddRandomButton from './components/AddRandomButton';
+import SortPopButton from './components/SortPopButton';
+import SortNameButton from './components/SortNameButton';
 
 const initialContacts = contactsData.slice(0,5)
 
 function App() {
   const [contacts, setContacts] = useState(initialContacts);
+  const sortByPop = () => { 
+    const contactsCopy = JSON.parse(JSON.stringify(contacts))
+    const sortedContacts = contactsCopy.sort((a, b) => b.popularity - a.popularity)
+    setContacts(sortedContacts)
+  }
+  
+  const sortByName = () => {
+    const contactsCopy = JSON.parse(JSON.stringify(contacts))
+    const sortedContacts = contactsCopy.sort((a, b) => {
+      if (a.name - b.name) return 1
+      else if (a.name < b.name) return -1
+      else return 0
+    })
+    setContacts(sortedContacts)
+  }
 
   const newContact = () => {
     if (contactsData.length > 0) { 
@@ -20,8 +37,11 @@ function App() {
     <div className="App">
         <ContactList contacts={contacts}/>
           <h2>IronContacts</h2>
-        <AddRandomButton contacts={contacts} newContact={newContact} />
-        <ContactList contacts={contacts} />
+          <div className='buttons'>
+            <AddRandomButton contacts={contacts} newContact={newContact} />
+            <SortPopButton sortMethod={sortByPop}/>
+            <SortNameButton sortMethod={sortByName}/>
+        </div>
     </div>
   );
 }
