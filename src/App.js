740e89e63479157ae30f3bbcdccb99619c909f0f
iceisland12 <luisguile495@gmail.com>
2023-07-31 15:50:29 -0400

    Iteration 1 & 2

--- src/App.js
+++ src/App.js
@@ -1,23 +1,56 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
+import contacts from './contacts.json';
 
 function App() {
+  // Create a state variable to store the first 5 contacts
+  const [contactList, setContactList] = useState(contacts.slice(15, 20));
+  const [remainingContacts, setRemainingContacts] = useState(contacts.slice(20));
+
+  // Function to add a random contact to the contactList
+  const addRandomContact = () => {
+    if (remainingContacts.length > 0) {
+      const randomIndex = Math.floor(Math.random() * remainingContacts.length);
+      const randomContact = remainingContacts[randomIndex];
+
+      // Create a new array with the random contact added to the contactList
+      setContactList((prevContactList) => [...prevContactList, randomContact]);
+
+      // Update the remainingContacts array by removing the selected random contact
+      setRemainingContacts((prevRemainingContacts) =>
+        prevRemainingContacts.filter((contact) => contact.id !== randomContact.id)
+      );
+    }
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Contact List</h1>
+      <button onClick={addRandomContact}>Add Random Contact</button>
+      <table>
+        <thead>
+          <tr>
+            <th>Picture</th>
+            <th>Name</th>
+            <th>Popularity</th>
+            <th>Won an Oscar</th>
+            <th>Won an Emmy</th>
+          </tr>
+        </thead>
+        <tbody>
+          {contactList.map((contact) => (
+            <tr key={contact.id}>
+              <td>
+                <img src={contact.pictureUrl} alt={contact.name} style={{ width: '50px', height: '50px' }} />
+              </td>
+              <td>{contact.name}</td>
+              <td>{contact.popularity}</td>
+              <td>{contact.wonOscar ? '🏆' : null}</td>
+              <td>{contact.wonEmmy ? '🏆' : null}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
     </div>
   );
 }
