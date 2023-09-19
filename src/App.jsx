10c92af3fea6dfbbaf973b3532f24449666ea4b4
Alice <johndoe@example.com>
2023-09-19 14:48:32 +0200

    first update

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,44 @@
 import "./App.css";
+import contacts from "./contacts.json";
+import { useState } from "react";
 
 function App() {
+  const [contactsArr, setContactsArr] = useState(contacts.slice(0, 5)); //5
+
   return (
     <div className="App">
       <h1>LAB | React IronContacts</h1>
+
+      <table>
+        <thead>
+          <tr>
+            <th>Picture</th>
+            <th>Name</th>
+            <th>Popularity</th>
+          </tr>
+        </thead>
+        <tbody>
+          {contactsArr.map((contact) => {
+            return <tr contact={contact} key={contact.id}></tr>;
+          })}
+
+          {contactsArr.map((contact) => {
+            return (
+              <tr contact={contact} key={contact.id}>
+                <td>
+                  <img
+                    className="contact-foto"
+                    src={contact.pictureUrl}
+                    alt={contact.name}
+                  />
+                </td>
+                <td>{contact.name}</td>
+                <td>{contact.popularity.toFixed(2)}</td>
+              </tr>
+            );
+          })}
+        </tbody>
+      </table>
     </div>
   );
 }
