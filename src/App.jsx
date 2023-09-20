9e5b477e38b70f7350fa662fe982cf5df7a79e37
Alice <johndoe@example.com>
2023-09-20 13:23:41 +0200

    omar's one added, explanation also

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,37 @@ function App() {
   const [contactsArr, setContactsArr] = useState(contacts.slice(0, 5)); //5
 
   function getRandomContact() {
+    
+    //Omar WAY (two ways)
+
+    // One Way:
+    // const randomIndex = Math.floor(Math.random() * remainingContacts.length);
+    // const randomContact = remainingContacts[randomIndex];
+    // setContacts((prevState) => [randomContact, ...prevState]);
+    // setRemainingContacts((prevState) =>
+    //   prevState.filter((e) => e.id !== randomContact.id)
+    // );
+    //FILTER Method (not mutating, creating a NEW ARRAY):  
+    // If they are not equal, the element is included 
+    // in the new array; otherwise, it's filtered out.
+
+
+    //Another Way:
+    // const updateRemaining = [...remainingContacts];
+    // let randomNum = Math.floor(Math.random() * remainingContacts.length);
+    // let randomContact = updateRemaining.splice(randomNum, 1)[0];
+    // const updatedContacts = [...contacts, randomContact];
+    // setContacts(updatedContacts);
+    // setRemainingContacts(updateRemaining);
+
+
+    // "let randomContact = updateRemaining.splice(randomNum, 1)[0];""
+    // HERE: mutating updateRemaining Array by slicing the chosen random
+    // contact (which is pushed to the contacts to show so that
+    //the remaining one doesn't have already chosen one)
+    
+    
+    // My WAY :D 
     const remainingContacts = contacts.slice(5);
     console.log(remainingContacts);
 
@@ -22,17 +53,17 @@ function App() {
     newNameArr.sort((a, b) => {
       return a.name.localeCompare(b.name); //return without -> can work?!?!?
     });
-    console.log(newNameArr) // new array
-    setContactsArr(newNameArr) //update
+    console.log(newNameArr); // new array
+    setContactsArr(newNameArr); //update
   }
+
   function sortPopularity() {
     const newNameArr = [...contactsArr];
     newNameArr.sort((a, b) => {
       return b.popularity - a.popularity; //return without -> can work?!?!?
     });
-    console.log(newNameArr) // new array
-    setContactsArr(newNameArr) //update
-
+    console.log(newNameArr); // new array
+    setContactsArr(newNameArr); //update
   }
 
   const deleteButton = (contactId) => {
@@ -40,7 +71,7 @@ function App() {
       return contact.id !== contactId; // not =, get the new array (filter)
     });
 
-    setContactsArr(filterContacts); // not deleted one 
+    setContactsArr(filterContacts); // not deleted one
   };
 
   return (
@@ -77,10 +108,12 @@ function App() {
                 <td>{contact.popularity.toFixed(2)}</td>
                 <td>{contact.wonOscar ? "🏆" : ""}</td>
                 <td>{contact.wonEmmy ? "🏆" : ""}</td>
-               <td>
-               <button onClick ={()=>deleteButton(contact.id)}>  Delete 🗑️</button>
-               </td>
-              
+                <td>
+                  <button onClick={() => deleteButton(contact.id)}>
+                    {" "}
+                    Delete 🗑️
+                  </button>
+                </td>
               </tr>
             );
           })}
