cc212d7ea55d8131d40e9644ec08e4fc9430f4ee
acanest83 <133660942+acanest83@users.noreply.github.com>
2023-10-06 21:36:08 +0200

    Iteración 3

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,36 @@
+import React, {useState} from "react";
 import "./App.css";
-
+import contacts from "./contacts.json";
 function App() {
+  const [contacts, setContacts]= useState (contacts.slice(0,5));
   return (
     <div className="App">
       <h1>LAB | React IronContacts</h1>
+      <table>
+        <thead>
+          <tr>
+            <th>Photo</th>
+            <th>Name</th>
+            <th>Popularity</th>
+            <th>Won Oscar</th>
+            <th>Won Emmy</th>
+          </tr>
+        </thead>
+        <tbody>
+          {contacts.map((contact, index) => (
+            <tr key={index}>
+              <td>
+                <img className="contact-image" src={contact.pictureUrl} alt={contact.name} />
+              </td>
+              <td>{contact.name}</td>
+              <td>{contact.popularity}</td>
+              <td>{contact.wonOscar ? "🏆": ""} </td>
+              <td>{contact.wonEmmy ? "🏆": "" } </td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
     </div>
   );
 }
-
 export default App;
