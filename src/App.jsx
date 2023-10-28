2224106e24101a0b43df30de8180678f2423fc0a
GasparBonari <gasparbonari@gmail.com>
2023-10-28 14:19:46 +0200

    working on lab

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,34 @@
+import { useState } from "react";
 import "./App.css";
+import contactsData from "./contacts.json"
 
 function App() {
+
+  const [contacts, setContacts] = useState(contactsData)
+
+  const firstFive = contacts.slice(0, 5);
+
   return (
     <div className="App">
       <h1>LAB | React IronContacts</h1>
+      <table>
+        <thead>
+          <tr>
+            <th>Picture</th>
+            <th>Name</th>
+            <th>Popularity</th>
+          </tr>
+        </thead>
+        {firstFive.map(person => (
+          <tbody key={person.id}>
+            <tr key={person.id}>
+              <td>{person.picture}</td>
+              <td>{person.name}</td>
+              <td>{person.popularity}</td>
+            </tr>
+          </tbody>
+        ))}
+      </table>
     </div>
   );
 }
