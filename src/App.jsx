be1123008c26a5782888514f396c7eb0b9f5f02a
GasparBonari <gasparbonari@gmail.com>
2023-10-30 10:42:20 +0100

    Done

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,75 @@
 import { useState } from "react";
 import "./App.css";
-import contactsData from "./contacts.json"
+import contactsData from "./contacts.json";
 
 function App() {
+  const [contacts, setContacts] = useState(contactsData);
+  const [displayedContacts, setDisplayedContacts] = useState(5);
 
-  const [contacts, setContacts] = useState(contactsData)
+  const handleRandomContact = () => {
+    const remainingContacts = contacts.slice(displayedContacts);
+    const randomContacts = [];
 
-  const firstFive = contacts.slice(0, 5);
+    for (let i = 0; i < 3 && i < remainingContacts.length; i++) {
+      const randomIndex = Math.floor(Math.random() * remainingContacts.length);
+      randomContacts.push(remainingContacts[randomIndex]);
+      remainingContacts.splice(randomIndex, 1);
+    }
+
+    setContacts([...randomContacts, ...contacts]);
+    setDisplayedContacts(displayedContacts + 3);
+  };
+
+  const handleSortName = () => {
+    const sortedContacts = [...contacts];
+    sortedContacts.sort((a, b) => a.name.localeCompare(b.name));
+    setContacts(sortedContacts);
+  };
+
+  const handleSortPopularity = () => {
+    const sortedContacts = [...contacts];
+    sortedContacts.sort((a, b) => b.popularity - a.popularity);
+    setContacts(sortedContacts);
+  }
+
+  const handleDelete = (id) => {
+    const newContacts = contacts.filter(contact => contact.id !== id);
+    setContacts(newContacts)
+  }
 
   return (
     <div className="App">
       <h1>LAB | React IronContacts</h1>
+      <div className="btns">
+        <button onClick={handleRandomContact}>Add Random Contact</button>
+        <button onClick={handleSortName}>Sort By Popularity</button>
+        <button onClick={handleSortPopularity}>Sort By Name</button>
+      </div>
       <table>
         <thead>
           <tr>
             <th>Picture</th>
             <th>Name</th>
             <th>Popularity</th>
+            <th>Won an Oscar</th>
+            <th>Won an Emmy</th>
+            <th>Action</th>
           </tr>
         </thead>
-        {firstFive.map(person => (
-          <tbody key={person.id}>
-            <tr key={person.id}>
-              <td>{person.picture}</td>
+        <tbody>
+          {contacts.slice(0, displayedContacts).map((person) => (
+            <tr className="actorsContainer" key={person.id}>
+              <td>
+                <img src={person.pictureUrl} alt={person.name}></img>
+              </td>
               <td>{person.name}</td>
-              <td>{person.popularity}</td>
+              <td>{person.popularity.toFixed(2)}</td>
+              <td>{person.wonOscar ? "🏆" : "-"}</td>
+              <td>{person.wonEmmy ? "🌟" : "-"}</td>
+              <td><button onClick={() => handleDelete(person.id)}>Delete</button></td>
             </tr>
-          </tbody>
-        ))}
+          ))}
+        </tbody>
       </table>
     </div>
   );
