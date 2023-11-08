6076fcda76f6d90e169f74f3d4dd091c46c6ea87
NISHA PATIL <prashantpatil@prashants-air.kpn>
2023-11-08 21:22:50 +0100

    iteration 1 solved, except table

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,34 @@
 import "./App.css";
+import contacts from "./contacts.json";
+import { useState } from "react";
+console.log("hello world");
 
 function App() {
+  const [firstFive, setFirstFive] = useState(contacts);
+
+  const newList = contacts.slice(0, 5);
+  console.log(newList);
   return (
-    <div className="App">
-      <h1>LAB | React IronContacts</h1>
-    </div>
+    <>
+      <table>
+  <tr>
+    <th>Picture</th>
+    <th>Name</th>
+    <th>Popularity</th>
+  </tr>
+  <tr>
+    <td></td>
+    <td></td>
+    <td></td>
+  </tr>
+  <tr>
+    <td></td>
+    <td></td>
+    <td></td>
+  </tr>
+</table>
+
+    </>
   );
 }
 
