f590caf78f3a9c9f3eeaf91fc11d9b1a9de14bf7
Fran Martinez <pmg.webdev@gmail.com>
2023-11-08 23:09:13 +0100

    until iteration 3, not managed to render new contact

--- src/App.jsx
+++ src/App.jsx
@@ -1,74 +1,57 @@
 import "./App.css";
-import contacts from "./contacts.json"
+import contacts from "./contacts.json";
 import { useState } from "react";
-
 
 function App() {
   const [contactsList, setContactsList] = useState(contacts);
 
   const fiveContacts = contactsList.slice(0, 5);
-  console.log(fiveContacts);
+  // console.log(fiveContacts);
+
+  console.log(contactsList);
+
+  const newRandomContact = () => {
+    const randomIndex = Math.floor(Math.random() * contactsList.length);
+    const randomContact = contactsList[randomIndex];
+    // console.log({randomContact})
+    // const updatedContactList = [...contactsList];
+    // updatedContactList.push(randomContact);
+
+    console.log(contactsList)
+    // setContactsList(newList);
+  };
 
   return (
     <div className="App">
       <h1>LAB | React IronContacts</h1>
+
+      <button onClick={() => newRandomContact()}>Add Random Contact</button>
+
       <table className="Table">
         <thead>
-        <tr>
-          <th>Picture</th>
-          <th>Name</th>
-          <th>Popularity</th>
-          <th>Won Oscar</th>
-          <th>Won Emy</th>
-        </tr>
+          <tr>
+            <th>Picture</th>
+            <th>Name</th>
+            <th>Popularity</th>
+            <th>Won Oscar</th>
+            <th>Won Emy</th>
+          </tr>
         </thead>
         <tbody>
-          <tr>
-            <td>
-              <img src={fiveContacts[0].pictureUrl} alt="First contact picture" />
-            </td>
-            <td>{fiveContacts[0].name} </td>
-            <td>{Math.floor(fiveContacts[0].popularity)} </td>
-            <td>{fiveContacts[0].wonOscar ? "🏆" : ""} </td>
-            <td>{fiveContacts[0].wonEmmy ? "🌟" : ""} </td>
-          </tr>
-          <tr>
-            <td>
-              <img src={fiveContacts[1].pictureUrl} alt="Second contact picture" />
-            </td>
-            <td>{fiveContacts[1].name} </td>
-            <td>{Math.floor(fiveContacts[1].popularity)} </td>
-            <td>{fiveContacts[1].wonOscar ? "🏆" : ""} </td>
-            <td>{fiveContacts[1].wonEmmy ? "🌟" : ""} </td>
-            
-          </tr>
-          <tr>
-            <td>
-              <img src={fiveContacts[2].pictureUrl} alt="Third contact picture" />
-            </td>
-            <td>{fiveContacts[2].name} </td>
-            <td>{Math.floor(fiveContacts[2].popularity)} </td>
-            <td>{fiveContacts[2].wonOscar ? "🏆" : ""} </td>
-            <td>{fiveContacts[2].wonEmmy ? "🌟" : ""} </td>
-          </tr>
-          <tr>
-            <td>
-              <img src={fiveContacts[3].pictureUrl} alt="Fourth contact picture" />
-            </td>
-            <td>{fiveContacts[3].name} </td>
-            <td>{Math.floor(fiveContacts[3].popularity)} </td>
-            <td>{fiveContacts[3].wonOscar ? "🏆" : ""} </td>
-            <td>{fiveContacts[3].wonEmmy ? "🌟" : ""} </td>
-          </tr>
-          <tr>
-            <td>
-              <img src={fiveContacts[4].pictureUrl} alt="Fifth contact picture" />
-            </td>
-            <td>{fiveContacts[4].name} </td>
-            <td>{Math.floor(fiveContacts[4].popularity)} </td>
-            <td>{fiveContacts[4].wonOscar ? "🏆" : ""} </td>
-            <td>{fiveContacts[4].wonEmmy ? "🌟" : ""} </td>
-          </tr>
+          {fiveContacts.map((contact, index) => (
+            <tr key={index}>
+              <td>
+                <img
+                  src={contact.pictureUrl}
+                  alt={`Contact picture ${index + 1}`}
+                />
+              </td>
+              <td>{contact.name}</td>
+              <td>{contact.popularity.toFixed(2)}</td>
+              <td>{contact.wonOscar ? "🏆" : ""}</td>
+              <td>{contact.wonEmmy ? "🌟" : ""}</td>
+            </tr>
+          ))}
         </tbody>
       </table>
     </div>
