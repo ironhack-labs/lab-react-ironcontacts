b8990fd29dbbb506f64d3685c38ae2e04a4bf693
Vasco Martins <vasco.martins.drummer@gmail.com>
2023-11-08 19:07:15 +0000

    Iterations 1, 2 and 4 so far

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,48 @@
 import "./App.css";
+import { useState } from "react";
+import contacts from "./contacts.json";
 
 function App() {
-  return (
-    <div className="App">
-      <h1>LAB | React IronContacts</h1>
-    </div>
-  );
+    const [firstFive, setFirstFive] = useState(contacts.slice(0, 5));
+
+    const firstFiveCopy = structuredClone(firstFive);
+
+    const sortAlphabetically = (() => {
+      //users.sort((a, b) => a.firstname.localeCompare(b.firstname))
+      firstFiveCopy.sort((a, b) => a.name.localeCompare(b.name));
+      setFirstFive(firstFiveCopy);
+    });
+
+    return (
+        <div className="App">
+            <h1>LAB | React IronContacts</h1>
+            <button onClick={sortAlphabetically}>Sort by name</button>
+            <button>Sort by Popularity</button>
+            <table>
+                <tr>
+                    <th>Picture</th>
+                    <th>Name</th>
+                    <th>Popularity</th>
+                    <th>Won Oscar</th>
+                    <th>Won Emmy</th> 
+                </tr>
+
+                {firstFive.map((contactObj) => {
+                    return (
+                        <tr>
+                            <td>
+                                <img src={contactObj.pictureUrl} />
+                            </td>
+                            <td>{contactObj.name}</td>
+                            <td>{Math.round(contactObj.popularity * 100) / 100}</td>
+                            <td>{contactObj.wonOscar ? "🏆" : ""} </td>
+                            <td>{contactObj.wonEmmy ? "🌟" : ""} </td>
+                        </tr>
+                    );
+                })}
+            </table>
+        </div>
+    );
 }
 
 export default App;
