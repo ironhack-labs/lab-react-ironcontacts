7720902d4682b3cf6ab84524ed51f64cb1076dd4
yvzbly <yavuzbalay90@gmail.com>
2023-11-08 21:34:42 +0100

    done until it.3

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,49 @@
+import React, { useState } from "react";
 import "./App.css";
+import contacts from "./contacts.json";
 
 function App() {
+  const firstFiveActors = contacts.slice(0, 5);
+  const [displayContacts, setDisplayContacts] = useState(firstFiveActors);
+
   return (
-    <div className="App">
-      <h1>LAB | React IronContacts</h1>
+    <div>
+      <table>
+        <thead>
+          <tr>
+            <th>Picture</th>
+            <th>Name</th>
+            <th>Popularity</th>
+            <th>Won an Oscar</th>
+            <th>Won an Emmy</th>
+          </tr>
+        </thead>
+        <tbody>
+          {displayContacts.map((contact) => (
+            <tr key={contact.id}>
+              <td>
+                <img src={contact.pictureUrl} alt={contact.name} width="100" />
+              </td>
+              <td>{contact.name}</td>
+              <td>{contact.popularity}</td>
+              <td>
+                {contact.wonOscar ? (
+                  <span role="img" aria-label="Trophy">
+                    🏆
+                  </span>
+                ) : null}
+              </td>
+              <td>
+                {contact.wonEmmy ? (
+                  <span role="img" aria-label="Star">
+                    🌟
+                  </span>
+                ) : null}
+              </td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
     </div>
   );
 }
