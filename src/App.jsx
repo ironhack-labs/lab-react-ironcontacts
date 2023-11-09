f88a5abd315ae4d1cb89d2a96641fe43064b9c41
Zohra <zohra.boukhatem1@gmail.com>
2023-11-09 08:52:48 +0100

    beginning

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,20 @@
 import "./App.css";
+import array from "./contacts.json"
+import { useState } from "react";
 
 function App() {
+
+  const [contacts, displayContacts] = useState(array)
+  const newArray = contacts.slice(0,5)
+
   return (
+    <>
+    {newArray}
     <div className="App">
       <h1>LAB | React IronContacts</h1>
     </div>
-  );
+    </>
+    );
 }
 
 export default App;
