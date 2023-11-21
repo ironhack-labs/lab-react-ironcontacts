00fc9f0c5ffc525c9b6f1c23aca140bb97950e84
alvaro sanchez <alvaro_piro_@hotmail.com>
2023-11-21 16:41:04 +0100

    ?

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,74 @@
-import "./App.css";
+import { useState } from "react"
+import "./App.css"
+import contacts from "./contacts.json"
+
+
 
 function App() {
+
+  const arrayCopy = [...contacts]
+
+  const contactss = arrayCopy.splice(0, 5)
+
+  const [contact, setContact] = useState(contactss)
+
+
+
+  const addRandomContact = () => {
+
+    const number = Math.floor(Math.random() * (contacts.length - 0) + 0)
+
+    const randomContact = contacts[number]
+    const contactsCopy = [randomContact, ...contact]
+
+    setContact(contactsCopy)
+
+  }
+
+  const sortName = () => {
+    const sortCopy = [...contact]
+    sortCopy.sort((contact1, contact2) => {
+      contact1.name.localeCompare(contact2.name)
+    })
+
+    setContact(sortCopy)
+  }
+
+
+
   return (
+
     <div className="App">
       <h1>LAB | React IronContacts</h1>
-    </div>
-  );
+      <table className="Contacts">
+        <button className="btn" onClick={addRandomContact}>New conctac</button>
+        <button className="btn" onClick={sortName}>Sort by name</button>
+        <tr>
+          <th>Picture</th>
+          <th>Name</th>
+          <th>Popularity</th>
+          <th>Won an Oscar</th>
+          <th>Won an Emmy</th>
+
+        </tr>
+        {
+          contact.map(elm => {
+            return (
+              <tr key={elm.id}>
+                <th><img src={elm.pictureUrl} style={{ width: '55px', height: '60px' }} /></th>
+                <th>{elm.name}</th>
+                <th>{elm.popularity}</th>
+                <th>{elm.wonOscar ? `🏆` : ""}</th>
+                <th>{elm.wonEmmy ? `🌟` : ""}</th>
+              </tr>
+            )
+          })
+        }
+      </table>
+
+    </div >
+
+  )
 }
 
-export default App;
+export default App
