6f0d2f354945a142711c5cd09ddfb55aff643dcd
jorge Centeno <eljorgeceneno@gmail.com>
2024-01-20 16:51:47 +0100

    Changes with Omar

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,36 @@
 import "./App.css";
+import contactsData from"./contacts.json"
+import { useState } from "react";
+
 
 function App() {
-  return (
+  const [contacts,setContacts] = useState(contactsData.slice(0,5))
+  console.log(contacts)
+  return(
     <div className="App">
       <h1>LAB | React IronContacts</h1>
+      <table>
+  <tr>
+    <td>Name</td>
+    <td>Image</td>
+    <td>Popularity</td>
+    <td>Amy</td>
+    <td>Oscar</td>
+  </tr>
+  {contacts.map((eachContact)=>{
+    return (
+      <tr key={eachContact.id}>
+   <td>{eachContact.name}</td>
+   <td ><img id="actor-picture" src={eachContact.pictureUrl} alt="picture of actor" /></td> 
+   <td>{eachContact.popularity}</td>
+   <td>{eachContact.wonEmmy && <p>🏆</p>}</td>
+   <td>{eachContact.wonOscar && <p>🌟</p>}</td>
+      </tr>
+    )
+  }
+  )}
+</table>
+      
     </div>
   );
 }
