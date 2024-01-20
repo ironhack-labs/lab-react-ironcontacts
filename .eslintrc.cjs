52ba58b53033decf11f3ab35fd6fcd93942bff3e
Ioannis Tourtouras <yiannistourtce@gmail.com>
2024-01-20 17:57:01 +0200

    1st iteration

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,16 +1,20 @@
 module.exports = {
-  env: { browser: true, es2020: true, node: true },
+  root: true,
+  env: { browser: true, es2020: true },
   extends: [
-    "eslint:recommended",
-    "plugin:react/recommended",
-    "plugin:react/jsx-runtime",
-    "plugin:react-hooks/recommended",
+    'eslint:recommended',
+    'plugin:react/recommended',
+    'plugin:react/jsx-runtime',
+    'plugin:react-hooks/recommended',
   ],
-  parserOptions: { ecmaVersion: "latest", sourceType: "module" },
-  settings: { react: { version: "18.2" } },
-  plugins: ["react-refresh"],
+  ignorePatterns: ['dist', '.eslintrc.cjs'],
+  parserOptions: { ecmaVersion: 'latest', sourceType: 'module' },
+  settings: { react: { version: '18.2' } },
+  plugins: ['react-refresh'],
   rules: {
-    "react-refresh/only-export-components": "warn",
-    "react/prop-types": 0,
+    'react-refresh/only-export-components': [
+      'warn',
+      { allowConstantExport: true },
+    ],
   },
-};
+}
--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,45 @@
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
 import "./App.css";
+import contact from "./contacts.json";
+
+// no components recommendation
 
 function App() {
+  const [contacts, setContacts] = useState(contact.slice(0, 5));
+  console.log(contacts);
+
   return (
-    <div className="App">
-      <h1>LAB | React IronContacts</h1>
+    <div>
+      <table>
+        <thead>
+          <tr>
+            <th colspan="2">Ironcontacts</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <th>Picture</th>
+            <th>Name</th>
+            <th>Popularity</th>
+          </tr>
+          {contacts.map((contact)=> {
+            return(<tr>
+            <td>
+              <img
+                src={contact.pictureUrl}
+                alt="Actor-image"
+                height={100}
+              ></img>
+            </td>
+            <td>{contact.name}</td>
+            <td>{contact.popularity}</td>
+            </tr>
+            )
+          })}          
+        </tbody>
+      </table>
     </div>
   );
 }
