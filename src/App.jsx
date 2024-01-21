9ca99db3e1836d0f13647339e034d5d4ca4a7c63
Nadine Rangosch <nrangosch@gmail.com>
2024-01-21 16:15:24 +0100

    all iterations finished

--- src/App.jsx
+++ src/App.jsx
@@ -5,10 +5,54 @@ import "./App.css";
 function App() {
   const [contacts, setContacts] = useState(contactsData.slice(0,5))
  
-  console.log(contacts)
+  function addRandomContact(){
+    let contactsCopy = [...contacts]
+    let randomNum = Math.floor(Math.random() * contactsData.length)
+    do {
+      randomNum = Math.floor(Math.random() * contactsData.length);
+      const isDuplicate = contactsCopy.some((element) => {
+        return contactsData[randomNum].id === element.id;
+      });
+  
+      if (!isDuplicate) {
+        contactsCopy.push(contactsData[randomNum]);
+        setContacts(contactsCopy);
+        break;
+      }
+    } while (true);
+  
+  }
+
+function sortByPopularity(){
+  let contactsCopy = [...contacts]
+  contactsCopy.sort((a,b)=>{
+    return b.popularity - a.popularity
+  })
+  setContacts(contactsCopy)
+}
+
+function sortByName(){
+  let contactsCopy = [...contacts]
+  contactsCopy.sort((a,b)=>{
+    return a.name.localeCompare(b.name)
+  })
+  setContacts(contactsCopy)
+}
+
+function deleteContact(actorId){
+  const filteredContacts = contacts.filter((element)=>{
+    return element.id !== actorId
+    
+  })
+  setContacts(filteredContacts)
+}
+
   return (
     <div className="App">
       <h1>LAB | React IronContacts</h1>
+      <button onClick={addRandomContact}>Add Random Contact</button>
+      <button onClick={sortByPopularity}>Sort by popularity</button>
+      <button onClick={sortByName}>Sort by name</button>
       <table>
         <thead>
           <tr>
@@ -28,6 +72,7 @@ function App() {
             <td>{actor.popularity.toFixed(2)}</td>
             {actor.wonOscar ? <td>🏆</td> : <td></td>}
             {actor.wonEmmy ? <td>🌟</td> : <td></td>}
+            <td><button onClick={()=>{deleteContact(actor.id)}}>Delete</button></td>
           </tr>)
         })}
           
