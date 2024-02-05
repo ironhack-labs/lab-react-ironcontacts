0f99410a10e8a04f9c5e7ffcd7652378a56eb4e7
Nihat Algan <burakalgan@gmail.com>
2024-02-05 13:25:27 +0100

    done first itenary

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,57 @@
+import { useState } from "react";
+import contacts from "./contacts.json";
 import "./App.css";
 
 function App() {
+  const [people, setPeople] = useState(contacts.slice(0, 5));
   return (
     <div className="App">
       <h1>LAB | React IronContacts</h1>
+      <table>
+        <tr>
+          <th>Picture</th>
+          <th>Name</th>
+          <th>Popularity</th>
+        </tr>
+        {people.map((person) => {
+          return (
+            <tr>
+              <td>
+                <img id="actor_img" src={person.pictureUrl} />
+              </td>
+              <td>{person.name}</td>
+              <td>{person.popularity}</td>
+            </tr>
+          );
+        })}
+      </table>
     </div>
   );
 }
 
 export default App;
+
+// import contactsData from "./contacts.json";
+// import "./App.css";
+
+// function people() {
+//   const [people, setPeople] = useState(contactsData);
+
+//   return (
+//     <div>
+//       <h2>Movie List</h2>
+
+//       {movies.map((contactsData) => {
+//         return (
+//           <MovieCard
+//             key={movieData._id}
+//             movie={movieData}
+//             deleteMovie={deleteMovie}
+//           />
+//         );
+//       })}
+//     </div>
+//   );
+// }
+
+// export default App;
