ddd7ead80ad75b63257aa2f84feeab28f65c950f
Ama Williams <53152990+Tetsewa@users.noreply.github.com>
2024-03-10 23:38:51 +0100

    done

--- src/App.jsx
+++ src/App.jsx
@@ -5,28 +5,32 @@ import { useState } from "react";
 
 
 function App() {
-  const initialContacts = contactsJSON.slice (0,5)
-  const [contacts, setContacts] = useState(initialContacts);
-  // const [showContact, setShowContact] = useState(true);
-// Iteration 3 begin
-const [remainingContacts, setRemainingContacts] = useState(
-  contactsJSON.slice(5)
-); 
+  const initialContacts=contactsJSON
+  const [contacts, setContacts] = useState(initialContacts.slice(0,5));
   
-  const addRandomContact = () => {
-     if (remainingContacts.length > 0) {
-      const randomIndex = Math.floor(Math.random() * remainingContacts.length);
-      const randomContact = remainingContacts[randomIndex];
-      setContacts((prevContacts) => [...prevContacts,randomContact ]);
 
-    setRemainingContacts((prevRemainingContacts) => {
-      const updatedRemainingContacts = [...prevRemainingContacts];
-      updatedRemainingContacts.splice(randomIndex, 1);
-      return updatedRemainingContacts;
-    });
+// Iteration 3 begin
+const AddRandomContact = () => {
+   
+  if (contacts.length === initialContacts.length) {
+    return;
   }
-};
- // Iteration 3 end 
+    
+  const random = Math.floor(Math.random() * initialContacts.length);
+  
+  const randomContact = initialContacts[random];
+
+  
+  
+  if (contacts.includes(randomContact)) {
+    AddRandomContact();
+  } else {
+    setContacts(  [randomContact, ...contacts] ); 
+
+  }
+
+}
+  //Iteration 3 end 
 
  //Iteration 4 begin sort function
  const handleSortPopularity = () => {
@@ -64,16 +68,18 @@ const handlesortName = () => {
       <h1>IronContacts</h1>
 
     {/* Iteration 3 button*/}
-      <button onClick= {addRandomContact}>Add Random Contact</button>
+      <div className="all-btn">
+      <button onClick= {AddRandomContact}>Add Random Contact</button>
       <button onClick= {handleSortPopularity}>Sort by popularity</button>
       <button onClick= {handlesortName}>Sort by name</button>
+      </div>
 
       
      
 
 
       <table className="table">
-        <thead>
+        <tr>
         <th>Picture</th>
         <th>Name</th>
         <th>Popularity</th>
@@ -81,8 +87,8 @@ const handlesortName = () => {
         <th>Won Oscar</th>
         <th>Won Emmy</th>
         <th>Actions</th>
+        </tr>
 
-        </thead>
           <tbody>
             {contacts.map((contact) => (
 
@@ -91,8 +97,12 @@ const handlesortName = () => {
             <td>{contact.name}</td>
             <td> {contact.popularity.toFixed (2)}</td>
             {/* Iteration 2 */}
-            {contact.wonOscar ? <td>🏆</td> : <td></td>}
-            {contact.wonEmmy ? <td>🌟</td> : <td></td>}
+            <td>
+                { contact.wonOscar ? "🏆": "" }
+              </td>
+              <td>
+                { contact.wonEmmy  ? "🌟 ": "" }
+              </td>
             {/* Iteration 5 delete button*/}
             <td>
               <button onClick={() => deleteContact(contact.id)} className="btn-delete">Delete</button>
